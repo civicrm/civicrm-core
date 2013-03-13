75d4d9d7ad6f14010d0df14688e2803cde887501
yashodha <yashodha.chaku@webaccess.co.in>
2013-03-13 12:31:19 +0530

    CRM-9394

--- CRM/Report/Form/Contribute/Detail.php
+++ CRM/Report/Form/Contribute/Detail.php
@@ -206,7 +206,7 @@ class CRM_Report_Form_Contribute_Detail extends CRM_Report_Form {
           'currency' =>
           array('title' => 'Currency',
             'operatorType' => CRM_Report_Form::OP_MULTISELECT,
-            'options' => CRM_Core_PseudoConstant::currencySymbols('name','name'),
+            'options' => CRM_Core_OptionGroup::values('currencies_enabled'),
             'type' => CRM_Utils_Type::T_STRING,
           ),
           'financial_type_id'   =>
@@ -490,7 +490,6 @@ class CRM_Report_Form_Contribute_Detail extends CRM_Report_Form {
 
     $group = "\nGROUP BY {$this->_aliases['civicrm_contribution']}.currency";
     $sql = "{$select} {$this->_from} {$this->_where} {$group}";
-    crm_core_error::Debug( $sql);
     $dao = CRM_Core_DAO::executeQuery($sql);
 
     while ($dao->fetch()) {
