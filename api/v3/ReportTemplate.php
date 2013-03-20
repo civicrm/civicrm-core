1cbea43e879b79d8f5e2a7184920798f219273db
CiviCRM <info@civicrm.org>
2013-03-20 08:42:25 -0400

    CRM-12133 - api/v3/ReportTemplate - Fix option_group_id and component_id errors

--- api/v3/ReportTemplate.php
+++ api/v3/ReportTemplate.php
@@ -38,6 +38,9 @@ function civicrm_api3_report_template_create($params) {
   $params['option_group_id'] = CRM_Core_DAO::getFieldValue(
     'CRM_Core_DAO_OptionGroup', 'report_template', 'id', 'name'
   );
+  if (!empty($params['component_id']) && !is_numeric($params['component_id'])) {
+    $params['component_id'] = array_search($params['component_id'], CRM_Core_PseudoConstant::component());
+  }
   return civicrm_api3_option_value_create($params);
 }
 
@@ -52,6 +55,9 @@ function _civicrm_api3_report_template_create_spec(&$params) {
   _civicrm_api3_option_value_create_spec($params);
   $params['value']['api.aliases'] = array('report_url');
   $params['name']['api.aliases'] = array('class_name');
+  $params['option_group_id']['api.default'] = CRM_Core_DAO::getFieldValue(
+    'CRM_Core_DAO_OptionGroup', 'report_template', 'id', 'name'
+  );
   // $params['component']['api.required'] = TRUE;
 }
 
