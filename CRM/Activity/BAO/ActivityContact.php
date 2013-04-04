adc7f09e53577b4f88d105bcc4dbd75b1e93225a
Donald A. Lobo <lobo@civicrm.org>
2013-04-03 21:55:18 -0700

    first set of patches for activity restructuring

---
+++ CRM/Activity/BAO/ActivityContact.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 4.3                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2013                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License and the CiviCRM Licensing Exception along                  |
+ | with this program; if not, contact CiviCRM LLC                     |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC (c) 2004-2013
+ * $Id$
+ *
+ */
+
+/**
+ * This class is for activity assignment functions
+ *
+ */
+class CRM_Activity_BAO_ActivityContact extends CRM_Activity_DAO_ActivityContact {
+
+  /**
+   * class constructor
+   */
+  function __construct() {
+    parent::__construct();
+  }
+
+  /**
+   * funtion to add activity contact
+   *
+   * @param array  $params      the values for this table: activity id, contact id, record type
+   *
+   * @return object activity_contact object
+   * @access public
+   *
+   */
+  public static function create(&$params) {
+    $activityContact = new CRM_Activity_DAO_ActivityContact();
+
+    $activityContact->copyValues($params);
+    return $activityContact->save();
+  }
+}
