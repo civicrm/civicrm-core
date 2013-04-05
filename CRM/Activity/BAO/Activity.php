d65e1a68e90aaf5eb3296aabc623cff15bbe6f65
Thom Wilhelm <thom@futurefirst.org.uk>
2013-04-05 17:50:40 +0100

    Only send SMS if it is a mobile number, do not delete parent activity as it might be shared between many users

--- CRM/Activity/BAO/Activity.php
+++ CRM/Activity/BAO/Activity.php
@@ -1515,7 +1515,12 @@ LEFT JOIN   civicrm_case_activity ON ( civicrm_case_activity.activity_id = tbl.a
       $tokenHtml = CRM_Utils_Token::replaceContactTokens($html, $values, TRUE, $messageToken, FALSE, $escapeSmarty);
       $tokenHtml = CRM_Utils_Token::replaceHookTokens($tokenHtml, $values, $categories, TRUE, $escapeSmarty);
 
-      $smsParams['To'] = $values['phone'];
+      // Only send if the phone is of type mobile
+      if($values['phone_type_id'] == 2) {
+        $smsParams['To'] = $values['phone'];
+      }else{
+        $smsParams['To'] = '';
+      }
 
       if (self::sendSMSMessage($contactId,
           $tokenText,
