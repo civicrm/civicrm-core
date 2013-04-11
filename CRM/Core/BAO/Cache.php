d35e469a260e2058884c3b4bd1e2fac2b7c9a032
Tim Otten <totten@civicrm.org>
2013-04-11 00:00:13 -0400

    CRM-12321 - Revert "cache civicrm_cache table"
    This reverts commit d9d00494d13ec66856526ac2426abaecf8cab257.

--- CRM/Core/BAO/Cache.php
+++ CRM/Core/BAO/Cache.php
@@ -58,32 +58,18 @@ class CRM_Core_BAO_Cache extends CRM_Core_DAO_Cache {
    * @access public
    */
   static function &getItem($group, $path, $componentID = NULL) {
-    static $_cache = NULL;
-    if ($_cache === NULL) {
-      $_cache = array();
-    }
-    
-    $argString = "CRM_CT_{$group}_{$path}_{$componentID}";
-    if (!array_key_exists($argString, $_cache)) {
-      $cache = CRM_Utils_Cache::singleton();
-      $_cache[$argString] = $cache->get($argString);
-      if (!$_cache[$argString]) {
-        $dao = new CRM_Core_DAO_Cache();
+    $dao = new CRM_Core_DAO_Cache();
 
-        $dao->group_name   = $group;
-        $dao->path         = $path;
-        $dao->component_id = $componentID;
+    $dao->group_name   = $group;
+    $dao->path         = $path;
+    $dao->component_id = $componentID;
 
-        $data = NULL;
-        if ($dao->find(TRUE)) {
-          $data = unserialize($dao->data);
-        }
-        $dao->free();
-        $_cache[$argString] = $data;
-        $cache->set($argString, $_cache[$argString]);
-      }
+    $data = NULL;
+    if ($dao->find(TRUE)) {
+      $data = unserialize($dao->data);
     }
-    return $_cache[$argString];
+    $dao->free();
+    return $data;
   }
 
   /**
@@ -97,34 +83,18 @@ class CRM_Core_BAO_Cache extends CRM_Core_DAO_Cache {
    * @access public
    */
   static function &getItems($group, $componentID = NULL) {
-    static $_cache = NULL;
-    if ($_cache === NULL) {
-      $_cache = array();
+    $dao = new CRM_Core_DAO_Cache();
+
+    $dao->group_name   = $group;
+    $dao->component_id = $componentID;
+    $dao->find();
+
+    $result = array(); // array($path => $data)
+    while ($dao->fetch()) {
+      $result[$dao->path] = unserialize($dao->data);
     }
-    
-    $argString = "CRM_CT_CI_{$group}_{$componentID}";
-    if (!array_key_exists($argString, $_cache)) {
-      $cache = CRM_Utils_Cache::singleton();
-      $_cache[$argString] = $cache->get($argString);
-      if (!$_cache[$argString]) {
-        $dao = new CRM_Core_DAO_Cache();
-
-        $dao->group_name   = $group;
-        $dao->component_id = $componentID;
-        $dao->find();
-
-        $result = array(); // array($path => $data)
-        while ($dao->fetch()) {
-          $result[$dao->path] = unserialize($dao->data);
-        }
-        $dao->free();
-        
-        $_cache[$argString] = $result;
-        $cache->set($argString, $_cache[$argString]);
-      }
-    }
-
-    return $_cache[$argString];
+    $dao->free();
+    return $result;
   }
 
   /**
@@ -140,11 +110,6 @@ class CRM_Core_BAO_Cache extends CRM_Core_DAO_Cache {
    * @access public
    */
   static function setItem(&$data, $group, $path, $componentID = NULL) {
-    static $_cache = NULL;
-    if ($_cache === NULL) {
-      $_cache = array();
-    }
-
     $dao = new CRM_Core_DAO_Cache();
 
     $dao->group_name   = $group;
@@ -168,11 +133,6 @@ class CRM_Core_BAO_Cache extends CRM_Core_DAO_Cache {
     $lock->release();
 
     $dao->free();
-
-    // set the cache in memory
-    $argString = "CRM_CT_{$group}_{$path}_{$componentID}";
-    $cache = CRM_Utils_Cache::singleton();
-    $cache->set($argString, $dao->data);
   }
 
   /**
