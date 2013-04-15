fa6bfe1b7f89d5214c442d978e831f80d63c70bf
kurund <kurund@civicrm.org>
2013-04-16 03:17:21 +0530

    worked on CRM-12235, added missing feature of inline edit for multi-value custom data
    ----------------------------------------
    * CRM-12235: Inline edit of multi-record custom fields broken
      http://issues.civicrm.org/jira/browse/CRM-12235

--- CRM/Contact/Form/Inline/CustomData.php
+++ CRM/Contact/Form/Inline/CustomData.php
@@ -61,9 +61,9 @@ class CRM_Contact_Form_Inline_CustomData extends CRM_Contact_Form_Inline {
 
     $this->_groupID = CRM_Utils_Request::retrieve('groupID', 'Positive', $this, TRUE, NULL, $_REQUEST);
     $this->assign('customGroupId', $this->_groupID);
-
+    $customRecId = CRM_Utils_Request::retrieve('customRecId', 'Positive', $this, FALSE, 1, $_REQUEST);
     $subType = CRM_Contact_BAO_Contact::getContactSubType($this->_contactId, ',');
-    CRM_Custom_Form_CustomData::preProcess($this, null, $subType, 1,
+    CRM_Custom_Form_CustomData::preProcess($this, null, $subType, $customRecId,
       $this->_contactType, $this->_contactId);
   }
 
