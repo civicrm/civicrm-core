091fe2a8036531e96097d16caffc50c4b2f91b3e
Coleman Watts <coleman@civicrm.org>
2013-05-01 18:54:32 -0700

    Support contact_type and sub_type CRM-12464
    ----------------------------------------
    * CRM-12464: Search improvements in 4.4
      http://issues.civicrm.org/jira/browse/CRM-12464

--- tests/phpunit/CRM/Core/PseudoConstantTest.php
+++ tests/phpunit/CRM/Core/PseudoConstantTest.php
@@ -44,11 +44,14 @@ class CRM_Core_PseudoConstantTest extends CiviUnitTestCase {
   }
 
   function testOptionValues() {
-    // We'll test these daoName/field combinations.
-    // array[DAO Name] = properties, where properties can be:
-    // - fieldName: the SQL column name within the DAO table.
-    // - sample: Any one value which is expected in the list of option values.
-    // - max: integer (default = 10) maximum number of option values expected.
+    /*
+     * daoName/field combinations to test
+     * array[DAO Name] = properties, where properties can be:
+     * - fieldName: the SQL column name within the DAO table.
+     * - sample: Any one value which is expected in the list of option values.
+     * - exclude: Any one value which should not be in the list.
+     * - max: integer (default = 10) maximum number of option values expected.
+     */
     $fields = array(
       'CRM_Core_DAO_OptionValue' => array(
         array(
@@ -148,6 +151,16 @@ class CRM_Core_PseudoConstantTest extends CiviUnitTestCase {
           'fieldName' => 'preferred_communication_method',
           'sample' => 'Postal Mail',
         ),
+        array(
+          'fieldName' => 'contact_type',
+          'sample' => 'Individual',
+          'exclude' => 'Team',
+        ),
+        array(
+          'fieldName' => 'contact_sub_type',
+          'sample' => 'Team',
+          'exclude' => 'Individual',
+        ),
       ),
       'CRM_Batch_DAO_Batch' => array(
         array(
@@ -175,14 +188,43 @@ class CRM_Core_PseudoConstantTest extends CiviUnitTestCase {
       foreach ($daoFields as $field) {
         $message = "DAO name: '{$daoName}', field: '{$field['fieldName']}'";
 
-        // Ensure sample value is contained in the returned optionValues.
         $optionValues = CRM_Core_PseudoConstant::get($daoName, $field['fieldName']);
+        $this->assertNotEmpty($optionValues, $message);
+
+        // Ensure sample value is contained in the returned optionValues.
         $this->assertContains($field['sample'], $optionValues, $message);
+
+        // Exclude test
+        if (!empty($field['exclude'])) {
+          $this->assertNotContains($field['exclude'], $optionValues, $message);
+        }
 
         // Ensure count of optionValues is not extraordinarily high.
         $max = CRM_Utils_Array::value('max', $field, 10);
         $this->assertLessThanOrEqual($max, count($optionValues), $message);
       }
     }
+  }
+
+  function testContactTypes() {
+    $byName = array(
+      'Individual' => 'Individual',
+      'Household' => 'Household',
+      'Organization' => 'Organization',
+    );
+    $byId = array(
+      1 => 'Individual',
+      2 => 'Household',
+      3 => 'Organization',
+    );
+    // By default this should return by name
+    $result = CRM_Core_PseudoConstant::get('CRM_Contact_DAO_Contact', 'contact_type');
+    $this->assertEquals($byName, $result);
+    // But we can also fetch by ID
+    $result = CRM_Core_PseudoConstant::get('CRM_Contact_DAO_Contact', 'contact_type', array('keyColumn' => 'id', 'labelColumn' => 'name'));
+    $this->assertEquals($byId, $result);
+    // Make sure flip param works
+    $result = CRM_Core_PseudoConstant::get('CRM_Contact_DAO_Contact', 'contact_type', array('keyColumn' => 'id', 'labelColumn' => 'name', 'flip' => TRUE));
+    $this->assertEquals(array_flip($byId), $result);
   }
 }
