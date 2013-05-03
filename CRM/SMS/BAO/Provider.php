503fc58c222b0bd2de1371ba76c06495ed9d28c7
mmikitka <matt@mikitka.net>
2013-05-03 00:26:46 -0400

    Load the api_type string value in getProviderInfo. Fixed a typo bug in the Clickatell and Twilio files.

--- CRM/SMS/BAO/Provider.php
+++ CRM/SMS/BAO/Provider.php
@@ -127,6 +127,11 @@ class CRM_SMS_BAO_Provider extends CRM_SMS_DAO_Provider {
           }
         }
         $providerInfo[$providerID]['api_params'] = $inputVals;
+
+        // Replace the api_type ID with the string value
+        $apiTypes = CRM_Core_OptionGroup::values('sms_api_type');
+        $apiTypeId = $providerInfo[$providerID]['api_type'];
+        $providerInfo[$providerID]['api_type'] = CRM_Utils_Array::value($apiTypeId, $apiTypes, $apiTypeId);
       }
     }
 
