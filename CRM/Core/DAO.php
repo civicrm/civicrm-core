265469e44a0073045426bfc9551ca4a179930f2b
yashodha <yashodha.chaku@webaccess.co.in>
2013-05-09 02:43:15 +0530

    CRM-12458

--- CRM/Core/DAO.php
+++ CRM/Core/DAO.php
@@ -589,18 +589,17 @@ LIKE %1
     static $show = array();
 
     if (!array_key_exists($tableName, $show)) {
-      $query = "SHOW CREATE TABLE $tableName";
-      $dao = CRM_Core_DAO::executeQuery($query);
+      $query = "SELECT CONSTRAINT_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE TABLE_NAME = %1";
+      $params = array( 1 => array($tableName, 'String'));
+      $dao = CRM_Core_DAO::executeQuery($query, $params);
 
-      if (!$dao->fetch()) {
-        CRM_Core_Error::fatal();
+      while ($dao->fetch()) {
+        $show[$tableName][] = $dao->CONSTRAINT_NAME;
       }
-
       $dao->free();
-      $show[$tableName] = $dao->Create_Table;
     }
 
-    return preg_match("/\b$constraint\b/i", $show[$tableName]) ? TRUE : FALSE;
+    return in_array($constraint, $show[$tableName]) ? TRUE : FALSE;
   }
 
   /**
