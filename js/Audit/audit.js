92385fb86e586e8f49641fa137b72e4467114719
eileen <eileen@fuzion.co.nz>
2013-05-13 14:28:03 +1200

    CRM-12595 fix formatting in js files

--- js/Audit/audit.js
+++ js/Audit/audit.js
@@ -26,28 +26,28 @@
 
 function selectActivity(i)
 {
-	// deselect current selection
-	j = document.forms["Report"].currentSelection.value;
-	ele = document.getElementById("civicase-audit-activity-" + j);
-	ele.className = "activity";
-	ele = document.getElementById("civicase-audit-header-" + j);
-	ele.style.display = "none";
-	ele = document.getElementById("civicase-audit-body-" + j);
-	ele.style.display = "none";
+  // deselect current selection
+  j = document.forms["Report"].currentSelection.value;
+  ele = document.getElementById("civicase-audit-activity-" + j);
+  ele.className = "activity";
+  ele = document.getElementById("civicase-audit-header-" + j);
+  ele.style.display = "none";
+  ele = document.getElementById("civicase-audit-body-" + j);
+  ele.style.display = "none";
 
-	if ( i != 1 ) {
-	    cj('#civicase-audit-activity-1').removeClass('activity selected');
-	    cj('#civicase-audit-activity-1').addClass('activity');
-	    cj('#civicase-audit-header-1').css({"display":"none"});
-	    cj('#civicase-audit-body-1').css({"display":"none"});
+  if ( i != 1 ) {
+      cj('#civicase-audit-activity-1').removeClass('activity selected');
+      cj('#civicase-audit-activity-1').addClass('activity');
+      cj('#civicase-audit-header-1').css({"display":"none"});
+      cj('#civicase-audit-body-1').css({"display":"none"});
         }
 
-	// select selected one
-	ele = document.getElementById("civicase-audit-activity-" + i);
-	ele.className = "activity selected";
-	ele = document.getElementById("civicase-audit-header-" + i);
-	ele.style.display = "block";
-	ele = document.getElementById("civicase-audit-body-" + i);
-	ele.style.display = "block";
-	document.forms["Report"].currentSelection.value = i;
+  // select selected one
+  ele = document.getElementById("civicase-audit-activity-" + i);
+  ele.className = "activity selected";
+  ele = document.getElementById("civicase-audit-header-" + i);
+  ele.style.display = "block";
+  ele = document.getElementById("civicase-audit-body-" + i);
+  ele.style.display = "block";
+  document.forms["Report"].currentSelection.value = i;
 }
