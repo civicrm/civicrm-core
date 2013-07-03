e5e3f1add872bdc1d248e83ab1787e718e1c08dd
Ravish Nair <ravish.nair@webaccess.co.in>
2013-07-03 12:50:32 +0530

    -- CRM-12941, CRM-12863 & CRM-12824 changes made as per the comments
    ----------------------------------------
    * CRM-12941: Redirect to proper contact tab after saving profile
      http://issues.civicrm.org/jira/browse/CRM-12941
    * CRM-12863: Hide profile titles in HR tabs
      http://issues.civicrm.org/jira/browse/CRM-12863
    * CRM-12824: Override message for empty tabs
      http://issues.civicrm.org/jira/browse/CRM-12824

--- CRM/Profile/Form/Edit.php
+++ CRM/Profile/Form/Edit.php
@@ -62,8 +62,7 @@ class CRM_Profile_Form_Edit extends CRM_Profile_Form {
   function preProcess() {
     $this->_mode = CRM_Profile_Form::MODE_CREATE;
 
-    $this->_tabDisplay = CRM_Utils_Request::retrieve('tabDisplay', 'String', $this);
-    $this->assign('tabDisplay', $this->_tabDisplay);
+    $this->_onPopupClose = CRM_Utils_Request::retrieve('onPopupClose', 'String', $this);
 
     //set the context for the profile
     $this->_context = CRM_Utils_Request::retrieve('context', 'String', $this);
@@ -217,7 +216,7 @@ SELECT module
         //proper redirection and proccess form errors if any
         $popupRedirect = CRM_Utils_System::url('civicrm/profile/edit', $urlParams, FALSE, NULL, FALSE);
 
-        if ($this->_tabDisplay) {
+        if ($this->_onPopupClose == 'redirectToTab') {
           $popupRedirect = CRM_Utils_System::url('civicrm/contact/view', 
             "reset=1&cid={$this->_id}&selectedChild=custom_{$this->_customGroupId}", FALSE, NULL, FALSE);
         }
