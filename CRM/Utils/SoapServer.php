b6c54d162343d59baf40c7720eb4275509efc3cf
Pratik Joshi <pratik.joshi@webaccess.co.in>
2013-07-08 13:38:17 +0530

    CRM-13007 fix

--- CRM/Utils/SoapServer.php
+++ CRM/Utils/SoapServer.php
@@ -124,9 +124,9 @@ class CRM_Utils_SoapServer {
    */
   public function authenticate($name, $pass, $loadCMSBootstrap = FALSE) {
     require_once (str_replace('_', DIRECTORY_SEPARATOR, $this->ufClass) . '.php');
-        
-    if ($this->ufClass == 'CRM_Utils_System_Joomla'){
-      $loadCMSBootstrap = true;
+
+    if ($this->ufClass == 'CRM_Utils_System_Joomla' || $this->ufClass == 'CRM_Utils_System_WordPress'){
+      $loadCMSBootstrap = TRUE;
     }
         
     eval('$result =& ' . $this->ufClass . '::authenticate($name, $pass, $loadCMSBootstrap );');
