civicrm/civicrm-core

commit 8bab0eb08b9862b49b02e914321bd15c0aca487e

CRM-12172
----------------------------------------
* CRM-12172: Use preg_replace_callback() in CRM_Utils_Token
  http://issues.civicrm.org/jira/browse/CRM-12172

CRM/Utils/Token.php
@@ -192,7 +192,7 @@ class CRM_Utils_Token {
    * @static
    */
   private static function tokenRegex($token_type) {
-    return '/(?<!\{|\\\\)\{' . $token_type . '\.([\w]+(\-[\w\s]+)?)\}(?!\})/e';
+    return '/(?<!\{|\\\\)\{' . $token_type . '\.([\w]+(\-[\w\s]+)?)\}(?!\})/';
   }
 
   /**
@@ -236,9 +236,11 @@ class CRM_Utils_Token {
       return $str;
     }
 
-    $str = preg_replace(
+    $str = preg_replace_callback(
       self::tokenRegex($key),
-      'self::getDomainTokenReplacement(\'\\1\',$domain,$html)',
+      function ($matches) use(&$domain, $html, $escapeSmarty) {
+        return CRM_Utils_Token::getDomainTokenReplacement($matches[1], $domain, $html, $escapeSmarty);
+      },
       $str
     );
     return $str;
@@ -399,9 +401,12 @@ class CRM_Utils_Token {
       return $str;
     }
 
-    $str = preg_replace(
+    $str = preg_replace_callback(
       self::tokenRegex($key),
-      'self::getMailingTokenReplacement(\'\\1\',$mailing,$escapeSmarty)', $str
+      function ($matches) use(&$mailing, $escapeSmarty) {
+        return CRM_Utils_Token::getMailingTokenReplacement($matches[1], $mailing, $escapeSmarty);
+      },
+      $str
     );
     return $str;
   }
@@ -519,8 +524,11 @@ class CRM_Utils_Token {
       return $str;
     }
 
-    $str = preg_replace(self::tokenRegex($key),
-      'self::getActionTokenReplacement(\'\\1\',$addresses,$urls,$escapeSmarty)',
+    $str = preg_replace_callback(
+      self::tokenRegex($key),
+      function ($matches) use(&$addresses, &$urls, $html, $escapeSmarty) {
+        return CRM_Utils_Token::getActionTokenReplacement($matches[1], $addresses, $urls, $html, $escapeSmarty);
+      },
       $str
     );
     return $str;
@@ -604,9 +612,11 @@ class CRM_Utils_Token {
       return $str;
     }
 
-    $str = preg_replace(
+    $str = preg_replace_callback(
       self::tokenRegex($key),
-      'self::getContactTokenReplacement(\'\\1\', $contact, $html, $returnBlankToken, $escapeSmarty)',
+      function ($matches) use(&$contact, $key, $html, $escapeSmarty) {
+        return CRM_Utils_Token::getHookTokenReplacement($matches[1], $contact, $key, $html, $escapeSmarty);
+      },
       $str
     );
 
@@ -692,9 +702,11 @@ class CRM_Utils_Token {
     $escapeSmarty = FALSE
   ) {
     foreach ($categories as $key) {
-      $str = preg_replace(
+      $str = preg_replace_callback(
         self::tokenRegex($key),
-        'self::getHookTokenReplacement(\'\\1\', $contact, $key, $html, $escapeSmarty)',
+        function ($matches) use(&$contact, $key, $html, $escapeSmarty) {
+          return CRM_Utils_Token::getHookTokenReplacement($matches[1], $contact, $key, $html, $escapeSmarty);
+        },
         $str
       );
     }
@@ -1236,8 +1248,12 @@ class CRM_Utils_Token {
       return $str;
     }
 
-    $str = preg_replace(self::tokenRegex($key),
-      'self::getUserTokenReplacement(\'\\1\',$escapeSmarty)', $str
+    $str = preg_replace_callback(
+      self::tokenRegex($key),
+      function ($matches) use($escapeSmarty) {
+        return CRM_Utils_Token::getUserTokenReplacement($matches[1], $escapeSmarty);
+      },
+      $str
     );
     return $str;
   }
@@ -1286,8 +1302,11 @@ class CRM_Utils_Token {
       return $str;
     }
 
-    $str = preg_replace(self::tokenRegex($key),
-      'self::getContributionTokenReplacement(\'\\1\', $contribution, $html, $escapeSmarty)',
+    $str = preg_replace_callback(
+      self::tokenRegex($key),
+      function ($matches) use(&$contribution, $html, $escapeSmarty) {
+        return CRM_Utils_Token::getContributionTokenReplacement($matches[1], $contribution, $html, $escapeSmarty);
+      },
       $str
     );
 
@@ -1332,4 +1351,3 @@ class CRM_Utils_Token {
 
   function getRoleEmails($roleName) {}
 }
-

commit 6d3c1eb9d682775ed2689cbfac9a0046eaef62be

Amend CRM-12995 fix
The regression introduced in 8bab0eb08b9862b49b02e914321bd15c0aca487e
(mismerge of patch for http://issues.civicrm.org/jira/browse/CRM-12172 )
was fixed in 14c8cdaa39005e0752e8beca670faa470b2147ce only partially.

This commit adds the rest of the necessary fix to fully undo the
original regression.

(Note: I have not really tested the effect of the amended fix, as I
don't know in what situations it shows -- I only discovered this in the
code while looking for another regression... It's a pretty obvious
change though.)

CRM/Utils/Token.php
@@ -613,8 +613,8 @@ class CRM_Utils_Token {
 
     $str = preg_replace_callback(
       self::tokenRegex($key),
-      function ($matches) use(&$contact, $key, $html, $escapeSmarty) {
-        return CRM_Utils_Token::getContactTokenReplacement($matches[1], $contact, $key, $html, $escapeSmarty);
+      function ($matches) use(&$contact, $html, $returnBlankToken, $escapeSmarty) {
+        return CRM_Utils_Token::getContactTokenReplacement($matches[1], $contact, $html, $returnBlankToken, $escapeSmarty);
       },
       $str
     );