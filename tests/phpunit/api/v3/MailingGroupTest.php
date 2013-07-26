7f0eb6cb13b514cfb512598434b9ec88119497d0
eileen <eileen@fuzion.co.nz>
2013-07-26 20:33:13 +1200

    CRM-13072 upgrade mailing group test

--- tests/phpunit/api/v3/MailingGroupTest.php
+++ tests/phpunit/api/v3/MailingGroupTest.php
@@ -65,9 +65,7 @@ class api_v3_MailingGroupTest extends CiviUnitTestCase {
     $params = array(
       'email' => $this->_email,
       'group_id' => 'Wrong Group ID',
-      'contact_id' => '2121',
-      'version' => $this->_apiversion,
-      'time_stamp' => '20111111010101',
+      'contact_id' => '2121',      'time_stamp' => '20111111010101',
       'hash' => 'sasa',
     );
     $result = $this->callAPIFailure('mailing_event_subscribe', 'create', $params);
@@ -87,22 +85,16 @@ class api_v3_MailingGroupTest extends CiviUnitTestCase {
       'first_name' => 'Test',
       'last_name' => 'Test',
       'email' => $this->_email,
-      'contact_type' => 'Individual',
-      'version' => $this->_apiversion,
-    );
+      'contact_type' => 'Individual',    );
     $contactID = $this->individualCreate($params);
 
     $params = array(
       'email' => $this->_email,
       'group_id' => $this->_groupID,
-      'contact_id' => $contactID,
-      'version' => $this->_apiversion,
-      'hash' => 'b15de8b64e2cec34',
+      'contact_id' => $contactID,      'hash' => 'b15de8b64e2cec34',
       'time_stamp' => '20101212121212',
     );
-    $result = civicrm_api('mailing_event_subscribe', 'create', $params);
-    $this->documentMe($params, $result, __FUNCTION__, __FILE__);
-    $this->assertAPISuccess($result, 'in line ' . __LINE__);
+    $result = $this->callAPIAndDocument('mailing_event_subscribe', 'create', $params, __FUNCTION__, __FILE__);
     $this->assertEquals($result['values']['contact_id'], $contactID);
 
     $this->contactDelete($contactID);
@@ -117,9 +109,7 @@ class api_v3_MailingGroupTest extends CiviUnitTestCase {
     $params = array(
       'job_id' => 'Wrong ID',
       'event_queue_id' => 'Wrong ID',
-      'hash' => 'Wrong Hash',
-      'version' => $this->_apiversion,
-      'time_stamp' => '20101212121212',
+      'hash' => 'Wrong Hash',      'time_stamp' => '20101212121212',
     );
 
     $result = $this->callAPIFailure('mailing_event_unsubscribe', 'create', $params);
@@ -136,9 +126,7 @@ class api_v3_MailingGroupTest extends CiviUnitTestCase {
       'job_id' => 'Wrong ID',
       'event_queue_id' => 'Wrong ID',
       'hash' => 'Wrong Hash',
-      'org_unsubscribe' => 1,
-      'version' => $this->_apiversion,
-      'time_stamp' => '20101212121212',
+      'org_unsubscribe' => 1,      'time_stamp' => '20101212121212',
     );
 
     $result = $this->callAPIFailure('mailing_event_unsubscribe', 'create', $params);
@@ -160,9 +148,7 @@ class api_v3_MailingGroupTest extends CiviUnitTestCase {
       'job_id' => 'Wrong ID',
       'event_queue_id' => 'Wrong ID',
       'hash' => 'Wrong Hash',
-      'org_unsubscribe' => 'test',
-      'version' => $this->_apiversion,
-      'time_stamp' => '20101212121212',
+      'org_unsubscribe' => 'test',      'time_stamp' => '20101212121212',
     );
     $result = $this->callAPIFailure('mailing_event_resubscribe', 'create', $params);
     $this->assertEquals($result['error_message'], 'Queue event could not be found', 'In line ' . __LINE__);
@@ -178,20 +164,16 @@ class api_v3_MailingGroupTest extends CiviUnitTestCase {
       'first_name' => 'Test',
       'last_name' => 'Test',
       'email' => $this->_email,
-      'contact_type' => 'Individual',
-      'version' => $this->_apiversion,
-    );
+      'contact_type' => 'Individual',    );
     $contactID = $this->individualCreate($params);
 
     $params = array(
       'email' => $this->_email,
       'group_id' => $this->_groupID,
-      'contact_id' => $contactID,
-      'version' => $this->_apiversion,
-      'hash' => 'b15de8b64e2cec34',
+      'contact_id' => $contactID,      'hash' => 'b15de8b64e2cec34',
       'time_stamp' => '20101212121212',
     );
-    $result = civicrm_api('mailing_event_subscribe', 'create', $params);
+    $result = $this->callAPISuccess('mailing_event_subscribe', 'create', $params);
 
     $this->assertAPISuccess($result, 'in line ' . __LINE__);
     $this->assertEquals($result['values']['contact_id'], $contactID);
@@ -199,14 +181,12 @@ class api_v3_MailingGroupTest extends CiviUnitTestCase {
     $params = array(
       'contact_id' => $result['values']['contact_id'],
       'subscribe_id' => $result['values']['subscribe_id'],
-      'hash' => $result['values']['hash'],
-      'version' => $this->_apiversion,
-      'time_stamp' => '20101212121212',
+      'hash' => $result['values']['hash'],      'time_stamp' => '20101212121212',
       'event_subscribe_id' => $result['values']['subscribe_id'],
     );
 
 
-    $result = civicrm_api('mailing_event_confirm', 'create', $params);
+    $result = $this->callAPISuccess('mailing_event_confirm', 'create', $params);
 
     $this->assertAPISuccess($result, 'in line ' . __LINE__);
     $this->contactDelete($contactID);
