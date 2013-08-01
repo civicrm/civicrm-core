bcbde5ca5f72870ec96b19ce0bcea33ea39d0d4a
Aaditya Walawalkar <aaditya.walawalkar@webaccess.co.in>
2013-08-01 20:19:45 +0530

    CRM-12439 fix with webtest - Added description textarea and exposed created_id and description fields in profile listing page
    ----------------------------------------
    * CRM-12439: Add description field for Profiles and expose the created_id field
      http://issues.civicrm.org/jira/browse/CRM-12439
    * CRM-12439-org:
      http://issues.civicrm.org/jira/browse/CRM-12439-org
    * CRM-12995-1:
      http://issues.civicrm.org/jira/browse/CRM-12995-1
    * CRM-12995-2:
      http://issues.civicrm.org/jira/browse/CRM-12995-2

--- CRM/UF/Page/Group.php
+++ CRM/UF/Page/Group.php
@@ -305,6 +305,9 @@ class CRM_UF_Page_Group extends CRM_Core_Page {
       $ufGroup[$id] = array();
       $ufGroup[$id]['id'] = $id;
       $ufGroup[$id]['title'] = $value['title'];
+      $ufGroup[$id]['created_id'] = $value['created_id'];
+      $ufGroup[$id]['created_by'] = CRM_Contact_BAO_Contact::displayName($value['created_id']);
+      $ufGroup[$id]['description'] = $value['description'];
       $ufGroup[$id]['is_active'] = $value['is_active'];
       $ufGroup[$id]['group_type'] = $value['group_type'];
       $ufGroup[$id]['is_reserved'] = $value['is_reserved'];
