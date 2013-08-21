c02f38333cb8c0f11c4b7eaad1a14c1c17e99c19
yashodha <yashodha.chaku@webaccess.co.in>
2013-08-21 14:54:44 +0530

    fix the broken webtest (CRM-13205)

--- tests/phpunit/WebTest/Contact/AdvanceSearchPaneTest.php
+++ tests/phpunit/WebTest/Contact/AdvanceSearchPaneTest.php
@@ -196,7 +196,7 @@ class WebTest_Contact_AdvanceSearchPaneTest extends CiviSeleniumTestCase {
         'activity' =>
         array(
           'headerLocator' => 'div#activity',
-          'bodyLocator' => 'input#activity_contact_name',
+          'bodyLocator' => 'input#activity_subject',
           'title' => 'Activities',
           'fields' =>
           array(
