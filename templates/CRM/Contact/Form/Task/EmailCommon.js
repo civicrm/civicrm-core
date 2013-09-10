50688277aab75bb444160f4a789e8fccf4e09e54
Eileen McNaughton <eileen@mcnaughty.com>
2013-09-11 08:58:33 +1200

    minor changes per Coleman's comment

--- templates/CRM/Contact/Form/Task/EmailCommon.js
+++ templates/CRM/Contact/Form/Task/EmailCommon.js
@@ -1,8 +1,9 @@
 cj(function ($) {
+  //do not copy & paste this - find a way to generalise it
   'use strict';
    $().crmAccordions();
    $('.form-submit').on("click", function(event){
-     $('.form-submit').attr('value','Processing');
-     $('.form-submit').attr('disabled','Disabled');
+     $('.form-submit').attr('value',ts('Processing'));
+     $('.form-submit').attr('disabled','disabled');
    });
 });
