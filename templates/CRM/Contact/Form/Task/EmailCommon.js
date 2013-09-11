55d99497e6d3cf9e45b8bc44d8211a47d857fc7b
Coleman Watts <coleman@civicrm.org>
2013-09-11 09:09:58 -0500

    fix js typo CRM-13331
    ----------------------------------------
    * CRM-13331: emails can be 'double-sent' from search action
      http://issues.civicrm.org/jira/browse/CRM-13331

--- templates/CRM/Contact/Form/Task/EmailCommon.js
+++ templates/CRM/Contact/Form/Task/EmailCommon.js
@@ -2,7 +2,7 @@ cj(function ($) {
   //do not copy & paste this - find a way to generalise it
   'use strict';
    $().crmAccordions();
-   $('.form-submit').on("click", function(event){
-     $('.form-submit').attr({value: ts('Processing'), disabled: 'disabled});
+   $('.form-submit').on("click", function() {
+     $('.form-submit').attr({value: ts('Processing'), disabled: 'disabled'});
    });
 });
