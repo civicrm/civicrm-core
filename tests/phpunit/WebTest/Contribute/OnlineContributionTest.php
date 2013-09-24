ac6e0435461f4e691bedbbf27a9af780b4df194c
Pratik Joshi <pratik.joshi@webaccess.co.in>
2013-09-24 18:01:23 +0530

    fix for webtest : WebTest_Contribute_OnlineContributionTest::testOnlineContributionAdd

--- tests/phpunit/WebTest/Contribute/OnlineContributionTest.php
+++ tests/phpunit/WebTest/Contribute/OnlineContributionTest.php
@@ -105,7 +105,7 @@ class WebTest_Contribute_OnlineContributionTest extends CiviSeleniumTestCase {
     $this->select("state_province-1", "value=1001");
 
     // Honoree Info
-    $this->click("xpath=id('Main')/x:div[2]/x:fieldset/x:div[2]/x:div/x:label[text()='In Honor of']");
+    $this->click("xpath=id('Main')/div[2]/fieldset/div[2]/div/label[text()='In Honor of']");
     $this->waitForElementPresent("honor_email");
 
     $this->select("honor_prefix_id", "label=Ms.");
