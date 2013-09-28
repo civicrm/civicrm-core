e0686b4a7ee8c2c537c00a14238db420b59ab239
Peter Haight <peterh@giantrabbit.com>
2013-09-28 13:31:29 -0700

    Switch test runner to use Console_Getopt.
    Lobo wanted to avoid adding the optionparser package to the packages
    directory because Console_Getopt duplicates some of that functionality.
    I made a new class called CRM_Utils_CommandLineParser which adds some of
    the extra functionality I wanted from optionparser on top of
    Console_Getopt.

--- tests/run.php
+++ tests/run.php
@@ -5,43 +5,34 @@ $civicrm_source_dir_path = dirname(__DIR__);
 $autoloader_path = implode(DIRECTORY_SEPARATOR, array('CRM', 'Core', 'ClassLoader.php'));
 require_once($autoloader_path);
 CRM_Core_ClassLoader::singleton()->register();
-
 $packages_path = CRM_Utils_Path::join($civicrm_source_dir_path, 'packages');
 if (!file_exists($packages_path)) {
   system("git clone https://github.com/giant-rabbit/civicrm-packages.git packages");
 }
+set_include_path(get_include_path() . PATH_SEPARATOR . $packages_path);
+require_once(CRM_Utils_Path::join($civicrm_source_dir_path, "packages", "Console", "Getopt.php"));
 
-require_once(CRM_Utils_Path::join($civicrm_source_dir_path, "packages", "optionparser", "lib", "OptionParser.php"));
-
-$option_parser = new OptionParser();
-$option_parser->addRule('h|help', 'Display this help message');
-$option_parser->addRule('m|mysql-ram-server', 'Start a MySQL daemon backed by a RAM disk that will be used for the tests');
-$option_parser->addRule('s|no-selenium', 'Don\'t try and use Selenium.');
-$option_parser->addRule('p|php-unit::', 'Arguments to be passed to phpunit. If there are any spaces, enclose this whole option value in single quotes.');
-$option_parser->addRule('c|clean', 'Clean up any daemons and files that running the tests created.');
-
-function usage($option_parser) {
-  $stderr = fopen('php://stderr', 'w');
-  fwrite($stderr, "Usage: " . $option_parser->getProgramName() . " [options]\n");
-  fwrite($stderr, $option_parser->getUsage());
-}
+$option_parser = new CRM_Utils_CommandLineParser();
+$option_parser->addOption('h', 'help', 'n', 'Display this help message');
+$option_parser->addOption('m', 'mysql-ram-server', 'n', 'Start a MySQL daemon backed by a RAM disk that will be used for the tests');
+$option_parser->addOption('s', 'no-selenium', 'n', "Don't try and use Selenium.");
+$option_parser->addOption('p', 'php-unit', 'r', 'Arguments to be passed to phpunit. If there are any spaces, enclose ARG in single quotes.');
+$option_parser->addOption('c', 'clean', 'n', 'Clean up any daemons and files that running the tests created.');
 
 try {
-  $option_parser->parse();
-  $options = $option_parser->getAllOptions();
+  $option_parser->parse($argv);
+  $options = $option_parser->options;
   $options['base_path'] = $civicrm_source_dir_path;
   $tests = new CRM_Tests_Runner($options);
-  if ($option_parser->help) {
-    usage($option_parser);
-  } elseif ($option_parser->clean) {
+  if (CRM_Utils_Array::fetch('help', $options, FALSE)) {
+    $option_parser->printUsage();
+  } elseif (CRM_Utils_Array::fetch('clean', $options, FALSE)) {
     $tests->clean();
   } else {
     $tests->run();
   }
-} catch (InvalidOption $e) {
-  $stderr = fopen('php://stderr', 'w');
-  fwrite($stderr, $e->getMessage() . "\n");
-  fwrite($stderr, "\n");
-  usage($option_parser);
+} catch (CRM_Utils_CommandLineParser_ParseException $e) {
+  $option_parser->printException($e);
+  $option_parser->printUsage();
 }
 
