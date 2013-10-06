c6edd786f01956482209828f05dd714ca2e0a6e8
kurund <kurund@civicrm.org>
2013-10-06 14:10:31 +0100

    Revert "Merge pull request #1645 from eileenmcnaughton/CRM-13379"
    This reverts commit dcb91473ed06b6f221d2b726e6a41f2d844b7c16, reversing
    changes made to bb40618ddaed08fc84508dc2178a9f37a016487a.

--- templates/CRM/Contact/Form/Task/EmailCommon.js
+++ templates/CRM/Contact/Form/Task/EmailCommon.js
@@ -2,4 +2,14 @@ cj(function ($) {
   //do not copy & paste this - find a way to generalise it
   'use strict';
    $().crmAccordions();
+  // NOTE: Might be safer to say $('[name=_qf_Email_upload]')
+   $('.form-submit').not('.cancel').on("click", function() {
+     $('.form-submit').not('.cancel').attr({value: ts('Processing')});
+     // CRM-13449 : setting a 0 ms timeout is needed 
+     // for some browsers like chrome. Used for purpose of
+     // submit the form and stop accidental multiple clicks
+     setTimeout(function(){
+       $('.form-submit').not('.cancel').attr({disabled: 'disabled'});
+     }, 0);
+   });
 });
