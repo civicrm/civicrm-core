f7a9471a0df3da2d49ef86f30bbe7889e13a4e85
Tim Otten <totten@civicrm.org>
2013-10-07 13:09:51 +0100

    CRM_Extension_Container_Basic - Display warning if no resource URL is defined

--- CRM/Extension/Container/Basic.php
+++ CRM/Extension/Container/Basic.php
@@ -136,6 +136,16 @@ class CRM_Extension_Container_Basic implements CRM_Extension_Container_Interface
    * {@inheritdoc}
    */
   public function getResUrl($key) {
+    if (! $this->baseUrl) {
+      CRM_Core_Session::setStatus(
+        ts('Failed to determine URL for extension (%1). Please update <a href="%2">Resource URLs</a>.',
+          array(
+            1 => $key,
+            2 => CRM_Utils_System::url('civicrm/admin/setting/url', 'reset=1'),
+          )
+        )
+      );
+    }
     return $this->baseUrl . $this->getRelUrl($key);
   }
 
