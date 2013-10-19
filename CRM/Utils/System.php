7aa571ba11329e17b93cea702acbddf824e5c402
Donald A. Lobo <lobo@civicrm.org>
2013-10-19 09:58:00 -0700

    CRM-13621 - add ability to redirect to main entry point for public pages
    ----------------------------------------
    * CRM-13621: Handle invalid session errors especially for public pages in a nicer manner
      http://issues.civicrm.org/jira/browse/CRM-13621

--- CRM/Utils/System.php
+++ CRM/Utils/System.php
@@ -47,11 +47,15 @@ class CRM_Utils_System {
    * pager, sort and qfc
    *
    * @param string $urlVar the url variable being considered (i.e. crmPageID, crmSortID etc)
+   * @param boolean $includeReset - should we include or ignore the reset GET string (if present)
+   * @param boolean $includeForce - should we include or ignore the force GET string (if present)
+   * @param string  $path - the path to use for the new url
+   * @param string  $absolute - do we need a absolute or relative URL?
    *
    * @return string the url fragment
    * @access public
    */
-  static function makeURL($urlVar, $includeReset = FALSE, $includeForce = TRUE, $path = NULL) {
+  static function makeURL($urlVar, $includeReset = FALSE, $includeForce = TRUE, $path = NULL, $absolute = FALSE) {
     if (empty($path)) {
       $config = CRM_Core_Config::singleton();
       $path = CRM_Utils_Array::value($config->userFrameworkURLVar, $_GET);
@@ -60,9 +64,12 @@ class CRM_Utils_System {
       }
     }
 
-    return self::url($path,
-      CRM_Utils_System::getLinksUrl($urlVar, $includeReset, $includeForce)
-    );
+    return
+      self::url(
+        $path,
+        CRM_Utils_System::getLinksUrl($urlVar, $includeReset, $includeForce),
+        $absolute
+      );
   }
 
   /**
@@ -131,7 +138,12 @@ class CRM_Utils_System {
     $querystring = array_merge($querystring, array_unique($arrays));
     $querystring = array_map('htmlentities', $querystring);
 
-    return implode('&amp;', $querystring) . (!empty($querystring) ? '&amp;' : '') . $urlVar . '=';
+    $url = implode('&amp;', $querystring);
+    if ($urlVar) {
+      $url .= (!empty($querystring) ? '&amp;' : '') . $urlVar . '=';
+    }
+
+    return $url;
   }
 
   /**
