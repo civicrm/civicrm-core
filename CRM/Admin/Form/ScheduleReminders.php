ffe4e27f95ecf69e960dbda09fbfb45864fe9158
Andrew Hunt <andrew@aghstrategies.com>
2013-10-24 17:05:14 -0400

    accidentally undid an old change

--- CRM/Admin/Form/ScheduleReminders.php
+++ CRM/Admin/Form/ScheduleReminders.php
@@ -207,12 +207,8 @@ class CRM_Admin_Form_ScheduleReminders extends CRM_Admin_Form {
     $this->assign('tokenUrl', $tokenUrl);
     $this->add('text', 'recipient_manual_id', ts('Manual Recipients'));
 
-    $this->addElement(
-      'select',
-      'group_id',
-      ts('Group'),
-      // CRM-13577
-      CRM_Core_PseudoConstant::group()
+    $this->addElement('select', 'group_id', ts('Group'),
+      CRM_Core_PseudoConstant::staticGroup() 
     );
 
     CRM_Mailing_BAO_Mailing::commonCompose($this);
