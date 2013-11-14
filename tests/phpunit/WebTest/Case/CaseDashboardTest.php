0e71b04f66cc0929882dfa5ef75fd5b8289f541a
Nileema <nileema@nileema.(none)>
2013-11-14 19:09:28 +0530

    -- CRM-13747 CiviCase label and title simplification to increase adaptability
    ----------------------------------------
    * CRM-13747: CiviCase label and title simplification to increase adaptability
      http://issues.civicrm.org/jira/browse/CRM-13747

--- tests/phpunit/WebTest/Case/CaseDashboardTest.php
+++ tests/phpunit/WebTest/Case/CaseDashboardTest.php
@@ -50,7 +50,7 @@ class WebTest_Case_CaseDashboardTest extends CiviSeleniumTestCase {
     // Should default to My Cases
     $this->assertTrue($this->isChecked("name=allupcoming value=0"), 'Case dashboard should default to My Cases.');
     // The header text of the table changes too
-    $this->assertElementContainsText('crm-container', "Summary of Case Involvement");
+    $this->assertElementContainsText('crm-container', "Summary of Involvement");
 
     $this->clickLink("name=allupcoming value=1", "css=a.button");
 
