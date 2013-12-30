520ec40373c082eefe00d6a6f2c08bf62cd70323
Pratik Joshi <pratik.joshi@webaccess.co.in>
2013-12-30 12:52:28 +0530

    CRM-13964 : minor query fix in bao function for fetching balance amount

--- CRM/Core/BAO/FinancialTrxn.php
+++ CRM/Core/BAO/FinancialTrxn.php
@@ -86,7 +86,7 @@ class CRM_Core_BAO_FinancialTrxn extends CRM_Financial_DAO_FinancialTrxn {
   static function getBalanceTrxnAmt($contributionId, $contributionFinancialTypeId) {
     $relationTypeId = key(CRM_Core_PseudoConstant::accountOptionValues('account_relationship', NULL, " AND v.name LIKE 'Accounts Receivable Account is' "));
     $toFinancialAccount = CRM_Contribute_PseudoConstant::financialAccountType($contributionFinancialTypeId, $relationTypeId);
-    $q = "SELECT ft.id, ft.total_amount FROM civicrm_financial_trxn ft LEFT JOIN civicrm_entity_financial_trxn eft ON (eft.financial_trxn_id = ft.id AND eft.entity_table = 'civicrm_contribution' AND eft.entity_id = %1) WHERE ft.to_financial_account_id = %2 ";
+    $q = "SELECT ft.id, ft.total_amount FROM civicrm_financial_trxn ft INNER JOIN civicrm_entity_financial_trxn eft ON (eft.financial_trxn_id = ft.id AND eft.entity_table = 'civicrm_contribution') WHERE eft.entity_id = %1 AND ft.to_financial_account_id = %2";
     $p[1] = array($contributionId, 'Integer');
     $p[2] = array($toFinancialAccount, 'Integer');
     $balanceAmtDAO = CRM_Core_DAO::executeQuery($q, $p);
