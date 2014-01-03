3d103be798d439399d3ca19f3a21b6d7bbca84e1
Eileen <eileen@fuzion.co.nz>
2014-01-03 16:03:03 +1300

    e-notice compliance on batch api / mark probable error

--- api/v3/Batch.php
+++ api/v3/Batch.php
@@ -55,7 +55,11 @@ function civicrm_api3_batch_create($params) {
  * @param array $params array or parameters determined by getfields
  */
 function _civicrm_api3_batch_create_spec(&$params) {
+  //@todo - the entity table field looks like it is not actually required & should probably be removed (or a description added if
+  // it is actually required)
   $params['entity_table']['api.default'] = "civicrm_batch";
+  $params['entity_table']['type'] = CRM_Utils_Type::T_STRING;
+
   $params['modified_date']['api.default'] = "now";
   $params['status_id']['api.required'] = 1;
   $params['title']['api.required'] = 1;
