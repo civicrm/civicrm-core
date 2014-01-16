4c687e7845935421de721e9ce60b4459ad573283
Donald A. Lobo <lobo@civicrm.org>
2014-01-16 13:41:38 -0800

    CRM-12882 - Payment Processor passwords should be better hidden
    http://issues.civicrm.org/jira/browse/CRM-12882

--- CRM/Core/Permission.php
+++ CRM/Core/Permission.php
@@ -555,6 +555,7 @@ class CRM_Core_Permission {
       'delete all manual batches' => $prefix . ts('delete all manual batches'),
       'export own manual batches' => $prefix . ts('export own manual batches'),
       'export all manual batches' => $prefix . ts('export all manual batches'),
+      'administer payment processors' => $prefix . ts('administer payment processors'),
     );
 
     return $permissions;
