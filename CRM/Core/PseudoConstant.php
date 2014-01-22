cfbf50241d5edae6076655d433649d71d9ffb15a
yashodha <yashodha.chaku@webaccess.co.in>
2014-01-22 12:29:08 +0530

    HR-252 : limit activity types to NULL grouping

--- CRM/Core/PseudoConstant.php
+++ CRM/Core/PseudoConstant.php
@@ -626,6 +626,7 @@ class CRM_Core_PseudoConstant {
         $componentClause = " v.component_id IS NOT NULL";
       }
 
+      $groupingClause = " v.grouping IS NULL";
       $componentIds = array();
       $compInfo = CRM_Core_Component::getEnabledComponents();
 
@@ -654,7 +655,7 @@ class CRM_Core_PseudoConstant {
           $componentClause = " ( v.component_id IN ($componentIds ) )";
         }
       }
-      $condition = $condition . ' AND ' . $componentClause;
+      $condition = $condition . ' AND ' . $componentClause . ' AND ' . $groupingClause;
 
       self::$activityType[$index] = CRM_Core_OptionGroup::values('activity_type', FALSE, FALSE, FALSE, $condition, $returnColumn);
     }
