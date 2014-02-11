9c235841493827e03fa44a1b4bb947b6c824361f
Andreas Hennings <andreas@dqxtech.net>
2014-02-11 16:32:24 +0100

    Mark a possible bug in CRM_Utils_Hook.

--- CRM/Utils/Hook.php
+++ CRM/Utils/Hook.php
@@ -1493,6 +1493,7 @@ abstract class CRM_Utils_Hook {
    * @return mixed
    */
   static function dashboard_defaults($availableDashlets, &$defaultDashlets) {
+    // @todo Required parameter $fnSuffix missing. Too few parameters?
     return self::singleton()->invoke(2, $availableDashlets, $defaultDashlets, self::$_nullObject, self::$_nullObject, self::$_nullObject, 'civicrm_dashboard_defaults');
   }
 }
