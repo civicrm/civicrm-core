563bfe648a0144525fbd86347015f7200fdf4f0c
Dave Greenberg <dave@civicrm.org>
2014-02-25 09:33:29 -0800

    CRM-14266 - include all system FROM emails.
    ----------------------------------------
    * CRM-14266: send receipt from contrib/event/membership should list all configured FROM email addresses
      http://issues.civicrm.org/jira/browse/CRM-14266

--- CRM/Core/BAO/Email.php
+++ CRM/Core/BAO/Email.php
@@ -287,10 +287,12 @@ AND    reset_date IS NULL
     $contactID       = $session->get('userID');
     $fromEmailValues = array();
 
-    // add the domain email id
-    $domainEmail = CRM_Core_BAO_Domain::getNameAndEmail();
-    $domainEmail = "$domainEmail[0] <$domainEmail[1]>";
-    $fromEmailValues[$domainEmail] = htmlspecialchars($domainEmail);
+    // add all configured FROM email addresses
+    $domainFrom = CRM_Core_OptionGroup::values('from_email_address');
+    foreach (array_keys($domainFrom) as $k) {
+      $domainEmail = $domainFrom[$k];
+      $fromEmailValues[$domainEmail] = htmlspecialchars($domainEmail);
+    }
 
     // add logged in user's active email ids
     if ($contactID) {
