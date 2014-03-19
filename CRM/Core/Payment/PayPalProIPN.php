e6bcd06ed109427a7c0f94c257e23eb3cd392785
Jamie McClelland <jm@mayfirst.org>
2014-03-19 16:48:11 -0400

    CRM-12679 - use properly FinancialType DAO call for 4.4
    ----------------------------------------
    * CRM-12679: Paypal Express Recurring IPN not working?
      http://issues.civicrm.org/jira/browse/CRM-12679

--- CRM/Core/Payment/PayPalProIPN.php
+++ CRM/Core/Payment/PayPalProIPN.php
@@ -514,7 +514,7 @@ INNER JOIN civicrm_membership_payment mp ON m.id = mp.membership_id AND mp.contr
     // membership would be an easy add - but not relevant to my customer...
     $this->_component  = $input['component'] = 'contribute';
     $input['trxn_date'] = date('Y-m-d-H-i-s', strtotime(self::retrieve('time_created', 'String')));
-    $paymentProcessorID = CRM_Core_DAO::getFieldValue('CRM_Core_DAO_PaymentProcessorType',
+    $paymentProcessorID = CRM_Core_DAO::getFieldValue('CRM_Financial_DAO_PaymentProcessorType',
       'PayPal', 'id', 'name'
     );
 
