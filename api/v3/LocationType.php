51d3af66013cab4c90ee70cd903f39e3e59b37dc
kurund <kurund@civicrm.org>
2014-03-28 15:44:39 +0530

    copyright and version fixes

--- api/v3/LocationType.php
+++ api/v3/LocationType.php
@@ -2,9 +2,9 @@
 
 /*
  +--------------------------------------------------------------------+
- | CiviCRM version 4.4                                                |
+ | CiviCRM version 4.5                                                |
  +--------------------------------------------------------------------+
- | Copyright CiviCRM LLC (c) 2004-2013                                |
+ | Copyright CiviCRM LLC (c) 2004-2014                                |
  +--------------------------------------------------------------------+
  | This file is a part of CiviCRM.                                    |
  |                                                                    |
@@ -32,7 +32,7 @@
  * @package CiviCRM_APIv3
  * @subpackage API_LocationType
  *
- * @copyright CiviCRM LLC (c) 2004-2013
+ * @copyright CiviCRM LLC (c) 2004-2014
  * @version $Id: LocationType.php 2012-11-02 Jon Goldberg $
  */
 
