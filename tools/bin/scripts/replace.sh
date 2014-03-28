34cd78e1e19add996f627decfbbc0ad6b2627f23
kurund <kurund@civicrm.org>
2014-03-28 15:45:34 +0530

    copyright and version fixes

--- tools/bin/scripts/replace.sh
+++ tools/bin/scripts/replace.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 for i in `find . -name '*.js' -or -name '*.module' -or -name '*.php' -or -name '*.po*' -or -name '*.tpl' -or -name '*.txt' -or -name '*.install' -or -name '*.inc' -or -name '*.test'`; do
   echo $i;
-  perl -pi -e 's/CiviCRM version .\../CiviCRM version 3.1/' $i;
-  perl -pi -e 's/Copyright CiviCRM LLC \(c\) 2004-20../Copyright CiviCRM LLC (c) 2004-2010/' $i;
-  perl -pi -e 's/CiviCRM LLC \(c\) 2004-20../CiviCRM LLC (c) 2004-2010/' $i;
+  perl -pi -e 's/CiviCRM version .\../CiviCRM version 4.5/' $i;
+  perl -pi -e 's/Copyright CiviCRM LLC \(c\) 2004-20../Copyright CiviCRM LLC (c) 2004-2014/' $i;
+  perl -pi -e 's/CiviCRM LLC \(c\) 2004-20../CiviCRM LLC (c) 2004-2014/' $i;
 done
