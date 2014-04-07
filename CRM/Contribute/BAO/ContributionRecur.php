0d4bfc195f3cbb17b126a2c9ab766158f1e624cc
Eileen McNaughton <eileen@mcnaughty.com>
2014-04-07 17:49:42 +1200

    E-notice fix, next_sched_contribution
    Field was renamed to reflect in built handling around suffix '_date' but this instance missed

--- CRM/Contribute/BAO/ContributionRecur.php
+++ CRM/Contribute/BAO/ContributionRecur.php
@@ -335,7 +335,7 @@ SELECT r.payment_processor_id
       $params[$recurDAO->id]['contactId'] = $recurDAO->contact_id;
       $params[$recurDAO->id]['start_date'] = $recurDAO->start_date;
       $params[$recurDAO->id]['end_date'] = $recurDAO->end_date;
-      $params[$recurDAO->id]['next_sched_contribution_date'] = $recurDAO->next_sched_contribution;
+      $params[$recurDAO->id]['next_sched_contribution_date'] = $recurDAO->next_sched_contribution_date;
       $params[$recurDAO->id]['amount'] = $recurDAO->amount;
       $params[$recurDAO->id]['currency'] = $recurDAO->currency;
       $params[$recurDAO->id]['frequency_unit'] = $recurDAO->frequency_unit;
