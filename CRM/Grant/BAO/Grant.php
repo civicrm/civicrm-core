2ce233a3df0b3ac3195880c3f240dab72488983a
Edsel <edsel.lopez@jmaconsulting.biz>
2014-04-30 20:07:46 +0530

    CRM-12778 Additional bug fixes
    ----------------------------------------
    * CRM-12778: Expose grants to the front-end
      https://issues.civicrm.org/jira/browse/CRM-12778

--- CRM/Grant/BAO/Grant.php
+++ CRM/Grant/BAO/Grant.php
@@ -424,28 +424,27 @@ class CRM_Grant_BAO_Grant extends CRM_Grant_DAO_Grant {
   }
 
 
-/**
- * Function to get list of grant fields for profile
- * For now we only allow custom grant fields to be in
- * profile
- *
- * @param boolean $addExtraFields true if special fields needs to be added
- *
- * @return return the list of grant fields
- * @static
- * @access public
- */
-function getGrantFields() {
-  $grantFields = CRM_Grant_DAO_Grant::export();
-  $grantFields = array_merge($grantFields, CRM_Core_OptionValue::getFields($mode = 'grant'));
+  /**
+   * Function to get list of grant fields for profile
+   * For now we only allow custom grant fields to be in
+   * profile
+   *
+   *
+   * @return return the list of grant fields
+   * @access public
+   * @static
+   */
+  static function getGrantFields() {
+    $grantFields = CRM_Grant_DAO_Grant::export();
+    $grantFields = array_merge($grantFields, CRM_Core_OptionValue::getFields($mode = 'grant'));
     
-  foreach ($grantFields as $key => $var) {
-    $fields[$key] = $var;
+    foreach ($grantFields as $key => $var) {
+      $fields[$key] = $var;
+    }
+    
+    $fields = array_merge($fields, CRM_Core_BAO_CustomField::getFieldsForImport('Grant'));
+    
+    return $fields;
   }
-
-  $fields = array_merge($fields, CRM_Core_BAO_CustomField::getFieldsForImport('Grant'));
-   
-  return $fields;
-}
 }
 
