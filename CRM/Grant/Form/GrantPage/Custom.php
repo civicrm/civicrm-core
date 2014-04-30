88aeb903db60a648fa5103d686494ece489a5a0e
Edsel <edsel.lopez@jmaconsulting.biz>
2014-04-30 18:18:37 +0530

    CRM-12778 Fixed version changes
    ----------------------------------------
    * CRM-12778: Expose grants to the front-end
      https://issues.civicrm.org/jira/browse/CRM-12778

--- CRM/Grant/Form/GrantPage/Custom.php
+++ CRM/Grant/Form/GrantPage/Custom.php
@@ -1,9 +1,9 @@
 <?php
 /*
  +--------------------------------------------------------------------+
- | CiviCRM version 4.2                                                |
+ | CiviCRM version 4.5                                                |
  +--------------------------------------------------------------------+
- | Copyright CiviCRM LLC (c) 2004-2012                                |
+ | Copyright CiviCRM LLC (c) 2004-2014                                |
  +--------------------------------------------------------------------+
  | This file is a part of CiviCRM.                                    |
  |                                                                    |
@@ -28,7 +28,7 @@
 /**
  *
  * @package CRM
- * @copyright CiviCRM LLC (c) 2004-2012
+ * @copyright CiviCRM LLC (c) 2004-2014
  * $Id$
  *
  */
