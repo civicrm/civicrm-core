a633a8a4b34580b67528b27ca68a020384841ef0
kurund <kurund@civicrm.org>
2014-05-02 14:00:44 -0700

    CRM-14084 fixes - Search by participant role does not work in Search Builder
    https://issues.civicrm.org/jira/browse/CRM-14084

--- CRM/Contact/BAO/Query.php
+++ CRM/Contact/BAO/Query.php
@@ -5239,7 +5239,6 @@ AND   displayRelType.is_active = 1
       return;
     }
     $values = array();
-    $sep = CRM_Core_DAO::VALUE_SEPARATOR;
     foreach ($this->_pseudoConstantsSelect as $key => $value) {
       if (!empty($this->_pseudoConstantsSelect[$key]['sorting'])) {
         continue;
@@ -5260,9 +5259,11 @@ AND   displayRelType.is_active = 1
         elseif ($value['pseudoField'] == 'state_province_abbreviation') {
           $dao->$key = CRM_Core_PseudoConstant::stateProvinceAbbreviation($val);
         }
+        // FIX ME: we should potentially move this to component Query and write a wrapper function that
+        // handles pseudoconstant fixes for all component
         elseif ($value['pseudoField'] == 'participant_role') {
           $viewRoles = array();
-          foreach (explode($sep, $val) as $k => $v) {
+          foreach (explode(CRM_Core_DAO::VALUE_SEPARATOR, $val) as $k => $v) {
             $viewRoles[] = CRM_Event_PseudoConstant::participantRole($v);
           }
           $dao->$key = implode(', ', $viewRoles);
