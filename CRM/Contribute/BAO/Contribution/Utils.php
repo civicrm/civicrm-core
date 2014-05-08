c76cb48883b49dc74cef554d70439e723840b0c9
Eileen McNaughton <eileen@fuzion.co.nz>
2014-05-08 12:13:02 +1200

    comment fixes

--- CRM/Contribute/BAO/Contribution/Utils.php
+++ CRM/Contribute/BAO/Contribution/Utils.php
@@ -37,16 +37,14 @@ class CRM_Contribute_BAO_Contribution_Utils {
   /**
    * Function to process payment after confirmation
    *
-   * @param object $form form object
-   * @param array $paymentParams array with payment related key
+   * @param object  $form   form object
+   * @param array   $paymentParams   array with payment related key
    * value pairs
-   * @param array $premiumParams array with premium related key
+   * @param array   $premiumParams   array with premium related key
    * value pairs
-   * @param int $contactID contact id
-   * @param int $contributionTypeId financial type id
-   * @param int|string $component component id
-   *
-   * @param null $fieldTypes
+   * @param int     $contactID       contact id
+     * @param int     $contributionTypeId   financial type id
+   * @param int     $component   component id
    *
    * @return array associated array
    *
