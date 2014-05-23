0ca4c00f951ea90487ef7e8c69a7ee448a6b13c0
jitendrapurohit <jitendra.purohit@webaccess.co.in>
2014-05-23 17:25:48 +0530

    CRM-143

--- CRM/Case/Page/DashBoard.php
+++ CRM/Case/Page/DashBoard.php
@@ -91,6 +91,11 @@ class CRM_Case_Page_DashBoard extends CRM_Core_Page {
     $upcoming = CRM_Case_BAO_Case::getCases($allCases, $userID, 'upcoming');
     $recent   = CRM_Case_BAO_Case::getCases($allCases, $userID, 'recent');
 
+    $upcoming_pop = array_pop($upcoming);
+    if (strtotime($upcoming_pop['case_scheduled_activity_date']) < time()) {
+      $upcoming_pop['activity_status'] = 'status-overdue';
+    }
+    array_push($upcoming, $upcoming_pop);
     $this->assign('casesSummary', $summary);
     if (!empty($upcoming)) {
       $this->assign('upcomingCases', $upcoming);
