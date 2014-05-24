355163aa150f02c79d7e7a9a141df7006a498b9b
Eileen McNaughton <eileen@fuzion.co.nz>
2014-05-24 14:46:58 +1200

    add CRM/Core/CodeGen comment blocks

--- CRM/Core/CodeGen/BaseTask.php
+++ CRM/Core/CodeGen/BaseTask.php
@@ -6,11 +6,17 @@
 abstract class CRM_Core_CodeGen_BaseTask implements CRM_Core_CodeGen_ITask {
   protected $config;
 
+  /**
+   *
+   */
   function __construct() {
   }
 
   // TODO: this is the most rudimentary possible hack.  CG config should
   // eventually be made into a first-class object.
+  /**
+   * @param is $config
+   */
   function setConfig($config) {
     $this->config = $config;
     $this->tables = $this->config->tables;
