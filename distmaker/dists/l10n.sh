d1426d23d01b297553cf57f9437949807195b6ee
totten@civicrm.org <Tim Otten>
2014-06-21 04:46:55 -0700

    distmaker - Simplify l10n.sh

--- distmaker/dists/l10n.sh
+++ distmaker/dists/l10n.sh
@@ -19,17 +19,15 @@ TRG=$DM_TMPDIR/civicrm
 
 # copy all the stuff
 dm_install_l10n "$SRC/l10n" "$TRG/l10n"
-dm_install_files "$SRC" "$TRG" {agpl-3.0,agpl-3.0.exception,gpl,README,CONTRIBUTORS}.txt
 
-# copy selected sqls
 [ ! -d $TRG/sql ] && mkdir $TRG/sql
-for F in $SRC/sql/civicrm*.mysql $SRC/sql/case_sample*.mysql; do
+for F in $SRC/sql/civicrm_*.??_??.mysql; do
 	cp $F $TRG/sql
 done
 
 # gen tarball
 cd $TRG/..
-tar czf $DM_TARGETDIR/civicrm-$DM_VERSION-l10n.tar.gz --exclude '*.po' --exclude pot civicrm/l10n civicrm/sql/civicrm_*.??_??.mysql
+tar czf $DM_TARGETDIR/civicrm-$DM_VERSION-l10n.tar.gz --exclude '*.po' --exclude pot civicrm
 
 # clean up
 rm -rf $TRG
