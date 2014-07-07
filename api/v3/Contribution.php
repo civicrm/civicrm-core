83d4687ec60330ccf686a86442a124643e63d9c0
Eileen McNaughton <eileen@fuzion.co.nz>
2014-07-07 19:24:56 +1200

    CRM-14950 don't complete transaction if already completed

--- api/v3/Contribution.php
+++ api/v3/Contribution.php
@@ -372,7 +372,7 @@ function civicrm_api3_contribution_completetransaction(&$params) {
   if(!$contribution->id == $params['id']){
     throw new API_Exception('A valid contribution ID is required', 'invalid_data');
   }
-  elseif ($contribution['contribution_status_id'] == CRM_Core_OptionGroup::getValue('contribution_status', 'Completed', 'name')) {
+  elseif ($contribution->contribution_status_id == CRM_Core_OptionGroup::getValue('contribution_status', 'Completed', 'name')) {
     throw new API_Exception(ts('Contribution already completed'));
   }
   try {
