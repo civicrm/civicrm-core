2ab6ee96d00086978c1e45e58a5e1894f4d6ad48
Tim Otten <totten@civicrm.org>
2014-07-14 17:27:44 -0700

    Fix hard crash of test-suite due to syntax error
    Note: This is *only* a syntax fix. I don't what's supposed to be going on,
    and there are other tests which fails... but at least they don't
    crash the test-suite.
    
    Original change was: 9e2ee07bf82624fe50070e14ccc6aa39ffa1b7d9

--- api/v3/Contribution.php
+++ api/v3/Contribution.php
@@ -379,7 +379,7 @@ function civicrm_api3_contribution_completetransaction(&$params) {
     if(!$contribution->loadRelatedObjects($input, $ids, FALSE, TRUE)){
       throw new API_Exception('failed to load related objects');
     }
-    elseif ($contribution['contribution_status_id'] == CRM_Core_OptionGroup::getValue('contribution_status', 'Completed', 'name')) {
+    elseif ($contribution->contribution_status_id == CRM_Core_OptionGroup::getValue('contribution_status', 'Completed', 'name')) {
       throw new API_Exception(ts('Contribution already completed'));
     }
     $objects = $contribution->_relatedObjects;
