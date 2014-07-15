f11ce11ef00a2bbb7d4e721059a1b5fdb760d008
Eileen McNaughton <eileen@fuzion.co.nz>
2014-07-15 16:34:47 +1200

    CRM-14986 fix for mishandled date fields
    switch to repo version of packages (with spaces all over the show)

--- packages/DB/DataObject.php
+++ packages/DB/DataObject.php
@@ -1261,6 +1261,19 @@ class DB_DataObject extends DB_DataObject_Overload
                 $settings .= "$kSql = NULL ";
                 continue;
             }
+          if (($v & DB_DATAOBJECT_DATE) || ($v & DB_DATAOBJECT_TIME)) {
+            if (strpos($this->$k, '-') !== FALSE) {
+             /*
+              * per CRM-14986 we have been having ongoing problems with the format returned from $dao->find(TRUE) NOT
+              * being acceptable for an immediate save. This has resulted in the codebase being smattered with
+              * instances of CRM_Utils_Date::isoToMysql for date fields retrieved in this way
+              * which seems both risky (have to remember to do it for every field) & cludgey.
+              * doing it here should be safe as only fields with a '-' in them will be affected - if they are
+              *  already formatted or empty then this line will not be hit
+              */
+              $this->$k = CRM_Utils_Date::isoToMysql($this->$k);
+            }
+          }
             // DATE is empty... on a col. that can be null..
             // note: this may be usefull for time as well..
             if (!$this->$k &&
