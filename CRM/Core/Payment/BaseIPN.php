0ec22607e1f7abc40072211789622e84c4c0dd82
CiviCRM <info@civicrm.org>
2014-07-16 09:57:52 +0530

    CRM-14951: no of terms should be calculated on contribution id rather than entity id/table

--- CRM/Core/Payment/BaseIPN.php
+++ CRM/Core/Payment/BaseIPN.php
@@ -401,6 +401,7 @@ LIMIT 1;";
             // else fall back to using current membership type
             $dao->free();
 
+            $num_terms = $contribution->getNumTermsByContributionAndMembershipType($membership->membership_type_id);
             if ($currentMembership) {
               /*
                * Fixed FOR CRM-4433
@@ -409,7 +410,6 @@ LIMIT 1;";
                */
               CRM_Member_BAO_Membership::fixMembershipStatusBeforeRenew($currentMembership, $changeToday);
 
-              $num_terms = $contribution->getNumTermsByContributionAndMembershipType($membership->membership_type_id);
               // @todo - we should pass membership_type_id instead of null here but not
               // adding as not sure of testing
               $dates = CRM_Member_BAO_MembershipType::getRenewalDatesForMembershipType($membership->id,
@@ -419,7 +419,7 @@ LIMIT 1;";
               $dates['join_date'] = CRM_Utils_Date::customFormat($currentMembership['join_date'], $format);
             }
             else {
-              $dates = CRM_Member_BAO_MembershipType::getDatesForMembershipType($membership->membership_type_id);
+              $dates = CRM_Member_BAO_MembershipType::getDatesForMembershipType($membership->membership_type_id, NULL, NULL, NULL, $num_terms);
             }
 
             //get the status for membership.
