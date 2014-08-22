74842007120e80a760659633fc7c7ef82ee2eb94
Coleman Watts <coleman@civicrm.org>
2014-08-22 17:15:43 +0100

    Ensure reports use nested group selector and remove boilerplate

--- CRM/Report/Form.php
+++ CRM/Report/Form.php
@@ -1282,7 +1282,7 @@ class CRM_Report_Form extends CRM_Core_Form {
         'title' => ts('Group'),
         'operatorType' => CRM_Report_Form::OP_MULTISELECT,
         'group' => TRUE,
-        'options' => CRM_Core_PseudoConstant::group(),
+        'options' => CRM_Core_PseudoConstant::nestedGroup(),
       ),
     );
     if (empty($this->_columns['civicrm_group']['dao'])) {
