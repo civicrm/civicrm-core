efd081246fa3503d04e976793c8eb917d75fd37c
Jitendra Purohit <jitendra.purohit@webaccessglobal.com>
2014-08-26 16:19:43 +0530

    CRM-15169

--- CRM/Report/Form/Contribute/OrganizationSummary.php
+++ CRM/Report/Form/Contribute/OrganizationSummary.php
@@ -244,7 +244,7 @@ class CRM_Report_Form_Contribute_OrganizationSummary extends CRM_Report_Form {
               }
             }
             else {
-              $select[] = "{$table['alias']}.{$fieldName} as {$tableName}_{$fieldName}";
+              $select[] = "{$field['dbAlias']} as {$tableName}_{$fieldName}";
 
               $this->_columnHeaders["{$tableName}_{$fieldName}"]['title'] = CRM_Utils_Array::value('title', $field);
               $this->_columnHeaders["{$tableName}_{$fieldName}"]['type'] = CRM_Utils_Array::value('type', $field);
