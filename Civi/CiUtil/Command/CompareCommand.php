3433cc3b39cbf02ae211b2d3ad89027eaa4a2704
Tim Otten <totten@civicrm.org>
2014-08-26 13:32:12 -0700

    phpunit-compare - Add support for reading results from Jenkins' XML files

--- Civi/CiUtil/Command/CompareCommand.php
+++ Civi/CiUtil/Command/CompareCommand.php
@@ -5,21 +5,30 @@ class CompareCommand {
   static function main($argv) {
     if (empty($argv[1])) {
       echo "summary: Compares the output of different test runs\n";
-      echo "usage: phpunit-compare <json-file1> [<json-file2>...]\n";
+      echo "usage: phpunit-compare [--phpunit-json|--jenkins-xml] <file1> <file2>...\n";
       exit(1);
     }
 
-
+    $parser = array('\Civi\CiUtil\PHPUnitParser', 'parseJsonResults');
     $suites = array(); // array('file' => string, 'results' => array)
     for ($i = 1; $i < count($argv); $i++) {
-      $suites[$i] = array(
-        'file' => $argv[$i],
-        'results' => \Civi\CiUtil\PHPUnitParser::parseJsonResults(file_get_contents($argv[$i]))
-      );
+      switch ($argv[$i]) {
+        case '--phpunit-json':
+          $parser = array('\Civi\CiUtil\PHPUnitParser', 'parseJsonResults');
+          break;
+        case '--jenkins-xml':
+          $parser = array('\Civi\CiUtil\JenkinsParser', 'parseXmlResults');
+          break;
+        default:
+          $suites[] = array(
+            'file' => $argv[$i],
+            'results' => call_user_func($parser, file_get_contents($argv[$i])),
+          );
+      }
     }
 
     $tests = array(); // array(string $name)
-    foreach ($suites as $suiteName => $suite) {
+    foreach ($suites as $suite) {
       $tests = array_unique(array_merge(
         $tests,
         array_keys($suite['results'])
@@ -30,7 +39,7 @@ class CompareCommand {
     $printer = new \Civi\CiUtil\ComparisonPrinter(\Civi\CiUtil\Arrays::collect($suites, 'file'));
     foreach ($tests as $test) {
       $values = array();
-      foreach ($suites as $suiteName => $suite) {
+      foreach ($suites as $suite) {
         $values[] = isset($suite['results'][$test]) ? $suite['results'][$test] : 'MISSING';
       }
 
