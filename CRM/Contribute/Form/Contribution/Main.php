6139fbd2633629c10e635374b080167564e98d20
monishdeb <monish.deb@webaccessglobal.com>
2014-08-27 14:13:47 +0530

    CRM-15103 additional fix
    ----------------------------------------
    * CRM-15103: Priceset for memberships on contribution-page not working
      https://issues.civicrm.org/jira/browse/CRM-15103

--- CRM/Contribute/Form/Contribution/Main.php
+++ CRM/Contribute/Form/Contribution/Main.php
@@ -844,9 +844,13 @@ class CRM_Contribute_Form_Contribution_Main extends CRM_Contribute_Form_Contribu
 
             if (CRM_Utils_Array::value('options', $value)) {
               foreach ($value['options'] as $val) {
-                if (CRM_Utils_Array::value('membership_type_id', $val) && $fields['price_' . $priceId] == $val['id']) {
-                  $priceFieldMemTypes[] = $val['membership_type_id'];
-                }
+                  if (CRM_Utils_Array::value('membership_type_id', $val) && (
+                      ($fields['price_' . $priceId] == $val['id']) ||
+                      (isset($fields['price_' . $priceId]) && !empty($fields['price_' . $priceId][$val['id']]))
+                    )
+                  ) {
+                    $priceFieldMemTypes[] = $val['membership_type_id'];
+                  }
               }
             }
           }
