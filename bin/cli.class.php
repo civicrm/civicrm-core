d142c9bd8c2dc355621b123a3099db2ac9ee78fd
TeNNoX <kai-manuel@web.de>
2014-09-04 10:25:00 +0100

    Added option to use semicolon as seperator for the import+export scripts

--- bin/cli.class.php
+++ bin/cli.class.php
@@ -46,6 +46,7 @@ class civicrm_cli {
   var $_action = NULL;
   var $_output = FALSE;
   var $_joblog = FALSE;
+  var $_semicolon = FALSE;
   var $_config;
 
   // optional arguments
@@ -173,6 +174,9 @@ class civicrm_cli {
       }
       elseif ($arg == '-j' || $arg == '--joblog') {
         $this->_joblog = TRUE;
+      }
+      elseif ($arg == '-sem' || $arg == '--semicolon') {
+        $this->_semicolon = TRUE;
       }
       else {
         while(list($short, $long) = each ($this->_additional_arguments)) {
@@ -327,6 +331,9 @@ class civicrm_cli_csv_exporter extends civicrm_cli {
   }
 
   function run() {
+  	if($this->_semicolon)
+  		$this->separator = ';';
+  	
     $out = fopen("php://output", 'w');
     fputcsv($out, $this->columns, $this->separator, '"');
 
