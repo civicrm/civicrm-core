ee03546a403b8e2317738ce79f702b03d44becc6
Eileen <eileen@fuzion.co.nz>
2014-09-04 04:48:26 +0000

    Add 4.4.7 upgrade files

--- civicrm-version.php
+++ civicrm-version.php
@@ -1,7 +1,7 @@
 <?php
 function civicrmVersion( ) {
-  return array( 'version'  => '4.4.6',
-                'cms'      => 'Drupal6',
-                'revision' => 'f72e57b0bc' );
+  return array( 'version'  => '4.4.7',
+                'cms'      => 'Drupal',
+                'revision' => 'b67360b113' );
 }
 
