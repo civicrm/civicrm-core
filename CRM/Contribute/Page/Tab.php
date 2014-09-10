8acda29a56ba35b25bfbc66c5dd8fa4e83431599
Eileen McNaughton <eileen@fuzion.co.nz>
2014-09-10 12:12:57 +1200

    CRM-15271 - broken last viewed links

--- CRM/Contribute/Page/Tab.php
+++ CRM/Contribute/Page/Tab.php
@@ -312,7 +312,10 @@ class CRM_Contribute_Page_Tab extends CRM_Core_Page {
       $this->_action = CRM_Core_Action::ADD;
     }
     else {
-      $this->_contactId = CRM_Utils_Request::retrieve('cid', 'Positive', $this, TRUE);
+      $this->_contactId = CRM_Utils_Request::retrieve('cid', 'Positive', $this, empty($this->_id));
+      if (empty($this->_contactId)) {
+        $this->_contactId = civicrm_api3('contribution', 'getvalue', array('id' => $this->_id, 'return' => 'contact_id'));
+      }
       $this->assign('contactId', $this->_contactId);
 
       // check logged in url permission
