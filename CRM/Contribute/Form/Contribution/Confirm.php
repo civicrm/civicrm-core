civicrm/civicrm-core

commit 133e2c99199891058cbe252dcd4d8e85b22554eb

CRM-13981 fix - Migrate "In Honor of" to Soft Credits
http://issues.civicrm.org/jira/browse/CRM-13981

Conflicts:
	CRM/Upgrade/Incremental/sql/4.5.alpha1.mysql.tpl
	templates/CRM/Contribute/Form/SoftCredit.js

----------------------------------------
* CRM-13981: Migrate "In Honor of" to Soft Credits
  http://issues.civicrm.org/jira/browse/CRM-13981

CRM/Contribute/Form/Contribution/Confirm.php
@@ -376,21 +376,39 @@ class CRM_Contribute_Form_Contribution_Confirm extends CRM_Contribute_Form_Contr
     $params = $this->_params;
     $honor_block_is_active = $this->get('honor_block_is_active');
     // make sure we have values for it
-    if ($honor_block_is_active &&
-      ((!empty($params['honor_first_name']) && !empty($params['honor_last_name'])) ||
-        (!empty($params['honor_email']))
-      )
-    ) {
-      $this->assign('honor_block_is_active', $honor_block_is_active);
-      $this->assign('honor_block_title', CRM_Utils_Array::value('honor_block_title', $this->_values));
+    if ($honor_block_is_active && CRM_Utils_Array::value('soft_credit_type_id', $params)) {
+      $honorName = null;
+      $softCreditTypes = CRM_Core_OptionGroup::values("soft_credit_type", FALSE);
 
-      $prefix = CRM_Core_PseudoConstant::get('CRM_Contact_DAO_Contact', 'prefix_id');
-      $honor = CRM_Core_PseudoConstant::get('CRM_Contribute_DAO_Contribution', 'honor_type_id');
-      $this->assign('honor_type', CRM_Utils_Array::value($params['honor_type_id'], $honor));
-      $this->assign('honor_prefix', CRM_Utils_Array::value($params['honor_prefix_id'], $prefix));
-      $this->assign('honor_first_name', $params['honor_first_name']);
-      $this->assign('honor_last_name', $params['honor_last_name']);
-      $this->assign('honor_email', $params['honor_email']);
+      $this->assign('honor_block_is_active', $honor_block_is_active);
+      $this->assign('soft_credit_type', $softCreditTypes[$params['soft_credit_type_id']]);
+      $profileContactType = CRM_Core_BAO_UFGroup::getContactType($params['honoree_profile_id']);
+      switch ($profileContactType) {
+        case 'Individual':
+          if (array_key_exists('prefix_id', $params['honor'])) {
+            $honorName = CRM_Utils_Array::value(CRM_Utils_Array::value('prefix_id',$params['honor']),
+              CRM_Core_PseudoConstant::get('CRM_Contact_DAO_Contact', 'prefix_id')
+            );
+          }
+          $honorName .= ' ' . $params['honor']['first_name'] . ' ' . $params['honor']['last_name'];
+          if (array_key_exists('suffix_id', $params['honor'])) {
+            $honorName .= ' ' . CRM_Utils_Array::value(CRM_Utils_Array::value('suffix_id',$params['honor']),
+              CRM_Core_PseudoConstant::get('CRM_Contact_DAO_Contact', 'suffix_id')
+            );
+          }
+          break;
+        case 'Organization':
+          $honorName = $params['honor']['organization_name'];
+          break;
+        case 'Household':
+          $honorName = $params['honor']['household_name'];
+          break;
+      }
+      $this->assign('honorName', $honorName);
+
+      $fieldTypes = array('Contact');
+      $fieldTypes[]  = CRM_Core_BAO_UFGroup::getContactType($params['honoree_profile_id']);
+      $this->buildCustom($params['honoree_profile_id'], 'honoreeProfileFields', TRUE, 'honor', $fieldTypes);
     }
     $this->assign('receiptFromEmail', CRM_Utils_Array::value('receipt_from_email', $this->_values));
     $amount_block_is_active = $this->get('amount_block_is_active');
@@ -444,7 +462,7 @@ class CRM_Contribute_Form_Contribution_Confirm extends CRM_Contribute_Form_Contr
         $fieldTypes = array_merge($fieldTypes, array('Contribution'));
       }
 
-      $this->buildCustom($profileId, 'onbehalfProfile', TRUE, TRUE, $fieldTypes);
+      $this->buildCustom($profileId, 'onbehalfProfile', TRUE, 'onbehalf', $fieldTypes);
     }
 
     $this->_separateMembershipPayment = $this->get('separateMembershipPayment');
@@ -507,12 +525,7 @@ class CRM_Contribute_Form_Contribution_Confirm extends CRM_Contribute_Form_Contr
     $defaults = array();
     $fields = array();
     foreach ($this->_fields as $name => $dontCare) {
-      if ($name == 'onbehalf') {
-        foreach ($dontCare as $key => $value) {
-          $fields['onbehalf'][$key] = 1;
-        }
-      }
-      else {
+      if ($name != 'onbehalf' || $name != 'honor') {
         $fields[$name] = 1;
       }
     }
@@ -520,17 +533,7 @@ class CRM_Contribute_Form_Contribution_Confirm extends CRM_Contribute_Form_Contr
 
     $contact = $this->_params;
     foreach ($fields as $name => $dontCare) {
-      if ($name == 'onbehalf') {
-        foreach ($dontCare as $key => $value) {
-          if (isset($contact['onbehalf'][$key])) {
-            $defaults[$key] = $contact['onbehalf'][$key];
-          }
-          if (isset($contact['onbehalf']["{$key}_id"])) {
-            $defaults["{$key}_id"] = $contact['onbehalf']["{$key}_id"];
-          }
-        }
-      }
-      elseif (isset($contact[$name])) {
+      if (isset($contact[$name])) {
         $defaults[$name] = $contact[$name];
         if (substr($name, 0, 7) == 'custom_') {
           $timeField = "{$name}_time";
@@ -971,6 +974,9 @@ class CRM_Contribute_Form_Contribution_Confirm extends CRM_Contribute_Form_Contr
         $fieldTypes
       );
     }
+
+    //processing honor contact into soft-credit contribution
+    CRM_Contact_Form_ProfileContact::postProcess($this);
   }
 
   /**
@@ -1124,11 +1130,7 @@ class CRM_Contribute_Form_Contribution_Confirm extends CRM_Contribute_Form_Contr
   ) {
     $transaction = new CRM_Core_Transaction();
     $className   = get_class($form);
-    $honorCId    = $recurringContributionID = NULL;
-
-    if ($online && $form->get('honor_block_is_active')) {
-      $honorCId = $form->createHonorContact();
-    }
+    $recurringContributionID = NULL;
 
     // add these values for the recurringContrib function ,CRM-10188
     $params['financial_type_id'] = $contributionType->id;
@@ -1142,10 +1144,6 @@ class CRM_Contribute_Form_Contribution_Confirm extends CRM_Contribute_Form_Contr
       $params['is_email_receipt'] = CRM_Utils_Array::value( 'is_email_receipt', $form->_values );
     }
     $recurringContributionID = self::processRecurringContribution($form, $params, $contactID, $contributionType, $online);
-
-    if (!$online && isset($params['honor_contact_id'])) {
-      $honorCId = $params['honor_contact_id'];
-    }
 
     $config = CRM_Core_Config::singleton();
     // CRM-11885
@@ -1262,11 +1260,6 @@ class CRM_Contribute_Form_Contribution_Confirm extends CRM_Contribute_Form_Contr
         'trxn_result_code' => CRM_Utils_Array::value('trxn_result_code', $result),
         'payment_processor' => CRM_Utils_Array::value('payment_processor', $result),
       );
-    }
-
-    if (isset($honorCId)) {
-      $contribParams['honor_contact_id'] = $honorCId;
-      $contribParams['honor_type_id'] = $params['honor_type_id'];
     }
 
     if ($recurringContributionID) {
@@ -1567,42 +1560,6 @@ class CRM_Contribute_Form_Contribution_Confirm extends CRM_Contribute_Form_Contr
     }
 
     return $recurring->id;
-  }
-
-  /**
-   * Create the Honor contact
-   *
-   * @return void
-   * @access public
-   */
-  function createHonorContact() {
-    $params = $this->controller->exportValues('Main');
-
-    // email is enough to create a contact
-    if (! CRM_Utils_Array::value('honor_email', $params) &&
-      // or we need first name AND last name
-      (! CRM_Utils_Array::value('honor_first_name', $params)
-      || ! CRM_Utils_Array::value('honor_last_name', $params))) {
-      //don't create contact - possibly the form was left blank
-      return null;
-    }
-
-    //assign to template for email receipt
-    $honor_block_is_active = $this->get('honor_block_is_active');
-
-    $this->assign('honor_block_is_active', $honor_block_is_active);
-    $this->assign('honor_block_title', CRM_Utils_Array::value('honor_block_title', $this->_values));
-
-    $prefix = CRM_Core_PseudoConstant::get('CRM_Contact_DAO_Contact', 'prefix_id');
-    $honorType = CRM_Core_PseudoConstant::get('CRM_Contribute_DAO_Contribution', 'honor_type_id');
-    $this->assign('honor_type', CRM_Utils_Array::value(CRM_Utils_Array::value('honor_type_id', $params), $honorType));
-    $this->assign('honor_prefix', CRM_Utils_Array::value(CRM_Utils_Array::value('honor_prefix_id', $params), $prefix));
-    $this->assign('honor_first_name', CRM_Utils_Array::value('honor_first_name', $params));
-    $this->assign('honor_last_name', CRM_Utils_Array::value('honor_last_name', $params));
-    $this->assign('honor_email', CRM_Utils_Array::value('honor_email', $params));
-
-    //create honoree contact
-    return CRM_Contribute_BAO_Contribution::createHonorContact($params);
   }
 
   /**

CRM/Contact/Form/ProfileContact.php
@@ -1,0 +1,150 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 4.4                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2013                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License and the CiviCRM Licensing Exception along                  |
+ | with this program; if not, contact CiviCRM LLC                     |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC (c) 2004-2013
+ * $Id$
+ *
+ */
+class CRM_Contact_Form_ProfileContact {
+
+  protected $_mode;
+
+  /**
+   * Function to set variables up before form is built
+   *
+   * @return void
+   * @access public
+   */
+  static function preProcess(&$form) {
+    $session = CRM_Core_Session::singleton();
+    $contactID = $session->get('userID');
+
+    $ufJoinParams = array(
+      'module' => 'soft_credit',
+      'entity_table' => 'civicrm_contribution_page',
+      'entity_id' => $form->_id,
+    );
+    $profileId = CRM_Core_BAO_UFJoin::getUFGroupIds($ufJoinParams);
+    $form->_honoreeProfileId = $profileId[0];
+
+    if (!$form->_honoreeProfileId ||
+      !CRM_Core_DAO::getFieldValue('CRM_Core_DAO_UFGroup', $form->_honoreeProfileId, 'is_active')
+    ) {
+      CRM_Core_Error::fatal(ts('This contribution page has been configured for contribution on behalf of honoree and the selected honoree profile is either disabled or not found.'));
+    }
+
+    $profileContactType = CRM_Core_BAO_UFGroup::getContactType($form->_honoreeProfileId);
+    $requiredProfileFields = array(
+      'Individual' => array('first_name', 'last_name'),
+      'Organization' => array('organization_name', 'email'),
+      'Household' => array('household_name', 'email')
+    );
+    $validProfile = CRM_Core_BAO_UFGroup::checkValidProfile($form->_honoreeProfileId, $requiredProfileFields[$profileContactType]);
+    if (!$validProfile) {
+      CRM_Core_Error::fatal(ts('This contribution page has been configured for contribution on behalf of honoree and the required fields of the selected honoree profile are disabled or doesn\'t exist.'));
+    }
+  }
+
+  /**
+   * Function to build form for honoree contact / on behalf of organization.
+   *
+   * @param $form              object  invoking Object
+   * @param $contactType       string  contact type
+   * @param $title             string  fieldset title
+   *
+   * @static
+   */
+  static function buildQuickForm(&$form) {
+    $ufGroup = new CRM_Core_DAO_UFGroup();
+    $ufGroup->id = $form->_honoreeProfileId;
+    if (!$ufGroup->find(TRUE)) {
+      CRM_Core_Error::fatal(ts('Chosen honoree profile is for this contribution is disabled'));
+    }
+
+    $prefix = 'honor';
+    $honoreeProfileFields = CRM_Core_BAO_UFGroup::getFields($form->_honoreeProfileId, FALSE, NULL,
+        NULL, NULL,
+        FALSE, NULL,
+        TRUE, NULL,
+        CRM_Core_Permission::CREATE
+      );
+    $form->addElement('hidden', 'honoree_profile_id', $form->_honoreeProfileId);
+    $form->assign('honoreeProfileFields', $honoreeProfileFields);
+
+    // add the form elements
+    foreach ($honoreeProfileFields as $name => $field) {
+      // If soft credit type is not chosen then make omit requiredness from honoree profile fields
+      if (count($form->_submitValues) &&
+        !CRM_Utils_Array::value('soft_credit_type_id', $form->_submitValues) &&
+        CRM_Utils_Array::value('is_required', $field)
+      ) {
+        $field['is_required'] = FALSE;
+      }
+      CRM_Core_BAO_UFGroup::buildProfile($form, $field, CRM_Profile_Form::MODE_CREATE, NULL, FALSE, FALSE, NULL, $prefix);
+    }
+  }
+
+  static function postProcess($form) {
+    $params = $form->_params;
+    $values = $form->_values;
+    if ($form->get('honor_block_is_active') && CRM_Utils_Array::value('soft_credit_type_id', $params)) {
+      $honorId = null;
+
+      //check if there is any duplicate contact
+      $profileContactType = CRM_Core_BAO_UFGroup::getContactType($params['honoree_profile_id']);
+      CRM_Core_Error::debug( '$profileContactType', $profileContactType );
+      $dedupeParams = CRM_Dedupe_Finder::formatParams($params['honor'], $profileContactType);
+      $dedupeParams['check_permission'] = FALSE;
+      $ids = CRM_Dedupe_Finder::dupesByParams($dedupeParams, $profileContactType);
+      if(count($ids)) {
+        $honorId = CRM_Utils_Array::value(0, $ids);
+      }
+
+      $honorId = CRM_Contact_BAO_Contact::createProfileContact(
+      $params['honor'], CRM_Core_DAO::$_nullArray,
+      $honorId, NULL,
+      $params['honoree_profile_id']
+      );
+      $softParams = array();
+      $softParams['contribution_id'] = $form->_contributionID;
+      $softParams['contact_id'] = $honorId;
+      $softParams['soft_credit_type_id'] = $params['soft_credit_type_id'];
+      $contribution = new CRM_Contribute_DAO_Contribution();
+      $contribution->id = $form->_contributionID;
+      $contribution->find();
+      while ($contribution->fetch()) {
+        $softParams['currency'] = $contribution->currency;
+        $softParams['amount'] = $contribution->total_amount;
+      }
+      CRM_Contribute_BAO_ContributionSoft::add($softParams);
+    }
+  }
+}
+

CRM/Contribute/BAO/Contribution.php
@@ -342,10 +342,6 @@ class CRM_Contribute_BAO_Contribution extends CRM_Contribute_DAO_Contribution {
       foreach ($softParams as $softParam) {
         $softParam['contribution_id'] = $contribution->id;
         $softParam['currency'] = $contribution->currency;
-        $softParam['pcp_id'] = 'null';
-        $softParam['pcp_display_in_roll'] = 'null';
-        $softParam['pcp_roll_nickname'] = 'null';
-        $softParam['pcp_personal_note'] = 'NULL';
         CRM_Contribute_BAO_ContributionSoft::add($softParam);
       }
     }
@@ -907,44 +903,6 @@ GROUP BY p.id
     else {
       return array(NULL, NULL);
     }
-  }
-
-  /**
-   * Function to create is honor of
-   *
-   * @param array $params  associated array of fields (by reference)
-   * @param int   $honorId honor Id
-   * @param array $honorParams any params that should be send to the create function
-   *
-   * @return contact id
-   */
-  static function createHonorContact(&$params, $honorId = NULL, $honorParams = array()) {
-    $honorParams = array_merge(
-      array(
-        'first_name' => $params['honor_first_name'],
-        'last_name' => $params['honor_last_name'],
-        'prefix_id' => $params['honor_prefix_id'],
-        'email-Primary' => $params['honor_email'],
-      ),
-      $honorParams
-    );
-    if (!$honorId) {
-      $honorParams['email'] = $params['honor_email'];
-
-      $dedupeParams = CRM_Dedupe_Finder::formatParams($honorParams, 'Individual');
-      $dedupeParams['check_permission'] = FALSE;
-      $ids = CRM_Dedupe_Finder::dupesByParams($dedupeParams, 'Individual');
-
-      // if we find more than one contact, use the first one
-      $honorId = CRM_Utils_Array::value(0, $ids);
-    }
-
-    $contactID = CRM_Contact_BAO_Contact::createProfileContact(
-      $honorParams,
-      CRM_Core_DAO::$_nullArray,
-      $honorId
-    );
-    return $contactID;
   }
 
   /**
@@ -1697,7 +1655,7 @@ LEFT JOIN  civicrm_contribution contribution ON ( componentPayment.contribution_
     return $componentDetails;
   }
 
-  static function contributionCount($contactId, $includeSoftCredit = TRUE, $includeHonoree = TRUE) {
+  static function contributionCount($contactId, $includeSoftCredit = TRUE) {
     if (!$contactId) {
       return 0;
     }
@@ -1706,11 +1664,6 @@ LEFT JOIN  civicrm_contribution contribution ON ( componentPayment.contribution_
       SELECT contribution.id AS id
       FROM civicrm_contribution contribution
       WHERE contribution.is_test = 0 AND contribution.contact_id = {$contactId} ";
-
-    $contactHonoreeContributionsSQL = "
-      SELECT contribution.id
-      FROM civicrm_contribution contribution
-      WHERE contribution.is_test = 0 AND contribution.honor_contact_id = {$contactId} ";
 
     $contactSoftCreditContributionsSQL = "
       SELECT contribution.id
@@ -1723,11 +1676,6 @@ LEFT JOIN  civicrm_contribution contribution ON ( componentPayment.contribution_
     if ($includeSoftCredit) {
       $query .= " UNION ";
       $query .= $contactSoftCreditContributionsSQL;
-    }
-
-    if ($includeHonoree) {
-      $query .= " UNION ";
-      $query .= $contactHonoreeContributionsSQL;
     }
 
     $query .= ") x";

CRM/Contribute/BAO/Query.php
@@ -127,38 +127,6 @@ class CRM_Contribute_BAO_Query {
       $query->_tables['contribution_payment_instrument'] = 1;
     }
 
-    // get honor contact name
-    if (CRM_Utils_Array::value('honor_contact_name', $query->_returnProperties)) {
-      $query->_select['contribution_honor_contact_name'] = "civicrm_contact_c.display_name as contribution_honor_contact_name";
-      $query->_element['contribution_honor_contact_name'] = 1;
-      $query->_tables['civicrm_contribution'] = 1;
-      $query->_tables['contribution_honor_contact_name'] = 1;
-    }
-
-    // get honor type label
-    if (CRM_Utils_Array::value('honor_type_label', $query->_returnProperties)) {
-      $query->_select['contribution_honor_type_label'] = "honor_type.label as contribution_honor_type_label";
-      $query->_element['contribution_honor_type_label'] = 1;
-      $query->_tables['civicrm_contribution'] = 1;
-      $query->_tables['contribution_honor_type_label'] = 1;
-    }
-
-    // get honor contact email
-    if (CRM_Utils_Array::value('honor_contact_email', $query->_returnProperties)) {
-      $query->_select['contribution_honor_contact_email'] = "honor_email.email as contribution_honor_contact_email";
-      $query->_element['contribution_honor_contact_email'] = 1;
-      $query->_tables['civicrm_contribution'] = 1;
-      $query->_tables['contribution_honor_contact_email'] = 1;
-    }
-
-    // get honor contact id
-    if (CRM_Utils_Array::value('honor_contact_id', $query->_returnProperties)) {
-      $query->_select['contribution_honor_contact_id'] = "civicrm_contribution.honor_contact_id as contribution_honor_contact_id";
-      $query->_element['contribution_honor_contact_id'] = 1;
-      $query->_tables['civicrm_contribution'] = 1;
-    }
-
-
     if (CRM_Utils_Array::value('check_number', $query->_returnProperties)) {
       $query->_select['contribution_check_number'] = "civicrm_contribution.check_number as contribution_check_number";
       $query->_element['contribution_check_number'] = 1;
@@ -370,22 +338,6 @@ class CRM_Contribute_BAO_Query {
         );
 
         $query->_qill[$grouping][] = ts('Paid By - %1', array(1 => $pis[$pi]));
-        $query->_tables['civicrm_contribution'] = $query->_whereTables['civicrm_contribution'] = 1;
-        return;
-
-      case 'contribution_in_honor_of':
-        $name    = trim($value);
-        $newName = str_replace(',', " ", $name);
-        $pieces  = explode(' ', $newName);
-        foreach ($pieces as $piece) {
-          $value = $strtolower(CRM_Core_DAO::escapeString(trim($piece)));
-          $value = "'%$value%'";
-          $sub[] = " ( contact_b.sort_name LIKE $value )";
-        }
-
-        $query->_where[$grouping][] = ' ( ' . implode('  OR ', $sub) . ' ) ';
-        $query->_qill[$grouping][] = ts('Honor name like - \'%1\'', array(1 => $name));
-        $query->_tables['civicrm_contact_b'] = $query->_whereTables['civicrm_contact_b'] = 1;
         $query->_tables['civicrm_contribution'] = $query->_whereTables['civicrm_contribution'] = 1;
         return;
 
@@ -665,10 +617,6 @@ class CRM_Contribute_BAO_Query {
                                AND option_group_payment_instrument.id = payment_instrument.option_group_id ) ";
         break;
 
-      case 'civicrm_contact_b':
-        $from .= " $side JOIN civicrm_contact contact_b ON (civicrm_contribution.honor_contact_id = contact_b.id )";
-        break;
-
       case 'contribution_status':
         $from = " $side JOIN civicrm_option_group option_group_contribution_status ON (option_group_contribution_status.name = 'contribution_status')";
         $from .= " $side JOIN civicrm_option_value contribution_status ON (civicrm_contribution.contribution_status_id = contribution_status.value
@@ -678,20 +626,6 @@ class CRM_Contribute_BAO_Query {
       case 'contribution_note':
         $from .= " $side JOIN civicrm_note ON ( civicrm_note.entity_table = 'civicrm_contribution' AND
                                                     civicrm_contribution.id = civicrm_note.entity_id )";
-        break;
-
-      case 'contribution_honor_contact_name':
-        $from .= " $side JOIN civicrm_contact civicrm_contact_c ON (civicrm_contribution.honor_contact_id = civicrm_contact_c.id )";
-        break;
-
-      case 'contribution_honor_contact_email':
-        $from .= " $side JOIN civicrm_email as honor_email ON (civicrm_contribution.honor_contact_id = honor_email.contact_id AND honor_email.is_primary = 1 )";
-        break;
-
-      case 'contribution_honor_type_label':
-        $from = " $side JOIN civicrm_option_group option_group_honor_type ON ( option_group_honor_type.name = 'honor_type')";
-        $from .= " $side JOIN civicrm_option_value honor_type ON (civicrm_contribution.honor_type_id = honor_type.value
-                               AND option_group_honor_type.id = honor_type.option_group_id ) ";
         break;
 
       case 'contribution_membership':
@@ -865,9 +799,6 @@ class CRM_Contribute_BAO_Query {
     // Add fields for thank you and receipt
     $form->addYesNo('contribution_thankyou_date_is_not_null', ts('Thank-you sent?'));
     $form->addYesNo('contribution_receipt_date_is_not_null', ts('Receipt sent?'));
-
-    // Add fields for honor search
-    $form->addElement('text', 'contribution_in_honor_of', ts("In Honor Of"));
 
     $form->addYesNo('contribution_pay_later', ts('Contribution is Pay Later?'));
     $form->addYesNo('contribution_recurring', ts('Contribution is Recurring?'));

CRM/Contribute/Form/AdditionalInfo.php
@@ -311,19 +311,6 @@ class CRM_Contribute_Form_AdditionalInfo {
       $params['receipt_date'] = $formatted['receipt_date'] = date('YmdHis');
     }
 
-    if (CRM_Utils_Array::value('honor_type_id', $params)) {
-      if ($form->_honorID) {
-        $honorId = CRM_Contribute_BAO_Contribution::createHonorContact($params, $form->_honorID);
-      }
-      else {
-        $honorId = CRM_Contribute_BAO_Contribution::createHonorContact($params);
-      }
-      $formatted["honor_contact_id"] = $honorId;
-    }
-    else {
-      $formatted["honor_contact_id"] = 'null';
-    }
-
     //special case to handle if all checkboxes are unchecked
     $customFields = CRM_Core_BAO_CustomField::getFields('Contribution',
       FALSE,

commit 3c14c00efb50759ec2460b7418f96ba8b2a4869f

CRM-13981 - Fix default value bug introduced in 133e2c9
This commit cleans up a useless foreach loop that didn't do anything (the conditional would always evaluate to TRUE)
and fixes a bug with setting default values:
It seems that with Quickform, calling $form->setDefults with $defaults['foo']['bar'] = $value is "almost" equivalent to
$defaults["foo[bar]"] = $value but with one key difference: the latter always trumps the former.
So because the buildProfile function uses the latter method to set country default value to defaultContactCountry
(e.g. United States), that would always override the "correct" default value which is set here.

CRM/Contribute/Form/Contribution/Confirm.php
@@ -576,17 +576,18 @@ class CRM_Contribute_Form_Contribution_Confirm extends CRM_Contribute_Form_Contr
     }
 
     $defaults = array();
-    $fields = array();
-    foreach ($this->_fields as $name => $dontCare) {
-      if ($name != 'onbehalf' || $name != 'honor') {
-        $fields[$name] = 1;
-      }
-    }
+    $fields = array_fill_keys(array_keys($this->_fields), 1);
     $fields["billing_state_province-{$this->_bltID}"] = $fields["billing_country-{$this->_bltID}"] = $fields["email-{$this->_bltID}"] = 1;
 
     $contact = $this->_params;
     foreach ($fields as $name => $dontCare) {
-      if (isset($contact[$name])) {
+      // Recursively set defaults for nested fields
+      if (isset($contact[$name]) && is_array($contact[$name]) && ($name == 'onbehalf' || $name == 'honor')) {
+        foreach ($contact[$name] as $fieldName => $fieldValue) {
+          $defaults["{$name}[{$fieldName}]"] = $fieldValue;
+        }
+      }
+      elseif (isset($contact[$name])) {
         $defaults[$name] = $contact[$name];
         if (substr($name, 0, 7) == 'custom_') {
           $timeField = "{$name}_time";