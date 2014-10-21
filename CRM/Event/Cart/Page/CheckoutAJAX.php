baacbf3d86aea4619060109497f1f1d70f815615
Eileen McNaughton <eileen@fuzion.co.nz>
2014-10-22 11:01:45 +1300

    CRM-15510 param retrieval issue
    Conflicts:
    	CRM/Event/Page/AJAX.php

--- CRM/Event/Cart/Page/CheckoutAJAX.php
+++ CRM/Event/Cart/Page/CheckoutAJAX.php
@@ -5,12 +5,11 @@
  */
 class CRM_Event_Cart_Page_CheckoutAJAX {
   function add_participant_to_cart() {
-    require 'CRM/Core/Transaction.php';
     $transaction = new CRM_Core_Transaction();
-    $cart_id     = $_GET['cart_id'];
-    $event_id    = $_GET['event_id'];
+    $cart_id = CRM_Utils_Request::retrieve('cart_id', 'Integer');
+    $event_id = CRM_Utils_Request::retrieve('event_id', 'Integer');
 
-    $cart = CRM_Event_Cart_BAO_Cart::find_by_id($_GET['cart_id']);
+    $cart = CRM_Event_Cart_BAO_Cart::find_by_id($cart_id);
 
 	  $params_array = array('cart_id' => $cart->id, 'contact_id' => CRM_Event_Cart_Form_Cart::find_or_create_contact(), 'event_id' => $event_id);
 
@@ -43,7 +42,8 @@ class CRM_Event_Cart_Page_CheckoutAJAX {
   }
 
   function remove_participant_from_cart() {
-    $participant = CRM_Event_Cart_BAO_MerParticipant::get_by_id($_GET['id']);
+    $id = CRM_Utils_Request::retrieve('id', 'Integer');
+    $participant = CRM_Event_Cart_BAO_MerParticipant::get_by_id($id);
     $participant->delete();
 
     CRM_Utils_System::civiExit();
