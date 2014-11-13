21d2903d2270e61157cad6f831dcee4424ece80a
Anthony Nemirovsky <anemirovsky@giantrabbit.com>
2014-11-13 10:23:17 -0800

    Add hook_civicrm_preProcess function.
    This seems to be a generically useful spot to intercept the form object and
    change things before the form is processed. Specifically, we need this hook
    for integrating the Event Cart with the CiviDiscount extension.

--- CRM/Utils/Hook.php
+++ CRM/Utils/Hook.php
@@ -303,6 +303,18 @@ abstract class CRM_Utils_Hook {
   }
 
   /**
+   * This hook is invoked during the CiviCRM form preProcess phase.
+   *
+   * @param string $formName the name of the form
+   * @param object $form     reference to the form object
+   *
+   * @return null the return value is ignored
+   */
+  static function preProcess($formName, &$form) {
+    return self::singleton()->invoke(2, $formName, $form, $formName, $formName,
+  }
+
+  /**
    * This hook is invoked when building a CiviCRM form. This hook should also
    * be used to set the default values of a form element
    *
