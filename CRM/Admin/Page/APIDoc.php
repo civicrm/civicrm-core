7aecce7455f7eaced6091b84128defa47032c598
Coleman Watts <coleman@civicrm.org>
2014-11-14 22:07:58 -0500

    CRM-15603 - Standardize capitalization of 'API Parameters'

--- CRM/Admin/Page/APIDoc.php
+++ CRM/Admin/Page/APIDoc.php
@@ -42,7 +42,7 @@ class CRM_Admin_Page_APIDoc extends CRM_Core_Page {
    * @return string
    */
   function run() {
-    CRM_Utils_System::setTitle(ts('API parameters'));
+    CRM_Utils_System::setTitle(ts('API Parameters'));
     return parent::run();
   }
 
