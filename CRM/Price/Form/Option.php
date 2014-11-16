6e9019bc92e3a4add5da2e90b5f0fdd86b728b47
Coleman Watts <coleman@civicrm.org>
2014-11-15 21:44:23 -0500

    CRM-15603 - Standardize case of 'Number of Terms'

--- CRM/Price/Form/Option.php
+++ CRM/Price/Form/Option.php
@@ -170,7 +170,7 @@ class CRM_Price_Form_Option extends CRM_Core_Form {
           '' => ' ') + $membershipTypes, FALSE,
           array('onClick' => "calculateRowValues( );")
         );
-        $this->add('text', 'membership_num_terms', ts('Number of terms'), $attributes['membership_num_terms']);
+        $this->add('text', 'membership_num_terms', ts('Number of Terms'), $attributes['membership_num_terms']);
       }
       else {
         $allComponents = explode(CRM_Core_DAO::VALUE_SEPARATOR, $extendComponentId);
