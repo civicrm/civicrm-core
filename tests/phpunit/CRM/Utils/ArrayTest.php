931fa6ec22f69db1d9e4942b56c989bf0a30d3ef
Coleman Watts <coleman@civicrm.org>
2014-11-18 12:45:33 -0500

    CRM_Utils_Array - Remove old unused functions

--- tests/phpunit/CRM/Utils/ArrayTest.php
+++ tests/phpunit/CRM/Utils/ArrayTest.php
@@ -5,24 +5,6 @@ require_once 'CiviTest/CiviUnitTestCase.php';
  * Class CRM_Utils_ArrayTest
  */
 class CRM_Utils_ArrayTest extends CiviUnitTestCase {
-  function testBreakReference() {
-    // Get a reference and make a change
-    $fooRef1 = self::returnByReference();
-    $this->assertEquals('original', $fooRef1['foo']);
-    $fooRef1['foo'] = 'modified';
-
-    // Make sure that the referenced item was actually changed
-    $fooRef2 = self::returnByReference();
-    $this->assertEquals('modified', $fooRef1['foo']);
-    $this->assertEquals('original', $fooRef2['foo']);
-
-    // Get a non-reference, make a change, and make sure the references were unaffected.
-    $fooNonReference = CRM_Utils_Array::breakReference(self::returnByReference());
-    $fooNonReference['foo'] = 'privately-modified';
-    $this->assertEquals('modified', $fooRef1['foo']);
-    $this->assertEquals('original', $fooRef2['foo']);
-    $this->assertEquals('privately-modified', $fooNonReference['foo']);
-  }
 
   /**
    * @return null
