ede2213cb62d162b216bd1090aeccb9a5cb30e0d
monishdeb <monish.deb@webaccessglobal.com>
2014-11-25 18:21:06 +0530

    minor fix

--- CRM/Contribute/BAO/Query.php
+++ CRM/Contribute/BAO/Query.php
@@ -320,6 +320,9 @@ class CRM_Contribute_BAO_Query {
       case 'contribution_trxn_id':
       case (strpos($name, '_amount') !== FALSE):
       case (strpos($name, '_date') !== FALSE):
+        if (!$value) {
+          break;
+        }
         $qillName = $name;
         $pseudoExtraParam = NULL;
         if (in_array($name, array('financial_type', 'contribution_status', 'contribution_payment_instrument'))) {
@@ -368,6 +371,9 @@ class CRM_Contribute_BAO_Query {
 
       case 'contribution_pcp_made_through_id':
       case 'contribution_soft_credit_type_id':
+        if (!$value) {
+          break;
+        }
         $qillName = $name;
         if ($name == 'contribution_pcp_made_through_id') {
           $qillName = $name = 'pcp_id';
