8960019f9fe4e37b61e899160d1f95d8eb823578
Dan Cruson <cruson.web@gmail.com>
2014-12-13 15:24:23 -0500

    File adjustments per Coleman
    Make changes to the file based on feedback from Coleman

--- js/crm.joomla.js
+++ js/crm.joomla.js
@@ -1,7 +1,14 @@
+// http://civicrm.org/licensing
 CRM.$(function($) {
   $(document)
-	.on('dialogopen', function(e) {
+    .on('dialogopen', function(e) {
       // J3 - Make footer admin bar hide behind popup windows (CRM-15723)
       $('#status').css('z-index', '100');
+    })
+    .on('dialogclose', function(e) {
+      if ($('.ui-dialog-content:visible').not(e.target).length < 1) {
+        // D7 hack, restore toolbar position (CRM-15341)
+        $('#toolbar').css('z-index', '');
+      }
     })
 });
