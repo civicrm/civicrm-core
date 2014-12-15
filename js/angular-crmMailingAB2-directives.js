1d4d0279c84ca222de3cd1d97e6bc49f20871405
Tim Otten <totten@civicrm.org>
2014-12-14 20:10:02 -0800

    CRM-15578 - crmMailingAB - Fill in "Setup" and "Targetting" tabs. Implement crmMailingAbBlockSetup directive.

--- js/angular-crmMailingAB2-directives.js
+++ js/angular-crmMailingAB2-directives.js
@@ -6,23 +6,30 @@
   // example:
   //   scope.myAbtest = new CrmMailingAB();
   //   <crm-mailing-ab-block-mailing="{fromAddressA: 1, fromAddressB: 1}" crm-abtest="myAbtest" />
-  angular.module('crmMailingAB2').directive('crmMailingAbBlockMailing', function ($parse) {
-    return {
-      scope: {
-        crmMailingAbBlockMailing: '@',
-        crmAbtest: '@'
-      },
-      templateUrl: partialUrl('joint-mailing.html'),
-      link: function (scope, elm, attr) {
-        var model = $parse(attr.crmAbtest);
-        scope.abtest = model(scope.$parent);
-        scope.crmMailingConst = CRM.crmMailing;
-        scope.ts = CRM.ts('CiviMail');
+  var simpleDirectives = {
+    crmMailingAbBlockMailing: partialUrl('joint-mailing.html'),
+    crmMailingAbBlockSetup: partialUrl('setup.html')
+  };
+  _.each(simpleDirectives, function (templateUrl, directiveName) {
+    angular.module('crmMailingAB2').directive(directiveName, function ($parse, crmMailingABCriteria) {
+      var scopeDesc = {crmAbtest: '@'};
+      scopeDesc[directiveName] = '@';
 
-        var fieldsModel = $parse(attr.crmMailingAbBlockMailing);
-        scope.fields = fieldsModel(scope.$parent);
-      }
-    };
+      return {
+        scope: scopeDesc,
+        templateUrl: templateUrl,
+        link: function (scope, elm, attr) {
+          var model = $parse(attr.crmAbtest);
+          scope.abtest = model(scope.$parent);
+          scope.crmMailingConst = CRM.crmMailing;
+          scope.crmMailingABCriteria = crmMailingABCriteria;
+          scope.ts = CRM.ts('CiviMail');
+
+          var fieldsModel = $parse(attr[directiveName]);
+          scope.fields = fieldsModel(scope.$parent);
+        }
+      };
+    });
   });
 
   // example: <div crm-mailing-ab-slider ng-model="abtest.ab.group_percentage"></div>
