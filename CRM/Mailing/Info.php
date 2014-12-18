84ebf7d6e2e1cc8c10dc2135be79431d7ae9e7a1
Tim Otten <totten@civicrm.org>
2014-12-17 22:38:27 -0800

    CRM-15578 - Rename crmMailing2 to crmMailing

--- CRM/Mailing/Info.php
+++ CRM/Mailing/Info.php
@@ -59,8 +59,8 @@ class CRM_Mailing_Info extends CRM_Core_Component_Info {
     $result = array();
     $result['crmMailing'] = array(
       'ext' => 'civicrm',
-      'js' => array('js/angular-crmMailing2.js', 'js/angular-crmMailing2-services.js', 'js/angular-crmMailing2-directives.js'),
-      'css' => array('css/angular-crmMailing2.css'),
+      'js' => array('js/angular-crmMailing.js', 'js/angular-crmMailing/services.js', 'js/angular-crmMailing/directives.js'),
+      'css' => array('css/angular-crmMailing.css'),
     );
     $result['crmMailingAB2'] = array(
       'ext' => 'civicrm',
