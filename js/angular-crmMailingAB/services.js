818d306d6fa7da7eba22ee9ef24a953dfea47429
Tim Otten <totten@civicrm.org>
2014-12-20 00:50:48 -0800

    CRM-15578 - crmMailingAB - Improve listing
     * Translate strings
     * Remove unused help section
     * Display status

--- js/angular-crmMailingAB/services.js
+++ js/angular-crmMailingAB/services.js
@@ -1,5 +1,19 @@
 (function (angular, $, _) {
 
+  function OptionGroup(values) {
+    this.get = function get(value) {
+      var r = _.where(values, {value: '' + value});
+      return r.length > 0 ? r[0] : null;
+    };
+    this.getByName = function get(name) {
+      var r = _.where(values, {name: '' + name});
+      return r.length > 0 ? r[0] : null;
+    };
+    this.getAll = function getAll() {
+      return values;
+    };
+  }
+
   angular.module('crmMailingAB').factory('crmMailingABCriteria', function () {
     // TODO Get data from server
     var values = {
@@ -7,15 +21,17 @@
       '2': {value: '2', name: 'From names', label: ts('Test different "From" lines')},
       '3': {value: '3', name: 'Two different emails', label: ts('Test entirely different emails')}
     };
-    return {
-      get: function get(value) {
-        var r = _.where(values, {value: '' + value});
-        return r.length > 0 ? r[0] : null;
-      },
-      getAll: function getAll() {
-        return values;
-      }
+    return new OptionGroup(values);
+  });
+
+  angular.module('crmMailingAB').factory('crmMailingABStatus', function () {
+    // TODO Get data from server
+    var values = {
+      '1': {value: '1', name: 'Draft', label: ts('Draft')},
+      '2': {value: '2', name: 'Testing', label: ts('Testing')},
+      '3': {value: '3', name: 'Final', label: ts('Final')}
     };
+    return new OptionGroup(values);
   });
 
   // CrmMailingAB is a data-model which combines an AB test (APIv3 "MailingAB"), three mailings (APIv3 "Mailing"),
