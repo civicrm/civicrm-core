4098f73938480156f322b97435d26e31e1839816
Tim Otten <totten@civicrm.org>
2014-12-22 12:34:34 -0800

    api_v3_PaymentProcessorTest - Change quickCleanup() to useTransaction()

--- tests/phpunit/api/v3/PaymentProcessorTest.php
+++ tests/phpunit/api/v3/PaymentProcessorTest.php
@@ -49,6 +49,7 @@ class api_v3_PaymentProcessorTest extends CiviUnitTestCase {
 
   function setUp() {
     parent::setUp();
+    $this->useTransaction(TRUE);
     // Create dummy processor
     $params = array(
       'name' => 'API_Test_PP_Type',
@@ -66,15 +67,6 @@ class api_v3_PaymentProcessorTest extends CiviUnitTestCase {
       'is_recur' => 0,
       'domain_id' => 1,
     );
-  }
-
-  function tearDown() {
-
-    $tablesToTruncate = array(
-      'civicrm_payment_processor',
-      'civicrm_payment_processor_type',
-    );
-    $this->quickCleanup($tablesToTruncate);
   }
 
   ///////////////// civicrm_payment_processor_add methods
