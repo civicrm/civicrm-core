7aaf6db0e1b2748043a5a5d8848e0b8fbd60f0c0
Tim Otten <totten@civicrm.org>
2015-01-06 11:41:07 -0800

    INFRA-132 - CRM/Campaign - Convert single-line @param to multi-line

--- CRM/Campaign/Selector/Search.php
+++ CRM/Campaign/Selector/Search.php
@@ -134,11 +134,15 @@ class CRM_Campaign_Selector_Search extends CRM_Core_Selector_Base implements CRM
   /**
    * Class constructor
    *
-   * @param array $queryParams array of parameters for query
+   * @param array $queryParams
+   *   Array of parameters for query.
    * @param \const|int $action - action of search basic or advanced.
-   * @param string $surveyClause if the caller wants to further restrict the search.
-   * @param boolean $single are we dealing only with one contact?
-   * @param int $limit how many voters do we want returned
+   * @param string $surveyClause
+   *   If the caller wants to further restrict the search.
+   * @param bool $single
+   *   Are we dealing only with one contact?.
+   * @param int $limit
+   *   How many voters do we want returned.
    *
    * @param string $context
    *
@@ -223,11 +227,16 @@ class CRM_Campaign_Selector_Search extends CRM_Core_Selector_Base implements CRM
   /**
    * Returns all the rows in the given offset and rowCount
    *
-   * @param enum   $action   the action being performed
-   * @param int    $offset   the row number to start from
-   * @param int    $rowCount the number of rows to return
-   * @param string $sort     the sql string that describes the sort order
-   * @param enum   $output   what should the result set include (web/email/csv)
+   * @param enum $action
+   *   The action being performed.
+   * @param int $offset
+   *   The row number to start from.
+   * @param int $rowCount
+   *   The number of rows to return.
+   * @param string $sort
+   *   The sql string that describes the sort order.
+   * @param enum $output
+   *   What should the result set include (web/email/csv).
    *
    * @return int   the total number of rows for this action
    */
@@ -310,8 +319,10 @@ FROM {$from}
    * Returns the column headers as an array of tuples:
    * (name, sortName (key to the sort array))
    *
-   * @param string $action the action being performed
-   * @param enum   $output what should the result set include (web/email/csv)
+   * @param string $action
+   *   The action being performed.
+   * @param enum $output
+   *   What should the result set include (web/email/csv).
    *
    * @return array the column headers that need to be displayed
    */
@@ -360,7 +371,8 @@ FROM {$from}
   /**
    * Name of export file.
    *
-   * @param string $output type of output
+   * @param string $output
+   *   Type of output.
    *
    * @return string name of the file
    */
