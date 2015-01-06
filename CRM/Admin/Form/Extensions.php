5173bd95fdbe5bed239a4b1a93e1536f8a9eb2a5
Tim Otten <totten@civicrm.org>
2015-01-06 11:41:06 -0800

    INFRA-132 - CRM/Admin - Convert single-line @param to multi-line

--- CRM/Admin/Form/Extensions.php
+++ CRM/Admin/Form/Extensions.php
@@ -151,9 +151,12 @@ class CRM_Admin_Form_Extensions extends CRM_Admin_Form {
   /**
    * Global form rule
    *
-   * @param array $fields  the input form values
-   * @param array $files   the uploaded files if any
-   * @param array $self    this object.
+   * @param array $fields
+   *   The input form values.
+   * @param array $files
+   *   The uploaded files if any.
+   * @param array $self
+   *   This object.
    *
    * @return true if no errors, else an array of errors
    * @static
