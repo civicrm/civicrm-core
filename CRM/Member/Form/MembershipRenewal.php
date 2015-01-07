9c8a654fffe834cbc75bdc1bbd4fb45a7415a475
Eileen McNaughton <eileen@fuzion.co.nz>
2015-01-07 16:46:57 +1300

    enotice fixes

--- CRM/Member/Form/MembershipRenewal.php
+++ CRM/Member/Form/MembershipRenewal.php
@@ -786,8 +786,12 @@ WHERE   id IN ( ' . implode(' , ', array_keys($membershipType)) . ' )';
         }
       }
       $formValues['contact_id'] = $this->_contactID;
-
-      CRM_Member_BAO_Membership::recordMembershipContribution(array_merge($formValues, array('membership_id' => $renewMembership->id)));
+      //recordMembershipContribution receives params as a reference & adds one variable. This is
+      // not a great pattern & ideally it would not receive as a reference. We assign our params as a
+      // temporary variable to avoid e-notice & to make it clear to future refactorer that
+      // this function is NOT reliant on that var being set
+      $temporaryParams = array_merge($formValues, array('membership_id' => $renewMembership->id));
+      CRM_Member_BAO_Membership::recordMembershipContribution($temporaryParams);
     }
 
     $receiptSend = FALSE;
