f75ab80739fb26be73f7913872f220456f9ba50c
ginkgoMZD <mzd@ginkgostreet.com>
2015-01-19 16:02:37 -0500

    make function static to stop strict warnings and reflect how it is actually used;

--- CRM/Event/Form/ManageEvent/Registration.php
+++ CRM/Event/Form/ManageEvent/Registration.php
@@ -733,7 +733,7 @@ class CRM_Event_Form_ManageEvent_Registration extends CRM_Event_Form_ManageEvent
    * @return boolean
    */
 
-  function canProfilesDedupe($profileIds, $rgId = 0) {
+  public static function canProfilesDedupe($profileIds, $rgId = 0) {
 
     // find the unsupervised rule
 
