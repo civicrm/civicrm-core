4089fcd9df5b7a0f3af9ddcc123f6172af955799
Monish Deb <monish.deb@jmaconsulting.biz>
2015-01-20 12:04:51 +0530

    CRM-15839 fix - Honoree profile doesn't show on Test contribution page
    https://issues.civicrm.org/jira/browse/CRM-15839

--- CRM/Contribute/Form/SoftCredit.php
+++ CRM/Contribute/Form/SoftCredit.php
@@ -76,7 +76,7 @@ class CRM_Contribute_Form_SoftCredit {
    * @return void
    */
   static function buildQuickForm(&$form) {
-    if ($form->_mode == 'live' && !empty($form->_honor_block_is_active)) {
+    if (!empty($form->_honor_block_is_active)) {
       $ufJoinDAO = new CRM_Core_DAO_UFJoin();
       $ufJoinDAO->module = 'soft_credit';
       $ufJoinDAO->entity_id = $form->_id;
