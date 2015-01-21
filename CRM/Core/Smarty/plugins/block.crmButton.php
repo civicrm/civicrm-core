9fe1d230446f7c269b639abdec59682e0591cc09
Coleman Watts <coleman@civicrm.org>
2015-01-20 21:32:05 -0500

    CRM-15789 - Move htmlAttributes helper to utils class
    ----------------------------------------
    * CRM-15789: Add icons to submit buttons
      https://issues.civicrm.org/jira/browse/CRM-15789

--- CRM/Core/Smarty/plugins/block.crmButton.php
+++ CRM/Core/Smarty/plugins/block.crmButton.php
@@ -33,8 +33,6 @@
  *
  */
 
-require_once 'CRM/Core/Smarty/plugins/function.crmAttributes.php';
-
 /**
  * Generate the html for a button-style link
  *
@@ -49,7 +47,7 @@ require_once 'CRM/Core/Smarty/plugins/function.crmAttributes.php';
  *   The generated html.
  */
 function smarty_block_crmButton($params, $text, &$smarty) {
-  // Generate url (pass 'html' param as false to avoid double-encode by crmAttributes)
+  // Generate url (pass 'html' param as false to avoid double-encode by htmlAttributes)
   $params['href'] = CRM_Utils_System::crmURL($params + array('h' => FALSE));
   // Always add class 'button' - fixme probably should be crm-button
   $params['class'] = 'button ' . CRM_Utils_Array::value('class', $params, '');
@@ -57,6 +55,6 @@ function smarty_block_crmButton($params, $text, &$smarty) {
   $icon = CRM_Utils_Array::value('icon', $params, 'pencil');
   // All other params are treated as html attributes
   CRM_Utils_Array::remove($params, 'icon', 'p', 'q', 'a', 'f', 'h', 'fb', 'fe');
-  $attributes = smarty_function_crmAttributes(array('a' => $params), CRM_Core_DAO::$_nullObject);
-  return "<a$attributes><span><span class='icon ui-icon-$icon'></span> $text</span></a>";
+  $attributes = CRM_Utils_String::htmlAttributes($params);
+  return "<a $attributes><span><span class='icon ui-icon-$icon'></span> $text</span></a>";
 }
