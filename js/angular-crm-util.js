bdd3f7810519cf81e123d0ff3ab20b216f915150
Tim Otten <totten@civicrm.org>
2015-01-23 19:17:43 -0800

    crmMailing - Block Save/Delete/Submit while saving/deleting/submitting.

--- js/angular-crm-util.js
+++ js/angular-crm-util.js
@@ -34,6 +34,26 @@
     };
   });
 
+  // usage:
+  // var block = $scope.block = crmBlocker();
+  // $scope.save = function() { return block(crmApi('MyEntity','create',...)); };
+  // <button ng-click="save()" ng-disabled="block.check()">Do something</button>
+  angular.module('crmUtil').factory('crmBlocker', function() {
+    return function() {
+      var blocks = 0;
+      var result = function(promise) {
+        blocks++;
+        promise.finally(function() {
+          blocks--;
+        });
+      };
+      result.check = function() {
+        return blocks > 0;
+      };
+      return result;
+    };
+  });
+
   angular.module('crmUtil').factory('crmLegacy', function() {
     return CRM;
   });
