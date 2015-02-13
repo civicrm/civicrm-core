22bb0e9df048193cd27ffae299d8353affb6f148
yashodha <yashodha.chaku@webaccessglobal.com>
2015-02-13 13:13:16 +0530

    CRM-15963: fix event title filter
    ----------------------------------------
    * CRM-15963: Event_title can't be filtered with the search builder
      https://issues.civicrm.org/jira/browse/CRM-15963

--- CRM/Event/BAO/Query.php
+++ CRM/Event/BAO/Query.php
@@ -260,12 +260,6 @@ class CRM_Event_BAO_Query {
         );
         return;
 
-      case 'event_title':
-        $query->_where[$grouping][] = CRM_Contact_BAO_Query::buildClause('civicrm_event.title', $op, $value, 'String');
-        $query->_qill[$grouping][] = ts('Event Title') . " $op '{$value}'";
-        $query->_tables['civicrm_event'] = $query->_whereTables['civicrm_event'] = 1;
-        return;
-
       case 'event_include_repeating_events':
         /**
          * Include Repeating Events
@@ -384,10 +378,11 @@ class CRM_Event_BAO_Query {
         $name = str_replace('participant_', '', $name);
       case 'event_is_public':
       case 'event_type_id':
-
+      case 'event_title':
         $qillName = $name;
         if (in_array($name, array(
               'event_id',
+              'event_title',
               'event_is_public',
             )
           )) {
