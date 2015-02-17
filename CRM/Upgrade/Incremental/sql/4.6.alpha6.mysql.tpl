civicrm/civicrm-core

commit 16e3341209f0c40b7d4f9aeca66f1a2021b840af

CRM-15934: more quota patterns
For responses:
Sorry, this user has overdrawn their diskspace quota
user overdrawn his diskspace quota
mail action aborted: exceeded storage allocation
QuotaExceeded
message could not be delivered because the user's mailfolder is full
The recipients inbox is full
recipient doesn't have enough disk space left for it
Mailbox is over the limit
The account is running out of disk space

CRM/Upgrade/Incremental/sql/4.6.alpha6.mysql.tpl
@@ -1,3 +1,17 @@
 {* file to handle db changes in 4.6.alpha6 during upgrade *}
 
 UPDATE `civicrm_navigation` SET url = 'civicrm/api' WHERE url = 'civicrm/api/explorer';
+
+-- CRM-15934
+
+SELECT @bounceTypeID := max(id) FROM civicrm_mailing_bounce_type WHERE name = 'Quota';
+INSERT INTO civicrm_mailing_bounce_pattern (bounce_type_id, pattern)
+    VALUES
+      (@bounceTypeID, 'doesn.t have enough disk space left'),
+      (@bounceTypeID, 'exceeded storage allocation'),
+      (@bounceTypeID, 'running out of disk space');
+
+UPDATE `civicrm_mailing_bounce_pattern` SET `pattern` = 'disk(space)?|over the allowed|exceed(ed|s)?|storage) quota' WHERE `id` = 87;
+UPDATE `civicrm_mailing_bounce_pattern` SET `pattern` = '(mail|in)(box|folder) ((for user \w+ )?is )?full' WHERE `id` = 92;
+UPDATE `civicrm_mailing_bounce_pattern` SET `pattern` = 'mailbox (has exceeded|is over) the limit' WHERE `id` = 93;
+UPDATE `civicrm_mailing_bounce_pattern` SET `pattern` = 'quota ?(usage|violation|exceeded)' WHERE `id` = 98;

xml/templates/civicrm_data.tpl
@@ -1315,20 +1315,23 @@ SELECT @bounceTypeID := max(id) FROM civicrm_mailing_bounce_type WHERE name = 'Q
 INSERT INTO civicrm_mailing_bounce_pattern
         (bounce_type_id, pattern)
         VALUES
-    (@bounceTypeID, '(disk|over the allowed|exceed(ed|s)?|storage) quota'),
+    (@bounceTypeID, '(disk(space)?|over the allowed|exceed(ed|s)?|storage) quota'),
     (@bounceTypeID, '522_mailbox_full'),
     (@bounceTypeID, 'exceeds allowed message count'),
     (@bounceTypeID, 'file too large'),
     (@bounceTypeID, 'full mailbox'),
-    (@bounceTypeID, 'mailbox ((for user \w+ )?is )?full'),
-    (@bounceTypeID, 'mailbox has exceeded the limit'),
+    (@bounceTypeID, '(mail|in)(box|folder) ((for user \w+ )?is )?full'),
+    (@bounceTypeID, 'mailbox (has exceeded|is over) the limit'),
     (@bounceTypeID, 'mailbox( exceeds allowed)? size'),
     (@bounceTypeID, 'no space left for this user'),
     (@bounceTypeID, 'over\\s?quota'),
     (@bounceTypeID, 'quota (for the mailbox )?has been exceeded'),
-    (@bounceTypeID, 'quota (usage|violation|exceeded)'),
+    (@bounceTypeID, 'quota ?(usage|violation|exceeded)'),
     (@bounceTypeID, 'recipient storage full'),
-    (@bounceTypeID, 'not able to receive more mail');
+    (@bounceTypeID, 'not able to receive more mail'),
+    (@bounceTypeID, 'doesn.t have enough disk space left'),
+    (@bounceTypeID, 'exceeded storage allocation'),
+    (@bounceTypeID, 'running out of disk space');
 
 INSERT INTO civicrm_mailing_bounce_type
         (name, description, hold_threshold)

commit 4c43999915210a7b8ef84fbcb84363e7777f43b3

fixed regression from 16e3341209f0

CRM/Upgrade/Incremental/sql/4.6.alpha6.mysql.tpl
@@ -11,7 +11,7 @@ INSERT INTO civicrm_mailing_bounce_pattern (bounce_type_id, pattern)
       (@bounceTypeID, 'exceeded storage allocation'),
       (@bounceTypeID, 'running out of disk space');
 
-UPDATE `civicrm_mailing_bounce_pattern` SET `pattern` = 'disk(space)?|over the allowed|exceed(ed|s)?|storage) quota' WHERE `id` = 87;
+UPDATE `civicrm_mailing_bounce_pattern` SET `pattern` = '(disk(space)?|over the allowed|exceed(ed|s)?|storage) quota' WHERE `id` = 87;
 UPDATE `civicrm_mailing_bounce_pattern` SET `pattern` = '(mail|in)(box|folder) ((for user \w+ )?is )?full' WHERE `id` = 92;
 UPDATE `civicrm_mailing_bounce_pattern` SET `pattern` = 'mailbox (has exceeded|is over) the limit' WHERE `id` = 93;
 UPDATE `civicrm_mailing_bounce_pattern` SET `pattern` = 'quota ?(usage|violation|exceeded)' WHERE `id` = 98;