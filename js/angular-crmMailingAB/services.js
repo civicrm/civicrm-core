00d99fa71b1de7c746994efd91a24886ea15964b
Tim Otten <totten@civicrm.org>
2015-02-19 14:59:04 -0800

    crmMailingAB - report.html - Fix reload after submitting final

--- js/angular-crmMailingAB/services.js
+++ js/angular-crmMailingAB/services.js
@@ -155,7 +155,7 @@
         };
         return crmApi('MailingAB', 'submit', params)
           .then(function () {
-            return crmMailingAB;
+            return crmMailingAB.load();
           });
       },
       // @param mailing Object (per APIv3)
