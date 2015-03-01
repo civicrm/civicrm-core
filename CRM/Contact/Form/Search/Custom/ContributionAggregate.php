17da84f5358c41f6191ea3b6f0e8b2172c021f81
Frank J. Gómez <frank@ginkgostreet.com>
2015-03-01 15:13:08 -0500

    CRM-15965: Updated built-in custom reports to be consistent with changes to CRM_Contact_Form_Search_Interface (namely the addition of the buildTaskList method).

--- CRM/Contact/Form/Search/Custom/ContributionAggregate.php
+++ CRM/Contact/Form/Search/Custom/ContributionAggregate.php
@@ -32,7 +32,7 @@
  * $Id$
  *
  */
-class CRM_Contact_Form_Search_Custom_ContributionAggregate implements CRM_Contact_Form_Search_Interface {
+class CRM_Contact_Form_Search_Custom_ContributionAggregate extends CRM_Contact_Form_Search_Custom_Base implements CRM_Contact_Form_Search_Interface {
 
   protected $_formValues;
   public $_permissionedComponent;
