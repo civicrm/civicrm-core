45ed8cb98b645c32e6c4593367fed0aaa6a6340c
Mathieu Lutfy <mathieu@bidon.ca>
2015-03-04 21:55:30 -0500

    CRM-13584: Minor fix to code style per build feedback.

--- install/index.php
+++ install/index.php
@@ -1263,9 +1263,9 @@ class Installer extends InstallRequirements {
       global $installType, $installURLPath;
 
       $registerSiteURL = "https://civicrm.org/register-site";
-      $commonOutputMessage =
-        "<li>" . ts("Have you registered this site at CiviCRM.org? If not, please help strengthen the CiviCRM ecosystem by taking a few minutes to <a %1>fill out the site registration form</a>. The information collected will help us prioritize improvements, target our communications and build the community. If you have a technical role for this site, be sure to check Keep in Touch to receive technical updates (a low volume mailing list).", array(1 => "href='$registerSiteURL' target='_blank'")) . "</li>"
-      . "<li>" . ts("We have integrated KCFinder with CKEditor and TinyMCE. This allows a user to upload images. All uploaded images are public.") . "</li>";
+      $commonOutputMessage
+        = "<li>" . ts("Have you registered this site at CiviCRM.org? If not, please help strengthen the CiviCRM ecosystem by taking a few minutes to <a %1>fill out the site registration form</a>. The information collected will help us prioritize improvements, target our communications and build the community. If you have a technical role for this site, be sure to check Keep in Touch to receive technical updates (a low volume mailing list).", array(1 => "href='$registerSiteURL' target='_blank'")) . "</li>"
+       . "<li>" . ts("We have integrated KCFinder with CKEditor and TinyMCE. This allows a user to upload images. All uploaded images are public.") . "</li>";
 
       $output = NULL;
 
