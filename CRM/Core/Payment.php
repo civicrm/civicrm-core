25db53da0edef3ed2317cca60d59aa6bbdeda63b
kurund <kurund@civicrm.org>
2015-03-09 01:03:16 +0530

    CRM-15756, add credit card class for validation
    ----------------------------------------
    * CRM-15756: Back-office Submit Credit Card Contribution form: card # validation is no longer working
      https://issues.civicrm.org/jira/browse/CRM-15756

--- CRM/Core/Payment.php
+++ CRM/Core/Payment.php
@@ -318,6 +318,7 @@ abstract class CRM_Core_Payment {
           'size' => 20,
           'maxlength' => 20,
           'autocomplete' => 'off',
+          'classs' => 'creditcard',
         ),
         'is_required' => TRUE,
       ),
