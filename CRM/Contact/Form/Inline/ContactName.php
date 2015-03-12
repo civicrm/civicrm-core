ed645ef976a94620079b30536b4a9c62ba9c6387
Coleman Watts <coleman@civicrm.org>
2015-03-11 21:58:48 -0400

    CRM-16085 - Contact inline edit - Fix handling of empty name/email

--- CRM/Contact/Form/Inline/ContactName.php
+++ CRM/Contact/Form/Inline/ContactName.php
@@ -49,6 +49,31 @@ class CRM_Contact_Form_Inline_ContactName extends CRM_Contact_Form_Inline {
     // Build contact type specific fields
     $class = 'CRM_Contact_Form_Edit_' . $this->_contactType;
     $class::buildQuickForm($this, 1);
+    $this->addFormRule(array('CRM_Contact_Form_Inline_ContactName', 'formRule'), $this);
+  }
+
+  /**
+   * Global validation rules for the form.
+   *
+   * @param array $fields
+   *   Posted values of the form.
+   * @param array $errors
+   *   List of errors to be posted back to the form.
+   * @param CRM_Contact_Form_Inline_ContactName $form
+   *
+   * @return array
+   */
+  public static function formRule($fields, $errors, $form) {
+    if (empty($fields['first_name']) && empty($fields['last_name'])
+      && empty($fields['organization_name'])
+      && empty($fields['household_name'])) {
+      $emails = civicrm_api3('Email', 'getcount', array('contact_id' => $form->_contactId));
+      if (!$emails) {
+        $errorField = $form->_contactType == 'Individual' ? 'last' : strtolower($form->_contactType);
+        $errors[$errorField . '_name'] = ts('Contact with no email must have a name.');
+      }
+    }
+    return $errors;
   }
 
   /**
