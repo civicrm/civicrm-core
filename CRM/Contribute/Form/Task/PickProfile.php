6c6a08db9195db22eade55561e6691283682c1b9
Mathieu Lutfy <mathieu@bidon.ca>
2015-03-14 10:45:48 -0400

    CRM-16111: Translation string fixes (reported by r4zoli).

--- CRM/Contribute/Form/Task/PickProfile.php
+++ CRM/Contribute/Form/Task/PickProfile.php
@@ -96,7 +96,7 @@ class CRM_Contribute_Form_Task_PickProfile extends CRM_Contribute_Form_Task {
     $profiles = CRM_Core_BAO_UFGroup::getProfiles($types, TRUE);
 
     if (empty($profiles)) {
-      CRM_Core_Session::setStatus(ts("You will need to create a Profile containing the %1 fields you want to edit before you can use Batch Update via Profile. Navigate to Administer CiviCRM > Customize Data and Screens > CiviCRM Profile to configure a Profile. Consult the online Administrator documentation for more information.", array(1 => $types[0])), 'Profile Required', 'error');
+      CRM_Core_Session::setStatus(ts("You will need to create a Profile containing the %1 fields you want to edit before you can use Batch Update via Profile. Navigate to Administer CiviCRM > Customize Data and Screens > CiviCRM Profile to configure a Profile. Consult the online Administrator documentation for more information.", array(1 => $types[0])), ts('Profile Required'), 'error');
       CRM_Utils_System::redirect($this->_userContext);
     }
 
