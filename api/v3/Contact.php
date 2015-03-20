328623d3a7f2018a2f63afa01e3e1c6136e89880
Coleman Watts <coleman@civicrm.org>
2015-03-20 15:53:12 -0400

    CRM-16112 - Remove unnecessary fields from contact spec

--- api/v3/Contact.php
+++ api/v3/Contact.php
@@ -265,12 +265,6 @@ function _civicrm_api3_contact_get_spec(&$params) {
     'title' => 'Primary Email',
     'type' => CRM_Utils_Type::T_STRING,
   );
-  $params['gender_id']['title'] = 'Gender ID';
-  $params['gender_id']['type'] = CRM_Utils_Type::T_INT;
-  $params['gender'] = array(
-    'title' => 'Gender',
-    'type' => CRM_Utils_Type::T_STRING,
-  );
   $params['on_hold'] = array(
     'title' => 'Primary Email On Hold',
     'type' => CRM_Utils_Type::T_BOOLEAN,
