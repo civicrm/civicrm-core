3e68351ef12b6c46ae407f0edc80793f62df7e06
Tim Otten <totten@civicrm.org>
2015-04-06 23:36:21 -0700

    CRM-16246 - Load ckeditor in "page-header" instead of "html-header".
    html-header goes through Drupal aggregation, which breaks ckeditor.

--- Civi/Angular/Page/Main.php
+++ Civi/Angular/Page/Main.php
@@ -84,7 +84,7 @@ class Main extends \CRM_Core_Page {
     $this->res->addScriptFile('civicrm', 'bower_components/angular-route/angular-route.min.js', 110, 'html-header', FALSE);
 
     // FIXME: crmUi depends on loading ckeditor, but ckeditor doesn't work with this aggregation.
-    $this->res->addScriptFile('civicrm', 'packages/ckeditor/ckeditor.js', 100, 'html-header', FALSE);
+    $this->res->addScriptFile('civicrm', 'packages/ckeditor/ckeditor.js', 100, 'page-header', FALSE);
 
     $config = \CRM_Core_Config::singleton();
     if ($config->debug) {
