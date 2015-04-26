9bf623e5e866317565df9dd0813061d2461b48d7
Jon goldberg <jon@palantetech.coop>
2015-04-26 07:54:05 -0600

    Incremental progress on DateFilter AJAX

--- templates/CRM/Admin/Page/DateFilter.js
+++ templates/CRM/Admin/Page/DateFilter.js
@@ -1,4 +1,12 @@
 CRM.$(function($) {
   'use strict';
-  console.log('test123'); 
+  //Pull the values for calculating the relative dates from the Form.
+  var relative = $('#relative_terms').val();
+  var units = $('#units').val();
+  var params = {relativeTerms: relative,
+                Units: units};
+  $.getJSON(CRM.url('civicrm/ajax/previewRelativeDateFilter', params), function(data) {
+    $('#relative-date-preview-from').text(data.fromDate);
+    $('#relative-date-preview-to').text(data.toDate);
+  });
 });
