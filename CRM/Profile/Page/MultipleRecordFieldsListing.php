791590dfb29c2ba069c41cf42f9048eb5c5e2e50
pratikshad <pratiksha.dubey@webaccess.co.in>
2015-04-27 15:00:50 +0530

    CRM-16068 Fix-Tab with table date field shows wrong date format
    ----------------------------------------
    * CRM-16068: Tab with table date field shows wrong date format
      https://issues.civicrm.org/jira/browse/CRM-16068

--- CRM/Profile/Page/MultipleRecordFieldsListing.php
+++ CRM/Profile/Page/MultipleRecordFieldsListing.php
@@ -235,6 +235,9 @@ class CRM_Profile_Page_MultipleRecordFieldsListing extends CRM_Core_Page_Basic {
         CRM_Core_DAO::commonRetrieve('CRM_Core_DAO_CustomField', $param, $returnValues, $returnProperities);
         if ($returnValues['data_type'] == 'Date') {
           $dateFields[$fieldIDs[$key]] = 1;
+          $actualPHPFormats = CRM_Core_SelectValues::datePluginToPHPFormats();
+          $dateFormat = (array) CRM_Utils_Array::value($returnValues['date_format'], $actualPHPFormats);
+          $timeFormat = CRM_Utils_Array::value('time_format', $returnValues);
         }
 
         $optionValuePairs = CRM_Core_BAO_CustomOption::getCustomOption($fieldIDs[$key]);
@@ -300,6 +303,22 @@ class CRM_Profile_Page_MultipleRecordFieldsListing extends CRM_Core_Page_Basic {
               if (!empty($dateFields) && array_key_exists($fieldId, $dateFields)) {
                 // formated date capture value capture
                 $dateFieldsVals[$fieldId][$recId] = CRM_Core_BAO_CustomField::getDisplayValue($customValue, $fieldId, $options);
+
+                //set date and time format
+                switch ($timeFormat) {
+                  case 1:
+                    $dateFormat[] = 'g:iA';
+                    break;
+
+                  case 2:
+                    $dateFormat[] = 'G:i';
+                    break;
+
+                  default:
+                    // if time is not selected remove time from value
+                    $result[$recId][$fieldId] = substr($result[$recId][$fieldId], 0, 10);
+                }
+                $result[$recId][$fieldId] = CRM_Utils_Date::processDate($result[$recId][$fieldId], NULL, FALSE, implode(" ", $dateFormat));
               }
               else {
                 // assign to $result
