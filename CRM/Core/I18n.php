1e6ad51aea20f75d8205966647db87d2beac74ae
Samuel Vanhove <samuel@symbiotic.coop>
2015-04-28 20:06:14 -0400

    CRM-16355 - checkstyle typos

--- CRM/Core/I18n.php
+++ CRM/Core/I18n.php
@@ -541,7 +541,7 @@ class CRM_Core_I18n {
    *   True if the domain was changed for an extension.
    */
   public function setLanguage($language) {
- 
+
     $config = CRM_Core_Config::singleton();
     if ($this->_nativegettext) {
       $locale = $language . '.utf8';
