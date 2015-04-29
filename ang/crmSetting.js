03cbe62ac0af827b2ba9aa3e338540ca5c5cb34e
Frank J. Gómez <frank@ginkgostreet.com>
2015-04-29 02:14:21 -0600

    CRM-16373: Work in progress. Added angular module for converting field metadata to field markup. Have mostly implemented the text and select widgets.

--- ang/crmSetting.js
+++ ang/crmSetting.js
@@ -1,6 +1,6 @@
 (function(angular, $, _) {
   // Declare a list of dependencies.
   angular.module('crmSetting', [
-    'crmUi', 'crmUtil', 'ngRoute'
+    'crmField', 'crmUi', 'crmUtil', 'ngRoute'
   ]);
 })(angular, CRM.$, CRM._);
