c098ca551b49533ee1dff7d54028223fdcec60de
Jon goldberg <jon@palantetech.coop>
2015-04-29 17:21:51 -0600

    CRM-16195 - DateFilter AJAX now fires whenever date selectors are changed

--- templates/CRM/Admin/Page/DateFilter.js
+++ templates/CRM/Admin/Page/DateFilter.js
@@ -1,12 +1,16 @@
 CRM.$(function($) {
   'use strict';
-  //Pull the values for calculating the relative dates from the Form.
-  var relative = $('#relative_terms').val();
-  var units = $('#units').val();
-  var params = {relativeTerms: relative,
-                Units: units};
-  $.getJSON(CRM.url('civicrm/ajax/previewRelativeDateFilter', params), function(data) {
-    $('#relative-date-preview-from').text(data.fromDate);
-    $('#relative-date-preview-to').text(data.toDate);
-  });
+  function getRelativeDates() {
+    //Pull the values for calculating the relative dates from the Form.
+    var relative = $('#relative_terms').val();
+    var units = $('#units').val();
+    var params = {relativeTerms: relative,
+                  Units: units};
+    $.getJSON(CRM.url('civicrm/ajax/previewRelativeDateFilter', params), function(data) {
+      $('#relative-date-preview-from').text(data.fromDate);
+      $('#relative-date-preview-to').text(data.toDate);
+    });
+  }
+  $('.crm-admin-relative-date-select').on('change', getRelativeDates);
+  getRelativeDates();
 });
