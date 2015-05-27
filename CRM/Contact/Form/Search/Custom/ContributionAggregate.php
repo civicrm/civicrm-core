822019c0af260f2fef17ad2fc1226cab5ac41d3d
jitendrapurohit <jitendra.purohit@webaccessglobal.com>
2015-05-27 16:58:38 +0530

    warning fix

--- CRM/Contact/Form/Search/Custom/ContributionAggregate.php
+++ CRM/Contact/Form/Search/Custom/ContributionAggregate.php
@@ -271,7 +271,7 @@ civicrm_contact AS contact_a
    * @param int $offset
    * @param int $rowcount
    * @param null $sort
-   * @param boolean $returnSQL Not used; included for consistency with parent; SQL is always returned
+   * @param bool $returnSQL Not used; included for consistency with parent; SQL is always returned
    *
    * @return string
    */
