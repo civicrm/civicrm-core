9b4c005adf2d23c3718a841e2249fb46143e1f63
Pradeep Nayak <pradpnayak@gmail.com>
2015-05-29 04:01:59 +0530

    --CRM-16188, added get Order api

---
+++ api/v3/Order.php
@@ -0,0 +1,93 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 4.7                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2015                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License and the CiviCRM Licensing Exception along                  |
+ | with this program; if not, contact CiviCRM LLC                     |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+ */
+
+/**
+ * This api exposes CiviCRM Order records.
+ *
+ * @package CiviCRM_APIv3
+ */
+
+/**
+ * Add or update a Order.
+ *
+ * @param array $params
+ *   Input parameters.
+ *
+ * @throws API_Exception
+ * @return array
+ *   Api result array
+ */
+function civicrm_api3_order_create(&$params) {}
+
+/**
+ * Delete a Order.
+ *
+ * @param array $params
+ *   Input parameters.
+ *
+ * @return array
+ */
+function civicrm_api3_order_delete($params) {}
+
+/**
+ * Retrieve a set of Order.
+ *
+ * @param array $params
+ *  Input parameters.
+ *
+ * @return array
+ *   Array of Order, if error an array with an error id and error message
+ */
+function civicrm_api3_order_get($params) {
+  $contributions = array();
+  $params['api.line_item.get'] = array('qty' => array('<>' => 0));
+  $result = civicrm_api3('Contribution', 'get', $params);
+  if (!empty($result['values'])) {
+    foreach ($result['values'] as $key => $contribution) {
+      $contributions[$key] = $contribution;
+      $contributions[$key]['line_items'] = $contribution['api.line_item.get']['values'];
+      unset($contributions[$key]['api.line_item.get']);
+    }
+  }
+  return civicrm_api3_create_success($contributions, $params, 'Order', 'get');  
+}
+
+/**
+ * Adjust Metadata for Get action.
+ *
+ * The metadata is used for setting defaults, documentation & validation.
+ *
+ * @param array $params
+ *   Array of parameters determined by getfields.
+ */
+function _civicrm_api3_order_get_spec(&$params) {
+  $params['contribution_id'] = array(
+    'api.required' => 1 ,
+    'title' => 'Contribution ID',
+    'type' => CRM_Utils_Type::T_INT,
+  );
+}
