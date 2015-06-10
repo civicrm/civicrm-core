9ca447759c90e5c01db83be9a8a1e518561ee53a
Coleman Watts <coleman@civicrm.org>
2015-06-10 10:53:41 -0400

    CRM-16650 - Improve entityRef fields for shortcodes
    ----------------------------------------
    * CRM-16650: Allow hooks to modify available shortcodes
      https://issues.civicrm.org/jira/browse/CRM-16650

--- CRM/Core/Form/ShortCode.php
+++ CRM/Core/Form/ShortCode.php
@@ -39,22 +39,36 @@
  */
 class CRM_Core_Form_ShortCode extends CRM_Core_Form {
   /**
-   * List of entities supported by shortcodes, and their form properties
+   * List of entities supported by shortcodes, and their form properties.
+   *
+   * Keys should be the "component" string for the shortcode
+   * Values should be an array with label and select.
+   * Select can be NULL if there is no entity to select.
+   * Otherwise it contains the shortcode key for this entity id (usually 'id') plus an array of params for the EntityRef field
+   * @see CRM_Core_Form::addEntityRef
    *
    * @var array
+   *   [component => [
+   *     label => Option Label
+   *     select => key + EntityRef params
+   *   ]]
    */
   public $components = array();
 
   /**
-   * List of options to display on the form
+   * List of radio option groups to display on the form
+   *
+   * Control the conditional logic of showing/hiding each group via the "components" array.
+   * Or set 'components' => TRUE if it applies to all
    *
    * @var array
+   *   [key, components, options]
    */
   public $options = array();
 
 
   /**
-   * Build form data. Overridable via hook_civicrm_preProcess
+   * Build form data. Can be modified via hook_civicrm_preProcess
    *
    * @return void
    */
@@ -69,8 +83,13 @@ class CRM_Core_Form_ShortCode extends CRM_Core_Form {
       'label' => ts("Profile"),
       'select' => array(
         'key' => 'gid',
-        'entity' => 'Profile',
+        'entity' => 'UFGroup',
         'select' => array('minimumInputLength' => 0),
+        'api' => array(
+          'params' => array(
+            'id' => $this->profileAccess(),
+          ),
+        ),
       ),
     );
 
@@ -103,6 +122,11 @@ class CRM_Core_Form_ShortCode extends CRM_Core_Form {
           'key' => 'id',
           'entity' => 'Survey',
           'select' => array('minimumInputLength' => 0),
+          'api' => array(
+            'params' => array(
+              'activity_type_id' => "Petition",
+            ),
+          ),
         ),
       );
     }
@@ -136,7 +160,7 @@ class CRM_Core_Form_ShortCode extends CRM_Core_Form {
       ),
       array(
         'key' => 'hijack',
-        'components' => '*',
+        'components' => TRUE,
         'label' => ts('If you only insert one shortcode, you can choose to override all page content with the content of the shortcode.'),
         'options' => array(
           '0' => ts("Don't override"),
@@ -164,7 +188,7 @@ class CRM_Core_Form_ShortCode extends CRM_Core_Form {
     $options = $defaults = array();
     foreach ($this->options as $num => $field) {
       $this->addRadio("option_$num", CRM_Utils_Array::value('label', $field), $field['options'], array('allowClear' => FALSE, 'data-key' => $field['key']));
-      if ($field['components'] === '*') {
+      if ($field['components'] === TRUE) {
         $field['components'] = array_keys($this->components);
       }
       $options["option_$num"] = $field;
@@ -177,6 +201,32 @@ class CRM_Core_Form_ShortCode extends CRM_Core_Form {
     $this->assign('options', $options);
     $this->assign('selects', array_keys(array_filter($data)));
     $this->setDefaults($defaults);
+  }
+
+  /**
+   * The CiviCRM api (and therefore EntityRef) does not support OR logic, ACLs or joins.
+   *
+   * I'm not proud of this, but here's a workaround to pre-filter the api params
+   *
+   * @return array
+   */
+  private function profileAccess() {
+    $sql = "
+      SELECT g.id
+      FROM   civicrm_uf_group g, civicrm_uf_join j
+      WHERE  g.is_active = 1
+      AND    j.is_active = 1
+      AND    ( group_type LIKE '%Individual%'
+         OR    group_type LIKE '%Contact%' )
+      AND    g.id = j.uf_group_id
+      AND    j.module = 'Profile'
+      ";
+    $dao = CRM_Core_DAO::executeQuery($sql);
+    $ids = array();
+    while ($dao->fetch()) {
+      $ids[] = $dao->id;
+    }
+    return array('IN' => $ids);
   }
 
   // No postProccess fn; this form never gets submitted
