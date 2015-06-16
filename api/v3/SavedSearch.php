b4ee829e27cd16cc69437cd8533c7cd7b8db9081
Johan Vervloet <johanv@johanv.org>
2015-06-16 23:05:04 +0200

    CRM-16701 - very basic api implementation.
    ----------------------------------------
    * CRM-16701: Create API for saved searches
      https://issues.civicrm.org/jira/browse/CRM-16701

---
+++ api/v3/SavedSearch.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 4.6                                                |
+ +--------------------------------------------------------------------+
+ | Copyright Chirojeugd-Vlaanderen vzw 2015                           |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License and the CiviCRM Licensing Exception along                  |
+ | with this program; if not, contact CiviCRM LLC                     |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+ */
+
+/**
+ * This api exposes CiviCRM saved searches.
+ *
+ * @package CiviCRM_APIv3
+ */
+
+/**
+ * Create a saved search.
+ *
+ * @param array $params
+ *
+ * @return array
+ */
+function civicrm_api3_saved_search_create($params) {
+  return _civicrm_api3_basic_create(_civicrm_api3_get_BAO(__FUNCTION__), $params);
+}
+
+/**
+ * Adjust Metadata for Create action.
+ *
+ * The metadata is used for setting defaults, documentation & validation.
+ *
+ * @param array $params
+ *   Array of parameters determined by getfields.
+ */
+function _civicrm_api3_saved_search_create_spec(&$params) {
+  // TODO: find out wherther something needs to be done here.
+}
+
+/**
+ * Deletes an existing saved search.
+ *
+ * @param array $params
+ *
+ * @return array
+ *   API result Array
+ */
+function civicrm_api3_saved_search_delete($params) {
+  return _civicrm_api3_basic_delete(_civicrm_api3_get_BAO(__FUNCTION__), $params);
+}
+
+/**
+ * Retrieve one or more saved search(es).
+ *
+ * @param array $params
+ *   An associative array of name/value pairs.
+ *
+ * @return array
+ *   details of found saved searches
+ */
+function civicrm_api3_saved_search_get($params) {
+  return _civicrm_api3_basic_get(_civicrm_api3_get_BAO(__FUNCTION__), $params);
+}
