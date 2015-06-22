f00c38c85eabe6671038b4d8e06a112dffd33c26
Johan Vervloet <johanv@johanv.org>
2015-06-22 12:55:43 +0200

    CRM-16701 - Fix in cleanup result values of SavedSearch API.
    If there are no result values (e.g. for a getCount operation), the
    API should not try to unserialize the form_values fields of the
    result values.
    
    ----------------------------------------
    * CRM-16701: Create API for saved searches
      https://issues.civicrm.org/jira/browse/CRM-16701

--- api/v3/SavedSearch.php
+++ api/v3/SavedSearch.php
@@ -99,7 +99,11 @@ function civicrm_api3_saved_search_get($params) {
  * @param array $result API result to be cleaned up.
  */
 function _civicrm_api3_saved_search_result_cleanup(&$result) {
-  foreach ($result['values'] as $key => $value) {
-    $result['values'][$key]['form_values'] = unserialize($value['form_values']);
+  // Only clean up the values if there are values. (A getCount operation
+  // for example does not return values.)
+  if (isset($result['values'])) {
+    foreach ($result['values'] as $key => $value) {
+      $result['values'][$key]['form_values'] = unserialize($value['form_values']);
+    }
   }
 }
