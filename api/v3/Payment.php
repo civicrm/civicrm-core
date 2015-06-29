1865bc47c657e38221aad0807b2413d9a21d6919
Edsel <edsel.lopez@jmaconsulting.biz>
2015-06-29 13:15:45 +0530

    RC-24 Bug fix

--- api/v3/Payment.php
+++ api/v3/Payment.php
@@ -83,11 +83,10 @@ function civicrm_api3_payment_create(&$params) {
     $trxn = CRM_Contribute_BAO_Contribution::recordPartialPayment($contribution, $params);
     $balance = CRM_Core_BAO_FinancialTrxn::getBalanceTrxnAmt($params['contribution_id']);
     $total = CRM_Price_BAO_LineItem::getLineTotal($params['contribution_id'], 'civicrm_contribution');
-    $paid = $balance['total_amount'] - $total;
-    if ($total >= $paid) {
+    if ((float)$total <= (float)$paid) {
       $params['contribution_status_id'] = CRM_Core_OptionGroup::getValue('contribution_status', 'Completed', 'name');
     }
-    else if ($total < $paid) {
+    else if ((float)$total > (float)$paid) {
       $params['contribution_status_id'] = CRM_Core_OptionGroup::getValue('contribution_status', 'Partially paid', 'name');
     }
   }
