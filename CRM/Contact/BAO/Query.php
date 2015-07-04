e3de8fde2a6d1864152cecee28f6eda07016c02b
Monish Deb <monish.deb@jmaconsulting.biz>
2015-07-04 20:00:02 +0530

    api failure fixes

--- CRM/Contact/BAO/Query.php
+++ CRM/Contact/BAO/Query.php
@@ -5654,7 +5654,7 @@ AND   displayRelType.is_active = 1
       $qillString = array();
       if (!empty($pseduoOptions)) {
         foreach ((array) $fieldValue as $val) {
-          $qillString[] = $pseduoOptions[$val];
+          $qillString[] = CRM_Utils_Array::value($val, $pseduoOptions, $val);
         }
         $fieldValue = implode(', ', $qillString);
       }
