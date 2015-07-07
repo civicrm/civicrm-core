d15c7cff49cb403889e1148c1adbf070e19a5951
monishdeb <monish.deb@webaccessglobal.com>
2015-07-07 15:58:46 +0530

    minor fix

--- CRM/Contribute/Form/Contribution.php
+++ CRM/Contribute/Form/Contribution.php
@@ -1203,10 +1203,8 @@ class CRM_Contribute_Form_Contribution extends CRM_Contribute_Form_AbstractEditP
       }
 
       $params['pcp'] = !empty($pcp) ? $pcp : NULL;
-      if (!empty($softParams)) {
-        $params['soft_credit'] = $softParams;
-        $params['soft_credit_ids'] = $softIDs;
-      }
+      $params['soft_credit'] = !empty($softParams) ? $softParams : array();
+      $params['soft_credit_ids'] = !empty($softIDs) ? $softIDs : array();
 
       // CRM-5740 if priceset is used, no need to cleanup money.
       if ($priceSetId) {
