a927919a4975f989fe5464e1bcd5223631d75807
Tim Otten <totten@civicrm.org>
2015-07-07 22:48:13 -0700

    CRM_Utils_Array::subset - Backport

--- CRM/Utils/Array.php
+++ CRM/Utils/Array.php
@@ -678,5 +678,26 @@ class CRM_Utils_Array {
 
     return $results;
   }
+
+  /**
+   * Extract any $keys from $array and copy to a new array.
+   *
+   * Note: If a $key does not appear in $array, then it will
+   * not appear in the result.
+   *
+   * @param array $array
+   * @param array $keys
+   *   List of keys to copy.
+   * @return array
+   */
+  public static function subset($array, $keys) {
+    $result = array();
+    foreach ($keys as $key) {
+      if (isset($array[$key])) {
+        $result[$key] = $array[$key];
+      }
+    }
+    return $result;
+  }
 }
 
