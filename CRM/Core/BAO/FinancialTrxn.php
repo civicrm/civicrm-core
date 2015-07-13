91ffcd022011f45d05ca139554c84577fe61f556
Edsel <edsel.lopez@jmaconsulting.biz>
2015-07-13 18:04:50 +0530

    RC-24 Bug fix to prevent addition in PHP, use SQL instead

--- CRM/Core/BAO/FinancialTrxn.php
+++ CRM/Core/BAO/FinancialTrxn.php
@@ -506,19 +506,14 @@ WHERE pp.participant_id = {$entityId} AND ft.to_financial_account_id != {$toFina
       $contributionFinancialTypeId = CRM_Core_DAO::getFieldValue('CRM_Contribute_BAO_Contribution', $contributionId, 'financial_type_id');
     }
     $statusId = CRM_Core_OptionGroup::getValue('contribution_status', 'Completed', 'name');
-    $sql = "SELECT ft.total_amount FROM civicrm_financial_trxn ft 
+    $sql = "SELECT SUM(ft.total_amount) FROM civicrm_financial_trxn ft 
       INNER JOIN civicrm_entity_financial_trxn eft ON (eft.financial_trxn_id = ft.id AND eft.entity_table = 'civicrm_contribution') 
-      WHERE eft.entity_id = %1 AND ft.is_payment = 1 AND ft.status_id = $statusId";
+      WHERE eft.entity_id = %1 AND ft.is_payment = 1 AND ft.status_id = $statusId
+      GROUP BY ft.id";
 
     $params[1] = array($contributionId, 'Integer');
 
-    $totalAmtDAO = CRM_Core_DAO::executeQuery($sql, $params);
-    $paid = 0;
-    while ($totalAmtDAO->fetch()) {
-      $paid += $totalAmtDAO->total_amount;
-    }
-
-    return $paid;
+    return CRM_Core_DAO::singleValueQuery($sql, $params);
   }
 
 }
