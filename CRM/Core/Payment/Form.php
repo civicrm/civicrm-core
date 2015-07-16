08979e42506e06370d1291588edd1a7d01c16f67
colemanw <coleman@civicrm.org>
2015-07-16 14:20:12 -0400

    Remove stray merge msg

--- CRM/Core/Payment/Form.php
+++ CRM/Core/Payment/Form.php
@@ -299,8 +299,6 @@ class CRM_Core_Payment_Form {
   }
 
   /**
-<<<<<<< HEAD
-=======
    * Billing mode button is basically synonymous with paypal express  - this is probably a good example of 'odds & sods' code we
    * need to find a way for the payment processor to assign. A tricky aspect is that the payment processor may need to set the order
    *
