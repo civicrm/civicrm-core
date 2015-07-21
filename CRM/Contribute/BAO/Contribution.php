8322d1d06cfd96652f21fe4ac2f46e1ac991e584
Edsel <edsel.lopez@jmaconsulting.biz>
2015-07-21 13:09:17 +0530

    RC-24 Fiixed warnings reported by Jenkins

--- CRM/Contribute/BAO/Contribution.php
+++ CRM/Contribute/BAO/Contribution.php
@@ -2959,7 +2959,7 @@ INNER JOIN civicrm_activity ON civicrm_activity_contact.activity_id = civicrm_ac
         $balanceTrxnParams['status_id'] = $statusId;
         $balanceTrxnParams['payment_instrument_id'] = $params['contribution']->payment_instrument_id;
         $balanceTrxnParams['check_number'] = CRM_Utils_Array::value('check_number', $params);
-        if (!empty($balanceTrxnParams['from_financial_account_id']) && 
+        if (!empty($balanceTrxnParams['from_financial_account_id']) &&
             ($statusId == array_search('Completed', $contributionStatuses) || $statusId == array_search('Partially paid', $contributionStatuses))) {
           $balanceTrxnParams['is_payment'] = 1;
         }
@@ -3210,7 +3210,7 @@ INNER JOIN civicrm_activity ON civicrm_activity_contact.activity_id = civicrm_ac
       && $context == 'changePaymentInstrument'
     ) {
       return;
-    } 
+    }
     if (($params['prevContribution']->contribution_status_id == array_search('Partially paid', $contributionStatus))
       && $params['contribution']->contribution_status_id == array_search('Completed', $contributionStatus)
       && $context == 'changedStatus'
@@ -4542,7 +4542,7 @@ LIMIT 1;";
     $balanceTrxnParams['status_id'] = $statusId;
     $balanceTrxnParams['payment_instrument_id'] = CRM_Utils_Array::value('payment_instrument_id', $params, $contribution['payment_instrument_id']);
     $balanceTrxnParams['check_number'] = CRM_Utils_Array::value('check_number', $params);
-    if ($fromFinancialAccountId != NULL && 
+    if ($fromFinancialAccountId != NULL &&
         ($statusId == array_search('Completed', $contributionStatuses) || $statusId == array_search('Partially paid', $contributionStatuses))) {
       $balanceTrxnParams['is_payment'] = 1;
     }
@@ -4554,8 +4554,9 @@ LIMIT 1;";
 
   public static function addPayments($lineItems, $contributions) {
     foreach ($contributions as $k => $contribution) {
-      if ($contribution->contribution_status_id != CRM_Core_OptionGroup::getValue('contribution_status', 'Partially paid', 'name')) 
+      if ($contribution->contribution_status_id != CRM_Core_OptionGroup::getValue('contribution_status', 'Partially paid', 'name')) {
         continue;
+      }
       // get financial trxn which is a payment
       $sql = "SELECT ft.id 
       FROM civicrm_financial_trxn ft 
@@ -4572,7 +4573,7 @@ LIMIT 1;";
         $ftIds[$dao->price_field_value_id] = $dao->id;
       }
       foreach ($lineItems as $key => $value) {
-        $paid = $value['line_total'] * ($contribution->net_amount/$contribution->total_amount);
+        $paid = $value['line_total'] * ($contribution->net_amount / $contribution->total_amount);
         // Record Entity Financial Trxn
         $params = array(
           'entity_table' => 'civicrm_financial_item',
@@ -4600,7 +4601,7 @@ LIMIT 1;";
         $ftIds[$dao->price_field_value_id] = $dao->id;
       }
       foreach ($lineItems as $key => $value) {
-        $paid = $value['line_total'] * ($params['total_amount']/$contribution['total_amount']);
+        $paid = $value['line_total'] * ($params['total_amount'] / $contribution['total_amount']);
         // Record Entity Financial Trxn
         $eftParams = array(
           'entity_table' => 'civicrm_financial_item',
--- CRM/Contribute/Page/UserDashboard.php
+++ CRM/Contribute/Page/UserDashboard.php
@@ -158,7 +158,7 @@ class CRM_Contribute_Page_UserDashboard extends CRM_Contact_Page_View_UserDashBo
     $invoicing = CRM_Utils_Array::value('invoicing', $invoiceSettings);
     $this->assign('invoicing', $invoicing);
     parent::preProcess();
-    $this->listContribution(); 
+    $this->listContribution();
     $userOptions = CRM_Core_BAO_Setting::valueOptions(CRM_Core_BAO_Setting::SYSTEM_PREFERENCES_NAME, 'user_dashboard_options');
     if (CRM_Utils_Array::value('Payments', $userOptions)) {
       $this->listPayments();
