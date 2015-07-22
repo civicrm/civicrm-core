8893a1fb71f9433ab23b7656c1d9ba31c48198ce
Pradeep Nayak <pradpnayak@gmail.com>
2015-07-22 17:32:55 +0530

    --ICM-15, added condition to check if FT-ACL is enabled or not

--- CRM/Event/Form/EventFees.php
+++ CRM/Event/Form/EventFees.php
@@ -410,7 +410,8 @@ SELECT  id, html_type
           $element->freeze();
         }
       }
-      if (!CRM_Utils_Array::value('fee',  $form->_values) 
+      if (CRM_Financial_BAO_FinancialType::isACLFinancialTypeStatus()
+        && !CRM_Utils_Array::value('fee',  $form->_values) 
         && CRM_Utils_Array::value('snippet',  $_REQUEST) == CRM_Core_Smarty::PRINT_NOFORM
       ) {
         $form->assign('isFTPermissionDenied', TRUE);
