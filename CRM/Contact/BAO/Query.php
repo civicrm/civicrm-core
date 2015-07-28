c0b154eb85735718ed866e9261df4096a0346c91
Eileen McNaughton <eileen@fuzion.co.nz>
2015-07-28 16:23:16 +1200

    CRM-16922 broken smart groups from participant_status_id

--- CRM/Contact/BAO/Query.php
+++ CRM/Contact/BAO/Query.php
@@ -5198,7 +5198,14 @@ SELECT COUNT( conts.total_amount ) as cancel_count,
               return $queryString;
             }
           }
+          else {
+            $dragonPlace = $iAmAnIntentionalENoticeThatWarnsOfAProblemYouShouldReportUsingOldFormat;
+            if (($queryString = CRM_Core_DAO::createSqlFilter($field, array($op => array_keys($value)), $dataType)) != FALSE) {
+              return $queryString;
+            }
+          }
         }
+
         $value = CRM_Utils_Type::escape($value, $dataType);
         // if we don't have a dataType we should assume
         if ($dataType == 'String' || $dataType == 'Text') {
