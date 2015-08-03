da0523a4d4b031bcbcabd77cae9e0b089bc26b7e
zorgalina <zorgalina>
2015-08-03 13:40:39 -0700

    'CRM-16481'

--- CRM/Event/Cart/Form/Checkout/ParticipantsAndPrices.php
+++ CRM/Event/Cart/Form/Checkout/ParticipantsAndPrices.php
@@ -78,7 +78,7 @@ class CRM_Event_Cart_Form_Checkout_ParticipantsAndPrices extends CRM_Event_Cart_
     $price_set_id = CRM_Price_BAO_PriceSet::getFor('civicrm_event', $event->id);
     //CRM-14492 display admin fields if user is admin
     $adminFieldVisible = FALSE;
-    if (CRM_Core_Permission::check('administer CiviCRM')){
+    if (CRM_Core_Permission::check('administer CiviCRM')) {
       $adminFieldVisible = TRUE;
     }
     if ($price_set_id) {
@@ -88,7 +88,7 @@ class CRM_Event_Cart_Form_Checkout_ParticipantsAndPrices extends CRM_Event_Cart_
       foreach ($price_set['fields'] as $field) {
         $index++;  //what is $index for?
         //check whether field is public or visible to admins only
-        if (CRM_Utils_Array::value('visibility', $field) == 'public' || (CRM_Utils_Array::value('visibility', $field) == 'admin' && $adminFieldVisible == TRUE)){
+        if (CRM_Utils_Array::value('visibility', $field) == 'public' || (CRM_Utils_Array::value('visibility', $field) == 'admin' && $adminFieldVisible == TRUE)) {
           $field_name = "event_{$event->id}_price_{$field['id']}";
           CRM_Price_BAO_PriceField::addQuickFormElement($this, $field_name, $field['id'], FALSE);
           $price_fields_for_event[] = $field_name;
@@ -135,12 +135,12 @@ class CRM_Event_Cart_Form_Checkout_ParticipantsAndPrices extends CRM_Event_Cart_
         //  CRM_Price_BAO_PriceSet::processAmount($this->_values['fee']['fields'], $fields, $lineItem);
         if (is_array($price_set['fields'])) {
           CRM_Price_BAO_PriceSet::processAmount($price_set['fields'], $params, $lineItem);
-          foreach ($lineItem as $line){
+          foreach ($lineItem as $line) {
             $total += $line['line_total'];
           }
           //XXX total...
           //if ($fields['amount'] < 0) {
-          if ($total < 0){
+          if ($total < 0) {
             $this->_errors['_qf_default'] = ts("Price Levels can not be less than zero. Please select the options accordingly");
           }
         }
@@ -200,15 +200,16 @@ class CRM_Event_Cart_Form_Checkout_ParticipantsAndPrices extends CRM_Event_Cart_
         $participant->email = NULL;
         //LSE set contact first, last name, etc.,not just ID
         $contactdata = array();
-        foreach( $this->_submitValues['field'] as $key=>$value) {
+        foreach($this->_submitValues['field'] as $key => $value) {
           $contactdata = $value;
         }
-        if ($contactdata['first_name'] != NULL){
+        if ($contactdata['first_name'] != NULL) {
           $participant->contact_id = self::find_or_create_contact($this->getContactID(),
-          array('email' => $contactdata['email-Primary'],
-          'first_name' => $contactdata['first_name'],
-          'last_name' => $contactdata['last_name'],
-          'is_deleted' => FALSE));
+          array(
+	    'email' => $contactdata['email-Primary'],
+            'first_name' => $contactdata['first_name'],
+	    'last_name' => $contactdata['last_name'],
+            'is_deleted' => FALSE));
           $participant->save();
           $this->cid = $participant->contact_id;
         }
@@ -220,21 +221,21 @@ class CRM_Event_Cart_Form_Checkout_ParticipantsAndPrices extends CRM_Event_Cart_
       foreach ($this->cart->get_main_events_in_carts() as $event_in_cart) {
         $event_id = $event_in_cart->event_id;
         $price_set_id = CRM_Event_BAO_Event::usesPriceSet($event_in_cart->event_id);
-        if ($price_set_id){
+        if ($price_set_id) {
           $price_sets = CRM_Price_BAO_PriceSet::getSetDetail($price_set_id, TRUE, TRUE);
           $price_set  = $price_sets[$price_set_id];
           foreach ($price_set['fields'] as $field) {
             $options = CRM_Utils_Array::value('options', $field);
-            if (!is_array($options)){
+            if (!is_array($options)) {
               continue;
             }
             $field_name = "event_{$event_id}_price_{$field['id']}";
-            foreach ($options as $value){
+            foreach ($options as $value) {
               if ($value['is_default']) {
                 if ($field['html_type'] == 'Checkbox') {
                   $defaults[$field_name] = 1;
                 }
-                else{
+                else {
                   $defaults[$field_name] = $value['id'];
                 }
               }
