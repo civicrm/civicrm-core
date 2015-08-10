3febe800a5f92bc5904c0218c89b470318fae5ed
eileenmcnaugton <eileen@fuzion.co.nz>
2015-08-10 14:31:03 +1200

    CRM-16692 follow-up fix, move assumptions back to calling-form

--- CRM/Contribute/BAO/Contribution/Utils.php
+++ CRM/Contribute/BAO/Contribution/Utils.php
@@ -103,17 +103,26 @@ class CRM_Contribute_BAO_Contribution_Utils {
         }
       }
 
+      $contributionParams = array(
+        'contact_id' => $contactID,
+        'line_item' => $lineItems,
+        'is_test' => $isTest,
+        'campaign_id' => CRM_Utils_Array::value('campaign_id', $paymentParams, CRM_Utils_Array::value('campaign_id', $form->_values)),
+        'contribution_page_id' => $form->_id,
+        'source' => CRM_Utils_Array::value('source', $paymentParams, CRM_Utils_Array::value('description', $paymentParams)),
+      );
+      $isMonetary = !empty($form->_values['is_monetary']);
+      if ($isMonetary) {
+        if (empty($paymentParams['is_pay_later'])) {
+          // @todo look up payment_instrument_id on payment processor table.
+          $contributionParams['payment_instrument_id'] = 1;
+        }
+      }
       $contribution = CRM_Contribute_Form_Contribution_Confirm::processFormContribution(
         $form,
         $paymentParams,
         NULL,
-        array(
-          'contact_id' => $contactID,
-          'line_item' => $lineItems,
-          'is_test' => $isTest,
-          'campaign_id' => CRM_Utils_Array::value('campaign_id', $paymentParams, CRM_Utils_Array::value('campaign_id', $form->_values)),
-          'contribution_page_id' => $form->_id,
-        ),
+        $contributionParams,
         $financialType,
         TRUE,
         TRUE,
