b0ec191282dda5bf68a5f0d5409867527c5f834c
utkarshsharma <utkarsh.hs@gmail.com>
2015-08-11 10:06:02 +0530

    Email Preview Cluster Roundtrip

--- ang/crmMailing/BlockEmailPreviewCluster.js
+++ ang/crmMailing/BlockEmailPreviewCluster.js
@@ -2,7 +2,7 @@
   // example: <div crm-mailing-block-preview crm-mailing="myMailing" on-preview="openPreview(myMailing, preview.mode)" on-send="sendEmail(myMailing,preview.recipient)">
   // note: the directive defines a variable called "preview" with any inputs supplied by the user (e.g. the target recipient for an example mailing)
 
-  angular.module('crmMailing').directive('crmMailingBlockEmailPreviewCluster', function(crmUiHelp) {
+  angular.module('crmMailing').directive('crmMailingBlockEmailPreviewCluster', function (crmUiHelp, $http) {
     return {
       templateUrl: '~/crmMailing/BlockEmailPreviewCluster.html',
       link: function(scope, elm, attr) {
@@ -14,12 +14,60 @@
         scope.hs = crmUiHelp({file: 'CRM/Mailing/MailingUI'});
         scope.testContact = {email: CRM.crmMailing.defaultTestEmail};
         scope.testGroup = {gid: null};
+        scope.yahooURL = 'javascript:';
+        scope.renderingDone = false;
 
-        scope.requestPreview = function requestPreview() {
+        scope.requestPreview = function requestPreview(renderers) {
           scope.$eval(attr.onRequest);
+          scope.requested= true;
+          if (renderers.gmail) {
+            scope.gmailShow = true;
+          }
+          else {
+            scope.gmailShow = false;
+          }
+          if (renderers.yahoo) {
+            scope.yahooShow = true;
+          }
+          else {
+            scope.yahooShow = false;
+          }
+          var statusURL = 'http://0.0.0.0:3000/api/PreviewBatches/status?batchId='
+          var batchId = 1234;
+          var Interval;
+          Interval = setInterval( function(){
+            checkStatus(statusURL);
+          }, 5000);
+          function checkStatus(statusURL) {
+            $http.get(statusURL+batchId)
+                .success(function(data, status, headers, config){
+                /*called for result & error because 200 status*/
+                //console.log(data)    
+                if (data.response){
+                    //handle success here
+                    //console.log(data.response.finished)
+                    if (data.response.finished == 1){
+                      clearInterval(Interval);
+                      //console.log("done")
+                      scope.yahooURL = data.response.yahoo;
+                      console.log(scope.yahooURL);
+                      scope.renderingDone = true;
+                    }
+                } else if (data.error) {
+                    //handle error here
+                    console.log(status)
+                }
+              })
+            .error(function(data, status, headers, config){
+                /*handle non 200 statuses*/
+                console.log(data);
+            });
+          }
         };
-        scope.doSend = function doSend(recipient) {
+        scope.cancelBatch = function cancelBatch() {
+          scope.requested= false;
           console.log("hello");
+          console.log(scope.yahooURL);
         };
 
         scope.previewTestGroup = function(e) {
