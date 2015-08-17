38623ad5413fd42f3c8d52127b7319f453888bbb
eileenmcnaugton <eileen@fuzion.co.nz>
2015-08-17 12:14:34 +1200

    increment version to 4.4.19rc1

--- civicrm-version.php
+++ civicrm-version.php
@@ -1,6 +1,6 @@
 <?php
 function civicrmVersion( ) {
-  return array( 'version'  => '4.4.17',
+  return array( 'version'  => '4.4.19',
                 'cms'      => 'Drupal',
                 'revision' => 'b67360b113' );
 }
