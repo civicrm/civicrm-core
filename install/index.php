4d8ce7f0d726faee9694a99cbf27594237ffbb3d
Tim Otten <totten@civicrm.org>
2015-08-16 18:17:38 -0700

    CRM-16373 - Config - Remove gettextResourceDir
    This field is not a genuine setting - it's a small variant on `$civicrm_root`.
    
    Use CRM_Core_I18n::getResourceDir() instead.

--- install/index.php
+++ install/index.php
@@ -170,7 +170,6 @@ $config = CRM_Core_Config::singleton(FALSE);
 $GLOBALS['civicrm_default_error_scope'] = NULL;
 
 // The translation files are in the parent directory (l10n)
-$config->gettextResourceDir = '..' . DIRECTORY_SEPARATOR . $config->gettextResourceDir;
 $i18n = CRM_Core_I18n::singleton();
 
 global $cmsPath;
