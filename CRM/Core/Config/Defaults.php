76cc52c54b780fe9e7fdbf8b1fdb890a6ebb75bd
Tim Otten <totten@civicrm.org>
2015-08-16 18:26:11 -0700

    CRM-16373 - Config - Remove `importDataSourceDir`
    This variable is a trivial variant on `$civicrm_root`. It does not appear to
    be editable, and it is only used in one place.

--- CRM/Core/Config/Defaults.php
+++ CRM/Core/Config/Defaults.php
@@ -57,8 +57,6 @@ class CRM_Core_Config_Defaults {
 
     $this->sqlDir = $civicrm_root . DIRECTORY_SEPARATOR . 'sql' . DIRECTORY_SEPARATOR;
 
-    $this->importDataSourceDir = $civicrm_root . DIRECTORY_SEPARATOR . 'CRM' . DIRECTORY_SEPARATOR . 'Import' . DIRECTORY_SEPARATOR . 'DataSource' . DIRECTORY_SEPARATOR;
-
     // show tree widget
     $this->groupTree = defined('CIVICRM_GROUPTREE') ? TRUE : FALSE;
 
