4d82a855eaaf6cd51ca6d0bf74ad6eb9aac9df89
sunil <esunil.pawar@gmail.com>
2015-08-18 13:30:57 +0530

    Getting started changes

--- CRM/Upgrade/Incremental/php/FourSix.php
+++ CRM/Upgrade/Incremental/php/FourSix.php
@@ -265,16 +265,23 @@ class CRM_Upgrade_Incremental_php_FourSix {
    * @param string $rev
    */
   public function upgrade_4_6_7($rev) {
-    $this->addTask(ts('Upgrade DB to %1: SQL', array(1 => $rev)), 'add_getting_started_dashlet');
+    $this->addTask(ts('Add Getting Started dashlet to %1: SQL', array(1 => $rev)), '  addGettingStartedDashlet', $rev);
   }
 
-  public function add_getting_started_dashlet(CRM_Queue_TaskContext $ctx) {
+  /**
+   * Add Getting Started dashlet to dashboard
+   *
+   * @param \CRM_Queue_TaskContext $ctx
+   *
+   * @return bool
+   */
+  public function addGettingStartedDashlet(CRM_Queue_TaskContext $ctx) {
     $sql = "SELECT count(*) FROM civicrm_dashboard WHERE name='gettingStarted'";
     $res = CRM_Core_DAO::singleValueQuery($sql);
     $domainId = CRM_Core_Config::domainID();
     if ($res <= 0) {
       $sql = "INSERT INTO `civicrm_dashboard`
-    ( `domain_id`, `name`, `label`, `url`, `permission`, `permission_operator`, `column_no`, `is_minimized`, `is_active`, `weight`, `fullscreen_url`, `is_fullscreen`, `is_reserved`)` VALUES ( {$domainId}, 'getting-started', 'CiviCRM Getting Started', 'civicrm/dashlet/getting-started?reset=1&snippet=5', 'access CiviCRM', NULL, 0, 0, 1, 0, 'civicrm/dashlet/getting-started?reset=1&snippet=5&context=dashletFullscreen', 1, 1)";
+    ( `domain_id`, `name`, `label`, `url`, `permission`, `permission_operator`, `column_no`, `is_minimized`, `is_active`, `weight`, `fullscreen_url`, `is_fullscreen`, `is_reserved`) VALUES ( {$domainId}, 'getting-started', 'Getting Started', 'civicrm/dashlet/getting-started?reset=1&snippet=5', 'access CiviCRM', NULL, 0, 0, 1, 0, 'civicrm/dashlet/getting-started?reset=1&snippet=5&context=dashletFullscreen', 1, 1)";
       CRM_Core_DAO::executeQuery($sql);
       // Add default position for Getting Started Dashlet ( left column)
       $sql = "INSERT INTO `civicrm_dashboard_contact` (dashboard_id, contact_id, column_no, is_active)
@@ -282,6 +289,7 @@ SELECT (SELECT MAX(id) FROM `civicrm_dashboard`), contact_id, 0, IF (SUM(is_acti
 FROM `civicrm_dashboard_contact` WHERE 1 GROUP BY contact_id";
       CRM_Core_DAO::executeQuery($sql);
     }
+    return TRUE;
   }
 
 }
