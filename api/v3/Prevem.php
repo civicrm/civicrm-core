d22fb543dc89cd913f9c51002ddba94903d6e222
utkarshsharma <utkarsh.hs@gmail.com>
2015-08-21 23:36:04 +0530

    Email Preview Cluster GSoC final commit

--- api/v3/Prevem.php
+++ api/v3/Prevem.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
  +--------------------------------------------------------------------+
  | CiviCRM version 4.6                                                |
  +--------------------------------------------------------------------+
@@ -24,52 +24,51 @@
  | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
  +--------------------------------------------------------------------+
  */
-
 function civicrm_api3_prevem_login($params) {
-	$prevemUrl = CRM_Core_BAO_Setting::getItem(CRM_Core_BAO_Setting::MAILING_PREFERENCES_NAME, 'prevem_url');
+  $prevemUrl = CRM_Core_BAO_Setting::getItem(CRM_Core_BAO_Setting::MAILING_PREFERENCES_NAME, 'prevem_url');
 
-	$prevemURL = !empty($prevemUrl) ? CRM_Utils_URL::mask($prevemUrl, array('user','pass')) : NULL;
-	if (!$prevemURL) {
-		return civicrm_api3_create_error("prevemURL is not configured. Go to Administer>CiviMail>CiviMail Component Settings to configure prevemURL");		
-	}
-	$prevemConsumer = parse_url($prevemUrl, PHP_URL_USER);
-	$prevemSecret = parse_url($prevemUrl, PHP_URL_PASS);
-	// TODO Parse $prevemUrl. Send login request to get token.
-	// To send login request, see eg http://stackoverflow.com/questions/2445276/how-to-post-data-in-php-using-file-get-contents
+  $prevemURL = !empty($prevemUrl) ? CRM_Utils_URL::mask($prevemUrl, array('user', 'pass')) : NULL;
+  if (!$prevemURL) {
+    return civicrm_api3_create_error("prevemURL is not configured. Go to Administer>CiviMail>CiviMail Component Settings to configure prevemURL");
+  }
+  $prevemConsumer = parse_url($prevemUrl, PHP_URL_USER);
+  $prevemSecret = parse_url($prevemUrl, PHP_URL_PASS);
+  /** TODO Parse $prevemUrl. Send login request to get token. **/
+  /** To send login request, see eg http://stackoverflow.com/questions/2445276/how-to-post-data-in-php-using-file-get-contents **/
 
-	$postdata = http_build_query(
-	    array(
-	        'email' => $prevemConsumer . "@foo.com",
-	        'password' => $prevemSecret
-	    )
-	);
+  $postdata = http_build_query(
+    array(
+      'email' => $prevemConsumer . "@foo.com",
+      'password' => $prevemSecret,
+    )
+  );
 
-	$opts = array('http' =>
-	    array(
-	        'method'  => 'POST',
-	        'header'  => 'Content-type: application/x-www-form-urlencoded',
-	        'content' => $postdata
-	    )
-	);
+  $opts = array(
+    'http' =>
+    array(
+      'method'  => 'POST',
+      'header'  => 'Content-type: application/x-www-form-urlencoded',
+      'content' => $postdata,
+    ),
+  );
 
-	$context  = stream_context_create($opts);
-	$result1 = file_get_contents($prevemURL.'/api/Users', false, $context);
-	$result = file_get_contents($prevemURL.'/api/Users/login', false, $context);
-	//CRM_Core_Error::debug_var('$http_response_header', $http_response_header);
-	if ($result === FALSE) {
-		return civicrm_api3_create_error("Failed to login. Check if Preview Manager is running on ".$prevemURL);
-	}
-	else {
-		$accessToken = json_decode($result)->{'id'};
-		if (!$accessToken){
-			return civicrm_api3_create_error("Failed to parse access token. Check if Preview Manager is running on ".$prevemURL);
-		}
-		$returnValues = array(
-			'url' => $prevemURL,
-			'consumerId' => $prevemConsumer,
-			'token' => $accessToken,
-			'password' => $prevemSecret
-		);
-		return civicrm_api3_create_success($returnValues, $params, 'Prevem', 'login');
-	}
+  $context  = stream_context_create($opts);
+  $result = file_get_contents($prevemURL . '/api/Users/login', FALSE, $context);
+
+  if ($result === FALSE) {
+    return civicrm_api3_create_error("Failed to login. Check if Preview Manager is running on " . $prevemURL);
+  }
+  else {
+    $accessToken = json_decode($result)->{'id'};
+    if (!$accessToken) {
+      return civicrm_api3_create_error("Failed to parse access token. Check if Preview Manager is running on " . $prevemURL);
+    }
+    $returnValues = array(
+      'url' => $prevemURL,
+      'consumerId' => $prevemConsumer,
+      'token' => $accessToken,
+      'password' => $prevemSecret,
+    );
+    return civicrm_api3_create_success($returnValues, $params, 'Prevem', 'login');
+  }
 }
