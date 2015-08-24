c39f189f89daa8fac4d56403eeabab6087d12150
eileenmcnaugton <eileen@fuzion.co.nz>
2015-08-25 11:35:41 +1200

    CRM-17078 contribution summary report fatals when custom data included

--- CRM/Report/Form/Contribute/Summary.php
+++ CRM/Report/Form/Contribute/Summary.php
@@ -527,6 +527,7 @@ class CRM_Report_Form_Contribute_Summary extends CRM_Report_Form {
     $group = "\nGROUP BY {$this->_aliases['civicrm_contribution']}.currency";
 
     $this->from('contribution');
+    $this->customDataFrom();
     $contriSQL = "SELECT
 COUNT({$this->_aliases['civicrm_contribution']}.total_amount )        as civicrm_contribution_total_amount_count,
 SUM({$this->_aliases['civicrm_contribution']}.total_amount )          as civicrm_contribution_total_amount_sum,
