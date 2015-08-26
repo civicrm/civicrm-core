ec022878580e581215e845f418797dddf416a325
eileenmcnaugton <eileen@fuzion.co.nz>
2015-08-27 00:22:22 +1200

    CRM-16808 paypal express follow up fixes

--- CRM/Event/Form/Registration/Confirm.php
+++ CRM/Event/Form/Registration/Confirm.php
@@ -582,6 +582,10 @@ class CRM_Event_Form_Registration_Confirm extends CRM_Event_Form_Registration {
         if (is_array($this->_paymentProcessor)) {
           $payment = $this->_paymentProcessor['object'];
         }
+        if (!empty($this->_paymentProcessor) &&  $this->_paymentProcessor['object']->supports('preApproval')) {
+          $preApprovalParams = $this->_paymentProcessor['object']->getPreApprovalDetails($this->get('pre_approval_parameters'));
+          $value = array_merge($value, $preApprovalParams);
+        }
         $result = NULL;
 
         if (!empty($value['is_pay_later']) ||
