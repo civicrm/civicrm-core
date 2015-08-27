5daa5bb2744c3292e13ce07b8303e30345fedc0b
Seamus Lee <seamuslee001@gmail.com>
2015-08-27 02:02:26 +0000

    Increment to version 4.6.8

--- civicrm-version.php
+++ civicrm-version.php
@@ -1,6 +1,6 @@
 <?php
 function civicrmVersion( ) {
-  return array( 'version'  => '4.6.7',
+  return array( 'version'  => '4.6.8',
                 'cms'      => 'Drupal',
                 'revision' => '' );
 }
