94514daf37c596480db0cd21be388d693d3df463
eileenmcnaugton <eileen@fuzion.co.nz>
2015-09-07 23:38:57 +1200

    CRM-17116 fix advanced search to support custom dates
    Conflicts:
    	CRM/Contact/BAO/Query.php

--- CRM/Contribute/BAO/Query.php
+++ CRM/Contribute/BAO/Query.php
@@ -346,7 +346,7 @@ class CRM_Contribute_BAO_Query {
       case (strpos($name, '_amount') !== FALSE):
       case (strpos($name, '_date') !== FALSE && $name != 'contribution_fulfilled_date'):
         $qillName = $name;
-        $pseudoExtraParam = NULL;
+        $pseudoExtraParam = array();
         // @todo including names using a switch statement & then using an 'if' to filter them out is ... odd!
         if ((strpos($name, '_amount') !== FALSE) || (strpos($name, '_date') !== FALSE) || in_array($name,
             array(
