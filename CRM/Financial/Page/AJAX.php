5b3fba719e74c76dfa39588b6aa3c70c670ec5d3
monishdeb <monish.deb@webaccessglobal.com>
2015-09-10 13:20:51 +0530

    CRM-17188 fix - [4.6 branch] Add transactions to batch step stuck at processing
    https://issues.civicrm.org/jira/browse/CRM-17188

--- CRM/Financial/Page/AJAX.php
+++ CRM/Financial/Page/AJAX.php
@@ -269,7 +269,7 @@ class CRM_Financial_Page_AJAX {
    *
    * @return string
    */
-  public static function getFinancialTransactionsList($return = FALSE) {
+  public static function getFinancialTransactionsList() {
     $sortMapper = array(
       0 => '',
       1 => '',
@@ -283,6 +283,7 @@ class CRM_Financial_Page_AJAX {
     );
 
     $sEcho = CRM_Utils_Type::escape($_REQUEST['sEcho'], 'Integer');
+    $return = isset($_REQUEST['return']) ? CRM_Utils_Type::escape($_REQUEST['return'], 'Boolean') : FALSE;
     $offset = isset($_REQUEST['iDisplayStart']) ? CRM_Utils_Type::escape($_REQUEST['iDisplayStart'], 'Integer') : 0;
     $rowCount = isset($_REQUEST['iDisplayLength']) ? CRM_Utils_Type::escape($_REQUEST['iDisplayLength'], 'Integer') : 25;
     $sort = isset($_REQUEST['iSortCol_0']) ? CRM_Utils_Array::value(CRM_Utils_Type::escape($_REQUEST['iSortCol_0'], 'Integer'), $sortMapper) : NULL;
