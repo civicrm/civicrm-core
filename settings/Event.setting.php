007e3bdcefc15254f99a645683a98cb8d3eea8c4
Tim Otten <totten@civicrm.org>
2015-09-15 00:18:10 -0700

    civicrm_navigation.tpl - Move defaults into settings/*.php
    For a large number of settings, defaults are declared twice -- once in
    `xml/templates/civicrm_navigation.tpl` and once in `settings/*.php`.  In
    many cases, these are identical/redundant, but there are a couple where the
    defaults diverged.
    
    I believe the `civicrm_navigation.tpl` values have generally taken precedent
    on new installations, so this commit revises `settings/*.php` to fix any
    discrepancies.

--- settings/Event.setting.php
+++ settings/Event.setting.php
@@ -36,18 +36,17 @@
  * Settings metadata file
  */
 return array(
-  'event_enable_cart' =>
-  array(
+  'enable_cart' => array(
     'name' => 'enable_cart',
     'group_name' => 'Event Preferences',
     'group' => 'event',
-    'type' => 'String',
+    'type' => 'Boolean',
     'quick_form_type' => 'Element',
-    'default' => 0,
-    'add' => 4.1,
+    'default' => '0',
+    'add' => '4.1',
     'title' => 'Enable Event Cart',
     'is_domain' => 1,
-    'is_contact' => 1,
+    'is_contact' => 0,
     'description' => "WRITE ME",
     'help_text' => 'WRITE ME',
   ),
