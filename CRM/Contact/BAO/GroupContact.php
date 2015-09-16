37f7fa4d58440985e2ee936e6d5908b7426dccab
eileenmcnaugton <eileen@fuzion.co.nz>
2015-09-16 23:28:31 +1200

    Revert "improvement"
    This reverts commit f013e5daba22226f5448d3927590ac6643d6fc9a.

--- CRM/Contact/BAO/GroupContact.php
+++ CRM/Contact/BAO/GroupContact.php
@@ -351,7 +351,7 @@ class CRM_Contact_BAO_GroupContact extends CRM_Contact_DAO_GroupContact {
                     civicrm_subscription_history.method as method';
     }
 
-    $where = " WHERE contact_a.id = %1 AND civicrm_group.is_active = 1";
+    $where = " WHERE contact_a.id = %1 AND civicrm_group.is_active = 1 AND civicrm_group.saved_search_id IS NULL";
 
     if ($excludeHidden) {
       $where .= " AND civicrm_group.is_hidden = 0 ";
@@ -383,7 +383,7 @@ class CRM_Contact_BAO_GroupContact extends CRM_Contact_DAO_GroupContact {
 
     //CRM-16945: seems hackish but as per CRM-16483 of using group criteria for Search Builder it is mandatory
     //to include group_contact_cache clause when group table is present, so following code remove duplicacy
-    $from = str_replace("OR civicrm_group.id = civicrm_group_contact_cache.group_id", 'AND civicrm_group.saved_search_id IS NULL', $from);
+    $from = str_replace("OR civicrm_group.id = civicrm_group_contact_cache.group_id", '', $from);
 
     $where .= " AND $permission ";
 
