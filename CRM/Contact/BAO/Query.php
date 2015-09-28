e34642c964513be1999ff0e1c9f3b7faad02b2fd
monishdeb <monish.deb@webaccessglobal.com>
2015-09-28 18:19:49 +0530

    legacy support for group and tag

--- CRM/Contact/BAO/Query.php
+++ CRM/Contact/BAO/Query.php
@@ -1485,6 +1485,9 @@ class CRM_Contact_BAO_Query {
     }
 
     foreach ($formValues as $id => $values) {
+
+      self::legacyConvertFormValues($id, $values);
+
       if (self::isAlreadyProcessedForQueryFormat($values)) {
         $params[] = $values;
         continue;
@@ -1561,6 +1564,25 @@ class CRM_Contact_BAO_Query {
     }
     return $params;
   }
+
+  /**
+   * Function to support legacy format for groups and tags.
+   *
+   * @param string $id
+   * @param array|int $values
+   *
+   */
+   public static function legacyConvertFormValues($id, &$values) {
+     if (in_array($id, array('group', 'tag')) && is_array($values)) {
+       // prior to 4.7, formValues for some attributes (e.g. group, tag) are stored in array(id1 => 1, id2 => 1),
+       // as per the recent Search fixes $values need to be in standard array(id1, id2) format
+       $ids = array_keys($values, 1);
+       if (count($ids) > 1 || (count($ids) == 1 && key($values) > 1)) {
+         $values = $ids;
+       }
+     }
+  }
+
 
   /**
    * Fix values from query from/to something no-one cared enough to document.
