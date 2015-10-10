03ad81aea66356739983323903b11d3ea59f2620
Andrew Hunt <andrew@aghstrategies.com>
2015-10-09 20:16:46 -0400

    CRM-17425 initial changes for pcp in contrib batch entry
    ----------------------------------------
    * CRM-17425: Allow PCP selection in contribution batch entry
      https://issues.civicrm.org/jira/browse/CRM-17425

--- CRM/Core/BAO/UFField.php
+++ CRM/Core/BAO/UFField.php
@@ -1090,6 +1090,10 @@ SELECT  id
           'name' => 'contribution_note',
           'title' => ts('Contribution Note'),
         ),
+        'contribution_soft_credit_pcp_id' => array(
+          'name' => 'contribution_soft_credit_pcp_id',
+          'title' => ts('Personal Campaign Page'),
+        ),
       );
     }
     return self::$_contriBatchEntryFields;
