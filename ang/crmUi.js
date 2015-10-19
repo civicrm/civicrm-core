8c3a97ed719da8d25aeaa228446c0d679eba509c
Coleman Watts <coleman@civicrm.org>
2015-10-18 22:22:16 -0400

    CRM-16415 - Fix crmSelect2 placeholder icons

--- ang/crmUi.js
+++ ang/crmUi.js
@@ -599,7 +599,7 @@
 
           function init() {
             // TODO watch select2-options
-            element.select2(scope.crmUiSelect || {});
+            element.crmSelect2(scope.crmUiSelect || {});
             if (ngModel) {
               element.on('change', refreshModel);
               $timeout(ngModel.$render);
