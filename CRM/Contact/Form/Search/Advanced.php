bcdb701d2e666e32333a203f754efe8d6b5051af
Monish Deb <monish.deb@jmaconsulting.biz>
2015-10-20 13:55:12 +0530

    minor fix

--- CRM/Contact/Form/Search/Advanced.php
+++ CRM/Contact/Form/Search/Advanced.php
@@ -360,7 +360,7 @@ class CRM_Contact_Form_Search_Advanced extends CRM_Contact_Form_Search {
           }
           $this->_formValues[$element] = array('IN' => $value);
         }
-        elseif ((substr($formValues[$key], 0, 1) == '%') || (substr($formValues[$key], -1, 1) == '%')) {
+        elseif ((substr($value, 0, 1) == '%') || (substr($value, -1, 1) == '%')) {
           $this->_formValues[$element] = array('LIKE' => $value);
         }
       }
