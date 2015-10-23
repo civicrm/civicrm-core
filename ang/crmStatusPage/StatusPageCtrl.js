44b8d7f13d6477eff28b5ee103fdb4dae2ea6e0d
Coleman Watts <coleman@civicrm.org>
2015-10-23 08:43:19 -0400

    CRM-13823 - Snooze/hush cleanup

--- ang/crmStatusPage/StatusPageCtrl.js
+++ ang/crmStatusPage/StatusPageCtrl.js
@@ -1,7 +1,5 @@
 (function(angular, $, _) {
 
-  // controller
-
   angular.module('statuspage').controller('statuspageStatusPage',
     function($scope, crmApi, crmStatus, statusData, statuspageSeverityList) {
 
@@ -18,62 +16,46 @@
         };
       });
 
-    // will "hush" a status - gets the severity level of the status that is being hushed, and hushes all alerts for that check at and below the level of the current check
-    $scope.hush = function(name, severity) {
-      return  crmStatus(
-        { start: ts('Saving Status Preference...')      , success: ts('Preference Saved') },
+      // will "hush" a status - gets the severity level of the status that is being hushed, and hushes all alerts for that check at and below the level of the current check
+      $scope.hush = function(status) {
         crmApi('StatusPreference', 'create', {
-          "sequential": 1,
-          "name": name,
-          "ignore_severity": severity,
+          "name": status.name,
+          "ignore_severity": status.severity,
           "hush_until":  ""
-        })
-        .then(function(){rmStatus($scope, name);})
-      );
-    };
+        }, true)
+          .then(function() {
+            status.is_visible = 0;
+          });
+      };
 
     // will reset ignore_severity to 0 to unhush the status alert.
-    $scope.unhush = function(name, severity) {
-      return  crmStatus(
-        { start: ts('Saving Status Preference...')      , success: ts('Preference Saved') },
+    $scope.unhush = function(status) {
+      crmApi('StatusPreference', 'create', {
+        "name": status.name,
+        "ignore_severity": 0,
+        "hush_until":  ""
+      }, true)
+        .then(function() {
+          status.is_visible = 1;
+        });
+    };
+
+      // will 'snooze' a status - will not show alerts at that level for that check + alerts below that level for that check until the specified date
+      $scope.snooze = function(status) {
+        $scope.showSnoozeOptions(status);
         crmApi('StatusPreference', 'create', {
-          "name": name,
-          "ignore_severity": 0,
-          "hush_until": ""
-        })
-        .then(function(){rmStatus($scope, name);})
-      );
-    };
+          "name": status.name,
+          "ignore_severity": status.snoozeOptions.severity,
+          "hush_until": status.snoozeOptions.until
+        }, true)
+          .then(function() {
+            status.is_visible = 0;
+          });
+      };
 
-    // will 'snooze' a status - will not show alerts at that level for that check + alerts below that level for that check until the specified date
-    $scope.snooze = function(status) {
-      $scope.showSnoozeOptions(status);
-      return crmStatus(
-        { status: ts('Saving Status Preference...')   , success: ts('Preference Saved') },
-          crmApi('StatusPreference', 'create', {
-            "name": status.name,
-            "ignore_severity": status.snoozeOptions.severity,
-            "hush_until": status.snoozeOptions.until
-          })        .then(function(){rmStatus($scope, status.name);})
-      );
-    };
-    $scope.showSnoozeOptions = function(status) {
-      status.snoozeOptions.show = !status.snoozeOptions.show;
-    };
-  });
-
-
-  /**
-   * remove a status after it has been hushed/snoozed
-   * @param {type} $scope
-   * @param {type} statusName
-   * @returns void
-   */
-   function rmStatus($scope, statusName) {
-    $scope.statuses.values =  _.reject($scope.statuses.values,
-      function(status) {
-        return status.name === statusName;
+      $scope.showSnoozeOptions = function(status) {
+        status.snoozeOptions.show = !status.snoozeOptions.show;
+      };
     });
-  }
 
 })(angular, CRM.$, CRM._);
