ec6774eb9abc812a26284022a4533ff0b19c3ee6
Coleman Watts <coleman@civicrm.org>
2015-10-24 12:52:09 -0400

    CRM-13823 - Use popup menu for snooze options

--- ang/crmStatusPage/StatusPageServices.js
+++ ang/crmStatusPage/StatusPageServices.js
@@ -1,9 +1,31 @@
 (function(angular, $, _) {
 
-  angular.module('statuspage').filter('trusted', function($sce){ return $sce.trustAsHtml; });
+  angular.module('statuspage')
+    .filter('trusted', function($sce){ return $sce.trustAsHtml; })
 
-  angular.module('statuspage').service('statuspageSeverityList', function() {
-    return ['debug', 'info', 'notice', 'warning', 'error', 'critical', 'alert', 'emergency'];
-  });
+    .service('statuspageSeverityList', function() {
+      return ['debug', 'info', 'notice', 'warning', 'error', 'critical', 'alert', 'emergency'];
+    })
+
+    // Todo: abstract this into a generic crmUi directive?
+    .directive('statuspagePopupMenu', function($timeout) {
+      return {
+        templateUrl: '~/statuspage/SnoozeOptions.html',
+        transclude: true,
+
+        link: function(scope, element, attr) {
+          element.on('click', '.hush-menu-button', function() {
+            $timeout(function() {
+              $('ul', element).show().menu();
+              element.closest('h3').addClass('menuopen');
+              $('body').one('click', function() {
+                $('ul', element).menu('destroy').hide();
+                element.closest('h3').removeClass('menuopen');
+              });
+            });
+          });
+        }
+      };
+    });
 
 })(angular, CRM.$, CRM._);
