b72de56c1b031fd27a457e0a33156961bd2ade19
Saurabh Batra <saurabh.batra96@gmail.com>
2015-10-25 21:25:58 +0530

    Minor comment fixes for CRM/Extensions directory.

--- CRM/Extension/Container/Basic.php
+++ CRM/Extension/Container/Basic.php
@@ -28,7 +28,6 @@
 /**
  * @package CRM
  * @copyright CiviCRM LLC (c) 2004-2015
- * $Id$
  */
 
 /**
@@ -102,6 +101,8 @@ class CRM_Extension_Container_Basic implements CRM_Extension_Container_Interface
 
   /**
    * @inheritDoc
+   *
+   * @return array
    */
   public function checkRequirements() {
     $errors = array();
@@ -124,6 +125,8 @@ class CRM_Extension_Container_Basic implements CRM_Extension_Container_Interface
 
   /**
    * @inheritDoc
+   *
+   * @return array_keys
    */
   public function getKeys() {
     return array_keys($this->getRelPaths());
--- CRM/Extension/Container/Static.php
+++ CRM/Extension/Container/Static.php
@@ -28,7 +28,6 @@
 /**
  * @package CRM
  * @copyright CiviCRM LLC (c) 2004-2015
- * $Id$
  */
 
 /**
