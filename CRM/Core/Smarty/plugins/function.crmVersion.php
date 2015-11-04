1cd5a5d8631b814b507c3d1c22fb82ac190f5441
Coleman Watts <coleman@civicrm.org>
2015-11-04 16:45:43 -0500

    Minor cleanup

--- CRM/Core/Smarty/plugins/function.crmVersion.php
+++ CRM/Core/Smarty/plugins/function.crmVersion.php
@@ -50,10 +50,8 @@
 function smarty_function_crmVersion($params, &$smarty) {
   $version = CRM_Utils_System::version();
 
-  $redact = !CRM_Core_Permission::check('access CiviCRM');
-  if ($redact) {
-    $parts = explode('.', $version);
-    $version = $parts[0] . '.' . $parts[1] . '.x';
+  if (!CRM_Core_Permission::check('access CiviCRM')) {
+    $version = CRM_Utils_System::majorVersion();
   }
 
   if (isset($params['assign'])) {
