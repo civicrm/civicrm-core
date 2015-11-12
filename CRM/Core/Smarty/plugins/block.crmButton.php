c18c6b851cda820792e5098862610fbcac1be94a
Coleman Watts <coleman@civicrm.org>
2015-11-12 16:34:37 -0500

    CRM-16415 - Update crmButton markup

--- CRM/Core/Smarty/plugins/block.crmButton.php
+++ CRM/Core/Smarty/plugins/block.crmButton.php
@@ -58,5 +58,5 @@ function smarty_block_crmButton($params, $text, &$smarty) {
   // All other params are treated as html attributes
   CRM_Utils_Array::remove($params, 'icon', 'p', 'q', 'a', 'f', 'h', 'fb', 'fe');
   $attributes = CRM_Utils_String::htmlAttributes($params);
-  return "<a $attributes><span><span class='crm-i fa-$icon'></span>&nbsp; $text</span></a>";
+  return "<a $attributes><span><i class='crm-i fa-$icon'></i>&nbsp; $text</span></a>";
 }
