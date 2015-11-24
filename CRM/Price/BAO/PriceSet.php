0aa9b939836f06caf6c60a21dcd7a4052f1b0315
jitendrapurohit <jitendra.purohit@webaccessglobal.com>
2015-11-24 11:02:03 +0530

    use implodepadded

--- CRM/Price/BAO/PriceSet.php
+++ CRM/Price/BAO/PriceSet.php
@@ -917,7 +917,7 @@ WHERE  id = %1";
     }
     // @todo - stop setting amount level in this function & call the getAmountLevel function to retrieve it.
     if (!empty($amount_level)) {
-      $params['amount_level'] = CRM_Core_DAO::VALUE_SEPARATOR . implode(CRM_Core_DAO::VALUE_SEPARATOR, $amount_level) . CRM_Core_DAO::VALUE_SEPARATOR;
+      $params['amount_level'] = CRM_Utils_Array::implodePadded($amount_level);
       if (!empty($displayParticipantCount)) {
         $params['amount_level'] = CRM_Core_DAO::VALUE_SEPARATOR . implode(CRM_Core_DAO::VALUE_SEPARATOR, $amount_level) . $displayParticipantCount . CRM_Core_DAO::VALUE_SEPARATOR;
       }
