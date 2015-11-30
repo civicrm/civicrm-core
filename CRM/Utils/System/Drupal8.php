0a9fbcc563b0d292de63e074d67a356d5f4a5859
Coleman Watts <coleman@civicrm.org>
2015-11-29 22:15:35 -0500

    Convert to old array syntax for the sake of jenkins

--- CRM/Utils/System/Drupal8.php
+++ CRM/Utils/System/Drupal8.php
@@ -337,11 +337,11 @@ class CRM_Utils_System_Drupal8 extends CRM_Utils_System_DrupalBase {
 
     // Not all links that CiviCRM generates are Drupal routes, so we use the weaker ::fromUri method.
     try {
-      $url = \Drupal\Core\Url::fromUri("base:{$url['path']}", [
+      $url = \Drupal\Core\Url::fromUri("base:{$url['path']}", array(
         'query' => $url['query'],
         'fragment' => $fragment,
         'absolute' => $absolute,
-      ])->toString();
+      ))->toString();
     }
     catch (Exception $e) {
       // @Todo: log to watchdog
