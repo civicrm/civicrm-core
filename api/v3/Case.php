6e2110a18cdb8c4a198fe3cf4077b4c6675b1747
Saurabh Batra <saurabh.batra96@gmail.com>
2015-12-02 00:03:35 +0530

    CRM-17549: Case api now filters using status id too.
    ----------------------------------------
    * CRM-17549: Incorrect API results when searching for cases using client ID and other paramaters
      https://issues.civicrm.org/jira/browse/CRM-17549

--- api/v3/Case.php
+++ api/v3/Case.php
@@ -210,9 +210,18 @@ function civicrm_api3_case_get($params) {
   //search by client
   if (!empty($params['contact_id'])) {
     $ids = array();
+
+    if (isset($params['status_id'])) {
+      $statusId = civicrm_api3('OptionValue', 'getvalue', array(
+        'sequential' => 1,
+        'return' => 'value',
+        'label' => $params['status_id'],
+        'option_group_id' => "case_status",
+      ));
+    }
     foreach ((array) $params['contact_id'] as $cid) {
       if (is_numeric($cid)) {
-        $ids = array_merge($ids, CRM_Case_BAO_Case::retrieveCaseIdsByContactId($cid, TRUE));
+        $ids = array_merge($ids, CRM_Case_BAO_Case::retrieveCaseIdsByContactId($cid, TRUE, NULL, $statusId));
       }
     }
     $cases = array();
