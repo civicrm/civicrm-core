18face18d16d8a0bd6298027be9cccf4e938074d
Coleman Watts <coleman@civicrm.org>
2015-12-02 22:04:10 -0500

    CRM-17620 - expandRow add margin and smooth scrolling

--- js/crm.expandRow.js
+++ js/crm.expandRow.js
@@ -5,12 +5,13 @@ CRM.$(function($) {
     .on('click.crmExpandRow', 'a.crm-expand-row', function(e) {
       var $row = $(this).closest('tr');
       if ($(this).hasClass('expanded')) {
-        $row.next('.crm-child-row').remove();
+        $row.next('.crm-child-row').children('td').children('div.crm-ajax-container')
+          .slideUp('fast', function() {$(this).closest('.crm-child-row').remove();});
       } else {
         var count = $('td', $row).length,
-          $newRow = $('<tr class="crm-child-row"><td colspan="' + count + '"></td></tr>')
+          $newRow = $('<tr class="crm-child-row"><td colspan="' + count + '"><div></div></td></tr>')
             .insertAfter($row);
-        CRM.loadPage(this.href, {target: $('td', $newRow.animate({height: '3em'}, 'fast'))});
+        CRM.loadPage(this.href, {target: $('div', $newRow).animate({minHeight: '3em'}, 'fast')});
       }
       $(this).toggleClass('expanded');
       e.preventDefault();
