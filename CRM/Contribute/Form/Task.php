33f730ee38d84c5a5a255be624f9d7811c30e84e
eileen <emcnaughton@wikimedia.org>
2015-12-10 17:20:24 +1300

    CRM-17691 when doing a query to retrieve ids for export ... only retrieve the ids
    This has a substantial impact on peformance
    
    Change-Id: Iad952ec7d0a0725b24c982efcca8c35482d6ae63

--- CRM/Contribute/Form/Task.php
+++ CRM/Contribute/Form/Task.php
@@ -121,7 +121,7 @@ class CRM_Contribute_Form_Task extends CRM_Core_Form {
       }
 
       $form->_includesSoftCredits = CRM_Contribute_BAO_Query::isSoftCreditOptionEnabled($queryParams);
-      $query = new CRM_Contact_BAO_Query($queryParams, NULL, NULL, FALSE, FALSE,
+      $query = new CRM_Contact_BAO_Query($queryParams, array('contribution_id'), NULL, FALSE, FALSE,
         CRM_Contact_BAO_Query::MODE_CONTRIBUTE
       );
       // @todo the function CRM_Contribute_BAO_Query::isSoftCreditOptionEnabled should handle this
