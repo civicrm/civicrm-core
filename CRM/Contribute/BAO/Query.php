5837835b5c93c674d41a7e7ba8ce159877b5cdfd
eileen <emcnaughton@wikimedia.org>
2015-12-14 14:49:09 +1300

    CRM-17700 allow contribution get to bypass permissions
    Note this just extends the existing 'check_permissions' to apply to custom data as well. Using
    check permission defaults to 0 for php api & 1 for json (can not be overriden in json)

--- CRM/Contribute/BAO/Query.php
+++ CRM/Contribute/BAO/Query.php
@@ -49,11 +49,11 @@ class CRM_Contribute_BAO_Query {
    * @return array
    *   self::$_contributionFields  associative array of contribution fields
    */
-  public static function &getFields() {
+  public static function &getFields($checkPermission = TRUE) {
     if (!self::$_contributionFields) {
       self::$_contributionFields = array();
 
-      $fields = CRM_Contribute_BAO_Contribution::exportableFields();
+      $fields = CRM_Contribute_BAO_Contribution::exportableFields($checkPermission);
 
       unset($fields['contribution_contact_id']);
 
