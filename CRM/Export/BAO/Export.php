4bae328d5ed73aafca8ee5526e3e5bf461eddef7
Seamus Lee <seamuslee001@gmail.com>
2015-12-15 03:29:24 +0000

    CRM-17693 Expand default sizes to varchar 255

--- CRM/Export/BAO/Export.php
+++ CRM/Export/BAO/Export.php
@@ -1348,7 +1348,7 @@ INSERT INTO {$componentTable} SELECT distinct gc.contact_id FROM civicrm_group_c
             $sqlColumns[$fieldName] = "$fieldName varchar({$query->_fields[$field]['maxlength']})";
           }
           else {
-            $sqlColumns[$fieldName] = "$fieldName varchar(64)";
+            $sqlColumns[$fieldName] = "$fieldName varchar(255)";
           }
           break;
 
@@ -1423,12 +1423,12 @@ INSERT INTO {$componentTable} SELECT distinct gc.contact_id FROM civicrm_group_c
                 break;
 
               default:
-                $sqlColumns[$fieldName] = "$fieldName varchar(64)";
+                $sqlColumns[$fieldName] = "$fieldName varchar(255)";
                 break;
             }
           }
           else {
-            $sqlColumns[$fieldName] = "$fieldName varchar(64)";
+            $sqlColumns[$fieldName] = "$fieldName varchar(255)";
           }
         }
       }
