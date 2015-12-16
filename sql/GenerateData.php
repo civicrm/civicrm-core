53192dafbf30c65086f7aaf5a46bb95e6713c3ce
Pradeep Nayak <pradpnayak@gmail.com>
2015-12-16 19:44:18 +0530

    CRM-16259, set is_payment to 1 for sample data
    ----------------------------------------
    * CRM-16259: Create Payment API
      https://issues.civicrm.org/jira/browse/CRM-16259

--- sql/GenerateData.php
+++ sql/GenerateData.php
@@ -1879,6 +1879,7 @@ WHERE cefa.account_relationship = 1";
         'to_financial_account_id' => empty($financialAccountId[$result->payment_instrument_id]) ? $defaultFinancialAccount : $financialAccountId[$result->payment_instrument_id],
         'payment_instrument_id' => $result->payment_instrument_id,
         'check_number' => $result->check_number,
+        'is_payment' => 1,
       );
       $trxn = CRM_Core_BAO_FinancialTrxn::create($trxnParams);
       $financialItem = array(
