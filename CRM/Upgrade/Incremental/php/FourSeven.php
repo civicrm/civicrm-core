13599400c54c9cbad29ec695eb2ed6fc52646d35
David Reedy Jr <davidrjr.konadave@gmail.com>
2015-12-29 08:03:43 -0600

    CRM-17686 fix disabling of extension on upgrade

--- CRM/Upgrade/Incremental/php/FourSeven.php
+++ CRM/Upgrade/Incremental/php/FourSeven.php
@@ -140,6 +140,7 @@ class CRM_Upgrade_Incremental_php_FourSeven extends CRM_Upgrade_Incremental_Base
    * @param string $rev
    */
   public function upgrade_4_7_beta5($rev) {
+    $this->addTask(ts('Upgrade DB to %1: SQL', array(1 => $rev)), 'runSql', $rev);
     $this->addTask('Disable flexible jobs extension', 'disableFlexibleJobsExtension');
   }
 
@@ -380,7 +381,7 @@ FROM `civicrm_dashboard_contact` WHERE 1 GROUP BY contact_id";
    * @return bool
    */
   public function disableFlexibleJobsExtension(CRM_Queue_TaskContext $ctx) {
-    CRM_Core_DAO::setFieldValue('CRM_Core_DAO_Job', 'Flexible Jobs', 'is_active', 0, 'name');
+    civicrm_api3('Extension', 'disable', array('key' => 'com.klangsoft.flexiblejobs'));
 
     return TRUE;
   }
