c58ba74baa59aa9f9b7fe65ed479ad8a18a9f2cd
David Reedy Jr <davidrjr.konadave@gmail.com>
2015-12-31 17:27:15 -0600

    CRM-17686 move from beta5 to beta6

--- CRM/Upgrade/Incremental/php/FourSeven.php
+++ CRM/Upgrade/Incremental/php/FourSeven.php
@@ -139,7 +139,7 @@ class CRM_Upgrade_Incremental_php_FourSeven extends CRM_Upgrade_Incremental_Base
    *
    * @param string $rev
    */
-  public function upgrade_4_7_beta5($rev) {
+  public function upgrade_4_7_beta6($rev) {
     $this->addTask(ts('Upgrade DB to %1: SQL', array(1 => $rev)), 'runSql', $rev);
     $this->addTask('Disable flexible jobs extension', 'disableFlexibleJobsExtension');
   }
