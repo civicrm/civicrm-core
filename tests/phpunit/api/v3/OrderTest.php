73c0e107070aba24531fc19bb91086003c8bf956
Pradeep Nayak <pradpnayak@gmail.com>
2016-01-27 02:01:47 +0530

    CRM-16188, added order api test for get action
    ----------------------------------------
    * CRM-16188: Create an order API
      https://issues.civicrm.org/jira/browse/CRM-16188

---
+++ tests/phpunit/api/v3/OrderTest.php
@@ -0,0 +1,153 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 4.7                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2015                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License and the CiviCRM Licensing Exception along                  |
+ | with this program; if not, contact CiviCRM LLC                     |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+ */
+
+require_once 'CiviTest/CiviUnitTestCase.php';
+
+
+/**
+ *  Test APIv3 civicrm_contribute_* functions
+ *
+ * @package CiviCRM_APIv3
+ * @subpackage API_Contribution
+ */
+class api_v3_OrderTest extends CiviUnitTestCase {
+
+  /**
+   * Assume empty database with just civicrm_data.
+   */
+  protected $_individualId;
+  protected $_financialTypeId = 1;
+  protected $_apiversion;
+  public $debug = 0;
+
+  /**
+   * Setup function.
+   */
+  public function setUp() {
+    parent::setUp();
+
+    $this->_apiversion = 3;
+    $this->_individualId = $this->individualCreate();
+  }
+
+  /**
+   * Clean up after each test.
+   */
+  public function tearDown() {
+    $this->quickCleanUpFinancialEntities();
+    $this->quickCleanup(array('civicrm_uf_match'));
+  }
+
+  /**
+   * Test Get Payment api.
+   */
+  public function testGetOrder() {
+    $contribution = $this->addOrder(FALSE, 100);
+
+    $params = array(
+      'contribution_id' => $contribution['id'],
+    );
+
+    $order = $this->callAPISuccess('Order', 'get', $params);
+
+    $this->assertEquals(1, $order['count']);
+    $expectedResult = array($contribution['id'] => array(
+      'total_amount' => 100,
+      'contribution_id' => $contribution['id'],
+      'contribution_status' => 'Completed',
+      'net_amount' => 100,
+    ));
+    $lineItems[] = array(
+      'entity_table' => 'civicrm_contribution',
+      'entity_id' => $contribution['id'],
+      'contribution_id' => $contribution['id'],
+      'unit_price' => 100,
+      'line_total' => 100,
+      'financial_type_id' => 1,
+    );
+    $this->checkPaymentResult($order, $expectedResult, $lineItems);
+    $this->callAPISuccess('Contribution', 'Delete', array(
+      'id' => $contribution['id'],
+    ));
+  }
+
+  /**
+   * Function to assert db values
+   */
+  public function checkPaymentResult($results, $expectedResult, $lineItems = NULL) {
+    foreach ($expectedResult[$results['id']] as $key => $value) {
+      $this->assertEquals($results['values'][$results['id']][$key], $value);
+    }
+
+    if ($lineItems) {
+      foreach ($lineItems as $key => $items) {
+        foreach ($items as $k => $item) {
+          $this->assertEquals($results['values'][$results['id']]['line_items'][$key][$k], $item);
+        }
+      }
+    }
+  }
+
+  /**
+   * add order
+   *
+   * @param bool $isPriceSet
+   * @param float $amount
+   * @param array $extraParams
+   *
+   * @return array
+   */
+  public function addOrder($isPriceSet, $amount = 300, $extraParams = array()) {
+    $p = array(
+      'contact_id' => $this->_individualId,
+      'receive_date' => '2010-01-20',
+      'total_amount' => $amount,
+      'financial_type_id' => $this->_financialTypeId,
+      'contribution_status_id' => 1,
+    );
+
+    if ($isPriceSet) {
+      $priceFields = $this->createPriceSet();
+      foreach ($priceFields['values'] as $key => $priceField) {
+        $lineItems[1][$key] = array(
+          'price_field_id' => $priceField['price_field_id'],
+          'price_field_value_id' => $priceField['id'],
+          'label' => $priceField['label'],
+          'field_title' => $priceField['label'],
+          'qty' => 1,
+          'unit_price' => $priceField['amount'],
+          'line_total' => $priceField['amount'],
+          'financial_type_id' => $priceField['financial_type_id'],
+        );
+      }
+      $p['line_item'] = $lineItems;
+    }
+    $p = array_merge($extraParams, $p);
+    return $this->callAPISuccess('Contribution', 'create', $p);
+  }
+
+}
