3a360b0c070b604f519527ba9c8cfb7c068ab89e
Tim Otten <totten@civicrm.org>
2016-01-25 17:49:32 -0800

    SettingsBag - Fix upgrade failure for WP 4.4=>4.7
    The WP integration attempts to define a new setting (`wpLoadPhp`) during
    bootstrap -- before upgrade logic has run.  This was failing because the
    `group_name` property wouldn't get passed through by DAO (because the DAO is
    newer than the schema).

--- CRM/Core/Config.php
+++ CRM/Core/Config.php
@@ -413,6 +413,15 @@ class CRM_Core_Config extends CRM_Core_Config_MagicMerge {
       return TRUE;
     }
 
+    if ($path && preg_match('/^civicrm\/ajax\/l10n-js/', $path)
+      && !empty($_SERVER['HTTP_REFERER'])
+    ) {
+      $ref = parse_url($_SERVER['HTTP_REFERER']);
+      if (preg_match('/civicrm\/upgrade/', $ref['path']) || preg_match('/civicrm\/upgrade/', urldecode($ref['query']))) {
+        return TRUE;
+      }
+    }
+
     return FALSE;
   }
 
