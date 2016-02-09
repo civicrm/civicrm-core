e96dfa5047e346391ba825477f49e1abdd320da0
Tim Otten <totten@civicrm.org>
2016-02-08 18:58:22 -0800

    CRM-17860 - Civi\Test - Add mixins for Headless, Hook, Transaction, and EndToEnd functionality.
    To describe the environment in which you want to run a test, add an
    interface to the test class, e.g.
    
     * `HeadlessInterface` (along with optional `HookInterface` and `TransactionalInterface`)
     * `EndToEndInterface`
    
    This is an alternative to other approaches like:
    
     * Extending a gigantic base-class (CiviUnitTestCase, CiviSeleniumTestCase)
     * Copy/paste a bunch of boiler-plate into your own class.

---
+++ Civi/Test/EndToEndInterface.php
@@ -0,0 +1,18 @@
+<?php
+namespace Civi\Test;
+
+/**
+ * Interface EndToEndInterface
+ * @package Civi\Test
+ *
+ * To run your test against a live, CMS-integrated database, flag it with the the
+ * EndToEndInterface.
+ *
+ * Alternatively, if you wish to run a test in a live (CMS-enabled) environment,
+ * flag it with EndToEndInterface.
+ *
+ * @see HeadlessInterface
+ */
+interface EndToEndInterface {
+
+}
