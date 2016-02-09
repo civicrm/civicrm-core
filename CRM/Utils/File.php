535270435e9588ec487e91fe4462b2e6aa1ac606
Tim Otten <totten@civicrm.org>
2016-02-09 10:45:40 -0800

    CRM_Utils_File::createDir - Better docs

--- CRM/Utils/File.php
+++ CRM/Utils/File.php
@@ -102,10 +102,14 @@ class CRM_Utils_File {
    *   The path name.
    * @param bool $abort
    *   Should we abort or just return an invalid code.
+   * @return bool|NULL
+   *   NULL: Folder already exists or was not specified.
+   *   TRUE: Creation succeeded.
+   *   FALSE: Creation failed.
    */
   public static function createDir($path, $abort = TRUE) {
     if (is_dir($path) || empty($path)) {
-      return;
+      return NULL;
     }
 
     CRM_Utils_File::createDir(dirname($path), $abort);
