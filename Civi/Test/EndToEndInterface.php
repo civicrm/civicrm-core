98f566826c214ca1a7c5abfd3a74648b648f487b
Tim Otten <totten@civicrm.org>
2016-02-09 18:08:38 -0800

    CRM-17860 - EndToEndInterface - Docs

--- Civi/Test/EndToEndInterface.php
+++ Civi/Test/EndToEndInterface.php
@@ -8,8 +8,18 @@ namespace Civi\Test;
  * To run your test against a live, CMS-integrated database, flag it with the the
  * EndToEndInterface.
  *
- * Alternatively, if you wish to run a test in a live (CMS-enabled) environment,
- * flag it with EndToEndInterface.
+ * Note: The global variable $_CV should be pre-populated with some interesting data:
+ *
+ * - $_CV['CMS_URL']
+ * - $_CV['ADMIN_USER']
+ * - $_CV['ADMIN_PASS']
+ * - $_CV['ADMIN_EMAIL']
+ * - $_CV['DEMO_USER']
+ * - $_CV['DEMO_PASS']
+ * - $_CV['DEMO_EMAIL']
+ *
+ * Alternatively, if you wish to run a test in a headless environment,
+ * flag it with HeadlessInterface.
  *
  * @see HeadlessInterface
  */
