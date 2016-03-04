739021bfd9dca6616c2af928ad7635e4fb767f62
Stephen Palmstrom <stephen.palmstrom@outlook.com>
2016-03-04 14:27:05 +0000

    Apply time zone patches

--- CRM/Utils/System/Joomla.php
+++ CRM/Utils/System/Joomla.php
@@ -725,5 +725,26 @@ class CRM_Utils_System_Joomla extends CRM_Utils_System_Base {
   public function appendCoreResources(&$list) {
     $list[] = 'js/crm.joomla.js';
   }
+  /**
+   * Get the time zone from Joomla
+   */
+  public function getTimeZoneString() {
+      $config = JFactory::getConfig();
+      $user = JFactory::getUser();
+      $timezone = $user->getParam('timezone');
+      //$phpTimezone = ini_get('date.timezone');
+      if(!$timezone)
+      {
+          // Get the timezone configured in Joomla
+          $timezone = $config->get('offset');
+      }
+      if (!timezone)
+      {
+          // Get the time in PHP.
+          $timezone = ini_get('date.timezone');
+      }
+
+    return $timezone; 
+}
 
 }
