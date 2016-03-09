62fe8f639df316f63ceca69aa20fc8f8ab5e5a73
Seamus Lee <seamuslee001@gmail.com>
2016-03-09 02:09:03 +0000

    Ensure that we are only running the Mailings query when we have found mailings

--- CRM/Mailing/BAO/Mailing.php
+++ CRM/Mailing/BAO/Mailing.php
@@ -2433,15 +2433,17 @@ LEFT JOIN civicrm_mailing_group g ON g.mailing_id   = m.id
         $mailingIDs[] = $dao->id;
       }
       //CRM-18181 Get all mailings that use the mailings found earlier as receipients
-      $mailings = implode(',', $mailingIDs);
-      $mailingQuery = "
-         SELECT DISTINCT ( m.id ) as id
-         FROM civicrm_mailing m 
-         LEFT JOIN civicrm_mailing_group g ON g.mailing_id = m.id
-         WHERE g.entity_table like 'civicrm_mailing%' AND g.entity_id IN ($mailings)";
-      $mailingDao = CRM_Core_DAO::executeQuery($mailingQuery);
-      while ($mailingDao->fetch()) {
-        $mailingIDs[] = $mailingDao->id;
+      if (!empty($mailingIDs)) {
+        $mailings = implode(',', $mailingIDs);
+        $mailingQuery = "
+           SELECT DISTINCT ( m.id ) as id
+           FROM civicrm_mailing m 
+           LEFT JOIN civicrm_mailing_group g ON g.mailing_id = m.id
+           WHERE g.entity_table like 'civicrm_mailing%' AND g.entity_id IN ($mailings)";
+        $mailingDao = CRM_Core_DAO::executeQuery($mailingQuery);
+        while ($mailingDao->fetch()) {
+          $mailingIDs[] = $mailingDao->id;
+        }
       }
     }
 
