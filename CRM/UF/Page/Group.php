34bcc8bce34c356c44e36f096f206da8082c4b02
Pradeep Nayak <pradpnayak@gmail.com>
2016-03-10 19:19:53 +0530

    --CRM-18204, remove Create mode, Edit mode, listing mode and HMTL form snippet links if uf group type is not contact

--- CRM/UF/Page/Group.php
+++ CRM/UF/Page/Group.php
@@ -357,6 +357,9 @@ class CRM_UF_Page_Group extends CRM_Core_Page {
       $componentFound = array_diff(array_keys($groupTypes), $contactTypes);
       if (!empty($componentFound)) {
         $action -= CRM_Core_Action::ADD;
+        $action -= CRM_Core_Action::ADVANCED;
+        $action -= CRM_Core_Action::BASIC;
+        $action -= CRM_Core_Action::PROFILE;
       }
 
       $ufGroup[$id]['group_type'] = self::formatGroupTypes($groupTypes);
