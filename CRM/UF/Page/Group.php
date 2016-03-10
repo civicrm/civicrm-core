c66caa2573ebcda38eeca1d7647688e62b0c390c
Pradeep Nayak <pradpnayak@gmail.com>
2016-03-10 19:18:52 +0530

    CRM-18204, used contact type and contact sub type for checking uf group type
    ----------------------------------------
    * CRM-18204: Enables use of Mixed Profile for create mode
      https://issues.civicrm.org/jira/browse/CRM-18204

--- CRM/UF/Page/Group.php
+++ CRM/UF/Page/Group.php
@@ -351,8 +351,10 @@ class CRM_UF_Page_Group extends CRM_Core_Page {
       $groupTypes = self::extractGroupTypes($value['group_type']);
 
       // drop Create, Edit and View mode links if profile group_type is one of the following:
-      $groupComponents = array('Contribution', 'Membership', 'Activity', 'Participant', 'Case');
-      $componentFound = array_intersect($groupComponents, array_keys($groupTypes));
+      // Contribution, Membership, Activity, Participant, Case, Grant
+      $contactTypes = CRM_Contact_BAO_ContactType::contactTypes();
+      $contactTypes[] = 'Contact';
+      $componentFound = array_diff(array_keys($groupTypes), $contactTypes);
       if (!empty($componentFound)) {
         $action -= CRM_Core_Action::ADD;
       }
