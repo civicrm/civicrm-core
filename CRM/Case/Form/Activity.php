ecdc78923bf172f6d6b1b1dda7d781ff094a3567
yashodha <yashodha.chaku@webaccessglobal.com>
2016-03-25 14:16:45 +0530

    CRM-18290: Edits to activities are not saved in CiviCase
    ----------------------------------------
    * CRM-18290: Edits to activities are not saved in CiviCase
      https://issues.civicrm.org/jira/browse/CRM-18290

--- CRM/Case/Form/Activity.php
+++ CRM/Case/Form/Activity.php
@@ -447,7 +447,7 @@ class CRM_Case_Form_Activity extends CRM_Activity_Form_Activity {
       if ($this->_activityId) {
         // retrieve and include the custom data of old Activity
         $oldActivity = civicrm_api3('Activity', 'getsingle', array('id' => $this->_activityId));
-        $params = array_merge($params, $oldActivity);
+        $params = array_merge($oldActivity, $params);
 
         // unset custom fields-id from params since we want custom
         // fields to be saved for new activity.
