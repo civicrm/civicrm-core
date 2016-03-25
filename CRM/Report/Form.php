9cc3aa442d103e418ccc67cc96bf078ee2e06d01
Joseph Lacey <joseph@palantetech.coop>
2016-03-25 11:00:00 -0400

    CRM-18276 fixing regression by initializing  array.
    ----------------------------------------
    * CRM-18276: Report custom data table retrieval fails when tables are more than 1024 characters
      https://issues.civicrm.org/jira/browse/CRM-18276

--- CRM/Report/Form.php
+++ CRM/Report/Form.php
@@ -3660,6 +3660,7 @@ ORDER BY cg.weight, cf.weight";
     $mapper = CRM_Core_BAO_CustomQuery::$extendsMap;
     //CRM-18276 GROUP_CONCAT could be used with singleValueQuery and then exploded,
     //but by default that truncates to 1024 characters, which causes errors with installs with lots of custom field sets
+    $customTables = array();
     $customTablesDAO = CRM_Core_DAO::executeQuery("SELECT table_name FROM civicrm_custom_group", CRM_Core_DAO::$_nullArray);
     while ($customTablesDAO->fetch()) {
       $customTables[] = $customTablesDAO->table_name;
