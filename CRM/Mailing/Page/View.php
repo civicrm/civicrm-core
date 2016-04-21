cea3e69fe51b706eaf5902a842f8473b91810fc8
Web Access <rohan.katkar@webaccessglobal.com>
2016-04-21 11:48:40 +0530

    CRM-18421: QA warning fix

--- CRM/Mailing/Page/View.php
+++ CRM/Mailing/Page/View.php
@@ -164,7 +164,7 @@ class CRM_Mailing_Page_View extends CRM_Core_Page {
       $details = CRM_Utils_Array::value(0, $details[0]);
       $contactId = 0;
     }
-    $mime = &$this->_mailing->compose(NULL, NULL, NULL, $contactId,
+    $mime = $this->_mailing->compose(NULL, NULL, NULL, $contactId,
       $this->_mailing->from_email,
       $this->_mailing->from_email,
       TRUE, $details, $attachments
