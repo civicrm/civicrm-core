c9e4a4a554d074d32503e79894b0dd71448702cf
KarinG <karin@semper-it.com>
2016-04-22 09:12:15 -0600

    CRM-18463 - Not in between filter - not working properly.

--- CRM/Report/Form.php
+++ CRM/Report/Form.php
@@ -1783,7 +1783,7 @@ class CRM_Report_Form extends CRM_Core_Form {
               $clauses[] = "( {$field['dbAlias']} >= $min )";
             }
             else {
-              $clauses[] = "( {$field['dbAlias']} < $min )";
+              $clauses[] = "( {$field['dbAlias']} < $min OR {$field['dbAlias']} IS NULL )";
             }
           }
           if ($max) {
@@ -1800,7 +1800,7 @@ class CRM_Report_Form extends CRM_Core_Form {
               $clause = implode(' AND ', $clauses);
             }
             else {
-              $clause = implode(' OR ', $clauses);
+              $clause = '(' . implode('OR', $clauses) . ')';
             }
           }
         }
