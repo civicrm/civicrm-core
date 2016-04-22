353918bc728cfe4f0e62b93fa0b58d7870581c7a
eileenmcnaugton <eileen@fuzion.co.nz>
2016-04-22 20:28:39 +1200

    Spec function for revert

--- api/v3/Logging.php
+++ api/v3/Logging.php
@@ -54,6 +54,32 @@ function civicrm_api3_logging_revert($params) {
  *
  * @param array $params
  *
+ * @throws \API_Exception
+ * @throws \Civi\API\Exception\UnauthorizedException
+ */
+function _civicrm_api3_logging_revert_spec(&$params) {
+  $params['log_conn_id'] = array(
+    'title' => 'Logging Connection ID',
+    'type' => CRM_Utils_Type::T_STRING,
+    'api.required' => TRUE,
+  );
+  $params['log_date'] = array(
+    'title' => 'Logging Timestamp',
+    'type' => CRM_Utils_Type::T_DATE + CRM_Utils_Type::T_TIME,
+  );
+  $params['interval'] = array(
+    'title' => ts('Interval (required if date is included)'),
+    'type' => CRM_Utils_Type::T_STRING,
+    'api.default' => '10 SECOND',
+    'description' => ts('Used when log_date is passed in'),
+  );
+}
+
+/**
+ * Get a log change.
+ *
+ * @param array $params
+ *
  * @return array
  *    API Success Array
  * @throws \API_Exception
