5e153f7f1430cdbcdcb0000256fa377bd5007dfb
eileenmcnaugton <eileen@fuzion.co.nz>
2016-04-27 19:37:02 +1200

    CRM-18469, CRM-17984 - getTree regression on multiple integers separated by the cnrtl char

--- CRM/Core/BAO/CustomGroup.php
+++ CRM/Core/BAO/CustomGroup.php
@@ -350,7 +350,12 @@ class CRM_Core_BAO_CustomGroup extends CRM_Core_DAO_CustomGroup {
         $subTypes = array();
       }
       else {
-        $subTypes = explode(',', $subTypes);
+        if (stristr(',', $subTypes)) {
+          $subTypes = explode(',', $subTypes);
+        }
+        else {
+          $subTypes = explode(CRM_Core_DAO::VALUE_SEPARATOR, trim($subTypes, CRM_Core_DAO::VALUE_SEPARATOR));
+        }
       }
     }
 
