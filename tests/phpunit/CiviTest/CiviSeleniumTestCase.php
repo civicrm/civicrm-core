4cb7f6e60a09752f3315d53926b077f1f505c9f5
jitendrapurohit <jitendra.purohit@webaccessglobal.com>
2016-04-29 19:00:59 +0530

    CRM-18454 - webtest fixes

--- tests/phpunit/CiviTest/CiviSeleniumTestCase.php
+++ tests/phpunit/CiviTest/CiviSeleniumTestCase.php
@@ -585,7 +585,7 @@ class CiviSeleniumTestCase extends PHPUnit_Extensions_SeleniumTestCase {
     $day = date('j', $timeStamp);
 
     if (!$multiselect) {
-      $this->click("xpath=//input[starts-with(@id, '{$dateElement}_display_')]");
+      $this->click($dateElement);
     }
     $this->waitForElementPresent("css=div#ui-datepicker-div.ui-datepicker div.ui-datepicker-header div.ui-datepicker-title select.ui-datepicker-month");
     $this->select("css=div#ui-datepicker-div.ui-datepicker div.ui-datepicker-header div.ui-datepicker-title select.ui-datepicker-month", "value=$mon");
