f82f7ac94a68732798ea87b7180bf3080481cd7e
Mattias Michaux <mattias.michaux@gmail.com>
2016-05-03 22:53:59 +0200

    Added extra Mysql types and rules + tests similar to 4.7

--- CRM/Utils/Rule.php
+++ CRM/Utils/Rule.php
@@ -103,6 +103,9 @@ class CRM_Utils_Rule {
     }
 
     // Ensure it only contains valid characters (alphanumeric and underscores).
+    //
+    // MySQL permits column names that don't match this (eg containing spaces),
+    // but CiviCRM won't create those ...
     if (!preg_match('/^\w{1,64}(\.\w{1,64})?$/i', $str)) {
       return FALSE;
     }
@@ -121,6 +124,25 @@ class CRM_Utils_Rule {
   public static function mysqlOrderByDirection($str) {
     if (!preg_match('/^(asc|desc)$/i', $str)) {
       return FALSE;
+    }
+
+    return TRUE;
+  }
+
+  /**
+   * Validate that a string is valid order by clause.
+   *
+   * @param $str
+   * @return bool
+   */
+  public static function mysqlOrderBy($str) {
+    // Making a regex for a comma separated list is quite hard and not readable
+    // at all, so we split and loop over.
+    $parts = explode(',', $str);
+    foreach ($parts as $part) {
+      if (!preg_match('/^((\w{1,64})((\.)(\w{1,64}))?( (asc|desc))?)$/i', trim($part))) {
+        return FALSE;
+      }
     }
 
     return TRUE;
