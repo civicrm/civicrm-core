15c84df04e79d074181367e9c7cedb65b7423ee9
Johan Vervloet <johanv@johanv.org>
2016-05-12 14:30:35 +0200

    CRM-18544 - This fixes the issue with custom fields for localized installs.
    In localized installs, custom fields on contact subtypes break the
    create/edit forms for contacts. This is a fix.
    
    ----------------------------------------
    * CRM-18544: Problem with custom fields for contact subtypes with localized installation
      https://issues.civicrm.org/jira/browse/CRM-18544

--- CRM/Core/BAO/CustomGroup.php
+++ CRM/Core/BAO/CustomGroup.php
@@ -652,7 +652,7 @@ ORDER BY civicrm_custom_group.weight,
       return $subType;
     }
     $contactTypes = civicrm_api3('Contact', 'getoptions', array('field' => 'contact_type'));
-    if ($entityType != 'Contact' && !in_array($entityType, $contactTypes['values'])) {
+    if ($entityType != 'Contact' && !isset($contactTypes['values'][$entityType])) {
       // Not quite sure if we want to fail this hard. But quiet ignore would be pretty bad too.
       // Am inclined to go with this for RC release & considering softening.
       throw new CRM_Core_Exception('Invalid Entity Filter');
