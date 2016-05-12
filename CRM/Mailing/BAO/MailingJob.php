4032f8f4f2ac3d6c47bdf976889994f3d4395c01
Jaap Jansma <jaap@edeveloper.nl>
2016-05-12 20:23:29 +0200

    CRM-18159 fixed styles issues and moved buildAcl to CRM_ACL_BAO_ACL

--- CRM/Mailing/BAO/MailingJob.php
+++ CRM/Mailing/BAO/MailingJob.php
@@ -478,40 +478,6 @@ VALUES (%1, %2, %3, %4, %5, %6, %7)
   }
 
   /**
-   * Build a join and where part for a query
-   *
-   * @param $contact_id
-   * @return array - the first key is join part of the query and the second key is the where part of the query
-   */
-  public function buildAcl($contact_id) {
-    $tables = array();
-    $whereTables = array();
-    $whereClause = CRM_ACL_BAO_ACL::whereClause(CRM_Core_Permission::VIEW, $tables, $whereTables, $contact_id);
-    if (strlen($whereClause)) {
-      $whereClause = " AND (".$whereClause.")";
-    }
-
-    $join = "";
-    foreach ($whereTables as $name => $value) {
-      if (!$value) {
-        continue;
-      }
-      if ($value != 1) {
-        // if there is already a join statement in value, use value itself
-        if (strpos($value, 'JOIN')) {
-          $join .= " $value ";
-        }
-        continue;
-      }
-    }
-
-    return array (
-      $join,
-      $whereClause
-    );
-  }
-
-  /**
    * Send the mailing.
    *
    * @param object $mailer
@@ -533,7 +499,7 @@ VALUES (%1, %2, %3, %4, %5, %6, %7)
     $edTable = CRM_Mailing_Event_BAO_Delivered::getTableName();
     $ebTable = CRM_Mailing_Event_BAO_Bounce::getTableName();
 
-    list($acl_join, $acl_where) = $this->buildAcl($mailing->created_id);
+    list($aclJoin, $aclWhere) = CRM_ACL_BAO_ACL::buildAcl($mailing->created_id);
 
     $query = "  SELECT      $eqTable.id,
                                 $emailTable.email as email,
@@ -549,12 +515,12 @@ VALUES (%1, %2, %3, %4, %5, %6, %7)
                             ON  $eqTable.id = $edTable.event_queue_id
                     LEFT JOIN   $ebTable
                             ON  $eqTable.id = $ebTable.event_queue_id
-                    $acl_join
+                    $aclJoin
                     WHERE       $eqTable.job_id = " . $this->id . "
                         AND     $edTable.id IS null
                         AND     $ebTable.id IS null
                         AND    contact_a.is_opt_out = 0 
-                        $acl_where";
+                        $aclWhere";
 
     if ($mailing->sms_provider_id) {
       $query = "
@@ -572,13 +538,13 @@ VALUES (%1, %2, %3, %4, %5, %6, %7)
                             ON  $eqTable.id = $edTable.event_queue_id
                     LEFT JOIN   $ebTable
                             ON  $eqTable.id = $ebTable.event_queue_id
-                    $acl_join
+                    $aclJoin
                     WHERE       $eqTable.job_id = " . $this->id . "
                         AND     $edTable.id IS null
                         AND     $ebTable.id IS null
                         AND    ( contact_a.is_opt_out = 0
                         OR       contact_a.do_not_sms = 0 )
-                        $acl_where}";
+                        $aclWhere}";
     }
     $eq->query($query);
 
