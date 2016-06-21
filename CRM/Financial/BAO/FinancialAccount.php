fed4d93dc251444734b9b99a3224f6a6e54a1c10
Edsel <edsel.lopez@jmaconsulting.biz>
2016-06-21 18:28:53 +0530

    CRM-16189 Bug fixes
    ----------------------------------------
    * CRM-16189: Improve support for Accrual Method bookkeeping
      https://issues.civicrm.org/jira/browse/CRM-16189

--- CRM/Financial/BAO/FinancialAccount.php
+++ CRM/Financial/BAO/FinancialAccount.php
@@ -317,7 +317,7 @@ WHERE ce.entity_table = 'civicrm_financial_type' AND ce.account_relationship = %
       return FALSE;
    }
     $sql = "SELECT f.financial_account_type_id FROM civicrm_financial_account f
-NNER JOIN civicrm_financial_item fi ON fi.financial_account_id = f.id
+INNER JOIN civicrm_financial_item fi ON fi.financial_account_id = f.id
 INNER JOIN civicrm_option_value cv ON cv.value = f.financial_account_type_id AND cv.name IN ('Revenue', 'Liability')
 INNER JOIN civicrm_option_group cg ON cg.id = cv.option_group_id
 AND  cg.name = 'financial_account_type'
@@ -325,7 +325,7 @@ WHERE f.id = %1
 LIMIT 1";
     $params = array(1 => array($financialAccountId, 'Integer'));
     $result = CRM_Core_DAO::singleValueQuery($sql, $params);
-   if ($result && $result != $financialAccountTypeID) {
+    if ($result && $result != $financialAccountTypeID) {
       return TRUE;
     }
     return FALSE;
