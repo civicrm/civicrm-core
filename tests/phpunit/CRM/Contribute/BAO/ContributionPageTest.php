a630a84edd8b61d087e994eda0b754fbd8f63088
eileen <emcnaughton@wikimedia.org>
2016-07-06 13:16:00 +1200

    [NFC] CRM-19033 improve standardisation of tests
    NB jenkins is doing wierd things here so trying a small subset to see what jenkins does

--- tests/phpunit/CRM/Contribute/BAO/ContributionPageTest.php
+++ tests/phpunit/CRM/Contribute/BAO/ContributionPageTest.php
@@ -25,8 +25,6 @@
  +--------------------------------------------------------------------+
  */
 
-require_once 'CiviTest/Contact.php';
-require_once 'CiviTest/ContributionPage.php';
 require_once 'CiviTest/Custom.php';
 require_once 'CiviTest/PaypalPro.php';
 
