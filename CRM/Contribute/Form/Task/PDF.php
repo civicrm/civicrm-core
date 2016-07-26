82d4a82426397ec908aa3c8486e1a46402832d2e
Seamus Lee <seamuslee001@gmail.com>
2016-07-26 15:06:55 +1000

    CRM-16923 Fix Enotice error by setting payment Processor id

--- CRM/Contribute/Form/Task/PDF.php
+++ CRM/Contribute/Form/Task/PDF.php
@@ -182,6 +182,15 @@ AND    {$this->_componentClause}";
       $input['trxn_date'] = isset($contribution->trxn_date) ? $contribution->trxn_date : NULL;
       $input['receipt_update'] = $params['receipt_update'];
       $input['contribution_status_id'] = $contribution->contribution_status_id;
+      $payment_processor_id = NULL;
+      $processorDAO = CRM_Core_DAO::executeQuery("SELECT payment_processor_id
+        FROM civicrm_financial_trxn
+        WHERE trxn_id = %1", array(
+          1 => array($contribution->trxn_id, 'String')));
+      while ($processorDAO->fetch()) {
+        $payment_processor_id = $processorDAO->payment_processor_id;
+      }
+      $input['paymentProcessor'] = $payment_processor_id;
 
       // CRM_Contribute_BAO_Contribution::composeMessageArray expects mysql formatted date
       $objects['contribution']->receive_date = CRM_Utils_Date::isoToMysql($objects['contribution']->receive_date);
