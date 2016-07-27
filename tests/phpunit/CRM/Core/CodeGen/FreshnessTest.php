4ef04170cf2b11c1b4ffbb0186519dffb90ccbe4
Tim Otten <totten@civicrm.org>
2016-07-27 14:26:49 -0700

    CRM_Core_DAO_AllCoreTables - Make persistent. Split data and logic.
    Changes:
     * Before, CRM_Core_DAO_AllCoreTables was generated with a mix of logic and data.
       Now, the class is a static file, and only the data is generated. This makes it
       easier to develop.
     * Before, the file was not commited. As with the DAOs, it is now committed.

--- tests/phpunit/CRM/Core/CodeGen/FreshnessTest.php
+++ tests/phpunit/CRM/Core/CodeGen/FreshnessTest.php
@@ -10,6 +10,48 @@
 class CRM_Core_CodeGen_FreshnessTest extends CiviUnitTestCase {
 
   public function testDAOs() {
+    $tasks = $this->findTasks('CRM_Core_CodeGen_DAO');
+    $names = array();
+    foreach ($tasks as $task) {
+      $names[] = $task->name;
+      $this->assertFalse($task->needsUpdate(), "Expect DAO for {$task->name} is up-to-date");
+    }
+
+    // Pick some example to ensure the loop ran with real values.
+    $this->assertTrue(in_array('civicrm_contact', $names),
+      'Expect the list of tables to include civicrm_contact');
+  }
+
+  public function testReflection() {
+    $tasks = $this->findTasks('CRM_Core_CodeGen_Reflection');
+    $this->assertEquals(1, count($tasks), 'Expect to find one Reflection task');
+    foreach ($tasks as $task) {
+      $this->assertFalse($task->needsUpdate(), "Expect AllCoreTables to be up-to-date");
+    }
+  }
+
+  /**
+   * Get a list of GenCode tasks, filtered by type.
+   *
+   * @param string $clazz
+   * @return array<CRM_Core_CodeGen_ITask>
+   */
+  protected function findTasks($clazz) {
+    $genCode = $this->createCodeGen();
+    $tasks = $genCode->getTasks();
+    $matches = array();
+    foreach ($tasks as $task) {
+      if ($task instanceof $clazz) {
+        $matches[] = $task;
+      }
+    }
+    return $matches;
+  }
+
+  /**
+   * @return \CRM_Core_CodeGen_Main
+   */
+  protected function createCodeGen() {
     $path = rtrim($GLOBALS['civicrm_root'], '/');
 
     $genCode = new CRM_Core_CodeGen_Main(
@@ -27,20 +69,7 @@ class CRM_Core_CodeGen_FreshnessTest extends CiviUnitTestCase {
       $path . '/xml/schema/Schema.xml', // schema file
       NULL  // path to digest file
     );
-
-    $tasks = $genCode->getTasks();
-    $names = array();
-    foreach ($tasks as $task) {
-      if ($task instanceof CRM_Core_CodeGen_DAO) {
-        $names[] = $task->name;
-        $this->assertFalse($task->needsUpdate(),
-          "Expect DAO for {$task->name} is up-to-date.");
-      }
-    }
-
-    // Pick some example to ensure the loop ran with real values.
-    $this->assertTrue(in_array('civicrm_contact', $names),
-      'Expect the list of tables to include civicrm_contact');
+    return $genCode;
   }
 
 }
