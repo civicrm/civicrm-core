1825e34debd3a67108f6f507fe851bf5ead71f78
Tim Otten <totten@civicrm.org>
2016-07-27 00:26:59 -0700

    CRM-14885 - CRM_Core_CodeGen_DAO - Use checksum to avoid recomputation

--- CRM/Core/CodeGen/DAO.php
+++ CRM/Core/CodeGen/DAO.php
@@ -10,27 +10,72 @@ class CRM_Core_CodeGen_DAO extends CRM_Core_CodeGen_BaseTask {
    */
   public $name;
 
+  /**
+   * @var string
+   */
+  private $checksum;
+
   public function __construct($config, $name) {
     parent::__construct($config);
     $this->name = $name;
   }
 
-  public function run() {
-    $name = $this->name;
-    echo "Generating $name as " . $this->tables[$name]['fileName'] . "\n";
+  /**
+   * @return bool
+   *   TRUE if an update is needed.
+   */
+  public function needsUpdate() {
+    if (!file_exists($this->getAbsFileName())) {
+      return TRUE;
+    }
+    return $this->getChecksum() !== self::extractChecksum($this->getAbsFileName(), ';\(GenCodeChecksum:([a-z0-9]+)\);');
+  }
 
-    if (empty($this->tables[$name]['base'])) {
-      echo "No base defined for $name, skipping output generation\n";
+  public function run() {
+    echo "Generating {$this->name} as " . $this->getRelFileName() . "\n";
+
+    if (empty($this->tables[$this->name]['base'])) {
+      echo "No base defined for {$this->name}, skipping output generation\n";
       return;
     }
 
     $template = new CRM_Core_CodeGen_Util_Template('php');
-    $template->assign('table', $this->tables[$name]);
+    $template->assign('table', $this->tables[$this->name]);
+    $template->assign('genCodeChecksum', $this->getChecksum());
+    $template->run('dao.tpl', $this->getAbsFileName());
+  }
 
-    $directory = $this->config->phpCodePath . $this->tables[$name]['base'];
+  public function getRelFileName() {
+    return $this->tables[$this->name]['fileName'];
+  }
+
+  /**
+   * @return string
+   */
+  public function getAbsFileName() {
+    $directory = $this->config->phpCodePath . $this->tables[$this->name]['base'];
     CRM_Core_CodeGen_Util_File::createDir($directory);
+    $absFileName = $directory . $this->getRelFileName();
+    return $absFileName;
+  }
 
-    $template->run('dao.tpl', $directory . $this->tables[$name]['fileName']);
+  protected static function extractChecksum($file, $regex) {
+    $content = file_get_contents($file);
+    if (preg_match($regex, $content, $matches)) {
+      return $matches[1];
+    }
+    else {
+      return NULL;
+    }
+  }
+
+  protected function getChecksum() {
+    if (!$this->checksum) {
+      CRM_Utils_Array::flatten($this->tables[$this->name], $flat);
+      ksort($flat);
+      $this->checksum = md5($this->config->getSourceDigest() . json_encode($flat));
+    }
+    return $this->checksum;
   }
 
 }
