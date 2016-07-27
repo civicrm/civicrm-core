37254324f511503924b16bed9bb563fecfacdfe7
Tim Otten <totten@civicrm.org>
2016-07-26 22:29:41 -0700

    CRM-14885 - CRM_Core_CodeGen_DAO - Separate instances for each DAO file

--- CRM/Core/CodeGen/DAO.php
+++ CRM/Core/CodeGen/DAO.php
@@ -4,26 +4,33 @@
  * Create DAO ORM classes.
  */
 class CRM_Core_CodeGen_DAO extends CRM_Core_CodeGen_BaseTask {
+
+  /**
+   * @var string
+   */
+  public $name;
+
+  public function __construct($config, $name) {
+    parent::__construct($config);
+    $this->name = $name;
+  }
+
   public function run() {
-    $this->generateDAOs();
-  }
+    $name = $this->name;
+    echo "Generating $name as " . $this->tables[$name]['fileName'] . "\n";
 
-  public function generateDAOs() {
-    foreach (array_keys($this->tables) as $name) {
-      echo "Generating $name as " . $this->tables[$name]['fileName'] . "\n";
-
-      if (empty($this->tables[$name]['base'])) {
-        echo "No base defined for $name, skipping output generation\n";
-        continue;
-      }
-
-      $template = new CRM_Core_CodeGen_Util_Template('php');
-      $template->assign('table', $this->tables[$name]);
-
-      $directory = $this->config->phpCodePath . $this->tables[$name]['base'];
-      CRM_Core_CodeGen_Util_File::createDir($directory);
-
-      $template->run('dao.tpl', $directory . $this->tables[$name]['fileName']);
+    if (empty($this->tables[$name]['base'])) {
+      echo "No base defined for $name, skipping output generation\n";
+      return;
     }
+
+    $template = new CRM_Core_CodeGen_Util_Template('php');
+    $template->assign('table', $this->tables[$name]);
+
+    $directory = $this->config->phpCodePath . $this->tables[$name]['base'];
+    CRM_Core_CodeGen_Util_File::createDir($directory);
+
+    $template->run('dao.tpl', $directory . $this->tables[$name]['fileName']);
   }
+
 }
