25c18fb993b8474d7828a912ec4cb4986db3a0f6
Seamus Lee <seamuslee001@gmail.com>
2016-07-31 04:53:21 +0000

    Upgrade ckeditor

--- packages/ckeditor/config.js
+++ packages/ckeditor/config.js
@@ -1,11 +1,11 @@
 /**
- * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.html or http://ckeditor.com/license
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here.
-	// For the complete reference:
+	// For complete reference see:
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
 	// The toolbar groups arrangement, optimized for two toolbar rows.
@@ -20,20 +20,23 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'others' },
 		'/',
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ] },
+		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
 		{ name: 'styles' },
 		{ name: 'colors' },
 		{ name: 'about' }
 	];
-
 	// Add some extra plugins
 	config.extraPlugins = 'justify,showborders,pastefromword,pastetext,htmlwriter';
 
-	// Remove some buttons, provided by the standard plugins, which we don't
-	// need to have in the Standard(s) toolbar.
+	// Remove some buttons provided by the standard plugins, which are
+	// not needed in the Standard(s) toolbar.
 	config.removeButtons = 'Underline,Subscript,Superscript';
 
-	// Force the use of a simple ampersand so CiviCRM links don't break
-	config.forceSimpleAmpersand = true;
-	config.basicEntities = false;
+	// Set the most common block elements.
+	// config.format_tags = 'p;h1;h2;h3;pre';
+
+	// Simplify the dialog windows.
+	config.removeDialogTabs = 'image:advanced;link:advanced';
+        config.basicEntities = false;
+        config.allowedContent = true;
 };
