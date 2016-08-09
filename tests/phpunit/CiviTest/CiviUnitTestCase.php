b7cdbeb802268b232fd98cd8769f2abf37d21cd6
Pradeep Nayak <pradpnayak@gmail.com>
2016-08-10 02:39:05 +0530

    --CRM-16189, added test

--- tests/phpunit/CiviTest/CiviUnitTestCase.php
+++ tests/phpunit/CiviTest/CiviUnitTestCase.php
@@ -3397,8 +3397,8 @@ AND    ( TABLE_NAME LIKE 'civicrm_value_%' )
    * @return array
    */
   protected function createPriceSet($component = 'contribution_page', $componentId = NULL) {
-    $paramsSet['title'] = 'Price Set';
-    $paramsSet['name'] = CRM_Utils_String::titleToVar('Price Set');
+    $paramsSet['title'] = 'Price Set' . substr(sha1(rand()), 0, 7);
+    $paramsSet['name'] = CRM_Utils_String::titleToVar($paramsSet['title']);
     $paramsSet['is_active'] = TRUE;
     $paramsSet['financial_type_id'] = 4;
     $paramsSet['extends'] = 1;
