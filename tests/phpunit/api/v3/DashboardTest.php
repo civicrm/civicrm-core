5abd071a046208d1750fcf32ebd6f33255ca279b
Seamus Lee <seamuslee001@gmail.com>
2016-08-11 14:58:57 +1000

    remove version param as unneded as callAPISucess doesn't need it

--- tests/phpunit/api/v3/DashboardTest.php
+++ tests/phpunit/api/v3/DashboardTest.php
@@ -51,7 +51,6 @@ class api_v3_DashboardTest extends CiviUnitTestCase {
   public function testDashboardCreate() {
     $oldCount = CRM_Core_DAO::singleValueQuery('select count(*) from civicrm_dashboard');
     $params = array(
-      'version' => 3,
       'label' => 'New Dashlet element',
       'name' => 'New Dashlet element',
       'url' => 'civicrm/report/list&reset=1&compid=99&snippet=5',
@@ -68,11 +67,11 @@ class api_v3_DashboardTest extends CiviUnitTestCase {
 
   /**
    * CRM-19217.
+   *
    * Ensure that where is_active is specifically set to 0 is_active returns 0.
    */
   public function testDashboardCreateNotActive() {
     $params = array(
-      'version' => 3,
       'label' => 'New Dashlet element',
       'name' => 'New Dashlet element',
       'url' => 'civicrm/report/list&reset=1&compid=99&snippet=5',
@@ -89,7 +88,6 @@ class api_v3_DashboardTest extends CiviUnitTestCase {
    */
   public function DashboardDelete($id, $oldCount) {
     $params = array(
-      'version' => 3,
       'id' => $id,
     );
     $dashboardget = $this->callAPISuccess('dashboard', 'get', $params);
