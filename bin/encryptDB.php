9c01d2c5e5ee6c0e44f8b4549e8cc4c323516ac6
Tim Otten <totten@civicrm.org>
2016-08-11 16:51:03 -0700

    CRM-19223 - bin/encryptDB.php - Require permission "administer CiviCRM"

--- bin/encryptDB.php
+++ bin/encryptDB.php
@@ -93,6 +93,9 @@ function run() {
 
   // this does not return on failure
   CRM_Utils_System::authenticateScript(TRUE);
+  if (!CRM_Core_Permission::check('administer CiviCRM')) {
+    CRM_Utils_System::authenticateAbort("User does not have required permission (administer CiviCRM).\n", TRUE);
+  }
 
   encryptDB();
 }
