f15ed25a9c60a6c154b72f65b09d7b0041e049c1
Tim Otten <totten@civicrm.org>
2016-08-16 18:57:07 -0700

    migrate/(export,import)JSON.php - Syntax cleanup

--- bin/migrate/importJSON.php
+++ bin/migrate/importJSON.php
@@ -38,7 +38,7 @@ function run() {
   $config = CRM_Core_Config::singleton();
 
   // this does not return on failure
-  CRM_Utils_System::authenticateScript( true );
+  CRM_Utils_System::authenticateScript(TRUE);
   if (!CRM_Core_Permission::check('administer CiviCRM')) {
     CRM_Utils_System::authenticateAbort("User does not have required permission (administer CiviCRM).\n", TRUE);
   }
