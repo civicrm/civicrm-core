4e36b44772e496cd6f62aecee1da1d80e104f61b
Pradeep Nayak <pradpnayak@gmail.com>
2016-08-21 14:20:11 +0530

    CRM-16189, minor change to code
    ----------------------------------------
    * CRM-16189: Improve support for Accrual Method bookkeeping
      https://issues.civicrm.org/jira/browse/CRM-16189

--- CRM/Core/BAO/FinancialTrxn.php
+++ CRM/Core/BAO/FinancialTrxn.php
@@ -720,7 +720,7 @@ WHERE pp.participant_id = {$entityId} AND ft.to_financial_account_id != {$toFina
     $closingDate = 'now()';
     if (!$onlyFromClause && Civi::settings()->get('closing_date')) {
       $closingDate = Civi::settings()->get('closing_date');
-      $closingDate = $closingDate['M'] . '/' . $closingDate['d'] . '/' . date('Y');
+      $closingDate = implode('/', $closingDate);
       $closingDate = "'" . date('Y-m-d', strtotime($closingDate)) . "'";
     }
     $priorDate = CRM_Contribute_BAO_Contribution::checkContributeSettings('prior_financial_period');
