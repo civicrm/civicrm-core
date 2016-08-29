c5dae75e7712aa842cebed89b11070dc0ed0ac11
Seamus Lee <seamuslee001@gmail.com>
2016-08-29 17:29:12 +1000

    Fix escaping

--- CRM/Utils/File.php
+++ CRM/Utils/File.php
@@ -305,7 +305,7 @@ class CRM_Utils_File {
     }
     $db->query('SET NAMES utf8');
     $transactionId = CRM_Utils_Type::escape(CRM_Utils_Request::id(), 'String');
-    $db->query('SET @uniqueID = ' . $transactionId);
+    $db->query('SET @uniqueID = ' . "'$transactionId'");
 
     if (!$isQueryString) {
       $string = $prefix . file_get_contents($fileName);
