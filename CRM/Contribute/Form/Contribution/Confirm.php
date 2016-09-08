bb54e6256191a2a9faf37cc056dd9cb5bfeecaf1
Edsel <edsel.lopez@jmaconsulting.biz>
2016-09-08 12:30:17 +0530

    CRM-19153 Fixed unit test
    ----------------------------------------
    * CRM-19153: Future pledge start date causes improper future pledge payment dates
      https://issues.civicrm.org/jira/browse/CRM-19153

--- CRM/Contribute/Form/Contribution/Confirm.php
+++ CRM/Contribute/Form/Contribution/Confirm.php
@@ -1875,6 +1875,7 @@ class CRM_Contribute_Form_Contribution_Confirm extends CRM_Contribute_Form_Contr
     $form->_fields['billing_last_name'] = 1;
     // CRM-18854 - Set form values to allow pledge to be created for api test.
     if (CRM_Utils_Array::value('pledge_block_id', $params)) {
+      $form->_values['pledge_id'] = CRM_Utils_Array::value('pledge_id', $params, NULL);
       $form->_values['pledge_block_id'] = $params['pledge_block_id'];
       $pledgeBlock = CRM_Pledge_BAO_PledgeBlock::getPledgeBlock($params['id']);
       $form->_values['max_reminders'] = $pledgeBlock['max_reminders'];
