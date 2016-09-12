7a1c2d6c4bcc036cc908c072e020f4b23ff0b3e1
Effy Elden <git@effy.is>
2016-09-12 05:13:34 -0400

    CRM-19346 - Enforce unique gender ids
    ----------------------------------------
    * CRM-19346:
      https://issues.civicrm.org/jira/browse/CRM-19346

--- CRM/Admin/Form/Options.php
+++ CRM/Admin/Form/Options.php
@@ -184,6 +184,16 @@ class CRM_Admin_Form_Options extends CRM_Admin_Form {
       );
     }
 
+    // Enforce unique gender ids - CRM-19346
+    if ($this->_gName == 'gender')
+    {
+      $this->addRule('value',
+        ts('This Value already exists in the database for this option group. Please select a different Value.'),
+        'optionExists',
+        array('CRM_Core_DAO_OptionValue', $this->_id, $this->_gid, 'value')
+      );
+    }
+
     if ($this->_gName == 'case_status') {
       $classes = array(
         'Opened' => ts('Opened'),
