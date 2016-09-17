52d283890554b872a650cc45ebe7a579288ada02
Seamus Lee <seamuslee001@gmail.com>
2016-09-18 09:58:32 +1000

    CRM-19380 allow for multiple from email addresses but only one per domain

--- CRM/Core/OptionValue.php
+++ CRM/Core/OptionValue.php
@@ -258,14 +258,19 @@ class CRM_Core_OptionValue {
    * @param int $optionGroupID
    * @param string $fieldName
    *   The name of the field in the DAO.
+   * @param bool $domainSpecific
    *
    * @return bool
    *   true if object exists
    */
-  public static function optionExists($value, $daoName, $daoID, $optionGroupID, $fieldName = 'name') {
+  public static function optionExists($value, $daoName, $daoID, $optionGroupID, $fieldName = 'name', $domainSpecific) {
     $object = new $daoName();
     $object->$fieldName = $value;
     $object->option_group_id = $optionGroupID;
+
+    if ($domainSpecific) {
+      $object->domain_id = CRM_Core_Config::domainID();
+    }
 
     if ($object->find(TRUE)) {
       return ($daoID && $object->id == $daoID) ? TRUE : FALSE;
