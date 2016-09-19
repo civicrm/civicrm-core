c1622e588e93d9dee981276c8819b90fc2845048
jitendrapurohit <jitendra.purohit@webaccessglobal.com>
2016-09-19 18:36:11 +0530

    CRM-19374 - Advanced Search fails on Contribution Receive date filter

--- CRM/Contact/BAO/Query.php
+++ CRM/Contact/BAO/Query.php
@@ -1538,7 +1538,10 @@ class CRM_Contact_BAO_Query {
 
     self::filterCountryFromValuesIfStateExists($formValues);
 
-    foreach ($formValues as $id => $values) {
+    foreach ($formValues as $id => &$val) {
+      // CRM-19374 - we don't want to change $val in $formValues.
+      // Assign it to a temp variable which operates while iteration.
+      $values = $val;
 
       if (self::isAlreadyProcessedForQueryFormat($values)) {
         $params[] = $values;
