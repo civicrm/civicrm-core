4674f8240c6670336713958c86e33b0a6c36969e
jitendrapurohit <jitendra.purohit@webaccessglobal.com>
2016-09-20 17:16:26 +0530

    CRM-16189 - Notice Fix #8848

--- CRM/Contribute/BAO/Contribution.php
+++ CRM/Contribute/BAO/Contribution.php
@@ -188,7 +188,7 @@ class CRM_Contribute_BAO_Contribution extends CRM_Contribute_DAO_Contribution {
 
     // CRM-16189
     CRM_Financial_BAO_FinancialAccount::checkFinancialTypeHasDeferred($params, $contributionID);
-    if ($contributionID && !empty($params['revenue_recognition_date'])
+    if ($contributionID && !empty($params['revenue_recognition_date']) && !empty($params['prevContribution'])
       && !($contributionStatus[$params['prevContribution']->contribution_status_id] == 'Pending')
       && !self::allowUpdateRevenueRecognitionDate($contributionID)
     ) {
