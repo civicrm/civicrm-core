b11d246d267f119f71582f5af0431ec36dcd0367
Seamus Lee <seamuslee001@gmail.com>
2016-09-22 00:31:37 +0000

    Update to CiviCRM PR version

--- packages/ckeditor/config.js
+++ packages/ckeditor/config.js
@@ -37,6 +37,6 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
-        config.basicEntities = false;
-        config.allowedContent = true;
+	config.basicEntities = false;
+	config.allowedContent = true;
 };
---
+++ packages/ckeditor/plugins/basicstyles/lang/sl.js
@@ -0,0 +1,12 @@
+﻿/*
+Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang( 'basicstyles', 'sl', {
+	bold: 'Krepko',
+	italic: 'Ležeče',
+	strike: 'Prečrtano',
+	subscript: 'Podpisano',
+	superscript: 'Nadpisano',
+	underline: 'Podčrtano'
+} );
