f7e08ee5ceae4a4c602e91fd5be65fe3208f32ff
jitendrapurohit <jitendra.purohit@webaccessglobal.com>
2016-09-22 13:16:03 +0530

    revert #8937

--- CRM/Core/BAO/CustomField.php
+++ CRM/Core/BAO/CustomField.php
@@ -822,7 +822,6 @@ class CRM_Core_BAO_CustomField extends CRM_Core_DAO_CustomField {
       'AdvMulti-Select',
       'CheckBox',
       'Radio',
-      'Autocomplete-Select',
     )));
 
     if ($isSelect) {
