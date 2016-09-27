ba1e82e7f842bb5472fa4c3ecda7007d2728afd0
Pradeep Nayak <pradpnayak@gmail.com>
2016-09-27 16:50:11 +0530

    CRM-19234, minor change
    ----------------------------------------
    * CRM-19234: Update Constituent (Detail) report to respect Financial ACL
      https://issues.civicrm.org/jira/browse/CRM-19234

--- CRM/Report/Form.php
+++ CRM/Report/Form.php
@@ -4689,12 +4689,12 @@ LEFT JOIN civicrm_contact {$field['alias']} ON {$field['alias']}.id = {$this->_a
       return FALSE;
     }
     $financialTypes = NULL;
-    CRM_Financial_BAO_FinancialType::getAvailableFinancialTypes($financialTypes);
+    CRM_Financial_BAO_FinancialType::getAvailableFinancialTypes($this->financialTypes);
     $this->financialTypes = $financialTypes;
     if (strpos($query->_from, 'civicrm_contribution') === FALSE) {
       return FALSE;
     }
-    if (empty($financialTypes)) {
+    if (empty($this->financialTypes)) {
       $contFTs = "0";
       $liFTs = implode(',', array_keys(CRM_Contribute_Pseudoconstant::financialType()));
     }
