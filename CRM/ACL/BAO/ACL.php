a0d5c8468a77d995345e4013babebdb1aa4f20bd
deb.monish <monish.deb@webaccessglobal.com>
2016-09-27 18:37:06 +0530

    Revert CRM-18159 fix [4.7.8 REGRESSION]

--- CRM/ACL/BAO/ACL.php
+++ CRM/ACL/BAO/ACL.php
@@ -723,54 +723,14 @@ SELECT count( a.id )
   }
 
   /**
-   * Build a join and where part for a query
-   *
-   * @param int $contactId
-   * @return array - the first key is join part of the query and the second key is the where part of the query
-   */
-  public static function buildAcl($contactId) {
-    // If there is no $contactId passed return empty ACL join and where clause
-    if (empty($contactId)) {
-      return array('', '');
-    }
-
-    $tables = array();
-    $whereTables = array();
-    $whereClause = CRM_ACL_BAO_ACL::whereClause(CRM_Core_Permission::VIEW, $tables, $whereTables, $contactId, TRUE);
-    if (strlen($whereClause)) {
-      $whereClause = " AND (" . $whereClause . ")";
-    }
-
-    $join = "";
-    foreach ($whereTables as $name => $value) {
-      if (!$value) {
-        continue;
-      }
-      if ($value != 1) {
-        // if there is already a join statement in value, use value itself
-        if (strpos($value, 'JOIN')) {
-          $join .= " $value ";
-        }
-        continue;
-      }
-    }
-
-    return array(
-      $join,
-      $whereClause,
-    );
-  }
-
-  /**
    * @param $type
    * @param $tables
    * @param $whereTables
    * @param int $contactID
-   * @param bool $strictReturn If there is no where clause build for ACL
    *
    * @return null|string
    */
-  public static function whereClause($type, &$tables, &$whereTables, $contactID = NULL, $strictReturn = FALSE) {
+  public static function whereClause($type, &$tables, &$whereTables, $contactID = NULL) {
     $acls = CRM_ACL_BAO_Cache::build($contactID);
 
     $whereClause = NULL;
@@ -880,7 +840,7 @@ SELECT g.*
     // call the hook to get additional whereClauses
     CRM_Utils_Hook::aclWhereClause($type, $tables, $whereTables, $contactID, $whereClause);
 
-    if (empty($whereClause) && !$strictReturn) {
+    if (empty($whereClause)) {
       $whereClause = ' ( 0 ) ';
     }
 
