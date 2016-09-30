075ead9d50f13a8a261858ebbb4de445bad19038
jitendrapurohit <jitendra.purohit@webaccessglobal.com>
2016-09-30 18:58:04 +0530

    include unit test

--- CRM/Price/BAO/PriceSet.php
+++ CRM/Price/BAO/PriceSet.php
@@ -1909,8 +1909,27 @@ WHERE       ps.id = %1
       // old code: self::setDefaultPriceSetField($priceFieldName, $optionId, $priceField['html_type'], $defaults);
       self::setDefaultPriceSetField($priceFieldName, $selectOptions[1], $selectOptions[2], $defaults);
     }
-
     return $defaults;
+  }
+
+  /**
+   * Get non-deductible amount from price options
+   *
+   * @param int $priceSetId
+   * @param array $lineItem
+   *
+   * @return int
+   *   calculated non-deductible amount.
+   */
+  public static function getNonDeductibleAmountFromPriceSet($priceSetId, $lineItem) {
+    $nonDeductibleAmount = 0;
+    if (!empty($lineItem[$priceSetId])) {
+      foreach ($lineItem[$priceSetId] as $fieldId => $options) {
+        $nonDeductibleAmount += $options['non_deductible_amount'] * $options['qty'];
+      }
+    }
+
+    return $nonDeductibleAmount;
   }
 
 }
