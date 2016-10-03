79564bd412ebb145ca1ca4bdc05f2d782441f8b9
Coleman Watts <coleman@civicrm.org>
2016-10-03 15:47:53 -0400

    CRM-19441 - Fix display and saving of case tags

--- CRM/Case/Page/AJAX.php
+++ CRM/Case/Page/AJAX.php
@@ -55,24 +55,22 @@ class CRM_Case_Page_AJAX {
       $tagIds = explode(',', $tags);
     }
 
-    if (!empty($tagIds)) {
-      $params = array(
-        'entity_id' => $caseId,
-        'entity_table' => 'civicrm_case',
-      );
+    $params = array(
+      'entity_id' => $caseId,
+      'entity_table' => 'civicrm_case',
+    );
 
-      CRM_Core_BAO_EntityTag::del($params);
+    CRM_Core_BAO_EntityTag::del($params);
 
-      foreach ($tagIds as $tagid) {
-        if (is_numeric($tagid)) {
-          $params['tag_id'] = $tagid;
-          CRM_Core_BAO_EntityTag::add($params);
-        }
+    foreach ($tagIds as $tagid) {
+      if (is_numeric($tagid)) {
+        $params['tag_id'] = $tagid;
+        CRM_Core_BAO_EntityTag::add($params);
       }
     }
 
     if (!empty($tagList)) {
-      CRM_Core_Form_Tag::postProcess($tagList, $caseId, 'civicrm_case', CRM_Core_DAO::$_nullObject);
+      CRM_Core_Form_Tag::postProcess($tagList, $caseId, 'civicrm_case');
     }
 
     $session = CRM_Core_Session::singleton();
