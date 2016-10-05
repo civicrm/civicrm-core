3b4858618ae92da0c57e5ffaf2d2fc00e9dc2b53
MiyaNoctem <camilo.rodriguez@gfecolombia.com.co>
2016-10-05 14:22:33 -0500

    Update Event.php

--- CRM/Event/BAO/Event.php
+++ CRM/Event/BAO/Event.php
@@ -96,6 +96,10 @@ class CRM_Event_BAO_Event extends CRM_Event_DAO_Event {
     }
 
     // CRM-16189
+    if (!empty($params['financial_type_id'])) {
+      CRM_Financial_BAO_FinancialAccount::validateFinancialType($params['financial_type_id']);
+    }
+   
     $isError = CRM_Financial_BAO_FinancialAccount::validateFinancialType(
       CRM_Utils_Array::value('financial_type_id', $params)
     );
