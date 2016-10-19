df0996131f2b511cc2c2943a35b6f68058d40e57
Jamie McClelland <jm@mayfirst.org>
2016-10-19 10:31:19 -0400

    removed extraneous lines and switched to static call.

--- CRM/Core/BAO/Dashboard.php
+++ CRM/Core/BAO/Dashboard.php
@@ -439,9 +439,7 @@ class CRM_Core_BAO_Dashboard extends CRM_Core_DAO_Dashboard {
     }
     else {
       //Get the id of Logged in User
-      $session = CRM_Core_Session::singleton();
-      $contactID = CRM_Core_Session::singleton()->getLoggedInContactID();
-      $contactID = $session->get('userID');
+      $contactID = CRM_Core_Session::getLoggedInContactID();
       if (!empty($contactID)) {
         $contactIDs[$contactID] = NULL;
       }
