8e6af65c94d4661038ee6120df04083dccfbde7c
Jamie McClelland <jm@mayfirst.org>
2016-11-16 15:17:12 -0500

    CRM-19657 - try to use systemwide default from address for recurring receipts.

--- CRM/Contribute/BAO/Contribution.php
+++ CRM/Contribute/BAO/Contribution.php
@@ -4720,14 +4720,30 @@ LIMIT 1;";
       throw new CRM_Core_Exception('Contribution does not exist');
     }
     $contribution->loadRelatedObjects($input, $ids, TRUE);
-    // set receipt from e-mail and name in value
     if (!$returnMessageText) {
-      $userID = CRM_Core_Session::singleton()->getLoggedInContactID();
-      if (!empty($userID)) {
-        list($userName, $userEmail) = CRM_Contact_BAO_Contact_Location::getEmailDetails($userID);
-        $values['receipt_from_email'] = CRM_Utils_Array::value('receipt_from_email', $input, $userEmail);
-        $values['receipt_from_name'] = CRM_Utils_Array::value('receipt_from_name', $input, $userName);
+      // Set receipt from e-mail and name in value if we are sending the message.
+      $userEmail = NULL;
+      $userName = NULL;
+
+      // First, try to retrieve default name and email address.
+      $sitewide_default_from = array_pop(CRM_Core_OptionGroup::values('from_email_address', NULL, NULL, NULL, ' AND is_default = 1'));
+      // Parse the results (which is in "Name" <email> format) into a name and an email.
+      if (!empty($sitewide_default_from)) {
+        if (preg_match('/"([^"]*)"\s*<([^>]*)>/', $sitewide_default_from, $matches)) {
+          $userName = $matches[1];
+          $userEmail = $matches[2];
+        }
       }
+
+      if (empty($userEmail)) {
+        // If that didn't work, try to use the logged in user.
+        $userID = CRM_Core_Session::singleton()->getLoggedInContactID();
+        if (!empty($userID)) {
+          list($userName, $userEmail) = CRM_Contact_BAO_Contact_Location::getEmailDetails($userID);
+        }
+      }
+      $values['receipt_from_email'] = CRM_Utils_Array::value('receipt_from_email', $input, $userEmail);
+      $values['receipt_from_name'] = CRM_Utils_Array::value('receipt_from_name', $input, $userName);
     }
 
     $return = $contribution->composeMessageArray($input, $ids, $values, $recur, $returnMessageText);
