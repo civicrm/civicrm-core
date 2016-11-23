29d5b3c83dacca5274ded16f2567981cea3c79d6
Seamus Lee <seamuslee001@gmail.com>
2016-11-23 22:52:06 +0000

    Update to fixed fuzion tar ball

--- civicrm-version.php
+++ civicrm-version.php
@@ -1,6 +1,6 @@
 <?php
 function civicrmVersion( ) {
-  return array( 'version'  => '4.6.24',
+  return array( 'version'  => '4.6.24.0',
                 'cms'      => 'Drupal',
                 'revision' => '' );
 }
