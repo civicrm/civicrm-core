c5ebc522df9e2ca54f159719b6da418c8cdff485
Tim Otten <totten@civicrm.org>
2016-12-19 15:04:37 -0800

    CRM-19690 - crmMailing - Pick editor layout using template_type
    This PR allows one to display a different CiviMail composition UI depending on the `template_type`. Key details:
    
     1. When creating a mailing, use the path `civicrm/a/#/mailing/new` to create a mailing with the default `template_type` (aka first-preferred, by weight).
     2. When creating a mailing, use the path `civicrm/a/#/mailing/new/{template_type}` to create a mailing with a specific `template_type`.
     3. When editing a mailing, it checks the `template_type` and loads the appropriate editor.
     4. Some of the boilerplate from `2step.html`, `unified.html`, etal has been moved to `base.html`.
    
    Note that this breaks some hidden functionality -- before, you could switch
    among a few different layouts (`2step`, `unified`, `unified2`, `wizard`) by
    manually editing the URL (e.g.  `civicrm/a/#/mailing/2/unified`).  Now, to
    change the layout of the traditional-style mailings, you can implement a
    hook, e.g.
    
    ```
    function mymod_civicrm_mailingTemplateTypes(&$types) {
      foreach ($types as &$typeDef) {
        if ($typeDef['name'] === 'traditional') {
          $typeDef['editorUrl'] = '~/crmMailing/EditMailingCtrl/unified.html';
        }
      }
    }
    ```
    
    Relatedly, if a downstream system has found some other way to patch/hack a
    replacement for the CiviMail layout, then they'll need to update it.
    (Specifically, port the changes on display in `2step.html` or any of the
    other layouts.).  However, this has always been a hidden trick with
    unrealistic UX -- and we've refrained from documenting/institutionalizing
    it...  until now.  With this PR, overloading CiviMail UI becomes a supported
    feature (by way of the public-facing `hook_civicrm_mailingTypes`).

--- ang/crmMailing.js
+++ ang/crmMailing.js
@@ -12,41 +12,50 @@
         controller: 'ListMailingsCtrl'
       });
 
-      var editorPaths = {
-        '': '~/crmMailing/EditMailingCtrl/2step.html',
-        '/unified': '~/crmMailing/EditMailingCtrl/unified.html',
-        '/unified2': '~/crmMailing/EditMailingCtrl/unified2.html',
-        '/wizard': '~/crmMailing/EditMailingCtrl/wizard.html'
-      };
-      angular.forEach(editorPaths, function(editTemplate, pathSuffix) {
-        if (CRM && CRM.crmMailing && CRM.crmMailing.workflowEnabled) {
-            editTemplate = '~/crmMailing/EditMailingCtrl/workflow.html'; // override
+      if (!CRM || !CRM.crmMailing) {
+        return;
+      }
+
+      $routeProvider.when('/mailing/new', {
+        template: '<p>' + ts('Initializing...') + '</p>',
+        controller: 'CreateMailingCtrl',
+        resolve: {
+          selectedMail: function(crmMailingMgr) {
+            var m = crmMailingMgr.create({
+              template_type: CRM.crmMailing.templateTypes[0].name
+            });
+            return crmMailingMgr.save(m);
+          }
         }
-        $routeProvider.when('/mailing/new' + pathSuffix, {
-          template: '<p>' + ts('Initializing...') + '</p>',
-          controller: 'CreateMailingCtrl',
-          resolve: {
-            selectedMail: function(crmMailingMgr) {
-              var m = crmMailingMgr.create();
-              return crmMailingMgr.save(m);
-            }
+      });
+
+      $routeProvider.when('/mailing/new/:templateType', {
+        template: '<p>' + ts('Initializing...') + '</p>',
+        controller: 'CreateMailingCtrl',
+        resolve: {
+          selectedMail: function($route, crmMailingMgr) {
+            var m = crmMailingMgr.create({
+              template_type: $route.current.params.templateType
+            });
+            return crmMailingMgr.save(m);
           }
-        });
-        $routeProvider.when('/mailing/:id' + pathSuffix, {
-          templateUrl: editTemplate,
-          controller: 'EditMailingCtrl',
-          resolve: {
-            selectedMail: function($route, crmMailingMgr) {
-              return crmMailingMgr.get($route.current.params.id);
-            },
-            attachments: function($route, CrmAttachments) {
-              var attachments = new CrmAttachments(function () {
-                return {entity_table: 'civicrm_mailing', entity_id: $route.current.params.id};
-              });
-              return attachments.load();
-            }
+        }
+      });
+
+      $routeProvider.when('/mailing/:id', {
+        templateUrl: '~/crmMailing/EditMailingCtrl/base.html',
+        controller: 'EditMailingCtrl',
+        resolve: {
+          selectedMail: function($route, crmMailingMgr) {
+            return crmMailingMgr.get($route.current.params.id);
+          },
+          attachments: function($route, CrmAttachments) {
+            var attachments = new CrmAttachments(function () {
+              return {entity_table: 'civicrm_mailing', entity_id: $route.current.params.id};
+            });
+            return attachments.load();
           }
-        });
+        }
       });
     }
   ]);
