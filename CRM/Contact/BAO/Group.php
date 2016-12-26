fb413e82fa814c7d6ab5f73b2802b8f7158758ef
jitendrapurohit <jitendra.purohit@webaccessglobal.com>
2016-12-26 11:42:28 +0530

    Fix group_type param in creating group

--- CRM/Contact/BAO/Group.php
+++ CRM/Contact/BAO/Group.php
@@ -360,7 +360,7 @@ class CRM_Contact_BAO_Group extends CRM_Contact_DAO_Group {
     if (isset($params['group_type'])) {
       if (is_array($params['group_type'])) {
         $params['group_type'] = CRM_Core_DAO::VALUE_SEPARATOR . implode(CRM_Core_DAO::VALUE_SEPARATOR,
-            $params['group_type']
+            CRM_Utils_Array::convertCheckboxFormatToArray($params['group_type'])
           ) . CRM_Core_DAO::VALUE_SEPARATOR;
       }
       else {
