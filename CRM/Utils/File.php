770c22e296a760b94762e83bc99e1f056152fd0e
Coleman Watts <coleman@civicrm.org>
2017-01-23 10:56:46 -0500

    CRM-19829 - Attachment API: return icons

--- CRM/Utils/File.php
+++ CRM/Utils/File.php
@@ -887,4 +887,45 @@ HTACCESS;
     return self::getFileURL($path, $mimeType);
   }
 
+
+  /**
+   * Get file icon class for specific MIME Type
+   *
+   * @param string $mimeType
+   * @return string
+   */
+  public static function getIconFromMimeType($mimeType) {
+    // List of official MIME Types: http://www.iana.org/assignments/media-types/media-types.xhtml
+    $iconClasses = array(
+      // Media
+      'image' => 'fa-file-image-o',
+      'audio' => 'fa-file-audio-o',
+      'video' => 'fa-file-video-o',
+      // Documents
+      'application/pdf' => 'fa-file-pdf-o',
+      'application/msword' => 'fa-file-word-o',
+      'application/vnd.ms-word' => 'fa-file-word-o',
+      'application/vnd.oasis.opendocument.text' => 'fa-file-word-o',
+      'application/vnd.openxmlformats-officedocument.wordprocessingml' => 'fa-file-word-o',
+      'application/vnd.ms-excel' => 'fa-file-excel-o',
+      'application/vnd.openxmlformats-officedocument.spreadsheetml' => 'fa-file-excel-o',
+      'application/vnd.oasis.opendocument.spreadsheet' => 'fa-file-excel-o',
+      'application/vnd.ms-powerpoint' => 'fa-file-powerpoint-o',
+      'application/vnd.openxmlformats-officedocument.presentationml' => 'fa-file-powerpoint-o',
+      'application/vnd.oasis.opendocument.presentation' => 'fa-file-powerpoint-o',
+      'text/plain' => 'fa-file-text-o',
+      'text/html' => 'fa-file-code-o',
+      'application/json' => 'fa-file-code-o',
+      // Archives
+      'application/gzip' => 'fa-file-archive-o',
+      'application/zip' => 'fa-file-archive-o',
+    );
+    foreach ($iconClasses as $text => $icon) {
+      if (strpos($mimeType, $text) === 0) {
+        return $icon;
+      }
+    }
+    return 'fa-file-o';
+  }
+
 }
