ea4c834befcef3a43c0f9c7bc0240e2a84bcff68
Pradeep Nayak <pradpnayak@gmail.com>
2017-01-23 18:43:37 +0530

    CRM-19585, added test
    ----------------------------------------
    * CRM-19585: Sales tax issue
      https://issues.civicrm.org/jira/browse/CRM-19585

--- tests/phpunit/CRM/Contribute/BAO/ContributionTest.php
+++ tests/phpunit/CRM/Contribute/BAO/ContributionTest.php
@@ -1228,4 +1228,26 @@ WHERE eft.entity_id = %1 AND ft.to_financial_account_id <> %2";
     return array($contribution, $financialAccount);
   }
 
+  /**
+   * test for function calculateTaxAfterChangeInFinancialTypeForLineItems()
+   */
+  public function testcalculateTaxAfterChangeInFinancialTypeForLineItems() {
+    list($contribution, $financialAccount) = $this->createContributionWithTax();
+    $lineItems = CRM_Price_BAO_LineItem::getLineItemsByContributionID($contribution['id']);
+    foreach ($lineItems as $id => $lineItem) {
+      $lineItems[$id]['line_total'] = 300;
+      $lineItems[$id]['tax_rate'] = 20;
+      $lineItems[$id]['id'] = $id;
+    }
+    $taxAmount = CRM_Contribute_BAO_Contribution::calculateTaxAfterChangeInFinancialTypeForLineItems(array($lineItems), $contribution['id']);
+    $this->assertEquals($taxAmount, 20.00, 'Amount does not match.');
+    foreach ($lineItems as $id => $lineItem) {
+      $lineItems[$id]['line_total'] = 300;
+      $lineItems[$id]['tax_rate'] = 0;
+      $lineItems[$id]['id'] = $id;
+    }
+    $taxAmount = CRM_Contribute_BAO_Contribution::calculateTaxAfterChangeInFinancialTypeForLineItems(array($lineItems), $contribution['id']);
+    $this->assertEquals($taxAmount, 0, 'Amount does not match.');
+  }
+
 }
