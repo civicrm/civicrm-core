308785d35bf060beb2eab30f9f63c061ec37f09b
Pradeep Nayak <pradpnayak@gmail.com>
2017-02-08 19:20:22 +0530

    CRM-19964, added validation to select atleast one price option when record contribution is checked
    ----------------------------------------
    * CRM-19964: Event registration creates contribution without line item
      https://issues.civicrm.org/jira/browse/CRM-19964

--- CRM/Event/Form/Participant.php
+++ CRM/Event/Form/Participant.php
@@ -820,6 +820,7 @@ class CRM_Event_Form_Participant extends CRM_Contribute_Form_AbstractEditPayment
       if (empty($values['payment_instrument_id'])) {
         $errorMsg['payment_instrument_id'] = ts('Payment Method is a required field.');
       }
+      CRM_Price_BAO_PriceField::priceSetValidation($values['priceSetId'], $values, $errorMsg);
     }
 
     // validate contribution status for 'Failed'.
