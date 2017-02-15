cdc5c4501ba1c6d6facbe4ef5d69de5600e7a344
eileen <emcnaughton@wikimedia.org>
2017-02-15 14:57:01 +1300

    CRM-20016 preliminary work - add basis for unit testing and failing unit test.
    In writing the test I identified the transation was not rolling back due
    to the api not following our crud format. I fixed it to 'fail' because
    we should not be passing despite mysql errors & ... we should fix the errors...
    
    The BAO code in fact holds transaction handling to ensure that a fail causes a fail - but
    we were bypassing it without passing in the 'is_transactional' param.

--- CRM/Mailing/Event/BAO/Bounce.php
+++ CRM/Mailing/Event/BAO/Bounce.php
@@ -46,8 +46,8 @@ class CRM_Mailing_Event_BAO_Bounce extends CRM_Mailing_Event_DAO_Bounce {
    *
    * @return bool|null
    */
-  public static function &create(&$params) {
-    $q = &CRM_Mailing_Event_BAO_Queue::verify($params['job_id'],
+  public static function create(&$params) {
+    $q = CRM_Mailing_Event_BAO_Queue::verify($params['job_id'],
       $params['event_queue_id'],
       $params['hash']
     );
---
+++ tests/phpunit/CRM/Utils/Mail/EmailProcessorTest.php
@@ -0,0 +1,107 @@
+<?php
+
+/**
+ * Class CRM_Utils_EmailProcessorTest
+ * @group headless
+ */
+
+class CRM_Utils_EmailProcessorTest extends CiviUnitTestCase {
+
+  /**
+   * Event queue record.
+   *
+   * @var array
+   */
+  protected $eventQueue = array();
+
+  /**
+   * ID of our sample contact.
+   *
+   * @var int
+   */
+  protected $contactID;
+
+  public function setUp() {
+    parent::setUp();
+    $this->callAPISuccess('MailSettings', 'get', array(
+      'api.MailSettings.create' => array(
+        'name' => 'local',
+        'protocol' => 'Localdir',
+        'source' => __DIR__ . '/data/mail',
+        'domain' => 'example.com',
+      ),
+    ));
+  }
+
+  /**
+   * Test the job processing function works and processes a bounce.
+   */
+  public function testBounceProcessing() {
+    $this->setUpMailing();
+
+    copy(__DIR__ . '/data/bounces/bounce_no_verp.txt', __DIR__ . '/data/mail/bounce_no_verp.txt');
+    $this->assertTrue(file_exists(__DIR__ . '/data/mail/bounce_no_verp.txt'));
+    $this->callAPISuccess('job', 'fetch_bounces', array());
+    $this->assertFalse(file_exists(__DIR__ . '/data/mail/bounce_no_verp.txt'));
+    $this->checkMailingBounces(1);
+  }
+
+  /**
+   * Test that a deleted email does not cause a hard fail.
+   *
+   * The civicrm_mailing_event_queue table tracks email ids to represent an
+   * email address. The id may not represent the same email by the time the bounce may
+   * come in - a weakness of storing the id not the email. Relevant here
+   * is that it might have been deleted altogether, in which case the bounce should be
+   * silently ignored. This ignoring is also at the expense of the contact
+   * having the same email address with a different id.
+   *
+   * Longer term it would make sense to track the email address & track bounces back to that
+   * rather than an id that may not reflect the email used. Issue logged CRM-20021.
+   *
+   * For not however, we are testing absence of mysql error in conjunction with CRM-20016.
+   */
+  public function testBounceProcessingDeletedEmail() {
+    $this->setUpMailing();
+    $this->callAPISuccess('Email', 'get', array(
+      'contact_id' => $this->contactID,
+      'api.email.delete' => 1,
+    ));
+
+    copy(__DIR__ . '/data/bounces/bounce_no_verp.txt', __DIR__ . '/data/mail/bounce_no_verp.txt');
+    $this->assertTrue(file_exists(__DIR__ . '/data/mail/bounce_no_verp.txt'));
+    $this->callAPISuccess('job', 'fetch_bounces', array());
+    $this->assertFalse(file_exists(__DIR__ . '/data/mail/bounce_no_verp.txt'));
+    $this->checkMailingBounces(1);
+  }
+
+  /**
+   * Wrapper to check for mailing bounces.
+   *
+   * Normally we would call $this->callAPISuccessGetCount but there is not one & there is resistance to
+   * adding apis for 'convenience' so just adding a hacky function to get past the impasse.
+   *
+   * @param int $expectedCount
+   */
+  public function checkMailingBounces($expectedCount) {
+    $this->assertEquals($expectedCount, CRM_Core_DAO::singleValueQuery(
+      "SELECT count(*) FROM civicrm_mailing_event_bounce WHERE event_queue_id = " . $this->eventQueue['id']
+    ));
+  }
+
+  /**
+   * Set up a mailing.
+   */
+  public function setUpMailing() {
+    $this->contactID = $this->individualCreate(array('email' => 'undeliverable@example.com'));
+    $groupID =  $this->callAPISuccess('Group', 'create', array(
+      'title' => 'Mailing group',
+      'api.GroupContact.create' => array(
+        'contact_id' => $this->contactID,
+    )));
+    $this->createMailing(array('scheduled_date' => 'now', 'groups' => array('include' => array($groupID))));
+    $this->callAPISuccess('job', 'process_mailing', array());
+    $this->eventQueue = $this->callAPISuccess('MailingEventQueue', 'get', array('api.MailingEventQueue.create' => array('hash' => 'aaaaaaaaaaaaaaaa')));
+  }
+
+}
