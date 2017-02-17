2f3afdc928477bec08e2ca13f85b7b271745cc19
Andrew Cormick-Dockery <andrew.cormick-dockery@greens.org.au>
2017-02-17 06:29:30 +0000

    Remove commented out code

--- CRM/Mailing/Info.php
+++ CRM/Mailing/Info.php
@@ -112,30 +112,12 @@ class CRM_Mailing_Info extends CRM_Core_Component_Info {
     $session = CRM_Core_Session::singleton();
     $contactID = $session->get('userID');
 
-    // Get past mailings.
-    // CRM-16155 - Limit to a reasonable number.
-    /*$civiMails = civicrm_api3('Mailing', 'get', array(
-      'is_completed' => 1,
-      'mailing_type' => array('IN' => array('standalone', 'winner')),
-      'domain_id' => CRM_Core_Config::domainID(),
-      'return' => array('id', 'name', 'scheduled_date'),
-      'sequential' => 1,
-      'options' => array(
-        'limit' => 500,
-        'sort' => 'is_archived asc, scheduled_date desc',
-      ),
-      ));*/
     // Generic params.
     $params = array(
       'options' => array('limit' => 0),
       'sequential' => 1,
     );
 
-    /*$groupNames = civicrm_api3('Group', 'get', $params + array(
-      'is_active' => 1,
-      'check_permissions' => TRUE,
-      'return' => array('title', 'visibility', 'group_type', 'is_hidden'),
-      ));*/
     $headerfooterList = civicrm_api3('MailingComponent', 'get', $params + array(
       'is_active' => 1,
       'return' => array('name', 'component_type', 'is_default', 'body_html', 'body_text'),
