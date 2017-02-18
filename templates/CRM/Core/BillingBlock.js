2c5af872b47fc9efb6769613c190181d81450f35
Seamus Lee <seamuslee001@gmail.com>
2017-02-19 10:01:22 +1100

    Minor Fix following testing on d7

--- templates/CRM/Core/BillingBlock.js
+++ templates/CRM/Core/BillingBlock.js
@@ -7,37 +7,39 @@
    * Changes the icon depending on the credit card number.
    * Removes spaces and dashes from credit card numbers.
    */
-  $(function civicrm_billingblock_creditcard_helper() {
-    $.each(CRM.config.creditCardTypes, function(key, val) {
-      var html = '<a href="#" title="' + val + '" class="crm-credit_card_type-icon-' + key + '"><span>' + val + '</span></a>';
-      $('.crm-credit_card_type-icons').append(html);
+  function civicrm_billingblock_creditcard_helper() {
+    $(function() {
+      $.each(CRM.config.creditCardTypes, function(key, val) {
+        var html = '<a href="#" title="' + val + '" class="crm-credit_card_type-icon-' + key + '"><span>' + val + '</span></a>';
+        $('.crm-credit_card_type-icons').append(html);
 
-      $('.crm-credit_card_type-icon-' + key).click(function() {
-        $('#credit_card_type').val(val);
+        $('.crm-credit_card_type-icon-' + key).click(function() {
+          $('#credit_card_type').val(val);
+          $('.crm-container .credit_card_type-section a').css('opacity', 0.25);
+          $('.crm-container .credit_card_type-section .crm-credit_card_type-icon-' + key).css('opacity', 1);
+          return false;
+        });
+      });
+
+      // Hide the CC type field (redundant)
+      $('#credit_card_type, .label', '.crm-container .credit_card_type-section').hide();
+
+      // Select according to the number entered
+      $('.crm-container input#credit_card_number').change(function() {
+        var ccnumber = cj(this).val();
+
+        // Remove spaces and dashes
+        ccnumber = ccnumber.replace(/[- ]/g, '');
+        cj(this).val(ccnumber);
+
+        // Semi-hide all images, we will un-hide the right one afterwards
         $('.crm-container .credit_card_type-section a').css('opacity', 0.25);
-        $('.crm-container .credit_card_type-section .crm-credit_card_type-icon-' + key).css('opacity', 1);
-        return false;
+        $('#credit_card_type').val('');
+
+        civicrm_billingblock_set_card_type(ccnumber);
       });
     });
-
-    // Hide the CC type field (redundant)
-    $('#credit_card_type, .label', '.crm-container .credit_card_type-section').hide();
-
-    // Select according to the number entered
-    $('.crm-container input#credit_card_number').change(function() {
-      var ccnumber = cj(this).val();
-
-      // Remove spaces and dashes
-      ccnumber = ccnumber.replace(/[- ]/g, '');
-      cj(this).val(ccnumber);
-
-      // Semi-hide all images, we will un-hide the right one afterwards
-      $('.crm-container .credit_card_type-section a').css('opacity', 0.25);
-      $('#credit_card_type').val('');
-
-      civicrm_billingblock_set_card_type(ccnumber);
-    });
-  });
+  }
 
   function civicrm_billingblock_set_card_type(ccnumber) {
     // Based on http://davidwalsh.name/validate-credit-cards
