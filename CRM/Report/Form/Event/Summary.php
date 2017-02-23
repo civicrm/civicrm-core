5d34f677b50055839b4bdcaee87efa429615bd43
JKingsnorth <john@johnkingsnorth.co.uk>
2017-02-23 11:22:48 +0000

    CRM-20170 Fix participant count in summary report for free events

--- CRM/Report/Form/Event/Summary.php
+++ CRM/Report/Form/Event/Summary.php
@@ -202,8 +202,7 @@ class CRM_Report_Form_Event_Summary extends CRM_Report_Form_Event {
                   $this->_participantWhere
 
         GROUP BY civicrm_participant.event_id,
-                 civicrm_participant.status_id,
-                 civicrm_participant.fee_currency";
+                 civicrm_participant.status_id";
 
     $info = CRM_Core_DAO::executeQuery($sql);
     $participant_data = $participant_info = $currency = array();
