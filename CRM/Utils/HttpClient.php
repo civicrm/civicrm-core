dc96f88784ca0b50613965cbdca16fc325c1a356
Coleman Watts <coleman@civicrm.org>
2017-03-08 21:25:28 -0500

    CRM-20235 - Don't set status messages from the BAO
    This function is already returning error codes. Calling functions ought to take responsibility for reporting the errors.

--- CRM/Utils/HttpClient.php
+++ CRM/Utils/HttpClient.php
@@ -92,17 +92,12 @@ class CRM_Utils_HttpClient {
 
     $fp = @fopen($localFile, "w");
     if (!$fp) {
-      // Fixme: throw error instead of setting message
-      CRM_Core_Session::setStatus(ts('Unable to write to %1.<br />Is the location writable?', array(1 => $localFile)), ts('Write Error'), 'error');
       return self::STATUS_WRITE_ERROR;
     }
     curl_setopt($ch, CURLOPT_FILE, $fp);
 
     curl_exec($ch);
     if (curl_errno($ch)) {
-      // Fixme: throw error instead of setting message
-      CRM_Core_Session::setStatus(ts('Unable to download extension from %1. Error Message: %2',
-        array(1 => $remoteFile, 2 => curl_error($ch))), ts('Extension download error'), 'error');
       return self::STATUS_DL_ERROR;
     }
     else {
