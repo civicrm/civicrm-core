1bef172eb9939267615449cb4d4310e58b51acdb
eileen <emcnaughton@wikimedia.org>
2017-03-23 09:51:17 +1300

    Enotice fix

--- CRM/Member/Form/Task/Batch.php
+++ CRM/Member/Form/Task/Batch.php
@@ -151,6 +151,7 @@ class CRM_Member_Form_Task_Batch extends CRM_Member_Form_Task {
       foreach ($this->_fields as $name => $field) {
         if ($customFieldID = CRM_Core_BAO_CustomField::getKeyID($name)) {
           $customValue = CRM_Utils_Array::value($customFieldID, $customFields);
+          $entityColumnValue = array();
           if (!empty($customValue['extends_entity_column_value'])) {
             $entityColumnValue = explode(CRM_Core_DAO::VALUE_SEPARATOR,
               $customValue['extends_entity_column_value']
