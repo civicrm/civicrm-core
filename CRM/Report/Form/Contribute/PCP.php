d2a1da5271b61c79d21fb7201d93a9340a7c644c
Edsel Roque Lopez <edsel.lopez@jmaconsulting.biz>
2017-03-24 03:14:37 +0530

    CRM-20283 Added credit card type field for PCP report (#9996)
    * CRM-20283 Added credit card type field to PCP report
    
    ----------------------------------------
    * CRM-19997: Add Credit card type fields in contribution related reports
      https://issues.civicrm.org/jira/browse/CRM-19997
    
    ----------------------------------------
    * CRM-20283:
      https://issues.civicrm.org/jira/browse/CRM-20283
    
    CRM-20283 Added credit card type field for PCP report
    
    ----------------------------------------
    * CRM-20283: Add credit card type field for PCP report
      https://issues.civicrm.org/jira/browse/CRM-20283
    
    CRM-20283 Bug fixes
    
    ----------------------------------------
    * CRM-20283: Add credit card type field for PCP report
      https://issues.civicrm.org/jira/browse/CRM-20283
    
    CRM-20283 Fixed report to read from card type rather than credit card type
    
    ----------------------------------------
    * CRM-20283: Add credit card type field for PCP report
      https://issues.civicrm.org/jira/browse/CRM-20283
    
    CRM-20283 Removed line break
    
    ----------------------------------------
    * CRM-20283:
      https://issues.civicrm.org/jira/browse/CRM-20283
    
    * QA fixes

--- CRM/Report/Form/Contribute/PCP.php
+++ CRM/Report/Form/Contribute/PCP.php
@@ -166,6 +166,24 @@ class CRM_Report_Form_Contribute_PCP extends CRM_Report_Form {
         ),
         'grouping' => 'pcp-fields',
       ),
+      'civicrm_financial_trxn' => array(
+        'dao' => 'CRM_Financial_DAO_FinancialTrxn',
+        'fields' => array(
+          'card_type' => array(
+            'title' => ts('Credit Card Type'),
+            'dbAlias' => 'GROUP_CONCAT(financial_trxn_civireport.card_type SEPARATOR ",")',
+          ),
+        ),
+        'filters' => array(
+          'card_type' => array(
+            'title' => ts('Credit Card Type'),
+            'operatorType' => CRM_Report_Form::OP_MULTISELECT,
+            'options' => CRM_Financial_DAO_FinancialTrxn::buildOptions('card_type'),
+            'default' => NULL,
+            'type' => CRM_Utils_Type::T_STRING,
+          ),
+        ),
+      ),
     );
 
     parent::__construct();
@@ -190,6 +208,9 @@ LEFT JOIN civicrm_contact {$this->_aliases['civicrm_contact']}
 LEFT JOIN civicrm_contribution_page {$this->_aliases['civicrm_contribution_page']}
           ON {$this->_aliases['civicrm_pcp']}.page_id =
              {$this->_aliases['civicrm_contribution_page']}.id";
+
+    // for credit card type
+    $this->addFinancialTrxnFromClause();
   }
 
   public function groupBy() {
@@ -352,6 +373,11 @@ LEFT JOIN civicrm_contribution_page {$this->_aliases['civicrm_contribution_page'
         );
         $rows[$rowNum]['civicrm_contact_sort_name_link'] = $url;
         $rows[$rowNum]['civicrm_contact_sort_name_hover'] = ts("View Contact Summary for this Contact.");
+        $entryFound = TRUE;
+      }
+
+      if (!empty($row['civicrm_financial_trxn_card_type'])) {
+        $rows[$rowNum]['civicrm_financial_trxn_card_type'] = $this->getLabels($row['civicrm_financial_trxn_card_type'], 'CRM_Financial_DAO_FinancialTrxn', 'card_type');
         $entryFound = TRUE;
       }
 
