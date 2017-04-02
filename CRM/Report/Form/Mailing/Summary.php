230b80807c7066d5377581efeffbf55a56db12ad
Brian Shaughnessy <brian@lcdservices.biz>
2017-04-01 22:12:49 -0400

    CRM-20369 mailing summary report

--- CRM/Report/Form/Mailing/Summary.php
+++ CRM/Report/Form/Mailing/Summary.php
@@ -87,10 +87,16 @@ class CRM_Report_Form_Mailing_Summary extends CRM_Report_Form {
         ),
         'mailing_name' => array(
           'name' => 'name',
-          'title' => ts('Mailing'),
+          'title' => ts('Mailing Name'),
           'operatorType' => CRM_Report_Form::OP_MULTISELECT,
           'type' => CRM_Utils_Type::T_STRING,
           'options' => self::mailing_select(),
+          'operator' => 'like',
+        ),
+        'mailing_subject' => array(
+          'name' => 'subject',
+          'title' => ts('Mailing Subject'),
+          'type'=> CRM_Utils_Type::T_STRING,
           'operator' => 'like',
         ),
       ),
