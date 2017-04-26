972a604273f6bbdcaae8dc60b131b9be307d0fb9
eileen <emcnaughton@wikimedia.org>
2017-04-27 11:37:45 +1200

    CRM-20424 remove call to deprecated function.
    This is the one covering the UI with errors. The fact it did not cause any test fails means it is
    not covered by unit tests :-(.
    
    As long as the Open Case link is still present for CiviCase users this is working

--- CRM/Case/Info.php
+++ CRM/Case/Info.php
@@ -212,15 +212,12 @@ class CRM_Case_Info extends CRM_Core_Component_Info {
     if (CRM_Core_Permission::check('access all cases and activities') ||
       CRM_Core_Permission::check('add cases')
     ) {
-      $atype = CRM_Core_OptionGroup::getValue('activity_type',
-        'Open Case',
-        'name'
-      );
-      if ($atype) {
+      $activityType = CRM_Core_PseudoConstant::getKey('CRM_Activity_BAO_Activity', 'activity_type_id', 'Open Case');
+      if ($activityType) {
         $shortCuts = array_merge($shortCuts, array(
           array(
             'path' => 'civicrm/case/add',
-            'query' => "reset=1&action=add&atype=$atype&context=standalone",
+            'query' => "reset=1&action=add&atype={$activityType}&context=standalone",
             'ref' => 'new-case',
             'title' => ts('Case'),
           ),
