50b0e14db47c206d05db6ea3235e958e6b01bff9
Jamie McClelland <jm@mayfirst.org>
2017-05-01 13:04:15 -0400

    CRM-20499 - syntax improvements.

--- tests/phpunit/CRM/Contact/BAO/SavedSearchTest.php
+++ tests/phpunit/CRM/Contact/BAO/SavedSearchTest.php
@@ -111,40 +111,42 @@ class CRM_Contact_BAO_SavedSearchTest extends CiviUnitTestCase {
       0 => array(
         0 => 'activity_date_low',
         1 => '=',
-        2 => '20170425000000'
+        2 => '20170425000000',
       ),
       1 => array(
         0 => 'activity_date_high',
         1 => '=',
-        2 => '20170501235959'
+        2 => '20170501235959',
       )
     );
     $formValues = array(
-      'activity_date_relative' => 'ending.week'
+      'activity_date_relative' => 'ending.week',
     );
     CRM_Contact_BAO_SavedSearch::saveRelativeDates($queryParams, $formValues);
-    $this->assertArrayHasKey('activity', $queryParams['relative_dates'], 'Relative date in activity date smart group failed.');
+    $err = 'Relative date in activity date smart group failed.';
+    $this->assertArrayHasKey('activity', $queryParams['relative_dates'], $err);
 
     // Now test using a custom field.
     $queryParams = array(
       0 => array(
         0 => 'custom_13_low',
         1 => '=',
-        2 => '20170425000000'
+        2 => '20170425000000',
       ),
       1 => array(
         0 => 'custom_13_high',
         1 => '=',
-        2 => '20170501235959'
+        2 => '20170501235959',
       )
     );
     $formValues = array(
-      'custom_13_relative' => 'ending.week'
+      'custom_13_relative' => 'ending.week',
     );
     CRM_Contact_BAO_SavedSearch::saveRelativeDates($queryParams, $formValues);
     // Since custom_13 doesn't have the word 'date' in it, the key is
     // set to 0, rather than the field name.
-    $this->assertArrayHasKey('0', $queryParams['relative_dates'], 'Relative date in custom field smart group creation failed.');
+    $err = 'Relative date in custom field smart group creation failed.';
+    $this->assertArrayHasKey('0', $queryParams['relative_dates'], $err);
 
   }
 
