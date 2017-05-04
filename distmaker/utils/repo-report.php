70c7841ab76361567b8505208268f820b7e41f83
Tim Otten <totten@civicrm.org>
2017-05-04 01:21:00 -0500

    distmaker/utils/repo-report.php - Add missing Joomla element

--- distmaker/utils/repo-report.php
+++ distmaker/utils/repo-report.php
@@ -36,6 +36,7 @@ $data = array(
     'civicrm-drupal@6.x' => repo("$DM_SOURCEDIR/drupal", getenv('DM_REF_DRUPAL6')),
     'civicrm-drupal@7.x' => repo("$DM_SOURCEDIR/drupal", getenv('DM_REF_DRUPAL')),
     //'civicrm-drupal@8.x' => repo("$DM_SOURCEDIR/drupal", getenv('DM_REF_DRUPAL8')),
+    'civicrm-joomla' => repo("$DM_SOURCEDIR/joomla", getenv('DM_REF_JOOMLA')),
     'civicrm-packages' => repo("$DM_SOURCEDIR/packages", getenv('DM_REF_PACKAGES')),
     'civicrm-wordpress' => repo("$DM_SOURCEDIR/WordPress", getenv('DM_REF_WORDPRESS')),
   ),
