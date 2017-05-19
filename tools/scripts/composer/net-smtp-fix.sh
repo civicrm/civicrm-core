0ccbacaeb1db89b75bf77f1fb968ba783574420a
Seamus Lee <seamuslee001@gmail.com>
2017-05-20 09:33:28 +1000

    Use composer for Net_SMTP and Net_Socket and patch for TLS and CiviCRM Custom Error
    Roll back Net_Socket for php compatabilty

---
+++ tools/scripts/composer/net-smtp-fix.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## Cleanup the vendor tree. The main issue here is that civi Civi is
+## deployed as a module inside a CMS, so all its source-code gets published.
+## Some libraries distribute admin tools and sample files which should not
+## be published.
+##
+## This script should be idempotent -- if you rerun it several times, it
+## should always produce the same post-condition.
+
+## Replace a line in a file
+## This is a bit like 'sed -i', but dumber and more cross-platform.
+
+# Add in CiviCRM custom error message for CRM-8744.
+if ! grep -q 'CRM-8744' vendor/pear/net_smtp/Net/SMTP.php; then
+patch vendor/pear/net_smtp/Net/SMTP.php < tools/scripts/composer/patches/net-smtp-patch.txt
+fi
+if ! grep -q '@STREAM_CRYPTO_METHOD_TLSv1_1_CLIENT' vendor/pear/net_smtp/Net/SMTP.php; then
+patch vendor/pear/net_smtp/Net/SMTP.php < tools/scripts/composer/patches/net-smtp-tls-patch.txt
+fi
