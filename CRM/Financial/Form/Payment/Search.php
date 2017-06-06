fd84d42910888e7b7f7f1184dc6d80a1d8ebeecc
deb.monish <monish.deb@jmaconsulting.biz>
2017-06-07 02:19:15 +0530

    initial commit

---
+++ CRM/Financial/Form/Payment/Search.php
@@ -0,0 +1,203 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 4.7                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2017                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License and the CiviCRM Licensing Exception along                  |
+ | with this program; if not, contact CiviCRM LLC                     |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+ */
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC (c) 2004-2017
+ */
+
+/**
+ * @todo Add comments if possible.
+ */
+class CRM_Financial_Form_Payment_Search extends CRM_Core_Form_Search {
+
+  /**
+   * The params that are sent to the query.
+   *
+   * @var array
+   */
+  protected $_queryParams;
+
+  /**
+   * Are we restricting ourselves to a single contact.
+   *
+   * @var boolean
+   */
+  protected $_single = FALSE;
+
+  /**
+   * Are we restricting ourselves to a single contact.
+   *
+   * @var boolean
+   */
+  protected $_limit = NULL;
+
+  /**
+   * Prefix for the controller.
+   */
+  protected $_prefix = "contribute_";
+
+  /**
+   * Processing needed for buildForm and later.
+   */
+  public function preProcess() {
+    $this->set('searchFormName', 'Search');
+
+    $this->_searchButtonName = $this->getButtonName('refresh');
+    $this->_actionButtonName = $this->getButtonName('next', 'action');
+
+    $this->_done = FALSE;
+    // @todo - is this an error - $this->_defaults is used.
+    $this->defaults = array();
+
+    /*
+     * we allow the controller to set force/reset externally, useful when we are being
+     * driven by the wizard framework
+     */
+
+    $this->_reset = CRM_Utils_Request::retrieve('reset', 'Boolean');
+    $this->_force = CRM_Utils_Request::retrieve('force', 'Boolean', $this, FALSE);
+    $this->_limit = CRM_Utils_Request::retrieve('limit', 'Positive', $this);
+    $this->_context = CRM_Utils_Request::retrieve('context', 'String', $this, FALSE, 'search');
+
+    $this->assign("context", $this->_context);
+
+    // get user submitted values
+    // get it from controller only if form has been submitted, else preProcess has set this
+    if (!empty($_POST)) {
+      $this->_formValues = $this->controller->exportValues($this->_name);
+    }
+    else {
+      $this->_formValues = $this->get('formValues');
+    }
+
+    $sortID = NULL;
+    if ($this->get(CRM_Utils_Sort::SORT_ID)) {
+      $sortID = CRM_Utils_Sort::sortIDValue($this->get(CRM_Utils_Sort::SORT_ID),
+        $this->get(CRM_Utils_Sort::SORT_DIRECTION)
+      );
+    }
+
+    $this->_queryParams = CRM_Contact_BAO_Query::convertFormValues($this->_formValues);
+    $selector = new CRM_Contribute_Selector_Search($this->_queryParams,
+      $this->_action,
+      NULL,
+      $this->_single,
+      $this->_limit,
+      $this->_context
+    );
+    $prefix = NULL;
+    if ($this->_context == 'user') {
+      $prefix = $this->_prefix;
+    }
+
+    $this->assign("{$prefix}limit", $this->_limit);
+    $this->assign("{$prefix}single", $this->_single);
+
+    $controller = new CRM_Core_Selector_Controller($selector,
+      $this->get(CRM_Utils_Pager::PAGE_ID),
+      $sortID,
+      CRM_Core_Action::VIEW,
+      $this,
+      CRM_Core_Selector_Controller::TRANSFER,
+      $prefix
+    );
+
+    $controller->setEmbedded(TRUE);
+    $controller->moveFromSessionToTemplate();
+
+    $this->assign('contributionSummary', $this->get('summary'));
+  }
+
+  /**
+   * Build the form object.
+   */
+  public function buildQuickForm() {
+    parent::buildQuickForm();
+
+    CRM_Contribute_BAO_Query::buildSearchForm($this);
+  }
+
+  public function postProcess() {
+    if ($this->_done) {
+      return;
+    }
+
+    $this->_done = TRUE;
+
+    if (!empty($_POST)) {
+      $this->_formValues = $this->controller->exportValues($this->_name);
+    }
+
+    $this->_queryParams = CRM_Contact_BAO_Query::convertFormValues($this->_formValues);
+
+    $this->set('formValues', $this->_formValues);
+    $this->set('queryParams', $this->_queryParams);
+
+    $sortID = NULL;
+    if ($this->get(CRM_Utils_Sort::SORT_ID)) {
+      $sortID = CRM_Utils_Sort::sortIDValue($this->get(CRM_Utils_Sort::SORT_ID),
+        $this->get(CRM_Utils_Sort::SORT_DIRECTION)
+      );
+    }
+
+    $this->_queryParams = CRM_Contact_BAO_Query::convertFormValues($this->_formValues);
+    $selector = new CRM_Contribute_Selector_Search($this->_queryParams,
+      $this->_action,
+      NULL,
+      $this->_single,
+      $this->_limit,
+      $this->_context
+    );
+    $selector->setKey($this->controller->_key);
+
+    $prefix = NULL;
+    if ($this->_context == 'basic' || $this->_context == 'user') {
+      $prefix = $this->_prefix;
+    }
+
+    $controller = new CRM_Core_Selector_Controller($selector,
+      $this->get(CRM_Utils_Pager::PAGE_ID),
+      $sortID,
+      CRM_Core_Action::VIEW,
+      $this,
+      CRM_Core_Selector_Controller::SESSION,
+      $prefix
+    );
+    $controller->setEmbedded(TRUE);
+
+    $query = &$selector->getQuery();
+    if ($this->_context == 'user') {
+      $query->setSkipPermission(TRUE);
+    }
+    $summary = &$query->summaryContribution($this->_context);
+    $this->set('summary', $summary);
+    $this->assign('contributionSummary', $summary);
+    $controller->run();
+  }
+}
