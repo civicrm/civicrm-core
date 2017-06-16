719d66eabb4059ab4e114871b8c133cd8352e73a
Herb <herb@3speedhub.com>
2017-06-15 23:40:14 -0400

    Update Base.php

--- CRM/Utils/System/Base.php
+++ CRM/Utils/System/Base.php
@@ -611,7 +611,7 @@ abstract class CRM_Utils_System_Base {
 
     return array(
       'url' => $filesURL,
-      'path' => CRM_Utils_File::baseFilePath(),
+      'path' => CRM_Utils_File::basePublicFilePath(),
     );
   }
 
