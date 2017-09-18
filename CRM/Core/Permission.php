5ad18cc2543d0069d8e26315435ddc99e2a6cd46
deb.monish <monish.deb@jmaconsulting.biz>
2017-09-18 17:14:53 +0530

    Add preUpgrade message

--- CRM/Core/Permission.php
+++ CRM/Core/Permission.php
@@ -828,19 +828,19 @@ class CRM_Core_Permission {
       ),
       'close own manual batches' => array(
         $prefix . ts('close own manual batches'),
-        ts('Close accounting batches created by user'),
+        ts('Close accounting batches created by user (with Access to CiviContribute)'),
       ),
       'close all manual batches' => array(
         $prefix . ts('close all manual batches'),
-        ts('Close all accounting batches'),
+        ts('Close all accounting batches (with Access to CiviContribute)'),
       ),
       'reopen own manual batches' => array(
         $prefix . ts('reopen own manual batches'),
-        ts('Reopen accounting batches created by user'),
+        ts('Reopen accounting batches created by user (with Access to CiviContribute)'),
       ),
       'reopen all manual batches' => array(
         $prefix . ts('reopen all manual batches'),
-        ts('Reopen all accounting batches'),
+        ts('Reopen all accounting batches (with Access to CiviContribute)'),
       ),
       'view own manual batches' => array(
         $prefix . ts('view own manual batches'),
