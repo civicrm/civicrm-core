958995464cf48a682a1c63ae4ee3f94e243a1b3e
Sean Madsen <sean@seanmadsen.com>
2017-09-19 15:54:09 -0600

    CRM-20906 - Fix regex for validating extension key

--- CRM/Admin/Form/Extensions.php
+++ CRM/Admin/Form/Extensions.php
@@ -45,7 +45,7 @@ class CRM_Admin_Form_Extensions extends CRM_Admin_Form {
     $this->_key = CRM_Utils_Request::retrieve('key', 'String',
       $this, FALSE, 0
     );
-    if (!preg_match('/[0-9a-zA-Z._-]+/', $this->_key)) {
+    if (!preg_match('/^[0-9a-zA-Z._-]+$/', $this->_key)) {
       throw new CRM_Core_Exception('Extension Key does not match expected standard');
     }
     $session = CRM_Core_Session::singleton();
