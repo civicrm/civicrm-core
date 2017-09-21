065f22273a1a1f6b9f38350a1fdc71b365de6702
Dave Jenkins <davej+git@circle-interactive.co.uk>
2017-09-21 14:03:53 +0100

    CRM-16836 - make Basic Search form group select respect ACLs (4.6 version)

--- CRM/Contact/Form/Search/Basic.php
+++ CRM/Contact/Form/Search/Basic.php
@@ -78,12 +78,15 @@ class CRM_Contact_Form_Search_Basic extends CRM_Contact_Form_Search {
     }
 
     // add select for groups
+    // Get hierarchical listing of groups, respecting ACLs for CRM-16836.
+    $groupHierarchy = CRM_Contact_BAO_Group::getGroupsHierarchy($this->_group, NULL, '&nbsp;&nbsp;', TRUE);
     if (!empty($searchOptions['groups'])) {
       $this->addSelect('group', array(
           'entity' => 'group_contact',
           'label' => ts('in'),
           'context' => 'search',
           'placeholder' => ts('- any group -'),
+          'options' => $groupHierarchy,
         ));
     }
 
