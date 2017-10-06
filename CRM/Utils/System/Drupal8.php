8caad0ce87005b1aa305a21bb8db247aeab7086f
eileen <emcnaughton@wikimedia.org>
2017-10-07 11:57:02 +1300

    CRM-21272 (preliminary tidy up) move definition of isUserRegistrationPermitted to CMS classes

--- CRM/Utils/System/Drupal8.php
+++ CRM/Utils/System/Drupal8.php
@@ -529,6 +529,16 @@ class CRM_Utils_System_Drupal8 extends CRM_Utils_System_DrupalBase {
   /**
    * @inheritDoc
    */
+  public function isUserRegistrationPermitted() {
+    if (\Drupal::config('user.settings')->get('register') == 'admin_only') {
+      return FALSE;
+    }
+    return TRUE;
+  }
+
+  /**
+   * @inheritDoc
+   */
   public function getLoggedInUfID() {
     if ($id = \Drupal::currentUser()->id()) {
       return $id;
