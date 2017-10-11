ed61a1b92b9f0e674df5d1c2fe809443c79705fb
systopia <endres@systopia.de>
2017-10-11 02:14:41 +0100

    more unit tests

--- tests/phpunit/CRM/Utils/Hook/GenerateIdentifierTest.php
+++ tests/phpunit/CRM/Utils/Hook/GenerateIdentifierTest.php
@@ -88,13 +88,30 @@ class CRM_Utils_Hook_Generate_Identifier_Test extends CiviUnitTestCase {
    * Verify the generateIdentifier hook for contribution's invoice ID
    */
   public function testInvoiceIDHook() {
-    // TODO: create contribution
+    $this->hookClass->setHook('civicrm_generateIdentifier', array($this, 'hooktest_civicrm_generateIdentifier'));
 
-    // TODO: verify: no invoice id
+    // create contribution
+    $this->callAPISuccess('Contribution', 'create', array(
+      'total_amount'           => '11.11',
+      'financial_type_id'      => 1,
+      'contact_id'             => 1,
+      'payment_instrument_id'  => 1,
+      'contribution_status_id' => 1,
+      'trxn_id'                => 'HOOKAH'
+    ));
+    $contribution = $this->callAPISuccess('Contribution', 'getsingle', array('trxn_id' => 'HOOKAH'));
 
-    // TODO: create invoice
+    // verify: no invoice number
+    $this->assertEquals('', CRM_Utils_Array::value('invoice_number', $contribution), "A new contribution should not have an invoice ID");
 
-    // TODO: verify: invoice id
+    // create invoice
+    $params = array('forPage' => 1, 'output' => 'pdf_invoice');
+    CRM_Contribute_Form_Task_Invoice::printPDF(array($contribution['id']), $params, array($contribution['contact_id']));
+    $this->assertEquals('invoice_number', $this->_last_context, "Hook was not called with the right context");
+
+    // verify: invoice number
+    $contribution = $this->callAPISuccess('Contribution', 'getsingle', array('trxn_id' => 'HOOKAH', 'return' => 'invoice_number'));
+    $this->assertEquals('HOOKAH', CRM_Utils_Array::value('invoice_number', $contribution), "The invoice number should be ours");
   }
 
   /**
@@ -106,5 +123,12 @@ class CRM_Utils_Hook_Generate_Identifier_Test extends CiviUnitTestCase {
 
     // set the identifiert to HOOKAH
     $identifier = "HOOKAH";
+
+    if ($context == 'invoice_number') {
+      // there is a bug in CiviCRM: the invoice_number doesn't get stored!
+      if (empty($object->invoice_number)) {
+        CRM_Core_DAO::setFieldValue('CRM_Contribute_DAO_Contribution', $object->id, 'invoice_number', $identifier);
+      }
+    }
   }
 }
