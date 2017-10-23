543aa76e6bb3d57d21c0ad838c4abf02d484fbe0
Alok Patel <alok@agileware.com.au>
2017-10-23 16:19:06 +0530

    CRM-20787: Unit test for this issue.

--- CRM/Core/Page/AJAX/RecurringEntity.php
+++ CRM/Core/Page/AJAX/RecurringEntity.php
@@ -20,46 +20,8 @@ class CRM_Core_Page_AJAX_RecurringEntity {
       $entityId = CRM_Utils_Type::escape($_REQUEST['entityId'], 'Integer');
       $entityTable = CRM_Utils_Type::escape($_REQUEST['entityTable'], 'String');
       $priceSet = CRM_Utils_Type::escape($_REQUEST['priceSet'], 'String');
-
-      if (!empty($_REQUEST['linkedEntityTable'])) {
-        $result = CRM_Core_BAO_RecurringEntity::updateModeLinkedEntity($entityId, $_REQUEST['linkedEntityTable'], $entityTable);
-      }
-
-      $dao = new CRM_Core_DAO_RecurringEntity();
-      if (!empty($result)) {
-        $dao->entity_id = $result['entityId'];
-        $dao->entity_table = $result['entityTable'];
-      }
-      else {
-        $dao->entity_id = $entityId;
-        $dao->entity_table = $entityTable;
-      }
-
-      if ($dao->find(TRUE)) {
-        $dao->mode = $mode;
-        $dao->save();
-
-        //CRM-20787 Fix
-        //I am not sure about other fields, if mode = 3 apply for an event then other fields
-        //should be save for all other series events or not so applying for price set only for now here.
-        if (CRM_Core_BAO_RecurringEntity::MODE_ALL_ENTITY_IN_SERIES === $mode) {
-
-          //Step-1: Get all events of series
-          $seriesEventRecords = CRM_Core_BAO_RecurringEntity::getEntitiesFor($entityId, $entityTable);
-          foreach ($seriesEventRecords as $event) {
-            //Step-3: Save price set in other series events
-            if (CRM_Price_BAO_PriceSet::removeFrom($event['table'], $event['id'])) {//Remove existing priceset
-              CRM_Core_BAO_Discount::del($event['id'], $event['table']);
-              CRM_Price_BAO_PriceSet::addTo($event['table'], $event['id'], $priceSet); //Add new price set
-            }
-          }
-        }
-        //CRM-20787 - Fix end
-        $finalResult['status'] = 'Done';
-      }
-      else {
-        $finalResult['status'] = 'Error';
-      }
+      $linkedEntityTable = $_REQUEST['linkedEntityTable'];
+      $finalResult = CRM_Core_BAO_RecurringEntity::updateModeAndPriceSet($entityId, $entityTable, $mode, $linkedEntityTable, $priceSet);
     }
     CRM_Utils_JSON::output($finalResult);
   }
