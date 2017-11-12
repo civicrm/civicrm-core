d8ccde4741efcfd470344eb7ffc27d3fd683c225
Vinu Varshith S <varshith89@gmail.com>
2017-11-12 10:35:08 +0530

    Avoid 'array_pop calling a function' situation

--- CRM/Contact/BAO/Contact.php
+++ CRM/Contact/BAO/Contact.php
@@ -309,7 +309,8 @@ class CRM_Contact_BAO_Contact extends CRM_Contact_DAO_Contact {
 
       // CRM-21041: set default 'Communication Style' if unset when creating a contact.
       if (empty($params['communication_style_id'])) {
-        $params['communication_style_id'] = array_pop(CRM_Core_OptionGroup::values('communication_style', TRUE, NULL, NULL, 'AND is_default = 1'));
+        $defaultCommunicationStyleId = CRM_Core_OptionGroup::values('communication_style', TRUE, NULL, NULL, 'AND is_default = 1');
+        $params['communication_style_id'] = array_pop($defaultCommunicationStyleId);
       }
     }
 
