6b671acad95ffdc99a29402892277b5a7f6c1286
Mihael Mladenov <mihael@compucorp.co.uk>
2017-11-14 21:18:49 +0200

    NDI-14: Changed label of cancel event participants submit button

--- CRM/Event/Form/Task/Cancel.php
+++ CRM/Event/Form/Task/Cancel.php
@@ -65,7 +65,7 @@ class CRM_Event_Form_Task_Cancel extends CRM_Event_Form_Task {
   public function buildQuickForm() {
     CRM_Utils_System::setTitle(ts('Cancel Registration for Event Participation'));
     $session = CRM_Core_Session::singleton();
-    $this->addDefaultButtons(ts('Continue'), 'done');
+    $this->addDefaultButtons(ts('Cancel Registrations'), 'done');
   }
 
   /**
