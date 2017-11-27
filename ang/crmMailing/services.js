7e7df1b775003aff51a2c174a3e10babdd5f5aad
deb.monish <monish.deb@jmaconsulting.biz>
2017-11-27 22:27:45 +0530

    CRM-21486: Support multiple test mail

--- ang/crmMailing/services.js
+++ ang/crmMailing/services.js
@@ -517,7 +517,7 @@
             return crmLegacy.url('civicrm/contact/search/advanced',
               'force=1&mailing_id=' + mailing.id + statType.searchFilter);
           case 'report':
-            var reportIds = CRM.crmMailing.reportIds; 
+            var reportIds = CRM.crmMailing.reportIds;
             return crmLegacy.url('civicrm/report/instance/' + reportIds[statType.reportType],
                 'reset=1&mailing_id_value=' + mailing.id + statType.reportFilter);
           default:
