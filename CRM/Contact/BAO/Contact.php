d7d0a239fe729adc2a7fd52b6afbaa0fbf7e96d8
Allen Shaw <allen@JoineryHQ.com>
2017-12-01 17:30:16 -0600

    Toward CRM-21180: removed static vars; removed unused method parameters.

--- CRM/Contact/BAO/Contact.php
+++ CRM/Contact/BAO/Contact.php
@@ -2638,7 +2638,7 @@ AND       civicrm_openid.is_primary = 1";
    * @return bool
    */
   public static function checkGreetingsUseCustomFieldToken($contact, $customFieldId) {
-    $greetingStrings = self::getGreetingStrings($contact, FALSE);
+    $greetingStrings = self::getGreetingStrings($contact);
     foreach ($greetingStrings as $greetingString) {
       if (strpos($greetingString, ".custom_{$customFieldId}}") !== FALSE) {
         return TRUE;
@@ -2651,91 +2651,55 @@ AND       civicrm_openid.is_primary = 1";
    * Get an array of template strings for greeting fields.
    *
    * @param object $contact Contact object.
-   * @param bool $useDefaults If TRUE, use system default greeting values;
-   *    otherwise, use values for this specific contact.
+   *
    * @return Array of greeting strings, with keys: 'email', 'postal', 'addressee'.
    */
-  public static function getGreetingStrings($contact, $useDefaults = FALSE) {
-    Civi::$statics[__CLASS__]['greeting_strings'] = array();
+  public static function getGreetingStrings($contact) {
+    $greetingStrings = array();
 
-    if (!array_key_exists($contact->id, Civi::$statics[__CLASS__]['greeting_strings'])) {
-      if ($useDefaults) {
-        //retrieve default greetings
-        $defaultGreetings = CRM_Core_PseudoConstant::greetingDefaults();
-        $contactDefaults = $defaultGreetings[$contact->contact_type];
-      }
-
-      Civi::$statics[__CLASS__]['greeting_strings'][$contact->id] = array(
-        'email' => NULL,
-        'postal' => NULL,
-        'addressee' => NULL,
+    //cache email and postal greeting to greeting display
+    if ($contact->email_greeting_custom != 'null' && $contact->email_greeting_custom) {
+      $greetingStrings['email_greeting'] = $contact->email_greeting_custom;
+    }
+    elseif ($contact->email_greeting_id != 'null' && $contact->email_greeting_id) {
+      // the filter value for Individual contact type is set to 1
+      $filter = array(
+        'contact_type' => $contact->contact_type,
+        'greeting_type' => 'email_greeting',
       );
 
-      //cache email and postal greeting to greeting display
-      if ($contact->email_greeting_custom != 'null' && $contact->email_greeting_custom) {
-        Civi::$statics[__CLASS__]['greeting_strings'][$contact->id]['email'] = $contact->email_greeting_custom;
-      }
-      elseif ($contact->email_greeting_id != 'null' && $contact->email_greeting_id) {
-        // the filter value for Individual contact type is set to 1
-        $filter = array(
-          'contact_type' => $contact->contact_type,
-          'greeting_type' => 'email_greeting',
-        );
-
-        $emailGreeting = CRM_Core_PseudoConstant::greeting($filter);
-        Civi::$statics[__CLASS__]['greeting_strings'][$contact->id]['email'] = $emailGreeting[$contact->email_greeting_id];
-      }
-      else {
-        if ($useDefaults) {
-          reset($contactDefaults['email_greeting']);
-          $emailGreetingID = key($contactDefaults['email_greeting']);
-          Civi::$statics[__CLASS__]['greeting_strings'][$contact->id]['email'] = $contactDefaults['email_greeting'][$emailGreetingID];
-        }
-      }
-
-      //postal greetings
-      if ($contact->postal_greeting_custom != 'null' && $contact->postal_greeting_custom) {
-        Civi::$statics[__CLASS__]['greeting_strings'][$contact->id]['postal'] = $contact->postal_greeting_custom;
-      }
-      elseif ($contact->postal_greeting_id != 'null' && $contact->postal_greeting_id) {
-        $filter = array(
-          'contact_type' => $contact->contact_type,
-          'greeting_type' => 'postal_greeting',
-        );
-        $postalGreeting = CRM_Core_PseudoConstant::greeting($filter);
-        Civi::$statics[__CLASS__]['greeting_strings'][$contact->id]['postal'] = $postalGreeting[$contact->postal_greeting_id];
-      }
-      else {
-        if ($useDefaults) {
-          reset($contactDefaults['postal_greeting']);
-          $postalGreetingID = key($contactDefaults['postal_greeting']);
-          Civi::$statics[__CLASS__]['greeting_strings'][$contact->id]['postal'] = $contactDefaults['postal_greeting'][$postalGreetingID];
-        }
-      }
-
-      // addressee
-      if ($contact->addressee_custom != 'null' && $contact->addressee_custom) {
-        Civi::$statics[__CLASS__]['greeting_strings'][$contact->id]['addressee'] = $contact->addressee_custom;
-      }
-      elseif ($contact->addressee_id != 'null' && $contact->addressee_id) {
-        $filter = array(
-          'contact_type' => $contact->contact_type,
-          'greeting_type' => 'addressee',
-        );
-
-        $addressee = CRM_Core_PseudoConstant::greeting($filter);
-        Civi::$statics[__CLASS__]['greeting_strings'][$contact->id]['addressee'] = $addressee[$contact->addressee_id];
-      }
-      else {
-        if ($useDefaults) {
-          reset($contactDefaults['addressee']);
-          $addresseeID = key($contactDefaults['addressee']);
-          Civi::$statics[__CLASS__]['greeting_strings'][$contact->id]['addressee'] = $contactDefaults['addressee'][$addresseeID];
-        }
-      }
+      $emailGreeting = CRM_Core_PseudoConstant::greeting($filter);
+      $greetingStrings['email_greeting'] = $emailGreeting[$contact->email_greeting_id];
     }
 
-    return Civi::$statics[__CLASS__]['greeting_strings'][$contact->id];
+    //postal greetings
+    if ($contact->postal_greeting_custom != 'null' && $contact->postal_greeting_custom) {
+      $greetingStrings['postal_greeting'] = $contact->postal_greeting_custom;
+    }
+    elseif ($contact->postal_greeting_id != 'null' && $contact->postal_greeting_id) {
+      $filter = array(
+        'contact_type' => $contact->contact_type,
+        'greeting_type' => 'postal_greeting',
+      );
+      $postalGreeting = CRM_Core_PseudoConstant::greeting($filter);
+      $greetingStrings['postal_greeting'] = $postalGreeting[$contact->postal_greeting_id];
+    }
+
+    // addressee
+    if ($contact->addressee_custom != 'null' && $contact->addressee_custom) {
+      $greetingStrings['addressee'] = $contact->addressee_custom;
+    }
+    elseif ($contact->addressee_id != 'null' && $contact->addressee_id) {
+      $filter = array(
+        'contact_type' => $contact->contact_type,
+        'greeting_type' => 'addressee',
+      );
+
+      $addressee = CRM_Core_PseudoConstant::greeting($filter);
+      $greetingStrings['addressee'] = $addressee[$contact->addressee_id];
+    }
+
+    return $greetingStrings;
   }
 
   /**
