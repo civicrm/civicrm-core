cd720ab9cae17c0bf7efb58ebb0097dd0b3fe0cb
Camilo Rodriguez <camilo@compucorp.co.uk>
2017-12-05 18:17:06 +0000

    CRM-21520: Refactor Form Quickform Building Code
    Now that we're only using one form, we don't need to have the FormBuilder
    class. Removed the class and sent the building code to the AddToCaseAsRole
    class.

--- CRM/Case/Form/AddToCaseAsRole.php
+++ CRM/Case/Form/AddToCaseAsRole.php
@@ -1,11 +1,56 @@
 <?php
 
+/**
+ * Class CRM_Case_Form_AddToCaseAsRole
+ */
 class CRM_Case_Form_AddToCaseAsRole extends CRM_Contact_Form_Task {
+
+  /**
+   * @inheritdoc
+   */
   public function buildQuickForm() {
-    $formBuilder = new CRM_Case_FormBuilder($this);
-    $formBuilder->build();
+
+    $this->add('text', 'assign_to', ts('Assign to'));
+    $roleTypes = $this->getRoleTypes();
+
+    $this->add(
+      'select',
+      'role_type',
+      ts('Relationship Type'),
+      array('' => ts('- select type -')) + $roleTypes,
+      FALSE,
+      array('class' => 'crm-select2 twenty')
+    );
+
+    $this->addButtons(array(
+      array(
+        'type' => 'submit',
+        'name' => ts('Submit'),
+        'isDefault' => TRUE,
+      ),
+    ));
   }
 
+  /**
+   * Returns list of configured role types for individuals.
+   *
+   * @return array
+   *   List of role types
+   */
+  private function getRoleTypes() {
+    $relType = CRM_Contact_BAO_Relationship::getRelationType('Individual');
+    $roleTypes = array();
+
+    foreach ($relType as $k => $v) {
+      $roleTypes[substr($k, 0, strpos($k, '_'))] = $v;
+    }
+
+    return $roleTypes;
+  }
+
+  /**
+   * @inheritdoc
+   */
   public function postProcess() {
     $values = $this->controller->exportValues();
 
