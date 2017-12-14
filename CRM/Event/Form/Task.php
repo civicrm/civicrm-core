6f44e287655f6bf072e136191fe1d8ffe23d6b1b
Jazz <Jazz-Man@users.noreply.github.com>
2017-12-14 15:59:04 +0200

    Update Task.php
    Fatal error: Uncaught Error: Using $this when not in object context

--- CRM/Event/Form/Task.php
+++ CRM/Event/Form/Task.php
@@ -127,7 +127,7 @@ class CRM_Event_Form_Task extends CRM_Core_Form {
     //set the context for redirection for any task actions
     $session = CRM_Core_Session::singleton();
 
-    $qfKey = CRM_Utils_Request::retrieve('qfKey', 'String', $this);
+    $qfKey = CRM_Utils_Request::retrieve('qfKey', 'String', $form);
     $urlParams = 'force=1';
     if (CRM_Utils_Rule::qfKey($qfKey)) {
       $urlParams .= "&qfKey=$qfKey";
