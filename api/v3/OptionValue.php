98fd592b08a5525790b8f63501175778cc9a36f5
eileen <emcnaughton@wikimedia.org>
2018-01-18 18:23:02 +1300

    Minor api tidy up.
    It is recommended practice to pass the entity to the api. This minor fix up supports that

--- api/v3/OptionValue.php
+++ api/v3/OptionValue.php
@@ -67,7 +67,7 @@ function _civicrm_api3_option_value_get_spec(&$params) {
  *   API result array
  */
 function civicrm_api3_option_value_create($params) {
-  $result = _civicrm_api3_basic_create(_civicrm_api3_get_BAO(__FUNCTION__), $params);
+  $result = _civicrm_api3_basic_create(_civicrm_api3_get_BAO(__FUNCTION__), $params, 'OptionValue');
   if (!empty($params['id']) && !array_key_exists('option_group_id', $params)) {
     $groupId = CRM_Core_DAO::getFieldValue('CRM_Core_DAO_OptionValue',
       $params['id'], 'option_group_id', 'id'
