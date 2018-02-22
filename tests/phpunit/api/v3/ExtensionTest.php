civicrm/civicrm-core

commit 517dfba8b21eb176a66fd0f21aea65fccaba592a

Fix CRM-19414.

api/v3/Extension.php
@@ -325,20 +325,22 @@ function _civicrm_api3_extension_refresh_spec(&$fields) {
  */
 function civicrm_api3_extension_get($params) {
   $statuses = CRM_Extension_System::singleton()->getManager()->getStatuses();
+  $mapper = CRM_Extension_System::singleton()->getMapper();
   $result = array();
   $id = 0;
   foreach ($statuses as $key => $status) {
-    //try {
-    //  $info = (array) $mapper->keyToInfo($key);
-    //} catch (CRM_Extension_Exception $e) {
-    $info = array();
+    try {
+      $obj = $mapper->keyToInfo($key);
+    }
+    catch (CRM_Extension_Exception $ex) {
+      CRM_Core_Session::setStatus(ts('Failed to read extension (%1). Please refresh the extension list.', array(1 => $key)));
+      continue;
+    }
+    $info = CRM_Extension_System::createExtendedInfo($obj);
     $info['id'] = $id++; // backward compatibility with indexing scheme
-    $info['key'] = $key;
-    //}
-    $info['status'] = $status;
     $result[] = $info;
   }
-  return _civicrm_api3_basic_array_get('Extension', $params, $result, 'id', array('id', 'key', 'status'));
+  return _civicrm_api3_basic_array_get('Extension', $params, $result, 'id', array());
 }
 
 /**

commit 1d470b758666648ede1c2d4cffdbeb1252ab3d18

Extension.get API - Fix regression (4.7.13) in result filtering
Overview
--------

When one calls the `Extension.get` API, it should support filtering.  For
example, these commands would return only installed extensions or only
uninstalled extensions (respectively):

```
cv api Extension.get status=installed
cv api Extension.get status=uninstalled
```

Before
------

The implementation of `Extension.get` passes a list of all extensions down to
`_civicrm_api3_basic_array_get()` which is supposed to interpret any APIv3
options (such as filter-values/offsets/limits).  However, it confuses the
`return` list (i.e.  the names of any columns *that the user wants to see*) with the
*filterable* list (i.e.  the names of any columns *for which filtering is
permitted*).

After
-----

The implementation of `Extension.get` passes a list of all extensions down to
`_civicrm_api3_basic_array_get()`.  It also passes a fixed list of columns
which one might reasonably want to filter.

It might be nice to support filtering on all fields, but (a) several fields
wouldn't be sensible (because they're nested arrays) and (b) this provides a
smaller surface-area to maintain.

Comments
--------

The doc-block for `_civicrm_api3_basic_array_get()` describes the purpose of this field:

https://github.com/civicrm/civicrm-core/blob/4.7.0/api/v3/utils.php#L2390-L2391

Since ~v4.7.13, this line of code appears to have gone through several revisions, e.g.

 * The regression was originally introduced by 517dfba8b
 * Subsequent partial-fixes were d4c44c700, 9776f417, 34239e81, 525ccb68

CC @lucianov88  @twomice @tschuettler @seamuslee001 for review

api/v3/Extension.php
@@ -368,12 +368,8 @@ function civicrm_api3_extension_get($params) {
       $result[] = $info;
     }
   }
-  $options = _civicrm_api3_get_options_from_params($params);
-  $returnFields = !empty($options['return']) ? $options['return'] : array();
-  if (!in_array('id', $returnFields)) {
-    $returnFields = array_merge($returnFields, array('id'));
-  }
-  return _civicrm_api3_basic_array_get('Extension', $params, $result, 'id', $returnFields);
+  $filterableFields = array('id', 'key', 'type', 'status', 'path');
+  return _civicrm_api3_basic_array_get('Extension', $params, $result, 'id', $filterableFields);
 }
 
 /**

tests/phpunit/api/v3/ExtensionTest.php
@@ -92,6 +92,26 @@ class api_v3_ExtensionTest extends CiviUnitTestCase {
     $this->assertEquals(6, $result['count']);
   }
 
+  /**
+   * Filtering by status=installed or status=uninstalled should produce different results.
+   */
+  public function testExtensionGetByStatus() {
+    $installed = $this->callAPISuccess('extension', 'get', array('status' => 'installed'));
+    $uninstalled = $this->callAPISuccess('extension', 'get', array('status' => 'uninstalled'));
+
+    // If the filter works, then results should be strictly independent.
+    $this->assertEquals(
+      array(),
+      array_intersect(
+        CRM_Utils_Array::collect('key', $installed['values']),
+        CRM_Utils_Array::collect('key', $uninstalled['values'])
+      )
+    );
+
+    $all = $this->callAPISuccess('extension', 'get', array());
+    $this->assertEquals($all['count'], $installed['count'] + $uninstalled['count']);
+  }
+
   public function testGetMultipleExtensions() {
     $result = $this->callAPISuccess('extension', 'get', array('key' => array('test.extension.manager.paymenttest', 'test.extension.manager.moduletest')));
     $this->assertEquals(2, $result['count']);