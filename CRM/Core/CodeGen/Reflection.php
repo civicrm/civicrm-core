187c1c7ccb52d42bcf8dc2449ffdbb7c9ef490dd
Coleman Watts <coleman@civicrm.org>
2018-03-02 15:18:04 -0500

    AllCoreTables.data.php - generate every time.
    The file was not being updated as often as necessary, and the freshness checks
    were almost as expensive as just generating the file, if not more.

--- CRM/Core/CodeGen/Reflection.php
+++ CRM/Core/CodeGen/Reflection.php
@@ -5,33 +5,15 @@
  */
 class CRM_Core_CodeGen_Reflection extends CRM_Core_CodeGen_BaseTask {
 
-  protected $checksum;
-
-  /**
-   * @var string
-   */
-  private $raw;
-
   /**
    * @return bool
    *   TRUE if an update is needed.
    */
   public function needsUpdate() {
-    if (!file_exists($this->getAbsFileName())) {
-      return TRUE;
-    }
-
     // Generating this file is fairly cheap, and we don't have robust heuristic
     // for the checksum.
-    //    if ($this->getSchemaChecksum() !== self::extractRegex($this->getAbsFileName(), ';\(GenCodeChecksum:([a-zA-Z0-9]+)\);')) {
-    //      return TRUE;
-    //    }
-
-    return !$this->isApproxPhpMatch(
-      file_get_contents($this->getAbsFileName()),
-      $this->getRaw());
+    return TRUE;
   }
-
 
   /**
    * Run generator.
@@ -45,21 +27,6 @@ class CRM_Core_CodeGen_Reflection extends CRM_Core_CodeGen_BaseTask {
   }
 
   /**
-   * Generate the raw PHP code for the data file.
-   *
-   * @return string
-   */
-  public function getRaw() {
-    if (!$this->raw) {
-      $template = new CRM_Core_CodeGen_Util_Template('php');
-      $template->assign('tables', $this->tables);
-      $template->assign('genCodeChecksum', 'NEW');
-      $this->raw = $template->fetch('listAll.tpl');
-    }
-    return $this->raw;
-  }
-
-  /**
    * Get absolute file name.
    *
    * @return string
@@ -67,19 +34,5 @@ class CRM_Core_CodeGen_Reflection extends CRM_Core_CodeGen_BaseTask {
   protected function getAbsFileName() {
     return $this->config->CoreDAOCodePath . "AllCoreTables.data.php";
   }
-
-  //   /**
-  //    * Get the checksum for the schema.
-  //    *
-  //    * @return string
-  //    */
-  //   protected function getSchemaChecksum() {
-  //     if (!$this->checksum) {
-  //       CRM_Utils_Array::flatten($this->tables, $flat);
-  //       ksort($flat);
-  //       $this->checksum = md5(json_encode($flat));
-  //     }
-  //     return $this->checksum;
-  //   }
 
 }
