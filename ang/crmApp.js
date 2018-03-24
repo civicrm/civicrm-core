314a33274e35e4c3fd0eae6557f5fce475190e23
Frank J. Gómez <frank@ginkgostreet.com>
2018-03-23 21:40:21 -0400

    Fixed regression of CRM-16995.
    The activeRoute parameter which allows setting the route which crmApp should
    immediately open was previously ignored.

--- ang/crmApp.js
+++ ang/crmApp.js
@@ -6,11 +6,19 @@
   crmApp.config(['$routeProvider',
     function($routeProvider) {
 
-      if (CRM.crmApp.defaultRoute) {
+      var route;
+      if (CRM.crmApp && CRM.crmApp.activeRoute) {
+        route = CRM.crmApp.activeRoute;
+      }
+      else if (CRM.crmApp && CRM.crmApp.defaultRoute) {
+        route = CRM.crmApp.defaultRoute;
+      }
+
+      if (route) {
         $routeProvider.when('/', {
           template: '<div></div>',
           controller: function($location) {
-            $location.path(CRM.crmApp.defaultRoute);
+            $location.path(route);
           }
         });
       }
