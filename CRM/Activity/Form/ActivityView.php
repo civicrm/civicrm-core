646516f748fbd031cdfe8f49276e3b9bf5574360
Niels Heinemann <heinemann@kompetenzz.de>
2018-03-28 14:49:06 +0200

    Add items to recents items

--- CRM/Activity/Form/ActivityView.php
+++ CRM/Activity/Form/ActivityView.php
@@ -108,6 +108,15 @@ class CRM_Activity_Form_ActivityView extends CRM_Core_Form {
 
     $values['attachment'] = CRM_Core_BAO_File::attachmentInfo('civicrm_activity', $activityId);
     $this->assign('values', $values);
+
+    $url = CRM_Utils_System::url(implode("/", $this->urlPath), "reset=1&id={$activityId}&action=view&cid={$values['source_contact_id']}");
+    CRM_Utils_Recent::add($this->_values['subject'],
+      $url,
+      $values['id'],
+      'Activity',
+      $values['source_contact_id'],
+      $values['source_contact']
+    );
   }
 
   /**
