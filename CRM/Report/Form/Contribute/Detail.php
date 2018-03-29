c27c0976de3ab1c1d749a543cb217cba97b44b0b
Guillermo Arana <garana@solucionestip.com>
2018-03-28 20:56:01 -0600

    make jenkins changes

--- CRM/Report/Form/Contribute/Detail.php
+++ CRM/Report/Form/Contribute/Detail.php
@@ -837,21 +837,16 @@ WHERE  civicrm_contribution_contribution_id={$row['civicrm_contribution_contribu
       /* Group (un-limited) report by all aliases and get counts. This might
        * be done more efficiently when the contents of $sql are known, ie. by
        * overriding this method in the report class.
+       * Bug: https://issues.civicrm.org/jira/browse/CRM-21831
+       * Alias civicrm_contribution_total_amount is DEPRECATED
        */
+      $addtotals = '';
+      $showsumcontribs = FALSE;
 
-       /**
-        * Bug: https://issues.civicrm.org/jira/browse/CRM-21831
-        * Alias civicrm_contribution_total_amount is DEPRECATED
-        */
-       $addtotals = '';
-       $showsumcontribs = FALSE;
-
-       if (array_search("civicrm_contribution_total_amount", $this->_selectAliases) !== FALSE ) {
-         $addtotals = ", sum(civicrm_contribution_total_amount) as sumcontribs";
-         $showsumcontribs = TRUE;
+       if (array_search("civicrm_contribution_total_amount", $this->_selectAliases) !== FALSE) {
+           $addtotals = ", sum(civicrm_contribution_total_amount) as sumcontribs";
+           $showsumcontribs = TRUE;
        }
-
-
       $query = $this->_select .
         "$addtotals, count(*) as ct from civireport_contribution_detail_temp3 group by " .
         implode(", ", $sectionAliases);
