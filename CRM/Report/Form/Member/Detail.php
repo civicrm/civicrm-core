51668b4d36484cc5b39c53091965f36f02ad743c
Srini <seenu4043@gmail.com>
2018-04-06 11:00:45 +0530

    CRM-6181 : style issue fixed

--- CRM/Report/Form/Member/Detail.php
+++ CRM/Report/Form/Member/Detail.php
@@ -306,7 +306,7 @@ class CRM_Report_Form_Member_Detail extends CRM_Report_Form {
   public function postProcess() {
 
     $this->beginPostProcess();
-	// CRM-6181- commenting out the below as buildACLClause taking care in buildquery
+    // CRM-6181- commenting out the below as buildACLClause taking care in buildquery
     // get the acl clauses built before we assemble the query
     //$this->buildACLClause($this->_aliases['civicrm_contact']);
     $sql = $this->buildQuery(TRUE);
