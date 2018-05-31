f2ff6efaad336c9ac4c1e87bc708e58287b482f5
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2018-06-01 00:50:55 +0100

    Only set defaults when creating the custom field

--- tests/phpunit/api/v3/CustomFieldTest.php
+++ tests/phpunit/api/v3/CustomFieldTest.php
@@ -569,4 +569,31 @@ class api_v3_CustomFieldTest extends CiviUnitTestCase {
     return $r;
   }
 
+  /**
+   * This test is designed to ensure that when a custom field is updated via the
+   * API, params that are not supplied do not revert to the defaults. This was
+   * happening with, for example, is_searchable
+   */
+  public function testDisableSearchableContactReferenceField() {
+    $customGroup = $this->customGroupCreate(array(
+      'name' => 'testCustomGroup',
+      'title' => 'testCustomGroup',
+      'extends' => 'Individual',
+    ));
+    $params = array(
+      'name' => 'testCustomField',
+      'label' => 'testCustomField',
+      'custom_group_id' => 'testCustomGroup',
+      'data_type' => 'ContactReference',
+      'html_type' => 'Autocomplete-Select',
+      'is_searchable' => '1',
+    );
+    $result = $this->callAPISuccess('CustomField', 'create', $params);
+    $params = [
+      'id' => $result['id'],
+      'is_active' => 0,
+    ];
+    $result = $this->callAPISuccess('CustomField', 'create', $params);
+  }
+
 }
