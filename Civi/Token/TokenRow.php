65ddce7b0bd404dc0d3954758741011509f89855
Aidan Saunders <aidan.saunders@squiffle.uk>
2018-06-14 11:08:46 +0100

    Render Note field tokens correctly - they are already HTML.

--- Civi/Token/TokenRow.php
+++ Civi/Token/TokenRow.php
@@ -218,6 +218,7 @@ class TokenRow {
       case 'text/html':
         // Plain => HTML.
         foreach ($textTokens as $entity => $values) {
+          $entityFields = civicrm_api3($entity, "getFields", array('api_action' => 'get'));
           foreach ($values as $field => $value) {
             if (!isset($htmlTokens[$entity][$field])) {
               // CRM-18420 - Activity Details Field are enclosed within <p>,
@@ -225,6 +226,10 @@ class TokenRow {
               // conversion of these tags resulting in raw HTML.
               if ($entity == 'activity' && $field == 'details') {
                 $htmlTokens[$entity][$field] = $value;
+              }
+              elseif (\CRM_Utils_Array::value('data_type', \CRM_Utils_Array::value($field, $entityFields['values'])) == 'Memo') {
+                // Memo fields aka custom fields of type Note are html.
+                $htmlTokens[$entity][$field] = CRM_Utils_String::purifyHTML($value);
               }
               else {
                 $htmlTokens[$entity][$field] = htmlentities($value);
