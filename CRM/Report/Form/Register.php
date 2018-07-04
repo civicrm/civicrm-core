ff62528042292809a54ca46d1efe4b6cb3195a6d
eileen <emcnaughton@wikimedia.org>
2018-07-04 20:09:03 +1200

    Simplify input params on OptionValue::addOptionValue
    The addOptionValue function is one we should probably deprecate/ remove but towards that we should
    start to remove it. This commit removes some complexity by passing option_group_name rather than
    groupParams - which is a more accurate reflection of usage and removes ambiguity around the passing
    by reference

--- CRM/Report/Form/Register.php
+++ CRM/Report/Form/Register.php
@@ -191,8 +191,7 @@ class CRM_Report_Form_Register extends CRM_Core_Form {
       // get the submitted form values.
       $params = $this->controller->exportValues($this->_name);
 
-      $groupParams = array('name' => ('report_template'));
-      $optionValue = CRM_Core_OptionValue::addOptionValue($params, $groupParams, $this->_action, $this->_id);
+      $optionValue = CRM_Core_OptionValue::addOptionValue($params, 'report_template', $this->_action, $this->_id);
       CRM_Core_Session::setStatus(ts('The %1 \'%2\' has been saved.', array(
             1 => 'Report Template',
             2 => $optionValue->label,
