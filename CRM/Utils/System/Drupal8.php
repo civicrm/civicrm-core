43936fcaf2a6b7c430148ac663c77f9e0cfaced6
Wannes De Roy <wannes.deroy@calibrate.be>
2018-07-17 13:31:22 +0200

    Update Drupal8.php

--- CRM/Utils/System/Drupal8.php
+++ CRM/Utils/System/Drupal8.php
@@ -422,7 +422,7 @@ class CRM_Utils_System_Drupal8 extends CRM_Utils_System_DrupalBase {
     chdir($root);
 
     // Create a mock $request object
-    $autoloader = require_once $root . '/vendor/autoload.php';
+    $autoloader = require_once $root . '/autoload.php';
     // @Todo: do we need to handle case where $_SERVER has no HTTP_HOST key, ie. when run via cli?
     $request = new \Symfony\Component\HttpFoundation\Request(array(), array(), array(), array(), array(), $_SERVER);
 
