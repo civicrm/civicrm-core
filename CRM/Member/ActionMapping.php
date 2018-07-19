aa0a1930832bec2467b050d77e69b9e5d57d162d
Andrew Hunt <andrew@aghstrategies.com>
2018-07-19 17:19:24 -0400

    Membership reminders: comments to document some shaky assumptions

--- CRM/Member/ActionMapping.php
+++ CRM/Member/ActionMapping.php
@@ -113,11 +113,28 @@ class CRM_Member_ActionMapping extends \Civi\ActionSchedule\Mapping {
         ->param('memberTypeValues', $selectedValues);
     }
     else {
+      // FIXME: The membership type is never null, so nobody will ever get a
+      // reminder if no membership types are selected.  Either this should be a
+      // validation on the reminder form or all types should get a reminder if
+      // no types are selected.
       $query->where("e.membership_type_id IS NULL");
     }
 
+    // FIXME: This makes a lot of sense for renewal reminders, but a user
+    // scheduling another kind of reminder might not expect members to be
+    // excluded if they have status overrides.  Ideally there would be some kind
+    // of setting per reminder.
     $query->where("( e.is_override IS NULL OR e.is_override = 0 )");
+
+    // FIXME: Similarly to overrides, excluding contacts who can't edit the
+    // primary member makes sense in the context of renewals (see CRM-11342) but
+    // would be a surprise for other use cases.
     $query->merge($this->prepareMembershipPermissionsFilter());
+
+    // FIXME: A lot of undocumented stuff happens with regard to
+    // `is_current_member`, and this is no exception.  Ideally there would be an
+    // opportunity to pick statuses when setting up the scheduled reminder
+    // rather than making the assumptions here.
     $query->where("e.status_id IN (#memberStatus)")
       ->param('memberStatus', \CRM_Member_PseudoConstant::membershipStatus(NULL, "(is_current_member = 1 OR name = 'Expired')", 'id'));
 
