103a04e3fe99ebd97cf5f577a33bb40d770c77c5
madhavimalgaonkar <madhavim2694@gmail.com>
2018-10-30 11:25:05 +0530

    fix code style warnings and switch to $this->isTableSelected

--- CRM/Report/Form/Event/ParticipantListing.php
+++ CRM/Report/Form/Event/ParticipantListing.php
@@ -295,14 +295,14 @@ class CRM_Report_Form_Event_ParticipantListing extends CRM_Report_Form_Event {
         ),
       ),
       'civicrm_note' => array(
-         'dao' => 'CRM_Core_DAO_Note',
-         'fields' => array(
-           'participant_note' => array(
-             'name' => 'note',
-             'title' => ts('Participant Note'),
-           ),
-         ),
-       ),
+        'dao' => 'CRM_Core_DAO_Note',
+        'fields' => array(
+          'participant_note' => array(
+            'name' => 'note',
+            'title' => ts('Participant Note'),
+          ),
+        ),
+      ),
       'civicrm_contribution' => array(
         'dao' => 'CRM_Contribute_DAO_Contribution',
         'fields' => array(
@@ -528,13 +528,13 @@ ORDER BY  cv.label
     $this->joinPhoneFromContact();
     $this->joinEmailFromContact();
 
-    // CRM-20236 Include participant note.
-    if (!empty($this->_params['fields']['participant_note']) || !empty($this->_params['note_value'])) {
+    // Include participant note.
+    if ($this->isTableSelected('civicrm_note')) {
       $this->_from .= "
             LEFT JOIN civicrm_note {$this->_aliases['civicrm_note']}
                    ON ( {$this->_aliases['civicrm_note']}.entity_table = 'civicrm_participant' AND
                    {$this->_aliases['civicrm_participant']}.id = {$this->_aliases['civicrm_note']}.entity_id )";
-  }
+    }
 
     if ($this->_contribField) {
       $this->_from .= "
