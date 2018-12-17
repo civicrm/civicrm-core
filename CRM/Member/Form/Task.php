41d0a2e7f6c8f968c0df988019f60f2d6addfa9a
andrewpthompson <andrewpthompson@users.noreply.github.com>
2018-12-17 12:02:55 +1030

    Fix e-notice
    only variables should be passed by reference

--- CRM/Member/Form/Task.php
+++ CRM/Member/Form/Task.php
@@ -130,7 +130,7 @@ class CRM_Member_Form_Task extends CRM_Core_Form_Task {
    * since its used for things like send email
    */
   public function setContactIDs() {
-    $this->_contactIds = &CRM_Core_DAO::getContactIDsFromComponent($this->_memberIds,
+    $this->_contactIds = CRM_Core_DAO::getContactIDsFromComponent($this->_memberIds,
       'civicrm_membership'
     );
   }
