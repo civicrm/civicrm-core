cf3d3207f9079a0508f03935faa8724c5a0115fe
Alok Patel <alok@agileware.com.au>
2018-12-20 14:58:22 +0530

    GREENS-201: Added Find contacts button. GREENS-202: Removed second contact filter field.

--- CRM/Contact/Page/DedupeException.php
+++ CRM/Contact/Page/DedupeException.php
@@ -56,14 +56,12 @@ class CRM_Contact_Page_DedupeException extends CRM_Core_Page {
     $params = array();
 
     $contactOneQ = CRM_Utils_Request::retrieve('crmContact1Q', 'String');
-    $contactTwoQ = CRM_Utils_Request::retrieve('crmContact2Q', 'String');
 
     if ($contactOneQ) {
       $params['contact_id1.display_name'] = array('LIKE' => '%' . $contactOneQ . '%');
-    }
+      $params['contact_id2.display_name'] = array('LIKE' => '%' . $contactOneQ . '%');
 
-    if ($contactTwoQ) {
-      $params['contact_id2.display_name'] = array('LIKE' => '%' . $contactTwoQ . '%');
+      $params['options']['or'] = [["contact_id1.display_name", "contact_id2.display_name"]];
     }
 
     $totalitems = civicrm_api3('Exception', "getcount", $params);
@@ -88,17 +86,12 @@ class CRM_Contact_Page_DedupeException extends CRM_Core_Page {
   protected function getExceptions() {
     list($offset, $limit) = $this->_pager->getOffsetAndRowCount();
     $contactOneQ = CRM_Utils_Request::retrieve('crmContact1Q', 'String');
-    $contactTwoQ = CRM_Utils_Request::retrieve('crmContact2Q', 'String');
 
     if (!$contactOneQ) {
       $contactOneQ = '';
     }
-    if (!$contactTwoQ) {
-      $contactTwoQ = '';
-    }
 
     $this->assign('searchcontact1', $contactOneQ);
-    $this->assign('searchcontact2', $contactTwoQ);
 
     $params = array(
       "options"     => array('limit' => $limit, 'offset' => $offset),
@@ -107,10 +100,9 @@ class CRM_Contact_Page_DedupeException extends CRM_Core_Page {
 
     if ($contactOneQ != '') {
       $params['contact_id1.display_name'] = array('LIKE' => '%' . $contactOneQ . '%');
-    }
+      $params['contact_id2.display_name'] = array('LIKE' => '%' . $contactOneQ . '%');
 
-    if ($contactTwoQ != '') {
-      $params['contact_id2.display_name'] = array('LIKE' => '%' . $contactTwoQ . '%');
+      $params['options']['or'] = [["contact_id1.display_name", "contact_id2.display_name"]];
     }
 
     $exceptions = civicrm_api3("Exception", "get", $params);
