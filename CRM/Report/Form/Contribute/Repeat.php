5210bcce2721d2088489a5ae61acdecc75565d51
francescbassas <francesc@babu.cat>
2018-12-24 11:18:50 +0100

    Update Repeat.php

--- CRM/Report/Form/Contribute/Repeat.php
+++ CRM/Report/Form/Contribute/Repeat.php
@@ -574,7 +574,7 @@ LEFT JOIN $this->tempTableRepeat2 {$this->_aliases['civicrm_contribution']}2
     }
     else {
       foreach ($fields['fields'] as $fld_id => $value) {
-        if (!($fld_id == 'total_amount1') && !($fld_id == 'total_amount2') && !(substr( $fld_id, 0, 7 ) === "custom_")) {
+        if (!($fld_id == 'total_amount1') && !($fld_id == 'total_amount2') && !(substr($fld_id, 0, 7) === "custom_")) {
           $found = FALSE;
           $invlidGroups = array();
           foreach ($fields['group_bys'] as $grp_id => $val) {
