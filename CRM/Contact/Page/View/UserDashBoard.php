390e34b3b9d1ce4990964329f18be5f6f4a5df9c
yashodha <yashodha@cividesk.com>
2019-01-01 18:38:10 +0530

    (dev/core#622) User Dashboard doesn't show Edit Contact Information link

--- CRM/Contact/Page/View/UserDashBoard.php
+++ CRM/Contact/Page/View/UserDashBoard.php
@@ -235,7 +235,7 @@ class CRM_Contact_Page_View_UserDashBoard extends CRM_Core_Page {
       );
 
       if (CRM_Core_Permission::check('access CiviCRM')) {
-        self::$_links = array_merge(self::$_links, array(
+        self::$_links += array(
           CRM_Core_Action::DISABLE => array(
             'name' => ts('Disable'),
             'url' => 'civicrm/contact/view/rel',
@@ -243,7 +243,7 @@ class CRM_Contact_Page_View_UserDashBoard extends CRM_Core_Page {
             'extra' => 'onclick = "return confirm(\'' . $disableExtra . '\');"',
             'title' => ts('Disable Relationship'),
           ),
-        ));
+        );
       }
     }
 
