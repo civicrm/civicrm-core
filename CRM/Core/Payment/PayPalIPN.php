e8780288b31e6d2583b93a85e652b3e19613a708
Seamus Lee <seamuslee001@gmail.com>
2019-01-12 17:41:43 -0500

    Utilize getTimeZoneString() method
    Co-Authored-By: jgillmanjr <jason@rrfaae.com>

--- CRM/Core/Payment/PayPalIPN.php
+++ CRM/Core/Payment/PayPalIPN.php
@@ -394,7 +394,7 @@ class CRM_Core_Payment_PayPalIPN extends CRM_Core_Payment_BaseIPN {
     $paymentDate = $this->retrieve('payment_date', 'String', FALSE);
     if (!empty($paymentDate)) {
       $receiveDateTime = new DateTime($paymentDate);
-      $systemTimeZone = new DateTimeZone(date_default_timezone_get());
+      $systemTimeZone = new DateTimeZone(CRM_Core_Config::singleton()->userSystem->getTimeZoneString());
       $receiveDateTime->setTimezone($systemTimeZone);
       $input['receive_date'] = $receiveDateTime->format('YmdHis');
     }
