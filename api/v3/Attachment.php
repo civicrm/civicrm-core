ff9aeadb41e09a68b89f57fd2ec8433381afe878
Seamus Lee <seamuslee001@gmail.com>
2019-01-19 09:01:17 +1100

    security/core#26 Add in a generated Hash to download files so that URLs can't just be tested by annon users

--- api/v3/Attachment.php
+++ api/v3/Attachment.php
@@ -435,8 +435,9 @@ function _civicrm_api3_attachment_format_result($fileDao, $entityFileDao, $retur
     'icon' => CRM_Utils_File::getIconFromMimeType($fileDao->mime_type),
     'created_id' => $fileDao->created_id,
   );
+  $fileHash = CRM_Core_BAO_File::generateFileHash($result['entity_id'], $result['id']);
   $result['url'] = CRM_Utils_System::url(
-    'civicrm/file', 'reset=1&id=' . $result['id'] . '&eid=' . $result['entity_id'],
+    'civicrm/file', 'reset=1&id=' . $result['id'] . '&eid=' . $result['entity_id'] . '&fcs=' . $fileHash,
     TRUE,
     NULL,
     FALSE,
