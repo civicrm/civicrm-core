6ac03efc17e5e2354d19874f4eeefb79cffb02af
ray-wright <34744834+ray-wright@users.noreply.github.com>
2019-01-29 13:18:26 -0500

    code clean up
    code rearranged for clarity

--- CRM/Case/Form/CaseView.php
+++ CRM/Case/Form/CaseView.php
@@ -94,19 +94,20 @@ class CRM_Case_Form_CaseView extends CRM_Core_Form {
     $statuses = CRM_Case_PseudoConstant::caseStatus('label', FALSE);
     $caseTypeName = CRM_Case_BAO_Case::getCaseType($this->_caseID, 'name');
     $caseType = CRM_Case_BAO_Case::getCaseType($this->_caseID);
+    $statusClass = civicrm_api3('OptionValue', 'getsingle', [
+      'option_group_id' => "case_status",
+      'value' => $values['case_status_id'],
+      'return' => 'grouping',
+    ]);
 
     $this->_caseDetails = array(
       'case_type' => $caseType,
       'case_status' => CRM_Utils_Array::value($values['case_status_id'], $statuses),
       'case_subject' => CRM_Utils_Array::value('subject', $values),
       'case_start_date' => $values['case_start_date'],
+      'status_class' => $statusClass['grouping'];,
     );
-      $statusClass = civicrm_api3('OptionValue', 'getsingle', [
-      'option_group_id' => "case_status",
-      'value' => $values['case_status_id'],
-      'return' => 'grouping',
-    ]);
-    $this->_caseDetails['status_class'] = $statusClass['grouping'];
+      
     $this->_caseType = $caseTypeName;
     $this->assign('caseDetails', $this->_caseDetails);
 
