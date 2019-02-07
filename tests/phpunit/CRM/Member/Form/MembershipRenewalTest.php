6e1054a0980a39c43f9941bd3cfaba25c720dc66
Jon Goldberg <jon@megaphonetech.com>
2019-02-07 15:43:53 -0500

    Membership#9 - show notifications for membership renewal

--- tests/phpunit/CRM/Member/Form/MembershipRenewalTest.php
+++ tests/phpunit/CRM/Member/Form/MembershipRenewalTest.php
@@ -184,6 +184,7 @@ class CRM_Member_Form_MembershipRenewalTest extends CiviUnitTestCase {
     $form->_contactID = $this->_individualId;
 
     $form->testSubmit($params);
+    $form->setRenewalMessage();
     $membership = $this->callAPISuccessGetSingle('Membership', array('contact_id' => $this->_individualId));
     $this->callAPISuccessGetCount('ContributionRecur', array('contact_id' => $this->_individualId), 0);
     $contribution = $this->callAPISuccess('Contribution', 'get', array(
@@ -205,6 +206,14 @@ class CRM_Member_Form_MembershipRenewalTest extends CiviUnitTestCase {
         'return' => 'payment_instrument_id',
       )),
       ), 'online');
+    $this->assertEquals([
+      [
+        'text' => 'AnnualFixed membership for Mr. Anthony Anderson II has been renewed. The new membership End Date is December 31st, ' . (date('Y') + 1) . '.',
+        'title' => 'Complete',
+        'type' => 'success',
+        'options' => NULL,
+      ],
+    ], CRM_Core_Session::singleton()->getStatus());
   }
 
   /**
