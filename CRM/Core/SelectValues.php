658ecb8b71c317bdbd29292e88c0388aa0e66b3d
Kator James <kator95@gmail.com>
2019-02-13 15:10:44 +0100

    C51-369: Do not attempt to copy custom fields from $optionNames

--- CRM/Core/SelectValues.php
+++ CRM/Core/SelectValues.php
@@ -1148,8 +1148,10 @@ class CRM_Core_SelectValues {
     $settings = civicrm_api3('Setting', 'getValue', array('name' => 'quicksearch_options'));
     $options = [];
     foreach($settings as $key => $setting) {
-      $options[$setting] = $optionNames[$setting];
-      unset($optionNames[$setting]);
+      if (isset($optionNames[$setting])) {
+        $options[$setting] = $optionNames[$setting];
+        unset($optionNames[$setting]);
+      }
     }
     // Not all options will be enabled so append the disabled options back into the array
     $options = array_merge($options, $optionNames);
