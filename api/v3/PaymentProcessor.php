71b4fdf9e2ce11b74a722f29040db01b4d110072
eileen <emcnaughton@wikimedia.org>
2019-04-04 14:06:29 +1300

    Add basic Pay api

--- api/v3/PaymentProcessor.php
+++ api/v3/PaymentProcessor.php
@@ -117,6 +117,40 @@ function _civicrm_api3_payment_processor_getlist_defaults(&$request) {
 }
 
 /**
+ * Action payment.
+ *
+ * @param array $params
+ *
+ * @return array
+ *   API result array.
+ * @throws CiviCRM_API3_Exception
+ */
+function civicrm_api3_payment_processor_pay($params) {
+  $processor = Civi\Payment\System::singleton()->getById($params['payment_processor_id']);
+  $processor->setPaymentProcessor(civicrm_api3('PaymentProcessor', 'getsingle', ['id' => $params['payment_processor_id']]));
+  $result = $processor->doPayment($params);
+  return civicrm_api3_create_success(array($result), $params);
+}
+
+/**
+ * Action payment.
+ *
+ * @param array $params
+ */
+function _civicrm_api3_payment_processor_pay_spec(&$params) {
+  $params['payment_processor_id'] = [
+    'api.required' => 1,
+    'title' => ts('Payment processor'),
+    'type' => CRM_Utils_Type::T_INT,
+  ];
+  $params['amount'] = [
+    'api.required' => TRUE,
+    'title' => ts('Amount to refund'),
+    'type' => CRM_Utils_Type::T_MONEY,
+  ];
+}
+
+/**
  * Action refund.
  *
  * @param array $params
