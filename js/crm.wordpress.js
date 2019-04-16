b02b8e4d0c2e97bb4e82222bd5b05f3df27d2c1c
Coleman Watts <coleman@civicrm.org>
2019-04-16 16:36:42 -0400

    Simplify hiding CMS elements behind modal dialogs
    Using css instead of js makes this a lot simpler

--- js/crm.wordpress.js
+++ js/crm.wordpress.js
@@ -1,16 +1,6 @@
 // http://civicrm.org/licensing
 CRM.$(function($) {
   $(document)
-    .on('dialogopen', function(e) {
-      // Make admin bar hide behind popup windows
-      $('#adminmenuwrap').css('z-index', '100');
-    })
-    .on('dialogclose', function(e) {
-      if ($('.ui-dialog-content:visible').not(e.target).length < 1) {
-        // Restore admin bar position
-        $('#adminmenuwrap').css('z-index', '');
-      }
-    })
     .on('crmWysiwygCreate', function(e, type, editor) {
       if (type === 'ckeditor') {
         editor.on('maximize', function(e) {
