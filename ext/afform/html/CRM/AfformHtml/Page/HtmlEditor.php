6d888111d49edf06012769fdd1028f4fa90f223c
Tim Otten <totten@civicrm.org>
2019-05-03 20:59:04 -0700

    afform_html - Add skeletal page civicrm/admin/afform/html

---
+++ ext/afform/html/CRM/AfformHtml/Page/HtmlEditor.php
@@ -0,0 +1,16 @@
+<?php
+use CRM_AfformHtml_ExtensionUtil as E;
+
+class CRM_AfformHtml_Page_HtmlEditor extends CRM_Core_Page {
+
+  public function run() {
+    // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
+    CRM_Utils_System::setTitle(E::ts('HtmlEditor'));
+
+    // Example: Assign a variable for use in a template
+    $this->assign('currentTime', date('Y-m-d H:i:s'));
+
+    parent::run();
+  }
+
+}
