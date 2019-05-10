6d3a046e37d333d4080a93da84724ec0523de9d4
Seamus Lee <seamuslee001@gmail.com>
2019-05-11 09:24:47 +1000

    dev/core#956 Fix hard breakage in find participants form when you do a filter on one event and n participant statues

--- CRM/Event/Form/Search.php
+++ CRM/Event/Form/Search.php
@@ -170,7 +170,7 @@ class CRM_Event_Form_Search extends CRM_Core_Form_Search {
           $seatClause[] = "( participant.is_test = {$this->_formValues['participant_test']} )";
         }
         if (!empty($this->_formValues['participant_status_id'])) {
-          $seatClause[] = CRM_Contact_BAO_Query::buildClause("participant.status_id", '=', $this->_formValues['participant_status_id'], 'Int');
+          $seatClause[] = CRM_Contact_BAO_Query::buildClause("participant.status_id", 'IN', $this->_formValues['participant_status_id'], 'Int');
           if ($status = CRM_Utils_Array::value('IN', $this->_formValues['participant_status_id'])) {
             $this->_formValues['participant_status_id'] = $status;
           }
