9e40c4169aa0a84ccb7c2e0bcf97d243d066e0e3
sushantpaste <sushant@osseed.com>
2019-05-24 17:20:48 +0530

    dev/core#877 Fixes alterReportVars hook invoke

--- CRM/Report/Form.php
+++ CRM/Report/Form.php
@@ -2377,12 +2377,6 @@ WHERE cg.extends IN ('" . implode("','", $this->_customGroupExtends) . "') AND
       }
     }
 
-    // build array of section totals
-    $this->sectionTotals();
-
-    // process grand-total row
-    $this->grandTotal($rows);
-
     // Find alter display functions.
     $firstRow = reset($rows);
     if ($firstRow) {
@@ -2422,6 +2416,12 @@ WHERE cg.extends IN ('" . implode("','", $this->_customGroupExtends) . "') AND
     // use this method for formatting rows for display purpose.
     $this->alterDisplay($rows);
     CRM_Utils_Hook::alterReportVar('rows', $rows, $this);
+
+    // build array of section totals
+    $this->sectionTotals();
+
+    // process grand-total row
+    $this->grandTotal($rows);
 
     // use this method for formatting custom rows for display purpose.
     $this->alterCustomDataDisplay($rows);
