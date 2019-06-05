96298d469113a0b69ff30ccde82b47d1ca24d006
Andrew Hunt <andrew@aghstrategies.com>
2019-06-05 12:33:24 -0400

    Minimum supported PHP version is 7.0

--- CRM/Upgrade/Incremental/General.php
+++ CRM/Upgrade/Incremental/General.php
@@ -53,7 +53,7 @@ class CRM_Upgrade_Incremental_General {
    *
    * @see install/index.php
    */
-  const MIN_INSTALL_PHP_VER = '5.6';
+  const MIN_INSTALL_PHP_VER = '7.0';
 
   /**
    * Compute any messages which should be displayed before upgrade.
