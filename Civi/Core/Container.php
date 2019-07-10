5d27adeb234189d53ca14612d64d02d3da062e98
Seamus Lee <seamuslee001@gmail.com>
2019-07-11 08:19:18 +1000

    Ensure recently converted groups cache matches previous behabiour my setting withArray as fast for it
    Ensure custom data cache use the fastArray Cache

--- Civi/Core/Container.php
+++ Civi/Core/Container.php
@@ -168,7 +168,7 @@ class Container {
       // For Caches that we don't really care about the ttl for and/or maybe accessed
       // fairly often we use the fastArrayDecorator which improves reads and writes, these
       // caches should also not have concurrency risk.
-      $fastArrayCaches = ['groups', 'navigation'];
+      $fastArrayCaches = ['groups', 'navigation', 'customData'];
       if (in_array($cacheSvc, $fastArrayCaches)) {
         $definitionParams['withArray'] = 'fast';
       }
