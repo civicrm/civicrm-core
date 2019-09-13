21843539050b1831e0a20302d5da70bf6b933dcb
Seamus Lee <seamuslee001@gmail.com>
2019-09-14 07:21:48 +1000

    dev/core#1249 Allow IPNs to be validated even if the supplied contact id is wrong by checking for contact ids in other objects
    Rework the checking code to get contribution first then try using the contact_id from the contribution and log if it is different and error if we can't find the linked contact_id
    
    Rationalise the validation as contact_id in the contribution should always be there and do not worry about checking is_deleted or is_deceased
    
    Ensure we load the contact object

--- tests/phpunit/CRM/Core/Payment/PayPalIPNTest.php
+++ tests/phpunit/CRM/Core/Payment/PayPalIPNTest.php
@@ -334,6 +334,40 @@ class CRM_Core_Payment_PayPalIPNTest extends CiviUnitTestCase {
   }
 
   /**
+   * Allow IPNs to validate when the supplied contact_id has been deleted from the database but there is a valid contact id in the contribution recur object or contribution object
+   */
+  public function testPayPalIPNSuccessDeletedContact() {
+    $contactTobeDeleted = $this->individualCreate();
+    $pendingStatusID = CRM_Core_PseudoConstant::getKey('CRM_Contribute_BAO_Contribution', 'contribution_status_id', 'Pending');
+    $completedStatusID = CRM_Core_PseudoConstant::getKey('CRM_Contribute_BAO_Contribution', 'contribution_status_id', 'Completed');
+    $params = [
+      'payment_processor_id' => $this->_paymentProcessorID,
+      'contact_id' => $this->_contactID,
+      'trxn_id' => NULL,
+      'invoice_id' => $this->_invoiceID,
+      'contribution_status_id' => $pendingStatusID,
+      'is_email_receipt' => TRUE,
+    ];
+    $this->_contributionID = $this->contributionCreate($params);
+    $contribution = $this->callAPISuccess('contribution', 'get', ['id' => $this->_contributionID, 'sequential' => 1]);
+    // assert that contribution created before handling payment via paypal standard has no transaction id set and pending status
+    $this->assertEquals(NULL, $contribution['values'][0]['trxn_id']);
+    $this->assertEquals($pendingStatusID, $contribution['values'][0]['contribution_status_id']);
+    $payPalIPNParams = $this->getPaypalTransaction();
+    $payPalIPNParams['contactID'] = $contactTobeDeleted;
+    $this->callAPISuccess('Contact', 'delete', ['id' => $contactTobeDeleted, 'skip_undelete' => 1]);
+    global $_REQUEST;
+    $_REQUEST = ['q' => CRM_Utils_System::url('civicrm/payment/ipn/' . $this->_paymentProcessorID)] + $payPalIPNParams;
+    // Now process the IPN noting that the contact id that was supplied with the IPN has been deleted but there is still a valid one on the contribution id
+    $payment = CRM_Core_Payment::handlePaymentMethod('PaymentNotification', ['processor_id' => $this->_paymentProcessorID]);
+
+    $contribution = $this->callAPISuccess('contribution', 'get', ['id' => $this->_contributionID, 'sequential' => 1]);
+    // assert that contribution is completed after getting response from paypal standard which has transaction id set and completed status
+    $this->assertEquals($_REQUEST['txn_id'], $contribution['values'][0]['trxn_id']);
+    $this->assertEquals($completedStatusID, $contribution['values'][0]['contribution_status_id']);
+  }
+
+  /**
    * Store Custom data passed in from the PayPalIPN in a custom field
    */
   public function hookCiviCRMAlterIPNData($data) {
