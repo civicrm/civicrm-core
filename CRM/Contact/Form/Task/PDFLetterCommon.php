civicrm/civicrm-core

commit 752cee47213094c29b8e6ae5d0b20f0800520d80

[REF] extract isLiveMode
I'm closing https://github.com/civicrm/civicrm-core/pull/13369 as it is stale, but
I figure it makes sense to pull out this extraction from it. I think the PR then
alters the button names taken into account but I'm not seeking to fix here, just
to do the extraction

CRM/Contact/Form/Task/PDFLetterCommon.php
@@ -126,17 +126,12 @@ class CRM_Contact_Form_Task_PDFLetterCommon extends CRM_Core_Form_Task_PDFLetter
   public static function postProcess(&$form) {
     $formValues = $form->controller->exportValues($form->getName());
     list($formValues, $categories, $html_message, $messageToken, $returnProperties) = self::processMessageTemplate($formValues);
-    $buttonName = $form->controller->getButtonName();
     $skipOnHold = isset($form->skipOnHold) ? $form->skipOnHold : FALSE;
     $skipDeceased = isset($form->skipDeceased) ? $form->skipDeceased : TRUE;
     $html = $activityIds = [];
 
-    // CRM-21255 - Hrm, CiviCase 4+5 seem to report buttons differently...
-    $c = $form->controller->container();
-    $isLiveMode = ($buttonName == '_qf_PDF_upload') || isset($c['values']['PDF']['buttons']['_qf_PDF_upload']);
-
     // CRM-16725 Skip creation of activities if user is previewing their PDF letter(s)
-    if ($isLiveMode) {
+    if (self::isLiveMode($form)) {
       $activityIds = self::createActivities($form, $html_message, $form->_contactIds, $formValues['subject'], CRM_Utils_Array::value('campaign_id', $formValues));
     }
 
@@ -346,6 +341,27 @@ class CRM_Contact_Form_Task_PDFLetterCommon extends CRM_Core_Form_Task_PDFLetter
       Civi::$statics[__CLASS__]['token_categories'] = array_keys($tokens);
     }
     return Civi::$statics[__CLASS__]['token_categories'];
+  }
+
+  /**
+   * Is the form in live mode (as opposed to being run as a preview).
+   *
+   * Returns true if the user has clicked the Download Document button on a
+   * Print/Merge Document (PDF Letter) search task form, or false if the Preview
+   * button was clicked.
+   *
+   * @param CRM_Core_Form $form
+   *
+   * @return bool
+   *   TRUE if the Download Document button was clicked (also defaults to TRUE
+   *     if the form controller does not exist), else FALSE
+   */
+  protected static function isLiveMode($form) {
+    // CRM-21255 - Hrm, CiviCase 4+5 seem to report buttons differently...
+    $buttonName = $form->controller->getButtonName();
+    $c = $form->controller->container();
+    $isLiveMode = ($buttonName == '_qf_PDF_upload') || isset($c['values']['PDF']['buttons']['_qf_PDF_upload']);
+    return $isLiveMode;
   }
 
 }

commit dd8e53c4b45461e966b4fcc38f1d9df0044db396

dev/core#1281 fix e-notice on isLiveMode
Looks like a regression from https://github.com/civicrm/civicrm-core/commit/752cee47213094c29b8e6ae5d0b20f0800520d80
which hit in 5.15 - seems good to merge to rc / about to cut to rc

CRM/Contact/Form/Task/PDFLetterCommon.php
@@ -180,7 +180,7 @@ class CRM_Contact_Form_Task_PDFLetterCommon extends CRM_Core_Form_Task_PDFLetter
     }
 
     $tee = NULL;
-    if ($isLiveMode && Civi::settings()->get('recordGeneratedLetters') === 'combined-attached') {
+    if (self::isLiveMode($form) && Civi::settings()->get('recordGeneratedLetters') === 'combined-attached') {
       if (count($activityIds) !== 1) {
         throw new CRM_Core_Exception("When recordGeneratedLetters=combined-attached, there should only be one activity.");
       }