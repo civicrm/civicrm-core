62a397865585c9bf10ba904e97bb14429f1e769b
eileen <emcnaughton@wikimedia.org>
2019-10-08 08:21:21 +0200

    dev/core#1293 Fix regression on  export filtering for postal address only

--- CRM/Export/BAO/ExportProcessor.php
+++ CRM/Export/BAO/ExportProcessor.php
@@ -803,7 +803,7 @@ class CRM_Export_BAO_ExportProcessor {
             $addressWhere .= " OR civicrm_address.supplemental_address_1 <> ''";
           }
         }
-        $whereClauses['address'] = $addressWhere;
+        $whereClauses['address'] = '(' . $addressWhere . ')';
       }
     }
 
