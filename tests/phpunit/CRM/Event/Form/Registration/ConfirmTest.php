32f63457671a660c11ab72b3cbd9b9f60b753d38
sarvesh211999 <sarvesh211999@gmail.com>
2019-10-18 05:40:01 +0530

    added test case for multiple paymentrecords check

--- tests/phpunit/CRM/Event/Form/Registration/ConfirmTest.php
+++ tests/phpunit/CRM/Event/Form/Registration/ConfirmTest.php
@@ -576,4 +576,85 @@ class CRM_Event_Form_Registration_ConfirmTest extends CiviUnitTestCase {
     list($contact_id, $participant_id) = $this->submitWithNote($event, $contact_id);
   }
 
+  /**
+   * Test for Multiple participant.
+   */
+  public function testMultipleParticipant() {
+    $params = ['is_monetary' => 1, 'financial_type_id' => 1];
+    $event = $this->eventCreate($params);
+    CRM_Event_Form_Registration_Confirm::testSubmit([
+      'id' => $event['id'],
+      'contributeMode' => 'direct',
+      'registerByID' => $this->createLoggedInUser(),
+      'totalAmount' => 440,
+      'event' => reset($event['values']),
+      'params' => [
+        [
+          'qfKey' => 'e6eb2903eae63d4c5c6cc70bfdda8741_2801',
+          'entryURL' => "http://dmaster.local/civicrm/event/register?reset=1&amp;id={$event['id']}",
+          'first_name' => 'Participant1',
+          'last_name' => 'LastName',
+          'email-Primary' => 'participant1@example.com',
+          'additional_participants' => 2,
+          'payment_processor_id' => 0,
+          'bypass_payment' => '',
+          'MAX_FILE_SIZE' => '33554432',
+          'is_primary' => 1,
+          'is_pay_later' => 1,
+          'campaign_id' => NULL,
+          'defaultRole' => 1,
+          'participant_role_id' => '1',
+          'currencyID' => 'USD',
+          'amount_level' => 'Tiny-tots (ages 5-8) - 1',
+          'amount' => '100.00',
+          'tax_amount' => 10,
+          'ip_address' => '127.0.0.1',
+          'invoiceID' => '57adc34957a29171948e8643ce906332',
+          'trxn_id' => '123456789',
+          'button' => '_qf_Register_upload',
+        ],
+        [
+          'qfKey' => 'e6eb2903eae63d4c5c6cc70bfdda8741_2801',
+          'entryURL' => "http://dmaster.local/civicrm/event/register?reset=1&amp;id={$event['id']}",
+          'first_name' => 'Participant2',
+          'last_name' => 'LastName',
+          'email-Primary' => 'participant2@example.com',
+          'campaign_id' => NULL,
+          'is_pay_later' => 1,
+          'participant_role_id' => '1',
+          'currencyID' => 'USD',
+          'amount_level' => 'Tiny-tots (ages 9-18) - 1',
+          'amount' => '200.00',
+          'tax_amount' => 20,
+        ],
+        [
+          'qfKey' => 'e6eb2903eae63d4c5c6cc70bfdda8741_2801',
+          'entryURL' => "http://dmaster.local/civicrm/event/register?reset=1&amp;id={$event['id']}",
+          'first_name' => 'Participant3',
+          'last_name' => 'LastName',
+          'email-Primary' => 'participant3@example.com',
+          'campaign_id' => NULL,
+          'is_pay_later' => 1,
+          'participant_role_id' => '1',
+          'currencyID' => 'USD',
+          'amount_level' => 'Tiny-tots (ages 5-8) - 1',
+          'amount' => '100.00',
+          'tax_amount' => 10,
+        ],
+      ],
+    ]);
+    $participants = $this->callAPISuccess('Participant', 'get', [])['values'];
+    $participantsPayment = $this->callAPISuccess('ParticipantPayment', 'get', [])['values'];
+
+    $ids_participant = array_column($participants, 'participant_id');
+
+    $ids_payment_participant = array_column($participantsPayment, 'participant_id');
+    if ($ids_participant == $ids_payment_participant) {
+      $this->assertTrue(TRUE);
+    }
+    else {
+      $this->assertFalse(TRUE);
+    }
+  }
+
 }
