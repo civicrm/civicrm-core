800a8607013268520dd1d5a2fa42f4645274812f
Patrick Figel <pfigel@greenpeace.org>
2019-10-22 13:59:05 +0200

    [WIP] Remove hardcoded DynamicFKAuthorization delegates

--- Civi/Core/Container.php
+++ Civi/Core/Container.php
@@ -433,8 +433,7 @@ class Container {
        FROM civicrm_custom_field fld
        INNER JOIN civicrm_custom_group grp ON fld.custom_group_id = grp.id
        WHERE fld.data_type = "File"
-      ',
-      ['civicrm_activity', 'civicrm_mailing', 'civicrm_contact', 'civicrm_grant']
+      '
     ));
 
     $kernel->setApiProviders([
