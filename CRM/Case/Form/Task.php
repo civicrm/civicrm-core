47c31bf93e7ac567146d5cd905f6085e049e9e6e
ray-wright <34744834+ray-wright@users.noreply.github.com>
2019-10-25 15:39:47 -0400

    Add orderBy function
    Updates reference to the getContactIDsFromComponent and adds order by function to order array by case id.

--- CRM/Case/Form/Task.php
+++ CRM/Case/Form/Task.php
@@ -44,11 +44,20 @@ class CRM_Case_Form_Task extends CRM_Core_Form_Task {
    * @inheritDoc
    */
   public function setContactIDs() {
-    $this->_contactIds = CRM_Core_DAO::getContactIDsFromComponent($this->_entityIds,
+    $this->_contactIds = CRM_Core_Form_Task::getContactIDsFromComponent($this->_entityIds,
       'civicrm_case_contact', 'case_id'
     );
   }
 
+  // order by case id 
+  public function orderBy() {
+    $order_array = 'ORDER BY ';
+    foreach ($this->_entityIds as $item) {
+       $order_array .= case_id . ' = ' . $item . ' DESC,';
+     }
+    return $order_array = trim($order_array, ',');
+  }
+  
   /**
    * Get the query mode (eg. CRM_Core_BAO_Query::MODE_CASE)
    *
