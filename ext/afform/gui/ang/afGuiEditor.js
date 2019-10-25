a2854e2e50160f4a04b593da6559ea5fd4f8f1ee
Coleman Watts <coleman@civicrm.org>
2019-10-25 12:01:48 -0400

    Horizontal tabs

--- ext/afform/gui/ang/afGuiEditor.js
+++ ext/afform/gui/ang/afGuiEditor.js
@@ -95,4 +95,48 @@
     return indexBy ? _.indexBy(items, indexBy) : items;
   }
 
+  // Editable titles using ngModel & html5 contenteditable
+  angular.module('afGuiEditor').directive("afGuiEditable", function() {
+    return {
+      restrict: "A",
+      require: "ngModel",
+      link: function(scope, element, attrs, ngModel) {
+        var ts = CRM.ts();
+
+        function read() {
+          var htmlVal = element.html();
+          if (!htmlVal) {
+            htmlVal = ts('Unnamed');
+            element.html(htmlVal);
+          }
+          ngModel.$setViewValue(htmlVal);
+        }
+
+        ngModel.$render = function() {
+          element.html(ngModel.$viewValue || ' ');
+        };
+
+        // Special handling for enter and escape keys
+        element.on('keydown', function(e) {
+          // Enter: prevent line break and save
+          if (e.which === 13) {
+            e.preventDefault();
+            element.blur();
+          }
+          // Escape: undo
+          if (e.which === 27) {
+            element.html(ngModel.$viewValue || ' ');
+            element.blur();
+          }
+        });
+
+        element.on("blur change", function() {
+          scope.$apply(read);
+        });
+
+        element.attr('contenteditable', 'true').addClass('crm-editable-enabled');
+      }
+    };
+  });
+
 })(angular, CRM.$, CRM._);
