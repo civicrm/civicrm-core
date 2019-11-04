9113bb82511126cbf4993457fdaedd52588f5af0
eileen <emcnaughton@wikimedia.org>
2019-11-04 13:59:26 +1300

    Fix exception thrown to std CRM_Core_Exception

--- CRM/Core/BAO/CustomField.php
+++ CRM/Core/BAO/CustomField.php
@@ -1064,14 +1064,15 @@ class CRM_Core_BAO_CustomField extends CRM_Core_DAO_CustomField {
    * @param int $entityId
    *
    * @return string
-   * @throws \Exception
+   *
+   * @throws \CRM_Core_Exception
    */
   public static function displayValue($value, $field, $entityId = NULL) {
     $field = is_array($field) ? $field['id'] : $field;
     $fieldId = is_object($field) ? $field->id : (int) str_replace('custom_', '', $field);
 
     if (!$fieldId) {
-      throw new Exception('CRM_Core_BAO_CustomField::displayValue requires a field id');
+      throw new CRM_Core_Exception('CRM_Core_BAO_CustomField::displayValue requires a field id');
     }
 
     if (!is_a($field, 'CRM_Core_BAO_CustomField')) {
