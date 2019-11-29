1ec08f097c2dd7afd981fd3950d3a86161a04705
eileen <emcnaughton@wikimedia.org>
2019-11-29 20:44:23 +1300

    [NFC] Cleanup comments, string comparision in grant search

--- CRM/Grant/Selector/Search.php
+++ CRM/Grant/Selector/Search.php
@@ -387,7 +387,9 @@ class CRM_Grant_Selector_Search extends CRM_Core_Selector_Base implements CRM_Co
   }
 
   /**
-   * @return string
+   * Get query.
+   *
+   * @return CRM_Contact_BAO_Query
    */
   public function &getQuery() {
     return $this->_query;
