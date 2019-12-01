94dd9f6ffea2479e4cabb5bcd38a8729d39ead67
Seamus Lee <seamuslee001@gmail.com>
2019-12-02 07:17:09 +1100

    dev/core#1434 Fix table alias used in select when ouputting as a chart

--- CRM/Report/Form/Mailing/Clicks.php
+++ CRM/Report/Form/Mailing/Clicks.php
@@ -238,7 +238,7 @@ class CRM_Report_Form_Mailing_Clicks extends CRM_Report_Form {
     }
 
     if (!empty($this->_params['charts'])) {
-      $select[] = "COUNT({$this->_aliases['civicrm_event_trackable_url_open']}.id) as civicrm_mailing_click_count";
+      $select[] = "COUNT({$this->_aliases['civicrm_mailing_event_trackable_url_open']}.id) as civicrm_mailing_click_count";
       $this->_columnHeaders["civicrm_mailing_click_count"]['title'] = ts('Click Count');
     }
 
