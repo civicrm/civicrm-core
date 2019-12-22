civicrm/civicrm-core

commit b1b7f4e35bbe566d9efccac1055d311245420974

Use the prepareParamsForPaymentProcessor function in more places

CRM/Event/Form/Registration/Confirm.php
@@ -90,15 +90,8 @@ class CRM_Event_Form_Registration_Confirm extends CRM_Event_Form_Registration {
     if (!$this->preProcessExpress()) {
       //process only primary participant params.
       $registerParams = $this->_params[0];
-      if (isset($registerParams["billing_state_province_id-{$this->_bltID}"])
-        && $registerParams["billing_state_province_id-{$this->_bltID}"]
-      ) {
-        $registerParams["billing_state_province-{$this->_bltID}"] = CRM_Core_PseudoConstant::stateProvinceAbbreviation($registerParams["billing_state_province_id-{$this->_bltID}"]);
-      }
+      $registerParams = $this->prepareParamsForPaymentProcessor($registerParams);
 
-      if (isset($registerParams["billing_country_id-{$this->_bltID}"]) && $registerParams["billing_country_id-{$this->_bltID}"]) {
-        $registerParams["billing_country-{$this->_bltID}"] = CRM_Core_PseudoConstant::countryIsoCode($registerParams["billing_country_id-{$this->_bltID}"]);
-      }
       if (isset($registerParams['credit_card_exp_date'])) {
         $registerParams['year'] = CRM_Core_Payment_Form::getCreditCardExpirationYear($registerParams);
         $registerParams['month'] = CRM_Core_Payment_Form::getCreditCardExpirationMonth($registerParams);

CRM/Event/Form/Registration/Register.php
@@ -1105,17 +1105,8 @@ class CRM_Event_Form_Registration_Register extends CRM_Event_Form_Registration {
       $this->set('contributeMode', 'direct');
 
       // This code is duplicated multiple places and should be consolidated.
-      if (isset($params["state_province_id-{$this->_bltID}"]) &&
-        $params["state_province_id-{$this->_bltID}"]
-      ) {
-        $params["state_province-{$this->_bltID}"] = CRM_Core_PseudoConstant::stateProvinceAbbreviation($params["state_province_id-{$this->_bltID}"]);
-      }
+      $params = $this->prepareParamsForPaymentProcessor($params);
 
-      if (isset($params["country_id-{$this->_bltID}"]) &&
-        $params["country_id-{$this->_bltID}"]
-      ) {
-        $params["country-{$this->_bltID}"] = CRM_Core_PseudoConstant::countryIsoCode($params["country_id-{$this->_bltID}"]);
-      }
       if (isset($params['credit_card_exp_date'])) {
         $params['year'] = CRM_Core_Payment_Form::getCreditCardExpirationYear($params);
         $params['month'] = CRM_Core_Payment_Form::getCreditCardExpirationMonth($params);

CRM/Event/Form/Participant.php
@@ -1228,9 +1228,7 @@ class CRM_Event_Form_Participant extends CRM_Contribute_Form_AbstractEditPayment
 
     if ($this->_mode) {
       // add all the additional payment params we need
-      $this->_params["state_province-{$this->_bltID}"] = $this->_params["billing_state_province-{$this->_bltID}"] = CRM_Core_PseudoConstant::stateProvinceAbbreviation($this->_params["billing_state_province_id-{$this->_bltID}"]);
-      $this->_params["country-{$this->_bltID}"] = $this->_params["billing_country-{$this->_bltID}"] = CRM_Core_PseudoConstant::countryIsoCode($this->_params["billing_country_id-{$this->_bltID}"]);
-
+      $this->_params = $this->prepareParamsForPaymentProcessor($this->_params);
       $this->_params['amount'] = $params['fee_amount'];
       $this->_params['amount_level'] = $params['amount_level'];
       $this->_params['currencyID'] = $config->defaultCurrency;

commit 1a093d07385739937b999dd9160d65ef38079d20

dev/event#25 Fix regression on billing name overwrite
https://lab.civicrm.org/dev/event/issues/25 was caused by https://github.com/civicrm/civicrm-core/commit/b1b7f4e35bbe566d9efccac1055d311245420974
where it started overwriting the name in that function.

In digging in the code I found that it seems that as long as we call this function before the processor all is
good so we should call right before the processor rather than broader contamination.

Also note ideally the settings bag would be used.

CRM/Event/Form/Registration/Confirm.php
@@ -70,17 +70,7 @@ class CRM_Event_Form_Registration_Confirm extends CRM_Event_Form_Registration {
       $this->assign('hookDiscount', $this->_params[0]['discount']);
     }
 
-    if (!$this->preProcessExpress()) {
-      //process only primary participant params.
-      $registerParams = $this->_params[0];
-      $registerParams = $this->prepareParamsForPaymentProcessor($registerParams);
-
-      if ($this->_values['event']['is_monetary']) {
-        $registerParams['currencyID'] = $this->_params[0]['currencyID'];
-      }
-      //assign back primary participant params.
-      $this->_params[0] = $registerParams;
-    }
+    $this->preProcessExpress();
 
     if ($this->_values['event']['is_monetary']) {
       $this->_params[0]['invoiceID'] = $this->get('invoiceID');
@@ -1294,7 +1284,8 @@ class CRM_Event_Form_Registration_Confirm extends CRM_Event_Form_Registration {
    */
   private function processPayment($payment, $value) {
     try {
-      $result = $payment->doPayment($value, 'event');
+      $params = $this->prepareParamsForPaymentProcessor($value);
+      $result = $payment->doPayment($params, 'event');
       return [$result, $value];
     }
     catch (\Civi\Payment\Exception\PaymentProcessorException $e) {

CRM/Event/Form/Registration/Register.php
@@ -1069,9 +1069,6 @@ class CRM_Event_Form_Registration_Register extends CRM_Event_Form_Registration {
       // assumptions we are working to remove.
       $this->set('contributeMode', 'direct');
 
-      // This code is duplicated multiple places and should be consolidated.
-      $params = $this->prepareParamsForPaymentProcessor($params);
-
       if ($this->_values['event']['is_monetary']) {
         $params['currencyID'] = $config->defaultCurrency;
         $params['invoiceID'] = $invoiceID;
@@ -1119,6 +1116,8 @@ class CRM_Event_Form_Registration_Register extends CRM_Event_Form_Registration {
         $params['invoiceID'] = $invoiceID;
 
         $params['component'] = 'event';
+        // This code is duplicated multiple places and should be consolidated.
+        $params = $this->prepareParamsForPaymentProcessor($params);
         $this->handlePreApproval($params);
       }
       elseif ($this->_paymentProcessor &&