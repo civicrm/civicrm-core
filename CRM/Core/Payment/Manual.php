c9058b9bd356cd9ebdb56a3896f7611e542d296c
Matthew Wire <mjw@mjwconsult.co.uk>
2020-01-13 10:30:50 +0000

    Explicitly support test and live mode for manual processor

--- CRM/Core/Payment/Manual.php
+++ CRM/Core/Payment/Manual.php
@@ -161,6 +161,24 @@ class CRM_Core_Payment_Manual extends CRM_Core_Payment {
   }
 
   /**
+   * Are live payments supported - e.g dummy doesn't support this.
+   *
+   * @return bool
+   */
+  protected function supportsLiveMode() {
+    return TRUE;
+  }
+
+  /**
+   * Are test payments supported.
+   *
+   * @return bool
+   */
+  protected function supportsTestMode() {
+    return TRUE;
+  }
+
+  /**
    * Declare that more than one payment can be processed at once.
    *
    * @return bool
