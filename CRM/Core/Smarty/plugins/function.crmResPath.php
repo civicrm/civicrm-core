502492c2bcad65c4a1e77f9a2b9595844a39dcb4
Tim Otten <totten@civicrm.org>
2020-01-16 01:21:20 -0800

    crmResURL, crmResPath - Allow full path expressions
    Before
    ------
    
    In Smarty, there is no way to request values from `Civi::paths()`.
    
    After
    -----
    
    In Smarty, you can request values from `Civi::paths()` using these notations:
    
    ```
    {crmResURL expr="[civicrm.root]/foo"}
    {crmResPath expr="[civicrm.root]/foo"}
    ```

--- CRM/Core/Smarty/plugins/function.crmResPath.php
+++ CRM/Core/Smarty/plugins/function.crmResPath.php
@@ -21,14 +21,21 @@
  * Determine the path of a resource file
  *
  * @param array $params
+ *   Identify the resource by either 'ext'+'file' or 'expr'.
+ *
  *   Array with keys:
  *   - ext: string, extension name. see CRM_Core_Resources::getPath
  *   - file: string, relative file path. see CRM_Core_Resources::getPath
+ *   - expr: string, a dynamic path expression. See: \Civi\Core\Paths::getPath()
  * @param CRM_Core_Smarty $smarty
  *
  * @return string
  */
 function smarty_function_crmResPath($params, &$smarty) {
+  if (!empty($params['expr'])) {
+    return Civi::paths()->getPath($params['expr']);
+  }
+
   $res = CRM_Core_Resources::singleton();
   if (!array_key_exists('ext', $params)) {
     $params['ext'] = 'civicrm';
--- CRM/Core/Smarty/plugins/function.crmResURL.php
+++ CRM/Core/Smarty/plugins/function.crmResURL.php
@@ -21,14 +21,21 @@
  * Determine the URL of a resource file
  *
  * @param array $params
+ *   Identify the resource by either 'ext'+'file' or 'expr'.
+ *
  *   Array with keys:
  *   - ext: string, extension name. see CRM_Core_Resources::getUrl
  *   - file: string, relative file path. see CRM_Core_Resources::getUrl
+ *   - expr: string, a dynamic path expression. See: \Civi\Core\Paths::getUrl()
  * @param CRM_Core_Smarty $smarty
  *
  * @return string
  */
 function smarty_function_crmResURL($params, &$smarty) {
+  if (!empty($params['expr'])) {
+    return Civi::paths()->getUrl($params['expr'], 'absolute');
+  }
+
   $res = CRM_Core_Resources::singleton();
   if (!array_key_exists('file', $params)) {
     $params['file'] = NULL;
