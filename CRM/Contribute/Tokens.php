bd282653553268ef62c7cb3cddd8d16307f75cc9
Matthew Wire <mjw@mjwconsult.co.uk>
2020-02-02 16:31:34 +0000

    Support contribution tokens using TokenProcessor

--- CRM/Contribute/Tokens.php
+++ CRM/Contribute/Tokens.php
@@ -20,6 +20,40 @@
  */
 class CRM_Contribute_Tokens extends \Civi\Token\AbstractTokenSubscriber {
 
+  use CRM_Core_TokenTrait;
+
+  /**
+   * @return string
+   */
+  private function getEntityName(): string {
+    return 'contribution';
+  }
+
+  /**
+   * @return string
+   */
+  private function getEntityTableName(): string {
+    return 'civicrm_contribution';
+  }
+
+  /**
+   * @return string
+   */
+  private function getEntityContextSchema(): string {
+    return 'contributionId';
+  }
+
+  /**
+   * Mapping from tokenName to api return field
+   * Use lists since we might need multiple fields
+   *
+   * @var array
+   */
+  private static $fieldMapping = [
+    'contribution_id' => ['id'],
+    'contribution_cancel_date' => ['cancel_date'],
+  ];
+
   /**
    * Get a list of tokens whose name and title match the DB fields.
    * @return array
@@ -58,41 +92,12 @@ class CRM_Contribute_Tokens extends \Civi\Token\AbstractTokenSubscriber {
   }
 
   /**
-   * Class constructor.
-   */
-  public function __construct() {
-    $tokens = CRM_Utils_Array::subset(
-      CRM_Utils_Array::collect('title', CRM_Contribute_DAO_Contribution::fields()),
-      $this->getPassthruTokens()
-    );
-    $tokens['id'] = ts('Contribution ID');
-    $tokens['payment_instrument'] = ts('Payment Instrument');
-    $tokens['source'] = ts('Contribution Source');
-    $tokens['status'] = ts('Contribution Status');
-    $tokens['type'] = ts('Financial Type');
-    $tokens = array_merge($tokens, CRM_Utils_Token::getCustomFieldTokens('Contribution'));
-    parent::__construct('contribution', $tokens);
-  }
-
-  /**
-   * Check if the token processor is active.
-   *
-   * @param \Civi\Token\TokenProcessor $processor
-   *
-   * @return bool
-   */
-  public function checkActive(\Civi\Token\TokenProcessor $processor) {
-    return !empty($processor->context['actionMapping'])
-      && $processor->context['actionMapping']->getEntity() === 'civicrm_contribution';
-  }
-
-  /**
    * Alter action schedule query.
    *
    * @param \Civi\ActionSchedule\Event\MailingQueryEvent $e
    */
   public function alterActionScheduleQuery(\Civi\ActionSchedule\Event\MailingQueryEvent $e) {
-    if ($e->mapping->getEntity() !== 'civicrm_contribution') {
+    if ($e->mapping->getEntity() !== $this->getEntityTableName()) {
       return;
     }
 
@@ -108,24 +113,84 @@ class CRM_Contribute_Tokens extends \Civi\Token\AbstractTokenSubscriber {
   /**
    * @inheritDoc
    */
+  public function prefetch(\Civi\Token\Event\TokenValueEvent $e) {
+    // Find all the entity IDs
+    $entityIds
+      = $e->getTokenProcessor()->getContextValues('actionSearchResult', 'entityID')
+      + $e->getTokenProcessor()->getContextValues($this->getEntityContextSchema());
+
+    if (!$entityIds) {
+      return NULL;
+    }
+
+    // Get data on all activities for basic and customfield tokens
+    $entities = civicrm_api3('Contribution', 'get', [
+      'id' => ['IN' => $entityIds],
+      'options' => ['limit' => 0],
+      'return' => self::getReturnFields($this->activeTokens),
+    ]);
+    $prefetch['contribution'] = $entities['values'];
+
+    return $prefetch;
+  }
+
+  /**
+   * @inheritDoc
+   */
   public function evaluateToken(\Civi\Token\TokenRow $row, $entity, $field, $prefetch = NULL) {
-    $actionSearchResult = $row->context['actionSearchResult'];
-    $fieldValue = isset($actionSearchResult->{"contrib_$field"}) ? $actionSearchResult->{"contrib_$field"} : NULL;
+    // maps token name to api field
+    $mapping = [
+      'contribution_id' => 'id',
+    ];
+
+    // Get EntityID either from actionSearchResult (for scheduled reminders) if exists
+    $entityId = $row->context['actionSearchResult']->entityID ?? $row->context[$this->getEntityContextSchema()];
+
+    $entityObject = (object) $prefetch['contribution'][$entityId];
+    $fieldValue = $entityObject->$field;
 
     $aliasTokens = $this->getAliasTokens();
     if (in_array($field, ['total_amount', 'fee_amount', 'net_amount'])) {
       return $row->format('text/plain')->tokens($entity, $field,
-        \CRM_Utils_Money::format($fieldValue, $actionSearchResult->contrib_currency));
+        \CRM_Utils_Money::format($fieldValue, $entityObject->currency));
+    }
+    elseif (isset($mapping[$field]) && (isset($entityObject->{$mapping[$field]}))) {
+      $row->tokens($entity, $field, $entityObject->{$mapping[$field]});
+    }
+    elseif ($field === 'contribution_status_id') {
+      $row->tokens($entity, $field, $entityObject->contribution_status);
     }
     elseif (isset($aliasTokens[$field])) {
       $row->dbToken($entity, $field, 'CRM_Contribute_BAO_Contribution', $aliasTokens[$field], $fieldValue);
     }
     elseif ($cfID = \CRM_Core_BAO_CustomField::getKeyID($field)) {
-      $row->customToken($entity, $cfID, $actionSearchResult->entity_id);
+      $row->customToken($entity, $cfID, $entityId);
     }
     else {
       $row->dbToken($entity, $field, 'CRM_Contribute_BAO_Contribution', $field, $fieldValue);
     }
+  }
+
+  /**
+   * Get the basic tokens provided.
+   *
+   * @return array token name => token label
+   */
+  protected function getBasicTokens() {
+    if (!isset($this->basicTokens)) {
+      $tokens = CRM_Utils_Array::subset(
+        CRM_Utils_Array::collect('title', CRM_Contribute_DAO_Contribution::fields()),
+        $this->getPassthruTokens()
+      );
+      $tokens['contribution_id'] = ts('Contribution ID');
+      $tokens['payment_instrument_id'] = ts('Payment Instrument');
+      $tokens['contribution_source'] = ts('Contribution Source');
+      $tokens['contribution_status_id'] = ts('Contribution Status');
+      $tokens['financial_type_id'] = ts('Financial Type');
+      $this->basicTokens = $tokens;
+    }
+
+    return $this->basicTokens;
   }
 
 }
