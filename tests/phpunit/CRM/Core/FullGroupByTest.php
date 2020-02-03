8d11c1c80900f545a7833f1b9f87c3416ca5308b
demeritcowboy <demeritcowboy@hotmail.com>
2020-02-03 11:48:49 -0500

    wip

--- tests/phpunit/CRM/Core/FullGroupByTest.php
+++ tests/phpunit/CRM/Core/FullGroupByTest.php
@@ -20,6 +20,20 @@ class CRM_Core_FullGroupByTest extends CiviUnitTestCase {
     $dao = CRM_Core_DAO::executeQuery("SELECT @@sql_mode as foo");
     $dao->fetch();
     echo "SQL MODE: {$dao->foo}";
+
+    $dao = CRM_Core_DAO::executeQuery("SELECT
+    DISTINCT ( price_set_id ) as id, s.title
+    FROM
+    civicrm_price_set s
+    INNER JOIN civicrm_price_field f ON f.price_set_id = s.id
+    INNER JOIN civicrm_price_field_value v ON v.price_field_id = f.id
+    WHERE
+    is_quick_config = 0  AND s.is_active = 1  AND s.financial_type_id IN (3,1,4,2) AND v.financial_type_id IN (3,1,4,2)
+    GROUP BY s.id");
+    echo "\nPrice sets found:\n";
+    while ($dao->fetch()) {
+      echo "\nTitle: $dao->title\n";
+    }
   }
 
 }
