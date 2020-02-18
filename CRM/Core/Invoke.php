bc95d82c443a61917298fcd002ba8bd478e9cb12
Patrick Figel <pfigel@greenpeace.org>
2020-02-18 20:44:11 +0100

    security/core#60 - Fix PHP Object Injection via Phar Deserialization
    This mitigates Phar deserialization vulnerabilities by registering an
    alternative Phar stream wrapper that filters out insecure Phar files.
    
    PHP makes it possible to trigger Object Injection vulnerabilities by using
    a side-effect of the phar:// stream wrapper that unserializes Phar
    metadata. To mitigate this vulnerability, projects such as TYPO3 and Drupal
    have implemented an alternative Phar stream wrapper that disallows
    inclusion of phar files based on certain parameters. This change implements
    a similar approach for Civi in environments where the vulnerability isn't
    mitigated by the CMS.
    
    Fixes security/core#60

--- CRM/Core/Invoke.php
+++ CRM/Core/Invoke.php
@@ -151,6 +151,47 @@ class CRM_Core_Invoke {
   }
 
   /**
+   * Register an alternative phar:// stream wrapper to filter out insecure Phars
+   *
+   * PHP makes it possible to trigger Object Injection vulnerabilities by using
+   * a side-effect of the phar:// stream wrapper that unserializes Phar
+   * metadata. To mitigate this vulnerability, projects such as TYPO3 and Drupal
+   * have implemented an alternative Phar stream wrapper that disallows
+   * inclusion of phar files based on certain parameters.
+   *
+   * This code attempts to register the TYPO3 Phar stream wrapper using the
+   * interceptor defined in \Civi\Core\Security\PharExtensionInterceptor. In an
+   * environment where the stream wrapper was already registered via
+   * \TYPO3\PharStreamWrapper\Manager (i.e. Drupal), this code does not do
+   * anything. In other environments (e.g. WordPress, at the time of this
+   * writing), the TYPO3 library is used to register the interceptor to mitigate
+   * the vulnerability.
+   */
+  private static function registerPharHandler() {
+    try {
+      // try to get the existing stream wrapper, registered e.g. by Drupal
+      \TYPO3\PharStreamWrapper\Manager::instance();
+    }
+    catch (\LogicException $e) {
+      if ($e->getCode() === 1535189872) {
+        // no phar stream wrapper was registered by \TYPO3\PharStreamWrapper\Manager.
+        // This means we're probably not on Drupal and need to register our own.
+        \TYPO3\PharStreamWrapper\Manager::initialize(
+          (new \TYPO3\PharStreamWrapper\Behavior())
+            ->withAssertion(new \Civi\Core\Security\PharExtensionInterceptor())
+        );
+        if (in_array('phar', stream_get_wrappers())) {
+          stream_wrapper_unregister('phar');
+          stream_wrapper_register('phar', \TYPO3\PharStreamWrapper\PharStreamWrapper::class);
+        }
+      } else {
+        // this is not an exception we can handle
+        throw $e;
+      }
+    }
+  }
+
+  /**
    * Given a menu item, call the appropriate controller and return the response
    *
    * @param array $item
@@ -160,6 +201,8 @@ class CRM_Core_Invoke {
   public static function runItem($item) {
     $ids = new CRM_Core_IDS();
     $ids->check($item);
+
+    self::registerPharHandler();
 
     $config = CRM_Core_Config::singleton();
     if ($config->userFramework == 'Joomla' && $item) {
