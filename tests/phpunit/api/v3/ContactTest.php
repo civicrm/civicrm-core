e15bd6d19a1532a7e7188a7fd3f4e723d3a0d339
Seamus Lee <seamuslee001@gmail.com>
2020-03-04 07:48:35 +1100

    [MOSS] CIV-01-020 Validate value in the query building logic for privacy flag fields

--- tests/phpunit/api/v3/ContactTest.php
+++ tests/phpunit/api/v3/ContactTest.php
@@ -4664,4 +4664,36 @@ class api_v3_ContactTest extends CiviUnitTestCase {
     return [$contact1, $contact2];
   }
 
+  public function versionAndPrivacyOption() {
+    $version = [3, 4];
+    $fields = ['do_not_mail', 'do_not_email', 'do_not_sms', 'is_opt_out', 'do_not_trade'];
+    $tests = [];
+    foreach ($fields as $field) {
+      foreach ($version as $v) {
+        $tests[] = [$v, 1, $field, 1];
+        $tests[] = [$v, 0, $field, 3];
+        $tests[] = [$v, ['!=' => 1], $field, 3];
+        $tests[] = [$v, ['!=' => 0], $field, 1];
+      }
+    }
+    return $tests;
+  }
+
+ /**
+   * CRM-14743 - test api respects search operators.
+   *
+   * @param int $version
+   *
+   * @dataProvider versionAndPrivacyOption
+   */
+  public function testGetContactsByPrivacyFlag($version, $query, $field, $expected) {
+    $this->_apiversion = $version;
+    $contact1 = $this->individualCreate();
+    $contact2 = $this->individualCreate([$field => 1]);
+    $contact = $this->callAPISuccess('Contact', 'get', [$field => $query]);
+    $this->assertEquals($expected, $contact['count']);
+    $this->callAPISuccess('Contact', 'delete', ['id' => $contact1, 'skip_undelete' => 1]);
+    $this->callAPISuccess('Contact', 'delete', ['id' => $contact2, 'skip_undelete' => 1]);
+  }
+
 }
