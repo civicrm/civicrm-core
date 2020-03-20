89af34203f42d22bdfc752df9a02c50b28ca6933
eileen <emcnaughton@wikimedia.org>
2020-03-20 17:00:32 +1300

    [REF] Remove redundant call to build permissions
    Per the comments I added to the function the output of this permissions clause is overwritten. I stepped through
    this in the process of reviewing the datepicker PR & confirmed it

--- CRM/Report/Form/Contribute/Summary.php
+++ CRM/Report/Form/Contribute/Summary.php
@@ -742,14 +742,6 @@ ROUND(AVG({$this->_aliases['civicrm_contribution_soft']}.amount), 2) as civicrm_
   }
 
   /**
-   * Post process function.
-   */
-  public function postProcess() {
-    $this->buildACLClause($this->_aliases['civicrm_contact']);
-    parent::postProcess();
-  }
-
-  /**
    * Build chart.
    *
    * @param array $rows
