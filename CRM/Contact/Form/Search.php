180a23c1dbbec09ac3e7849898cf09ae7c607e55
Seamus Lee <seamuslee001@gmail.com>
2020-04-29 17:53:46 +1000

    Security/core#61 Limit Access to update smart group task to only if the logged in user has edit groups permission.
    Put a permission restriction on loading page without manage groups permission when saved search id is specified in the URL

--- CRM/Contact/Form/Search.php
+++ CRM/Contact/Form/Search.php
@@ -529,6 +529,10 @@ class CRM_Contact_Form_Search extends CRM_Core_Form_Search {
     $this->_componentMode = CRM_Utils_Request::retrieve('component_mode', 'Positive', $this, FALSE, CRM_Contact_BAO_Query::MODE_CONTACTS, $_REQUEST);
     $this->_operator = CRM_Utils_Request::retrieve('operator', 'String', $this, FALSE, CRM_Contact_BAO_Query::SEARCH_OPERATOR_AND, 'REQUEST');
 
+    if (!empty($this->_ssID) && !CRM_Core_Permission::check('edit groups')) {
+      CRM_Core_Error::statusBounce(ts('You do not have permission to modify smart groups'));
+    }
+
     /**
      * set the button names
      */
