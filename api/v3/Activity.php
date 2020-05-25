2c9e83fafa1c322e60c39defd2662a99f839085c
Matthew Wire <mjw@mjwconsult.co.uk>
2020-05-25 15:29:03 +0100

    Set default using spec for activity_date_time

--- api/v3/Activity.php
+++ api/v3/Activity.php
@@ -202,6 +202,8 @@ function _civicrm_api3_activity_create_spec(&$params) {
     'FKApiName' => 'Case',
   ];
 
+  $params['activity_date_time']['api.default'] = date('YmdHis');
+
 }
 
 /**
@@ -737,13 +739,6 @@ function _civicrm_api3_activity_check_params(&$params) {
   // needs testing
   if (isset($params['duration_minutes']) && !is_numeric($params['duration_minutes'])) {
     throw new API_Exception('Invalid Activity Duration (in minutes)');
-  }
-
-  //if adding a new activity & date_time not set make it now
-  // this should be managed by the wrapper layer & setting ['api.default'] in speces
-  // needs testing
-  if (empty($params['id']) && empty($params['activity_date_time'])) {
-    $params['activity_date_time'] = CRM_Utils_Date::processDate(date('Y-m-d H:i:s'));
   }
 
   return NULL;
