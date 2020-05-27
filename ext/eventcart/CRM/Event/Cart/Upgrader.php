42ca563bc5a9c4956e850d7e3daddf39a7376b3a
Matthew Wire <mjw@mjwconsult.co.uk>
2020-05-27 17:14:40 +0100

    Add upgrader functions

---
+++ ext/eventcart/CRM/Event/Cart/Upgrader.php
@@ -0,0 +1,12 @@
+<?php
+use CRM_Event_Cart_ExtensionUtil as E;
+
+/**
+ * Collection of upgrade steps.
+ */
+class CRM_Event_Cart_Upgrader extends CRM_Event_Cart_Upgrader_Base {
+
+  // By convention, functions that look like "function upgrade_NNNN()" are
+  // upgrade tasks. They are executed in order (like Drupal's hook_update_N).
+
+}
--- ext/eventcart/eventcart.civix.php
+++ ext/eventcart/eventcart.civix.php
@@ -193,9 +193,8 @@ function _eventcart_civix_civicrm_disable() {
  * @param $op string, the type of operation being performed; 'check' or 'enqueue'
  * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
  *
- * @return mixed
- *   based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *   for 'enqueue', returns void
+ * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ *                for 'enqueue', returns void
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
  */
@@ -256,7 +255,6 @@ function _eventcart_civix_find_files($dir, $pattern) {
   }
   return $result;
 }
-
 /**
  * (Delegated) Implements hook_civicrm_managed().
  *
@@ -472,6 +470,8 @@ function _eventcart_civix_civicrm_alterSettingsFolders(&$metaDataFolders = NULL)
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
  */
+
 function _eventcart_civix_civicrm_entityTypes(&$entityTypes) {
-  $entityTypes = array_merge($entityTypes, array());
+  $entityTypes = array_merge($entityTypes, array (
+  ));
 }
