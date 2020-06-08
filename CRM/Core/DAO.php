2d162bf85615e4a45b2642110a7f9653913f57e2
eileen <emcnaughton@wikimedia.org>
2020-06-08 13:34:55 +1200

    Deprecate error handling bypasses
    I don't believe these are passed in anywhere but it's impossible to be sure so
    deprecation makes sense

--- CRM/Core/DAO.php
+++ CRM/Core/DAO.php
@@ -1515,10 +1515,12 @@ FROM   civicrm_domain
       $dao = new CRM_Core_DAO();
     }
     else {
+      CRM_Core_Error::deprecatedFunctionWarning('passing in DAOName is deprecated');
       $dao = new $daoName();
     }
 
     if ($trapException) {
+      CRM_Core_Error::deprecatedFunctionWarning('calling functions should handle exceptions');
       $errorScope = CRM_Core_TemporaryErrorScope::ignoreException();
     }
 
@@ -1622,6 +1624,9 @@ FROM   civicrm_domain
    * @throws CRM_Core_Exception
    */
   public static function composeQuery($query, $params = [], $abort = TRUE) {
+    if (!$abort) {
+      CRM_Core_Error::deprecatedFunctionWarning('calling functions should not bypass error handling');
+    }
     $tr = [];
     foreach ($params as $key => $item) {
       if (is_numeric($key)) {
