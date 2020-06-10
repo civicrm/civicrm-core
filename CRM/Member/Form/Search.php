41f09a1a8c4f4920c2bcc9f5befccd8762a14089
Monish Deb <deb.monish@gmail.com>
2020-06-10 11:45:29 +0530

    core#1805: Autocomplete-select custom field is not searchable

--- CRM/Member/Form/Search.php
+++ CRM/Member/Form/Search.php
@@ -222,8 +222,6 @@ class CRM_Member_Form_Search extends CRM_Core_Form_Search {
       $this->_formValues["member_test"] = 0;
     }
 
-    CRM_Core_BAO_CustomValue::fixCustomFieldValue($this->_formValues);
-
     $this->_queryParams = CRM_Contact_BAO_Query::convertFormValues($this->_formValues, 0, FALSE, NULL, $this->entityReferenceFields);
 
     $this->set('queryParams', $this->_queryParams);
