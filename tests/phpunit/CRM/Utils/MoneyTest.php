ca4cfe7a69ea53a41bc043d57c803fb94a1c3630
Seamus Lee <seamuslee001@gmail.com>
2020-06-14 14:24:36 +1000

    dev/translation#48 Implement Brick/Money to better handle currecny values
    Added in test case as per Eileen comment so we know we can handle large floats

--- tests/phpunit/CRM/Utils/MoneyTest.php
+++ tests/phpunit/CRM/Utils/MoneyTest.php
@@ -41,6 +41,7 @@ class CRM_Utils_MoneyTest extends CiviUnitTestCase {
       [2, 1, 'USD', 1],
       [0, 0, 'USD', 0],
       [1, 2, 'USD', -1],
+      [269.565217391, 1, 'USD', 268.57],
       [number_format(19.99, 2), number_format(20.00, 2), 'USD', number_format(-0.01, 2)],
       ['notanumber', 5.00, 'USD', NULL],
     ];
