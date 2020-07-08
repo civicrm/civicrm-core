2552305999f807864bbf678cd2a6af6394ed9f6d
Coleman Watts <coleman@civicrm.org>
2020-07-08 18:15:39 -0400

    Add search extension

---
+++ ext/search/CRM/Search/Upgrader.php
@@ -0,0 +1,36 @@
+<?php
+use CRM_Search_ExtensionUtil as E;
+
+/**
+ * Collection of upgrade steps.
+ */
+class CRM_Search_Upgrader extends CRM_Search_Upgrader_Base {
+
+  /**
+   * Add menu item when enabled.
+   */
+  public function enable() {
+    \Civi\Api4\Navigation::create()
+      ->setCheckPermissions(FALSE)
+      ->addValue('parent_id:name', 'Search')
+      ->addValue('label', E::ts('Build Search for...'))
+      ->addValue('name', 'build_search')
+      ->addValue('url', 'civicrm/search')
+      ->addValue('icon', 'crm-i fa-search-plus')
+      ->addValue('has_separator', 2)
+      ->addValue('weight', 99)
+      ->execute();
+  }
+
+  /**
+   * Delete menu item when disabled.
+   */
+  public function disable() {
+    \Civi\Api4\Navigation::delete()
+      ->setCheckPermissions(FALSE)
+      ->addWhere('name', '=', 'build_search')
+      ->addWhere('domain_id', '=', 'current_domain')
+      ->execute();
+  }
+
+}
---
+++ ext/search/search.php
@@ -0,0 +1,128 @@
+<?php
+
+require_once 'search.civix.php';
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
+ */
+function search_civicrm_config(&$config) {
+  _search_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
+ */
+function search_civicrm_xmlMenu(&$files) {
+  _search_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function search_civicrm_install() {
+  _search_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function search_civicrm_postInstall() {
+  _search_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
+ */
+function search_civicrm_uninstall() {
+  _search_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function search_civicrm_enable() {
+  _search_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
+ */
+function search_civicrm_disable() {
+  _search_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
+ */
+function search_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _search_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
+ */
+function search_civicrm_managed(&$entities) {
+  _search_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
+ */
+function search_civicrm_angularModules(&$angularModules) {
+  _search_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
+ */
+function search_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _search_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * Declare entity types provided by this module.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function search_civicrm_entityTypes(&$entityTypes) {
+  _search_civix_civicrm_entityTypes($entityTypes);
+}
+
+/**
+ * Implements hook_civicrm_thems().
+ */
+function search_civicrm_themes(&$themes) {
+  _search_civix_civicrm_themes($themes);
+}
