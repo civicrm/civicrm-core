58efbb45bce132156f5bd86288fde8f2b1858531
Coleman Watts <coleman@civicrm.org>
2020-07-30 12:38:15 -0400

    Improve docs for APIv4 Save action

--- Civi/Api4/Generic/DAOSaveAction.php
+++ Civi/Api4/Generic/DAOSaveAction.php
@@ -20,11 +20,7 @@
 namespace Civi\Api4\Generic;
 
 /**
- * Create or update one or more $ENTITIES.
- *
- * If creating more than one $ENTITY with similar values, use the `defaults` param.
- *
- * Set `reload` if you need the api to return complete records for each saved $ENTITY.
+ * @inheritDoc
  */
 class DAOSaveAction extends AbstractSaveAction {
   use Traits\DAOActionTrait;
