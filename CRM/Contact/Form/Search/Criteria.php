b54a8676c678de4d208c777b574660a78377a947
Monish Deb <deb.monish@gmail.com>
2020-07-31 17:17:26 +0530

    QA fixes

--- CRM/Contact/Form/Search/Criteria.php
+++ CRM/Contact/Form/Search/Criteria.php
@@ -28,9 +28,6 @@ class CRM_Contact_Form_Search_Criteria {
     self::setBasicSearchFields($form);
     $form->addElement('hidden', 'hidden_basic', 1);
 
-    $form->add('text', 'first_name', ts('First Name'), CRM_Core_DAO::getAttribute('CRM_Contact_DAO_Contact', 'first_name'));
-    $form->add('text', 'last_name', ts('Last Name'), CRM_Core_DAO::getAttribute('CRM_Contact_DAO_Contact', 'last_name'));
-
     if ($form->_searchOptions['contactType']) {
       $contactTypes = CRM_Contact_BAO_ContactType::getSelectElements();
 
@@ -252,6 +249,8 @@ class CRM_Contact_Form_Search_Criteria {
         'template_grouping' => 'basic',
         'template' => 'CRM/Contact/Form/Search/Criteria/Fields/sort_name.tpl',
       ],
+      'first_name' => ['template_grouping' => 'basic'],
+      'last_name' => ['template_grouping' => 'basic'],
       'email' => ['title' => ts('Complete OR Partial Email'), 'entity' => 'Email', 'template_grouping' => 'basic'],
       'contact_tags' => ['name' => 'contact_tags', 'type' => CRM_Utils_Type::T_INT, 'is_pseudofield' => TRUE, 'template_grouping' => 'basic'],
       'created_date' => ['name' => 'created_date', 'template_grouping' => 'changeLog'],
@@ -324,6 +323,8 @@ class CRM_Contact_Form_Search_Criteria {
     return [
       // For now an empty array is still left in place for ordering.
       'sort_name' => [],
+      'first_name' => [],
+      'last_name' => [],
       'email' => ['name' => 'email'],
       'contact_type' => ['name' => 'contact_type'],
       'group' => [
