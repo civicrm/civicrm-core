b6cda82a8a7a35e3d734dd31681bd267309f8008
Sunil Pawar <sunil@skvare.com>
2020-08-10 17:39:22 +0530

    dev/core#1943 add functionality to add civicrm log into Drupal access log

--- CRM/Core/Error.php
+++ CRM/Core/Error.php
@@ -586,6 +586,9 @@ class CRM_Core_Error extends PEAR_ErrorStack {
       if ($config->userSystem->is_drupal and function_exists('watchdog')) {
         watchdog('civicrm', '%message', ['%message' => $message], $priority ?? WATCHDOG_DEBUG);
       }
+      else if ($config->userSystem->is_drupal and CIVICRM_UF == 'Drupal8') {
+        \Drupal::logger('civicrm')->debug($message);
+      }
     }
 
     return $str;
