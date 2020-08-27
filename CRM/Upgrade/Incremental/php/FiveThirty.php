e7339d592be523cb2a591bc5d0a413b6f1b9836b
eileen <emcnaughton@wikimedia.org>
2020-08-28 10:49:32 +1200

    Shell Financial ACLs extension
    This is intended as a hidden extension while we work through the process of migrating core functionality into it.
    
    I'm expected there to be a bit of work to unravel this into hook interactions - but the core extension mechanism gives us that space

--- CRM/Upgrade/Incremental/php/FiveThirty.php
+++ CRM/Upgrade/Incremental/php/FiveThirty.php
@@ -65,8 +65,50 @@ class CRM_Upgrade_Incremental_php_FiveThirty extends CRM_Upgrade_Incremental_Bas
   //    // The above is an exception because 'Upgrade DB to %1: SQL' is generic & reusable.
   //  }
 
+  /**
+   * Upgrade function.
+   *
+   * @param string $rev
+   */
+  public function upgrade_5_30_alpha1($rev) {
+    $this->addTask(ts('Upgrade DB to %1: SQL', [1 => $rev]), 'runSql', $rev);
+    $this->addTask('Add core (required) extension Financial ACLs', 'installFinancialAcls');
+  }
+
   // public static function taskFoo(CRM_Queue_TaskContext $ctx, ...) {
   //   return TRUE;
   // }
+
+  /**
+   * Install financialacls extension.
+   *
+   * This feature is restructured as a core extension - which is primarily a code cleanup step.
+   *
+   * @param \CRM_Queue_TaskContext $ctx
+   *
+   * @return bool
+   *
+   * @throws \CiviCRM_API3_Exception
+   * @throws \CRM_Core_Exception
+   */
+  public static function installFinancialAcls(CRM_Queue_TaskContext $ctx) {
+    // Install via direct SQL manipulation. Note that:
+    // (1) This extension has no activation logic.
+    // (2) On new installs, the extension is activated purely via default SQL INSERT.
+    // (3) Caches are flushed at the end of the upgrade.
+    // ($) Over long term, upgrade steps are more reliable in SQL. API/BAO sometimes don't work mid-upgrade.
+    $insert = CRM_Utils_SQL_Insert::into('civicrm_extension')->row([
+      'type' => 'module',
+      'full_name' => 'financialacls',
+      'name' => 'financialacls',
+      'label' => 'Financial ACLs',
+      'file' => 'financialacls',
+      'schema_version' => NULL,
+      'is_active' => 1,
+    ]);
+    CRM_Core_DAO::executeQuery($insert->usingReplace()->toSQL());
+
+    return TRUE;
+  }
 
 }
