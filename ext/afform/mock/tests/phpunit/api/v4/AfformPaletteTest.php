colemanw/afform

Add skeletal APIs for AfformPalette and AfformTag

mock/tests/phpunit/api/v4/AfformPaletteTest.php
@@ -1,0 +1,24 @@
+<?php
+
+use Civi\Test\HeadlessInterface;
+use Civi\Test\TransactionalInterface;
+
+/**
+ * AfformPalette API Test
+ * @group headless
+ */
+class api_v4_AfformTest extends api_v4_AfformTestCase {
+
+  public function testGetPalette() {
+    $r = Civi\Api4\AfformPalette::get()
+      ->addWhere('id', '=', 'Parent:afl-name')
+      ->execute();
+    $this->assertEquals(1, $r->count());
+
+    $r = Civi\Api4\AfformPalette::get()
+      ->setLimit(10)
+      ->execute();
+    $this->assertTrue($r->count() > 1);
+  }
+
+}

civicrm/civicrm-core

ext/afform/mock/tests/phpunit/api/v4/AfformPaletteTest.php
@@ -1,0 +1,24 @@
+<?php
+
+use Civi\Test\HeadlessInterface;
+use Civi\Test\TransactionalInterface;
+
+/**
+ * AfformPalette API Test
+ * @group headless
+ */
+class api_v4_AfformPaletteTest extends api_v4_AfformTestCase {
+
+  public function testGetPalette() {
+    $r = Civi\Api4\AfformPalette::get()
+      ->addWhere('id', '=', 'Parent:afl-name')
+      ->execute();
+    $this->assertEquals(1, $r->count());
+
+    $r = Civi\Api4\AfformPalette::get()
+      ->setLimit(10)
+      ->execute();
+    $this->assertTrue($r->count() > 1);
+  }
+
+}