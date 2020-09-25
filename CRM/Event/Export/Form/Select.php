0a595a2ed9dd4a16ef794e69c8628efea581bbae
eileen <emcnaughton@wikimedia.org>
2020-09-25 12:16:58 +1200

    Separate export form classes

---
+++ CRM/Event/Export/Form/Select.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC. All rights reserved.                        |
+ |                                                                    |
+ | This work is published under the GNU AGPLv3 license with some      |
+ | permitted exceptions and without any warranty. For full license    |
+ | and copyright information, see https://civicrm.org/licensing       |
+ +--------------------------------------------------------------------+
+ */
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC https://civicrm.org/licensing
+ */
+
+/**
+ * This class gets the name of the file to upload
+ */
+class CRM_Event_Export_Form_Select extends CRM_Export_Form_Select {
+
+  /**
+   * Call the pre-processing function.
+   */
+  protected function callPreProcessing(): void {
+    CRM_Event_Form_Task::preProcessCommon($this);
+  }
+
+}
