9b057f1ed7f540d51f99f1a4fc0a580d896e9467
Coleman Watts <coleman@civicrm.org>
2020-10-06 20:56:49 -0400

    Search ext & APIv4 Explorer: Refactor clause from directive to component
    Improves compatibility with Angular 1.8; uses the more modern component system
    with less coupling and cleaner interfaces.

--- ang/api4Explorer/Explorer.js
+++ ang/api4Explorer/Explorer.js
@@ -1024,75 +1024,86 @@
     };
   });
 
-  angular.module('api4Explorer').directive('crmApi4Clause', function() {
-    return {
-      scope: {
-        data: '<crmApi4Clause'
-      },
-      templateUrl: '~/api4Explorer/Clause.html',
-      controller: function ($scope, $element, $timeout) {
-        var ts = $scope.ts = CRM.ts(),
-          ctrl = $scope.$ctrl = this;
-        this.conjunctions = {AND: ts('And'), OR: ts('Or'), NOT: ts('Not')};
-        this.operators = CRM.vars.api4.operators;
-        this.sortOptions = {
-          axis: 'y',
-          connectWith: '.api4-clause-group-sortable',
-          containment: $element.closest('.api4-clause-fieldset'),
-          over: onSortOver,
-          start: onSort,
-          stop: onSort
-        };
+  angular.module('api4Explorer').component('crmApi4Clause', {
+    bindings: {
+      fields: '<',
+      clauses: '<',
+      format: '@',
+      op: '@',
+      skip: '<',
+      isRequired: '<',
+      label: '@',
+      deleteGroup: '&'
+    },
+    templateUrl: '~/api4Explorer/Clause.html',
+    controller: function ($scope, $element, $timeout) {
+      var ts = $scope.ts = CRM.ts(),
+        ctrl = this;
+      this.conjunctions = {AND: ts('And'), OR: ts('Or'), NOT: ts('Not')};
+      this.operators = CRM.vars.api4.operators;
+      this.sortOptions = {
+        axis: 'y',
+        connectWith: '.api4-clause-group-sortable',
+        containment: $element.closest('.api4-clause-fieldset'),
+        over: onSortOver,
+        start: onSort,
+        stop: onSort
+      };
 
-        this.addGroup = function(op) {
-          $scope.data.clauses.push([op, []]);
-        };
+      this.$onInit = function() {
+        ctrl.hasParent = !!$element.attr('delete-group');
+      };
 
-        this.removeGroup = function() {
-          $scope.data.groupParent.splice($scope.data.groupIndex, 1);
-        };
+      this.addGroup = function(op) {
+        ctrl.clauses.push([op, []]);
+      };
 
-        function onSort(event, ui) {
-          $($element).closest('.api4-clause-fieldset').toggleClass('api4-sorting', event.type === 'sortstart');
-          $('.api4-input.form-inline').css('margin-left', '');
-        }
-
-        // Indent clause while dragging between nested groups
-        function onSortOver(event, ui) {
-          var offset = 0;
-          if (ui.sender) {
-            offset = $(ui.placeholder).offset().left - $(ui.sender).offset().left;
-          }
-          $('.api4-input.form-inline.ui-sortable-helper').css('margin-left', '' + offset + 'px');
-        }
-
-        this.addClause = function() {
-          $timeout(function() {
-            if (ctrl.newClause) {
-              $scope.data.clauses.push([ctrl.newClause, '=', '']);
-              ctrl.newClause = null;
-            }
-          });
-        };
-        $scope.$watch('data.clauses', function(values) {
-          // Iterate in reverse order so index doesn't get out-of-sync during splice
-          _.forEachRight(values, function(clause, index) {
-            // Remove empty values
-            if (index >= ($scope.data.skip  || 0)) {
-              if (typeof clause !== 'undefined' && !clause[0]) {
-                values.splice(index, 1);
-              }
-              // Add/remove value if operator allows for one
-              else if (typeof clause[1] === 'string' && _.contains(clause[1], 'NULL')) {
-                clause.length = 2;
-              } else if (typeof clause[1] === 'string' && clause.length === 2) {
-                clause.push('');
-              }
-            }
-          });
-        }, true);
+      function onSort(event, ui) {
+        $($element).closest('.api4-clause-fieldset').toggleClass('api4-sorting', event.type === 'sortstart');
+        $('.api4-input.form-inline').css('margin-left', '');
       }
-    };
+
+      // Indent clause while dragging between nested groups
+      function onSortOver(event, ui) {
+        var offset = 0;
+        if (ui.sender) {
+          offset = $(ui.placeholder).offset().left - $(ui.sender).offset().left;
+        }
+        $('.api4-input.form-inline.ui-sortable-helper').css('margin-left', '' + offset + 'px');
+      }
+
+      this.addClause = function() {
+        $timeout(function() {
+          if (ctrl.newClause) {
+            if (ctrl.skip && ctrl.clauses.length < ctrl.skip) {
+              ctrl.clauses.push(null);
+            }
+            ctrl.clauses.push([ctrl.newClause, '=', '']);
+            ctrl.newClause = null;
+          }
+        });
+      };
+
+      this.deleteRow = function(index) {
+        ctrl.clauses.splice(index, 1);
+      };
+
+      // Remove empty values
+      this.changeClauseField = function(clause, index) {
+        if (clause[0] === '') {
+          ctrl.deleteRow(index);
+        }
+      };
+
+      // Add/remove value if operator allows for one
+      this.changeClauseOperator = function(clause) {
+        if (_.contains(clause[1], 'NULL')) {
+          clause.length = 2;
+        } else if (clause.length === 2) {
+          clause.push('');
+        }
+      };
+    }
   });
 
   angular.module('api4Explorer').directive('api4ExpValue', function($routeParams, crmApi4) {
