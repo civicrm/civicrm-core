4587a5c52f1d4d969437385a7679f16f0b362e2d
Sunil Pawar <sunil@skvare.com>
2020-10-09 19:33:43 +0530

    Fix BreadCrumb function input on multiple pages

--- CRM/Contact/Form/Domain.php
+++ CRM/Contact/Form/Domain.php
@@ -73,7 +73,7 @@ class CRM_Contact_Form_Domain extends CRM_Core_Form {
   public function preProcess() {
     CRM_Utils_System::setTitle(ts('Organization Address and Contact Info'));
     $breadCrumbPath = CRM_Utils_System::url('civicrm/admin', 'reset=1');
-    CRM_Utils_System::appendBreadCrumb(ts('Administer CiviCRM'), $breadCrumbPath);
+    CRM_Utils_System::appendBreadCrumb([['title' => ts('Administer CiviCRM'), 'url' => $breadCrumbPath]]);
     $session = CRM_Core_Session::singleton();
     $session->replaceUserContext(CRM_Utils_System::url('civicrm/admin', 'reset=1'));
 
