bc529d996eeb4d3ad90e7e004f5150d115e4d2b9
Tim Otten <totten@civicrm.org>
2020-10-28 01:39:15 -0700

    dev/core#2141 - APIv4 - Add OAuthSysToken.refresh action

--- ext/oauth-client/Civi/Api4/OAuthSysToken.php
+++ ext/oauth-client/Civi/Api4/OAuthSysToken.php
@@ -10,12 +10,24 @@ namespace Civi\Api4;
  */
 class OAuthSysToken extends Generic\DAOEntity {
 
+  /**
+   * Load and conditionally refresh a stored token.
+   *
+   * @param bool $checkPermissions
+   * @return \Civi\Api4\Action\OAuthSysToken\Refresh
+   */
+  public static function refresh($checkPermissions = TRUE) {
+    $action = new \Civi\Api4\Action\OAuthSysToken\Refresh(static::class, __FUNCTION__);
+    return $action->setCheckPermissions($checkPermissions);
+  }
+
   public static function permissions() {
     return [
       'meta' => ['access CiviCRM'],
       'default' => ['manage OAuth client'],
       'delete' => ['manage OAuth client'],
       'get' => ['manage OAuth client'],
+      'refresh' => ['manage OAuth client'],
       'create' => ['manage OAuth client secrets'],
       'update' => ['manage OAuth client secrets'],
       // In theory, there might be cases to 'create' or 'update' an OAuthSysToken
