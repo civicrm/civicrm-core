4a7ce9462a7eae0755f1f718ebb820eefcc4eb64
Tim Otten <totten@civicrm.org>
2020-11-02 17:08:20 -0800

    oauth-client - Add menu item ("Admin: System Settings: OAuth")

--- ext/oauth-client/CRM/OAuth/Upgrader.php
+++ ext/oauth-client/CRM/OAuth/Upgrader.php
@@ -10,6 +10,24 @@ class CRM_OAuth_Upgrader extends CRM_OAuth_Upgrader_Base {
   // upgrade tasks. They are executed in order (like Drupal's hook_update_N).
 
   /**
+   * @see CRM_Utils_Hook::install()
+   */
+  public function install() {
+    $domainId = CRM_Core_Config::domainID();
+    civicrm_api3('Navigation', 'create', [
+      'sequential' => 1,
+      'domain_id' => $domainId,
+      'url' => "civicrm/admin/oauth",
+      'permission' => "manage OAuth client",
+      'label' => "OAuth",
+      'permission_operator' => "OR",
+      'has_separator' => 0,
+      'is_active' => 1,
+      'parent_id' => "System Settings",
+    ]);
+  }
+
+  /**
    * Example: Run an external SQL script when the module is installed.
    *
    * public function install() {
