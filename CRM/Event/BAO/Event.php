1d9a72c842e8c6ac8d927648d1f7c7abd8ef9e4e
Samuel Vanhove <samuel@symbiotic.coop>
2020-12-10 17:44:08 -0500

    dev/core#527 Fix the profile title in receipts

--- CRM/Event/BAO/Event.php
+++ CRM/Event/BAO/Event.php
@@ -1320,7 +1320,7 @@ WHERE civicrm_event.is_active = 1
         $groupTitle = NULL;
         foreach ($fields as $k => $v) {
           if (!$groupTitle) {
-            $groupTitle = $v['groupTitle'];
+            $groupTitle = !empty($v['groupDisplayTitle']) ? $v['groupDisplayTitle'] : $v['groupTitle'];
           }
           // suppress all file fields from display
           if (
