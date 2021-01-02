3e223759c1bbf1a6b3621352f9bdf0170cda53c5
eileen <emcnaughton@wikimedia.org>
2021-01-03 09:52:51 +1300

    dev/core#2251 provide default receive_date

--- CRM/Event/Form/Participant.php
+++ CRM/Event/Form/Participant.php
@@ -1261,7 +1261,7 @@ class CRM_Event_Form_Participant extends CRM_Contribute_Form_AbstractEditPayment
         $contributionParams['non_deductible_amount'] = 'null';
         $contributionParams['receipt_date'] = !empty($params['send_receipt']) ? CRM_Utils_Array::value('receive_date', $params) : 'null';
         $contributionParams['contact_id'] = $this->_contactID;
-        $contributionParams['receive_date'] = CRM_Utils_Array::value('receive_date', $params, 'null');
+        $contributionParams['receive_date'] = $params['receive_date'] ?? date('Y-m-d');
 
         $recordContribution = [
           'financial_type_id',
