e91ee81da963972dce7f592808c8677ee9ee9028
Coleman Watts <coleman@civicrm.org>
2021-01-11 13:27:07 -0500

    Afform - add option group for afform types

---
+++ ext/afform/core/managed/AfformType.mgd.php
@@ -0,0 +1,74 @@
+<?php
+// Adds option group for Afform.type
+
+$mgd = [
+  [
+    'name' => 'AfformType',
+    'entity' => 'OptionGroup',
+    'params' => [
+      'name' => 'afform_type',
+      'title' => 'Afform Type',
+    ],
+  ],
+  [
+    'name' => 'AfformType:form',
+    'entity' => 'OptionValue',
+    'params' => [
+      'option_group_id' => 'afform_type',
+      'name' => 'form',
+      'value' => 'form',
+      'label' => 'Custom Form',
+      'weight' => 0,
+      'icon' => 'fa-list-alt',
+    ],
+  ],
+  [
+    'name' => 'AfformType:block',
+    'entity' => 'OptionValue',
+    'params' => [
+      'option_group_id' => 'afform_type',
+      'name' => 'block',
+      'value' => 'block',
+      'label' => 'Field Block',
+      'weight' => 20,
+      'icon' => 'fa-th-large',
+    ],
+  ],
+  [
+    'name' => 'AfformType:system',
+    'entity' => 'OptionValue',
+    'params' => [
+      'option_group_id' => 'afform_type',
+      'name' => 'system',
+      'value' => 'system',
+      'label' => 'System Form',
+      'weight' => 50,
+      'icon' => 'fa-lock',
+    ],
+  ],
+];
+
+try {
+  $search = civicrm_api3('Extension', 'getsingle', [
+    'full_name' => "org.civicrm.search",
+  ]);
+  if ($search['status'] === 'installed') {
+    $mgd[] = [
+      'name' => 'AfformType:search',
+      'entity' => 'OptionValue',
+      'params' => [
+        'option_group_id' => 'afform_type',
+        'name' => 'search',
+        'value' => 'search',
+        'label' => 'Search Display',
+        'weight' => 10,
+        'icon' => 'fa-search',
+      ],
+    ];
+  }
+}
+catch (Exception $e) {
+  // ¯\_(ツ)_/¯
+}
+
+return $mgd;
