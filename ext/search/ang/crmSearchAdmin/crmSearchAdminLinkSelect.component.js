30a8f784a0a139fc5576cc430a3326088beb76fd
Coleman Watts <coleman@civicrm.org>
2021-02-23 08:31:38 -0500

    SearchKit - More efficient use of space when configuring display fields

--- ext/search/ang/crmSearchAdmin/crmSearchAdminLinkSelect.component.js
+++ ext/search/ang/crmSearchAdmin/crmSearchAdminLinkSelect.component.js
@@ -49,22 +49,31 @@
         return links;
       }
 
-      function onChange() {
-        var val = $('select', $element).val();
-        if (val !== ctrl.column.link) {
-          var link = ctrl.getLink(val);
-          if (link) {
-            ctrl.column.link = link.path;
-            ctrl.column.title = link.title;
-          } else if (val === 'civicrm/') {
+      this.setValue = function(val) {
+        var link = ctrl.getLink(val),
+          oldLink = ctrl.getLink(ctrl.column.link);
+        if (link) {
+          ctrl.column.link = link.path;
+          ctrl.column.title = link.title;
+        } else {
+          if (val === 'civicrm/') {
             ctrl.column.link = val;
-            $timeout(function() {
-              $('input', $element).focus();
+            $timeout(function () {
+              $('input[type=text]', $element).focus();
             });
           } else {
             ctrl.column.link = '';
+          }
+          if (oldLink && ctrl.column.title === oldLink.title) {
             ctrl.column.title = '';
           }
+        }
+      };
+
+      function onChange() {
+        var val = $('select', $element).val();
+        if (val !== ctrl.column.link) {
+          ctrl.setValue(val);
         }
       }
 
