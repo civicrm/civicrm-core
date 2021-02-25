53fbbe0551e4b1eb6474f94b1ff21c3ffb7698db
Tim Otten <totten@civicrm.org>
2021-02-24 21:01:26 -0800

    (security/core#97) PHP CLI guard is the opposite of correct

--- sql/GenerateReportData.php
+++ sql/GenerateReportData.php
@@ -76,7 +76,7 @@
  * php versions.
  * @todo look to remove this file completely.
  */
-if (php_sapi_name() == 'cli' || (is_numeric($_SERVER['argc']) && $_SERVER['argc'] > 0)) {
+if (!(php_sapi_name() == 'cli' || (is_numeric($_SERVER['argc']) && $_SERVER['argc'] > 0))) {
   header("HTTP/1.0 404 Not Found");
   return;
 }
--- tools/bin/scripts/ckeditorConfigScraper.php
+++ tools/bin/scripts/ckeditorConfigScraper.php
@@ -1,5 +1,5 @@
 <?php
-if (php_sapi_name() == 'cli' || (is_numeric($_SERVER['argc']) && $_SERVER['argc'] > 0)) {
+if (!(php_sapi_name() == 'cli' || (is_numeric($_SERVER['argc']) && $_SERVER['argc'] > 0))) {
   header("HTTP/1.0 404 Not Found");
   return;
 }
