0a7837ca9e0662b294488ee80db76bddf6f5ae4e
Coleman Watts <coleman@civicrm.org>
2021-03-02 08:52:43 -0500

    Afform - use search params instead of route params

--- ext/afform/core/ang/afCore.js
+++ ext/afform/core/ang/afCore.js
@@ -3,7 +3,7 @@
   angular.module('afCore', CRM.angRequires('afCore'));
 
   // Use `afCoreDirective(string name)` to generate an AngularJS directive.
-  angular.module('afCore').service('afCoreDirective', function($routeParams, crmApi4, crmStatus, crmUiAlert) {
+  angular.module('afCore').service('afCoreDirective', function($location, crmApi4, crmStatus, crmUiAlert) {
     return function(camelName, meta, d) {
       d.restrict = 'E';
       d.scope = {};
@@ -11,12 +11,16 @@
       d.link = {
         pre: function($scope, $el, $attr) {
           $scope.ts = CRM.ts(camelName);
-          $scope.routeParams = $routeParams;
           $scope.meta = meta;
           $scope.crmApi4 = crmApi4;
           $scope.crmStatus = crmStatus;
           $scope.crmUiAlert = crmUiAlert;
           $scope.crmUrl = CRM.url;
+
+          // Afforms do not use routing, but some forms get input from search params
+          $scope.$watch(function() {return $location.search();}, function(params) {
+            $scope.routeParams = params;
+          });
         }
       };
       return d;
