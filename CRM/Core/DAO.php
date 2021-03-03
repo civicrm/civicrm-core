db9174625bd49a2e799a8c80af585806d9d4d210
Jaap Jansma <jaap.jansma@civicoop.org>
2021-03-03 16:48:56 +0100

    dev/core#2432 remove limit on maximum number of characters during input

--- CRM/Core/DAO.php
+++ CRM/Core/DAO.php
@@ -835,7 +835,11 @@ class CRM_Core_DAO extends DB_DataObject {
         $attributes['cols'] = $cols;
         return $attributes;
       }
-      elseif (CRM_Utils_Array::value('type', $field) == CRM_Utils_Type::T_INT || CRM_Utils_Array::value('type', $field) == CRM_Utils_Type::T_FLOAT || CRM_Utils_Array::value('type', $field) == CRM_Utils_Type::T_MONEY) {
+      elseif (CRM_Utils_Array::value('type', $field) == CRM_Utils_Type::T_INT || CRM_Utils_Array::value('type', $field) == CRM_Utils_Type::T_FLOAT) {
+        $attributes['size'] = 6;
+        $attributes['maxlength'] = 14;
+        return $attributes;
+      } elseif (CRM_Utils_Array::value('type', $field) == CRM_Utils_Type::T_MONEY) {
         $attributes['size'] = 6;
         return $attributes;
       }
