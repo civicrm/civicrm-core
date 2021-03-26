f0c2d2580789d88d21331377e3a3877a87cc1288
eileen <emcnaughton@wikimedia.org>
2021-03-26 14:36:39 +1300

    Revert attempt to make results re-usable
    the DAO throws them away - so we are better just to take the obscure performance hit until we can do
    other refactoring

--- CRM/Contribute/Form/Task/TaskTrait.php
+++ CRM/Contribute/Form/Task/TaskTrait.php
@@ -35,34 +35,31 @@ trait CRM_Contribute_Form_Task_TaskTrait {
    * @throws \CRM_Core_Exception
    */
   public function getSearchQueryResults(): CRM_Core_DAO {
-    if (!$this->queryBAO) {
-      $form = $this;
-      $queryParams = $this->getQueryParams();
-      $returnProperties = ['contribution_id' => 1];
-      $sortOrder = $sortCol = NULL;
-      if ($form->get(CRM_Utils_Sort::SORT_ORDER)) {
-        $sortOrder = $form->get(CRM_Utils_Sort::SORT_ORDER);
-        //Include sort column in select clause.
-        $sortCol = trim(str_replace(['`', 'asc', 'desc'], '', $sortOrder));
-        $returnProperties[$sortCol] = 1;
-      }
-
-      $query = new CRM_Contact_BAO_Query($queryParams, $returnProperties, NULL, FALSE, FALSE,
-        CRM_Contact_BAO_Query::MODE_CONTRIBUTE
-      );
-      // @todo the function CRM_Contribute_BAO_Query::isSoftCreditOptionEnabled should handle this
-      // can we remove? if not why not?
-      if ($this->isQueryIncludesSoftCredits()) {
-        $query->_rowCountClause = ' count(civicrm_contribution.id)';
-        $query->_groupByComponentClause = ' GROUP BY contribution_search_scredit_combined.id, contribution_search_scredit_combined.contact_id, contribution_search_scredit_combined.scredit_id ';
-      }
-      else {
-        $query->_distinctComponentClause = ' civicrm_contribution.id';
-        $query->_groupByComponentClause = ' GROUP BY civicrm_contribution.id ';
-      }
-      $this->queryBAO = $query->searchQuery(0, 0, $sortOrder);
+    $form = $this;
+    $queryParams = $this->getQueryParams();
+    $returnProperties = ['contribution_id' => 1];
+    $sortOrder = $sortCol = NULL;
+    if ($form->get(CRM_Utils_Sort::SORT_ORDER)) {
+      $sortOrder = $form->get(CRM_Utils_Sort::SORT_ORDER);
+      //Include sort column in select clause.
+      $sortCol = trim(str_replace(['`', 'asc', 'desc'], '', $sortOrder));
+      $returnProperties[$sortCol] = 1;
     }
-    return $this->queryBAO;
+
+    $query = new CRM_Contact_BAO_Query($queryParams, $returnProperties, NULL, FALSE, FALSE,
+      CRM_Contact_BAO_Query::MODE_CONTRIBUTE
+    );
+    // @todo the function CRM_Contribute_BAO_Query::isSoftCreditOptionEnabled should handle this
+    // can we remove? if not why not?
+    if ($this->isQueryIncludesSoftCredits()) {
+      $query->_rowCountClause = ' count(civicrm_contribution.id)';
+      $query->_groupByComponentClause = ' GROUP BY contribution_search_scredit_combined.id, contribution_search_scredit_combined.contact_id, contribution_search_scredit_combined.scredit_id ';
+    }
+    else {
+      $query->_distinctComponentClause = ' civicrm_contribution.id';
+      $query->_groupByComponentClause = ' GROUP BY civicrm_contribution.id ';
+    }
+    return $query->searchQuery(0, 0, $sortOrder);
   }
 
   /**
