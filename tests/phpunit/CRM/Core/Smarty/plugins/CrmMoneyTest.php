f9044d40a01324c1cc0c029bd02f3d6298bba4f6
demeritcowboy <demeritcowboy@hotmail.com>
2021-03-31 09:36:50 -0400

    don't try to money_format html

--- tests/phpunit/CRM/Core/Smarty/plugins/CrmMoneyTest.php
+++ tests/phpunit/CRM/Core/Smarty/plugins/CrmMoneyTest.php
@@ -22,10 +22,6 @@ class CRM_Core_Smarty_plugins_CrmMoneyTest extends CiviUnitTestCase {
     $cases = [];
     $cases[] = ['$ 4.00', '{assign var="amount" value="4.00"}{$amount|crmMoney:USD}'];
     $cases[] = ['€ 1,234.00', '{assign var="amount" value="1234.00"}{$amount|crmMoney:EUR}'];
-    $cases[] = [
-      '$ <input size="10" style="background-color:#EBECE4" readonly="readonly" name="eachPaymentAmount" type="text" id="eachPaymentAmount" class="crm-form-text">',
-      '{assign var="amount" value=\'<input size="10" style="background-color:#EBECE4" readonly="readonly" name="eachPaymentAmount" type="text" id="eachPaymentAmount" class="crm-form-text">\'}{$amount|crmMoney:USD}',
-    ];
     return $cases;
   }
 
