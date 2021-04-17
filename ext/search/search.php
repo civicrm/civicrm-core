0402c104f99d322669e76f3a5539b39e2b45ea51
Tim Otten <totten@civicrm.org>
2021-04-16 23:46:15 -0700

    ext/search - Switch to CRM_Extension_Upgrader_Base

--- ext/search/search.php
+++ ext/search/search.php
@@ -22,60 +22,6 @@ function search_civicrm_xmlMenu(&$files) {
 }
 
 /**
- * Implements hook_civicrm_install().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
- */
-function search_civicrm_install() {
-  _search_civix_civicrm_install();
-}
-
-/**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function search_civicrm_postInstall() {
-  _search_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function search_civicrm_uninstall() {
-  _search_civix_civicrm_uninstall();
-}
-
-/**
- * Implements hook_civicrm_enable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
- */
-function search_civicrm_enable() {
-  _search_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function search_civicrm_disable() {
-  _search_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function search_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _search_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
  * Implements hook_civicrm_managed().
  *
  * Generate a list of entities to create/deactivate/delete when this module
