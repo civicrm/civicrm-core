dc903ec51e2aba844b597b12eb8e28d36412efc3
Coleman Watts <coleman@civicrm.org>
2021-04-23 14:39:23 -0400

    Avoid unnecessary client cache clear when saving settings
    These settings are now passed into assetBuilder as params which will trigger an automatic refresh
    of the l10n.js file, no need for a global flush.
    And incidently, this form-layer code would have been skipped when updating settings via API.

--- CRM/Admin/Form/PreferencesDate.php
+++ CRM/Admin/Form/PreferencesDate.php
@@ -109,9 +109,6 @@ class CRM_Admin_Form_PreferencesDate extends CRM_Admin_Form {
 
     $dao->save();
 
-    // Update dynamic js to reflect new date settings
-    CRM_Core_Resources::singleton()->resetCacheCode();
-
     CRM_Core_Session::setStatus(ts("The date type '%1' has been saved.",
       [1 => $params['name']]
     ), ts('Saved'), 'success');
