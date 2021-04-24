01a5461df0f17518505f45d3f59eaa6e895083ea
eileen <emcnaughton@wikimedia.org>
2021-04-25 11:20:15 +1200

    [REF] Move functions back to class that uses it

--- tests/phpunit/CRM/Contribute/Form/Task/PDFLetterCommonTest.php
+++ tests/phpunit/CRM/Contribute/Form/Task/PDFLetterCommonTest.php
@@ -152,7 +152,7 @@ class CRM_Contribute_Form_Task_PDFLetterCommonTest extends CiviUnitTestCase {
       ],
     ];
 
-    [$contributions, $contacts] = CRM_Contribute_Form_Task_PDFLetterCommon::buildContributionArray('contact_id', $contributionIDs, $returnProperties, TRUE, TRUE, $messageToken, 'test', '**', FALSE);
+    [$contributions, $contacts] = CRM_Contribute_Form_Task_PDFLetter::buildContributionArray('contact_id', $contributionIDs, $returnProperties, TRUE, TRUE, $messageToken, 'test', '**', FALSE);
 
     $this->assertEquals('Anthony', $contacts[$this->_individualId]['first_name']);
     $this->assertEquals('emo', $contacts[$this->_individualId]['favourite_emoticon']);
