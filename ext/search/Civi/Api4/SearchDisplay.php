faa79dbf28e2132ddd5911817dcd564a54dec0c6
Coleman Watts <coleman@civicrm.org>
2021-04-28 20:49:06 -0400

    SearchKit - Pass-thu permission checks from SearchDisplay::run to underlying API
    The SearchDisplay::run api action essentially wraps api.get for the search entity,
    so it makes sense to allow open access to the run api but require permission checks
    on the delegated api call.

--- ext/search/Civi/Api4/SearchDisplay.php
+++ ext/search/Civi/Api4/SearchDisplay.php
@@ -29,4 +29,10 @@ class SearchDisplay extends Generic\DAOEntity {
       ->setCheckPermissions($checkPermissions);
   }
 
+  public static function permissions() {
+    $permissions = parent::permissions();
+    $permissions['run'] = [];
+    return $permissions;
+  }
+
 }
