52129c88971bf4c8749a6766121de65994b3032a
Matthew Wire <mjw@mjwconsult.co.uk>
2021-05-17 11:42:10 +0100

    Deprecated duplicate getLoggedInContactID function

--- CRM/Core/Form.php
+++ CRM/Core/Form.php
@@ -2243,7 +2243,7 @@ class CRM_Core_Form extends HTML_QuickForm_Page {
       return (int) $tempID;
     }
 
-    $userID = $this->getLoggedInUserContactID();
+    $userID = CRM_Core_Session::getLoggedInContactID();
 
     if (!is_null($tempID) && $tempID === $userID) {
       CRM_Core_Resources::singleton()->addVars('coreForm', ['contact_id' => (int) $tempID]);
@@ -2283,10 +2283,12 @@ class CRM_Core_Form extends HTML_QuickForm_Page {
 
   /**
    * Get the contact id of the logged in user.
+   * @deprecated
    *
    * @return int|false
    */
   public function getLoggedInUserContactID() {
+    CRM_Core_Error::deprecatedFunctionWarning('CRM_Core_Session::getLoggedInContactID()');
     // check if the user is logged in and has a contact ID
     $session = CRM_Core_Session::singleton();
     return $session->get('userID') ? (int) $session->get('userID') : FALSE;
@@ -2725,7 +2727,7 @@ class CRM_Core_Form extends HTML_QuickForm_Page {
     if (!$contactID) {
       return FALSE;
     }
-    if ($contactID === $this->getLoggedInUserContactID()) {
+    if ($contactID === CRM_Core_Session::getLoggedInContactID()) {
       return $contactID;
     }
     $userChecksum = CRM_Utils_Request::retrieve('cs', 'String', $this);
