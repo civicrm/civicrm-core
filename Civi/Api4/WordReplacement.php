9c88ed4fab6f355bf0d8d58ef350069cd0b4e408
Coleman Watts <coleman@civicrm.org>
2021-06-08 15:08:15 -0400

    dev/core#2486 - Add WordReplacement APIv4 entity

---
+++ Civi/Api4/WordReplacement.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC. All rights reserved.                        |
+ |                                                                    |
+ | This work is published under the GNU AGPLv3 license with some      |
+ | permitted exceptions and without any warranty. For full license    |
+ | and copyright information, see https://civicrm.org/licensing       |
+ +--------------------------------------------------------------------+
+ */
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC https://civicrm.org/licensing
+ */
+
+
+namespace Civi\Api4;
+
+/**
+ * WordReplacement entity.
+ *
+ * This entity stores global string replacements for the CiviCRM UI.
+ *
+ * @searchable secondary
+ * @package Civi\Api4
+ */
+class WordReplacement extends Generic\DAOEntity {
+
+}
