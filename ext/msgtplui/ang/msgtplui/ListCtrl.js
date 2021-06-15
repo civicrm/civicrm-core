a94867962dfb76369177f568c1c432f3f2c6fc66
Tim Otten <totten@civicrm.org>
2021-06-14 22:07:05 -0700

    msgtplui - Misc - Add filter box. Cleanup some links. Consolidate list ctrls.

---
+++ ext/msgtplui/ang/msgtplui/ListCtrl.js
@@ -0,0 +1,42 @@
+(function(angular, $, _) {
+
+  angular.module('msgtplui').controller('MsgtpluiListCtrl', function($scope, $route, crmApi4, crmStatus, crmUiAlert, crmUiHelp, records, $location) {
+    var ts = $scope.ts = CRM.ts('msgtplui');
+    var hs = $scope.hs = crmUiHelp({file: 'CRM/msgtplui/User'}); // See: templates/CRM/msgtplui/User.hlp
+    $scope.crmUrl = CRM.url;
+    $scope.crmUiAlert = crmUiAlert;
+
+    var ctrl = this;
+    ctrl.records = records;
+
+    ctrl.editUrl = function(record, stage) {
+      var url = '#/edit?id=' + encodeURIComponent(record.id);
+      if (record['tx.language']) {
+        url = url + '&lang=' + encodeURIComponent(record['tx.language']);
+      }
+      if (stage === 'draft') {
+        url = url + '&status=draft';
+      }
+      return url;
+    };
+
+    ctrl.delete = function (record) {
+      var q = crmApi4('MessageTemplate', 'delete', {where: [['id', '=', record.id]]}).then(function(){
+        $route.reload();
+      });
+      return crmStatus({start: ts('Deleting...'), success: ts('Deleted')}, q);
+    };
+
+    ctrl.toggle = function (record) {
+      var wasActive = !!record.is_active;
+      var q = crmApi4('MessageTemplate', 'update', {where: [['id', '=', record.id]], values: {is_active: !wasActive}})
+        .then(function(resp){
+          record.is_active = !wasActive;
+        });
+      return wasActive ? crmStatus({start: ts('Disabling...'), success: ts('Disabled')}, q)
+        : crmStatus({start: ts('Enabling...'), success: ts('Enabled')}, q);
+    };
+
+  });
+
+})(angular, CRM.$, CRM._);
--- ext/msgtplui/ang/msgtplui/Workflow.js
+++ ext/msgtplui/ang/msgtplui/Workflow.js
@@ -1,8 +1,9 @@
 (function(angular, $, _) {
 
+  // Display a list of system-workflow message-templates.
   angular.module('msgtplui').config(function($routeProvider) {
       $routeProvider.when('/workflow', {
-        controller: 'MsgtpluiWorkflow',
+        controller: 'MsgtpluiListCtrl',
         controllerAs: '$ctrl',
         templateUrl: '~/msgtplui/Workflow.html',
         resolve: {
@@ -12,7 +13,7 @@
               join: [["Translation AS tx", "LEFT", null, ["tx.entity_table", "=", "'civicrm_msg_template'"], ["tx.entity_id", "=", "id"]]],
               where: [["workflow_name", "IS NOT EMPTY"]],
               groupBy: ["id", "tx.language"],
-              orderBy: {"msg_title":"ASC"},
+              orderBy: {"msg_title":"ASC", "tx.language:label":"ASC"},
               chain: {"statuses":["Translation", "get", {"select":["status_id:name"], "where":[["entity_table", "=", "civicrm_msg_template"], ["entity_id", "=", "$id"], ["language", "=", "$tx.language"]], "groupBy":["status_id"]}, "status_id:name"]}
             });
           },
@@ -20,16 +21,5 @@
       });
     }
   );
-
-  // The controller uses *injection*. This default injects a few things:
-  //   $scope -- This is the set of variables shared between JS and HTML.
-  //   crmApi, crmStatus, crmUiHelp -- These are services provided by civicrm-core.
-  //   myContact -- The current contact, defined above in config().
-  angular.module('msgtplui').controller('MsgtpluiWorkflow', function($scope, crmStatus, crmUiHelp, records) {
-    var ts = $scope.ts = CRM.ts('msgtplui');
-    var hs = $scope.hs = crmUiHelp({file: 'CRM/msgtplui/Workflow'}); // See: templates/CRM/msgtplui/Workflow.hlp
-    var ctrl = this;
-    ctrl.records = records;
-  });
 
 })(angular, CRM.$, CRM._);
