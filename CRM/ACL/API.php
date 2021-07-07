c4103ce06668ac00fa7584b80ae5e750b87c96f4
Matthew Wire <mjw@mjwconsult.co.uk>
2021-07-07 19:39:14 +0100

    Add deprecated flag to functions

--- CRM/ACL/API.php
+++ CRM/ACL/API.php
@@ -38,6 +38,8 @@ class CRM_ACL_API {
    *
    * @return bool
    *   true if yes, else false
+   *
+   * @deprecated
    */
   public static function check($str, $contactID = NULL) {
     \CRM_Core_Error::deprecatedWarning(__CLASS__ . '::' . __FUNCTION__ . ' is deprecated.');
