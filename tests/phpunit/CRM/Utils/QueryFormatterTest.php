4797a2d272f52652310826ae95d153e1bf016b45
Seamus Lee <seamuslee001@gmail.com>
2021-08-04 10:58:48 +1000

    [NFC] Do a proper tear down of fts in QueryFormatterTest

--- tests/phpunit/CRM/Utils/QueryFormatterTest.php
+++ tests/phpunit/CRM/Utils/QueryFormatterTest.php
@@ -43,6 +43,15 @@ class CRM_Utils_QueryFormatterTest extends CiviUnitTestCase {
     }
   }
 
+  public static function tearDown(): void {
+    $idx = new CRM_Core_InnoDBIndexer(FALSE, [
+      'civicrm_contact' => [
+        ['first_name', 'last_name'],
+      ],
+    ]);
+    $idx->fixSchemaDifferences();
+  }
+
   public static function tearDownAfterClass(): void {
     CRM_Core_DAO::executeQuery('DROP TABLE IF EXISTS civicrm_fts_example');
     parent::tearDownAfterClass();
