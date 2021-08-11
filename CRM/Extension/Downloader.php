6cda8fd1f782b2a6cdfa0538b618ce060092c0cd
Matthew Wire <mjw@mjwconsult.co.uk>
2021-08-11 17:11:57 +0100

    Switch extension downloader to use Guzzle.

--- CRM/Extension/Downloader.php
+++ CRM/Extension/Downloader.php
@@ -136,14 +136,12 @@ class CRM_Extension_Downloader {
    *   Whether the download was successful.
    */
   public function fetch($remoteFile, $localFile) {
-    $result = CRM_Utils_HttpClient::singleton()->fetch($remoteFile, $localFile);
-    switch ($result) {
-      case CRM_Utils_HttpClient::STATUS_OK:
-        return TRUE;
-
-      default:
-        return FALSE;
+    $client = new GuzzleHttp\Client();
+    $response = $client->request('GET', $remoteFile, ['sink' => $localFile, 'timeout' => \Civi::settings()->get('http_timeout')]);
+    if ($response->getStatusCode() === 200) {
+      return TRUE;
     }
+    return FALSE;
   }
 
   /**
