e405006c358be3d9d8d0cde2ba83b5d446482071
Coleman Watts <coleman@civicrm.org>
2021-08-28 15:16:34 -0400

    SearchKit - Break part of Run action into a base class

--- ext/search_kit/Civi/Api4/SearchDisplay.php
+++ ext/search_kit/Civi/Api4/SearchDisplay.php
@@ -29,6 +29,15 @@ class SearchDisplay extends Generic\DAOEntity {
       ->setCheckPermissions($checkPermissions);
   }
 
+  /**
+   * @param bool $checkPermissions
+   * @return Action\SearchDisplay\Run
+   */
+  public static function download($checkPermissions = TRUE) {
+    return (new Action\SearchDisplay\Download(__CLASS__, __FUNCTION__))
+      ->setCheckPermissions($checkPermissions);
+  }
+
   public static function permissions() {
     $permissions = parent::permissions();
     $permissions['default'] = ['administer CiviCRM data'];
