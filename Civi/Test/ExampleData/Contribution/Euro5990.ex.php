13515f0c6bc8837babea3d5d149b190f915f126a
Tim Otten <totten@civicrm.org>
2021-09-14 16:42:54 -0700

    Convert `contribution{,_recur}.ex.php` to `Civi/Test/ExampleData/*`
    1. Use revised class format
    2. Use `entity/{$ENTITY}/{$EXAMPLE}` instead of `workflow/{$WORKFLOW}/{$EXAMPLE}`

---
+++ Civi/Test/ExampleData/Contribution/Euro5990.ex.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Civi\Test\ExampleData\Contribution;
+
+class Euro5990 extends \Civi\Test\EntityExample {
+
+  public function getExamples(): iterable {
+    yield [
+      'name' => "entity/{$this->entityName}/{$this->exName}/completed",
+    ];
+  }
+
+  public function build(array &$example): void {
+    $base = [
+      'id' => 50,
+      'contact_id' => 100,
+      'financial_type_id' => 'Member dues',
+      'payment_instrument_id:label' => 'Debit Card',
+      'contribution_page_id' => 2,
+      'receive_date' => '2021-07-23 15:39:20',
+      'revenue_recognition_date' => '2021-07-23 00:00:00',
+      'thankyou_date' => '2021-07-23 15:39:20',
+      'cancel_date' => '',
+      'cancel_reason' => '',
+      'non_deductible_amount' => 5,
+      'total_amount' => 5990.99,
+      'fee_amount' => 0.99,
+      'net_amount' => 5990,
+      'tax_amount' => 60,
+      'currency' => 'EUR',
+      'source' => 'Online donation',
+      'amount_level' => 'premium purchased',
+      'contribution_recur_id' => 50,
+      'check_number' => '',
+      'campaign_id:label' => 'Outreach',
+      'creditnote_id' => '',
+      'trxn_id' => 123,
+      'invoice_id' => 'inv123',
+      'is_test' => TRUE,
+      'is_pay_later' => FALSE,
+    ];
+
+    $extras['completed'] = [
+      'contribution_status_id' => 1,
+    ];
+
+    $example['data'] = $base + $extras[basename($example['name'])];
+  }
+
+}
