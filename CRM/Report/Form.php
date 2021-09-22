694df7a0b6b8ad33443ccba44b3531126deb2ddc
yashodha <yashodha@cividesk.com>
2021-09-22 18:38:57 +0530

    (dev/core#2855) Preserve pristine ids' for further manipulation via hooks for reports

--- CRM/Report/Form.php
+++ CRM/Report/Form.php
@@ -2558,6 +2558,7 @@ WHERE cg.extends IN ('" . implode("','", $this->_customGroupExtends) . "') AND
    */
   protected function alterStateProvinceID($value, &$row, $selectedfield, $criteriaFieldName) {
     $url = CRM_Utils_System::url(CRM_Utils_System::currentPath(), "reset=1&force=1&{$criteriaFieldName}_op=in&{$criteriaFieldName}_value={$value}", $this->_absoluteUrl);
+    $row[$selectedfield . '_pristine'] = $value;
     $row[$selectedfield . '_link'] = $url;
     $row[$selectedfield . '_hover'] = ts("%1 for this state.", [
       1 => $value,
