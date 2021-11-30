80262cf2bb1d6196f63ecd0673d6d8659fe51acd
Tim Otten <totten@civicrm.org>
2021-11-30 14:26:58 -0800

    mixin/mgd-php - Import

---
+++ mixin/mgd-php@1/example/CRM/ShimmyGroup.mgd.php
@@ -0,0 +1,11 @@
+<?php
+return [
+  [
+    'name' => 'the_managed_group',
+    'entity' => 'OptionGroup',
+    'params' => [
+      'name' => 'shimmy_group',
+      'title' => 'Shimmy Group',
+    ],
+  ],
+];
