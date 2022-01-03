f54dff72eb90829423b682eed083b39ca2dcd03d
Coleman Watts <coleman@civicrm.org>
2022-01-03 14:25:20 -0500

    Add menu item for "Import MultiValued Custom Data"

--- CRM/Upgrade/Incremental/php/FiveFortySix.php
+++ CRM/Upgrade/Incremental/php/FiveFortySix.php
@@ -49,6 +49,51 @@ class CRM_Upgrade_Incremental_php_FiveFortySix extends CRM_Upgrade_Incremental_B
    */
   public function upgrade_5_46_alpha1($rev): void {
     $this->addTask(ts('Upgrade DB to %1: SQL', [1 => $rev]), 'runSql', $rev);
+    $this->addTask('Add "Import Custom Data" menu', 'addImportCustomMenu');
+  }
+
+  /**
+   * Add menu item "Import MultiValued Custom" below "Import Activities"
+   *
+   * @param \CRM_Queue_TaskContext $ctx
+   * @return bool
+   */
+  public static function addImportCustomMenu(CRM_Queue_TaskContext $ctx) {
+    try {
+      // Get "Import Activities" and remove separator
+      $importActivities = civicrm_api3('Navigation', 'get', [
+        'name' => 'Import Activities',
+        'return' => ['id', 'parent_id', 'weight'],
+        'sequential' => 1,
+        'domain_id' => CRM_Core_Config::domainID(),
+        'api.Navigation.create' => ['has_separator' => ''],
+      ])['values'][0] ?? NULL;
+      $existing = civicrm_api3('Navigation', 'getcount', [
+        'name' => "Import MultiValued Custom",
+        'domain_id' => CRM_Core_Config::domainID(),
+      ]);
+      // Insert "Import MultiValued Custom" below "Import Activities"
+      if ($importActivities && !$existing) {
+        // Use APIv4 because it will auto-adjust weights unlike v3
+        civicrm_api4('Navigation', 'create', [
+          'checkPermissions' => FALSE,
+          'values' => [
+            'parent_id' => $importActivities['parent_id'],
+            'label' => ts('Import Custom Data'),
+            'weight' => $importActivities['weight'] + 1,
+            'name' => 'Import MultiValued Custom',
+            'permission' => "import contacts",
+            'url' => "civicrm/import/custom?reset=1",
+            'has_separator' => 1,
+            'is_active' => 1,
+          ],
+        ]);
+      }
+    }
+    catch (Exception $e) {
+      // Couldn't create menu item.
+    }
+    return TRUE;
   }
 
 }
