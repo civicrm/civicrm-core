834989057c8ce4e16d590e977733f08c6aa5bc03
Coleman Watts <coleman@civicrm.org>
2022-01-17 21:41:32 -0500

    SearchKit - Support conditional links
    Advanced feature to conditionally show/hide links in a links/buttons/menu column,
    based on user permissions or row values.

--- ext/search_kit/ang/crmSearchAdmin/crmSearchAdminLinkGroup.component.js
+++ ext/search_kit/ang/crmSearchAdmin/crmSearchAdminLinkGroup.component.js
@@ -8,16 +8,51 @@
       apiParams: '<',
       links: '<'
     },
+    require: {
+      crmSearchAdmin: '^crmSearchAdmin'
+    },
     templateUrl: '~/crmSearchAdmin/crmSearchAdminLinkGroup.html',
     controller: function ($scope, $element, $timeout, searchMeta) {
       var ts = $scope.ts = CRM.ts('org.civicrm.search_kit'),
         ctrl = this,
-        linkProps = ['path', 'entity', 'action', 'join', 'target', 'icon', 'text', 'style'];
+        linkProps = ['path', 'entity', 'action', 'join', 'target', 'icon', 'text', 'style', 'condition'];
+
+      var permissionOperators = [
+        {key: '=', value: ts('Has')},
+        {key: '!=', value: ts('Lacks')}
+      ];
+
+      this.getOperators = function(clause) {
+        if (clause[0] === 'check user permission') {
+          return permissionOperators;
+        }
+        return CRM.crmSearchAdmin.operators;
+      };
 
       this.styles = CRM.crmSearchAdmin.styles;
 
       this.getStyle = function(item) {
         return _.findWhere(this.styles, {key: item.style});
+      };
+
+      this.getField = searchMeta.getField;
+
+      this.fields = function() {
+        var selectFields = ctrl.crmSearchAdmin.getSelectFields();
+        var permissionField = [{
+          text: ts('Current User Permission'),
+          id: 'check user permission',
+          description: ts('Check permission of logged-in user')
+        }];
+        return {results: permissionField.concat(selectFields)};
+      };
+
+      this.onChangeCondition = function(item) {
+        if (item.condition[0]) {
+          item.condition[1] = '=';
+        } else {
+          item.condition = [];
+        }
       };
 
       this.sortableOptions = {
@@ -32,23 +67,31 @@
         }
       };
 
+      this.permissions = CRM.crmSearchAdmin.permissions;
+
       $scope.pickIcon = function(index) {
         searchMeta.pickIcon().then(function(icon) {
           ctrl.group[index].icon = icon;
         });
       };
 
+      function setDefaults(item, newValue) {
+        _.each(linkProps, function(prop) {
+          item[prop] = newValue[prop] || (prop === 'condition' ? [] : '');
+        });
+      }
+
       this.addItem = function(item) {
-        ctrl.group.push(_.pick(item, linkProps));
+        var newItem = _.pick(item, linkProps);
+        setDefaults(newItem, newItem);
+        ctrl.group.push(newItem);
       };
 
       this.onChangeLink = function(item, newValue) {
         if (newValue.path === 'civicrm/') {
           newValue = JSON.parse(this.default);
         }
-        _.each(linkProps, function(prop) {
-          item[prop] = newValue[prop] || '';
-        });
+        setDefaults(item, newValue);
       };
 
       this.serialize = JSON.stringify;
@@ -58,10 +101,14 @@
           style: 'default',
           text: ts('Link'),
           icon: 'fa-external-link',
+          condition: [],
           path: 'civicrm/'
         });
         var defaultLinks = _.filter(ctrl.links, function(link) {
           return !link.join;
+        });
+        _.each(ctrl.group, function(item) {
+          setDefaults(item, item);
         });
         if (!ctrl.group.length) {
           if (defaultLinks.length) {
