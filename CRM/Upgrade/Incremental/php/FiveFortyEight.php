19a1c852192f9f879bfd8976387356461c057805
Tim Otten <totten@civicrm.org>
2022-02-01 21:57:01 -0800

    (Schema) Queue and QueueItem - Track detailed settings (runner, lease_time, retry_count, batch_limit, etc) (5.48)

--- CRM/Upgrade/Incremental/php/FiveFortyEight.php
+++ CRM/Upgrade/Incremental/php/FiveFortyEight.php
@@ -29,6 +29,36 @@ class CRM_Upgrade_Incremental_php_FiveFortyEight extends CRM_Upgrade_Incremental
    */
   public function upgrade_5_48_alpha1($rev): void {
     $this->addTask(ts('Upgrade DB to %1: SQL', [1 => $rev]), 'runSql', $rev);
+    $this->addTask('Add "runner" to "civicrm_queue"', 'addColumn', 'civicrm_queue', 'runner',
+      "varchar(64) NULL COMMENT 'Name of the task runner'"
+    );
+    $this->addTask('Convert "is_autorun" to "runner"', 'convertAutorun');
+    $this->addTask('Drop "is_autorun" from "civicrm_queue"', 'dropColumn', 'civicrm_queue', 'is_autorun');
+    $this->addTask('Add "batch_limit" to "civicrm_queue"', 'addColumn', 'civicrm_queue', 'batch_limit',
+      "int unsigned NOT NULL DEFAULT 1 COMMENT 'Maximum number of items in a batch.'"
+    );
+    $this->addTask('Add "lease_time" to "civicrm_queue"', 'addColumn', 'civicrm_queue', 'lease_time',
+      "int unsigned NOT NULL DEFAULT 3600 COMMENT 'When claiming an item (or batch of items) for work, how long should the item(s) be reserved. (Seconds)'"
+    );
+    $this->addTask('Add "retry_limit" to "civicrm_queue"', 'addColumn', 'civicrm_queue', 'retry_limit',
+      "int NULL COMMENT 'Number of permitted retries. Decreases with each retry. Zero (0) to disable. Null for system default.'"
+    );
+    $this->addTask('Add "retry_interval" to "civicrm_queue"', 'addColumn', 'civicrm_queue', 'retry_interval',
+      "int NULL COMMENT 'Number of seconds to wait before retrying a failed execution.'"
+    );
+  }
+
+  /**
+   * The `is_autorun` column was introduced in 5.47,  but we didn't finish adding the
+   * additional changes to use, so there shouldn't be any real usage. But just to be
+   * paranoid, we'll convert to 5.48's `runner`.
+   *
+   * @param \CRM_Queue_TaskContext $ctx
+   * @return bool
+   */
+  public static function convertAutorun(CRM_Queue_TaskContext $ctx) {
+    CRM_Core_DAO::executeQuery('UPDATE civicrm_queue SET runner = "task" WHERE is_autorun = 1');
+    return TRUE;
   }
 
 }
