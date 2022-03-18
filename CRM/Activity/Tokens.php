8b0d8f6f6c3b5601c5311e25cc86763122423435
Matthew Wire <mjw@mjwconsult.co.uk>
2022-03-18 12:11:13 +0000

    Clean activity getBespokeTokens

--- CRM/Activity/Tokens.php
+++ CRM/Activity/Tokens.php
@@ -101,16 +101,13 @@ class CRM_Activity_Tokens extends CRM_Core_EntityTokens {
   protected function getBespokeTokens(): array {
     $tokens = [];
     if (CRM_Core_Component::isEnabled('CiviCase')) {
-      $tokens['case_id'] = ts('Activity Case ID');
-      return [
-        'case_id' => [
-          'title' => ts('Activity Case ID'),
-          'name' => 'case_id',
-          'type' => 'calculated',
-          'options' => NULL,
-          'data_type' => 'Integer',
-          'audience' => 'user',
-        ],
+      $tokens['case_id'] = [
+        'title' => ts('Activity Case ID'),
+        'name' => 'case_id',
+        'type' => 'calculated',
+        'options' => NULL,
+        'data_type' => 'Integer',
+        'audience' => 'user',
       ];
     }
     return $tokens;
