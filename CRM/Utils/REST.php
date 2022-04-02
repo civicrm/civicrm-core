c4195d0c1f504d3529c29620e898f04da7968742
ginkgoMZD <mzd@ginkgostreet.com>
2022-04-01 20:32:46 -0400

    stripslashes from $_POST;

--- CRM/Utils/REST.php
+++ CRM/Utils/REST.php
@@ -323,7 +323,7 @@ class CRM_Utils_REST {
     ];
 
     if (array_key_exists('json', $requestParams) && $requestParams['json'][0] == "{") {
-      $params = json_decode($requestParams['json'], TRUE);
+      $params = json_decode(stripslashes($requestParams['json']), TRUE);
       if ($params === NULL) {
         CRM_Utils_JSON::output([
           'is_error' => 1,
