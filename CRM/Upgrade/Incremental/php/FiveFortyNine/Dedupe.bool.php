b599132667693a9d9498015d7d9cca1656c9fdc6
Monish Deb <monish.deb@jmaconsulting.biz>
2022-04-07 18:12:16 -0700

    Schema - Fix boolean fields in 'Dedupe'

---
+++ CRM/Upgrade/Incremental/php/FiveFortyNine/Dedupe.bool.php
@@ -0,0 +1,6 @@
+<?php
+return [
+  'civicrm_dedupe_rule_group' => [
+    'is_reserved' => "DEFAULT 0 COMMENT 'Is this a reserved rule - a rule group that has been optimized and cannot be changed by the admin'",
+  ],
+];
