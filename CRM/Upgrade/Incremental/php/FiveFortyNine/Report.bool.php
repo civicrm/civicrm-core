e45b470799df6db2303f172d3cd8cbd7b64a39c4
Monish Deb <monish.deb@jmaconsulting.biz>
2022-04-09 07:35:54 +0530

    revert Menu.is_public requiredness

--- CRM/Upgrade/Incremental/php/FiveFortyNine/Report.bool.php
+++ CRM/Upgrade/Incremental/php/FiveFortyNine/Report.bool.php
@@ -1,11 +1,7 @@
 <?php
 return [
   'civicrm_report_instance' => [
-    'is_active' => "DEFAULT 0 COMMENT 'Is this entry active?'",
+    'is_active' => "DEFAULT 1 COMMENT 'Is this entry active?'",
     'is_reserved' => "DEFAULT 0",
   ],
-  'civicrm_report_instance' => [
-    'is_active' => "DEFAULT 1 COMMENT 'Is this entry active?'",
-  ],
-    // WTF
 ];
