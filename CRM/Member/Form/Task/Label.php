43515021d1d55de9e3677155bfe8183a2550519c
Pradeep Nayak <pradpnayak@gmail.com>
2022-04-15 23:45:00 +0100

    Limit issue fix

--- CRM/Member/Form/Task/Label.php
+++ CRM/Member/Form/Task/Label.php
@@ -112,6 +112,7 @@ class CRM_Member_Form_Task_Label extends CRM_Member_Form_Task {
       $memberships = civicrm_api3('membership', 'get', [
         'id' => ['IN' => $this->_memberIds],
         'return' => 'contact_id',
+        'options' => ['limit' => 0],
       ]);
       foreach ($memberships['values'] as $id => $membership) {
         if (isset($rows[$membership['contact_id']])) {
