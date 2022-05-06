e4fac88c8caf90c01780642f85c4c7cd60caf405
Coleman Watts <coleman@civicrm.org>
2022-05-05 20:29:52 -0400

    SearchKit - Default to search for individuals

--- ext/search_kit/Civi/Search/Admin.php
+++ ext/search_kit/Civi/Search/Admin.php
@@ -46,6 +46,7 @@ class Admin {
       'defaultPagerSize' => \Civi::settings()->get('default_pager_size'),
       'defaultDisplay' => SearchDisplay::getDefault(FALSE)->setSavedSearch(['id' => NULL])->execute()->first(),
       'modules' => $extensions,
+      'defaultContactType' => \CRM_Contact_BAO_ContactType::basicTypeInfo()['Individual']['name'] ?? NULL,
       'tags' => Tag::get()
         ->addSelect('id', 'name', 'color', 'is_selectable', 'description')
         ->addWhere('used_for', 'CONTAINS', 'civicrm_saved_search')
