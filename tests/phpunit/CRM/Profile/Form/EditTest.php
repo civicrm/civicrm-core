e661b0f61e1f3fefe0598f9c8b2dca212485db62
Jamie McClelland <jm@mayfirst.org>
2022-05-31 15:46:55 -0400

    Enable ability to require tags in profiles
    Prior to this change, it was not possible to make tags required in a
    profile.

--- tests/phpunit/CRM/Profile/Form/EditTest.php
+++ tests/phpunit/CRM/Profile/Form/EditTest.php
@@ -46,4 +46,62 @@ class CRM_Profile_Form_EditTest extends CiviUnitTestCase {
     $this->assertEquals('civicrm/Mr. Anthony Anderson II', CRM_Core_Session::singleton()->popUserContext());
   }
 
+  /**
+   * Test that requiring tags on a profile works.
+   *
+   * @throws \API_Exception
+   */
+  public function testProfileRequireTag(): void {
+    $ufGroupParams = [
+      'group_type' => 'Individual,Contact',
+      'name' => 'test_individual_contact_tag_profile',
+      'title' => 'Gimme a tag',
+      'api.uf_field.create' => [
+        [
+          'field_name' => 'first_name',
+          'is_required' => 1,
+          'visibility' => 'Public Pages and Listings',
+          'field_type' => 'Individual',
+          'label' => 'First Name',
+        ],
+        [
+          'field_name' => 'last_name',
+          'is_required' => 1,
+          'visibility' => 'Public Pages and Listings',
+          'field_type' => 'Individual',
+          'label' => 'Last Name',
+        ],
+        [
+          'field_name' => 'tag',
+          'is_required' => 1,
+          'visibility' => 'Public Pages and Listings',
+          'field_type' => 'Contact',
+          'label' => 'Tag',
+        ],
+      ],
+    ];
+
+    $profile = $this->callAPISuccess('uf_group', 'create', $ufGroupParams);
+    $profileID = $profile['id'];
+
+    // Configure the profile to be used as a standalone profile for data entry.
+    UFJoin::create(FALSE)->setValues([
+      'module' => 'Profile',
+      'uf_group_id' => $profileID,
+    ])->execute();
+
+    // Populate the form.
+    $formParams = [
+      'first_name' => 'Foo',
+      'last_name' => 'McGoo',
+      'gid' => $profileID,
+      'tag' => [],
+    ];
+    $form = $this->getFormObject('CRM_Profile_Form_Edit', $formParams);
+    $form->set('gid', $profileID);
+    $form->preProcess();
+    $form->buildQuickForm();
+    $this->assertFalse($form->validate(), 'Ensure tags can be required on a form.');
+  }
+
 }
