5bee23815cd56c5214634ee7f40a1b8f52f0ac20
Coleman Watts <coleman@civicrm.org>
2022-06-02 11:06:10 -0400

    SearchKit - Switch nav menu to mgd.php with new permission

--- ext/search_kit/CRM/Search/Upgrader.php
+++ ext/search_kit/CRM/Search/Upgrader.php
@@ -7,32 +7,6 @@ use CRM_Search_ExtensionUtil as E;
 class CRM_Search_Upgrader extends CRM_Search_Upgrader_Base {
 
   /**
-   * Add menu item when enabled.
-   */
-  public function enable() {
-    \Civi\Api4\Navigation::create(FALSE)
-      ->addValue('parent_id:name', 'Search')
-      ->addValue('label', E::ts('Search Kit'))
-      ->addValue('name', 'search_kit')
-      ->addValue('url', 'civicrm/admin/search')
-      ->addValue('icon', 'crm-i fa-search-plus')
-      ->addValue('has_separator', 2)
-      ->addValue('weight', 99)
-      ->addValue('permission', ['administer CiviCRM data'])
-      ->execute();
-  }
-
-  /**
-   * Delete menu item when disabled.
-   */
-  public function disable() {
-    \Civi\Api4\Navigation::delete(FALSE)
-      ->addWhere('name', '=', 'search_kit')
-      ->addWhere('domain_id', '=', 'current_domain')
-      ->execute();
-  }
-
-  /**
    * Upgrade 1000 - install schema
    * @return bool
    */
@@ -42,7 +16,6 @@ class CRM_Search_Upgrader extends CRM_Search_Upgrader_Base {
     if (!CRM_Core_DAO::singleValueQuery("SHOW TABLES LIKE 'civicrm_search_display'")) {
       $this->executeSqlFile('sql/auto_install.sql');
     }
-    CRM_Core_DAO::executeQuery("UPDATE civicrm_navigation SET url = 'civicrm/admin/search', name = 'search_kit' WHERE url = 'civicrm/search'");
     return TRUE;
   }
 
@@ -139,16 +112,6 @@ class CRM_Search_Upgrader extends CRM_Search_Upgrader_Base {
           ->execute();
       }
     }
-    return TRUE;
-  }
-
-  /**
-   * Upgrade 1004 - fix menu permission.
-   * @return bool
-   */
-  public function upgrade_1004(): bool {
-    $this->ctx->log->info('Applying update 1004 - fix menu permission.');
-    CRM_Core_DAO::executeQuery("UPDATE civicrm_navigation SET permission = 'administer CiviCRM data' WHERE url = 'civicrm/admin/search'");
     return TRUE;
   }
 
