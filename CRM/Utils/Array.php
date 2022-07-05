bbb4b90ad2105a9e07f2767b383c6569b8ab8eac
Tim Otten <totten@civicrm.org>
2022-07-05 16:23:36 -0700

    CRM_Utils_Array - All `pathGet()`-like methods will accept empty-path
    These two are equivalent:
    
      (A) $data['sub1']['sub2']['sub3']
      (B) CRM_Utils_Array::pathGet($data, ['sub1', 'sub2', 'sub3'])
    
    But what if the list of subpaths is empty -- ie you're accessing the top-level item?
    
      (A) $data
      (B) CRM_Utils_Array::pathGet($data, [])
    
    It worked correctly on `pathGet()`, but accessing the top-level using a
    related methods (eg `pathSet()`) would generate a PHP error.
    
    This commit ensures that related methods can be used to check/update the
    top-level item (ie path===[]).
    
    Note that there is some decent test-coverage already for the regular
    scenarios (eg `testGetSetPathParts()`).

--- CRM/Utils/Array.php
+++ CRM/Utils/Array.php
@@ -1077,6 +1077,9 @@ class CRM_Utils_Array {
    * @return bool
    */
   public static function pathIsset($values, $path) {
+    if ($path === []) {
+      return ($values !== NULL);
+    }
     foreach ($path as $key) {
       if (!is_array($values) || !isset($values[$key])) {
         return FALSE;
@@ -1100,6 +1103,11 @@ class CRM_Utils_Array {
    *   TRUE if anything has been removed. FALSE if no changes were required.
    */
   public static function pathUnset(&$values, $path, $cleanup = FALSE) {
+    if (count($path) === 0) {
+      $values = NULL;
+      return TRUE;
+    }
+
     if (count($path) === 1) {
       if (isset($values[$path[0]])) {
         unset($values[$path[0]]);
@@ -1131,6 +1139,10 @@ class CRM_Utils_Array {
    *   Ex: 456.
    */
   public static function pathSet(&$values, $pathParts, $value) {
+    if ($pathParts === []) {
+      $values = $value;
+      return;
+    }
     $r = &$values;
     $last = array_pop($pathParts);
     foreach ($pathParts as $part) {
