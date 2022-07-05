b5230fbbcc6c8e663bb9910d687f8c0ed399a515
Thomas Schüttler <tschuettler@oxfam.de>
2022-07-05 10:33:09 +0200

    Fix sorting by mapping type

--- CRM/Admin/Page/Mapping.php
+++ CRM/Admin/Page/Mapping.php
@@ -123,8 +123,8 @@ class CRM_Admin_Page_Mapping extends CRM_Core_Page_Basic {
    * Run the basic page.
    */
   public function run() {
-    $sort = 'mapping_type asc';
-    return parent::run($sort);
+    $sort = 'mapping_type_id ASC, name ASC';
+    return parent::run(NULL, NULL, $sort);
   }
 
 }
