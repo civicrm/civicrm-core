8c3a1fd9a56e2161648113d531a4f0f0247634f6
Eileen McNaughton <emcnaughton@wikimedia.org>
2022-09-17 10:15:31 +1200

    Pass correct variable type to CRM_Core_OptionValue::getValues

--- CRM/Core/I18n.php
+++ CRM/Core/I18n.php
@@ -179,6 +179,7 @@ class CRM_Core_I18n {
     static $enabled = NULL;
 
     if (!$all) {
+      $optionValues = [];
       // Use `getValues`, not `buildOptions` to bypass hook_civicrm_fieldOptions.  See core#1132.
       CRM_Core_OptionValue::getValues(['name' => 'languages'], $optionValues, 'weight', TRUE);
       $all = array_column($optionValues, 'label', 'name');
