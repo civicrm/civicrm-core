9b31c491e3e883831453ad471a3dd140d797db43
Tim Otten <totten@civicrm.org>
2022-09-19 17:57:00 -0700

    (NFC) Cross-reference Joomla cleanup lists

--- CRM/Contact/Import/Parser.php
+++ CRM/Contact/Import/Parser.php
@@ -1,4 +1,6 @@
 <?php
 
 // Temporary file to ensure the old file does not linger on Joomla installs
-// who usually fail to delete files. The old file will cause an error if present
+// who usually fail to delete files. The old file will cause an error if present.
+// TODO: Implement proper Joomla-deletion support. For partial support:
+// @see CRM_Utils_Check_Component_Source
