128b8fa9f94829eb6680c4ed13c563e6cd85b3bb
magnolia61 <richard.van.oosterhout@gmail.com>
2022-09-21 17:03:17 +0200

    add message template id to sendmail function

--- CRM/Contact/Form/Task/EmailTrait.php
+++ CRM/Contact/Form/Task/EmailTrait.php
@@ -748,6 +748,7 @@ trait CRM_Contact_Form_Task_EmailTrait {
     $additionalDetails = NULL,
     $campaignId = NULL,
     $caseId = NULL
+    $template
   ) {
 
     $userID = CRM_Core_Session::getLoggedInContactID();
@@ -760,6 +761,7 @@ trait CRM_Contact_Form_Task_EmailTrait {
       $contactId = $values['contact_id'];
       $emailAddress = $values['email'];
       $renderedTemplate = CRM_Core_BAO_MessageTemplate::renderTemplate([
+        'messageTemplateID' => $template,
         'messageTemplate' => [
           'msg_text' => $text,
           'msg_html' => $html,
