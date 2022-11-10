47cfb6bb6811af2d488885769ac7320df09b487d
Coleman Watts <coleman@civicrm.org>
2022-11-09 21:49:57 -0500

    Afform - Upgrade ext to civix 22.10.0

--- ext/afform/admin/afform_admin.civix.php
+++ ext/afform/admin/afform_admin.civix.php
@@ -24,7 +24,7 @@ class CRM_AfformAdmin_ExtensionUtil {
    *   Translated text.
    * @see ts
    */
-  public static function ts($text, $params = []) {
+  public static function ts($text, $params = []): string {
     if (!array_key_exists('domain', $params)) {
       $params['domain'] = [self::LONG_NAME, NULL];
     }
@@ -41,7 +41,7 @@ class CRM_AfformAdmin_ExtensionUtil {
    *   Ex: 'http://example.org/sites/default/ext/org.example.foo'.
    *   Ex: 'http://example.org/sites/default/ext/org.example.foo/css/foo.css'.
    */
-  public static function url($file = NULL) {
+  public static function url($file = NULL): string {
     if ($file === NULL) {
       return rtrim(CRM_Core_Resources::singleton()->getUrl(self::LONG_NAME), '/');
     }
@@ -138,7 +138,7 @@ function _afform_admin_civix_civicrm_postInstall() {
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
  */
-function _afform_admin_civix_civicrm_uninstall() {
+function _afform_admin_civix_civicrm_uninstall(): void {
   _afform_admin_civix_civicrm_config();
   if ($upgrader = _afform_admin_civix_upgrader()) {
     $upgrader->onUninstall();
@@ -150,7 +150,7 @@ function _afform_admin_civix_civicrm_uninstall() {
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
-function _afform_admin_civix_civicrm_enable() {
+function _afform_admin_civix_civicrm_enable(): void {
   _afform_admin_civix_civicrm_config();
   if ($upgrader = _afform_admin_civix_upgrader()) {
     if (is_callable([$upgrader, 'onEnable'])) {
@@ -165,7 +165,7 @@ function _afform_admin_civix_civicrm_enable() {
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
  * @return mixed
  */
-function _afform_admin_civix_civicrm_disable() {
+function _afform_admin_civix_civicrm_disable(): void {
   _afform_admin_civix_civicrm_config();
   if ($upgrader = _afform_admin_civix_upgrader()) {
     if (is_callable([$upgrader, 'onDisable'])) {
