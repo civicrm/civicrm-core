5511ef1e62ad75398843004926e5aa389dfeefb5
Coleman Watts <coleman@civicrm.org>
2022-11-25 11:35:35 -0500

    SearchKit - Fix Campaign, State, Country selectors (again)
    This fixes the attempted fix from 7deb8c37 which broke more than it fixed.
    
    The goal was to use ajax to load state, country & county lists because they are too long to prefetch.
    But switching ALL FK fields to use ajax instead of option suffixes was too heavy-handed and would
    have broken a lot of existing searches by no longer supporting those pseudoconstants in the UI.
    
    This restores all previous option list fields, and targets only address country/county/state fields.
    Ideally we'd turn the prefetch flag off on those fields but that would have major consequenses for
    formBuilder and existing searches.
    So this uses a subtler approach and tweaks their "suffixes" metadata to remove :name which didn't really
    make sense for those fields anyway since they don't actually have machine names.
    Then it teaches SearchKit to use ajax for selecting from fields with no :name, while still using :label
    for their display value in the table.

--- ext/search_kit/ang/crmSearchAdmin.module.js
+++ ext/search_kit/ang/crmSearchAdmin.module.js
@@ -401,7 +401,7 @@
                 // For fields with both an FK and an option list, prefer the FK
                 // because it's more efficient to render an autocomplete than to
                 // pre-load potentially thousands of options into a select dropdown.
-                where: [['options', '!=', false], ['fk_entity', 'IS NULL']],
+                where: [['options', '!=', false], ['suffixes', 'CONTAINS', 'name']],
                 select: ['options']
               }, {name: 'options'}];
             }
