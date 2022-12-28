220f8a5854ac51a27c3921f8fedf6d15c83bc222
Eileen McNaughton <emcnaughton@wikimedia.org>
2022-12-28 13:35:15 +1300

    Add Report Instance apiv4

---
+++ Civi/Api4/ReportInstance.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC. All rights reserved.                        |
+ |                                                                    |
+ | This work is published under the GNU AGPLv3 license with some      |
+ | permitted exceptions and without any warranty. For full license    |
+ | and copyright information, see https://civicrm.org/licensing       |
+ +--------------------------------------------------------------------+
+ */
+namespace Civi\Api4;
+
+/**
+ * ReportInstance entity.
+ *
+ * @searchable secondary
+ * @since 5.58
+ * @package Civi\Api4
+ */
+class ReportInstance extends Generic\DAOEntity {
+  use Generic\Traits\ManagedEntity;
+
+}
