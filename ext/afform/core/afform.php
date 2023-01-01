b48a2ee2c7baa2535f80c419b01528939c6a8efe
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-01-01 15:58:11 +1300

    Run Civix upgrade on afform/core with template patch applied

--- ext/afform/core/afform.php
+++ ext/afform/core/afform.php
@@ -77,48 +77,12 @@ function afform_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function afform_civicrm_postInstall() {
-  _afform_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function afform_civicrm_uninstall() {
-  _afform_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function afform_civicrm_enable() {
   _afform_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function afform_civicrm_disable() {
-  _afform_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function afform_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _afform_civix_civicrm_upgrade($op, $queue);
 }
 
 /**
