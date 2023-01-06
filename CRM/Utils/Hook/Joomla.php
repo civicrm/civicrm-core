99999a6f9c9f9463332a5520d952a695235f6f66
Aidan Saunders <aidan.saunders@squiffle.uk>
2023-01-06 10:48:14 +0000

    Fix `cv` failure on J3.

--- CRM/Utils/Hook/Joomla.php
+++ CRM/Utils/Hook/Joomla.php
@@ -73,6 +73,9 @@ class CRM_Utils_Hook_Joomla extends CRM_Utils_Hook {
         if (version_compare(JVERSION, '3.0', 'lt')) {
           $app = JCli::getInstance();
         }
+        elseif (version_compare(JVERSION, '4.0', 'lt')) {
+          $app = JApplicationCli::getInstance();
+        }
         else {
           $app = \Joomla\CMS\Factory::getApplication();
         }
