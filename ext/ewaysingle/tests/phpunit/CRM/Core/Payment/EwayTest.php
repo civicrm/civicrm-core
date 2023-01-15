a8a08b20a29c3919317971691cc1e44d911f0f24
Seamus Lee <seamuslee001@gmail.com>
2023-01-15 20:19:12 +0000

    [REF][PHP8.2] Fix Eway Single Payment Processor extension for dynamic properties

--- ext/ewaysingle/tests/phpunit/CRM/Core/Payment/EwayTest.php
+++ ext/ewaysingle/tests/phpunit/CRM/Core/Payment/EwayTest.php
@@ -23,6 +23,18 @@ class CRM_Core_Payment_EwayTest extends \PHPUnit\Framework\TestCase implements H
   use \Civi\Test\GuzzleTestTrait;
   use \Civi\Test\Api3TestTrait;
 
+  /**
+   * Instance of CRM_Core_Payment_eWay|null
+   * @var CRM_Core_Payment_eWay
+   */
+  protected $processor;
+
+  /**
+   * Created Object Ids
+   * @var array
+   */
+  public $ids;
+
   public function setUpHeadless() {
     // Civi\Test has many helpers, like install(), uninstall(), sql(), and sqlFile().
     // See: https://docs.civicrm.org/dev/en/latest/testing/phpunit/#civitest
