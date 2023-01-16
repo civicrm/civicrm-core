af34abeaf60e2b7fe080308bfb07bc62d833b5d3
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-01-17 10:05:50 +1300

    Re-run civix upgrade on flexmailer
    Also see https://github.com/civicrm/civicrm-core/pull/25249

--- ext/flexmailer/flexmailer.php
+++ ext/flexmailer/flexmailer.php
@@ -33,48 +33,12 @@ function flexmailer_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function flexmailer_civicrm_postInstall() {
-  _flexmailer_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function flexmailer_civicrm_uninstall() {
-  _flexmailer_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function flexmailer_civicrm_enable() {
   _flexmailer_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function flexmailer_civicrm_disable() {
-  _flexmailer_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function flexmailer_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _flexmailer_civix_civicrm_upgrade($op, $queue);
 }
 
 /**
