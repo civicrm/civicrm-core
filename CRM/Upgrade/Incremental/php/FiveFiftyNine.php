c5c0a84d28ca2b2928e7f0f8c4569e038a859c6c
demeritcowboy <demeritcowboy@hotmail.com>
2023-01-31 15:20:38 -0500

    typo

--- CRM/Upgrade/Incremental/php/FiveFiftyNine.php
+++ CRM/Upgrade/Incremental/php/FiveFiftyNine.php
@@ -24,7 +24,7 @@ class CRM_Upgrade_Incremental_php_FiveFiftyNine extends CRM_Upgrade_Incremental_
   public function setPreUpgradeMessage(&$preUpgradeMessage, $rev, $currentVer = NULL): void {
     if ($rev === '5.59.alpha1') {
       if (empty(CRM_Core_Config::singleton()->userSystem->is_wordpress)) {
-        $preUpgradeMessage .= '<p>' . ts('The handling of invalid smarty template code in mailings, reminders and other automated messages has changed . For details, see <a %1>upgrade notes</a>.',
+        $preUpgradeMessage .= '<p>' . ts('The handling of invalid smarty template code in mailings, reminders and other automated messages has changed. For details, see <a %1>upgrade notes</a>.',
             [1 => 'href="https://docs.civicrm.org/sysadmin/en/latest/upgrade/version-specific/#civicrm-559" target="_blank"']) . '</p>';
       }
     }
