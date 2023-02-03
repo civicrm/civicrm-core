6d23f7e94ae9f61da3f3fa263e024a44939205f9
Rich Lott / Artful Robot <code.commits@artfulrobot.uk>
2023-02-03 10:39:00 +0000

    standalone: add standaloneusers core extension

---
+++ ext/standaloneusers/Civi/Api4/UserRole.php
@@ -0,0 +1,13 @@
+<?php
+namespace Civi\Api4;
+
+/**
+ * UserRole entity.
+ *
+ * Provided by the Standalone Users extension.
+ *
+ * @package Civi\Api4
+ */
+class UserRole extends Generic\DAOEntity {
+
+}
---
+++ ext/standaloneusers/standaloneusers.php
@@ -0,0 +1,108 @@
+<?php
+
+require_once 'standaloneusers.civix.php';
+// phpcs:disable
+use CRM_Standaloneusers_ExtensionUtil as E;
+// phpcs:enable
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
+ */
+function standaloneusers_civicrm_config(&$config) {
+  _standaloneusers_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function standaloneusers_civicrm_install() {
+  _standaloneusers_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function standaloneusers_civicrm_postInstall() {
+  _standaloneusers_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
+ */
+function standaloneusers_civicrm_uninstall() {
+  _standaloneusers_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function standaloneusers_civicrm_enable() {
+  _standaloneusers_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
+ */
+function standaloneusers_civicrm_disable() {
+  _standaloneusers_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
+ */
+function standaloneusers_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _standaloneusers_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * Declare entity types provided by this module.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function standaloneusers_civicrm_entityTypes(&$entityTypes) {
+  _standaloneusers_civix_civicrm_entityTypes($entityTypes);
+}
+
+// --- Functions below this ship commented out. Uncomment as required. ---
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
+ */
+//function standaloneusers_civicrm_preProcess($formName, &$form) {
+//
+//}
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_navigationMenu
+ */
+//function standaloneusers_civicrm_navigationMenu(&$menu) {
+//  _standaloneusers_civix_insert_navigation_menu($menu, 'Mailings', [
+//    'label' => E::ts('New subliminal message'),
+//    'name' => 'mailing_subliminal_message',
+//    'url' => 'civicrm/mailing/subliminal',
+//    'permission' => 'access CiviMail',
+//    'operator' => 'OR',
+//    'separator' => 0,
+//  ]);
+//  _standaloneusers_civix_navigationMenu($menu);
+//}
