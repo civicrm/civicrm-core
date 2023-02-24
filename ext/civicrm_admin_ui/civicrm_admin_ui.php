fdc476c40e062e105b86e2f444df536bbedaa7e8
Tim Otten <totten@civicrm.org>
2023-02-23 16:44:22 -0800

    ext/civicrm_admin_ui - Update to civix v23.02.0

--- ext/civicrm_admin_ui/civicrm_admin_ui.php
+++ ext/civicrm_admin_ui/civicrm_admin_ui.php
@@ -13,14 +13,3 @@ use CRM_CivicrmAdminUi_ExtensionUtil as E;
 function civicrm_admin_ui_civicrm_config(&$config) {
   _civicrm_admin_ui_civix_civicrm_config($config);
 }
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function civicrm_admin_ui_civicrm_entityTypes(&$entityTypes) {
-  _civicrm_admin_ui_civix_civicrm_entityTypes($entityTypes);
-}
