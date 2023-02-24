7168793c03ef57c06bbfe45f5ff873ebb3657806
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-02-24 20:36:50 +1300

    Fix mixin

--- CRM/Core/Smarty.php
+++ CRM/Core/Smarty.php
@@ -315,7 +315,9 @@ class CRM_Core_Smarty extends Smarty {
       return parent::addTemplateDir($template_dir, $key, $isConfig);
     }
     if (is_array($this->template_dir)) {
-      array_unshift($this->template_dir, $template_dir);
+      if (!in_array($template_dir, $this->template_dir)) {
+        array_unshift($this->template_dir, $template_dir);
+      }
     }
     else {
       $this->template_dir = [$template_dir, $this->template_dir];
