accd0f6412b39c24470fac13e06bb8a9669dcee5
Dominic Tubach <tubach@systopia.de>
2023-04-27 09:19:55 +0200

    Add APIv4 for `EntityFile`

---
+++ Civi/Api4/EntityFile.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC. All rights reserved.                        |
+ |                                                                    |
+ | This work is published under the GNU AGPLv3 license with some      |
+ | permitted exceptions and without any warranty. For full license    |
+ | and copyright information, see https://civicrm.org/licensing       |
+ +--------------------------------------------------------------------+
+ */
+
+namespace Civi\Api4;
+
+/**
+ * EntityFile entity.
+ *
+ * @searchable bridge
+ * @package Civi\Api4
+ */
+class EntityFile extends Generic\DAOEntity {
+
+  use Generic\Traits\EntityBridge;
+
+}
