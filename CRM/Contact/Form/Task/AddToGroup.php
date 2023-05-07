008a175734988e6c9252e41a3e64895d4aa34deb
larssandergreen <lars@wildsight.ca>
2023-05-06 21:19:51 -0600

    fix php8 warnings for AddToGroup

--- CRM/Contact/Form/Task/AddToGroup.php
+++ CRM/Contact/Form/Task/AddToGroup.php
@@ -109,10 +109,6 @@ class CRM_Contact_Form_Task_AddToGroup extends CRM_Contact_Form_Task {
 
     if ($this->_context === 'amtg') {
       $groupElement->freeze();
-
-      // also set the group title
-      $groupValues = ['id' => $this->_id, 'title' => $this->_title];
-      $this->assign_by_ref('group', $groupValues);
     }
 
     // Set dynamic page title for 'Add Members Group (confirm)'
