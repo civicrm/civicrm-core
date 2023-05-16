fa3ee32c6c334a3f136dba616c5bd2451aef2f92
Tim Otten <totten@civicrm.org>
2023-05-16 01:33:20 -0700

    RegionTest - Update to match default loader (`esm.loader.shim`)

--- tests/phpunit/CRM/Core/RegionTest.php
+++ tests/phpunit/CRM/Core/RegionTest.php
@@ -139,8 +139,8 @@ class CRM_Core_RegionTest extends CiviUnitTestCase {
       . "<script type=\"text/javascript\" src=\"/foo%20bar.js\">\n</script>\n"
       . "<script type=\"text/javascript\">\nalert(\"hi\");\n</script>\n"
       . "<script type=\"text/javascript\">\nCRM.\$(function(\$) {\n\$(\"div\");\n});\n</script>\n"
-      . "<script type=\"module\" src=\"/my%20module.mjs\">\n</script>\n"
-      . "<script type=\"module\">\nimport foo from \"./foobar.mjs\";\n</script>\n"
+      . "<script type=\"module-shim\" src=\"/my%20module.mjs\">\n</script>\n"
+      . "<script type=\"module-shim\">\nimport foo from \"./foobar.mjs\";\n</script>\n"
       . "<link href=\"/foo%20bar.css\" rel=\"stylesheet\" type=\"text/css\"/>\n"
       . "<style type=\"text/css\">\nbody { background: black; }\n</style>\n";
     $this->assertEquals($expected, $actual);
