16c28764d17543f5208e6da6365523d2628f3371
Tim Otten <totten@civicrm.org>
2023-05-16 19:10:59 -0700

    CRM_Utils_SQL - Add option `allowLiterals()` for building queries with literal SQL snippets

--- CRM/Utils/SQL/EscapeStringTrait.php
+++ CRM/Utils/SQL/EscapeStringTrait.php
@@ -2,13 +2,31 @@
 
 trait CRM_Utils_SQL_EscapeStringTrait {
 
+  protected $allowLiterals = FALSE;
+
+  public function allowLiterals(bool $allowLiterals = TRUE) {
+    $this->allowLiterals = $allowLiterals;
+    return $this;
+  }
+
   /**
    * @param string|null $value
    * @return string
    *   SQL expression, e.g. "it\'s great" (with-quotes) or NULL (without-quotes)
    */
   public function escapeString($value) {
-    return $value === NULL ? 'NULL' : '"' . CRM_Core_DAO::escapeString($value) . '"';
+    if ($value === NULL) {
+      return 'NULL';
+    }
+    if ($value instanceof CRM_Utils_SQL_Literal) {
+      if ($this->allowLiterals) {
+        return $value->getValue();
+      }
+      else {
+        throw new CRM_Core_Exception('SQL builder does not support literal expressions. Must call allowLiterals() first.');
+      }
+    }
+    return '"' . CRM_Core_DAO::escapeString($value) . '"';
   }
 
 }
