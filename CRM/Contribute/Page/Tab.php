2d7965c8035ac50c481fe19616b9ef4e0bbcb597
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-05-17 15:26:51 +1200

    Adjust weights so link order is inchanged

--- CRM/Contribute/Page/Tab.php
+++ CRM/Contribute/Page/Tab.php
@@ -139,6 +139,7 @@ class CRM_Contribute_Page_Tab extends CRM_Core_Page {
         'title' => ts('Cancel'),
         // Only display on-site links in a popup.
         'class' => (stripos($url, 'http') !== FALSE) ? 'no-popup' : '',
+        'weight' => -50,
       ];
     }
 
@@ -152,6 +153,7 @@ class CRM_Contribute_Page_Tab extends CRM_Core_Page {
         'url' => $url,
         // Only display on-site links in a popup.
         'class' => (stripos($url, 'http') !== FALSE) ? 'no-popup' : '',
+        'weight' => -15,
       ];
     }
 
@@ -166,6 +168,7 @@ class CRM_Contribute_Page_Tab extends CRM_Core_Page {
         'url' => $url,
         // Only display on-site links in a popup.
         'class' => (stripos($url, 'http') !== FALSE) ? 'no-popup' : '',
+        'weight' => -10,
       ];
     }
     return $links;
