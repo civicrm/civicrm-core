4eb4a194652919d30b78da03ca2a25b94e2455f3
colemanw <coleman@civicrm.org>
2023-05-19 09:25:25 -0400

    dev/core#4302 Revert "Remove old deprecated class aliases"
    This reverts the file deletions from f687b5a4eed2b67411a873c3346487bd5599497
    as the files were still in use by some extensions.

---
+++ CRM/Mailing/BAO/Component.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @deprecated copy of renamed class.
+ * @package CRM
+ * @copyright CiviCRM LLC https://civicrm.org/licensing
+ */
+class CRM_Mailing_BAO_Component extends CRM_Mailing_BAO_MailingComponent {
+
+}
