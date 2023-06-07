6996cbb56664012bf5ce0b5b04e8f3c7be63ff3f
Tim Otten <totten@civicrm.org>
2023-06-06 21:11:38 -0700

    (NFC) Smarty - Expand test coverage for scope-cleanup. Show inconsistency.

--- tests/phpunit/CRM/Core/Smarty/plugins/CrmScopeTest.php
+++ tests/phpunit/CRM/Core/Smarty/plugins/CrmScopeTest.php
@@ -13,6 +13,8 @@ class CRM_Core_Smarty_plugins_CrmScopeTest extends CiviUnitTestCase {
     // Templates should normally be file names, but for unit-testing it's handy to use "string:" notation
     require_once 'CRM/Core/Smarty/resources/String.php';
     civicrm_smarty_register_string_resource();
+
+    $this->useTransaction();
   }
 
   /**
