e1367e21e462e72f278ea9cac27b758b9e8f2c10
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-06-18 12:11:15 +1200

    Deprecate calling repeatTransaction via completeOrder

--- CRM/Contribute/BAO/Contribution.php
+++ CRM/Contribute/BAO/Contribution.php
@@ -3816,6 +3816,7 @@ INNER JOIN civicrm_activity ON civicrm_activity_contact.activity_id = civicrm_ac
    */
   public static function completeOrder($input, $recurringContributionID, $contributionID, $isPostPaymentCreate = FALSE) {
     if (!$contributionID) {
+      CRM_Core_Error::deprecatedWarning('call the v3 Contribution.repeattransaction api');
       return self::repeatTransaction($input, $recurringContributionID);
     }
     $transaction = new CRM_Core_Transaction();
