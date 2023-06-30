civicrm/civicrm-core

commit 65a6ae6b9228ad945b0f2dc9b80886468098555f

crmUi - more reliable method of checking whether autocomplete element needs updating

ang/crmUi.js
@@ -755,15 +755,12 @@
           };
 
           if (ctrl.ngModel) {
-            var oldValue;
             // Ensure widget is updated when model changes
             ctrl.ngModel.$render = function() {
-              element.val(ctrl.ngModel.$viewValue || '');
               // Trigger change so the Select2 renders the current value,
               // but only if the value has actually changed (to avoid recursion)
-              if (!angular.equals(ctrl.ngModel.$viewValue, oldValue)) {
-                oldValue = ctrl.ngModel.$viewValue;
-                element.change();
+              if (!angular.equals(ctrl.ngModel.$viewValue || '', element.val())) {
+                element.val(ctrl.ngModel.$viewValue || '').change();
               }
             };
 

commit e27f2e5a55e0e61c66782d54f74f7b5d123557a8

crmAutocomplete - Fix endless loop (again)
This regressed in 65a6ae6b9228ad945b0f2dc9b80886468098555f because the comparison
needs to equivocate numbers and numeric strings.

Update ang/crmUi.js

Co-authored-by: Rich Lott <artfulrobot@users.noreply.github.com>

ang/crmUi.js
@@ -759,8 +759,11 @@
             ctrl.ngModel.$render = function() {
               // Trigger change so the Select2 renders the current value,
               // but only if the value has actually changed (to avoid recursion)
-              if (!angular.equals(ctrl.ngModel.$viewValue || '', element.val())) {
-                element.val(ctrl.ngModel.$viewValue || '').change();
+              // We need to coerce null|false in the model to '' and numbers to strings.
+              // We need 0 not to be equivalent to null|false|''
+              const newValue = (ctrl.ngModel.$viewValue === null || ctrl.ngModel.$viewValue === undefined || ctrl.ngModel.$viewValue === false) ? '' : ctrl.ngModel.$viewValue.toString();
+              if (newValue !== element.val().toString()) {
+                element.val(newValue).change();
               }
             };
 