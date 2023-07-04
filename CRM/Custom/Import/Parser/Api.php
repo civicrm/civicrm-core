815f47dc4b313e3760aa97f6398988b775e9424b
howardshand <howardshand@gmail.com>
2023-07-04 11:22:17 -0500

    Update Api.php to correct external_identifier to type STRING

--- CRM/Custom/Import/Parser/Api.php
+++ CRM/Custom/Import/Parser/Api.php
@@ -83,7 +83,7 @@ class CRM_Custom_Import_Parser_Api extends CRM_Import_Parser {
       $this->importableFieldsMetadata = array_merge([
         'do_not_import' => ['title' => ts('- do not import -')],
         'contact_id' => ['title' => ts('Contact ID'), 'name' => 'contact_id', 'type' => CRM_Utils_Type::T_INT, 'options' => FALSE, 'headerPattern' => '/contact?|id$/i'],
-        'external_identifier' => ['title' => ts('External Identifier'), 'name' => 'external_identifier', 'type' => CRM_Utils_Type::T_INT, 'options' => FALSE, 'headerPattern' => '/external\s?id/i'],
+        'external_identifier' => ['title' => ts('External Identifier'), 'name' => 'external_identifier', 'type' => CRM_Utils_Type::T_STRING, 'options' => FALSE, 'headerPattern' => '/external\s?id/i'],
       ], $importableFields);
     }
   }
