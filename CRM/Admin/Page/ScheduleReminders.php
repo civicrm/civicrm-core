e44a64f6e54a305aeed96e7a5f50091f4425c7d5
colemanw <coleman@civicrm.org>
2023-07-19 21:38:49 -0400

    ScheduledReminders - Refactor form to work at a standalone url

--- CRM/Admin/Page/ScheduleReminders.php
+++ CRM/Admin/Page/ScheduleReminders.php
@@ -23,13 +23,6 @@ class CRM_Admin_Page_ScheduleReminders extends CRM_Core_Page_Basic {
   public $useLivePageJS = TRUE;
 
   /**
-   * The action links that we need to display for the browse screen.
-   *
-   * @var array
-   */
-  public static $_links = NULL;
-
-  /**
    * Get BAO Name.
    *
    * @return string
@@ -37,45 +30,6 @@ class CRM_Admin_Page_ScheduleReminders extends CRM_Core_Page_Basic {
    */
   public function getBAOName() {
     return 'CRM_Core_BAO_ActionSchedule';
-  }
-
-  /**
-   * Get action Links.
-   *
-   * @return array
-   *   (reference) of action links
-   */
-  public function &links() {
-    if (!(self::$_links)) {
-      // helper variable for nicer formatting
-      self::$_links = [
-        CRM_Core_Action::UPDATE => [
-          'name' => ts('Edit'),
-          'class' => 'no-popup',
-          'url' => 'civicrm/admin/scheduleReminders',
-          'qs' => 'action=update&id=%%id%%&reset=1',
-          'title' => ts('Edit Schedule Reminders'),
-        ],
-        CRM_Core_Action::ENABLE => [
-          'name' => ts('Enable'),
-          'ref' => 'crm-enable-disable',
-          'title' => ts('Enable Label Format'),
-        ],
-        CRM_Core_Action::DISABLE => [
-          'name' => ts('Disable'),
-          'ref' => 'crm-enable-disable',
-          'title' => ts('Disable Label Format'),
-        ],
-        CRM_Core_Action::DELETE => [
-          'name' => ts('Delete'),
-          'url' => 'civicrm/admin/scheduleReminders',
-          'qs' => 'action=delete&id=%%id%%',
-          'title' => ts('Delete Schedule Reminders'),
-        ],
-      ];
-    }
-
-    return self::$_links;
   }
 
   /**
