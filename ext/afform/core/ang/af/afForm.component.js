civicrm/civicrm-core

commit e2c263c427119a6cc40f45e67898fa71240d57bf

Afform - Enable creating event from a template
This expands autofill functionality to work with create actions in addition to update.

ext/afform/core/ang/af/afForm.component.js
@@ -42,13 +42,13 @@
       };
       // With no arguments this will prefill the entire form based on url args
       // With selectedEntity, selectedIndex & selectedId provided this will prefill a single entity
-      this.loadData = function(selectedEntity, selectedIndex, selectedId) {
+      this.loadData = function(selectedEntity, selectedIndex, selectedId, selectedField) {
         var toLoad = 0,
           params = {name: ctrl.getFormMeta().name, args: {}};
         // Load single entity
         if (selectedEntity) {
-          toLoad = selectedId;
-          params.fillMode = 'entity';
+          toLoad = 1;
+          params.matchField = selectedField;
           params.args[selectedEntity] = {};
           params.args[selectedEntity][selectedIndex] = selectedId;
         }

Civi/Api4/Event/Subscriber/AutocompleteFieldSubscriber.php
@@ -26,7 +26,7 @@ class AutocompleteFieldSubscriber extends AutoService implements EventSubscriber
    */
   public static function getSubscribedEvents() {
     return [
-      'civi.api.prepare' => ['onApiPrepare', -50],
+      'civi.api.prepare' => ['onApiPrepare', 150],
     ];
   }
 

Civi/Api4/Service/Autocomplete/EventAutocompleteProvider.php
@@ -1,0 +1,62 @@
+<?php
+
+/*
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC. All rights reserved.                        |
+ |                                                                    |
+ | This work is published under the GNU AGPLv3 license with some      |
+ | permitted exceptions and without any warranty. For full license    |
+ | and copyright information, see https://civicrm.org/licensing       |
+ +--------------------------------------------------------------------+
+ */
+
+namespace Civi\Api4\Service\Autocomplete;
+
+use Civi\API\Events;
+use Civi\Core\Event\GenericHookEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * @service
+ * @internal
+ */
+class EventAutocompleteProvider extends \Civi\Core\Service\AutoService implements EventSubscriberInterface {
+
+  public static function getSubscribedEvents() {
+    return [
+      'civi.api.prepare' => ['onApiPrepare', 140],
+      'civi.search.defaultDisplay' => ['alterDefaultDisplay', Events::W_LATE],
+    ];
+  }
+
+  /**
+   * Add is_template filter to event template autocompletes
+   * @param \Civi\API\Event\PrepareEvent $event
+   */
+  public function onApiPrepare(\Civi\API\Event\PrepareEvent $event): void {
+    $apiRequest = $event->getApiRequest();
+    if (is_object($apiRequest) && is_a($apiRequest, 'Civi\Api4\Generic\AutocompleteAction')) {
+      [$entityName, $fieldName] = array_pad(explode('.', (string) $apiRequest->getFieldName(), 2), 2, '');
+
+      if ($entityName === 'Event' && $fieldName === 'template_id') {
+        $apiRequest->addFilter('is_template', TRUE);
+      }
+    }
+  }
+
+  /**
+   * Alter default display of events based on the is_template filter.
+   *
+   * @param \Civi\Core\Event\GenericHookEvent $e
+   */
+  public static function alterDefaultDisplay(GenericHookEvent $e) {
+    if ($e->display['type'] !== 'autocomplete' || $e->savedSearch['api_entity'] !== 'Event') {
+      return;
+    }
+    $filters = $e->context['filters'] ?? [];
+    if (!empty($filters['is_template'])) {
+      $e->display['settings']['columns'][0]['key'] = 'template_title';
+    }
+  }
+
+}

ext/afform/admin/Civi/AfformAdmin/AfformAdminMeta.php
@@ -129,7 +129,8 @@ class AfformAdminMeta {
       $idField = CoreUtil::getIdFieldName($entityName);
       $fields[$idField]['readonly'] = FALSE;
       $fields[$idField]['input_type'] = 'EntityRef';
-      $fields[$idField]['is_id'] = TRUE;
+      // Afform-only (so far) metadata tells the form to update an existing entity autofilled from this value
+      $fields[$idField]['input_attrs']['autofill'] = 'update';
       $fields[$idField]['fk_entity'] = $entityName;
       $fields[$idField]['label'] = E::ts('Existing %1', [1 => CoreUtil::getInfoItem($entityName, 'title')]);
       // Mix in alterations declared by afform entities

ext/afform/admin/ang/afGuiEditor/elements/afGuiField-menu.html
@@ -1,4 +1,4 @@
-<li ng-if="!$ctrl.fieldDefn.is_id">
+<li ng-if="!$ctrl.fieldDefn.input_attrs || !$ctrl.fieldDefn.input_attrs.autofill">
   <div href ng-click="$event.stopPropagation()" class="af-gui-field-select-in-dropdown">
     <label>{{:: ts('Type:') }}</label>
     <select class="form-control" ng-model="getSet('input_type')" ng-model-options="{getterSetter: true}" title="{{:: ts('Field type') }}">
@@ -50,7 +50,7 @@
     {{:: ts('Required') }}
   </a>
 </li>
-<li ng-if="!$ctrl.fieldDefn.is_id">
+<li ng-if="!$ctrl.fieldDefn.input_attrs || !$ctrl.fieldDefn.input_attrs.autofill">
   <a href ng-click="toggleDefaultValue(); $event.stopPropagation(); $event.target.blur();" title="{{:: ts('Pre-fill this field with a value') }}">
     <i class="crm-i fa-{{ $ctrl.hasDefaultValue ? 'check-' : '' }}square-o"></i>
     {{:: ts('Default value') }}

ext/afform/core/Civi/Afform/AfformMetadataInjector.php
@@ -138,7 +138,7 @@ class AfformMetadataInjector {
     if ($inputType === 'Select' || $inputType === 'ChainSelect') {
       $fieldInfo['input_attrs']['placeholder'] = E::ts('Select');
     }
-    elseif ($inputType === 'EntityRef' && empty($field['is_id'])) {
+    elseif ($inputType === 'EntityRef' && empty($field['input_attrs']['placeholder'])) {
       $info = civicrm_api4('Entity', 'get', [
         'where' => [['name', '=', $fieldInfo['fk_entity']]],
         'checkPermissions' => FALSE,

ext/afform/core/Civi/Afform/FormDataModel.php
@@ -123,9 +123,10 @@ class FormDataModel {
       return TRUE;
     }
 
-    // "Update" effectively means "read+save".
+    // "Get" is used for autofilling entities in "update" mode, but also for
+    // pre-populating fields from a template in "create" mode.
     if ($action === 'get') {
-      $action = 'update';
+      return TRUE;
     }
 
     $result = !empty($entityDefn['actions'][$action]);
@@ -237,8 +238,9 @@ class FormDataModel {
       $entityTitle = CoreUtil::getInfoItem($entityName, 'title');
       $field['input_type'] = 'EntityRef';
       $field['fk_entity'] = $entityName;
-      $field['is_id'] = TRUE;
       $field['label'] = E::ts('Existing %1', [1 => $entityTitle]);
+      // Afform-only (so far) metadata tells the form to update an existing entity autofilled from this value
+      $field['input_attrs']['autofill'] = 'update';
       $field['input_attrs']['placeholder'] = E::ts('Select %1', [1 => $entityTitle]);
     }
     // If this is an implicit join, get new field from fk entity

ext/afform/core/Civi/Api4/Action/Afform/AbstractProcessor.php
@@ -31,13 +31,6 @@ abstract class AbstractProcessor extends \Civi\Api4\Generic\AbstractAction {
   protected $args = [];
 
   /**
-   * Used by prefill action to indicate if the entire form or just one entity is being filled.
-   * @var string
-   * @options form,entity
-   */
-  protected $fillMode = 'form';
-
-  /**
    * @var array
    */
   protected $_afform;
@@ -84,11 +77,12 @@ abstract class AbstractProcessor extends \Civi\Api4\Generic\AbstractAction {
     foreach ($sortedEntities as $entityName) {
       $entity = $this->_formDataModel->getEntity($entityName);
       $this->_entityIds[$entityName] = [];
-      $idField = CoreUtil::getIdFieldName($entity['type']);
-      if (!empty($entity['actions']['update'])) {
+      $matchField = $this->matchField ?? CoreUtil::getIdFieldName($entity['type']);
+      $matchFieldDefn = $this->_formDataModel->getField($entity['type'], $matchField, 'create');
+      if (!empty($entity['actions'][$matchFieldDefn['input_attrs']['autofill']])) {
         if (
           !empty($this->args[$entityName]) &&
-          (!empty($entity['url-autofill']) || isset($entity['fields'][$idField]))
+          (!empty($entity['url-autofill']) || isset($entity['fields'][$matchField]))
         ) {
           $ids = (array) $this->args[$entityName];
           $this->loadEntity($entity, $ids);

commit 3c52227df1dfd3495642372355a1bb388f4fbeab

Afform - Fix clearing fields when deselecting existing entity
This regressed in e2c263c427119a6cc40f45e67898fa71240d57bf

ext/afform/core/ang/af/afForm.component.js
@@ -43,11 +43,11 @@
       // With no arguments this will prefill the entire form based on url args
       // With selectedEntity, selectedIndex & selectedId provided this will prefill a single entity
       this.loadData = function(selectedEntity, selectedIndex, selectedId, selectedField) {
-        var toLoad = 0,
-          params = {name: ctrl.getFormMeta().name, args: {}};
+        let toLoad = false;
+        const params = {name: ctrl.getFormMeta().name, args: {}};
         // Load single entity
         if (selectedEntity) {
-          toLoad = 1;
+          toLoad = !!selectedId;
           params.matchField = selectedField;
           params.args[selectedEntity] = {};
           params.args[selectedEntity][selectedIndex] = selectedId;
@@ -57,7 +57,7 @@
           args = _.assign({}, $scope.$parent.routeParams || {}, $scope.$parent.options || {});
           _.each(schema, function (entity, entityName) {
             if (args[entityName] || entity.actions.update) {
-              toLoad++;
+              toLoad = true;
             }
             if (args[entityName] && typeof args[entityName] === 'string') {
               args[entityName] = args[entityName].split(',');
@@ -79,10 +79,11 @@
         }
         // Clear existing contact selection
         else if (selectedEntity) {
-          data[selectedEntity][selectedIndex].fields = {};
-          if (data[selectedEntity][selectedIndex].joins) {
-            data[selectedEntity][selectedIndex].joins = {};
-          }
+          // Delete object keys without breaking object references
+          Object.keys(data[selectedEntity][selectedIndex].fields).forEach(key => delete data[selectedEntity][selectedIndex].fields[key]);
+          // Fill pre-set values
+          angular.merge(data[selectedEntity][selectedIndex].fields, _.cloneDeep(schema[selectedEntity].data || {}));
+          data[selectedEntity][selectedIndex].joins = {};
         }
       };
 