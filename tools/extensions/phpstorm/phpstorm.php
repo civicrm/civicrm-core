edb3c0470f00a457ab3d5b253aad2a3c087b3f2c
Tim Otten <totten@civicrm.org>
2023-08-25 15:58:25 -0700

    phpstorm - Re-enable standard civix bits

--- tools/extensions/phpstorm/phpstorm.php
+++ tools/extensions/phpstorm/phpstorm.php
@@ -1,10 +1,11 @@
 <?php
 
-// require_once 'phpstorm.civix.php';
-// phpcs:disable
-// use CRM_Phpstorm_ExtensionUtil as E;
-// phpcs:enable
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+
+require_once 'phpstorm.civix.php';
+// phpcs:disable
+use CRM_Phpstorm_ExtensionUtil as E;
+// phpcs:enable
 
 /**
  * Determine the folder where we will store PhpStorm metadata files.
@@ -25,9 +26,9 @@ function phpstorm_metadata_dir(): string {
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
  */
-// function phpstorm_civicrm_config(&$config): void {
-//  _phpstorm_civix_civicrm_config($config);
-// }
+function phpstorm_civicrm_config(&$config): void {
+ _phpstorm_civix_civicrm_config($config);
+}
 
 function phpstorm_civicrm_container(\Symfony\Component\DependencyInjection\ContainerBuilder $container) {
   $container->addCompilerPass(new \Civi\PhpStorm\PhpStormCompilePass(), PassConfig::TYPE_AFTER_REMOVING, 2000);
