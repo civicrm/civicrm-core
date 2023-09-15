57763debb5b27ea47ecc1e6e214d836570bc08ab
colemanw <coleman@civicrm.org>
2023-09-15 09:36:11 -0400

    CiviCase - Optimize managed reconcile

--- ext/civi_case/civi_case.php
+++ ext/civi_case/civi_case.php
@@ -7,10 +7,13 @@ use CRM_Case_ExtensionUtil as E;
  * Implements hook_civicrm_managed().
  */
 function civi_case_civicrm_managed(&$entities, $modules) {
-  // Don't optimize for $modules because the below functions delegate to other extensions
-  $entities = array_merge($entities,
-    CRM_Case_ManagedEntities::createManagedCaseTypes(),
-    CRM_Case_ManagedEntities::createManagedActivityTypes(CRM_Case_XMLRepository::singleton(), CRM_Core_ManagedEntities::singleton()),
-    CRM_Case_ManagedEntities::createManagedRelationshipTypes(CRM_Case_XMLRepository::singleton(), CRM_Core_ManagedEntities::singleton())
-  );
+  // Don't optimize for $modules because `createManagedCaseTypes` delegates to other extensions
+  $entities = array_merge($entities, CRM_Case_ManagedEntities::createManagedCaseTypes());
+  // These functions always declare module = civicrm
+  if (!$modules || in_array('civicrm', $modules, TRUE)) {
+    $entities = array_merge($entities,
+      CRM_Case_ManagedEntities::createManagedActivityTypes(CRM_Case_XMLRepository::singleton()),
+      CRM_Case_ManagedEntities::createManagedRelationshipTypes(CRM_Case_XMLRepository::singleton())
+    );
+  }
 }
