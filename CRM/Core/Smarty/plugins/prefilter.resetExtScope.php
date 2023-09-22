490b21621f2144f0f2315a4f524a13f272f061e4
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-09-23 10:12:55 +1200

    Remove unused variables from custom smarty functions

--- CRM/Core/Smarty/plugins/prefilter.resetExtScope.php
+++ CRM/Core/Smarty/plugins/prefilter.resetExtScope.php
@@ -3,11 +3,11 @@
 /**
  * Wrap every Smarty template in a {crmScope} tag that sets the
  * variable "extensionKey" to blank.
- * @param $tpl_source
- * @param $smarty
+ * @param string $tpl_source
+ *
  * @return string
  */
-function smarty_prefilter_resetExtScope($tpl_source, &$smarty) {
+function smarty_prefilter_resetExtScope($tpl_source) {
   return '{crmScope extensionKey=""}'
     . $tpl_source
     . '{/crmScope}';
