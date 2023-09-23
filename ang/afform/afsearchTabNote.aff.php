191e92331e67d022ff132a7c2a240643524e47ae
colemanw <coleman@civicrm.org>
2023-09-23 16:36:26 -0400

    ContactSummary - Add SearchKit display for Notes tab

---
+++ ang/afform/afsearchTabNote.aff.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+  'type' => 'search',
+  'title' => ts('Notes'),
+  'description' => '',
+  'contact_summary' => 'tab',
+  'summary_weight' => 100,
+  'icon' => 'fa-sticky-note-o',
+  'summary_contact_type' => NULL,
+  'permission' => [
+    'access CiviCRM',
+  ],
+  'permission_operator' => 'AND',
+  'navigation' => NULL,
+];
