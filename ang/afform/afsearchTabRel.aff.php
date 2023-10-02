e8eee5cfaff4a29a13a737ae3503922a7ff95601
colemanw <coleman@civicrm.org>
2023-10-01 21:48:59 -0400

    ContactSummary - Add SearchKit display for relationships tab

---
+++ ang/afform/afsearchTabRel.aff.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  'type' => 'search',
+  'title' => ts('Relationships'),
+  'permission' => [
+    'access CiviCRM',
+  ],
+  'contact_summary' => 'tab',
+  'icon' => 'fa-handshake-o',
+  'summary_weight' => 80,
+  'permission_operator' => 'AND',
+];
