91e394b9f0a4f55bfe52b4248235b3bacbebd36c
Jo Franz <franz@systopia.de>
2023-10-10 22:10:53 +0200

    PHP 8 fix undefines_property

--- CRM/Event/Form/SelfSvcUpdate.php
+++ CRM/Event/Form/SelfSvcUpdate.php
@@ -95,6 +95,7 @@ class CRM_Event_Form_SelfSvcUpdate extends CRM_Core_Form {
    * @var bool
    */
   protected $isBackoffice = FALSE;
+  private string $_userContext;
 
   /**
    * Set variables up before form is built based on participant ID from URL
