8a9292cf903f156583f8e8ee744ea5a5540deaca
Tim Otten <totten@civicrm.org>
2023-10-12 18:03:18 -0700

    (REF) Extract TaskHandler and BasicHandler
    This splits the class `CRM_Queue_TaskRunner` in two parts:
    
    * `CRM_Queue_BasicHandler` is a queue-listener that breaks-down a batch and
      individually executes each item. It enforces queue options like
      `retry_limit=>5` and `error=>abort`
    * `CRM_Queue_TaskHandler` specifically handles instances of `CRM_Queue_Task`.

---
+++ CRM/Queue/TaskHandler.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC. All rights reserved.                        |
+ |                                                                    |
+ | This work is published under the GNU AGPLv3 license with some      |
+ | permitted exceptions and without any warranty. For full license    |
+ | and copyright information, see https://civicrm.org/licensing       |
+ +--------------------------------------------------------------------+
+ */
+
+/**
+ * `CRM_Queue_TaskHandler`  a list tasks from a queue. It is designed to supported background
+ * tasks which run automatically.
+ *
+ * This runner is not appropriate for all queues or workloads, so you might choose or create
+ * a different runner. For example, `CRM_Queue_Runner` is geared toward background task lists.
+ *
+ * @service civi.queue.task_handler
+ */
+class CRM_Queue_TaskHandler extends CRM_Queue_BasicHandler {
+
+  public static function getTypeName(): string {
+    return 'task';
+  }
+
+  /**
+   * Do a unit of work with one item from the queue.
+   *
+   * @param $item
+   * @param $queue
+   * @return mixed
+   *   Boolean-ish. TRUE for success. FALSE for failure.
+   */
+  protected function runItem($item, $queue) {
+    return $item->data->run($this->createContext($queue));
+  }
+
+  /**
+   * Get a nice title for the item.
+   *
+   * @param $item
+   * @return string|null
+   */
+  protected function getItemTitle($item): string {
+    $title = parent::getItemTitle($item);
+    if (isset($item->data->title)) {
+      $title .= '(' . $item->data->title . ')';
+    }
+    return $title;
+  }
+
+  /**
+   * Get detailed info about the item. This is used for debugging.
+   *
+   * @param $item
+   * @return array
+   */
+  protected function getItemDetails($item): array {
+    return CRM_Utils_Array::subset((array) $item->data, ['title', 'callback', 'arguments']);
+  }
+
+  /**
+   * @param \CRM_Queue_Queue $queue
+   * return CRM_Queue_TaskContext;
+   */
+  private function createContext(\CRM_Queue_Queue $queue): \CRM_Queue_TaskContext {
+    $taskCtx = new \CRM_Queue_TaskContext();
+    $taskCtx->queue = $queue;
+    $taskCtx->log = \CRM_Core_Error::createDebugLogger();
+    return $taskCtx;
+  }
+
+}
