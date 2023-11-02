d41d080bd9990e3c137e6a8cda2267d91578a150
colemanw <coleman@civicrm.org>
2023-11-01 20:59:35 -0400

    APIv4 - Ensure 'search_fields' defaults to 'label_field' for Entity.get
    This default is set in the AbstractEntity class, but not every entity inherits from that class,
    e.g. ECK does not.
    So it needs to be filled in by the Entity.get action

--- Civi/Api4/Provider/ActionObjectProvider.php
+++ Civi/Api4/Provider/ActionObjectProvider.php
@@ -161,11 +161,20 @@ class ActionObjectProvider extends AutoService implements EventSubscriberInterfa
       // Allow extensions to modify the list of entities
       $event = GenericHookEvent::create(['entities' => &$entities]);
       \Civi::dispatcher()->dispatch('civi.api4.entityTypes', $event);
+      $this->fillEntityDefaults($entities);
       ksort($entities);
       $cache->set('api4.entities.info', $entities);
     }
 
     return $entities;
+  }
+
+  public function fillEntityDefaults(array &$entities) {
+    foreach ($entities as &$entity) {
+      if (!isset($entity['search_fields'])) {
+        $entity['search_fields'] = (array) ($entity['label_field'] ?? NULL);
+      }
+    }
   }
 
   /**
