033196b527eb551e83d057032a84f160e6b68088
colemanw <coleman@civicrm.org>
2023-11-07 15:51:22 -0500

    Angular - cleanup core angular module declarations

--- ext/civi_mail/ang/crmMailing.ang.php
+++ ext/civi_mail/ang/crmMailing.ang.php
@@ -1,9 +1,7 @@
 <?php
-// This file declares an Angular module which can be autoloaded
-// ODDITY: Only loads if you have CiviMail permissions.
+// This file declares crmMailing Angular module.
 
 return [
-  'ext' => 'civicrm',
   'js' => [
     'ang/crmMailing.js',
     'ang/crmMailing/*.js',
