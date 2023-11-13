aac3be0fb3812930fd174c2f91aa9a61dc0942f7
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-11-14 07:19:51 +1300

    Move set up of complicated price set to the ContributionPageTestTrait & test to the confirmTest
    We need to add some more testing but most of our existing tests are within the apiv3 test harness
    which is not a great location. This moves one test over to the full form test flow
    and also pimps the helper trait to support this complex price scenario (with fields
    of a mixture of types & financial types.
    
    I want to add some checks on the email output but this specific test is pay later
    so I won't do it in this PR

--- Civi/Test/ContributionPageTestTrait.php
+++ Civi/Test/ContributionPageTestTrait.php
@@ -56,6 +56,12 @@ trait ContributionPageTestTrait {
   }
 
   /**
+   * Create a paid contribution page.
+   *
+   * This function ensures that the page has pay later configured, unless specified as false
+   * and that there is a payment_processor configured, unless the key payment_processor
+   * is already set.
+   *
    * @param array $contributionPageValues
    * @param array $priceSetParameters
    *   Currently if 'id' is passed in then no update is made, but this could change
@@ -65,6 +71,23 @@ trait ContributionPageTestTrait {
    */
   public function contributionPageCreatePaid(array $contributionPageValues, array $priceSetParameters = [], string $identifier = 'ContributionPage'): array {
     $contributionPageValues['is_monetary'] = TRUE;
+    $contributionPageValues += ['is_pay_later' => TRUE, 'pay_later_text' => 'Send check by mail'];
+    if (!array_key_exists('payment_processor', $contributionPageValues)) {
+      $this->createTestEntity('PaymentProcessor', [
+        'name' => 'dummy',
+        'label' => 'Dummy',
+        'payment_processor_type_id:name' => 'Dummy',
+        'frontend_title' => 'Dummy Front end',
+      ], 'dummy');
+      $this->createTestEntity('PaymentProcessor', [
+        'name' => 'dummy',
+        'label' => 'Dummy Test',
+        'payment_processor_type_id:name' => 'Dummy',
+        'frontend_title' => 'Dummy Front end (test)',
+        'is_test' => TRUE,
+      ], 'dummy_test');
+      $contributionPageValues['payment_processor'] = [$this->ids['PaymentProcessor']['dummy']];
+    }
     $contributionPageResult = $this->contributionPageCreate($contributionPageValues, $identifier);
     $priceSetParameters += [
       'title' => 'Price Set',
@@ -108,6 +131,127 @@ trait ContributionPageTestTrait {
     $this->fail('Could not identify ContributionPage ID');
     // Unreachable but reduces IDE noise.
     return 0;
+  }
+
+  /**
+   * Set up a contribution page with a complex price set.
+   *
+   * The created price set has 5 fields using a mixture of financial type 1 & 2, 3 which are created.
+   *
+   * More fields may be added.
+   *
+   * - Radio field (key= 'radio_field') with 3 options ('20_dollars','10_dollars','free'), financial type ID is 'first'
+   * - Select field (key= 'select_field') with 2 options ('40_dollars','30_dollars'), financial type ID is 'second'
+   * - Text field ('text_field_16.95') with amount = 16.95 - ie if qty is 2 then amount is 33.90, financial type ID is 'second'
+   * - Text field ('text_field_2.95') with amount = 2.95 - ie if qty is 2 then amount is 5.90, financial type ID is 'second'
+   * - Text field ('text_field') with amount = 1 - ie if qty is 2 then amount is 2, financial type ID is 'first'
+   * - CheckBox field ('check_box') with amount = 55,  financial type ID is 'third'
+   *
+   * @param array $contributionPageParameters
+   * @param array $priceSetParameters
+   */
+  public function contributionPageWithPriceSetCreate(array $contributionPageParameters = [], array $priceSetParameters = []): void {
+    $this->contributionPageCreatePaid($contributionPageParameters, $priceSetParameters);
+    $priceSetID = $this->ids['PriceSet']['ContributionPage'];
+    $this->createTestEntity('FinancialType', ['name' => 'Financial Type 1'], 'first');
+    $this->createTestEntity('FinancialType', ['name' => 'Financial Type 2'], 'second');
+    $this->createTestEntity('FinancialType', ['name' => 'Financial Type 3'], 'third');
+    $priceField = $this->createTestEntity('PriceField', [
+      'price_set_id' => $priceSetID,
+      'label' => 'Financial Type 1, radio field',
+      'html_type' => 'Radio',
+      'name' => 'radio_field',
+    ], 'radio_field');
+    $this->createTestEntity('PriceFieldValue', [
+      'price_field_id' => $this->ids['PriceField']['radio_field'],
+      'label' => 'Twenty dollars',
+      'financial_type_id:name' => 'Financial Type 1',
+      'amount' => 20,
+      'non_deductible_amount' => 15,
+    ], '20_dollars');
+    $this->createTestEntity('PriceFieldValue', [
+      'price_field_id' => $priceField['id'],
+      'label' => '10 dollars',
+      'financial_type_id:name' => 'Financial Type 1',
+      'amount' => 10,
+      'non_deductible_amount' => 5,
+    ], '10_dollars');
+
+    $this->createTestEntity('PriceFieldValue', [
+      'price_field_id' => $priceField['id'],
+      'label' => 'Free',
+      'financial_type_id:name' => 'Financial Type 1',
+      'amount' => 0,
+      'non_deductible_amount' => 0,
+      'name' => 'free',
+    ], 'free')['id'];
+
+    $this->createTestEntity('PriceField', [
+      'price_set_id' => $priceSetID,
+      'label' => 'Financial Type 2, select field',
+      'html_type' => 'Select',
+      'name' => 'select_field',
+    ], 'select_field');
+
+    $this->createTestEntity('PriceFieldValue', [
+      'price_field_id' => $this->ids['PriceField']['select_field'],
+      'label' => 'Forty dollars',
+      'financial_type_id:name' => 'Financial Type 2',
+      'amount' => 40,
+      'non_deductible_amount' => 5,
+    ], '40_dollars');
+
+    $this->createTestEntity('PriceFieldValue', [
+      'price_field_id' => $this->ids['PriceField']['select_field'],
+      'label' => 'Thirty dollars',
+      'financial_type_id:name' => 'Financial Type 2',
+      'amount' => 30,
+      'non_deductible_amount' => 5,
+    ], '30_dollars');
+
+    $this->createTestEntity('PriceField', [
+      'price_set_id' => $priceSetID,
+      'label' => 'Quantity * 16.95',
+      'html_type' => 'Text',
+      'name' => 'text_field_16.95',
+    ], 'text_field_16.95');
+
+    $this->createTestEntity('PriceFieldValue', [
+      'price_field_id' => $this->ids['PriceField']['text_field_16.95'],
+      'label' => 'Quantity * 16.95',
+      'financial_type_id:name' => 'Financial Type 2',
+      'amount' => '16.95',
+      'name' => 'text_field_16.95',
+    ], 'text_field_16.95');
+
+    $this->createTestEntity('PriceField', [
+      'price_set_id' => $priceSetID,
+      'label' => '2.95 text field',
+      'name' => 'text_field_2.95',
+      'html_type' => 'Text',
+    ], 'text_field_2.95');
+
+    $this->createTestEntity('PriceFieldValue', [
+      'price_field_id' => $this->ids['PriceField']['text_field_2.95'],
+      'label' => 'Quantity * 2.95',
+      'name' => 'text_field_2.95',
+      'financial_type_id:name' => 'Financial Type 2',
+      'amount' => '2.95',
+    ], 'text_field_2.95');
+
+    $this->createTestEntity('PriceField', [
+      'price_set_id' => $priceSetID,
+      'label' => 'Checkbox',
+      'name' => 'check_box',
+      'html_type' => 'CheckBox',
+    ], 'check_box');
+    $this->createTestEntity('PriceFieldValue', [
+      'price_field_id' => $this->ids['PriceField']['check_box'],
+      'label' => 'CheckBox, 55 donation',
+      'financial_type_id:name' => 'Financial Type 3',
+      'amount' => 55,
+      'name' => 'check_box',
+    ], 'check_box');
   }
 
 }
