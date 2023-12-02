7adeebb8380b6ef8b4794afe19ec3f4bdcc82cba
Rich Lott / Artful Robot <code.commits@artfulrobot.uk>
2023-12-02 23:37:41 +0000

    standalone: fix not returning absolute paths from System class
    Simple one this.

--- CRM/Utils/System/Standalone.php
+++ CRM/Utils/System/Standalone.php
@@ -205,7 +205,7 @@ class CRM_Utils_System_Standalone extends CRM_Utils_System_Base {
   ) {
     $fragment = $fragment ? ('#' . $fragment) : '';
     if ($absolute) {
-      return Civi::paths()->getUrl("[cms.root]/{$path}?{$query}$fragment");
+      return Civi::paths()->getUrl("[cms.root]/{$path}?{$query}$fragment", 'absolute');
     }
     else {
       return "/{$path}?{$query}$fragment";
