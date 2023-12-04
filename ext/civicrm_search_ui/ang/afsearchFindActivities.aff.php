fc66c9e4724179a23deb2deb6543494d89715e0c
Usha Matisson <matisson@makoa.fr>
2023-12-04 12:39:55 -0800

    Search UI afsearchFindActivities

---
+++ ext/civicrm_search_ui/ang/afsearchFindActivities.aff.php
@@ -0,0 +1,10 @@
+<?php
+use CRM_CivicrmSearchUi_ExtensionUtil as E;
+
+return [
+  'type' => 'search',
+  'title' => E::ts('Find Activities'),
+  'icon' => 'fa-list-alt',
+  'server_route' => 'civicrm/searchkit_ui/activity/search',
+  'modified_date' => '2023-12-04 12:33:30',
+];
