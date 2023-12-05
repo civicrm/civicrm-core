109a73a85502197992d2b8518df057a679c3e408
Usha Matisson <matisson@makoa.fr>
2023-12-05 11:29:33 -0800

    AdminUI : add afsearchPrintPagePDFFormats

---
+++ ext/civicrm_admin_ui/ang/afsearchPrintPagePDFFormats.aff.php
@@ -0,0 +1,10 @@
+<?php
+use CRM_CivicrmAdminUi_ExtensionUtil as E;
+
+return [
+  'type' => 'search',
+  'title' => E::ts('Print Page (PDF) Formats'),
+  'icon' => 'fa-list-alt',
+  'server_route' => 'civicrm/admin_ui/admin/pdfFormats',
+  'modified_date' => '2023-12-05 08:10:02',
+];
