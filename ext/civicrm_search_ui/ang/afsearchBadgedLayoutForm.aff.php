7f7e5b4b60cc9fa110853e28dfb295386fd6a4c4
Dewy <dewymercerais@gmail.com>
2023-12-05 10:22:34 -0800

    searchui : fix searchkit on reserved field

---
+++ ext/civicrm_search_ui/ang/afsearchBadgedLayoutForm.aff.php
@@ -0,0 +1,11 @@
+<?php
+use CRM_CivicrmSearchUi_ExtensionUtil as E;
+
+return [
+  'type' => 'search',
+  'title' => E::ts('Badged Layout Form'),
+  'description' => E::ts('Badged Layout Form'),
+  'icon' => 'fa-list-alt',
+  'server_route' => 'civicrm/searchkit_ui/admin/badgelayout',
+  'modified_date' => '2023-12-05 09:31:54',
+];
