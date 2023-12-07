8bcde0aec7b05ce760887474c4c0208a019d31db
Aidan Saunders <aidan.saunders@squiffle.uk>
2023-12-07 23:01:36 +0000

    Label formats v2

--- ext/civicrm_admin_ui/ang/afsearchLabelFormats.aff.php
+++ ext/civicrm_admin_ui/ang/afsearchLabelFormats.aff.php
@@ -4,11 +4,10 @@ use CRM_CivicrmAdminUi_ExtensionUtil as E;
 return [
   'type' => 'search',
   'title' => E::ts('Label Formats'),
-  'description' => E::ts('Label Formats'),
   'icon' => 'fa-list-alt',
   'server_route' => 'civicrm/admin/labelFormats',
   'permission' => [
     'administer CiviCRM',
   ],
-  'modified_date' => '2023-12-06 18:52:54',
+  'modified_date' => '2023-12-07 22:54:39',
 ];
