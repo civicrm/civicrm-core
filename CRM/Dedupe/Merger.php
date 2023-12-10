70fe357b67ac9f3882c46f859a2bad5ed3f3092c
colemanw <coleman@civicrm.org>
2023-12-10 15:17:23 -0500

    Joomla - Remove old link format

--- CRM/Dedupe/Merger.php
+++ CRM/Dedupe/Merger.php
@@ -48,7 +48,7 @@ class CRM_Dedupe_Merger {
         ]);
       }
       elseif ($config->userFramework === 'Joomla') {
-        $userRecordUrl = $config->userSystem->getVersion() > 1.5 ? $config->userFrameworkBaseURL . "index.php?option=com_users&view=user&task=user.edit&id=" . '%ufid' : $config->userFrameworkBaseURL . "index2.php?option=com_users&view=user&task=edit&id[]=" . '%ufid';
+        $userRecordUrl = $config->userFrameworkBaseURL . 'index.php?option=com_users&view=user&task=user.edit&id=%ufid';
         $title = ts('%1 User: %2; user id: %3', [
           1 => $config->userFramework,
           2 => '$ufname',
