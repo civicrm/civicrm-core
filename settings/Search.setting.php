ff85e96bd08673c998fc84031aa1a7cafbd4e7d1
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-12-21 19:35:33 +1300

    Clean up search settings page

--- settings/Search.setting.php
+++ settings/Search.setting.php
@@ -17,6 +17,8 @@
 /*
  * Settings metadata file
  */
+$optimizationSeeAlso = '<br/>' . ts('See also: <a href="%1">Search Optimization</a>', [1 => 'https://docs.civicrm.org/sysadmin/en/latest/setup/optimizations/" target="_blank"']);
+$searchConfigSeeAlso = '<br/>' . ts('See also: <a %1>Search Configuration Options</a>', [1 => 'https://docs.civicrm.org/en/user/latest/initial-set-up/customizing-the-user-interface/#customizing-search-preferences" target="_blank"']);
 return [
   'search_autocomplete_count' => [
     'group_name' => 'Search Preferences',
@@ -32,6 +34,7 @@ return [
     'is_contact' => 0,
     'description' => ts('The maximum number of contacts to show at a time when typing in an autocomplete field.'),
     'help_text' => NULL,
+    'settings_pages' => ['search' => ['weight' => 150]],
   ],
   'includeOrderByClause' => [
     'group_name' => 'Search Preferences',
@@ -46,6 +49,7 @@ return [
     'is_contact' => 0,
     'description' => ts('If disabled, the search results will not be ordered. This may improve response time on search results on large datasets.'),
     'help_text' => NULL,
+    'settings_pages' => ['search' => ['weight' => 60]],
   ],
   'includeWildCardInName' => [
     'group_name' => 'Search Preferences',
@@ -60,6 +64,7 @@ return [
     'is_contact' => 0,
     'description' => ts("If enabled, wildcards are automatically added to the beginning AND end of the search term when users search for contacts by Name. EXAMPLE: Searching for 'ada' will return any contact whose name includes those letters - e.g. 'Adams, Janet', 'Nadal, Jorge', etc. If disabled, a wildcard is added to the end of the search term only. EXAMPLE: Searching for 'ada' will return any contact whose last name begins with those letters - e.g. 'Adams, Janet' but NOT 'Nadal, Jorge'. Disabling this feature will speed up search significantly for larger databases, but users must manually enter wildcards ('%' or '_') to the beginning of the search term if they want to find all records which contain those letters. EXAMPLE: '%ada' will return 'Nadal, Jorge'."),
     'help_text' => NULL,
+    'settings_pages' => ['search' => ['weight' => 10]],
   ],
   'includeEmailInName' => [
     'group_name' => 'Search Preferences',
@@ -74,6 +79,7 @@ return [
     'is_contact' => 0,
     'description' => ts('If enabled, email addresses are automatically included when users search by Name. Disabling this feature will speed up search significantly for larger databases, but users will need to use the Email search fields (from Advanced Search, Search Builder, or Profiles) to find contacts by email address.'),
     'help_text' => NULL,
+    'settings_pages' => ['search' => ['weight' => 20]],
   ],
   'includeNickNameInName' => [
     'group_name' => 'Search Preferences',
@@ -88,6 +94,7 @@ return [
     'is_contact' => 0,
     'description' => ts('If enabled, nicknames are automatically included when users search by Name.'),
     'help_text' => NULL,
+    'settings_pages' => ['search' => ['weight' => 40]],
   ],
   'includeAlphabeticalPager' => [
     'group_name' => 'Search Preferences',
@@ -102,6 +109,7 @@ return [
     'is_contact' => 0,
     'description' => ts('If disabled, the alphabetical pager will not be displayed on the search screens. This will improve response time on search results on large datasets.'),
     'help_text' => NULL,
+    'settings_pages' => ['search' => ['weight' => 50]],
   ],
   'smartGroupCacheTimeout' => [
     'group_name' => 'Search Preferences',
@@ -115,8 +123,9 @@ return [
     'title' => ts('Smart group cache timeout'),
     'is_domain' => 1,
     'is_contact' => 0,
-    'description' => ts('The number of minutes to cache smart group contacts. We strongly recommend that this value be greater than zero, since a value of zero means no caching at all. If your contact data changes frequently, you should set this value to at least 5 minutes.'),
-    'help_text' => NULL,
+    'description' => ts('The number of minutes to cache smart group contacts. The best value will depend on your site and the complexity of the groups and acls you use. A value of zero means no caching at all. You may need to experiment with this.') . $optimizationSeeAlso,
+    'help_text' => '',
+    'settings_pages' => ['search' => ['weight' => 80]],
   ],
   'defaultSearchProfileID' => [
     'group_name' => 'Search Preferences',
@@ -138,6 +147,7 @@ return [
     'is_contact' => 0,
     'description' => ts('If set, this will be the default profile used for contact search.'),
     'help_text' => NULL,
+    'settings_pages' => ['search' => ['weight' => 70]],
   ],
   'prevNextBackend' => [
     'group_name' => 'Search Preferences',
@@ -159,6 +169,9 @@ return [
     ],
     'description' => ts('When performing a search, how should the search-results be cached?'),
     'help_text' => '',
+    // Not exposed in UI as breakage possible. As with the SmartGroupCache time out a different page
+    // might make more sense.
+    'settings_pages' => [],
   ],
   'searchPrimaryDetailsOnly' => [
     'group_name' => 'Search Preferences',
@@ -173,6 +186,7 @@ return [
     'is_contact' => 0,
     'description' => ts('If enabled, only primary details (eg contact\'s primary email, phone, etc) will be included in Basic and Advanced Search results. Disabling this feature will allow users to match contacts using any email, phone etc detail.'),
     'help_text' => NULL,
+    'settings_pages' => ['search' => ['weight' => 30]],
   ],
   'quicksearch_options' => [
     'group_name' => 'Search Preferences',
@@ -203,6 +217,7 @@ return [
     'is_contact' => 0,
     'description' => ts("Which fields can be searched on in the menubar quicksearch box? Don't see your custom field here? Make sure it is marked as Searchable."),
     'help_text' => NULL,
+    'settings_pages' => ['search' => ['weight' => 90]],
   ],
   'default_pager_size' => [
     'group_name' => 'Search Preferences',
