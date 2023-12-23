ff1d16afe51baee0bee10f92cb6f0fe89b7cc84d
Usha Matisson <matisson@makoa.fr>
2023-12-23 15:27:49 +0100

    Update ext/civicrm_admin_ui/managed/SavedSearch_Print_Page_PDF_Formats.mgd.php
    Co-authored-by: colemanw <coleman@civicrm.org>

--- ext/civicrm_admin_ui/managed/SavedSearch_Print_Page_PDF_Formats.mgd.php
+++ ext/civicrm_admin_ui/managed/SavedSearch_Print_Page_PDF_Formats.mgd.php
@@ -165,7 +165,7 @@ return [
               'entity' => '',
               'action' => '',
               'join' => '',
-              'target' => '',
+              'target' => 'crm-popup',
             ],
           ],
           'cssRules' => [
