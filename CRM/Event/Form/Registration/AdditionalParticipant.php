8831c54f43763380009534d56e31c52ad091e445
eileen <emcnaughton@wikimedia.org>
2024-01-02 09:52:45 +1300

    Fix error due to merges co-inciding

--- CRM/Event/Form/Registration/AdditionalParticipant.php
+++ CRM/Event/Form/Registration/AdditionalParticipant.php
@@ -67,14 +67,6 @@ class CRM_Event_Form_Registration_AdditionalParticipant extends CRM_Event_Form_R
   public $_pId;
 
   /**
-   * Tracks whether we are at the last participant.
-   *
-   * @var bool
-   * @internal
-   */
-  public $_lastParticipant = FALSE;
-
-  /**
    * Get the active UFGroups (profiles) on this form
    * Many forms load one or more UFGroups (profiles).
    * This provides a standard function to retrieve the IDs of those profiles from the form
@@ -808,7 +800,7 @@ class CRM_Event_Form_Registration_AdditionalParticipant extends CRM_Event_Form_R
    * @param bool $isButtonJs
    *
    * @return bool
-   *   ture on success.
+   *   true on success.
    */
   public function isLastParticipant($isButtonJs = FALSE) {
     $participant = $isButtonJs ? $this->_params[0]['additional_participants'] : $this->_params[0]['additional_participants'] + 1;
