d222986913db1d837bdf42c4716bdd7bf9642cef
Stephen Palmstrom <stephen.palmstrom@missionassist.org.uk>
2024-01-02 17:47:22 +0000

    Following suggested additional changes.

--- CRM/Mailing/Event/BAO/MailingEventForward.php
+++ CRM/Mailing/Event/BAO/MailingEventForward.php
@@ -233,11 +233,6 @@ class CRM_Mailing_Event_BAO_MailingEventForward extends CRM_Mailing_Event_DAO_Ma
 
     // get the formatted location blocks into params - w/ 3.0 format, CRM-4605
     if (!empty($values['location_type_id'])) {
-      $fields = NULL;
-      if ($fields == NULL) {
-        $fields = [];
-      }
-
       foreach (['Phone', 'Email', 'IM', 'OpenID', 'Phone_Ext'] as $block) {
         $name = strtolower($block);
         if (!array_key_exists($name, $values)) {
