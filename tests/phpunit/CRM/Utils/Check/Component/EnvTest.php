918ae179d73b7229eceaa638d88ddccdd4294b4e
eileen <emcnaughton@wikimedia.org>
2024-01-04 17:04:51 +1300

    Add test for logging report status check

--- tests/phpunit/CRM/Utils/Check/Component/EnvTest.php
+++ tests/phpunit/CRM/Utils/Check/Component/EnvTest.php
@@ -8,9 +8,11 @@
  */
 class CRM_Utils_Check_Component_EnvTest extends CiviUnitTestCase {
 
-  public function setUp(): void {
-    parent::setUp();
-    $this->useTransaction();
+  public function tearDown(): void {
+    Civi::settings()->set('logging', FALSE);
+    $this->callAPISuccess('Extension', 'enable', ['key' => 'civi_report']);
+    $this->callAPISuccess('Extension', 'enable', ['key' => 'civi_mail']);
+    parent::tearDown();
   }
 
   /**
@@ -22,9 +24,43 @@ class CRM_Utils_Check_Component_EnvTest extends CiviUnitTestCase {
     $failRequest = $check->fileExists('https://civicrm.org', 0.001);
     $successRequest = $check->fileExists('https://civicrm.org', 0);
 
-    $this->assertEquals(FALSE, $failRequest, 'Request should fail for minimum timeout.');
-    $this->assertEquals(TRUE, $successRequest, 'Request should not fail for infinite timeout.');
+    $this->assertFalse($failRequest, 'Request should fail for minimum timeout.');
+    $this->assertTrue($successRequest, 'Request should not fail for infinite timeout.');
+  }
 
+  /**
+   * Test the check that warns users if they have enabled logging but not Civi-report.
+   *
+   * The check should not be triggered if logging is not enabled or it
+   * is enabled and civi-report is enabled.
+   *
+   * @return void
+   */
+  public function testLoggingWithReport(): void {
+    $this->callAPISuccess('Extension', 'disable', ['key' => 'civi_report']);
+    $this->assertFalse($this->checkChecks('checkLoggingHasCiviReport'));
+
+    Civi::settings()->set('logging', 1);
+    $check = $this->checkChecks('checkLoggingHasCiviReport');
+    $this->assertEquals('CiviReport required to display detailed logging.', $check['title']);
+
+    $this->callAPISuccess('Extension', 'enable', ['key' => 'civi_report']);
+    $this->assertFalse($this->checkChecks('checkLoggingHasCiviReport'));
+  }
+
+  /**
+   * Check the checks are checking for the checky thing.
+   *
+   * @return bool|array
+   */
+  public function checkChecks($checkName) {
+    $checks = $this->callAPISuccess('System', 'check');
+    foreach ($checks['values'] as $check) {
+      if ($check['name'] === $checkName) {
+        return $check;
+      }
+    }
+    return FALSE;
   }
 
 }
