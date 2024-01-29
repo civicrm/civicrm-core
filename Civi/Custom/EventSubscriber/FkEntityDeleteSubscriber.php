b6d92e274138a6264bb936e064ecca84c24098fc
Dominic Tubach <tubach@systopia.de>
2024-01-29 10:27:46 +0100

    fixup! Allow cascade delete for custom entity reference fields
    Use `CRM_Core_BAO_CustomGroup::getAll()`.

--- Civi/Custom/EventSubscriber/FkEntityDeleteSubscriber.php
+++ Civi/Custom/EventSubscriber/FkEntityDeleteSubscriber.php
@@ -3,8 +3,6 @@ declare(strict_types = 1);
 
 namespace Civi\Custom\EventSubscriber;
 
-use Civi\Api4\CustomField;
-use Civi\Api4\Generic\Result;
 use Civi\Core\Event\PreEvent;
 use Civi\Core\Service\AutoSubscriber;
 
@@ -49,32 +47,37 @@ final class FkEntityDeleteSubscriber extends AutoSubscriber {
    *   Entity name mapped to custom entity reference field name.
    */
   private function getReferenceFieldNames(string $fkEntity, string $onDelete, bool $onlySoftDelete): iterable {
-    foreach ($this->getCustomFields($fkEntity, $onDelete, $onlySoftDelete) as $customField) {
-      yield $customField['custom_group_id.extends'] => $customField['custom_group_id.name'] . '.' . $customField['name'];
+    $fkEntities = $this->getFkEntityTypes($fkEntity);
+
+    foreach (\CRM_Core_BAO_CustomGroup::getAll() as $group) {
+      foreach ($group['fields'] as $field) {
+        if ($field['data_type'] === 'EntityReference' && $field['fk_entity_on_delete'] === $onDelete
+          && (!$onlySoftDelete || $field['is_on_delete_includes_soft_delete'])
+          && in_array($field['fk_entity'], $fkEntities, TRUE)
+        ) {
+          yield $group['extends'] => $group['name'] . '.' . $field['name'];
+        }
+      }
     }
   }
 
-  private function getCustomFields(string $fkEntity, string $onDelete, bool $onlySoftDelete): Result {
-    $action = CustomField::get(FALSE)
-      ->setSelect([
-        'custom_group_id.extends',
-        'custom_group_id.name',
-        'name',
-      ])->addWhere('data_type', '=', 'EntityReference')
-      ->addWhere('fk_entity_on_delete', '=', $onDelete);
+  /**
+   * @phpstan-return list<string>
+   */
+  private function getFkEntityTypes(string $fkEntity): array {
+    $contactTypes = \CRM_Contact_BAO_ContactType::basicTypes(TRUE);
 
-    if (in_array($fkEntity, ['Individual', 'Organization', 'Household'], TRUE)) {
-      $action->addWhere('fk_entity', 'IN', [$fkEntity, 'Contact']);
-    }
-    else {
-      $action->addWhere('fk_entity', '=', $fkEntity);
+    // "Contact" should include all contact types (Individual, Organization, Household)
+    if ($fkEntity === 'Contact') {
+      return array_merge(['Contact'], $contactTypes);
     }
 
-    if ($onlySoftDelete) {
-      $action->addWhere('is_on_delete_includes_soft_delete', '=', TRUE);
+    // A contact type (e.g. "Individual") should include "Contact"
+    if (in_array($fkEntity, $contactTypes, TRUE)) {
+      return ['Contact', $fkEntity];
     }
 
-    return $action->execute();
+    return [$fkEntity];
   }
 
 }
