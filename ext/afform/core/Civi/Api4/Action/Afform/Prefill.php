bd86a8c9697fe8357c0cb90f2997a16aef1fd219
colemanw <coleman@civicrm.org>
2024-02-01 15:49:58 -0500

    Afform - Extract handleMatchField function for clarity

--- ext/afform/core/Civi/Api4/Action/Afform/Prefill.php
+++ ext/afform/core/Civi/Api4/Action/Afform/Prefill.php
@@ -21,18 +21,31 @@ class Prefill extends AbstractProcessor {
     foreach ($entityValues as $afformEntityName => &$valueSets) {
       $afformEntity = $this->_formDataModel->getEntity($afformEntityName);
       if ($this->matchField) {
-        $matchFieldDefn = $this->_formDataModel->getField($afformEntity['type'], $this->matchField, 'create');
-        // When creating an entity based on an existing one e.g. Event.template_id
-        if (($matchFieldDefn['input_attrs']['autofill'] ?? NULL) === 'create') {
-          $idField = CoreUtil::getIdFieldName($afformEntity['type']);
-          foreach ($valueSets as &$valueSet) {
-            $valueSet['fields'][$this->matchField] = $valueSet['fields'][$idField];
-            unset($valueSet['fields'][$idField]);
-          }
-        }
+        $this->handleMatchField($afformEntity['type'], $valueSets);
       }
     }
     return \CRM_Utils_Array::makeNonAssociative($entityValues, 'name', 'values');
+  }
+
+  /**
+   * Set entity values based on an existing record.
+   *
+   * This is used for e.g. Event.template_id,
+   * based on 'autofill' => 'create' metadata in APIv4 getFields.
+   *
+   * @param string $entityType
+   * @param array $valueSets
+   */
+  private function handleMatchField(string $entityType, array &$valueSets): void {
+    $matchFieldDefn = $this->_formDataModel->getField($entityType, $this->matchField, 'create');
+    // @see EventCreationSpecProvider for the `template_id` declaration which includes this 'autofill' = 'create' flag.
+    if (($matchFieldDefn['input_attrs']['autofill'] ?? NULL) === 'create') {
+      $idField = CoreUtil::getIdFieldName($entityType);
+      foreach ($valueSets as &$valueSet) {
+        $valueSet['fields'][$this->matchField] = $valueSet['fields'][$idField];
+        unset($valueSet['fields'][$idField]);
+      }
+    }
   }
 
 }
