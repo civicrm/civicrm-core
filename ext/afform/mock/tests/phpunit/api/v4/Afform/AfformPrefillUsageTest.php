6f16ac87917ddc5f38644765b9ba923fff03f32c
colemanw <coleman@civicrm.org>
2024-02-01 20:01:53 -0500

    Afform - Format DisplayOnly fields as view value
    Fixes dev/core#3779

--- ext/afform/mock/tests/phpunit/api/v4/Afform/AfformPrefillUsageTest.php
+++ ext/afform/mock/tests/phpunit/api/v4/Afform/AfformPrefillUsageTest.php
@@ -74,11 +74,12 @@ EOHTML;
     // One email should have been filled
     $this->assertCount(1, $prefill['Individual1']['values'][1]['joins']['Email']);
     $this->assertEquals('b@afform.test', $prefill['Individual1']['values'][1]['joins']['Email'][0]['email']);
-    $this->assertEmpty($prefill['Individual1']['values'][0]['joins']['Email']);
+    $joins = $prefill['Individual1']['values'][0]['joins'];
+    $this->assertEmpty($joins['Email']);
     $this->assertEmpty($prefill['Individual1']['values'][2]['joins']['Email']);
 
     // Phone join has `max="2"`
-    $this->assertCount(2, $prefill['Individual1']['values'][0]['joins']['Phone']);
+    $this->assertCount(2, $joins['Phone']);
     $this->assertCount(1, $prefill['Individual1']['values'][2]['joins']['Phone']);
     $this->assertEquals('2-1', $prefill['Individual1']['values'][2]['joins']['Phone'][0]['phone']);
     $this->assertEmpty($prefill['Individual1']['values'][1]['joins']['Phone']);
@@ -163,6 +164,70 @@ EOHTML;
     $this->assertContains('Co', array_column($parents, 'first_name'));
     $this->assertContains($uid, array_column($parents, 'id'));
     $this->assertContains($cid[0], array_column($parents, 'id'));
+  }
+
+  public function testPrefillWithDisplayOnlyFields(): void {
+
+    $layout = <<<EOHTML
+<af-form ctrl="afform">
+  <af-entity data="{contact_type: 'Individual'}" type="Contact" name="Individual1" label="Individual 1" actions="{create: true, update: true}" security="RBAC"  />
+  <fieldset af-fieldset="Individual1" class="af-container" af-title="Individual 1" min="1" af-repeat="Add">
+    <af-field name="id" />
+    <afblock-name-individual />
+    <af-field name="gender_id" defn="{input_type: 'DisplayOnly', input_attrs: {}}" />
+    <af-field name="is_opt_out" defn="{input_type: 'DisplayOnly', input_attrs: {}}" />
+    <af-field name="birth_date" defn="{input_type: 'DisplayOnly', input_attrs: {}}" />
+    <div af-join="Address" af-repeat="Add" min="1" max="2">
+      <af-field name="country_id" defn="{input_type: 'DisplayOnly', input_attrs: {}}" />
+      <af-field name="state_province_id" defn="{input_type: 'DisplayOnly', input_attrs: {}}" />
+      <af-field name="is_primary" defn="{input_type: 'DisplayOnly', input_attrs: {}}" />
+    </div>
+  </fieldset>
+  <button class="af-button btn btn-primary" crm-icon="fa-check" ng-click="afform.submit()">Submit</button>
+</af-form>
+EOHTML;
+
+    $this->useValues([
+      'layout' => $layout,
+      'permission' => \CRM_Core_Permission::ALWAYS_ALLOW_PERMISSION,
+    ]);
+
+    $cid = $this->saveTestRecords('Individual', [
+      'records' => [
+        ['first_name' => 'One', 'last_name' => 'Name', 'gender_id:name' => 'Female', 'is_opt_out' => TRUE, 'birth_date' => '01-02-2001'],
+        ['first_name' => 'Two', 'last_name' => 'Name', 'gender_id:name' => 'Male', 'is_opt_out' => FALSE],
+      ],
+    ])->column('id');
+
+    $this->saveTestRecords('Address', [
+      'records' => [
+        ['contact_id' => $cid[0], 'country_id:name' => 'United States', 'state_province_id:name' => 'California', 'is_primary' => TRUE],
+        ['contact_id' => $cid[0], 'country_id:name' => 'Canada', 'state_province_id:name' => 'Manitoba', 'is_primary' => FALSE],
+      ],
+    ]);
+
+    $prefill = Afform::prefill()
+      ->setName($this->formName)
+      ->setArgs(['Individual1' => $cid])
+      ->execute()
+      ->indexBy('name');
+    $this->assertCount(2, $prefill['Individual1']['values']);
+    // First contact
+    $this->assertEquals('One', $prefill['Individual1']['values'][0]['fields']['first_name']);
+    $this->assertEquals('Female', $prefill['Individual1']['values'][0]['fields']['gender_id']);
+    $this->assertEquals('Yes', $prefill['Individual1']['values'][0]['fields']['is_opt_out']);
+    $this->assertEquals('February 1st, 2001', $prefill['Individual1']['values'][0]['fields']['birth_date']);
+    $joins = $prefill['Individual1']['values'][0]['joins'];
+    $this->assertEquals('United States', $joins['Address'][0]['country_id']);
+    $this->assertEquals('California', $joins['Address'][0]['state_province_id']);
+    $this->assertEquals('Yes', $joins['Address'][0]['is_primary']);
+    $this->assertEquals('Canada', $joins['Address'][1]['country_id']);
+    $this->assertEquals('Manitoba', $joins['Address'][1]['state_province_id']);
+    $this->assertEquals('No', $joins['Address'][1]['is_primary']);
+    // Second contact
+    $this->assertEquals('Two', $prefill['Individual1']['values'][1]['fields']['first_name']);
+    $this->assertEquals('Male', $prefill['Individual1']['values'][1]['fields']['gender_id']);
+    $this->assertEquals('No', $prefill['Individual1']['values'][1]['fields']['is_opt_out']);
   }
 
 }
