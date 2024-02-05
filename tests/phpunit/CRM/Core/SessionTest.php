625e0f258cbd5bb342791db115dca7b9218437c7
eileen <emcnaughton@wikimedia.org>
2024-02-06 10:54:02 +1300

    Remove clearAllVars from SessionTest

--- tests/phpunit/CRM/Core/SessionTest.php
+++ tests/phpunit/CRM/Core/SessionTest.php
@@ -8,27 +8,30 @@ class CRM_Core_SessionTest extends CiviUnitTestCase {
 
   public function setUp(): void {
     parent::setUp();
-    CRM_Core_Smarty::singleton()->clearTemplateVars();
     // set null defaults
     foreach (['infoOptions', 'infoType', 'infoMessage', 'infoTitle'] as $info) {
-      CRM_Core_Smarty::singleton()->assign($info, NULL);
+      CRM_Core_Smarty::singleton()->assign($info);
     }
   }
 
   /**
    * Test that the template setStatus uses gives reasonable output.
    * Test with text only.
+   *
+   * @throws \SmartyException
    */
   public function testSetStatusWithTextOnly(): void {
     $smarty = CRM_Core_Smarty::singleton();
-    $smarty->assign('infoMessage', 'Your refridgerator door is open.');
+    $smarty->assign('infoMessage', 'Your refrigerator door is open.');
     $output = $smarty->fetch('CRM/common/info.tpl');
-    $this->assertStringContainsString('Your refridgerator door is open.', $output);
+    $this->assertStringContainsString('Your refrigerator door is open.', $output);
   }
 
   /**
    * Test that the template setStatus uses gives reasonable output.
    * Test with title only.
+   *
+   * @throws \SmartyException
    */
   public function testSetStatusWithTitleOnly(): void {
     $smarty = CRM_Core_Smarty::singleton();
@@ -40,14 +43,16 @@ class CRM_Core_SessionTest extends CiviUnitTestCase {
   /**
    * Test that the template setStatus uses gives reasonable output.
    * Test with both text and title.
+   *
+   * @throws \SmartyException
    */
   public function testSetStatusWithBoth(): void {
     $smarty = CRM_Core_Smarty::singleton();
     $smarty->assign('infoTitle', 'Spoiler alert!');
-    $smarty->assign('infoMessage', 'Your refridgerator door is open.');
+    $smarty->assign('infoMessage', 'Your refrigerator door is open.');
     $output = $smarty->fetch('CRM/common/info.tpl');
     $this->assertStringContainsString('Spoiler alert!', $output);
-    $this->assertStringContainsString('Your refridgerator door is open.', $output);
+    $this->assertStringContainsString('Your refrigerator door is open.', $output);
   }
 
 }
