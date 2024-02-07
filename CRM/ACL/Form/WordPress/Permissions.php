87d6738f5ed88e281102949781cff1fd4cf00626
colemanw <coleman@civicrm.org>
2024-02-07 10:17:00 -0500

    WP - Handle updated permissions format from civicrm/civicrm-core#29173

--- CRM/ACL/Form/WordPress/Permissions.php
+++ CRM/ACL/Form/WordPress/Permissions.php
@@ -68,8 +68,8 @@ class CRM_ACL_Form_WordPress_Permissions extends CRM_Core_Form {
 
     $descArray = [];
     foreach ($permissionsDesc as $perm => $attr) {
-      if (count($attr) > 1) {
-        $descArray[$perm] = $attr['description'] ?? $attr[1];
+      if (!empty($attr['description'])) {
+        $descArray[$perm] = $attr['description'];
       }
     }
 
