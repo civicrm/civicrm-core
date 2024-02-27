6a16a1e59480bf5a6c062f5ee9fa569f0c125810
Tim Otten <totten@civicrm.org>
2024-02-27 13:37:36 -0500

    oembed - Skeletal civicrm/oembed

---
+++ ext/oembed/CRM/Oembed/Page/Oembed.php
@@ -0,0 +1,16 @@
+<?php
+use CRM_Oembed_ExtensionUtil as E;
+
+class CRM_Oembed_Page_Oembed extends CRM_Core_Page {
+
+  public function run() {
+    // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
+    CRM_Utils_System::setTitle(E::ts('Oembed'));
+
+    // Example: Assign a variable for use in a template
+    $this->assign('currentTime', date('Y-m-d H:i:s'));
+
+    parent::run();
+  }
+
+}
