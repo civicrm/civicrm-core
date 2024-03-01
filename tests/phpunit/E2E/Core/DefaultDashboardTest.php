b13dfd15b4e90087b6c16106d4fa429f9080bff6
Jon Goldberg <jon@megaphonetech.com>
2024-03-01 12:27:14 -0500

    refactor tests for best practice

--- tests/phpunit/E2E/Core/DefaultDashboardTest.php
+++ tests/phpunit/E2E/Core/DefaultDashboardTest.php
@@ -3,11 +3,7 @@
 namespace E2E\Core;
 
 use Civi;
-<<<<<<< HEAD
 use Civi\Api4\Group;
-=======
-
->>>>>>> d62ed1633f (mink WIP)
 
 /**
  * Class DefaultDashboardTest
@@ -18,7 +14,6 @@ use Civi\Api4\Group;
 class DefaultDashboardTest extends \MinkBase {
 
   public function testDashboard() {
-<<<<<<< HEAD
     $session = $this->mink->getSession();
     $page = $session->getPage();
 
@@ -58,18 +53,18 @@ class DefaultDashboardTest extends \MinkBase {
     // Delete all groups.
     Group::delete(FALSE)->addWhere('id', 'IN', [$gid1, $gid2])->execute();
   }
-=======
+    $session = $this->mink->getSession();
+    $page = $session->getPage();
+
     $this->login($GLOBALS['_CV']['ADMIN_USER']);
     file_put_contents('/tmp/test-login.png', $this->mink->getSession()->getDriver()->getScreenshot());
 
     $this->visit(Civi::url('backend://civicrm/dashboard'));
-    $this->mink->getSession()->wait(5000, "document.getElementsByClassName('crm-hover-button').length");
-    $inactiveDashletLink = $this->mink->getSession()->getPage()->find('xpath', '//a[contains(@class, "crm-hover-button")]');
-    $inactiveDashletLink->click();
+    $session->wait(5000, "document.getElementsByClassName('crm-hover-button').length");
+    $inactiveDashletLink = $page->find('xpath', '//a[contains(@class, "crm-hover-button")]')->click();
     
     file_put_contents('/tmp/test-dashboard.png', $this->mink->getSession()->getDriver()->getScreenshot());
     $this->assertSession()->pageTextContains('Event Income Summary');
   }
 
->>>>>>> d62ed1633f (mink WIP)
 }
