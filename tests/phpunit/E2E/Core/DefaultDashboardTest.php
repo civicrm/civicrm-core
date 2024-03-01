1aea3279cb4a09737a06a8dee7c74f9ff0af5f3c
Jon Goldberg <jon@megaphonetech.com>
2024-03-01 17:51:10 -0500

    manage groups WIP

--- tests/phpunit/E2E/Core/DefaultDashboardTest.php
+++ tests/phpunit/E2E/Core/DefaultDashboardTest.php
@@ -3,7 +3,7 @@
 namespace E2E\Core;
 
 use Civi;
-
+use Civi\Api4\Group;
 
 /**
  * Class DefaultDashboardTest
@@ -19,7 +19,6 @@ class DefaultDashboardTest extends \MinkBase {
 
     $this->login($GLOBALS['_CV']['ADMIN_USER']);
     $this->createScreenshot('/tmp/test-login.png');
-
     $this->visit(Civi::url('backend://civicrm/dashboard'));
     $session->wait(5000, "document.getElementsByClassName('crm-hover-button').length");
     $page->find('xpath', '//a[contains(@class, "crm-hover-button")]')->click();
@@ -27,4 +26,31 @@ class DefaultDashboardTest extends \MinkBase {
     $this->assertSession()->pageTextContains('Event Income Summary');
   }
 
+  public function testManageGroups() {
+    $session = $this->mink->getSession();
+    $page = $session->getPage();
+
+    $this->login($GLOBALS['_CV']['ADMIN_USER']);
+    $gid1 = Group::create(FALSE)
+      ->addValue('name', 'group1')
+      ->addValue('title', 'Group 1')
+      ->addValue('description', 'This is a basic group.')
+      ->execute()[0]['id'];
+    $gid2 = Group::create(FALSE)
+      ->addValue('name', 'mailing_group')
+      ->addValue('title', 'A Mailing Group')
+      ->addValue('description', 'This is a mailing group.')
+      ->addValue('group_type:name', ['Mailing List'])
+      ->execute()[0]['id'];
+
+    $this->visit(Civi::url('backend://civicrm/group'));
+    $session->wait(5000, 'document.querySelector("afsearch-manage-groups") != null');
+    $this->createScreenshot('/tmp/manage-groups-1.png');
+    $afformTable = $page->find('xpath', '//afsearch-manage-groups//table');
+    // $group1Row = $afformTable->find('xpath', '//tr[contains(text(), "Group 1")]');
+    $group1Row = $afformTable->find('xpath', '//*[contains(text(), "Group 1")]/ancestor::tr');
+    // $rows->find('named', ['content', 'Group 1']);
+    // Delete all groups.
+    Group::delete(FALSE)->addWhere('id', 'IN', [$gid1, $gid2])->execute();
+  }
 }
