3bd80095ecfdd8efa91a65f719495ddde461d62d
eileen <emcnaughton@wikimedia.org>
2024-03-12 09:05:05 +1300

    Ditch all those variable variables on pledge dashboard

--- CRM/Pledge/Page/DashBoard.php
+++ CRM/Pledge/Page/DashBoard.php
@@ -28,48 +28,54 @@ class CRM_Pledge_Page_DashBoard extends CRM_Core_Page {
   public function preProcess() {
     CRM_Utils_System::setTitle(ts('CiviPledge'));
 
-    $startToDate = [];
-    $yearToDate = [];
-    $monthToDate = [];
-    $previousToDate = [];
-
-    $prefixes = ['start', 'month', 'year', 'previous'];
-    $status = ['Completed', 'Cancelled', 'Pending', 'In Progress', 'Overdue'];
-
-    // cumulative (since inception) - prefix = 'start'
-    $startDate = NULL;
-    $startDateEnd = NULL;
+    $this->assign('startToDate', [
+      'Completed' => CRM_Pledge_BAO_Pledge::getTotalAmountAndCount('Completed'),
+      'Cancelled' => CRM_Pledge_BAO_Pledge::getTotalAmountAndCount('Cancelled'),
+      'Pending' => CRM_Pledge_BAO_Pledge::getTotalAmountAndCount('Pending'),
+      'InProgress' => CRM_Pledge_BAO_Pledge::getTotalAmountAndCount('In Progress'),
+      'Overdue' => CRM_Pledge_BAO_Pledge::getTotalAmountAndCount('Overdue'),
+    ]);
 
     // current year - prefix = 'year'
     $yearDate = \Civi::settings()->get('fiscalYearStart');
     $year = ['Y' => date('Y')];
     $this->assign('curYear', $year['Y']);
     $yearDate = array_merge($year, $yearDate);
-    $yearDate = CRM_Utils_Date::format($yearDate);
-    $yearDate = $yearDate . '000000';
+    $yearDate = CRM_Utils_Date::format($yearDate) . '000000';
     $yearDateEnd = $year['Y'] . '1231235959';
+    $this->assign('yearToDate', [
+      'Completed' => CRM_Pledge_BAO_Pledge::getTotalAmountAndCount('Completed', $yearDate, $yearDateEnd),
+      'Cancelled' => CRM_Pledge_BAO_Pledge::getTotalAmountAndCount('Cancelled', $yearDate, $yearDateEnd),
+      'Pending' => CRM_Pledge_BAO_Pledge::getTotalAmountAndCount('Pending', $yearDate, $yearDateEnd),
+      'InProgress' => CRM_Pledge_BAO_Pledge::getTotalAmountAndCount('In Progress', $yearDate, $yearDateEnd),
+      'Overdue' => CRM_Pledge_BAO_Pledge::getTotalAmountAndCount('Overdue', $yearDate, $yearDateEnd),
+    ]);
 
     // current month - prefix = 'month'
     $currentMonth = date("F Y", mktime(0, 0, 0, date("m"), 01, date("Y")));
     $this->assign('currentMonthYear', $currentMonth);
     $monthDate = date('Ym') . '01000000';
     $monthDateEnd = CRM_Utils_Date::customFormat(date("Y-m-t", mktime(0, 0, 0, date("m"), 01, date("Y"))), '%Y%m%d') . '235959';
+    $this->assign('monthToDate', [
+      'Completed' => CRM_Pledge_BAO_Pledge::getTotalAmountAndCount('Completed', $monthDate, $monthDateEnd),
+      'Cancelled' => CRM_Pledge_BAO_Pledge::getTotalAmountAndCount('Cancelled', $monthDate, $monthDateEnd),
+      'Pending' => CRM_Pledge_BAO_Pledge::getTotalAmountAndCount('Pending', $monthDate, $monthDateEnd),
+      'InProgress' => CRM_Pledge_BAO_Pledge::getTotalAmountAndCount('In Progress', $monthDate, $monthDateEnd),
+      'Overdue' => CRM_Pledge_BAO_Pledge::getTotalAmountAndCount('Overdue', $monthDate, $monthDateEnd),
+    ]);
 
     // previous month - prefix = 'previous'
     $previousDate = CRM_Utils_Date::customFormat(date("Y-m-d", mktime(0, 0, 0, date("m") - 1, 01, date("Y"))), '%Y%m%d') . '000000';
     $previousDateEnd = CRM_Utils_Date::customFormat(date("Y-m-t", mktime(0, 0, 0, date("m") - 1, 01, date("Y"))), '%Y%m%d') . '235959';
     $previousMonth = date("F Y", mktime(0, 0, 0, date("m") - 1, 01, date("Y")));
     $this->assign('previousMonthYear', $previousMonth);
-
-    foreach ($prefixes as $prefix) {
-      $aName = $prefix . 'ToDate';
-      $startName = $prefix . 'Date';
-      $endName = $prefix . 'DateEnd';
-      foreach ($status as $s) {
-        ${$aName}[str_replace(" ", "", $s)] = CRM_Pledge_BAO_Pledge::getTotalAmountAndCount($s, $$startName, $$endName);
-      }
-      $this->assign($aName, $$aName);
-    }
+    $this->assign('previousToDate', [
+      'Completed' => CRM_Pledge_BAO_Pledge::getTotalAmountAndCount('Completed', $previousDate, $previousDateEnd),
+      'Cancelled' => CRM_Pledge_BAO_Pledge::getTotalAmountAndCount('Cancelled', $previousDate, $previousDateEnd),
+      'Pending' => CRM_Pledge_BAO_Pledge::getTotalAmountAndCount('Pending', $previousDate, $previousDateEnd),
+      'InProgress' => CRM_Pledge_BAO_Pledge::getTotalAmountAndCount('In Progress', $previousDate, $previousDateEnd),
+      'Overdue' => CRM_Pledge_BAO_Pledge::getTotalAmountAndCount('Overdue', $previousDate, $previousDateEnd),
+    ]);
   }
 
   /**
