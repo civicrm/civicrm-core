639baefb8f2074fc02d63a9f514029c1f646f07b
Tim Otten <totten@civicrm.org>
2024-03-26 17:44:00 -0700

    civimix-schema - Migrate SqlGenerator. Use it to implement SchemaHelper.

--- mixin/lib/civimix-schema/src/CiviMixSchema.php
+++ mixin/lib/civimix-schema/src/CiviMixSchema.php
@@ -13,8 +13,12 @@ return new class() {
    * @return \CiviMix\Schema\SchemaHelperInterface
    */
   public function getHelper(string $extensionKey) {
-    throw new \RuntimeException("Not implemented");
-    return NULL;
+    $store = &\Civi::$statics['CiviMixSchema-helpers'];
+    if (!isset($store[$extensionKey])) {
+      $class = get_class(require __DIR__ . '/SchemaHelper.php');
+      $store[$extensionKey] = new $class($extensionKey);
+    }
+    return $store[$extensionKey];
   }
 
 };
