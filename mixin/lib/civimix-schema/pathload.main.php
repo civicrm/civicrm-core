d4e8256668d4378ae85e1e737f3cbf9e7993e3a6
Tim Otten <totten@civicrm.org>
2024-03-26 17:28:30 -0700

    civimix-schema - Add skeleton library with `SchemaHelperInterface`

---
+++ mixin/lib/civimix-schema/pathload.main.php
@@ -0,0 +1,18 @@
+<?php
+namespace CiviMix\Schema;
+
+\pathload()->activatePackage('civimix-schema@5', __DIR__, [
+  'reloadable' => TRUE,
+  // The civimix-schema library specifically supports installation processes. From a
+  // bootstrap/service-availability POV, this is a rough environment which leads to
+  // the "Multi-Activation Issue" and "Multi-Download Issue". To adapt to them,
+  // civimix-schema follows "Reloadable Library" patterns.
+  // More information: https://github.com/totten/pathload-poc/blob/master/doc/issues.md
+]);
+
+// When reloading, we make newer instance of the Facade object.
+$GLOBALS['CiviMixSchema'] = require __DIR__ . '/src/CiviMixSchema.php';
+
+if (!interface_exists(__NAMESPACE__ . '\SchemaHelperInterface')) {
+  require __DIR__ . '/src/SchemaHelperInterface.php';
+}
