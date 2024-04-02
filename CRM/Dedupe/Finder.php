25b3778089aafe82808cbc520f11e3c34760abec
eileen <emcnaughton@wikimedia.org>
2024-04-02 15:31:15 +1300

    Pass parameters to dedupe fill table

--- CRM/Dedupe/Finder.php
+++ CRM/Dedupe/Finder.php
@@ -41,12 +41,11 @@ class CRM_Dedupe_Finder {
   public static function dupes($rgid, $cids = [], $checkPermissions = TRUE) {
     $rgBao = new CRM_Dedupe_BAO_DedupeRuleGroup();
     $rgBao->id = $rgid;
-    $rgBao->contactIds = $cids;
     if (!$rgBao->find(TRUE)) {
       throw new CRM_Core_Exception('Dedupe rule not found for selected contacts');
     }
 
-    $rgBao->fillTable();
+    $rgBao->fillTable($rgid, $cids, []);
     $dao = CRM_Core_DAO::executeQuery($rgBao->thresholdQuery($checkPermissions));
     $dupes = [];
     while ($dao->fetch()) {
@@ -120,8 +119,8 @@ class CRM_Dedupe_Finder {
       $orig = $params['civicrm_phone']['phone_numeric'];
       $params['civicrm_phone']['phone_numeric'] = preg_replace('/[^\d]/', '', $orig);
     }
-    $rgBao->params = $params;
-    $rgBao->fillTable();
+
+    $rgBao->fillTable($rgBao->id, [], $params);
 
     $dao = CRM_Core_DAO::executeQuery($rgBao->thresholdQuery($checkPermission));
     $dupes = [];
