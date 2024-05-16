860ee47024c294e9e0da33628c41b87e347b7da6
colemanw <coleman@civicrm.org>
2024-05-16 16:49:28 -0400

    Afform - Fix edit links permission for multiple users
    Followup to https://github.com/civicrm/civicrm-core/pull/29922 this fixes a caching bug where
    links might be incorrectly shown/hidden to the wrong users

--- ext/afform/admin/Civi/AfformAdmin/AfformAdminInjector.php
+++ ext/afform/admin/Civi/AfformAdmin/AfformAdminInjector.php
@@ -38,59 +38,62 @@ class AfformAdminInjector extends AutoSubscriber {
     $changeSet = \Civi\Angular\ChangeSet::create('afformAdmin')
       ->alterHtml(';\\.aff\\.html$;', function($doc, $path) {
         try {
-          // If the user has "administer CiviCRM", inject gear menu with edit links
-          if (\CRM_Core_Permission::check('administer CiviCRM')) {
-            $afform = Afform::get()
-              ->addWhere('module_name', '=', basename($path, '.aff.html'))
-              ->addSelect('name', 'search_displays', 'title')
-              ->execute()->single();
-            // Create a link to edit the form, plus all embedded SavedSearches
-            $links = [
-              [
-                'url' => \CRM_Utils_System::url('civicrm/admin/afform', NULL, FALSE, "/edit/{$afform['name']}", TRUE, FALSE, TRUE),
-                'text' => E::ts('Edit %1 in FormBuilder', [1 => "<em>{$afform['title']}</em>"]),
-                'icon' => 'fa-pencil',
-              ],
-            ];
-            if ($afform['search_displays']) {
-              $searchNames = [];
-              foreach ($afform['search_displays'] as $searchAndDisplayName) {
-                $searchNames[] = explode('.', $searchAndDisplayName)[0];
-              }
-              $savedSearches = SavedSearch::get()
-                ->addWhere('name', 'IN', $searchNames)
-                ->addSelect('id', 'label')
-                ->execute();
-              foreach ($savedSearches as $savedSearch) {
-                $links[] = [
-                  'url' => \CRM_Utils_System::url('civicrm/admin/search', NULL, FALSE, "/edit/{$savedSearch['id']}", TRUE, FALSE, TRUE),
-                  'text' => E::ts('Edit %1 in SearchKit', [1 => "<em>{$savedSearch['label']}</em>"]),
-                  'icon' => 'fa-search-plus',
-                ];
-              }
+          // Inject gear menu with edit links which will be shown if the user has permission
+          $afform = Afform::get(FALSE)
+            ->addWhere('module_name', '=', basename($path, '.aff.html'))
+            ->addSelect('name', 'search_displays', 'title')
+            ->execute()->single();
+          // Create a link to edit the form, plus all embedded SavedSearches
+          $links = [
+            [
+              'url' => \CRM_Utils_System::url('civicrm/admin/afform', NULL, FALSE, "/edit/{$afform['name']}", TRUE, FALSE, TRUE),
+              'text' => E::ts('Edit %1 in FormBuilder', [1 => "<em>{$afform['title']}</em>"]),
+              'icon' => 'fa-pencil',
+              'permission' => 'administer afform',
+            ],
+          ];
+          if ($afform['search_displays']) {
+            $searchNames = [];
+            foreach ($afform['search_displays'] as $searchAndDisplayName) {
+              $searchNames[] = explode('.', $searchAndDisplayName)[0];
             }
-            $linksMarkup = '';
-            foreach ($links as $link) {
-              $linksMarkup .= <<<HTML
-                <li>
-                  <a href="{$link['url']}" target="_blank">
-                    <i class="crm-i fa-fw {$link['icon']}"></i> {$link['text']}
-                  </a>
-                </li>
-              HTML;
+            $savedSearches = SavedSearch::get(FALSE)
+              ->addWhere('name', 'IN', $searchNames)
+              ->addSelect('id', 'label', 'COUNT(permissioned_display.id) AS is_locked')
+              ->addGroupBy('id')
+              ->addJoin('SearchDisplay AS permissioned_display', 'LEFT', ['id', '=', 'permissioned_display.saved_search_id'], ['permissioned_display.acl_bypass', '=', TRUE])
+              ->execute();
+            foreach ($savedSearches as $savedSearch) {
+              $links[] = [
+                'url' => \CRM_Utils_System::url('civicrm/admin/search', NULL, FALSE, "/edit/{$savedSearch['id']}", TRUE, FALSE, TRUE),
+                'text' => E::ts('Edit %1 in SearchKit', [1 => "<em>{$savedSearch['label']}</em>"]),
+                'icon' => 'fa-search-plus',
+                // Saved Searches with "bypass_permission" displays are locked to non-super-admins
+                'permission' => $savedSearch['is_locked'] ? 'all CiviCRM permissions and ACLs' : 'administer search_kit',
+              ];
             }
-            $editMenu = <<<HTML
-              <div class="pull-right btn-group af-admin-edit-form-link">
-                <button type="button" class="btn dropdown-toggle btn-sm" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <i class="crm-i fa-gear"></i> <span class="caret"></span>
-                </button>
-                <ul class="dropdown-menu">$linksMarkup</ul>
-              </div>
-            HTML;
-            // Append link to end of afform markup so it has the highest z-index and is clickable.
-            // afCore.css will control placement at the top of the form.
-            pq($doc)->append($editMenu);
           }
+          $linksMarkup = '';
+          foreach ($links as $link) {
+            $linksMarkup .= <<<HTML
+              <li ng-if="checkPerm('{$link['permission']}')">
+                <a href="{$link['url']}" target="_blank">
+                  <i class="crm-i fa-fw {$link['icon']}"></i> {$link['text']}
+                </a>
+              </li>
+            HTML;
+          }
+          $editMenu = <<<HTML
+            <div class="pull-right btn-group af-admin-edit-form-link" ng-if="checkPerm('{$links[0]['permission']}')">
+              <button type="button" class="btn dropdown-toggle btn-sm" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="crm-i fa-gear"></i> <span class="caret"></span>
+              </button>
+              <ul class="dropdown-menu">$linksMarkup</ul>
+            </div>
+          HTML;
+          // Append link to end of afform markup so it has the highest z-index and is clickable.
+          // afCore.css will control placement at the top of the form.
+          pq($doc)->append($editMenu);
         }
         catch (\Exception $e) {
         }
