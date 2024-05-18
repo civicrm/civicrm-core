1dec76110500a4ebf010e2a030a332dfd48a2c91
demeritcowboy <demeritcowboy@hotmail.com>
2024-05-18 15:53:59 -0400

    delete

--- Civi/ActionSchedule/Mapping.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-// Empty file to ensure the old one is overwritten on Joomla! installs.
-// It otherwise causes Declaration of Civi\\ActionSchedule\\Mapping::getValueHeader() must be compatible with Civi\\ActionSchedule\\MappingInterface::getValueHeader():
