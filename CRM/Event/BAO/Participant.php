5c8cd929ac8c182cba8a0ec0861ecc068373e122
eileen <emcnaughton@wikimedia.org>
2024-05-24 15:13:14 +1200

    No properties

--- CRM/Event/BAO/Participant.php
+++ CRM/Event/BAO/Participant.php
@@ -1,4 +1,5 @@
-<?php
+<?php /** @noinspection PhpUndefinedFieldInspection */
+
 /*
  +--------------------------------------------------------------------+
  | Copyright CiviCRM LLC. All rights reserved.                        |
