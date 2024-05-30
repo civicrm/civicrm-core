ae8da2e153f64ed8988ecc94c210260ab9b5faab
colemanw <coleman@civicrm.org>
2024-05-30 07:41:16 -0400

    Smarty - remove duplicate template dirs

--- mixin/smarty@1/mixin.php
+++ mixin/smarty@1/mixin.php
@@ -22,7 +22,7 @@ return function ($mixInfo, $bootCache) {
     $smarty = CRM_Core_Smarty::singleton();
     $v2 = isset($smarty->_version) && version_compare($smarty->_version, 3, '<');
     $templateDirs = (array) ($v2 ? $smarty->template_dir : $smarty->getTemplateDir());
-    $templateDirs = array_merge($newDirs, $templateDirs);
+    $templateDirs = array_unique(array_merge($newDirs, $templateDirs));
     if ($v2) {
       $smarty->template_dir = $templateDirs;
     }
