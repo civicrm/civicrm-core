bbfffc55e859bc85ae2f54bd0f322e2148111d1b
Stephen Palmstrom <stephen.palmstrom@missionassist.org.uk>
2024-06-18 09:22:46 +0100

    Further style corrections.

--- CRM/Member/BAO/Query.php
+++ CRM/Member/BAO/Query.php
@@ -222,14 +222,14 @@ class CRM_Member_BAO_Query extends CRM_Core_BAO_Query {
           $query->_where[$grouping][] = "civicrm_membership.join_date {$op} {$date}";
           if (!is_array($value)) {
             $temp = $value;
-             // erase it
+            // Erase it.
             $value = NULL;
             $value[] = [];
-            // Year
+            // Year.
             $value[0] = substr($temp, 0, 4);
-             // Month
+            // Month.
             $value[1] = substr($temp, 4, 2);
-             // Day
+            // Day.
             $value[2] = substr($temp, 6, 2);
           }
           $format = CRM_Utils_Date::customFormat(CRM_Utils_Date::format(array_reverse($value), '-'));
