110afa4d4deae6c962e8832de09c08617295fe57
Tim Otten <totten@civicrm.org>
2024-07-26 17:34:55 -0700

    ext/* - Update civix templates for newer entities
    Overview
    --------
    
    The boilerplate code in here is based on an older draft of civix templates. This is based on a more recent draft.
    
    Changes
    ------
    
    * `E::schema()` should return a schema object if you're using the final/published version of `civimix-schema5`. (Old snippet was unpublished draft.)
       You can see the problem/resolution by running this command:
    
        cv ev 'echo CRM_Search_ExtensionUtil::schema()->generateInstallSql();'
    
    * Since these are core exts, they don't include their own copies of `mixin/lib/civimix-schema@5` - so that's not added to pathload's search.
    * Define the new 'CRM_XXXX_DAO_Base` alias

--- ext/standaloneusers/standaloneusers.civix.php
+++ ext/standaloneusers/standaloneusers.civix.php
@@ -79,18 +79,32 @@ class CRM_Standaloneusers_ExtensionUtil {
    * @return \CiviMix\Schema\SchemaHelperInterface
    */
   public static function schema() {
-    return $GLOBALS['CiviMixSchema5']->getHelper(static::LONG_NAME);
+    if (!isset($GLOBALS['CiviMixSchema'])) {
+      pathload()->loadPackage('civimix-schema@5', TRUE);
+    }
+    return $GLOBALS['CiviMixSchema']->getHelper(static::LONG_NAME);
   }
 
 }
 
 use CRM_Standaloneusers_ExtensionUtil as E;
 
-($GLOBALS['_PathLoad'][0] ?? require __DIR__ . '/mixin/lib/pathload-0.php');
-pathload()->addSearchDir(__DIR__ . '/mixin/lib');
 spl_autoload_register('_standaloneusers_civix_class_loader', TRUE, TRUE);
 
 function _standaloneusers_civix_class_loader($class) {
+  if ($class === 'CRM_Standaloneusers_DAO_Base') {
+    if (version_compare(CRM_Utils_System::version(), '5.74.beta', '>=')) {
+      class_alias('CRM_Core_DAO_Base', 'CRM_Standaloneusers_DAO_Base');
+      // ^^ Materialize concrete names -- encourage IDE's to pick up on this association.
+    }
+    else {
+      $realClass = 'CiviMix\\Schema\\Standaloneusers\\DAO';
+      class_alias($realClass, $class);
+      // ^^ Abstract names -- discourage IDE's from picking up on this association.
+    }
+    return;
+  }
+
   // This allows us to tap-in to the installation process (without incurring real file-reads on typical requests).
   if (strpos($class, 'CiviMix\\Schema\\Standaloneusers\\') === 0) {
     // civimix-schema@5 is designed for backported use in download/activation workflows,
