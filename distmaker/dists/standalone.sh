1f791d81ee2746fd818766894945fba679296f06
Tim Otten <totten@civicrm.org>
2024-08-16 18:24:40 -0700

    distmaker - Rename root tar-folder for civicrm-standalone
    Before: "civicrm-X.Y.Z-standalone.tar.gz" ==> "./web/"
    
    After: "civicrm-X.Y.Z-standalone.tar.gz" ==> "./civicrm-standalone/"

--- distmaker/dists/standalone.sh
+++ distmaker/dists/standalone.sh
@@ -11,22 +11,22 @@ else
 fi
 . "$P/common.sh"
 
-SRC=$DM_SOURCEDIR
-TRG=$DM_TMPDIR/civicrm
+SRC="$DM_SOURCEDIR"
+TRG="$DM_TMPDIR/civicrm-standalone"
 
 # copy all the stuff
-dm_reset_dirs "$TRG" "$TRG/civicrm/web/core"
-dm_install_core "$SRC" "$TRG/civicrm/web/core"
-dm_install_coreext "$SRC" "$TRG/civicrm/web/core" $(dm_core_exts)
-dm_install_packages "$SRC/packages" "$TRG/civicrm/web/core/packages"
-dm_install_vendor "$SRC/vendor" "$TRG/civicrm/web/core/vendor"
-dm_install_bower "$SRC/bower_components" "$TRG/civicrm/web/core/bower_components"
-dm_install_cvext com.iatspayments.civicrm "$TRG/civicrm/web/core/ext/iatspayments"
-$SRC/tools/standalone/bin/scaffold $TRG/civicrm/web
+dm_reset_dirs "$TRG" "$TRG/core"
+dm_install_core "$SRC" "$TRG/core"
+dm_install_coreext "$SRC" "$TRG/core" $(dm_core_exts)
+dm_install_packages "$SRC/packages" "$TRG/core/packages"
+dm_install_vendor "$SRC/vendor" "$TRG/core/vendor"
+dm_install_bower "$SRC/bower_components" "$TRG/core/bower_components"
+dm_install_cvext com.iatspayments.civicrm "$TRG/core/ext/iatspayments"
+"$SRC/tools/standalone/bin/scaffold" "$TRG"
 
 # gen tarball
-cd $TRG/civicrm
-tar czf $DM_TARGETDIR/civicrm-$DM_VERSION-standalone.tar.gz web
+cd "$DM_TMPDIR"
+tar czf "$DM_TARGETDIR/civicrm-$DM_VERSION-standalone.tar.gz" civicrm-standalone
 
 # clean up
 rm -rf $TRG
