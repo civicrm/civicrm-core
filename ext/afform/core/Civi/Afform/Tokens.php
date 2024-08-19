042fd60b4abe9d68649a1686e6e6059dd2b94649
eileen <emcnaughton@wikimedia.org>
2024-08-20 11:41:03 +1200

    Remove afform duplication of tokens hook
    My understanding is that these are implemented only for the purposes of
    when listTokens and evaluateTokens are not being run - ie
    the legacy hooks rather than the token Processor
    are being called. There are no longer any core callers that do this

--- ext/afform/core/Civi/Afform/Tokens.php
+++ ext/afform/core/Civi/Afform/Tokens.php
@@ -34,8 +34,6 @@ class Tokens extends AutoService implements EventSubscriberInterface {
 
     return [
       'hook_civicrm_alterMailContent' => 'applyCkeditorWorkaround',
-      'hook_civicrm_tokens' => 'hook_civicrm_tokens',
-      'hook_civicrm_tokenValues' => 'hook_civicrm_tokenValues',
       'civi.token.list' => 'listTokens',
       'civi.token.eval' => 'evaluateTokens',
     ];
@@ -52,61 +50,6 @@ class Tokens extends AutoService implements EventSubscriberInterface {
       if (is_string($e->content[$field])) {
         $e->content[$field] = preg_replace(';https?://(\{afform.*Url\});', '$1', $e->content[$field]);
       }
-    }
-  }
-
-  /**
-   * Expose tokens for use in UI.
-   *
-   * @param \Civi\Core\Event\GenericHookEvent $e
-   * @see \CRM_Utils_Hook::tokens()
-   */
-  public static function hook_civicrm_tokens(GenericHookEvent $e) {
-    $tokenForms = static::getTokenForms();
-    foreach ($tokenForms as $tokenName => $afform) {
-      $e->tokens['afform']["afform.{$tokenName}Url"] = E::ts('%1 (URL)', [1 => $afform['title'] ?? $afform['name']]);
-      $e->tokens['afform']["afform.{$tokenName}Link"] = E::ts('%1 (Full Hyperlink)', [1 => $afform['title'] ?? $afform['name']]);
-    }
-  }
-
-  /**
-   * Substitute any tokens of the form `{afform.myFormUrl}` or `{afform.myFormLink}` with actual values.
-   *
-   * @param \Civi\Core\Event\GenericHookEvent $e
-   * @see \CRM_Utils_Hook::tokenValues()
-   */
-  public static function hook_civicrm_tokenValues(GenericHookEvent $e) {
-    try {
-      // Depending on the caller, $tokens['afform'] might be ['fooUrl'] or ['fooUrl'=>1]. Because... why not!
-      $activeAfformTokens = array_merge(array_keys($e->tokens['afform'] ?? []), array_values($e->tokens['afform'] ?? []));
-
-      $tokenForms = static::getTokenForms();
-      foreach ($tokenForms as $formName => $afform) {
-        if (!array_intersect($activeAfformTokens, ["{$formName}Url", "{$formName}Link"])) {
-          continue;
-        }
-
-        if (empty($afform['server_route'])) {
-          continue;
-        }
-
-        if (!is_array($e->contactIDs)) {
-          $url = self::createUrl($afform, $e->contactIDs);
-          $e->details["afform.{$formName}Url"] = $url;
-          $e->details["afform.{$formName}Link"] = sprintf('<a href="%s">%s</a>', htmlentities($url), htmlentities($afform['title'] ?? $afform['name']));
-        }
-        else {
-          foreach ($e->contactIDs as $cid) {
-            $url = self::createUrl($afform, $cid);
-            $e->details[$cid]["afform.{$formName}Url"] = $url;
-            $e->details[$cid]["afform.{$formName}Link"] = sprintf('<a href="%s">%s</a>', htmlentities($url), htmlentities($afform['title'] ?? $afform['name']));
-          }
-        }
-      }
-    }
-    catch (CryptoException $ex) {
-      \Civi::log()->warning(__CLASS__ . ' cannot generate tokens due to a crypto exception.',
-        ['exception' => $ex]);
     }
   }
 
@@ -188,38 +131,6 @@ class Tokens extends AutoService implements EventSubscriberInterface {
       \Civi::$statics[__CLASS__]['tokenForms'] = $tokenForms;
     }
     return \Civi::$statics[__CLASS__]['tokenForms'];
-  }
-
-  /**
-   * Generate an authenticated URL for viewing this form.
-   *
-   * @param array $afform
-   * @param int $contactId
-   *
-   * @return string
-   * @throws \Civi\Crypto\Exception\CryptoException
-   */
-  public static function createUrl($afform, $contactId): string {
-    $expires = \CRM_Utils_Time::time() +
-      (\Civi::settings()->get('checksum_timeout') * 24 * 60 * 60);
-
-    /** @var \Civi\Crypto\CryptoJwt $jwt */
-    $jwt = \Civi::service('crypto.jwt');
-
-    $bearerToken = "Bearer " . $jwt->encode([
-      'exp' => $expires,
-      'sub' => "cid:" . $contactId,
-      'scope' => 'authx',
-    ]);
-
-    $url = \CRM_Utils_System::url($afform['server_route'],
-      ['_authx' => $bearerToken, '_authxSes' => 1],
-      TRUE,
-      NULL,
-      FALSE,
-      $afform['is_public'] ?? TRUE
-    );
-    return $url;
   }
 
 }
