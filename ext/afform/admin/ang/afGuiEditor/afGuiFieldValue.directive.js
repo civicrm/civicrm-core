civicrm/civicrm-core

commit afde330ff8e974b9bef93b3d678cd20d5b846c89

afformAdmin - Ensure conditional values have correct data type
This was a big fight between me & AngularJS.
It had been incorrectly formatting field values & I eventually tracked the problem
to a load-order issue, where AngularJS provides its own formatter (`stringBasedInputType`),
which casts the value to a string and ruins the serialization of arrays. After much trial-and-error
I figured out that formatters are applied in reverse-order, and for some reason the angular core
formatter is added quite late. The only way I could find to add one even later was to use a postLink
function in the directive.

ext/afform/admin/ang/afGuiEditor/afGuiFieldValue.directive.js
@@ -13,17 +13,37 @@
         ngModel: 'ngModel',
         editor: '?^^afGuiEditor'
       },
+      link: {
+        post: function ngOptionsPreLink(scope, element, attr, ctrls) {
+
+          // Formatter for ngModel to convert value to a string for select2
+          // AngularJS provides its own formatter (`stringBasedInputType`) which simply casts the value to a string
+          // Formatters are applied in reverse-order so using postLink ensures this one comes last in the array & gets applied first
+          function formatViewValue(value) {
+            if (Array.isArray(value)) {
+              return value.join("\u0001");
+            }
+            if (typeof value === 'boolean') {
+              return value ? '1' : '0';
+            }
+            return value;
+          }
+          ctrls.ngModel.$formatters.push(formatViewValue);
+        }
+      },
       controller: function ($element, $timeout) {
         var ts = CRM.ts('org.civicrm.afform_admin'),
           ctrl = this,
+          dataType,
           multi;
 
         function makeWidget(field) {
           var options,
             filters,
             $el = $($element),
-            inputType = field.input_type,
-            dataType = field.data_type;
+            inputType = field.input_type;
+
+          getDataType();
 
           // Decide whether the input should be multivalued
           if (ctrl.op) {
@@ -86,6 +106,19 @@
           return $element.is('.select2-container + input');
         }
 
+        function getDataType() {
+          if (ctrl.field) {
+            dataType = ctrl.field.data_type;
+          }
+        }
+
+        function convertDataType(val) {
+          if (dataType === 'Integer') {
+            return +val;
+          }
+          return val;
+        }
+
         // Copied from ng-list but applied conditionally if field is multi-valued
         var parseFieldInput = function(viewValue) {
           // If the viewValue is invalid (say required but empty) it will be `undefined`
@@ -96,34 +129,24 @@
           }
 
           if (!multi || !isSelect2()) {
-            return viewValue;
+            return convertDataType(viewValue);
           }
 
           var list = [];
 
           if (viewValue) {
             _.each(viewValue.split("\u0001"), function(value) {
-              if (value) list.push(_.trim(value));
+              list.push(convertDataType(value));
             });
           }
 
           return list;
         };
 
-        var formatViewValue = function(value) {
-          if (Array.isArray(value)) {
-            return value.join(',');
-          }
-          if (typeof value === 'boolean') {
-            return value ? '1' : '0';
-          }
-          return value;
-        };
-
         this.$onInit = function() {
+          getDataType();
           // Copied from ng-list
           ctrl.ngModel.$parsers.push(parseFieldInput);
-          ctrl.ngModel.$formatters.push(formatViewValue);
 
           // Copied from ng-list
           ctrl.ngModel.$isEmpty = function(value) {

ext/afform/admin/ang/afGuiEditor/afGuiCondition.html
@@ -2,5 +2,5 @@
   <option ng-repeat="(name, label) in $ctrl.getOperators()" value="{{:: name }}">{{:: label }}</option>
 </select>
 <div class="form-group" ng-if="$ctrl.operatorTakesInput()">
-  <input af-gui-field-value="$ctrl.field" ng-model="$ctrl.getSetValue" op="$ctrl.clause[$ctrl.offset]" ng-model-options="{getterSetter: true}" class="form-control"></input>
+  <input af-gui-field-value="$ctrl.field" ng-model="$ctrl.getSetValue" op="$ctrl.clause[$ctrl.offset]" ng-model-options="{getterSetter: true}" class="form-control">
 </div>

commit b5a3b63df74c4c0150ee6f25f5e73e5235d2e141

Afform - Fix FK selects of form entities
This is a regression caused by afde330ff

Forcing the data to match an integer doesn't work for FK fields because of the way
Afform oveloads those values with string references to other form entities (e.g. 'Individual1') or 'user_contact_id'.

ext/afform/admin/ang/afGuiEditor/afGuiFieldValue.directive.js
@@ -109,6 +109,13 @@
         function getDataType() {
           if (ctrl.field) {
             dataType = ctrl.field.data_type;
+            // FK Entities can use a mix of numeric & string values (see `"static": options` above)
+            if (ctrl.field.fk_entity) {
+              dataType = 'String';
+            }
+          }
+          else {
+            dataType = null;
           }
         }
 

ext/afform/admin/ang/afGuiEditor/behaviors/autofillRelationshipBehaviorForm.component.js
@@ -22,18 +22,15 @@
       // Initialize or rebuild form field
       this.$onChanges = function(changes) {
         if (changes.selectedType) {
-          var selectedType = _.find(ctrl.relTypes, {name: ctrl.selectedType});
-          if (!ctrl.relatedContactField || ctrl.relatedContactField.input_attrs.filter.contact_type !== selectedType.contact_type) {
+          let selectedType = _.find(ctrl.relTypes, {name: ctrl.selectedType});
+          let contactType = selectedType.contact_type || 'Contact';
+          if (!ctrl.relatedContactField || ctrl.relatedContactField.fk_entity !== contactType) {
             // Replacing the variable with a new object will trigger the afGuiFieldValue to refresh
             ctrl.relatedContactField = {
               input_type: 'EntityRef',
               data_type: 'Integer',
-              fk_entity: 'Contact',
-              input_attrs: {filter: {}}
+              fk_entity: contactType,
             };
-            if (selectedType.contact_type) {
-              ctrl.relatedContactField.input_attrs.filter.contact_type = selectedType.contact_type;
-            }
           }
         }
       };