e07f5be98486ab67a736f449423cb11ae18f360c
eileen <emcnaughton@wikimedia.org>
2024-09-02 17:41:05 +1200

    dev/core#5422 Add unit test to check event full message

---
+++ Civi/Test/PageWrapper.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC. All rights reserved.                        |
+ |                                                                    |
+ | This work is published under the GNU AGPLv3 license with some      |
+ | permitted exceptions and without any warranty. For full license    |
+ | and copyright information, see https://civicrm.org/licensing       |
+ +--------------------------------------------------------------------+
+ */
+
+namespace Civi\Civi\Test;
+
+use Civi\Api4\Utils\ReflectionUtils;
+
+class PageWrapper {
+
+  /**
+   * @var \CRM_Core_Page
+   */
+  private \CRM_Core_Page $page;
+
+  private array $statusMessage;
+
+  /**
+   * @var false|string
+   */
+  private string $output;
+
+  /**
+   * @param string $class
+   * @param array $urlParameters
+   */
+  public function __construct(string $class, array $urlParameters = []) {
+    $_GET = $urlParameters;
+    $this->page = new $class();
+    $_SERVER['REQUEST_METHOD'] = 'GET';
+    $_REQUEST = array_merge($_REQUEST, $urlParameters);
+  }
+
+  public function run(): void {
+    ob_start();
+    $this->page->run();
+    $this->output = ob_get_clean();
+  }
+
+  public function getOutput(): string {
+    return $this->output;
+  }
+
+}
