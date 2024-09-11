2e9dec5a0a532c659579f25d3f3a911f6d088896
colemanw <coleman@civicrm.org>
2024-09-11 08:53:21 -0400

    Remove brand names from website types

--- sql/civicrm_data/civicrm_option_group/website_type.sqldata.php
+++ sql/civicrm_data/civicrm_option_group/website_type.sqldata.php
@@ -6,13 +6,6 @@ return CRM_Core_CodeGen_OptionGroup::create('website_type', 'a/0045')
   ->addValueTable(['name', 'value'], [
     ['Work', 1, 'is_default' => 1],
     ['Main', 2],
-    ['Facebook', 3],
-    ['Instagram', 5],
-    ['LinkedIn', 6],
-    ['MySpace', 7],
-    ['Pinterest', 8],
-    ['SnapChat', 9],
-    ['Tumblr', 10],
-    ['Twitter', 11],
+    ['Social', 3],
   ])
   ->syncColumns('fill', ['value' => 'weight', 'name' => 'label']);
