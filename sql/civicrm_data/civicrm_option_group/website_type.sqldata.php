7ad07b344ab45a6539e9e8da58ea65579e803bc2
colemanw <coleman@civicrm.org>
2024-09-11 12:15:27 -0400

    WebsiteType - Add missing translated labels

--- sql/civicrm_data/civicrm_option_group/website_type.sqldata.php
+++ sql/civicrm_data/civicrm_option_group/website_type.sqldata.php
@@ -3,9 +3,9 @@ return CRM_Core_CodeGen_OptionGroup::create('website_type', 'a/0045')
   ->addMetadata([
     'title' => ts('Website Type'),
   ])
-  ->addValueTable(['name', 'value'], [
-    ['Work', 1, 'is_default' => 1],
-    ['Main', 2],
-    ['Social', 3],
+  ->addValueTable(['label', 'name', 'value'], [
+    [ts('Work'), 'Work', 1, 'is_default' => 1],
+    [ts('Main'), 'Main', 2],
+    [ts('Social'), 'Social', 3],
   ])
   ->syncColumns('fill', ['value' => 'weight', 'name' => 'label']);
