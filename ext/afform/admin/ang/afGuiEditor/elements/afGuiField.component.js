civicrm/civicrm-core

commit f4bb470bafddc7e86bb05060a2143eed9ff4265d

Afform - Fix disappearing time input on field default value
The widget needs access to all input_attrs, so use merge() instead of extend() to
do a deep copy that includes all input_attrs such as .time.

ext/afform/admin/ang/afGuiEditor/elements/afGuiField.component.js
@@ -260,7 +260,7 @@
       }
 
       function setFieldDefn() {
-        ctrl.fieldDefn = angular.extend({}, ctrl.getDefn(), ctrl.node.defn);
+        ctrl.fieldDefn = angular.merge({}, ctrl.getDefn(), ctrl.node.defn);
       }
 
       function setDateOptions() {

commit c5677fad2ae85ef9d0479447fb7530e8e571f192

dev/core#5513 - Fix Afform option editing
Fixes https://lab.civicrm.org/dev/core/-/issues/5513
Regressed with f4bb470bafddc7e86bb05060a2143eed9ff4265d

ext/afform/admin/ang/afGuiEditor/elements/afGuiField.component.js
@@ -266,7 +266,12 @@
       }
 
       function setFieldDefn() {
+        // Deeply merge defn to include nested settings e.g. `input_attrs.time`.
         ctrl.fieldDefn = angular.merge({}, ctrl.getDefn(), ctrl.node.defn);
+        // Undo deep merge of options array.
+        if (ctrl.node.defn && ctrl.node.defn.options) {
+          ctrl.fieldDefn.options = JSON.parse(JSON.stringify(ctrl.node.defn.options));
+        }
       }
 
       $scope.toggleDefaultValue = function() {