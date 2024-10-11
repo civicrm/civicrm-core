14b7f6589b5512f14fe9c6a72d9d59535ba86dd1
Tim Otten <totten@civicrm.org>
2024-10-10 18:39:24 -0700

    EnglishNumber - Create camel-case and hypenated names for 100-999

--- tests/phpunit/CRM/Utils/EnglishNumberTest.php
+++ tests/phpunit/CRM/Utils/EnglishNumberTest.php
@@ -30,6 +30,10 @@ class CRM_Utils_EnglishNumberTest extends CiviUnitTestCase {
     $this->assertEquals('Thirty', CRM_Utils_EnglishNumber::toCamelCase(30));
     $this->assertEquals('FiftyFour', CRM_Utils_EnglishNumber::toCamelCase(54));
     $this->assertEquals('NinetyNine', CRM_Utils_EnglishNumber::toCamelCase(99));
+    $this->assertEquals('OneHundred', CRM_Utils_EnglishNumber::toCamelCase(100));
+    $this->assertEquals('OneOhFive', CRM_Utils_EnglishNumber::toCamelCase(105));
+    $this->assertEquals('OneTwelve', CRM_Utils_EnglishNumber::toCamelCase(112));
+    $this->assertEquals('TwoFiftyTwo', CRM_Utils_EnglishNumber::toCamelCase(252));
   }
 
   public function testHyphen(): void {
@@ -38,6 +42,10 @@ class CRM_Utils_EnglishNumberTest extends CiviUnitTestCase {
     $this->assertEquals('thirty', CRM_Utils_EnglishNumber::toHyphen(30));
     $this->assertEquals('fifty-four', CRM_Utils_EnglishNumber::toHyphen(54));
     $this->assertEquals('ninety-nine', CRM_Utils_EnglishNumber::toHyphen(99));
+    $this->assertEquals('one-hundred', CRM_Utils_EnglishNumber::toHyphen(100));
+    $this->assertEquals('one-oh-five', CRM_Utils_EnglishNumber::toHyphen(105));
+    $this->assertEquals('one-twelve', CRM_Utils_EnglishNumber::toHyphen(112));
+    $this->assertEquals('two-fifty-two', CRM_Utils_EnglishNumber::toHyphen(252));
   }
 
   public function testIsNumeric(): void {
