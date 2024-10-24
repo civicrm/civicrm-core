7bdf4625c502f3b7a63a438a5a95aea2a34a7eff
Torben Bertram <30273351+wintermoor@users.noreply.github.com>
2024-10-24 13:19:08 +0200

    Update authx.php
    removed tabs and spaces - whitespacing

--- ext/authx/authx.php
+++ ext/authx/authx.php
@@ -124,11 +124,11 @@ function authx_civicrm_enable() {
 function authx_civicrm_permission(&$permissions) {
   $permissions['authenticate with password'] = [
     'label' => E::ts('AuthX: Authenticate to services with password'),
-	  'description' => E::ts('AuthX: Authenticate to services with password'),
+    'description' => E::ts('AuthX: Authenticate to services with password'),
   ];
   $permissions['authenticate with api key'] = [
     'label' => E::ts('AuthX: Authenticate to services with API key'),
-	  'description' => E::ts('AuthX: Authenticate to services with API key'),
+    'description' => E::ts('AuthX: Authenticate to services with API key'),
   ];
   $permissions['generate any authx credential'] = [
     'label' => E::ts('Authx: Generate new JWT credentials for other users via the API'),
