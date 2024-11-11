467fd05943cc8c6e05d7943510e82247dc0b2661
benjamin <ben@civicrm.org>
2024-11-11 16:44:15 +0000

    afform - move authx cred status check to afform_login_token

--- ext/afform/login_token/afform_login_token.php
+++ ext/afform/login_token/afform_login_token.php
@@ -30,3 +30,24 @@ function afform_login_token_civicrm_install(): void {
 function afform_login_token_civicrm_enable(): void {
   _afform_login_token_civix_civicrm_enable();
 }
+
+/**
+ * Implements hook_civicrm_check().
+ *
+ * Afform Login Token requires JWT is an acceptable cred for Auto Login in Authx
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_check
+ */
+function afform_login_token_civicrm_check(&$messages, $statusNames, $includeDisabled) {
+  if (!in_array('jwt', \Civi::settings()->get('authx_auto_cred'))) {
+    $messages[] = new \CRM_Utils_Check_Message(
+      'afform_login_token_authx',
+      E::ts('Afform Login-Tokens requires that JSON Web Tokens are included as an acceptable credential for Auto Login in your AuthX configuration. Please <a href="%1">review your configuration here</a>.', [
+        1 => \Civi::url('backend://civicrm/admin/setting/authx'),
+      ]),
+      E::ts('AuthX Configuration'),
+      \Psr\Log\LogLevel::ERROR,
+      'fa-chain-broken'
+    );
+  }
+}
