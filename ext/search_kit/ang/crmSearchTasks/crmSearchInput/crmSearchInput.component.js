civicrm/civicrm-core

commit 5d3e09ed770d44f399bc9a455fbd41515e11b66d

SearchKit - Add 'whole row' and 'add row' editable features
Adds 2 useful modes to SearchKit editable settings:

1. Edit whole row: enables all fields in row to be edited and saved at once
2. Add new row: enables a new row to be added in-place

ext/search_kit/ang/crmSearchTasks/crmSearchInput/crmSearchInput.component.js
@@ -34,8 +34,20 @@
         });
 
         this.ngModel.$render = function() {
-          ctrl.value = ctrl.ngModel.$viewValue;
+          ctrl.value = formatDataType(ctrl.ngModel.$viewValue);
         };
+
+        function formatDataType(val) {
+          if (_.isArray(val)) {
+            const formatted = angular.copy(val);
+            formatted.forEach((v, i) => formatted[i] = formatDataType(v));
+            return formatted;
+          }
+          if (ctrl.field.data_type === 'Integer' || ctrl.field.data_type === 'Float') {
+            return Number(val);
+          }
+          return val;
+        }
 
       };
     }

CRM/Utils/String.php
@@ -1112,4 +1112,21 @@ class CRM_Utils_String {
     return $tokens;
   }
 
+  public static function isQuotedString($value): bool {
+    return is_string($value) && strlen($value) > 1 && $value[0] === $value[-1] && in_array($value[0], ['"', "'"]);
+  }
+
+  public static function unquoteString(string $string): string {
+    // Strip the outer quotes if the string starts and ends with the same quote type
+    if (self::isQuotedString($string)) {
+      $string = substr($string, 1, -1);
+
+      // Replace escaped quotes with unescaped quotes, avoiding escaped backslashes
+      $string = preg_replace('/(?<!\\\\)\\\\\\"/', '"', $string);
+      $string = preg_replace('/(?<!\\\\)\\\\\\\'/', "'", $string);
+    }
+
+    return $string;
+  }
+
 }

ext/civicrm_admin_ui/managed/SavedSearch_Administer_Custom_Field_Options.mgd.php
@@ -138,6 +138,10 @@ return [
             ],
           ],
           'actions' => FALSE,
+          'editableRow' => [
+            'create' => TRUE,
+            'createLabel' => E::ts('Add Option'),
+          ],
           'classes' => ['table', 'table-striped'],
           'draggable' => 'weight',
         ],

ext/search_kit/Civi/Api4/Action/SearchDisplay/AbstractRunAction.php
@@ -149,7 +149,7 @@ abstract class AbstractRunAction extends \Civi\Api4\Generic\AbstractAction {
         $data[$key] = $this->getValue($key, $record, $index);
       }
       foreach ($this->display['settings']['columns'] as $column) {
-        $columns[] = $this->formatColumn($column, $data);
+        $columns[] = $this->formatColumn($column, $data, $this->display['settings']);
       }
       $row = [
         'data' => $data,
@@ -232,9 +232,10 @@ abstract class AbstractRunAction extends \Civi\Api4\Generic\AbstractAction {
   /**
    * @param array $column
    * @param array $data
+   * @param array $settings
    * @return array{val: mixed, links: array, edit: array, label: string, title: string, image: array, cssClass: string}
    */
-  private function formatColumn($column, $data) {
+  private function formatColumn(array $column, array $data, array $settings) {
     $column += ['rewrite' => NULL, 'label' => NULL, 'key' => ''];
     $out = [];
     switch ($column['type']) {
@@ -262,11 +263,11 @@ abstract class AbstractRunAction extends \Civi\Api4\Generic\AbstractAction {
             $out['links'] = $links;
           }
         }
-        elseif (!empty($column['editable']) && !$column['rewrite']) {
+        elseif (!empty($column['editable']) && !$column['rewrite'] && empty($settings['editableRow']['disable'])) {
           $edit = $this->formatEditableColumn($column, $data);
           if ($edit) {
             // When internally processing an inline-edit, get all metadata
-            if (isset($this->colKey) && $this->colKey === $column['key']) {
+            if (isset($this->rowKey) && isset($this->values) && array_key_exists($column['key'], $this->values)) {
               $out['edit'] = $edit;
             }
             // Otherwise, the client only needs a boolean
@@ -1837,6 +1838,36 @@ abstract class AbstractRunAction extends \Civi\Api4\Generic\AbstractAction {
       return 'relationship_id';
     }
     return CoreUtil::getIdFieldName($entityName);
+  }
+
+  /**
+   * Get data from the where/having clauses, useful for inferring values to create a new entity
+   *
+   * If $this->applyFilters has already run, it will include data from filters
+   *
+   * @return array
+   * @throws \CRM_Core_Exception
+   */
+  public function getQueryData(): array {
+    // First pass: gather raw data from the where & having clauses
+    $data = [];
+    foreach (array_merge($this->_apiParams['where'], $this->_apiParams['having'] ?? []) as $clause) {
+      if ($clause[1] === '=' || $clause[1] === 'IN') {
+        $data[$clause[0]] = $clause[2];
+      }
+    }
+    // Second pass: format values (because data from first pass could be useful to FormattingUtil)
+    foreach ($this->_apiParams['where'] as $clause) {
+      if ($clause[1] === '=' || $clause[1] === 'IN') {
+        [$fieldPath] = explode(':', $clause[0]);
+        $fieldSpec = $this->getField($fieldPath);
+        $data[$fieldPath] = $clause[2];
+        if ($fieldSpec) {
+          FormattingUtil::formatInputValue($data[$fieldPath], $clause[0], $fieldSpec, $data, $clause[1]);
+        }
+      }
+    }
+    return $data;
   }
 
 }

ext/search_kit/Civi/Api4/Action/SearchDisplay/InlineEdit.php
@@ -7,14 +7,12 @@ use Civi\Api4\Result\SearchDisplayRunResult;
 use Civi\Api4\Utils\CoreUtil;
 
 /**
- * Perform an inline-edit to a search display result, then re-run the search to return the updated row.
+ * Perform an inline-edit to a search display row, then re-run the search to return created/updated row.
  *
- * @method $this setValue(mixed $value)
- * @method mixed getValue()
+ * @method $this setValues(mixed $values)
+ * @method mixed getValues()
  * @method $this setRowKey(mixed $rowKey)
  * @method mixed getRowKey()
- * @method $this setCowKey(string $colKey)
- * @method string getColKey()
  * @package Civi\Api4\Action\SearchDisplay
  */
 class InlineEdit extends Run {
@@ -28,24 +26,36 @@ class InlineEdit extends Run {
 
   /**
    * @var int
-   * @required
    */
   protected $rowKey;
 
   /**
-   * @var string
-   * @required
+   * @var array
    */
-  protected $colKey;
-
-  /**
-   * @var mixed
-   */
-  protected $value;
+  protected $values;
 
   protected function processResult(SearchDisplayRunResult $result) {
-    $checkPermissions = empty($this->display['settings']['acl_bypass']);
+    if (isset($this->rowKey)) {
+      $this->updateExistingRow();
+    }
+    elseif (!empty($this->display['settings']['editableRow']['create'])) {
+      $this->createNewRow();
+    }
+    else {
+      throw new \CRM_Core_Exception('Cannot create new item in search display');
+    }
 
+    // This prevents unnecessary metadata from being returned in the reload
+    $this->rowKey = NULL;
+
+    // Now reload the updated row to refresh client-side
+    parent::processResult($result);
+  }
+
+  /**
+   * @throws \CRM_Core_Exception
+   */
+  public function updateExistingRow(): void {
     // Apply rowKey to filters
     $keyName = CoreUtil::getIdFieldName($this->savedSearch['api_entity']);
     $this->applyFilter($keyName, $this->rowKey);
@@ -53,32 +63,139 @@ class InlineEdit extends Run {
     $this->_apiParams['offset'] = 0;
 
     // First get existing values
+    // This will include inline-edit metadata
     $existingValues = new SearchDisplayRunResult();
     parent::processResult($existingValues);
     $existingValues = $existingValues->single();
 
+    $tasks = [];
+
+    // Gather tasks from values; group by entity+action+id
     $columns = $this->display['settings']['columns'];
     foreach ($columns as $columnIndex => $column) {
-      if (($column['key'] ?? NULL) === $this->colKey) {
-        break;
+      if (array_key_exists($column['key'], $this->values)) {
+        $editableInfo = $existingValues['columns'][$columnIndex]['edit'] ?? NULL;
+        if (!$editableInfo) {
+          throw new \CRM_Core_Exception('Cannot edit column ' . $column['key']);
+        }
+        $value = $this->values[$column['key']];
+        if (empty($editableInfo['nullable']) && ($value === NULL || $value === '')) {
+          continue;
+        }
+        $idField = CoreUtil::getIdFieldName($editableInfo['entity']);
+        $taskKey = $editableInfo['action'] . ($editableInfo['record'][$idField] ?? '');
+        if (!isset($tasks[$editableInfo['entity']][$taskKey])) {
+          $tasks[$editableInfo['entity']][$taskKey] = $editableInfo;
+        }
+        $tasks[$editableInfo['entity']][$taskKey]['record'][$editableInfo['value_key']] = $value;
       }
     }
-    if ($column['key'] !== $this->colKey || empty($existingValues['columns'][$columnIndex]['edit'])) {
-      throw new \CRM_Core_Exception('Cannot edit column ' . $this->colKey);
+
+    $checkPermissions = empty($this->display['settings']['acl_bypass']);
+    // Run create/update tasks
+    foreach ($tasks as $editableItems) {
+      foreach ($editableItems as $editableItem) {
+        civicrm_api4($editableItem['entity'], $editableItem['action'], [
+          'checkPermissions' => $checkPermissions,
+          'values' => $editableItem['record'],
+        ]);
+      }
     }
-    $editableInfo = $existingValues['columns'][$columnIndex]['edit'];
-    $entityValues = $editableInfo['record'];
-    $entityValues[$editableInfo['value_key']] = $this->value;
-    civicrm_api4($editableInfo['entity'], $editableInfo['action'], [
-      'checkPermissions' => $checkPermissions,
-      'values' => $entityValues,
-    ]);
+  }
 
-    // This prevents unnecessary metadata from being returned in the reload
-    $this->colKey = NULL;
+  /**
+   * @throws \CRM_Core_Exception
+   */
+  public function createNewRow(): void {
+    // Gather tasks from values; key by explicit_join
+    $joins = $this->getJoins();
 
-    // Now reload the updated row to refresh client-side
-    parent::processResult($result);
+    // Ensure tasks are in the same order as joins
+    $tasks = [
+      // Base entity is always the first task
+      '' => NULL,
+    ] + array_fill_keys(array_keys($joins), NULL);
+
+    $this->applyFilters();
+    $data = $this->getQueryData();
+
+    $columns = $this->display['settings']['columns'];
+    foreach ($columns as $column) {
+      if (array_key_exists($column['key'], $this->values)) {
+        $editableInfo = $this->getEditableInfo($column['key']);
+        if (!$editableInfo) {
+          throw new \CRM_Core_Exception('Cannot edit column ' . $column['key']);
+        }
+        $value = $this->values[$column['key']];
+        if ($value === NULL || $value === '') {
+          continue;
+        }
+        $joinName = $editableInfo['explicit_join'] ?? '';
+        if (!isset($tasks[$joinName])) {
+          $tasks[$joinName] = $editableInfo;
+          $record = $data;
+          $prefix = $joinName ? "$joinName." : '';
+          $record = \CRM_Utils_Array::filterByPrefix($record, $prefix);
+          $tasks[$joinName]['record'] = array_filter($record, fn($key) => !str_contains($key, '.'), ARRAY_FILTER_USE_KEY);
+        }
+        $tasks[$joinName]['record'][$editableInfo['value_key']] = $value;
+      }
+    }
+
+    if (!isset($tasks[''])) {
+      throw new \CRM_Core_Exception('Not enough data to create new row');
+    }
+
+    $checkPermissions = empty($this->display['settings']['acl_bypass']);
+    $saved = [];
+
+    foreach ($tasks as $joinName => $editableItem) {
+      if (!$editableItem) {
+        continue;
+      }
+      if ($joinName) {
+        $join = $joins[$joinName];
+        foreach ($join['on'] ?? [] as $clause) {
+          $key = $value = NULL;
+          if (isset($clause[2]) && $clause[1] === '=') {
+            unset($clause[1]);
+            foreach ($clause as $item) {
+              if (is_string($item) && str_starts_with($item, "$joinName.")) {
+                $key = substr($item, strlen($joinName) + 1);
+              }
+              else {
+                $value = $item;
+                if (\CRM_Utils_String::isQuotedString($value)) {
+                  $value = \CRM_Utils_String::unquoteString($value);
+                }
+                elseif (is_string($value)) {
+                  foreach (array_reverse($saved, TRUE) as $taskName => $taskValues) {
+                    $taskPrefix = $taskName ? "$taskName." : '';
+                    if (str_starts_with($value, $taskPrefix)) {
+                      $taskValueKey = substr($value, strlen($taskPrefix));
+                      $value = $taskValues[$taskValueKey] ?? NULL;
+                      break;
+                    }
+                  }
+                }
+              }
+            }
+            if (isset($key) && isset($value)) {
+              $editableItem['record'][$key] = $value;
+            }
+          }
+        }
+      }
+      $result = civicrm_api4($editableItem['entity'], 'create', [
+        'checkPermissions' => $checkPermissions,
+        'values' => $editableItem['record'],
+      ])->first();
+      $saved[$joinName] = $result;
+    }
+
+    // Apply id of newly saved row to filters
+    $keyName = CoreUtil::getIdFieldName($this->savedSearch['api_entity']);
+    $this->applyFilter($keyName, $saved[''][$keyName]);
   }
 
 }

ext/search_kit/Civi/Api4/Action/SearchDisplay/Run.php
@@ -187,30 +187,14 @@ class Run extends AbstractRunAction {
   }
 
   private function formatToolbar(): array {
-    $toolbar = $data = [];
+    $toolbar = [];
     $settings = $this->display['settings'];
     // If no toolbar, early return
     if (empty($settings['toolbar']) && empty($settings['addButton']['path'])) {
       return [];
     }
     // There is no row data, but some values can be inferred from query filters
-    // First pass: gather raw data from the where & having clauses
-    foreach (array_merge($this->_apiParams['where'], $this->_apiParams['having'] ?? []) as $clause) {
-      if ($clause[1] === '=' || $clause[1] === 'IN') {
-        $data[$clause[0]] = $clause[2];
-      }
-    }
-    // Second pass: format values (because data from first pass could be useful to FormattingUtil)
-    foreach ($this->_apiParams['where'] as $clause) {
-      if ($clause[1] === '=' || $clause[1] === 'IN') {
-        [$fieldPath] = explode(':', $clause[0]);
-        $fieldSpec = $this->getField($fieldPath);
-        $data[$fieldPath] = $clause[2];
-        if ($fieldSpec) {
-          FormattingUtil::formatInputValue($data[$fieldPath], $clause[0], $fieldSpec, $data, $clause[1]);
-        }
-      }
-    }
+    $data = $this->getQueryData();
     // Support legacy 'addButton' setting
     if (empty($settings['toolbar']) && !empty($settings['addButton']['path'])) {
       $settings['toolbar'][] = $settings['addButton'] + ['style' => 'primary', 'target' => 'crm-popup'];

ext/search_kit/ang/crmSearchAdmin/displays/searchAdminDisplayTable.component.js
@@ -61,6 +61,36 @@
         ctrl.parent.initColumns({label: true, sortable: true});
       };
 
+      this.toggleEditableRowMode = function(name, value) {
+        ctrl.display.settings.editableRow = ctrl.display.settings.editableRow || {};
+        if (arguments.length < 2) {
+          value = !ctrl.display.settings.editableRow[name];
+        }
+        if (value === ctrl.display.settings.editableRow[name]) {
+          return;
+        }
+        ctrl.display.settings.editableRow[name] = value;
+        if (!value) {
+          delete ctrl.display.settings.editableRow[name];
+          if (name === 'create') {
+            delete ctrl.display.settings.editableRow.disable;
+            delete ctrl.display.settings.editableRow.createLabel;
+          }
+        }
+        else if (name === 'create') {
+          ctrl.display.settings.editableRow.createLabel = ts('Add');
+        }
+        if (name === 'full') {
+          delete ctrl.display.settings.editableRow.disable;
+        }
+        if (name === 'disable') {
+          delete ctrl.display.settings.editableRow.full;
+        }
+        if (angular.equals({}, ctrl.display.settings.editableRow)) {
+          delete ctrl.display.settings.editableRow;
+        }
+      };
+
       this.toggleTally = function() {
         if (ctrl.display.settings.tally) {
           delete ctrl.display.settings.tally;

ext/search_kit/ang/crmSearchAdmin/displays/searchAdminDisplayTable.html
@@ -74,6 +74,33 @@
       </div>
     </div>
   </details>
+
+  <details>
+    <summary>{{:: ts('In-Place Edit') }}</summary>
+    <div class="form-inline">
+      <label class="radio-inline">
+        <input type="radio" ng-click="$ctrl.toggleEditableRowMode('full', false)" ng-checked="!$ctrl.display.settings.editableRow.full && !$ctrl.display.settings.editableRow.disable">
+        {{:: ts('Single Field') }}
+      </label>
+      <label class="radio-inline">
+        <input type="radio" ng-click="$ctrl.toggleEditableRowMode('full', true)" ng-checked="!!$ctrl.display.settings.editableRow.full">
+        {{:: ts('Entire Row') }}
+      </label>
+      <label class="radio-inline" ng-class="{disabled: !$ctrl.display.settings.editableRow.create}">
+        <input type="radio" ng-click="$ctrl.toggleEditableRowMode('disable', true)" ng-checked="!!$ctrl.display.settings.editableRow.disable">
+        {{:: ts('Disabled (create only)') }}
+      </label>
+    </div>
+    <div class="form-inline">
+      <div class="checkbox-inline form-control" title="{{:: ts('Create a new record using in-place edit.') }}">
+        <label>
+          <input type="checkbox" ng-click="$ctrl.toggleEditableRowMode('create')" ng-checked="!!$ctrl.display.settings.editableRow.create">
+          <span>{{:: ts('Add New Button') }}</span>
+        </label>
+      </div>
+      <input ng-if="$ctrl.display.settings.editableRow.create" class="form-control" placeholder="{{:: ts('(no label)') }}" ng-model="$ctrl.display.settings.editableRow.createLabel">
+    </div>
+  </details>
 </fieldset>
 
 <fieldset class="crm-search-admin-edit-columns-wrapper">

commit d4c53c647953eb7127db96c16ce6c14a8c5be7a7

SearchKit - Fix saving dropdown options
Fixes dev/core#5681 regression error in commit 5d3e09ed770d44f399bc9a455fbd41515e11b66d

ext/search_kit/ang/crmSearchTasks/crmSearchInput/crmSearchInput.component.js
@@ -38,6 +38,10 @@
         };
 
         function formatDataType(val) {
+          // Do not reformat pseudoconstant values (:name, :label, etc)
+          if (ctrl.optionKey && ctrl.optionKey !== 'id') {
+            return val;
+          }
           if (_.isArray(val)) {
             const formatted = angular.copy(val);
             formatted.forEach((v, i) => formatted[i] = formatDataType(v));