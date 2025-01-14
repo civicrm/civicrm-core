129c65adcd72c1a3205c8e2d5eff6a9d091c76c3
Tim Otten <totten@civicrm.org>
2025-01-13 16:35:00 -0800

    civimix-schema - Update numbering for civicrm-core@6.x

--- mixin/lib/pathload.index.php
+++ mixin/lib/pathload.index.php
@@ -11,5 +11,14 @@ namespace Civi\PathLoadSetup;
 //
 // So instead, we use `addSearchItem()` and register with explicit versions.
 
-$version = \CRM_Utils_System::version() . '.1'; /* Higher priority than contrib copies of same version... */
-\pathload()->addSearchItem('civimix-schema', $version, __DIR__ . '/civimix-schema');
+$version6 = \CRM_Utils_System::version() . '.1'; /* Higher priority than contrib copies of same version... */
+$version5 = preg_replace_callback(';^6\.(\d+)\.;', function ($m) {
+  /* civimix-schema@5.83=>6.0 was purely superficial (to match core#)s. Continue 5.x option for compat. */
+  return '5.' . (83 + $m[1]) . '.';
+}, $version6);
+
+// Register civimix-schema@5.x
+\pathload()->addSearchItem('civimix-schema', $version5, __DIR__ . '/civimix-schema');
+
+// (Optional) Register civimix-schema@6.x
+// \pathload()->addSearchItem('civimix-schema', $version6, __DIR__ . '/civimix-schema');
