8ecbf44a1e3ec9fe90197f1168adbe9448cf4876
Tim Otten <totten@civicrm.org>
2025-01-15 14:53:41 -0800

    SloppyTestChecker - Focus more on system metadata, less on data

--- Civi/Test/SloppyTestChecker.php
+++ Civi/Test/SloppyTestChecker.php
@@ -114,16 +114,35 @@ class SloppyTestChecker {
     $tables = \Civi\Test::schema()->getTables('BASE TABLE');
     $views = \Civi\Test::schema()->getTables('VIEW');
 
+    $ignoreSettings = '("resCacheCode")';
+
     $snapshot = [];
     $snapshot['tables'] = array_combine($tables, $tables);
     $snapshot['view'] = array_combine($views, $views);
-    $snapshot['extensions'] = static::fetchAll('SELECT full_name, is_active FROM %s.civicrm_extension ORDER BY full_name', ['full_name']);
-    $snapshot['custom_groups'] = static::fetchAll('SELECT id, name FROM %s.civicrm_custom_group ORDER BY name', ['name']);
-    $snapshot['custom_fields'] = static::fetchAll('SELECT id, name, custom_group_id FROM %s.civicrm_custom_field ORDER BY name', ['name']);
-    $snapshot['settings'] = static::fetchAll('SELECT name, domain_id, value FROM %s.civicrm_setting WHERE name NOT IN ("resCacheCode") ORDER BY name, domain_id', ['name', 'domain_id']);
-    // $snapshot['settings'] = static::fetchAll('SELECT name, domain_id, value FROM %s.civicrm_setting ORDER BY name, domain_id', ['name', 'domain_id']);
-    $snapshot['contacts'] = static::fetchAll('SELECT id, display_name FROM %s.civicrm_contact', ['display_name']);
-    $snapshot['events'] = static::fetchAll('SELECT id, title FROM %s.civicrm_event', ['title']);
+    $snapshot['civicrm_extension'] = static::fetchAll('SELECT full_name, is_active FROM %s.civicrm_extension ORDER BY full_name', ['full_name']);
+    $snapshot['civicrm_component'] = static::fetchAll('SELECT id, name, namespace FROM %s.civicrm_component', ['name']);
+    $snapshot['civicrm_custom_group'] = static::fetchAll('SELECT id, name FROM %s.civicrm_custom_group ORDER BY name', ['name']);
+    $snapshot['civicrm_custom_field'] = static::fetchAll('SELECT id, name, custom_group_id FROM %s.civicrm_custom_field ORDER BY name', ['name']);
+    $snapshot['civicrm_setting'] = static::fetchAll("SELECT name, domain_id, value FROM %s.civicrm_setting WHERE name NOT IN $ignoreSettings ORDER BY name, domain_id", ['name', 'domain_id']);
+    $snapshot['civicrm_preferences_date'] = static::fetchAll('SELECT id, name, start, end, date_format, time_format FROM %s.civicrm_preferences_date ORDER BY name', ['name']);
+    $snapshot['civicrm_domain'] = static::fetchAll('SELECT name, version, locales, locale_custom_strings FROM %s.civicrm_domain', ['name']);
+    $snapshot['civicrm_membership_status'] = static::fetchAll('SELECT * FROM %s.civicrm_membership_status ORDER BY name', ['name']);
+
+    $snapshot['civicrm_worldregion'] = static::fetchAll('SELECT id, name FROM %s.civicrm_worldregion ORDER BY name', ['name']);
+    $snapshot['civicrm_timezone'] = static::fetchAll('SELECT name, abbreviation, gmt FROM %s.civicrm_timezone ORDER BY name', ['name']);
+    $snapshot['civicrm_country'] = static::fetchAll('SELECT * FROM %s.civicrm_country ORDER BY name', ['name']);
+    $snapshot['civicrm_currency'] = static::fetchAll('SELECT id, name, symbol, numeric_code FROM %s.civicrm_currency ORDER BY name', ['name']);
+
+    // Maybe: civicrm_contact_type, civicrm_financial_type, civicrm_case_type, civicrm_dashboard, civicrm_dedupe_rule,
+    // civicrm_mail_settings, civicrm_membership_type, civicrm_membership_status
+
+    // These have a mix of reserved and bespoke data. Only reserved is tracked.
+    $snapshot['civicrm_msg_template'] = static::fetchAll('SELECT id, msg_title, msg_subject, workflow_name, is_active, is_default FROM %s.civicrm_msg_template WHERE is_reserved = 1 ORDER BY id', ['id']);
+    $snapshot['civicrm_location_type'] = static::fetchAll('SELECT * FROM %s.civicrm_location_type WHERE is_reserved = 1 ORDER BY name', ['name']);
+    $snapshot['civicrm_option_group'] = static::fetchAll('SELECT * FROM %s.civicrm_option_group WHERE is_reserved = 1 ORDER BY name', ['name']);
+    $snapshot['civicrm_option_value'] = static::fetchAll('SELECT * FROM %s.civicrm_option_value WHERE is_reserved = 1 ORDER BY option_group_id, name', ['option_group_id', 'name']);
+    $snapshot['civicrm_relationship_type'] = static::fetchAll('SELECT * FROM %s.civicrm_relationship_type WHERE is_reserved = 1 ORDER BY name_a_b', ['name_a_b']);
+
     return $snapshot;
   }
 
@@ -135,6 +154,7 @@ class SloppyTestChecker {
       foreach ($index as $indexPart) {
         $key[] = $item[$indexPart] ?? 'NULL';
       }
+      unset($item['modified_date'], $item['created_date']);
       $result[implode(' ', $key)] = $item;
     }
     return $result;
