bb8169f64b088ec7ff014b2f105283aa4bf7896d
Sandor Semsey <sandor@es-progress.hu>
2025-01-17 23:06:52 +0100

    add @throws tag

--- CRM/Mailing/Event/BAO/MailingEventUnsubscribe.php
+++ CRM/Mailing/Event/BAO/MailingEventUnsubscribe.php
@@ -287,6 +287,8 @@ WHERE  email = %2
    *   Is this domain-level?.
    * @param int $job
    *   The job ID.
+   *
+   * @throws \CRM_Core_Exception
    */
   public static function send_unsub_response($queue_id, $groups, $is_domain, $job) {
     $domain = CRM_Core_BAO_Domain::getDomain();
