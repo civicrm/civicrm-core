civicrm/civicrm-core

commit ecc0b99423530890bc1a276e20548c3e71ad7e40

refactor contact summary tabs to use TabHeader.tpl

CRM/Contact/Page/View/Summary.php
@@ -469,9 +469,18 @@ class CRM_Contact_Page_View_Summary extends CRM_Contact_Page_View {
     // ensure all keys used in the template are set, to avoid notices
     $allTabs = \CRM_Core_Smarty::setRequiredTabTemplateKeys($allTabs);
 
+    // ensure the array keys match the value in the "id" of each item
+    $tabs = [];
+
+    foreach ($allTabs as $key => $tab) {
+      $finalKey = $tab['id'] ?? $key;
+      $tabs[$finalKey] = $tab;
+    }
+
     // now sort the tabs based on weight
-    usort($allTabs, ['CRM_Utils_Sort', 'cmpFunc']);
-    return $allTabs;
+    usort($tabs, ['CRM_Utils_Sort', 'cmpFunc']);
+
+    return $tabs;
   }
 
   /**

CRM/Core/Smarty.php
@@ -299,12 +299,17 @@ class CRM_Core_Smarty extends CRM_Core_SmartyCompatibility {
       'count' => NULL,
       'hideCount' => FALSE,
       'template' => NULL,
+      'active' => TRUE,
+      'valid' => TRUE,
       // Afform tabs set the afform module and directive - NULL for non-afform tabs
       'module' => NULL,
       'directive' => NULL,
     ];
 
     foreach ($tabs as $i => $tab) {
+      if (empty($tab['url'])) {
+        $tab['url'] = $tab['link'] ?? '';
+      }
       $tabs[$i] = array_merge($defaults, (array) $tab);
     }
     return $tabs;

templates/CRM/Contact/Page/View/Summary.tpl
@@ -101,35 +101,8 @@
     </div><!-- .crm-actions-ribbon -->
   {/if}
 
-  <div class="crm-block crm-content-block crm-contact-page crm-inline-edit-container">
-    <div id="mainTabContainer">
-      <ul class="crm-contact-tabs-list">
-        {foreach from=$allTabs item=tabValue}
-          <li id="tab_{$tabValue.id}" class="crm-tab-button ui-corner-all{if is_numeric($tabValue.count)} crm-count-{$tabValue.count}{/if}{if $tabValue.class} {$tabValue.class}{/if}">
-            <a href="{if $tabValue.template}#contact-{$tabValue.id}{else}{$tabValue.url|smarty:nodefaults}{/if}" title="{$tabValue.title|escape}">
-              <i class="{if !empty($tabValue.icon)}{$tabValue.icon}{else}crm-i fa-puzzle-piece{/if}" aria-hidden="true"></i>
-              <span>{$tabValue.title}</span>
-              {if empty($tabValue.hideCount)}<em>{if is_numeric($tabValue.count)}{$tabValue.count}{/if}</em>{/if}
-            </a>
-          </li>
-        {/foreach}
-      </ul>
+  {include file='CRM/common/TabHeader.tpl' tabHeader=$allTabs containerClasses="crm-contact-page crm-inline-edit-container" listClasses="crm-contact-tabs-list" tabIdPrefix="contact-"}
 
-      {foreach from=$allTabs item=tabValue}
-        {if $tabValue.template}
-          <div id="contact-{$tabValue.id}">
-            {if $tabValue.module}
-              <!-- afform tab - need to pass module and directive to afform param -->
-              {include file=$tabValue.template afform=$tabValue}
-            {else}
-              {include file=$tabValue.template}
-            {/if}
-          </div>
-        {/if}
-      {/foreach}
-    </div>
-    <div class="clear"></div>
-  </div>
 {/if}
 
 {* CRM-10560 *}

templates/CRM/common/TabHeader.tpl
@@ -8,27 +8,28 @@
  +--------------------------------------------------------------------+
 *}
 {* enclose all tabs and its content in a block *}
-<div class="crm-block crm-content-block">
+<div class="crm-block crm-content-block {$containerClasses|default:''}">
   {if $tabHeader}
     <div id="mainTabContainer">
-    <ul>
-       {foreach from=$tabHeader key=tabName item=tabValue}
-          <li id="tab_{$tabName}" class="crm-tab-button ui-corner-all{if !$tabValue.valid} disabled{/if} {$tabValue.class}" {$tabValue.extra}>
-          {if $tabValue.active}
-             <a href="{if $tabValue.template}#panel_{$tabName}{else}{$tabValue.link|smarty:nodefaults}{/if}" title="{$tabValue.title|escape}{if !$tabValue.valid} ({ts}disabled{/ts}){/if}">
-               {if $tabValue.icon}<i class="{$tabValue.icon}"></i>{/if}
-               <span>{$tabValue.title}</span>
-               {if is_numeric($tabValue.count)}<em>{$tabValue.count}</em>{/if}
-             </a>
-          {else}
-             <span {if !$tabValue.valid} title="{ts}disabled{/ts}"{/if}>{$tabValue.title}</span>
-          {/if}
+      <ul class="{$listClasses|default:''}">
+        {foreach from=$tabHeader key=tabName item=tabValue}
+          <li id="tab_{$tabName}" class="crm-tab-button ui-corner-all {if !$tabValue.valid}disabled{/if} {if is_numeric($tabValue.count)}crm-count-{$tabValue.count}{/if} {if $tabValue.class} {$tabValue.class}{/if}" {$tabValue.extra}>
+            {if $tabValue.active}
+              <a href="{if $tabValue.template}#{$tabIdPrefix|default:'panel_'}{$tabName}{else}{$tabValue.url|smarty:nodefaults}{/if}" title="{$tabValue.title|escape} {if !$tabValue.valid}({ts}disabled{/ts}){/if}">
+                <i class="{$tabValue.icon|default:'crm-i fa-puzzle-piece'}" aria-hidden="true"></i>
+                <span>{$tabValue.title}</span>
+                {if empty($tabValue.hideCount) && is_numeric($tabValue.count)}<em>{$tabValue.count}</em>{/if}
+              </a>
+            {else}
+               <span {if !$tabValue.valid} title="{ts}disabled{/ts}"{/if}>{$tabValue.title}</span>
+            {/if}
           </li>
-       {/foreach}
-    </ul>
+        {/foreach}
+      </ul>
+
       {foreach from=$tabHeader key=tabName item=tabValue}
         {if $tabValue.template}
-          <div id="panel_{$tabName}">
+          <div id="{$tabIdPrefix|default:'panel_'}{$tabName}">
             {if $tabValue.module}
               <!-- afform tab - need to pass module and directive to afform param -->
               {include file=$tabValue.template afform=$tabValue}
@@ -41,4 +42,4 @@
     </div>
   {/if}
   <div class="clear"></div>
-</div> {* crm-content-block ends here *}
+</div>

commit 0f44da2fa8a7b48cae91a81aec8f7076ae586ba8

ContactSummary - Fix missing tabs
Fixes a bug caused by ecc0b99423530890bc1a276e20548c3e71ad7e40

CRM/Contact/Page/View/Summary.php
@@ -456,10 +456,10 @@ class CRM_Contact_Page_View_Summary extends CRM_Contact_Page_View {
       }
     }
 
-    foreach ($allTabs as &$tab) {
+    foreach ($allTabs as $key => $tab) {
       // Get tab counts last to avoid wasting time; if a tab was removed by hook, the count isn't needed.
       if (!isset($tab['count']) && isset($getCountParams[$tab['id']])) {
-        $tab['count'] = call_user_func_array([
+        $allTabs[$key]['count'] = call_user_func_array([
           'CRM_Contact_BAO_Contact',
           'getCountComponent',
         ], $getCountParams[$tab['id']]);