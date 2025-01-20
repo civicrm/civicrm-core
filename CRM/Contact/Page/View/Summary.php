civicrm/civicrm-core

commit bbb569a12df4714c56c0c7143d8f2ee06122320f

dev/core#4905 - Add CRM_Core_BAO_CustomGroup::getFiltered

CRM/Contact/Page/View/Summary.php
@@ -417,14 +417,14 @@ class CRM_Contact_Page_View_Summary extends CRM_Contact_Page_View {
     }
 
     // now add all the custom tabs
-    $extends = ['Contact', $this->get('contactType')];
-    $style = ['Tab', 'Tab with table'];
-    $activeGroups = CRM_Core_BAO_CustomGroup::getPermitted(CRM_Core_Permission::VIEW);
+    $filters = [
+      'is_active' => TRUE,
+      'extends' => $this->get('contactType'),
+      'style' => ['Tab', 'Tab with table'],
+    ];
+    $activeGroups = CRM_Core_BAO_CustomGroup::getFiltered($filters, CRM_Core_Permission::VIEW);
 
     foreach ($activeGroups as $group) {
-      if (!in_array($group['extends'], $extends) || !in_array($group['style'], $style)) {
-        continue;
-      }
       $id = "custom_{$group['id']}";
       $allTabs[] = [
         'id' => $id,

CRM/Core/BAO/CustomGroup.php
@@ -29,43 +29,62 @@ class CRM_Core_BAO_CustomGroup extends CRM_Core_DAO_CustomGroup implements \Civi
   }
 
   /**
-   * Retrieve all enabled custom groups and fields in a nested array.
+   * Retrieve custom groups and fields in a nested array, with optional filters and permissions applied.
    *
-   * @return array[]
-   */
-  public static function getActive(): array {
-    $allGroups = self::getAll();
-    $allGroups = array_values(array_filter($allGroups, fn($group) => $group['is_active']));
-    foreach ($allGroups as $groupIndex => $group) {
-      $allGroups[$groupIndex]['fields'] = array_values(array_filter($group['fields'], fn($field) => $field['is_active']));
-    }
-    return $allGroups;
-  }
-
-  /**
-   * Same as self::getActive() but returns only active groups and
-   * fields which the given user is allowed to see.
+   * With no params, this returns the same output as self::getAll().
    *
-   * @param int $permissionType (CRM_Core_Permission::VIEW or CRM_Core_Permission::EDIT)
+   * @param array $filters
+   *   [key => value] pairs to filter each custom group.
+   *   - $filters[extends] will auto-expand Contact types
+   *   - $filters[is_active] will also filter the fields
+   * @param int|null $permissionType
+   *   Check permission: (CRM_Core_Permission::VIEW | CRM_Core_Permission::EDIT)
    * @param int|null $userId
+   *   User contact id for permission check (defaults to current user)
    * @return array[]
    */
-  public static function getPermitted(int $permissionType, ?int $userId = NULL) {
-    if (!in_array($permissionType, [CRM_Core_Permission::EDIT, CRM_Core_Permission::VIEW], TRUE)) {
-      throw new CRM_Core_Exception('permissionType must be CRM_Core_Permission::VIEW or CRM_Core_Permission::EDIT');
+  public static function getFiltered(array $filters = [], int $permissionType = NULL, int $userId = NULL): array {
+    if (isset($permissionType)) {
+      if (!in_array($permissionType, [CRM_Core_Permission::EDIT, CRM_Core_Permission::VIEW], TRUE)) {
+        throw new CRM_Core_Exception('permissionType must be CRM_Core_Permission::VIEW or CRM_Core_Permission::EDIT');
+      }
+      $filters['id'] = CRM_Core_Permission::customGroup($permissionType, FALSE, $userId);
     }
-    $allGroups = self::getActive();
-    $allowedGroupIds = CRM_Core_Permission::customGroup($permissionType, FALSE, $userId);
-    $allGroups = array_filter($allGroups, function($group) use ($allowedGroupIds) {
-      return in_array($group['id'], $allowedGroupIds);
+    if (!empty($filters['extends']) && is_string($filters['extends'])) {
+      $contactTypes = CRM_Contact_BAO_ContactType::basicTypes(TRUE);
+      if ($filters['extends'] === 'Contact') {
+        $filters['extends'] = array_merge(['Contact'], $contactTypes);
+      }
+      elseif (in_array($filters['extends'], $contactTypes, TRUE)) {
+        $filters['extends'] = ['Contact', $filters['extends']];
+      }
+    }
+    $allGroups = array_filter(self::getAll(), function($group) use ($filters) {
+      foreach ($filters as $key => $value) {
+        if (is_array($value)) {
+          if (!in_array($group[$key], $value)) {
+            return FALSE;
+          }
+        }
+        elseif ($group[$key] != $value) {
+          return FALSE;
+        }
+      }
+      return TRUE;
     });
+    // The `is_active` filter applies to fields as well as groups.
+    if (!empty($filters['is_active'])) {
+      foreach ($allGroups as $groupIndex => $group) {
+        $allGroups[$groupIndex]['fields'] = array_values(array_filter($group['fields'], fn($field) => $field['is_active']));
+      }
+    }
     return array_values($allGroups);
   }
 
   /**
    * Fetch all custom groups and fields in a nested array.
    *
-   * Avoids calling the API to prevent recursion or early-bootstrap issues.
+   * Output includes all custom group data + fields.
    *
    * @return array[]
    */
@@ -80,6 +99,7 @@ class CRM_Core_BAO_CustomGroup extends CRM_Core_DAO_CustomGroup implements \Civi
           $select[] = "f.`$fieldKey` AS `field__$fieldKey`";
         }
       }
+      // Avoid calling the API to prevent recursion or early-bootstrap issues.
       $data = \CRM_Utils_SQL_Select::from('civicrm_custom_group g')
         ->join('f', 'LEFT JOIN civicrm_custom_field f ON g.id = f.custom_group_id')
         ->select($select)

Civi/Api4/Service/Schema/Joinable/CustomGroupJoinable.php
@@ -49,12 +49,14 @@ class CustomGroupJoinable extends Joinable {
   /**
    * @inheritDoc
    */
-  public function getEntityFields() {
+  public function getEntityFields(): array {
+    $entityFields = [];
     $baseEntity = CoreUtil::getApiNameFromTableName($this->getBaseTable());
-    foreach (\CRM_Core_BAO_CustomGroup::getActive() as $customGroup) {
-      if ($customGroup['table_name'] !== $this->getTargetTable()) {
-        continue;
-      }
+    $filters = [
+      'is_active' => TRUE,
+      'table_name' => $this->getTargetTable(),
+    ];
+    foreach (\CRM_Core_BAO_CustomGroup::getFiltered($filters) as $customGroup) {
       foreach ($customGroup['fields'] as $fieldArray) {
         $entityFields[] = SpecFormatter::arrayToField($fieldArray, $baseEntity, $customGroup);
       }

Civi/Api4/Service/Schema/SchemaMapBuilder.php
@@ -105,11 +105,12 @@ class SchemaMapBuilder extends AutoService {
     if (!$customInfo) {
       return;
     }
-
-    foreach (\CRM_Core_BAO_CustomGroup::getActive() as $customGroup) {
-      if (!$customGroup['fields'] || !in_array($customGroup['extends'], $customInfo['extends'])) {
-        continue;
-      }
+    $filters = [
+      'extends' => $customInfo['extends'],
+      'is_active' => TRUE,
+      'fields' => TRUE,
+    ];
+    foreach (\CRM_Core_BAO_CustomGroup::getFiltered($filters) as $customGroup) {
       $customTable = new Table($customGroup['table_name']);
 
       // Add entity_id join from multi-record custom group to the base entity

Civi/Api4/Service/Spec/SpecGatherer.php
@@ -158,18 +158,21 @@ class SpecGatherer extends AutoService {
       $grouping = 'contact_sub_type';
     }
 
+    $filters = [
+      'is_active' => TRUE,
+      'extends' => $customInfo['extends'],
+      'is_multiple' => FALSE,
+    ];
+    $permissionType = NULL;
     if ($checkPermissions) {
       $permissionType = in_array($spec->getAction(), ['create', 'update', 'save', 'delete', 'replace']) ?
         \CRM_Core_Permission::EDIT :
         \CRM_Core_Permission::VIEW;
-      $customGroups = \CRM_Core_BAO_CustomGroup::getPermitted($permissionType);
     }
-    else {
-      $customGroups = \CRM_Core_BAO_CustomGroup::getActive();
-    }
+    $customGroups = \CRM_Core_BAO_CustomGroup::getFiltered($filters, $permissionType);
 
     foreach ($customGroups as $customGroup) {
-      if ($this->customGroupBelongsTo($customGroup, $customInfo['extends'], $values, $grouping)) {
+      if ($this->customGroupBelongsTo($customGroup, $values, $grouping)) {
         foreach ($customGroup['fields'] as $fieldArray) {
           $field = SpecFormatter::arrayToField($fieldArray, $entity, $customGroup);
           $spec->addFieldSpec($field);
@@ -179,12 +182,9 @@ class SpecGatherer extends AutoService {
   }
 
   /**
-   * Check if custom group belongs to $extends entity and meets criteria from $values
+   * Check if custom group meets criteria from $values
    */
-  private function customGroupBelongsTo(array $customGroup, array $extends, array $values, $grouping): bool {
-    if ($customGroup['is_multiple'] || !in_array($customGroup['extends'], $extends)) {
-      return FALSE;
-    }
+  private function customGroupBelongsTo(array $customGroup, array $values, $grouping): bool {
     // No values or grouping = no filtering needed
     if (empty($values) ||
       (empty($customGroup['extends_entity_column_value']) && empty($customGroup['extends_entity_column_id']))

tests/phpunit/CRM/Core/BAO/CustomGroupTest.php
@@ -27,24 +27,33 @@ class CRM_Core_BAO_CustomGroupTest extends CiviUnitTestCase {
     parent::tearDown();
   }
 
-  public function testLoadAll(): void {
+  public function testGetFiltered(): void {
     $this->quickCleanup([], TRUE);
 
-    $activeGroup = $this->CustomGroupCreate(['title' => 'ActiveGroup', 'weight' => 1]);
+    $activeGroup = $this->CustomGroupCreate(['title' => 'ActiveGroup', 'weight' => 1, 'extends' => 'Household']);
     $this->customFieldCreate(['label' => 'Active', 'custom_group_id' => $activeGroup['id']]);
     $this->customFieldCreate(['label' => 'Disabled', 'is_active' => 0, 'custom_group_id' => $activeGroup['id']]);
 
-    $inactiveGroup = $this->CustomGroupCreate(['title' => 'InactiveGroup', 'weight' => 2, 'is_active' => 0]);
+    $inactiveGroup = $this->CustomGroupCreate(['title' => 'InactiveGroup', 'weight' => 2, 'is_active' => 0, 'extends' => 'Activity']);
     $this->customFieldCreate(['label' => 'Inactive', 'custom_group_id' => $inactiveGroup['id']]);
 
     $allGroups = CRM_Core_BAO_CustomGroup::getAll();
-    $activeGroups = CRM_Core_BAO_CustomGroup::getActive();
+    $activeGroups = CRM_Core_BAO_CustomGroup::getFiltered(['is_active' => TRUE]);
 
     $this->assertCount(2, $allGroups);
     $this->assertCount(2, $allGroups[0]['fields']);
     $this->assertCount(1, $allGroups[1]['fields']);
     $this->assertCount(1, $activeGroups);
+    $this->assertEquals($activeGroup['id'], $activeGroups[0]['id']);
     $this->assertCount(1, $activeGroups[0]['fields']);
+
+    $activityGroups = CRM_Core_BAO_CustomGroup::getFiltered(['extends' => 'Activity']);
+    $this->assertCount(1, $activityGroups);
+    $this->assertEquals($inactiveGroup['id'], $activityGroups[0]['id']);
+
+    $contactGroups = CRM_Core_BAO_CustomGroup::getFiltered(['extends' => 'Contact']);
+    $this->assertEquals($activeGroup['id'], $contactGroups[0]['id']);
+    $this->assertCount(2, $contactGroups[0]['fields']);
   }
 
   /**

commit 98e1c851759225d83e8e7c0e84494743d197a035

dev/core#5690 - Fix subtype filtering on contact summary custom field tabs
Fixes regression caused by bbb569a12df4714c56c0c7143d8f2ee06122320f

CRM/Contact/Page/View/Summary.php
@@ -422,6 +422,15 @@ class CRM_Contact_Page_View_Summary extends CRM_Contact_Page_View {
       'extends' => $this->get('contactType'),
       'style' => ['Tab', 'Tab with table'],
     ];
+    $subTypes = CRM_Utils_Array::explodePadded($this->get('contactSubtype'));
+    if ($subTypes) {
+      // Include groups for this subtype OR groups with no subtype
+      $subTypes[] = NULL;
+      $filters['extends_entity_column_value'] = $subTypes;
+    }
+    else {
+      $filters['extends_entity_column_value'] = NULL;
+    }
     $activeGroups = CRM_Core_BAO_CustomGroup::getAll($filters, CRM_Core_Permission::VIEW);
 
     foreach ($activeGroups as $group) {