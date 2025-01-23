civicrm/civicrm-core

commit 9d46d4849eab374eef1d5ec831ba8d802ff0b7ec

dev/core#1735 Advanced Search et al: ignore the is_searchable as a criteria for displaying a custom field
Also provides an alternative getCustomGroupDetail() function without the $searchable
argument, and removes the pass-by-ref on $extends, in an attempt to de-uglify some
other code. Also replaces some calls by getAll().

CRM/Case/XMLProcessor/Report.php
@@ -959,8 +959,7 @@ LIMIT  1
 
     // Retrieve custom values for cases.
     $customValues = CRM_Core_BAO_CustomValueTable::getEntityValues($caseID, 'Case');
-    $extends = ['Case'];
-    $groupTree = CRM_Core_BAO_CustomGroup::getGroupDetail(NULL, NULL, $extends);
+    $groupTree = CRM_Core_BAO_CustomGroup::getAll(['extends' => ['Case']]);
     $caseCustomFields = [];
     foreach ($groupTree as $gid => $group_values) {
       foreach ($group_values['fields'] as $id => $field_values) {

CRM/Contact/Form/Search/Advanced.php
@@ -85,9 +85,7 @@ class CRM_Contact_Form_Search_Advanced extends CRM_Contact_Form_Search {
       CRM_Contact_BAO_ContactType::basicTypes(),
       CRM_Contact_BAO_ContactType::subTypes()
     );
-    $groupDetails = CRM_Core_BAO_CustomGroup::getGroupDetail(NULL, TRUE,
-      $extends
-    );
+    $groupDetails = CRM_Core_BAO_CustomGroup::getAll(['extends' => $extends]);
     // if no searchable fields unset panel
     if (empty($groupDetails)) {
       unset($paneNames[ts('Custom Fields')]);

CRM/Contact/Form/Search/Criteria.php
@@ -607,10 +607,7 @@ class CRM_Contact_Form_Search_Criteria {
       CRM_Contact_BAO_ContactType::basicTypes(),
       CRM_Contact_BAO_ContactType::subTypes()
     );
-    $groupDetails = CRM_Core_BAO_CustomGroup::getGroupDetail(NULL, TRUE,
-      $extends
-    );
-
+    $groupDetails = CRM_Core_BAO_CustomGroup::getAll(['extends' => $extends]);
     $form->assign('groupTree', $groupDetails);
 
     foreach ($groupDetails as $key => $group) {

CRM/Contribute/BAO/ContributionRecur.php
@@ -648,8 +648,7 @@ LEFT  JOIN civicrm_membership_payment mp  ON ( mp.contribution_id = con.id )
       }
 
       // copy custom data
-      $extends = ['Contribution'];
-      $groupTree = CRM_Core_BAO_CustomGroup::getGroupDetail(NULL, NULL, $extends);
+      $groupTree = CRM_Core_BAO_CustomGroup::getAll(['extends' => ['Contribution']]);
       if ($groupTree) {
         foreach ($groupTree as $groupID => $group) {
           $table[$groupTree[$groupID]['table_name']] = ['entity_id'];

CRM/Core/BAO/Query.php
@@ -21,7 +21,7 @@ class CRM_Core_BAO_Query {
    * @param array $extends
    */
   public static function addCustomFormFields(&$form, $extends) {
-    $groupDetails = CRM_Core_BAO_CustomGroup::getGroupDetail(NULL, TRUE, $extends);
+    $groupDetails = CRM_Core_BAO_CustomGroup::getAll(['extends' => $extends]);
     if ($groupDetails) {
       foreach ($groupDetails as $group) {
         if (empty($group['fields'])) {

CRM/Event/BAO/Event.php
@@ -175,9 +175,7 @@ class CRM_Event_BAO_Event extends CRM_Event_DAO_Event implements \Civi\Core\Hook
    */
   public static function self_hook_civicrm_pre(\Civi\Core\Event\PreEvent $event) {
     if ($event->action === 'delete' && $event->id) {
-
-      $extends = ['Event'];
-      $groupTree = CRM_Core_BAO_CustomGroup::getGroupDetail(NULL, NULL, $extends);
+      $groupTree = CRM_Core_BAO_CustomGroup::getAll(['extends' => ['Event']]);
       // @todo is this custom field loop necessary? The cascade delete on the
       // db foreign key should do it already.
       foreach ($groupTree as $values) {

CRM/Core/BAO/CustomGroup.php
@@ -938,6 +938,18 @@ class CRM_Core_BAO_CustomGroup extends CRM_Core_DAO_CustomGroup implements \Civi
   }
 
   /**
+   * Get custom group details for a group. Legacy function for backwards compatibility.
+   * @deprecated Legacy function
+   *
+   * @see CRM_Core_BAO_CustomGroup::getAll()
+   * for a better alternative.
+   */
+  public static function &getGroupDetail($groupId = NULL, $searchable = FALSE, &$extends = NULL, $inSelector = NULL) {
+    CRM_Core_Error::deprecatedFunctionWarning('getCustomGroupDetail');
+    return self::getCustomGroupDetail($groupId, $extends, $inSelector);
+  }
+
+  /**
    * @deprecated Legacy function
    *
    * @see CRM_Core_BAO_CustomGroup::getAll()
@@ -945,8 +957,6 @@ class CRM_Core_BAO_CustomGroup extends CRM_Core_DAO_CustomGroup implements \Civi
    *
    * @param int $groupId
    *   Group id whose details are needed.
-   * @param bool $searchable
-   *   Is this field searchable.
    * @param array $extends
    *   Which table does it extend if any.
    * @param bool $inSelector
@@ -954,16 +964,13 @@ class CRM_Core_BAO_CustomGroup extends CRM_Core_DAO_CustomGroup implements \Civi
    * @return array
    *   array consisting of all group and field details
    */
-  public static function &getGroupDetail($groupId = NULL, $searchable = NULL, &$extends = NULL, $inSelector = NULL) {
+  public static function &getCustomGroupDetail($groupId = NULL, $extends = NULL, $inSelector = NULL) {
     $groupFilters = [
       'is_active' => TRUE,
     ];
     $fieldFilters = [];
     if ($groupId) {
       $groupFilters['id'] = $groupId;
-    }
-    if ($searchable) {
-      $fieldFilters['is_searchable'] = TRUE;
     }
     if ($inSelector) {
       $groupFilters['is_multiple'] = TRUE;

CRM/Custom/Form/Preview.php
@@ -77,7 +77,7 @@ class CRM_Custom_Form_Preview extends CRM_Core_Form {
     // Group preview
     else {
       $this->_groupId = CRM_Utils_Request::retrieve('gid', 'Positive', $this, TRUE);
-      $groupTree = CRM_Core_BAO_CustomGroup::getGroupDetail($this->_groupId);
+      $groupTree = CRM_Core_BAO_CustomGroup::getCustomGroupDetail($this->_groupId);
       $this->_groupTree = CRM_Core_BAO_CustomGroup::formatGroupTree($groupTree, TRUE, $this);
       $this->assign('preview_type', 'group');
     }

commit 87a4ca0191e14cb7ad331416ace9eb22fa678d44

CRM Search - Fix disabled custom fields appearing on forms
Fixes dev/core#5698

This regressed with 9d46d4849eab374eef1d5ec831ba8d802ff0b7ec

CRM/Case/XMLProcessor/Report.php
@@ -959,7 +959,7 @@ LIMIT  1
 
     // Retrieve custom values for cases.
     $customValues = CRM_Core_BAO_CustomValueTable::getEntityValues($caseID, 'Case');
-    $groupTree = CRM_Core_BAO_CustomGroup::getAll(['extends' => ['Case']]);
+    $groupTree = CRM_Core_BAO_CustomGroup::getAll(['extends' => ['Case'], 'is_active' => TRUE]);
     $caseCustomFields = [];
     foreach ($groupTree as $gid => $group_values) {
       foreach ($group_values['fields'] as $id => $field_values) {

CRM/Contact/Form/Search/Advanced.php
@@ -81,7 +81,7 @@ class CRM_Contact_Form_Search_Advanced extends CRM_Contact_Form_Search {
     ];
 
     //check if there are any custom data searchable fields
-    $groupDetails = CRM_Core_BAO_CustomGroup::getAll(['extends' => 'Contact']);
+    $groupDetails = CRM_Core_BAO_CustomGroup::getAll(['extends' => 'Contact', 'is_active' => TRUE]);
     // if no searchable fields unset panel
     if (empty($groupDetails)) {
       unset($paneNames[ts('Custom Fields')]);

CRM/Contact/Form/Search/Criteria.php
@@ -603,7 +603,7 @@ class CRM_Contact_Form_Search_Criteria {
    */
   public static function custom(&$form) {
     $form->add('hidden', 'hidden_custom', 1);
-    $groupDetails = CRM_Core_BAO_CustomGroup::getAll(['extends' => 'Contact']);
+    $groupDetails = CRM_Core_BAO_CustomGroup::getAll(['extends' => 'Contact', 'is_active' => TRUE]);
     $form->assign('groupTree', $groupDetails);
 
     foreach ($groupDetails as $key => $group) {

CRM/Contribute/BAO/ContributionRecur.php
@@ -634,7 +634,7 @@ LEFT  JOIN civicrm_membership_payment mp  ON ( mp.contribution_id = con.id )
       }
 
       // copy custom data
-      $groupTree = CRM_Core_BAO_CustomGroup::getAll(['extends' => ['Contribution']]);
+      $groupTree = CRM_Core_BAO_CustomGroup::getAll(['extends' => ['Contribution'], 'is_active' => TRUE]);
       if ($groupTree) {
         foreach ($groupTree as $groupID => $group) {
           $table[$groupTree[$groupID]['table_name']] = ['entity_id'];

CRM/Core/BAO/Query.php
@@ -21,7 +21,7 @@ class CRM_Core_BAO_Query {
    * @param array $extends
    */
   public static function addCustomFormFields(&$form, $extends) {
-    $groupDetails = CRM_Core_BAO_CustomGroup::getAll(['extends' => $extends]);
+    $groupDetails = CRM_Core_BAO_CustomGroup::getAll(['extends' => $extends, 'is_active' => TRUE]);
     if ($groupDetails) {
       foreach ($groupDetails as $group) {
         if (empty($group['fields'])) {

CRM/Event/BAO/Event.php
@@ -176,7 +176,7 @@ class CRM_Event_BAO_Event extends CRM_Event_DAO_Event implements \Civi\Core\Hook
    */
   public static function self_hook_civicrm_pre(\Civi\Core\Event\PreEvent $event) {
     if ($event->action === 'delete' && $event->id) {
-      $groupTree = CRM_Core_BAO_CustomGroup::getAll(['extends' => ['Event']]);
+      $groupTree = CRM_Core_BAO_CustomGroup::getAll(['extends' => ['Event'], 'is_active' => TRUE]);
       // @todo is this custom field loop necessary? The cascade delete on the
       // db foreign key should do it already.
       foreach ($groupTree as $values) {