1945c63a416408deff3c4c43ef58e6f2fb5133c4
colemanw <coleman@civicrm.org>
2025-02-07 17:32:37 -0500

    [REF] CRM/Custom - Refactor out uses of deprecated CRM_Utils_Array::value.

--- CRM/Custom/Form/Option.php
+++ CRM/Custom/Form/Option.php
@@ -109,7 +109,7 @@ class CRM_Custom_Form_Option extends CRM_Core_Form {
         }
       }
       else {
-        if (($fieldDefaults['default_value'] ?? NULL) == CRM_Utils_Array::value('value', $defaults)) {
+        if (($fieldDefaults['default_value'] ?? NULL) == ($defaults['value'] ?? NULL)) {
           $defaults['default_value'] = 1;
         }
       }
