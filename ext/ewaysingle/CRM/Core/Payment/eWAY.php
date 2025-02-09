e7803c0053c9162d6588750bbee664d32c79a55b
colemanw <coleman@civicrm.org>
2025-02-09 11:51:13 -0500

    [REF] ext/payments - Refactor out uses of deprecated CRM_Utils_Array::value

--- ext/ewaysingle/CRM/Core/Payment/eWAY.php
+++ ext/ewaysingle/CRM/Core/Payment/eWAY.php
@@ -157,7 +157,7 @@ class CRM_Core_Payment_eWAY extends CRM_Core_Payment {
       return $result;
     }
 
-    if (CRM_Utils_Array::value('is_recur', $params) == TRUE) {
+    if (!empty($params['is_recur'])) {
       throw new CRM_Core_Exception(ts('eWAY - recurring payments not implemented'));
     }
 
@@ -279,7 +279,7 @@ class CRM_Core_Payment_eWAY extends CRM_Core_Payment {
     //----------------------------------------------------------------------------------------------------
     // Check to see if we have a duplicate before we send
     //----------------------------------------------------------------------------------------------------
-    if ($this->checkDupe($params['invoiceID'], CRM_Utils_Array::value('contributionID', $params))) {
+    if ($this->checkDupe($params['invoiceID'], $params['contributionID'] ?? NULL)) {
       throw new PaymentProcessorException('It appears that this transaction is a duplicate.  Have you already submitted the form once?  If so there may have been a connection problem.  Check your email for a receipt from eWAY.  If you do not receive a receipt within 2 hours you can try your transaction again.  If you continue to have problems please contact the site administrator.', 9003);
     }
 
