681c9c641706e225c4528cbcdc4f862ecf12db7d
colemanw <coleman@civicrm.org>
2025-02-13 13:40:28 -0500

    ContactLayout - Run upgrader again to handle updates to CRM_Utils_String::convertStringToSnakeCase
    This string function gives slightly different output than it used to, so update any stored strings accordingly
    Also added handling for custom field group tab names in afform_civicrm_tabset().

--- CRM/Upgrade/Incremental/php/SixOne.php
+++ CRM/Upgrade/Incremental/php/SixOne.php
@@ -30,6 +30,7 @@ class CRM_Upgrade_Incremental_php_SixOne extends CRM_Upgrade_Incremental_Base {
   public function upgrade_6_1_alpha1($rev): void {
     $this->addTask(ts('Upgrade DB to %1: SQL', [1 => $rev]), 'runSql', $rev);
 
+    $this->addTask('Update afform tab names', 'updateAfformTabs');
     $this->addTask('Replace Clear Caches & Reset Paths with Clear Caches in Nav Menu', 'updateUpdateConfigBackendNavItem');
   }
 
@@ -82,6 +83,64 @@ class CRM_Upgrade_Incremental_php_SixOne extends CRM_Upgrade_Incremental_Base {
         )
     ");
 
+    return TRUE;
+  }
+
+  /**
+   * If the ContactLayout extension is installed, update its stored tab names to keep up
+   * with core changes to Afform tabs.
+   *
+   * @see https://github.com/civicrm/civicrm-core/pull/27196
+   *
+   * @param \CRM_Queue_TaskContext $ctx
+   *
+   * @return bool
+   */
+  public static function updateAfformTabs(CRM_Queue_TaskContext $ctx) {
+    $convert = function($id) {
+      if ($id === 'afsearchGrants') {
+        return 'grant';
+      }
+      if (preg_match('#^(afform|afsearch)#i', $id)) {
+        $tabId = CRM_Utils_String::convertStringToSnakeCase(preg_replace('#^(afformtab|afsearchtab|afform|afsearch)#i', '', $id));
+        // @see afform_civicrm_tabset()
+        if (str_starts_with($tabId, 'custom_')) {
+          // custom group tab forms use name, but need to replace tabs using ID
+          // remove 'afsearchTabCustom_' from the form name to get the group name
+          $groupName = substr($id, 18);
+          $groupId = CRM_Core_DAO::getFieldValue('CRM_Core_DAO_CustomGroup', $groupName, 'id', 'name');
+          if ($groupId) {
+            $tabId = 'custom_' . $groupId;
+          }
+        }
+        return $tabId;
+      }
+      return $id;
+    };
+
+    $setting = \Civi::settings()->get('contactlayout_default_tabs');
+    if ($setting && is_array($setting)) {
+      foreach ($setting as $index => $tab) {
+        $setting[$index]['id'] = $convert($tab['id']);
+      }
+      \Civi::settings()->set('contactlayout_default_tabs', $setting);
+    }
+    if (CRM_Core_DAO::checkTableExists('civicrm_contact_layout')) {
+      // Can't use the api due to extension loading issues
+      $dao = CRM_Core_DAO::executeQuery('SELECT * FROM civicrm_contact_layout');
+      while ($dao->fetch()) {
+        if (!empty($dao->tabs)) {
+          $tabs = CRM_Core_DAO::unSerializeField($dao->tabs, CRM_Core_DAO::SERIALIZE_JSON);
+          foreach ($tabs as $index => $tab) {
+            $tabs[$index]['id'] = $convert($tab['id']);
+          }
+          CRM_Core_DAO::executeQuery('UPDATE civicrm_contact_layout SET tabs = %1 WHERE id = %2', [
+            1 => [CRM_Core_DAO::serializeField($tabs, CRM_Core_DAO::SERIALIZE_JSON), 'String'],
+            2 => [$dao->id, 'Integer'],
+          ]);
+        }
+      }
+    }
     return TRUE;
   }
 
