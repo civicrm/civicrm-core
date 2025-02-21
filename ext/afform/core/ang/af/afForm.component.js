2c58e0fa572c37874d60e52261be1d3008dddeb8
ufundo <ufundo@users.noreply.github.com>
2025-02-21 13:58:39 +0000

    afform - limit prefill block to form mode

--- ext/afform/core/ang/af/afForm.component.js
+++ ext/afform/core/ang/af/afForm.component.js
@@ -90,7 +90,9 @@
           ctrl.showSubmitButton = displaySubmitButton(args);
         }
         if (toLoad) {
-          $element.block();
+          if (params.fillMode === 'form') {
+            $element.block();
+          }
           return crmApi4('Afform', 'prefill', params)
             .then((result) => {
               // In some cases (noticed on Wordpress) the response header incorrectly outputs success when there's an error.
