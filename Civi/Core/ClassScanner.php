6d61d1fd393d4e9a421754558dbca6073a3e45eb
Tim Otten <totten@civicrm.org>
2025-03-12 01:20:12 -0700

    (dev/core#5787) ClassScanner - Ignore (more) stale files during Joomla upgrade

--- Civi/Core/ClassScanner.php
+++ Civi/Core/ClassScanner.php
@@ -142,7 +142,7 @@ class ClassScanner {
     $classes = [];
     static::scanFolders($classes, $civicrmRoot, 'Civi/Test/ExampleData', '\\');
     // Most older CRM_ stuff doesn't implement event listeners & services so can be skipped.
-    static::scanFolders($classes, $civicrmRoot, 'CRM', '_', ';(Upgrade|Utils|Exception|_DAO|_Page|_Form|_Controller|_StateMachine|_Selector|_CodeGen|_Cxn|_QuickForm);');
+    static::scanFolders($classes, $civicrmRoot, 'CRM', '_', ';(Upgrade|Utils|Exception|_DAO|_Page|_Form|_Controller|_StateMachine|_Selector|_CodeGen|_Cxn|_QuickForm|CRM_Dedupe_BAO_QueryBuilder);');
     static::scanFolders($classes, $civicrmRoot, 'Civi', '\\', ';\\\(Security|Test)\\\;');
 
     if (CIVICRM_UF === 'UnitTests') {
