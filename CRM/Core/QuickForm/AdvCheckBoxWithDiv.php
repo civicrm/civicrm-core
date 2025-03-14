3147ce523d8d85d0b98828cfa3c262f8b454e0a2
Seamus Lee <seamus.lee@jmaconsulting.biz>
2025-03-14 19:29:00 +1100

    dev/core#5716 Fix Regression with CheckBox custom fields not respecting the options per line setting

---
+++ CRM/Core/QuickForm/AdvCheckBoxWithDiv.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC. All rights reserved.                        |
+ |                                                                    |
+ | This work is published under the GNU AGPLv3 license with some      |
+ | permitted exceptions and without any warranty. For full license    |
+ | and copyright information, see https://civicrm.org/licensing       |
+ +--------------------------------------------------------------------+
+ */
+
+/**
+ * @package CRM
+ */
+
+require_once 'HTML/QuickForm/advcheckbox.php';
+
+/**
+ * Class CRM_Core_QuickForm_AdvCheckBoxWithDiv
+ */
+class CRM_Core_QuickForm_AdvCheckBoxWithDiv extends HTML_QuickForm_advcheckbox {
+
+  /**
+   * Returns the advcheckbox element in HTML
+   *
+   * @since     1.0
+   * @access    public
+   * @return    string
+   */
+  public function toHtml(): string {
+    $html = parent::toHtml();
+    if (is_numeric($this->getAttribute('options_per_line'))) {
+      return '<div class="crm-option-label-pair" >' . $html . '</div>';
+    }
+    return $html;
+  }
+
+}
