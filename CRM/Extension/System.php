5d7d8c3d6ef5418b749255135746921678223f71
Tim Otten <totten@civicrm.org>
2025-03-19 20:22:40 -0700

    (REF) createExtendedInfo - Avoid unnecessary lookups
    We already the CRM_Extension_Info object with all of its tags.
    There's no need for further lookups.

--- CRM/Extension/System.php
+++ CRM/Extension/System.php
@@ -336,7 +336,6 @@ class CRM_Extension_System {
       $extensionRow['path'] = '';
     }
     $extensionRow['status'] = $manager->getStatus($obj->key);
-    $requiredExtensions = $mapper->getKeysByTag('mgmt:required');
 
     switch ($extensionRow['status']) {
       case CRM_Extension_Manager::STATUS_UNINSTALLED:
@@ -368,7 +367,7 @@ class CRM_Extension_System {
     if ($manager->isIncompatible($obj->key)) {
       $extensionRow['statusLabel'] = ts('Obsolete') . ($extensionRow['statusLabel'] ? (' - ' . $extensionRow['statusLabel']) : '');
     }
-    elseif (in_array($obj->key, $requiredExtensions)) {
+    elseif (in_array('mgmt:required', $obj->tags)) {
       $extensionRow['statusLabel'] = ts('Required');
     }
     return $extensionRow;
