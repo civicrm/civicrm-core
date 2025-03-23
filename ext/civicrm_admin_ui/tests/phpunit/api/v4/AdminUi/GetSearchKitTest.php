civicrm/civicrm-core

commit 4b5095b8271271bc64007c5c9213b5ef21b501f2

Revert "afform tabs - use Api4 for custom group / field info"
This reverts commit c929400fa75254097e2d50d582166bb6907e88e0.
Fixes dev/core#5804

ext/civicrm_admin_ui/Civi/Api4/Action/CustomGroup/GetSearchKit.php
@@ -32,15 +32,10 @@ class GetSearchKit extends \Civi\Api4\Generic\BasicBatchAction {
     // SavedSearch and SearchDisplays
     $item['search_name'] = $item['entity_name'] . '_Search';
 
-    // get active fields for this group to include as columns
-    $item['fields'] = (array) \Civi\Api4\CustomField::get(FALSE)
-      ->addSelect('name', 'label', 'option_group_id')
-      ->addWhere('custom_group_id', '=', $item['id'])
-      ->addWhere('is_active', '=', TRUE)
-      // respect "Display in table" config on each field
-      // (Q: should we respect this for other displays?)
-      ->addWhere('in_selector', '=', TRUE)
-      ->execute();
+    // get Active + Display In Table fields for this group to include as columns
+    // note: `in_selector` is the field key for "display in table"
+    $activeFields = \CRM_Core_BAO_CustomGroup::getGroup(['id' => $item['id']])['fields'];
+    $item['fields'] = array_filter($activeFields, fn ($field) => $field['in_selector']);
 
     $managed = [];
 

ext/afform/core/Civi/Afform/Placement/TabsetPlacement.php
@@ -64,12 +64,9 @@ class TabsetPlacement extends AutoSubscriber {
           // custom group tab forms use name, but need to replace tabs using ID
           // remove 'afsearchTabCustom_' from the form name to get the group name
           $groupName = substr($afform['name'], 18);
-          $groupId = \Civi\Api4\CustomGroup::get(FALSE)
-            ->addSelect('id')
-            ->addWhere('name', '=', $groupName)
-            ->execute()->first()['id'] ?? NULL;
-          if ($groupId) {
-            $tabId = 'custom_' . $groupId;
+          $group = \CRM_Core_BAO_CustomGroup::getGroup(['name' => $groupName]);
+          if ($group) {
+            $tabId = 'custom_' . $group['id'];
           }
         }
         // If a tab with that id already exists, allow the afform to replace it.

ext/afform/core/Civi/Api4/Action/CustomGroup/GetAfforms.php
@@ -56,12 +56,8 @@ class GetAfforms extends \Civi\Api4\Generic\BasicBatchAction {
     $forms = [];
 
     // get field names once, for use across all the generate actions
-    $item['field_names'] = \Civi\Api4\CustomField::get(FALSE)
-      ->addSelect('name')
-      ->addWhere('custom_group_id', '=', $item['id'])
-      ->addWhere('is_active', '=', TRUE)
-      ->execute()
-      ->column('name');
+    $fields = \CRM_Core_BAO_CustomGroup::getGroup(['id' => $item['id']])['fields'];
+    $item['field_names'] = array_column($fields, 'name');
 
     // Custom group has no enabled fields; nothing to generate.
     if (!$item['field_names']) {

commit 2f33425f127b541c457fe12766b4747a454a2628

AdminUI - Fix hiding disabled fields in custom field tabs
This regressed in 4b5095b8271271bc64007c5c9213b5ef21b501f2
Adding a test to ensure it stays put.

ext/civicrm_admin_ui/Civi/Api4/Action/CustomGroup/GetSearchKit.php
@@ -33,9 +33,9 @@ class GetSearchKit extends \Civi\Api4\Generic\BasicBatchAction {
     $item['search_name'] = $item['entity_name'] . '_Search';
 
     // get Active + Display In Table fields for this group to include as columns
+    $groupFields = \CRM_Core_BAO_CustomGroup::getGroup(['id' => $item['id']])['fields'];
     // note: `in_selector` is the field key for "display in table"
-    $activeFields = \CRM_Core_BAO_CustomGroup::getGroup(['id' => $item['id']])['fields'];
-    $item['fields'] = array_filter($activeFields, fn ($field) => $field['in_selector']);
+    $item['fields'] = array_filter($groupFields, fn ($field) => $field['in_selector'] && $field['is_active']);
 
     $managed = [];
 
@@ -58,7 +58,7 @@ class GetSearchKit extends \Civi\Api4\Generic\BasicBatchAction {
     $searchLabel = E::ts('%1 Search', [1 => $group['title']]);
 
     // select all fields by name
-    $select = array_map(fn ($field) => $field['name'], $group['fields']);
+    $select = array_column($group['fields'], 'name');
     // add id and entity_id - always useful
     $select[] = 'id';
     $select[] = 'entity_id';

ext/civicrm_admin_ui/tests/phpunit/api/v4/AdminUi/GetSearchKitTest.php
@@ -1,0 +1,68 @@
+<?php
+namespace api\v4\AdminUi;
+
+use Civi\Test\HeadlessInterface;
+
+/**
+ * @group headless
+ */
+class GetSearchKitTest extends \PHPUnit\Framework\TestCase implements HeadlessInterface {
+  use \Civi\Test\Api4TestTrait;
+
+  public function setUpHeadless() {
+    return \Civi\Test::headless()
+      ->installMe(__DIR__)
+      ->apply();
+  }
+
+  public function tearDown(): void {
+    $this->conditionallyDeleteTestRecords();
+    parent::tearDown();
+  }
+
+  public function testGetSearchKit() {
+    $this->createTestRecord('CustomGroup', [
+      'title' => __FUNCTION__,
+      'extends' => 'Contact',
+      'style' => 'Tab with table',
+      'is_multiple' => TRUE,
+    ]);
+
+    $this->saveTestRecords('CustomField', [
+      'records' => [
+        ['label' => 'column1'],
+        ['label' => 'column2', 'is_active' => FALSE],
+        ['label' => 'column3', 'in_selector' => FALSE],
+        ['label' => 'column4'],
+      ],
+      'defaults' => [
+        'custom_group_id.name' => __FUNCTION__,
+        'is_active' => TRUE,
+        'in_selector' => TRUE,
+      ],
+    ]);
+
+    $mgd = \Civi\Api4\CustomGroup::getSearchKit(FALSE)
+      ->addWhere('name', '=', __FUNCTION__)
+      ->execute()->single()['managed'];
+
+    $this->assertCount(2, $mgd);
+    // Match params should be set to prevent error on existing records
+    $this->assertSame(['name'], $mgd[0]['params']['match']);
+    $this->assertEquals(['saved_search_id', 'name'], $mgd[1]['params']['match']);
+    // Inactive and not-in-selector columns should be excluded
+    $this->assertEquals([
+      'column1',
+      'column4',
+      'id',
+      'entity_id',
+    ], $mgd[0]['params']['values']['api_params']['select']);
+    $this->assertEquals([
+      'column1',
+      'column4',
+    ], array_column($mgd[1]['params']['values']['settings']['columns'], 'key'));
+    $this->assertEquals('Custom_testGetSearchKit', $mgd[1]['params']['values']['settings']['toolbar'][0]['entity']);
+    $this->assertEquals('add', $mgd[1]['params']['values']['settings']['toolbar'][0]['action']);
+  }
+
+}