614b9045ffd6196f1e6ecf87af023cc6201f90f1
benjamin <ben@civicrm.org>
2025-04-04 21:16:01 +0100

    [NFC] stricter linting for chart kit

--- ext/chart_kit/ang/crmChartKit/chartKitReduceTypes.service.js
+++ ext/chart_kit/ang/crmChartKit/chartKitReduceTypes.service.js
@@ -9,65 +9,65 @@
     const ts = CRM.ts('chart_kit');
 
     return [
-        {
-            key: 'sum',
-            label: ts('Sum'),
-            final: (f) => f,
-            add: (p, v) => p + v,
-            sub: (p, v) => p - v,
-            start: () => 0
+      {
+        key: 'sum',
+        label: ts('Sum'),
+        final: (f) => f,
+        add: (p, v) => p + v,
+        sub: (p, v) => p - v,
+        start: () => 0
+      },
+      {
+        key: 'count',
+        label: ts('Count'),
+        final: (f) => f,
+        add: (p, v) => p + 1,
+        sub: (p, v) => p - 1,
+        start: () => 0
+      },
+      {
+        key: 'mean',
+        label: ts('Average'),
+        final: (f) => f[0] / f[1],
+        add: (p, v) => [
+          p[0] + v,
+          p[1] + 1,
+        ],
+        sub: (p, v) => [
+          p[0] - v,
+          p[1] - 1,
+        ],
+        start: () => [0, 0]
+      },
+      {
+        key: "percentage_sum",
+        label: ts("Percentage of total"),
+        final: (f, total) => f / total,
+        add: (p, v) => p + v,
+        sub: (p, v) => p - v,
+        start: () => 0
+      },
+      {
+        key: "percentage_count",
+        label: ts("Percentage of count"),
+        final: (f, total) => f / total,
+        add: (p, v) => p + 1,
+        sub: (p, v) => p - 1,
+        start: () => 0
+      },
+      {
+        key: 'list',
+        label: ts('List'),
+        final: (f) => f,
+        add: (p, v) => {
+          if (p.indexOf(v) < 0) {
+            p.push(v);
+          }
+          return p;
         },
-        {
-            key: 'count',
-            label: ts('Count'),
-            final: (f) => f,
-            add: (p, v) => p + 1,
-            sub: (p, v) => p - 1,
-            start: () => 0
-        },
-        {
-            key: 'mean',
-            label: ts('Average'),
-            final: (f) => f[0] / f[1],
-            add: (p, v) => [
-              p[0] + v,
-              p[1] + 1,
-            ],
-            sub: (p, v) => [
-              p[0] - v,
-              p[1] - 1,
-            ],
-            start: () => [0, 0]
-        },
-        {
-            key: "percentage_sum",
-            label: ts("Percentage of total"),
-            final: (f, total) => f / total,
-            add: (p, v) => p + v,
-            sub: (p, v) => p - v,
-            start: () => 0
-        },
-        {
-            key: "percentage_count",
-            label: ts("Percentage of count"),
-            final: (f, total) => f / total,
-            add: (p, v) => p + 1,
-            sub: (p, v) => p - 1,
-            start: () => 0
-        },
-        {
-            key: 'list',
-            label: ts('List'),
-            final: (f) => f,
-            add: (p, v) => {
-              if (p.indexOf(v) < 0) {
-                p.push(v);
-              }
-              return p;
-            },
-            sub: (p, v) => p.filter((x) => x != v),
-            start: () => []
-        }
+        sub: (p, v) => p.filter((x) => x != v),
+        start: () => []
+      }
     ];
   });
 })(angular, CRM.$, CRM._);
