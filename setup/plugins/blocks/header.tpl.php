d1df2e3450df83cff2d30ed81b7577c1bb78648b
Mathieu Lu <mathieu@symbiotic.coop>
2025-04-17 15:02:29 -0400

    setup: move Sample Data lower, tweak the wording, remove redundant thanks

--- setup/plugins/blocks/header.tpl.php
+++ setup/plugins/blocks/header.tpl.php
@@ -1,17 +1,13 @@
 <?php if (!defined('CIVI_SETUP')): exit("Installation plugins must only be loaded by the installer.\n");
 endif; ?>
 <div class="civicrm-setup-header">
-    <div class="title">
-        <h1><?php echo ts("Thanks for choosing CiviCRM. You're nearly there!"); ?><hr></h1>
-    </div>
-    <div class="civicrm-logo"><strong><?php echo ts('Version %1', array(1 => "{$civicrm_version} {$model->cms}")); ?></strong>
-        <span><img src=<?php echo $installURLPath . "updated-logo.jpg"?> /></span>
-     </div>
+  <p><strong><?php echo ts('Version %1', array(1 => "{$civicrm_version} {$model->cms}")); ?></strong></p>
+  <div class="civicrm-logo">
+    <img src=<?php echo $installURLPath . "updated-logo.jpg"?> />
+  </div>
 </div>
-<h2><?php echo ts("CiviCRM Installer"); ?></h2>
-
 <noscript>
 <p class="error"><?php echo ts("Error: Javascipt appears to be disabled. The CiviCRM web-based installer requires Javascript.");?></p>
 </noscript>
-
-<p><?php echo ts("Thanks for choosing CiviCRM! Please follow the instructions below to install CiviCRM."); ?></p>
+<h2><?php echo ts("CiviCRM Installer"); ?></h2>
+<p class="crm-slide-effect"><?php echo ts("Thanks for choosing CiviCRM! You're nearly there!") . ' ' . ts("Please follow the instructions below to install CiviCRM."); ?> &#x1f331;</p>
