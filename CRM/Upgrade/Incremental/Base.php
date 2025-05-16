d1a8138f23cfda44503d8ac8077a1e756e949a12
colemanw <coleman@civicrm.org>
2025-05-15 20:29:54 -0600

    UserJob - Add `search_display_id` column

--- CRM/Upgrade/Incremental/Base.php
+++ CRM/Upgrade/Incremental/Base.php
@@ -398,16 +398,20 @@ class CRM_Upgrade_Incremental_Base {
    * @param string $fieldName
    * @param array $fieldSpec
    *   As definied in the .entityType.php file for $entityName
+   * @param string|null $after
    * @return bool
    * @throws CRM_Core_Exception
    */
-  public static function alterSchemaField($ctx, string $entityName, string $fieldName, array $fieldSpec): bool {
+  public static function alterSchemaField($ctx, string $entityName, string $fieldName, array $fieldSpec, ?string $after = NULL): bool {
     $tableName = Civi::entity($entityName)->getMeta('table');
     $fieldSql = Civi::schemaHelper()->arrayToSql($fieldSpec);
     if (CRM_Core_BAO_SchemaHandler::checkIfFieldExists($tableName, $fieldName, FALSE)) {
       return self::alterColumn($ctx, $tableName, $fieldName, $fieldSql, !empty($fieldSpec['localizable']));
     }
     else {
+      if ($after) {
+        $fieldSql .= " AFTER `$after`";
+      }
       return self::addColumn($ctx, $tableName, $fieldName, $fieldSql, !empty($fieldSpec['localizable']));
     }
   }
