a911a77abc29735edf7a194cd5ddedaff1e10898
colemanw <coleman@civicrm.org>
2025-05-15 20:28:57 -0600

    SqlGenerator - Make foreign keys optional for EntityReference
    Allows a field to declare an entity reference
    without adding a FK index in SQL.

--- mixin/lib/civimix-schema@5/src/SqlGenerator.php
+++ mixin/lib/civimix-schema@5/src/SqlGenerator.php
@@ -144,7 +144,8 @@ return new class() {
   private function getTableConstraints(array $entity): array {
     $constraints = [];
     foreach ($entity['getFields']() as $fieldName => $field) {
-      if (!empty($field['entity_reference']['entity'])) {
+      // `entity_reference.fk` defaults to TRUE if not set. If FALSE, do not add constraint.
+      if (!empty($field['entity_reference']['entity']) && ($field['entity_reference']['fk'] ?? TRUE)) {
         $fkName = \CRM_Core_BAO_SchemaHandler::getIndexName($entity['table'], $fieldName);
         $constraint = "CONSTRAINT `FK_$fkName` FOREIGN KEY (`$fieldName`)" .
           " REFERENCES `" . $this->getTableForEntity($field['entity_reference']['entity']) . "`(`{$field['entity_reference']['key']}`)";
