b8edd6f361f4fe82691fdcf4f5052d85d858c73c
colemanw <coleman@civicrm.org>
2025-05-17 15:31:28 -0600

    CiviImport - prevent hard crash in Api4 getFields

--- ext/search_kit/CRM/Search/Import/Parser.php
+++ ext/search_kit/CRM/Search/Import/Parser.php
@@ -277,6 +277,10 @@ class CRM_Search_Import_Parser extends CRM_Import_Parser {
 
   public function init() {
     $userJob = $this->getUserJob();
+    if (empty($userJob['search_display_id.name'])) {
+      // Exception is caught by ImportSpecProvider::modifySpec to prevent hard crash in Api4 getFields
+      throw new \CRM_Core_Exception('No search display found for this job.');
+    }
     $this->display = $userJob['search_display_id.name'];
     $this->savedSearch = $userJob['search_display_id.saved_search_id.name'];
     $this->loadSavedSearch();
