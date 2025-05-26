119c5f8e5b608bff0825457a2b5e80b347179d36
Shane <shane@symbiotic.coop>
2025-05-26 13:21:42 -0400

    Selectors for auto-ts in one place

--- ext/afform/core/Civi/Afform/Translator.php
+++ ext/afform/core/Civi/Afform/Translator.php
@@ -2,8 +2,8 @@
 
 namespace Civi\Afform;
 
+use Civi\Angular\ChangeSet;
 use Civi\Core\Service\AutoService;
-use Smarty\Filter\FilterInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -11,6 +11,9 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
  */
 class Translator extends AutoService implements EventSubscriberInterface {
 
+  /**
+   *
+   */
   public static function getSubscribedEvents() {
     return [
       '&hook_civicrm_alterAngular' => 'translateAfform',
@@ -22,58 +25,87 @@ class Translator extends AutoService implements EventSubscriberInterface {
    */
   public function translateAfform($angular) {
 
-   /**
-    * Find all the content that should be auto send to ts function
-    * There are 3 kinds of translation :
-    * - html tag content
-    * - html attribute values
-    * - json sub-attribute in defn html attribute
-    */
+    /**
+     * Find all the content that should be auto send to ts function
+     * There are 3 kinds of translation :
+     * - html tag content
+     * - html attribute values
+     * - json sub-attribute in defn html attribute
+     */
 
-    $changeSet = \Civi\Angular\ChangeSet::create('translate')
-      ->alterHtml(';\.aff\.html$;', function (\phpQueryObject $doc) {
+    $changeSet = ChangeSet::create('translate')
+      ->alterHtml(
+        ';\.aff\.html$;', function (\phpQueryObject $doc) {
 
-        // content to be translated
-        $contentSelectors = 'p.af-text, div.af-markup, button';
-        $doc->find($contentSelectors)->each(function(\DOMElement $item) {
-          $this->translateContent($item);
-        });
-
-        // attributes to be translated
-        foreach (['af-title', 'af-copy', 'af-repeat'] as $attribute) {
-          $doc->find('[' . $attribute . ']')->each(function(\DOMElement $item) use ($attribute) {
-            $this->translateAttribute($item, $attribute);
-          });
-        }
-
-        // defn sub-attributes to be translated
-        $doc->find('af-field[defn]')->each(function(\DOMElement $item) {
-          $defn = \CRM_Utils_JS::decode($item->getAttribute('defn'));
-          foreach (['label', 'help_pre', 'help_post', 'placeholder'] as $attribute) {
-            if (isset($defn[$attribute])) {
-              $defn[$attribute] = ts($defn[$attribute]);
-            }
-          }
-          if (isset($defn['options'])) {
-            foreach ($defn['options'] as $idx => $option) {
-              if (isset($option['label'])) {
-                $defn['options'][$idx]['label'] = ts($option['label']);
+          // Translate content.
+          $contentSelectors = \CRM_Utils_JS::getContentSelectors();
+          $contentSelectors = implode(',', $contentSelectors);
+          $doc->find($contentSelectors)->each(
+            function (\DOMElement $item) use ($contentSelectors) {
+              $markup = '';
+              foreach ($item->childNodes as $child) {
+                $markup .= $child->ownerDocument->saveXML($child);
+              }
+              $markup = trim($markup);
+              if (!empty($markup)) {
+                $item->replaceWith(_ts($markup));
               }
             }
+          );
+
+          // Translate Attributes.
+          $attributeSelectors = \CRM_Utils_JS::getAttributeSelectors();
+          foreach ($attributeSelectors as $attribute) {
+            $doc->find('[' . $attribute . ']')->each(
+              function (\DOMElement $item) use ($attribute) {
+                $this->translateAttribute($item, $attribute);
+              }
+            );
           }
-          $item->setAttribute('defn', \CRM_Utils_JS::encode($defn));
-        });
-      });
+
+          // Translate Defn values.
+          $defnSelectors = \CRM_Utils_JS::getDefnSelectors();
+          $inputSelectors = \CRM_Utils_JS::getInputAttributeSelectors();
+          $doc->find('af-field[defn]')->each(
+            function (\DOMElement $item) use ($defnSelectors, $inputSelectors) {
+              $defn = \CRM_Utils_JS::decode($item->getAttribute('defn'), 1);
+              // $item->setAttribute('debug', gettype($defn));
+              foreach ($defnSelectors as $attribute) {
+                if (isset($defn[$attribute])) {
+                  if (is_array($defn[$attribute])) {
+                    foreach ($defn[$attribute] as $input) {
+                      $this->translateItemArray($input, $inputSelectors);
+                    }
+                  }
+                  else {
+                    $this->translateItemArray($defn[$attribute], $inputSelectors);
+                  }
+                }
+              }
+              $item->setAttribute('defn', \CRM_Utils_JS::encode($defn));
+            }
+          );
+        }
+      );
     $angular->add($changeSet);
   }
 
-  public function translateContent(&$item) {
-    $item->textContent = _ts($item->textContent);
+  /**
+   * Helper to translate attributes.
+   */
+  public function translateAttribute(&$item, $attribute) {
+    $item->setAttribute($attribute, _ts($item->getAttribute($attribute)));
   }
 
-  public function translateAttribute(&$item, $attribute) {
-    $item->setAttribute($attribute, ts($item->getAttribute($attribute)));
+  /**
+   * Helper to translate subattributes.
+   */
+  public function translateItemArray(&$item, $selectors) {
+    foreach ($selectors as $selector) {
+      if (isset($item[$selector])) {
+        $item[$selector] = _ts($item[$selector]);
+      }
+    }
   }
 
 }
-
