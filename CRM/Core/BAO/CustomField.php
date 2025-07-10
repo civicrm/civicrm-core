3ec61dce5cbd9311ac789164ba130e73355caa11
Adam Wood <72983627+webmaster-cses-org-uk@users.noreply.github.com>
2025-07-10 08:34:51 +0100

    force frontEnd when $absolute is true
    Co-authored-by: colemanw <coleman@civicrm.org>

--- CRM/Core/BAO/CustomField.php
+++ CRM/Core/BAO/CustomField.php
@@ -1382,7 +1382,7 @@ class CRM_Core_BAO_CustomField extends CRM_Core_DAO_CustomField implements \Civi
             'uri'
           );
           $fileHash = CRM_Core_BAO_File::generateFileHash(NULL, $fileID);
-          $url = CRM_Utils_System::url('civicrm/file', "reset=1&id=$fileID&eid=$contactID&fcs=$fileHash", $absolute);
+          $url = CRM_Utils_System::url('civicrm/file', "reset=1&id=$fileID&eid=$contactID&fcs=$fileHash", $absolute, NULL, TRUE, $absolute);
           $result['file_url'] = CRM_Utils_File::getFileURL($uri, $fileType, $url);
         }
       }
