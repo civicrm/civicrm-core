008643fb0d449686bc2babe48cffc9092391f528
eileen <emcnaughton@wikimedia.org>
2025-07-15 16:10:03 +1200

    Align Contribution Import header handling with other imports

--- CRM/Contribute/Import/Parser/Contribution.php
+++ CRM/Contribute/Import/Parser/Contribution.php
@@ -144,10 +144,15 @@ class CRM_Contribute_Import_Parser_Contribution extends CRM_Import_Parser {
         continue;
       }
       $fieldSpec = $this->getFieldMetadata($mappedField['name']);
-      $columnHeader = $this->getUserJob()['metadata']['DataSource']['column_headers'][$i] ?? '';
       // If there is no column header we are dealing with an added value mapping, do not use
       // the database value as it will be for (e.g.) `_status`
-      $fieldValue = $columnHeader ? $values[$i] : '';
+      $headers = $this->getUserJob()['metadata']['DataSource']['column_headers'];
+      if (array_key_exists($i, $headers) && empty($headers[$i])) {
+        $fieldValue = '';
+      }
+      else {
+        $fieldValue = $values[$i];
+      }
       if ($fieldValue === '' && isset($mappedField['default_value'])) {
         $fieldValue = $mappedField['default_value'];
       }
