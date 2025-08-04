e6949958a4c529111bdad093b5592740d7a3e566
benjamin <ben@civicrm.org>
2025-08-04 13:33:28 +0100

    search_kit_reports - separate out permission check from CiviReport

--- ext/search_kit_reports/search_kit_reports.php
+++ ext/search_kit_reports/search_kit_reports.php
@@ -30,3 +30,19 @@ function search_kit_reports_civicrm_install(): void {
 function search_kit_reports_civicrm_enable(): void {
   _search_kit_reports_civix_civicrm_enable();
 }
+
+/**
+ * Implements hook_civicrm_permissions().
+ *
+ * Add a new permission 'access Reports' which is implied by access CiviReport, but does
+ * not require you to have the component enabled
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_permission
+ */
+function search_kit_reports_civicrm_permission(&$permissions): void {
+  $permissions['access Reports'] = [
+    'label' => ts('access Reports'),
+    'description' => ts('Access new-style SearchKit Reports'),
+    'implied_by' => ['access CiviReport'],
+  ];
+}
