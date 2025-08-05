77419e142b9e1e3c5dc95d47c64c9fb6b7772c80
eileen <emcnaughton@wikimedia.org>
2025-08-05 18:21:04 +1200

    Handle legacy user jobs with no baseEntity

--- ext/civiimport/CRM/CiviImport/Form/Generic/GenericTrait.php
+++ ext/civiimport/CRM/CiviImport/Form/Generic/GenericTrait.php
@@ -11,8 +11,13 @@ trait CRM_CiviImport_Form_Generic_GenericTrait {
     if (!$this->isStandalone()) {
       return $this->controller->getStateMachine()->getEntity();
     }
-    elseif ($this->getUserJobID()) {
+    elseif ($this->getUserJobID() && isset($this->getUserJob()['metadata']['base_entity'])) {
       return $this->getUserJob()['metadata']['base_entity'];
+    }
+    // We don't try this for import_generic as that import will either get it from the url (below)
+    // or have it saved in the metadata already (above).
+    elseif ($this->getUserJobID() && $this->getUserJobType() !== 'import_generic') {
+      return $this->getParser()->getBaseEntity();
     }
     $pathArguments = explode('/', (CRM_Utils_System::currentPath() ?: ''));
     unset($pathArguments[0], $pathArguments[1]);
