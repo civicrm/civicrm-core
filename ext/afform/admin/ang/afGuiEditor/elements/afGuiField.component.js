civicrm/civicrm-core

commit be2efb5b7d6bd81aa5abf584ca01730f185cdee1

allow read-only fields in FormBuilder

ext/afform/admin/ang/afGuiEditor/elements/afGuiField.component.js
@@ -169,6 +169,16 @@
 
       function inputTypeCanBe(type) {
         var defn = ctrl.getDefn();
+        if (defn.readonly) {
+          switch (type) {
+            case 'DisplayOnly':
+            case 'Hidden':
+              return true;
+
+            default:
+              return false;
+          }
+        }
         if (defn.input_type === type) {
           return true;
         }

Civi/Api4/Generic/BasicGetFieldsAction.php
@@ -109,6 +109,8 @@ class BasicGetFieldsAction extends BasicGetAction {
    *
    * Format option lists.
    *
+   * Configure read-only input fields based on the action.
+   *
    * In most cases it's not necessary to override this function, even if your entity is really weird.
    * Instead just override $this->fields and this function will respect that.
    *
@@ -143,6 +145,9 @@ class BasicGetFieldsAction extends BasicGetAction {
       }
       if (isset($defaults['options'])) {
         $this->formatOptionList($field);
+      }
+      if ($this->getAction() === 'create' && $field['readonly'] === TRUE) {
+        $field['input_type'] = 'DisplayOnly';
       }
       $field = array_diff_key($field, $internalProps);
     }
@@ -328,6 +333,7 @@ class BasicGetFieldsAction extends BasicGetAction {
           'ChainSelect' => ts('Chain-Select'),
           'CheckBox' => ts('Checkboxes'),
           'Date' => ts('Date Picker'),
+          'DisplayOnly' => ts('Display Only'),
           'Email' => ts('Email'),
           'EntityRef' => ts('Autocomplete Entity'),
           'File' => ts('File'),

ext/afform/admin/ang/afGuiEditor/afGuiEntity.component.js
@@ -68,9 +68,7 @@
 
         function filterFields(fields) {
           return _.transform(fields, function(fieldList, field) {
-            if (!field.readonly &&
-              (!search || _.contains(field.name, search) || _.contains(field.label.toLowerCase(), search))
-            ) {
+            if (!search || _.contains(field.name, search) || _.contains(field.label.toLowerCase(), search)) {
               fieldList.push(fieldDefaults(field));
             }
           }, []);

commit e046032aefac15b0406c11096ea0ccc1aefaecef

FormBuilder - Fix using readonly fields as filters
Regressed with be2efb5b7d6bd81aa5abf584ca01730f185cdee1

ext/afform/admin/ang/afGuiEditor/elements/afGuiField.component.js
@@ -186,7 +186,7 @@
         if (defn.input_type === type) {
           return true;
         }
-        if (defn.readonly) {
+        if (defn.readonly && !ctrl.isSearch()) {
           switch (type) {
             case 'DisplayOnly':
             case 'Hidden':