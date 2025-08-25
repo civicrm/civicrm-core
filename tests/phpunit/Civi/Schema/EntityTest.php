7c35bb08c0ab735fc8e9631fde1ac5d2b234a211
colemanw <coleman@civicrm.org>
2025-08-25 19:23:16 -0400

    DAO_Base - Switch to using entityProvider
    Using the entityProvider object improves consistency, and ensures that the new 'civi.entity.fields' event
    is respected for field alterations.

--- tests/phpunit/Civi/Schema/EntityTest.php
+++ tests/phpunit/Civi/Schema/EntityTest.php
@@ -2,7 +2,9 @@
 
 namespace Civi\Schema;
 
+use Civi\Api4\Activity;
 use Civi\Core\Event\GenericHookEvent;
+use CRM_Activity_BAO_Activity;
 
 class EntityTest extends \CiviUnitTestCase {
 
@@ -27,7 +29,12 @@ class EntityTest extends \CiviUnitTestCase {
     }
   }
 
-  public function testGetFields(): void {
+  /**
+   * Ensures getFields returns expected results and tests the `civi.entity.fields` listener.
+   *
+   * @return void
+   */
+  public function testGetFieldsWithHookAlterations(): void {
     $dispatcher = \Civi::service('dispatcher');
     $dispatcher->addListener('civi.entity.fields::Activity', [$this, 'onActivityFields']);
 
@@ -55,11 +62,41 @@ class EntityTest extends \CiviUnitTestCase {
     $this->assertSame('Altered ID Title', $fields['id']['title']);
     // Ensure it also works with getField
     $this->assertSame('Altered ID Title', $entity->getField('id')['title']);
+    // Test new field
+    $this->assertSame('New Field', $fields['new_field']['title']);
+
+    // Test with legacy DAO adapter
+    $baoFields = CRM_Activity_BAO_Activity::fields();
+    $this->assertSame('Altered ID Title', $baoFields['activity_id']['title']);
+    $this->assertSame('New Field', $baoFields['new_field']['title']);
+
+    // Test with Api4 getFields
+    $apiFields = Activity::getFields(FALSE)
+      ->execute()->indexBy('name');
+    // Test hook alterations
+    $this->assertSame('Altered ID Title', $apiFields['id']['title']);
+    // Test new field
+    $this->assertSame('New Field', $apiFields['new_field']['title']);
+    $this->assertSame('New fake field', $apiFields['new_field']['description']);
   }
 
   public function onActivityFields(GenericHookEvent $event): void {
     $this->assertSame('Activity', $event->entity);
+    // Alter existing field
     $event->fields['id']['title'] = 'Altered ID Title';
+
+    // Insert new field
+    $event->fields['new_field'] = [
+      'title' => 'New Field',
+      'sql_type' => 'varchar(255)',
+      'input_type' => 'Text',
+      'description' => 'New fake field',
+      'usage' => [
+        'import',
+        'export',
+        'duplicate_matching',
+      ],
+    ];
   }
 
 }
