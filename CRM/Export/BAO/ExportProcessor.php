662dc9f74015f887b1a0332de3ddb884e721514c
Seamus Lee <seamus.lee@jmaconsulting.biz>
2025-09-03 10:22:58 +1000

    Fix notice error

--- CRM/Export/BAO/ExportProcessor.php
+++ CRM/Export/BAO/ExportProcessor.php
@@ -1439,6 +1439,12 @@ class CRM_Export_BAO_ExportProcessor {
     // tests will fail on the enotices until they all are & then all the 'else'
     // below can go.
     $fieldSpec = $queryFields[$columnName] ?? [];
+    if (empty($fieldSpec['html_type']) && !empty($fieldSpec['html'])) {
+      $fieldSpec['html_type'] = $fieldSpec['html']['type'];
+    }
+    elseif (empty($fieldSpec['html_type'])) {
+      $fieldSpec['html_type'] = '';
+    }
     $type = $fieldSpec['type'] ?? ($fieldSpec['data_type'] ?? '');
     // set the sql columns
     if ($type) {
