da4e5e089bf8cb08b270580da5ee57319bceca71
colemanw <coleman@civicrm.org>
2025-09-23 09:50:57 -0400

    Quicksearch - fix state, country, etc.
    Fixes dev/core#6081
    
    Pseudoconstant suffixes shouldn't be in the inner-part
    of the UNION, as they get looked up after the final query.
    
    Also, in the case of the joins to address_primary, etc,
    must use the field name exactly, without the join prefix
    (e.g. `country_id`, not `address_primary.country_id`) when
    looking up pseudoconstant.

--- tests/phpunit/api/v4/Action/AutocompleteQuicksearchTest.php
+++ tests/phpunit/api/v4/Action/AutocompleteQuicksearchTest.php
@@ -31,6 +31,7 @@ class AutocompleteQuicksearchTest extends \api\v4\Api4TestBase {
     \Civi::settings()->revert('quicksearch_options');
     \Civi::settings()->revert('contact_autocomplete_options');
     \Civi::settings()->revert('includeNickNameInName');
+    \Civi::settings()->revert('includeWildCardInName');
     parent::tearDown();
   }
 
@@ -310,7 +311,8 @@ class AutocompleteQuicksearchTest extends \api\v4\Api4TestBase {
     Setting::set(FALSE)
       ->addValue('includeNickNameInName', FALSE)
       ->addValue('includeEmailInName', TRUE)
-      ->addValue('contact_autocomplete_options', [1, 2, 4, 5, 6])
+      ->addValue('includeWildCardInName', TRUE)
+      ->addValue('contact_autocomplete_options', [1, 2, 4, 6, 7])
       ->execute();
     $contacts = $this->saveTestRecords('Contact', [
       'records' => [
@@ -319,24 +321,24 @@ class AutocompleteQuicksearchTest extends \api\v4\Api4TestBase {
           'last_name' => 'AttestXYZsmith',
           'email_primary.email' => 'bob@example.com',
           'address_primary.street_address' => '1270 Marigold Lane',
-          'address_priamry.state_prvoince_id:abbr' => 'FL',
-          'address_primary.country_id:abbr' => 'US',
+          'address_primary.state_province_id:abbr' => 'FL',
+          'address_primary.country_id.name' => 'United States',
         ],
         [
           'first_name' => 'William',
           'last_name' => 'TestXYZsmithson',
           'email_primary.email' => 'bill@example.com',
           'address_primary.street_address' => '1100 Marigold Lane',
-          'address_priamry.state_prvoince_id:abbr' => 'FL',
-          'address_primary.country_id:abbr' => 'US',
+          'address_primary.state_province_id:abbr' => 'FL',
+          'address_primary.country_id.name' => 'United States',
         ],
         [
           'first_name' => 'Mary',
           'last_name' => 'TestXYZblacksmith',
           'email_primary.email' => 'mary@example.com',
           'address_primary.street_address' => '1100 Marigold Lane',
-          'address_priamry.state_prvoince_id:abbr' => 'FL',
-          'address_primary.country_id:abbr' => 'US',
+          'address_primary.state_province_id:abbr' => 'FL',
+          'address_primary.country_id.name' => 'United States',
         ],
       ],
     ]);
@@ -344,10 +346,14 @@ class AutocompleteQuicksearchTest extends \api\v4\Api4TestBase {
       ->setFormName('crmMenubar')
       ->setFieldName('crm-qsearch-input')
       ->setInput('testXYZsmith')
-      ->execute();
+      ->execute()
+      ->indexBy('id');
     $this->assertCount(2, $result);
     $this->assertEquals('AttestXYZsmith, Robert', $result[$contacts[0]['id']]['label']);
     $this->assertEquals('bob@example.com', $result[$contacts[0]['id']]['description'][0]);
+    $this->assertEquals('1270 Marigold Lane', $result[$contacts[0]['id']]['description'][1]);
+    $this->assertEquals('FL', $result[$contacts[0]['id']]['description'][2]);
+    $this->assertEquals('United States', $result[$contacts[0]['id']]['description'][3]);
     $this->assertEquals('TestXYZsmithson, William', $result[$contacts[1]['id']]['label']);
 
   }
