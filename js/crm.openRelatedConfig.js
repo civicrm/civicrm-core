d1a3fc6d6c2d2865188f7f1a58e5934822dfaba6
colemanw <coleman@civicrm.org>
2025-10-02 21:41:58 -0400

    AdminUI - Fix links to profile fields
    Fixes dev/core#6134

--- js/crm.openRelatedConfig.js
+++ js/crm.openRelatedConfig.js
@@ -4,13 +4,16 @@ CRM.$(function($) {
    * Set the href for the "Fields" button associated with a select field
    * or hide the button if there was no value selected.
    */
-  function setRelatedConfigLink($select, path, params) {
+  function setRelatedConfigLink($select, path, key) {
     var val = $select.val();
     if (val) {
-      params[params.key] = val;
+      const params = {};
+      params[key] = val;
       // Most URLs expect reset=1
       params.reset = 1;
       params.action = 'browse';
+      // Sorta hackish solution to support both with and without the AdminUI extension
+      path += '#/?' + key + '=' + val;
       var url = CRM.url(path, params);
       $select.siblings('.crm-button').attr('href', url).show();
     }
@@ -23,19 +26,19 @@ CRM.$(function($) {
     // Let administrators to view the related configuration
     // for a Profile or a PriceSet
     .on('change', 'select.crm-form-select-profile', function() {
-      setRelatedConfigLink($(this), 'civicrm/admin/uf/group/field', {key: 'gid'});
+      setRelatedConfigLink($(this), 'civicrm/admin/uf/group/field', 'gid');
     })
     .on('change', 'select.crm-form-select-priceset', function() {
-      setRelatedConfigLink($(this), 'civicrm/admin/price/field', {key: 'sid'});
+      setRelatedConfigLink($(this), 'civicrm/admin/price/field', 'sid');
     });
 
   // Initial state (and additional profiles, ex: for Events)
   $('body').on('crmLoad', function() {
     $('select.crm-form-select-profile', this).each(function() {
-      setRelatedConfigLink($(this), 'civicrm/admin/uf/group/field', {key: 'gid'});
+      setRelatedConfigLink($(this), 'civicrm/admin/uf/group/field', 'gid');
     });
     $('select.crm-form-select-priceset', this).each(function() {
-      setRelatedConfigLink($(this), 'civicrm/admin/price/field', {key: 'sid'});
+      setRelatedConfigLink($(this), 'civicrm/admin/price/field', 'sid');
     });
   });
 });
