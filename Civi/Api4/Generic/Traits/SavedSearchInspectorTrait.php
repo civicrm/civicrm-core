civicrm/civicrm-core

commit 739d2e31afe7e22f864f24d1ad7d44254bc678c5

SearchKit - Fix filters on boolean functions
When exposing a filter for a transformed field e.g. IS_NOT_NULL,
this ensures the `=` operator is used instead of defaulting to `CONTAINS`.

Fixes dev/core#6056

Civi/Api4/Generic/Traits/SavedSearchInspectorTrait.php
@@ -5,6 +5,7 @@ namespace Civi\Api4\Generic\Traits;
 use Civi\API\Exception\UnauthorizedException;
 use Civi\API\Request;
 use Civi\Api4\Action\SearchDisplay\AbstractRunAction;
+use Civi\Api4\Query\Api4SelectQuery;
 use Civi\Api4\Query\SqlEquation;
 use Civi\Api4\Query\SqlExpression;
 use Civi\Api4\Query\SqlField;
@@ -145,17 +146,18 @@ trait SavedSearchInspectorTrait {
   /**
    * Returns a Query object for the search entity, or FALSE if it doesn't have a DAO
    *
-   * @return \Civi\Api4\Query\Api4SelectQuery|bool
+   * @return \Civi\Api4\Query\Api4SelectQuery|null
    */
-  private function getQuery() {
+  private function getQuery():? Api4SelectQuery {
     if (!isset($this->_selectQuery) && !empty($this->savedSearch['api_entity'])) {
       if (!CoreUtil::isType($this->savedSearch['api_entity'], 'DAOEntity')) {
-        return $this->_selectQuery = FALSE;
+        $this->_selectQuery = FALSE;
+        return NULL;
       }
       $api = Request::create($this->savedSearch['api_entity'], 'get', $this->savedSearch['api_params']);
-      $this->_selectQuery = new \Civi\Api4\Query\Api4SelectQuery($api);
+      $this->_selectQuery = new Api4SelectQuery($api);
     }
-    return $this->_selectQuery;
+    return $this->_selectQuery ?: NULL;
   }
 
   /**
@@ -302,16 +304,27 @@ trait SavedSearchInspectorTrait {
     $filterClauses = [];
 
     foreach ($fieldNames as $fieldName) {
-      $field = $this->getField($fieldName);
-      $dataType = $field['data_type'] ?? NULL;
-      $operators = array_values($field['operators'] ?? []) ?: CoreUtil::getOperators();
+      // If the filter is an alias, it needs to be fetched from the select clause
+      $selectExpr = $this->getSelectExpression($fieldName);
+      // Else parse the expression directly
+      $expr = $selectExpr['expr'] ?? SqlExpression::convert($fieldName);
+      $dataType = $expr->getRenderedDataType($this->getQuery());
+      $operators = CoreUtil::getOperators();
+      if (is_a($expr, SqlField::class)) {
+        $field = $this->getField($fieldName);
+        $operators = array_values($field['operators'] ?? []) ?: CoreUtil::getOperators();
+        $options = $field['options'] ?? NULL;
+      }
+      elseif (is_a($expr, SqlFunction::class)) {
+        $options = $expr->getOptions();
+      }
       // Array is either associative `OP => VAL` or sequential `IN (...)`
       if (is_array($value)) {
         $value = array_filter($value, [$this, 'hasValue']);
         // If array does not contain operators as keys, assume array of values
         if (array_diff_key($value, array_flip(CoreUtil::getOperators()))) {
           // Use IN for regular fields
-          if (empty($field['serialize'])) {
+          if ($dataType !== 'Array') {
             $op = in_array('IN', $operators, TRUE) ? 'IN' : $operators[0];
             $filterClauses[] = [$fieldName, $op, $value, FALSE];
           }
@@ -339,10 +352,10 @@ trait SavedSearchInspectorTrait {
           }
         }
       }
-      elseif (!empty($field['serialize']) && in_array('CONTAINS', $operators, TRUE)) {
+      elseif ($dataType === 'Array' && in_array('CONTAINS', $operators, TRUE)) {
         $filterClauses[] = [$fieldName, 'CONTAINS', $value, FALSE];
       }
-      elseif ((!empty($field['options']) || in_array($dataType, ['Integer', 'Boolean', 'Date', 'Timestamp'])) && in_array('=', $operators, TRUE)) {
+      elseif ((!empty($options) || in_array($dataType, ['Integer', 'Boolean', 'Date', 'Timestamp'])) && in_array('=', $operators, TRUE)) {
         $filterClauses[] = [$fieldName, '=', $value, FALSE];
       }
       elseif ($prefixWithWildcard && in_array('CONTAINS', $operators, TRUE)) {

Civi/Api4/Query/Api4EntitySetQuery.php
@@ -134,7 +134,7 @@ class Api4EntitySetQuery extends Api4Query {
    * @param string $expr
    * @return array|null
    */
-  public function getField($expr) {
+  public function getField(string $expr):? array {
     $col = strpos($expr, ':');
     $fieldName = $col ? substr($expr, 0, $col) : $expr;
     return $this->apiFieldSpec[$fieldName] ?? NULL;

Civi/Api4/Query/Api4Query.php
@@ -64,6 +64,8 @@ abstract class Api4Query {
     $this->api = $api;
   }
 
+  abstract public function getField(string $expr):? array;
+
   /**
    * Builds main final sql statement after initialization.
    *
@@ -328,7 +330,7 @@ abstract class Api4Query {
           $expr = $this->getExpression($this->selectAliases[$fieldAlias], ['SqlFunction']);
           $fauxField = [
             'name' => NULL,
-            'data_type' => $expr->getRenderedDataType($this->apiFieldSpec),
+            'data_type' => $expr->getRenderedDataType($this),
           ];
           FormattingUtil::formatInputValue($value, NULL, $fauxField, $this->entityValues, $operator);
         }

Civi/Api4/Query/Api4SelectQuery.php
@@ -394,11 +394,11 @@ class Api4SelectQuery extends Api4Query {
    * @param bool $strict
    *   In strict mode, this will throw an exception if the field doesn't exist
    *
-   * @return array|bool|null
+   * @return array|null
    * @throws \CRM_Core_Exception
    * @throws UnauthorizedException
    */
-  public function getField($expr, $strict = FALSE) {
+  public function getField(string $expr, bool $strict = FALSE):? array {
     // If the expression contains a pseudoconstant filter like activity_type_id:label,
     // strip it to look up the base field name, then add the field:filter key to apiFieldSpec
     $col = strpos($expr, ':');
@@ -420,7 +420,7 @@ class Api4SelectQuery extends Api4Query {
     if ($field) {
       $this->apiFieldSpec[$expr] = $field;
     }
-    return $field;
+    return $field ?: NULL;
   }
 
   public function getFieldSibling(array $field, string $siblingFieldName) {

Civi/Api4/Query/SqlExpression.php
@@ -228,6 +228,10 @@ abstract class SqlExpression {
     return static::$dataType;
   }
 
+  public function getRenderedDataType(?Api4Query $query):? string {
+    return static::getDataType();
+  }
+
   /**
    * Shift a keyword off the beginning of the argument string and return it.
    *

Civi/Api4/Query/SqlField.php
@@ -38,4 +38,18 @@ class SqlField extends SqlExpression {
     return ts('Field');
   }
 
+  /**
+   * Returns the output dataType based on field definition
+   *
+   * @param
+   * @return string|null
+   */
+  public function getRenderedDataType(?Api4Query $query): ?string {
+    $field = $query?->getField($this->expr);
+    if (!empty($field['serialize'])) {
+      return 'Array';
+    }
+    return $field['data_type'] ?? NULL;
+  }
+
 }

Civi/Api4/Query/SqlFunction.php
@@ -285,11 +285,10 @@ abstract class SqlFunction extends SqlExpression {
   /**
    * Returns the dataType of rendered output, based on the fields passed into the function
    *
-   * @param array $fieldSpecs
-   *   List of available fields, e.g. Api4Query::$apiFieldSpec
+   * @param
    * @return string|null
    */
-  public function getRenderedDataType(array $fieldSpecs): ?string {
+  public function getRenderedDataType(?Api4Query $query): ?string {
     $dataType = $this::getDataType();
     if ($dataType) {
       return $dataType;
@@ -298,8 +297,9 @@ abstract class SqlFunction extends SqlExpression {
       return 'Array';
     }
     $fields = $this->getFields();
-    if (!empty($fields[0])) {
-      return $fieldSpecs[$fields[0]]['data_type'] ?? NULL;
+    if ($query && !empty($fields[0])) {
+      $sqlField = parent::convert($fields[0]);
+      return $sqlField->getRenderedDataType($query);
     }
     return NULL;
   }

ext/search_kit/tests/phpunit/api/v4/SearchDisplay/SearchRunTest.php
@@ -3143,4 +3143,73 @@ class SearchRunTest extends Api4TestBase implements TransactionalInterface {
     }
   }
 
+  public function testRunWithBooleanFunctionFilters(): void {
+    $lastName = uniqid(__FUNCTION__);
+    $cids = $this->saveTestRecords('Individual', [
+      'records' => [
+        [],
+        [],
+        ['phone_primary.phone' => '1234567890'],
+        ['phone_primary.phone' => '2345678900'],
+      ],
+      'defaults' => ['last_name' => $lastName],
+    ])->column('id');
+
+    $params = [
+      'display' => NULL,
+      'savedSearch' => [
+        'api_entity' => 'Individual',
+        'api_params' => [
+          'version' => 4,
+          'select' => [
+            'id',
+            'last_name',
+            'Contact_Phone_contact_id_01.phone',
+            'ISNOTNULL(Contact_Phone_contact_id_01.id) AS notnull',
+          ],
+          'where' => [],
+          'groupBy' => [],
+          'join' => [
+            [
+              'Phone AS Contact_Phone_contact_id_01',
+              'LEFT',
+              [
+                'id',
+                '=',
+                'Contact_Phone_contact_id_01.contact_id',
+              ],
+              [
+                'Contact_Phone_contact_id_01.is_primary',
+                '=',
+                TRUE,
+              ],
+            ],
+          ],
+          'having' => [],
+        ],
+      ],
+      'sort' => [
+        ['id', 'ASC'],
+      ],
+    ];
+
+    $result = civicrm_api4('SearchDisplay', 'run', $params + [
+      'filters' => ['last_name' => $lastName],
+    ]);
+    $this->assertCount(4, $result);
+    $this->assertEquals($cids, $result->column('key'));
+
+    $result = civicrm_api4('SearchDisplay', 'run', $params + [
+      'filters' => ['last_name' => $lastName, 'notnull' => TRUE],
+    ]);
+    $this->assertCount(2, $result);
+    $this->assertEquals([$cids[2], $cids[3]], $result->column('key'));
+
+    $result = civicrm_api4('SearchDisplay', 'run', $params + [
+      'filters' => ['last_name' => $lastName, 'notnull' => FALSE],
+    ]);
+    $this->assertCount(2, $result);
+    $this->assertEquals([$cids[0], $cids[1]], $result->column('key'));
+  }
+
 }

commit ac09b43d1114ff392bb15c7ea45b06312036e897

Fix filter on relationship tab
Fixes dev/core#6128

This regressed with commit 739d2e31; the problem was that the RelationshipCache entity wasn't declared as a DAOEntity and therefore SavedSearchInspectorTrait::getQuery failed to load a query object for it.

I've fixed the metadata for that entity and a couple others, and also added a fallback in SavedSearchInspectorTrait::applyFilter so it's not so heavily dependent on the query.

Civi/Api4/Generic/Traits/SavedSearchInspectorTrait.php
@@ -314,6 +314,8 @@ trait SavedSearchInspectorTrait {
         $field = $this->getField($fieldName);
         $operators = array_values($field['operators'] ?? []) ?: CoreUtil::getOperators();
         $options = $field['options'] ?? NULL;
+        // Fallback in case getQuery wasn't available
+        $dataType ??= $field['data_type'] ?? NULL;
       }
       elseif (is_a($expr, SqlFunction::class)) {
         $options = $expr->getOptions();

Civi/Api4/RelationshipCache.php
@@ -56,6 +56,8 @@ class RelationshipCache extends Generic\AbstractEntity {
   public static function getInfo() {
     $info = parent::getInfo();
     $info['bridge_title'] = ts('Relationship');
+    // This entity uses DAOGetAction so counts as a DAOEntity
+    $info['type'][0] = 'DAOEntity';
     $info['bridge'] = [
       'near_contact_id' => [
         'to' => 'far_contact_id',

ext/search_kit/Civi/Api4/Event/Subscriber/SKEntitySubscriber.php
@@ -56,7 +56,7 @@ class SKEntitySubscriber extends AutoService implements EventSubscriberInterface
         'title' => $display['label'],
         'title_plural' => $display['label'],
         'description' => $display['settings']['description'] ?? NULL,
-        'type' => ['SavedSearch'],
+        'type' => ['DAOEntity', 'SavedSearch'],
         'table_name' => $display['tableName'],
         'class_args' => [$display['name']],
         'label_field' => NULL,