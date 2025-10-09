03ef3cd019d9c0e56c99cc9f53f2ad0739410ab7
colemanw <coleman@civicrm.org>
2025-10-08 20:44:48 -0400

    Settings - Convert Component settings to Generic, add sections

--- CRM/Admin/Form/Setting/Component.php
+++ CRM/Admin/Form/Setting/Component.php
@@ -18,41 +18,6 @@
 /**
  * This class generates form components for Component.
  */
-class CRM_Admin_Form_Setting_Component extends CRM_Admin_Form_Setting {
-
-  /**
-   * Build the form object.
-   */
-  public function buildQuickForm() {
-    $this->addFormRule(['CRM_Admin_Form_Setting_Component', 'formRule'], $this);
-    parent::buildQuickForm();
-  }
-
-  /**
-   * Global form rule.
-   *
-   * @param array $fields
-   *   The input form values.
-   * @param array $files
-   *   The uploaded files if any.
-   * @param array $options
-   *   Additional user data.
-   *
-   * @return bool|array
-   *   true if no errors, else array of errors
-   */
-  public static function formRule($fields, $files, $options) {
-    $errors = [];
-
-    if (array_key_exists('enable_components', $fields) && is_array($fields['enable_components'])) {
-      if (!empty($fields['enable_components']['CiviPledge']) &&
-        empty($fields['enable_components']['CiviContribute'])
-      ) {
-        $errors['enable_components'] = ts('You need to enable CiviContribute before enabling CiviPledge.');
-      }
-    }
-
-    return $errors;
-  }
+class CRM_Admin_Form_Setting_Component extends CRM_Admin_Form_Generic {
 
 }
