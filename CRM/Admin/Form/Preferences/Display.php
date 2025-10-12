1affb28a49e93d14cc88811f113278bc7a052f3b
colemanw <coleman@civicrm.org>
2025-10-12 19:31:56 -0400

    Settings - Move more CKEditor config to extension, fix button visibility

--- CRM/Admin/Form/Preferences/Display.php
+++ CRM/Admin/Form/Preferences/Display.php
@@ -51,18 +51,6 @@ class CRM_Admin_Form_Preferences_Display extends CRM_Admin_Form_Generic {
     //changes for freezing the invoices/credit notes checkbox if invoicing is uncheck
     $this->assign('invoicing', Civi::settings()->get('invoicing'));
 
-    $this->addElement(
-      'xbutton',
-      'ckeditor_config',
-      CRM_Core_Page::crmIcon('fa-wrench') . ' ' . ts('Configure CKEditor'),
-      [
-        'type' => 'submit',
-        'class' => 'crm-button',
-        'style' => 'display:inline-block;vertical-align:middle;float:none!important;',
-        'value' => 1,
-      ]
-    );
-
     $editOptions = CRM_Core_OptionGroup::values('contact_edit_options', FALSE, FALSE, FALSE, 'AND v.filter = 0');
     $this->assign('editOptions', $editOptions);
 
@@ -96,15 +84,6 @@ class CRM_Admin_Form_Preferences_Display extends CRM_Admin_Form_Generic {
       }
       $opGroupId = CRM_Core_DAO::getFieldValue('CRM_Core_DAO_OptionGroup', 'contact_edit_options', 'id', 'name');
       CRM_Core_BAO_OptionValue::updateOptionWeights($opGroupId, array_flip($preferenceWeights));
-    }
-
-    // If "Configure CKEditor" button was clicked
-    if (!empty($this->_params['ckeditor_config'])) {
-      // Suppress the "Saved" status message and redirect to the CKEditor Config page
-      $session = CRM_Core_Session::singleton();
-      $session->getStatus(TRUE);
-      $url = CRM_Utils_System::url('civicrm/admin/ckeditor', 'reset=1');
-      $session->pushUserContext($url);
     }
   }
 
