3ca5e3fa6a56dd6a8102e4fa89ebdcc365551566
benjamin <ben@civicrm.org>
2025-10-14 14:48:21 +0100

    afFilterSet - render display values

--- ext/afform/core/ang/af/afFilterSets.component.js
+++ ext/afform/core/ang/af/afFilterSets.component.js
@@ -8,14 +8,14 @@
     controller: function($scope, $element, crmApi4, $window, $location) {
       const ts = $scope.ts = CRM.ts('org.civicrm.afform');
 
-      // local model of filter set options
-      this.options = [];
+      // local model of filter set savedSets
+      this.savedSets = [];
 
       this.$onInit = () => {
         this.formName = this.afFieldset.getFormName();
         this.saveDialog.reset();
 
-        this.fetchFilterSets()
+        this.fetchFilterSets();
       };
 
       this.fetchFilterSets = () => crmApi4('AfformFilterSet', 'get', {
@@ -23,12 +23,10 @@
           where: [['afform_name', '=', this.formName]],
           orderBy: {'label': 'ASC'}
         })
-        .then((results) => this.options = results)
-        .catch(() => this.options = [{id: '', label: ts('fetching filter sets failed')}])
+        .then((results) => this.savedSets = results)
+        .catch(() => this.savedSets = [{id: '', label: ts('fetching filter sets failed')}]);
 
-      this.applyFilterSet = (id) => {
-        $window.location.hash = id ? `#?_filterset=${id}` : '';
-      }
+      this.applyFilterSet = (id) => $window.location.hash = id ? `#?_filterset=${id}` : '';
 
       this.getSetFilterSet = (value) => {
         if (value !== undefined) {
@@ -37,15 +35,53 @@
         }
         const filterSet = this.getCurrentFilterSet();
         return (filterSet && filterSet.id) ? `${filterSet.id}` : '';
-      }
+      };
 
       this.getCurrentFilterSet = () => {
         const hashParams = new URLSearchParams($window.location.hash.slice(1));
         const urlValue = parseInt(hashParams.get('_filterset'));
-        return this.options.find((filterSet) => filterSet.id === urlValue);
-      }
+        return this.savedSets.find((filterSet) => filterSet.id === urlValue);
+      };
 
-      this.getCurrentFilters = () => this.afFieldset ? this.afFieldset.getFilterValues() : {};
+      this.getCurrentFilterValues = () => this.afFieldset ? this.afFieldset.getFilterValues() : {};
+
+      this.getFieldsetMeta = () => {
+        const meta = {};
+        const fieldElements = $element[0].closest('[af-fieldset]').querySelectorAll('af-field');
+        fieldElements.forEach((field) => {
+          const name = field.getAttribute('name');
+          const defn = angular.element(field).controller('afField').defn;
+          meta[name] = defn;
+        });
+        return meta;
+      };
+
+      this.renderFilterSetValues = (values) => {
+        const meta = this.getFieldsetMeta();
+
+        const rendered = {};
+
+        Object.keys(values).forEach((key) => {
+          const defn = meta[key];
+          const rawValue = values[key];
+
+          const formatValue = (v) => {
+            if (defn.options && defn.options.length) {
+              const selected = defn.options.find((o) => o.id == v);
+              if (selected) {
+                return selected.label;
+              }
+            }
+            return v;
+          };
+
+          const value = (rawValue && rawValue.map) ? rawValue.map(formatValue).join(', ') : formatValue(rawValue);
+
+          rendered[defn.label] = value;
+        });
+
+        return rendered;
+      };
 
       this.saveDialog = {
         open: () => $element[0].querySelector('dialog.af-filter-sets-new').showModal(),
@@ -54,8 +90,8 @@
           this.saveDialog.label = ts('New filter set');
           this.saveDialog.inProgress = false;
         },
-        canOpen: () => Object.keys(this.getCurrentFilters()).length,
-        canSave: () => !this.saveDialog.inProgress && this.saveDialog.label && Object.keys(this.getCurrentFilters()).length,
+        canOpen: () => Object.keys(this.getCurrentFilterValues()).length,
+        canSave: () => !this.saveDialog.inProgress && this.saveDialog.label && Object.keys(this.getCurrentFilterValues()).length,
         save: () => {
           if (!this.saveDialog.canSave()) {
             return;
@@ -65,7 +101,7 @@
 
           const values = {
             afform_name: this.formName,
-            filters: this.getCurrentFilters(),
+            filters: this.getCurrentFilterValues(),
             label: this.saveDialog.label,
             created_by: CRM.config.cid,
           };
@@ -87,7 +123,6 @@
         }
       };
 
-
       this.manageDialog = {
         open: () => $element[0].querySelector('dialog.af-filter-sets-manage').showModal(),
         close: () => $element[0].querySelector('dialog.af-filter-sets-manage').close(),
@@ -101,10 +136,52 @@
           1: filterSet.created_date,
           2: (filterSet['created_by.display_name'] ? filterSet['created_by.display_name'] : 'UNKNOWN')
         }),
-      }
+      };
 
+      this.updateDialog = {
+        id: null,
+        label: '',
+        open: () => {
+          const current = this.getCurrentFilterSet();
+          this.updateDialog.id = current.id;
+          this.updateDialog.label = current.label;
+          $element[0].querySelector('dialog.af-filter-set-update').showModal();
+        },
+        close: () => $element[0].querySelector('dialog.af-filter-set-update').close(),
+        inProgress: false,
+        canOpen: () => {
+          const saved = this.getCurrentFilterSet();
+          if (!saved) {
+            return false;
+          }
+          const currentValues = this.getCurrentFilterValues();
+          if (JSON.stringify(saved.filters) === JSON.stringify(currentValues)) {
+            return false;
+          }
+          return true;
+        },
+        canUpdate: () => this.updateDialog.id && this.updateDialog.label,
+        update: () => {
+          if (!this.updateDialog.canUpdate()) {
+            return;
+          }
 
+          this.updateDialog.inProgress = true;
 
+          const newValues = this.getCurrentFilterValues();
+
+          crmApi4('AfformFilterSet', 'update', {
+            where: [['id', '=', this.updateDialog.id]],
+            values: {
+              label: this.updateDialog.label,
+              filters: newValues
+            }
+          })
+          .then(() => this.fetchFilterSets())
+          .then(() => this.updateDialog.inProgress = false)
+          .then(() => this.updateDialog.close());
+        }
+      };
     }
   });
 })(angular, CRM.$, CRM._);
