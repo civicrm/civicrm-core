973bfe460b88f8408a5c270ca01e27224bd3894e
eileen <emcnaughton@wikimedia.org>
2025-10-23 13:51:42 +0200

    Fix event import to work

--- CRM/Import/StateMachine.php
+++ CRM/Import/StateMachine.php
@@ -58,7 +58,13 @@ class CRM_Import_StateMachine extends CRM_Core_StateMachine {
         throw new CRM_Core_Exception(ts('Invalid import entity %1', [1 => htmlentities($this->entity)]));
       }
       $entityPath = explode('_', $entityName);
-      $this->classPrefix = $entityPath[0] . '_' . $entityPath[1] . '_Import';
+      if ($this->entity === 'Event') {
+        // If we allow this to be set it finds the participant import...
+        $this->classPrefix = '';
+      }
+      else {
+        $this->classPrefix = $entityPath[0] . '_' . $entityPath[1] . '_Import';
+      }
     }
     else {
       CRM_Core_Error::deprecatedWarning('entity parameter expected, always passed in core & few outside core uses so this will go');
