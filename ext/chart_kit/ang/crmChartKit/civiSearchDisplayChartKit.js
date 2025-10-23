a50e3b5e9c9e8c924e55ba64687215b7dada90cf
benjamin <ben@civicrm.org>
2025-10-24 00:03:40 +0100

    chart kit => web component - reposition

--- ext/chart_kit/ang/crmChartKit/civiSearchDisplayChartKit.js
+++ ext/chart_kit/ang/crmChartKit/civiSearchDisplayChartKit.js
@@ -1,21 +1,5 @@
 (function (angular, $, _, dc, d3, crossfilter) {
-  "use strict";
 
-  angular.module('crmChartKit').component('crmSearchDisplayChartKit', {
-    bindings: {
-      apiEntity: '@',
-      search: '<',
-      display: '<',
-      apiParams: '<',
-      settings: '<',
-      filters: '<',
-      totalCount: '=?'
-    },
-    require: {
-      afFieldset: '?^^afFieldset'
-    },
-    templateUrl: '~/crmChartKit/chartKitCanvas.html',
-    controller: function ($scope, $element, searchDisplayBaseTrait, chartKitChartTypes, chartKitColumn) {
       const ts = $scope.ts = CRM.ts('chart_kit');
 
       // Mix in base display trait
@@ -567,6 +551,4 @@
         img.src = svgData;
       };
 
-    }
-  });
 })(angular, CRM.$, CRM._, CRM.chart_kit.dc, CRM.chart_kit.d3, CRM.chart_kit.crossfilter);
