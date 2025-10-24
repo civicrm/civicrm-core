ee99b399d18f6bdcc33e7074404c01623089b1f7
eileen <emcnaughton@wikimedia.org>
2025-10-24 09:59:47 +0200

    Move variables to where they are used

--- CRM/Contribute/BAO/Contribution.php
+++ CRM/Contribute/BAO/Contribution.php
@@ -2847,17 +2847,6 @@ INNER JOIN civicrm_activity ON civicrm_activity_contact.activity_id = civicrm_ac
 
     $contributionStatus = $financialProcessor->getUpdatedContributionStatus();
 
-    if (!empty($params['membership_id'])) {
-      //so far $params['membership_id'] should only be set coming in from membershipBAO::create so the situation where multiple memberships
-      // are created off one contribution should be handled elsewhere
-      $entityId = $params['membership_id'];
-      $entityTable = 'civicrm_membership';
-    }
-    else {
-      $entityId = $contribution->id;
-      $entityTable = 'civicrm_contribution';
-    }
-
     // Checking $params['is_pay_later'] means we only pick this up if
     // is_pay_later has been passed in - this feels like a mistake but it
     // is an entrenched mistake (the previous code did the same although
@@ -3101,6 +3090,16 @@ INNER JOIN civicrm_activity ON civicrm_activity_contact.activity_id = civicrm_ac
     }
     // record line items and financial items
     if (empty($params['skipLineItem'])) {
+      if (!empty($params['membership_id'])) {
+        //so far $params['membership_id'] should only be set coming in from membershipBAO::create so the situation where multiple memberships
+        // are created off one contribution should be handled elsewhere
+        $entityId = $params['membership_id'];
+        $entityTable = 'civicrm_membership';
+      }
+      else {
+        $entityId = $contribution->id;
+        $entityTable = 'civicrm_contribution';
+      }
       CRM_Price_BAO_LineItem::processPriceSet($entityId, $params['line_item'] ?? NULL, $params['contribution'], $entityTable, $isUpdate);
     }
 
