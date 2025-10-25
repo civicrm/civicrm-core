3dda0fd5438374ca8f74a91589786e1bd4c4fb16
benjamin <ben@civicrm.org>
2025-10-25 16:01:11 +0100

    Riverlea StyleLoader - use AutoService base class rather than trait

--- ext/riverlea/Civi/Riverlea/StyleLoader.php
+++ ext/riverlea/Civi/Riverlea/StyleLoader.php
@@ -2,6 +2,8 @@
 
 namespace Civi\Riverlea;
 
+use Civi\Core\Event\GenericHookEvent;
+use Civi\Core\Service\AutoService;
 use CRM_riverlea_ExtensionUtil as E;
 
 /**
@@ -12,9 +14,7 @@ use CRM_riverlea_ExtensionUtil as E;
  *
  * @service riverlea.style_loader
  */
-class StyleLoader implements \Symfony\Component\EventDispatcher\EventSubscriberInterface, \Civi\Core\Service\AutoServiceInterface {
-
-  use \Civi\Core\Service\AutoServiceTrait;
+class StyleLoader extends AutoService implements \Symfony\Component\EventDispatcher\EventSubscriberInterface {
 
   public const DYNAMIC_FILE = 'river.css';
 
