248eb2a85e53eb7f2698a2662c5b4015000a4783
Tim Otten <totten@civicrm.org>
2025-10-26 07:01:34 -0700

    Extensions - Define <parent> tag

--- CRM/Extension/Info.php
+++ CRM/Extension/Info.php
@@ -61,6 +61,16 @@ class CRM_Extension_Info {
   public $requires = [];
 
   /**
+   * (Optional) The parent of a submodule.
+   *
+   * If the parent is installed, then the submodule becomes eligible for auto-installation.
+   * If the parent is uninstalled, then the submodule must be uninstalled.
+   *
+   * @var string|null
+   */
+  public $parent = NULL;
+
+  /**
    * @var array
    *   List of expected mixins.
    *   Ex: ['civix@2.0.0']
@@ -361,6 +371,15 @@ class CRM_Extension_Info {
       }
       else {
         $this->$attr = $eval(CRM_Utils_XML::xmlObjToArray($val));
+      }
+    }
+
+    if (in_array('mgmt:enable-when-satisfied', $this->tags)) {
+      if ($this->parent && !in_array($this->parent, $this->requires)) {
+        $this->requires[] = $this->parent;
+      }
+      else {
+        \Civi::log()->warning("Extension ($info->key) is tagged \"mgmt:enable-when-satisfied\", but no parent is declared.");
       }
     }
   }
