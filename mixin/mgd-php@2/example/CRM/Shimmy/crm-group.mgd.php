2442175547aa43718bbaa01fbc61fcfa18813e5b
Tim Otten <totten@civicrm.org>
2025-10-27 22:19:45 -0700

    Mixins - Add mgd-php@2. Use tighter search to ignore nested submodules.

---
+++ mixin/mgd-php@2/example/CRM/Shimmy/crm-group.mgd.php
@@ -0,0 +1,11 @@
+<?php
+return [
+  [
+    'name' => 'the_managed_group_crm',
+    'entity' => 'OptionGroup',
+    'params' => [
+      'name' => 'shimmy_group_crm',
+      'title' => 'Shimmy Group (CRM/Shimmy/)',
+    ],
+  ],
+];
