e3ee32d989a6151974e956bbc60359a1c4b65ff6
Seamus Lee <seamus.lee@jmaconsulting.biz>
2025-10-30 08:31:52 +1100

    dev/core#6180 [NFC] Add Unit test to demonstrate type error in getLocales

---
+++ ext/afform/admin/tests/phpunit/CiviAfformAdminAfformAdminMetaTest.php
@@ -0,0 +1,56 @@
+<?php
+
+use CRM_AfformAdmin_ExtensionUtil as E;
+use Civi\Test\CiviEnvBuilder;
+use Civi\Test\HeadlessInterface;
+
+/**
+ * FIXME - Add test description.
+ *
+ * Tips:
+ *  - With HookInterface, you may implement CiviCRM hooks directly in the test class.
+ *    Simply create corresponding functions (e.g. "hook_civicrm_post(...)" or similar).
+ *  - With TransactionalInterface, any data changes made by setUp() or test****() functions will
+ *    rollback automatically -- as long as you don't manipulate schema or truncate tables.
+ *    If this test needs to manipulate schema or truncate tables, then either:
+ *       a. Do all that using setupHeadless() and Civi\Test.
+ *       b. Disable TransactionalInterface, and handle all setup/teardown yourself.
+ *
+ * @group headless
+ */
+class CiviAfformAdminAfformAdminMetaTest extends \PHPUnit\Framework\TestCase implements HeadlessInterface {
+
+  /**
+   * Setup used when HeadlessInterface is implemented.
+   *
+   * Civi\Test has many helpers, like install(), uninstall(), sql(), and sqlFile().
+   *
+   * @link https://github.com/civicrm/org.civicrm.testapalooza/blob/master/civi-test.md
+   *
+   * @return \Civi\Test\CiviEnvBuilder
+   *
+   * @throws \CRM_Extension_Exception_ParseException
+   */
+  public function setUpHeadless(): CiviEnvBuilder {
+    return \Civi\Test::headless()
+      ->installMe(__DIR__)
+      ->apply();
+  }
+
+  public function setUp():void {
+    parent::setUp();
+  }
+
+  public function tearDown():void {
+    parent::tearDown();
+  }
+
+  /**
+   * Verify that getLocales works without php error.
+   */
+  public function testAdminSettings():void {
+    $adminSettings = \Civi\AfformAdmin\AfformAdminMeta::getAdminSettings();
+    $this->assertSame([], $adminSettings['locales']);
+  }
+
+}
