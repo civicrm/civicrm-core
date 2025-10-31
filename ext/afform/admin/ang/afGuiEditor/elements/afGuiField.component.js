civicrm/civicrm-core

commit cf53f387186527ee0b0d40b52b9e53e7009101f7

FormBuilder - Fix labels for CheckBoxes
Editing checkbox labels was messed up for boolean fields.

ext/afform/admin/ang/afGuiEditor/elements/afGuiField.component.js
@@ -385,9 +385,18 @@
         return afGui.getSearchDisplayFields(ctrl.container.getSearchDisplay(), _.noop, [ctrl.getFieldName()]);
       };
 
+      this.showLabel = () => {
+        if (this.node.defn.label === false) {
+          return false;
+        }
+        // Single checkboxes don't get a separate label
+        return !(getSet('input_type') === 'CheckBox' && this.getDefn().data_type === 'Boolean');
+      };
+
       $scope.defaultValueContains = function(val) {
         const defaultVal = getSet('afform_default');
-        return defaultVal === val || (Array.isArray(defaultVal) && defaultVal.includes(val));      };
+        return defaultVal === val || (Array.isArray(defaultVal) && defaultVal.includes(val));
+      };
 
       $scope.toggleDefaultValueItem = function(val) {
         if (defaultValueShouldBeArray()) {

ext/afform/admin/ang/afGuiEditor/elements/afGuiField.html
@@ -2,7 +2,6 @@
 <div ng-if="!editingOptions" class="af-gui-element af-gui-field" >
   <div class="af-gui-bar" title="{{:: getOriginalLabel() }}">
     <div class="form-inline">
-      <span ng-if="$ctrl.node.defn.label === false || ($ctrl.getDefn().input_type === 'CheckBox' && $ctrl.getDefn().data_type === 'Boolean')">{{:: getOriginalLabel() }} </span>
       <div class="btn-group pull-right" af-gui-menu >
         <button type="button" class="btn btn-default btn-xs dropdown-toggle af-gui-add-element-button" data-toggle="dropdown" title="{{:: ts('Configure') }}">
           <span><i class="crm-i fa-gear" role="img" aria-hidden="true"></i></span>
@@ -11,7 +10,7 @@
       </div>
     </div>
   </div>
-  <label ng-style="{visibility: $ctrl.node.defn.label === false || ($ctrl.getDefn().input_type === 'CheckBox' && $ctrl.getDefn().data_type === 'Boolean') ? 'hidden' : 'visible'}" ng-class="{'af-gui-field-required': getProp('required')}" class="af-gui-node-title">
+  <label ng-style="{visibility: $ctrl.showLabel() ? 'visible' : 'hidden'}" ng-class="{'af-gui-field-required': getProp('required')}" class="af-gui-node-title">
     <span crm-ui-editable ng-model="getSet('label')" ng-model-options="{getterSetter: true}" default-value="$ctrl.getDefn().label">{{ getProp('label') }}</span>
   </label>
   <div class="af-gui-field-help" ng-if="propIsset('help_pre')">

ext/afform/admin/ang/afGuiEditor/inputType/CheckBox.html
@@ -6,7 +6,8 @@
 </ul>
 <div class="form-inline" ng-if="$ctrl.getDefn().data_type === 'Boolean'">
   <input type="checkbox" ng-checked="defaultValueContains(true)" ng-click="toggleDefaultValueItem(true) ">
-  <label class="af-gui-node-title" ng-if="$ctrl.node.defn.label != false">{{:: $ctrl.getDefn().label }}
-  <span class="crm-marker" title="{{:: ts('Required') }}" ng-if="getProp('required')">*</span>
+  <label ng-if="$ctrl.node.defn.label != false">
+    <span crm-ui-editable ng-model="getSet('label')" ng-model-options="{getterSetter: true}" default-value="$ctrl.getDefn().label">{{ getProp('label') }}</span>
+    <span class="crm-marker" title="{{:: ts('Required') }}" ng-if="getProp('required')">*</span>
   </label>
 </div>

ext/afform/core/ang/af/fields/CheckBox.html
@@ -4,8 +4,8 @@
     <label for="{{ fieldId + opt.id }}">{{:: opt.label }}</label>
   </li>
 </ul>
-  <input type="checkbox" ng-required="$ctrl.defn.required" ng-if="!$ctrl.isMultiple()" id="{{:: fieldId }}" ng-model="getSetValue" ng-model-options="{getterSetter: true}"  aria-label="{{$ctrl.defn.label}}"  />
-<label class="crm-af-field-label" for="{{:: fieldId }}" ng-if="!$ctrl.isMultiple() && $ctrl.defn.label" >
+<label class="crm-af-field-label" ng-if="!$ctrl.isMultiple()" >
+  <input type="checkbox" ng-required="$ctrl.defn.required" ng-if="!$ctrl.isMultiple()" id="{{:: fieldId }}" ng-model="getSetValue" ng-model-options="{getterSetter: true}" aria-label="{{$ctrl.defn.label}}" />
   {{:: $ctrl.defn.label }}
   <span class="crm-marker" title="{{:: ts('Required') }}" ng-if="$ctrl.defn.required">*</span>
 </label>

commit c7538bf999bfcfe8f2b85d66b8b478012c72888a

FormBuilder - Fix js error
Fixes error caused by cf53f387186527ee0b0d40b52b9e53e7009101f7

ext/afform/admin/ang/afGuiEditor/elements/afGuiField.component.js
@@ -386,7 +386,7 @@
       };
 
       this.showLabel = () => {
-        if (this.node.defn.label === false) {
+        if (this.node.defn && this.node.defn.label === false) {
           return false;
         }
         // Single checkboxes don't get a separate label