ae387b0d4ca2f05790ea95e2eb31bfeb99f30fd9
Jaap Jansma <jaapjansma@users.noreply.github.com>
2025-11-06 17:01:55 +0100

    Update AbstractProcessor.php

--- ext/afform/core/Civi/Api4/Action/Afform/AbstractProcessor.php
+++ ext/afform/core/Civi/Api4/Action/Afform/AbstractProcessor.php
@@ -873,7 +873,7 @@ abstract class AbstractProcessor extends \Civi\Api4\Generic\AbstractAction {
    */
   public function replaceTokens(string $text): string {
     $matches = [];
-    preg_match_all('/[[a-zA-Z0-9_]{1,}\.[0-9]{1,}\..*]/', $text, $matches);
+    preg_match_all('/[[a-zA-Z0-9_]{1,}\.[0-9]{1,}\.[^]]+]/', $text, $matches);
 
     foreach ($matches[0] as $match) {
       // strip [ ] and split on .
