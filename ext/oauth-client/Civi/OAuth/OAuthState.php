2f670c51f11b33d14d696acc4a1ffe5c270d5955
Tim Otten <totten@civicrm.org>
2025-11-12 11:11:50 -0800

    OAuthState - Harden the handling of wildcard-session bindings

--- ext/oauth-client/Civi/OAuth/OAuthState.php
+++ ext/oauth-client/Civi/OAuth/OAuthState.php
@@ -12,6 +12,23 @@ class OAuthState extends AutoService {
   const LEGACY_TTL = 3600;
 
   /**
+   * Session IDs are cookie values, so... "Any US-ASCII character excluding control characters
+   * (ASCII characters 0 up to 31 and ASCII character 127), Whitespace, double quotes, commas,
+   * semicolons, and backslashes."
+   *
+   * @link https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Set-Cookie#attributes
+   */
+  const SESSION_ID_REGEX = '/^([\x21\x23-\x2B\x2D-\x3A\x3C-\x5B\x5D-\x7E]+)$/';
+
+  /**
+   * When beginning OAuth flow from CLI, set the state with `session=>SESSION_WILDCARD`
+   * to allow the pageflow to continue in a browser with an unknown session ID.
+   *
+   * Ideal value is (1) serializable and (2) invalid as cookie-content and (3) recognizable.
+   */
+  const SESSION_WILDCARD = ',;"wildcard';
+
+  /**
    * @var \CRM_Utils_Cache_Interface
    * @inject cache.session
    */
@@ -56,7 +73,7 @@ class OAuthState extends AutoService {
     if (!$state) {
       throw new \Civi\OAuth\OAuthException("OAuth: Received invalid or expired state");
     }
-    if (!($state['session'] === '*' || hash_equals($state['session'], $this->getSessionId()))) {
+    if (!($state['session'] === static::SESSION_WILDCARD || hash_equals($state['session'], $this->getSessionId()))) {
       throw new \Civi\OAuth\OAuthException("OAuth: Received invalid or expired state");
     }
     $ttl = $state['ttl'] ?? self::LEGACY_TTL;
@@ -70,11 +87,19 @@ class OAuthState extends AutoService {
   protected function getSessionId(): string {
     if (PHP_SAPI === 'cli') {
       // CLI doesn't have a session. If we generate OAuthState on CLI, then allow it to be used in any browser session.
-      return '*';
+      return static::SESSION_WILDCARD;
     }
 
     // In general, we want `state`s to only be valid within a particular session.
-    return session_id();
+    $id = session_id();
+    if (!preg_match(static::SESSION_ID_REGEX, $id)) {
+      // We double-check that ID is well-formed (n.b. not WILDCARD). This would
+      // be true in any sensible session-management design. However, we have a mix
+      // of HTTPDs, PHP versions, and PHP frameworks (and a box of magical $_COOKIEs),
+      // so... we do an extra guard against unusual/non-conformant cases.
+      throw new OAuthException("OAuth: Invalid session ID");
+    }
+    return $id;
   }
 
 }
