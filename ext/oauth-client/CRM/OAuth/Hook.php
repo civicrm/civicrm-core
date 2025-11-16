08b30c8dd6673375f25570923afa61d354bbaced
Tim Otten <totten@civicrm.org>
2025-11-16 14:14:24 -0800

    (REF) hook_civicrm_oauthProvider - Add documentation stub

--- ext/oauth-client/CRM/OAuth/Hook.php
+++ ext/oauth-client/CRM/OAuth/Hook.php
@@ -5,6 +5,28 @@ use Civi\Core\Event\GenericHookEvent;
 class CRM_OAuth_Hook {
 
   /**
+   * Generate a list of available OAuth providers.
+   *
+   * @param array $providers
+   *   Alterable list of providers, keyed by symbolic name. Each has these properties:
+   *     - name: string (MUST match the key in the array)
+   *     - class: string, the controller class which generates and sends token requests (default: CiviGenericProvider)
+   *     - options: array, constructor arguments for the driver class. Typical properties are:
+   *         - urlAuthorize: string (e.g. "https://api.example.com/auth")
+   *         - urlAccessToken: string (e.g. "https://api.example.com/token")
+   *         - urlResourceOwnerDetails: string (e.g. "https://api.example.com/owner")
+   *         - scopes: array (e.g. "read_profile", "frobnicate_widgets")
+   *         - responseModes: array (e.g. "query", "web_message")
+   *     - tags: string[], list of free-tags describing the purpose/behavior of this provider
+   */
+  public static function oauthProviders(array &$providers): void {
+    $event = GenericHookEvent::create([
+      'providers' => &$providers,
+    ]);
+    \Civi::dispatcher()->dispatch('hook_civicrm_oauthProviders', $event);
+  }
+
+  /**
    * Fires a hook whenever we receive an updated OAuth token.
    *
    * @param string $flow
--- ext/oauth-client/Civi/Api4/Action/OAuthProvider/GetProviders.php
+++ ext/oauth-client/Civi/Api4/Action/OAuthProvider/GetProviders.php
@@ -4,7 +4,6 @@ namespace Civi\Api4\Action\OAuthProvider;
 
 use Civi\Api4\Generic\BasicGetAction;
 use Civi\Api4\OAuthProvider;
-use Civi\Core\Event\GenericHookEvent;
 use Civi\OAuth\CiviGenericProvider;
 
 class GetProviders extends BasicGetAction {
@@ -13,10 +12,7 @@ class GetProviders extends BasicGetAction {
     $cache = \Civi::cache('long');
     if (!$cache->has('OAuthProvider_list')) {
       $providers = [];
-      $event = GenericHookEvent::create([
-        'providers' => &$providers,
-      ]);
-      \Civi::dispatcher()->dispatch('hook_civicrm_oauthProviders', $event);
+      \CRM_OAuth_Hook::oauthProviders($providers);
 
       foreach ($providers as $name => &$provider) {
         if ($provider['name'] !== $name) {
