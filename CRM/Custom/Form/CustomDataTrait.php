civicrm/civicrm-core

commit 6a5544ab5f1b1684ca72594867d1f3e82016e3c0

CustomField - Refactor quickform addCustomDataFieldsToForm to fetch all fields consistently
Before: Fetches custom fields in 2 different ways and then has to do extra filtering

After: Fetches custom fields in 1 way, no extra filtering needed

TODO: Prior to this refactor, the function only loaded multivalued groups if the entity was Contact.
Keeping that quirk for now because I'm not sure all the ramifications of removing it,
but non-contact forms should be checked to see if it's safe to just remove that condition.

CRM/Custom/Form/CustomDataTrait.php
@@ -50,59 +50,36 @@ trait CRM_Custom_Form_CustomDataTrait {
    * @throws \CRM_Core_Exception
    */
   protected function addCustomDataFieldsToForm(string $entity, array $filters = []): void {
-    $fields = (array) civicrm_api4($entity, 'getFields', [
-      'action' => 'create',
-      'values' => $filters,
-      'where' => [
-        ['type', '=', 'Custom'],
-        ['readonly', '=', FALSE],
-      ],
-      'checkPermissions' => TRUE,
-    ])->indexBy('custom_field_id');
-    $fieldFilters = ['style' => 'Inline'];
+    // Reuse the same spec-gatherer from Api4.getFields
+    $spec = new \Civi\Api4\Service\Spec\RequestSpec($entity, 'create', $filters);
+    $fieldFilters = Civi::service('spec_gatherer')->getCustomGroupFilters($spec);
+    if ($fieldFilters === NULL) {
+      return;
+    }
+    // Api4 normally filters out multivalued groups but forms include them
+    // TODO: For now only support multivalued groups for Contact forms.
+    // FIXME: This condition should be removed after verifying it does work for other entity forms (`unset` should be called unconditionally).
     if ($entity === 'Contact') {
-      // Ideally this would not be contact specific but the function being
-      // called here does not handle the filters as received.
-      $fieldFilters += [
-        'extends' => [$entity, $filters['contact_type']],
-        'is_multiple' => TRUE,
-      ];
-      if (!empty($filters['contact_sub_type'])) {
-        $fieldFilters['extends_entity_column_value'] = [NULL, $filters['contact_sub_type']];
-      }
+      unset($fieldFilters['is_multiple']);
+    }
+    // Only inline groups belong on the form: dev/core#5613
+    $fieldFilters['style'] = 'Inline';
 
-      $multipleCustomGroups = CRM_Core_BAO_CustomGroup::getAll($fieldFilters);
-      foreach ($multipleCustomGroups as $multipleCustomGroup) {
-        foreach ($multipleCustomGroup['fields'] as $groupField) {
-          $groupField['custom_group_id.is_multiple'] = TRUE;
-          $groupField['table_name'] = $multipleCustomGroup['table_name'];
-          $groupField['custom_field_id'] = $groupField['id'];
-          $groupField['required'] = $groupField['is_required'];
-          $groupField['input_type'] = $groupField['html_type'];
-          $fields[$groupField['id']] = $groupField;
-        }
+    $customGroups = CRM_Core_BAO_CustomGroup::getAll($fieldFilters, CRM_Core_Permission::EDIT);
+    $fields = [];
+    foreach ($customGroups as $customGroup) {
+      foreach ($customGroup['fields'] as $groupField) {
+        $groupField['custom_group_id.is_multiple'] = $customGroup['is_multiple'];
+        $groupField['table_name'] = $customGroup['table_name'];
+        $groupField['custom_field_id'] = $groupField['id'];
+        $groupField['required'] = $groupField['is_required'];
+        $groupField['input_type'] = $groupField['html_type'];
+        $fields[$groupField['id']] = $groupField;
       }
     }
 
     $formValues = [];
     foreach ($fields as $field) {
-      // Filter for fields in "Inline" style custom groups only, as others are not added to forms via Ajax. Not removing
-      // them here would register them in the QuickForm but not have POST values for them, which might result in data
-      // loss (most notably radio fields, for which no POST value will be interpreted as a reset).
-      // See https://lab.civicrm.org/dev/core/-/issues/5613
-      if (isset($field['custom_group'])) {
-        if (!isset(Civi::$statics[__CLASS__]['customGroups'][$field['custom_group']])) {
-          Civi::$statics[__CLASS__]['customGroups'][$field['custom_group']] = \Civi\Api4\CustomGroup::get(FALSE)
-            ->addSelect('style')
-            ->addWhere('name', '=', $field['custom_group'])
-            ->execute()
-            ->single();
-        }
-        if ('Inline' !== Civi::$statics[__CLASS__]['customGroups'][$field['custom_group']]['style']) {
-          continue;
-        }
-      }
-
       // Here we add the custom fields to the form
       // based on whether they have been 'POSTed'
       foreach ($this->getInstancesOfField($field['custom_field_id']) as $elementName) {

Civi/Api4/Service/Spec/SpecGatherer.php
@@ -208,23 +208,16 @@ class SpecGatherer extends AutoService implements EventSubscriberInterface {
     }
   }
 
-  /**
-   * Get custom fields that extend this entity
-   *
-   * @param string $entityName
-   * @param \Civi\Api4\Service\Spec\RequestSpec $spec
-   * @throws \CRM_Core_Exception
-   * @see \CRM_Core_SelectValues::customGroupExtends
-   */
-  private function addCustomFields(string $entityName, RequestSpec $spec) {
+  public function getCustomGroupFilters(RequestSpec $spec): ?array {
+    $entityName = $spec->getEntity();
     // If contact type is given, treat it as the api entity
     if ($entityName === 'Contact' && $spec->getValue('contact_type')) {
       $entityName = $spec->getValue('contact_type');
     }
 
-    $customInfo = \Civi\Api4\Utils\CoreUtil::getCustomGroupExtends($entityName);
+    $customInfo = CoreUtil::getCustomGroupExtends($entityName);
     if (!$customInfo) {
-      return;
+      return NULL;
     }
     $grouping = $customInfo['grouping'];
     if (CoreUtil::isContact($entityName)) {
@@ -257,6 +250,22 @@ class SpecGatherer extends AutoService implements EventSubscriberInterface {
         }
         $filters['id'] = $ids;
       }
+    }
+    return $filters;
+  }
+
+  /**
+   * Get custom fields that extend this entity
+   *
+   * @param string $entityName
+   * @param \Civi\Api4\Service\Spec\RequestSpec $spec
+   * @throws \CRM_Core_Exception
+   * @see \CRM_Core_SelectValues::customGroupExtends
+   */
+  private function addCustomFields(string $entityName, RequestSpec $spec) {
+    $filters = $this->getCustomGroupFilters($spec);
+    if ($filters === NULL) {
+      return;
     }
     $entity = \Civi::entity(CoreUtil::isContact($entityName) ? 'Contact' : $entityName);
     $customFields = $entity->getCustomFields($filters);

commit d395a507171e341139393abf59f57009bae03b65

dev/core#6124 - Fix regression with required custom file fields
This regressed with 6a5544ab for an odd reason:

- Before that commit, fields were fetched with Api4.getFields which always returns 'required => false' for every custom field (the api doesn't ever consider custom fields required).
- That commit switched to a different getter which actually returns correct values for 'required'
- But for some reason, quickforms weren't expecting the value to be correct!

This hotfix patch restores the "incorrect" 'required' value which gets QuickForms working again as they were. For reasons I haven't dug into, the forms still manage to enforce required custom fields for every field type *except* files, so they clearly have their own extra/redundant lookup for the 'required' custom field property.

CRM/Custom/Form/CustomDataTrait.php
@@ -73,7 +73,8 @@ trait CRM_Custom_Form_CustomDataTrait {
         $groupField['custom_group_id.is_multiple'] = $customGroup['is_multiple'];
         $groupField['table_name'] = $customGroup['table_name'];
         $groupField['custom_field_id'] = $groupField['id'];
-        $groupField['required'] = $groupField['is_required'];
+        // dev/core#6124 QuickForms set 'required' based on other criteria
+        $groupField['required'] = FALSE;
         $groupField['input_type'] = $groupField['html_type'];
         $fields[$groupField['id']] = $groupField;
       }