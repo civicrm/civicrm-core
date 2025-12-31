7c9ea93bf9f737b78a0da96605b3e94c06878a9d
colemanw <coleman@civicrm.org>
2025-12-30 22:16:03 -0500

    SearchKit - Add reusable fields trait for tasks

--- ext/search_kit/ang/crmSearchTasks/crmSearchTaskUpdate.ctrl.js
+++ ext/search_kit/ang/crmSearchTasks/crmSearchTaskUpdate.ctrl.js
@@ -1,73 +1,14 @@
 (function(angular, $, _) {
   "use strict";
 
-  angular.module('crmSearchTasks').controller('crmSearchTaskUpdate', function ($scope, $timeout, crmApi4, searchTaskBaseTrait) {
-    const ts = $scope.ts = CRM.ts('org.civicrm.search_kit'),
-      // Combine this controller with model properties (ids, entity, entityInfo) and searchTaskBaseTrait
-      ctrl = angular.extend(this, $scope.model, searchTaskBaseTrait);
+  angular.module('crmSearchTasks').controller('crmSearchTaskUpdate', function ($scope, $timeout, crmApi4, searchTaskBaseTrait, searchTaskFieldsTrait) {
+    const ts = $scope.ts = CRM.ts('org.civicrm.search_kit');
+    // Combine this controller with model properties (ids, entity, entityInfo) and base traits
+    const ctrl = angular.extend(this, $scope.model, searchTaskBaseTrait, searchTaskFieldsTrait);
 
     this.entityTitle = this.getEntityTitle();
-    this.values = [];
-    this.add = null;
-    this.fields = null;
 
-    crmApi4({
-      getFields: [this.entity, 'getFields', {
-        action: 'update',
-        select: ['name', 'label', 'description', 'input_type', 'data_type', 'serialize', 'options', 'fk_entity', 'nullable'],
-        loadOptions: ['id', 'name', 'label', 'description', 'color', 'icon'],
-        where: [['deprecated', '=', false], ["readonly", "=", false]],
-      }],
-    }).then(function(results) {
-        ctrl.fields = results.getFields;
-      });
-
-    this.updateField = function(index) {
-      // Debounce the onchange event using timeout
-      $timeout(function() {
-        if (!ctrl.values[index][0]) {
-          ctrl.values.splice(index, 1);
-        }
-      });
-    };
-
-    this.addField = function() {
-      // Debounce the onchange event using timeout
-      $timeout(function() {
-        if (ctrl.add) {
-          const field = ctrl.getField(ctrl.add);
-          let value = '';
-          if (field.serialize || field.data_type === 'Array') {
-            value = [];
-          } else if (field.data_type === 'Boolean') {
-            value = true;
-          } else if (field.options && field.options.length) {
-            value = field.options[0].id;
-          }
-          ctrl.values.push([ctrl.add, value]);
-        }
-        ctrl.add = null;
-      });
-    };
-
-    this.getField = function(fieldName) {
-      return _.where(ctrl.fields, {name: fieldName})[0];
-    };
-
-    function fieldInUse(fieldName) {
-      return _.includes(_.collect(ctrl.values, 0), fieldName);
-    }
-
-    this.availableFields = function() {
-      const results = _.transform(ctrl.fields, function(result, item) {
-        const formatted = {id: item.name, text: item.label, description: item.description};
-        if (fieldInUse(item.name)) {
-          formatted.disabled = true;
-        }
-        result.push(formatted);
-      }, []);
-      return {results: results};
-    };
+    this.loadFieldsAndValues(this.task, this.entity);
 
     this.save = function() {
       ctrl.start({
