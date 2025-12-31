ba8c9bc953902ba394aa88ce93c8bdf11ba34ebf
colemanw <coleman@civicrm.org>
2025-12-31 17:18:18 -0500

    SearchKit - Add "Add Grant" task

---
+++ ext/civigrant/ang/civiGrantTasks.ang.php
@@ -0,0 +1,8 @@
+<?php
+// This file declares civiGrantTasks Angular module.
+
+return [
+  'js' => ['ang/civiGrantTasks.js', 'ang/civiGrantTasks/*.js'],
+  'partials' => ['ang/civiGrantTasks'],
+  'requires' => [],
+];
