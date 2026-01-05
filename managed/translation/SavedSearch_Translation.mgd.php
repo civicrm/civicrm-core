civicrm/civicrm-core

commit 2ed0c4293905e4eec4f350555a2d3ecec7cc7b6e

Adding additional menu options

managed/translation/SavedSearch_Translation.mgd.php
@@ -120,14 +120,24 @@ foreach ($locales as $index => $langCode) {
                     'IS EMPTY',
                   ],
                 ],
+                [
+                  'icon': 'fa-pencil',
+                  'side': 'left',
+                  'if': []
+                ],
               ],
             ],
           ],
-          'actions' => FALSE,
+          'actions' => [
+            'delete',
+            'download',
+          ],
           'classes' => [
             'table',
             'table-striped',
+            'crm-sticky-header',
           ],
+          'actions_display_mode': 'menu',
         ],
       ],
       'match' => [

commit 9d9b5b0637852a2cc8c6c962a1c1c40931ee233a

Fix syntax error in mgd file
Fixes error introduced by 2ed0c4293905e4eec4f350555a2d3ecec7cc7b6e

managed/translation/SavedSearch_Translation.mgd.php
@@ -121,9 +121,9 @@ foreach ($locales as $index => $langCode) {
                   ],
                 ],
                 [
-                  'icon': 'fa-pencil',
-                  'side': 'left',
-                  'if': []
+                  'icon' => 'fa-pencil',
+                  'side' => 'left',
+                  'if' => [],
                 ],
               ],
             ],
@@ -137,7 +137,7 @@ foreach ($locales as $index => $langCode) {
             'table-striped',
             'crm-sticky-header',
           ],
-          'actions_display_mode': 'menu',
+          'actions_display_mode' => 'menu',
         ],
       ],
       'match' => [