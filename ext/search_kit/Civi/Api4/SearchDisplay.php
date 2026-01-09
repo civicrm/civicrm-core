29f74fde500c34f6effe532b46118bf47432baa6
colemanw <coleman@civicrm.org>
2026-01-09 10:04:13 -0500

    SearchKit - Add GetMarkup api action
    Sometimes you want to embed a search display into a page without the overhead of also putting it into an Afform.
    
    This generates the markup for you to do so.

--- ext/search_kit/Civi/Api4/SearchDisplay.php
+++ ext/search_kit/Civi/Api4/SearchDisplay.php
@@ -80,6 +80,15 @@ class SearchDisplay extends Generic\DAOEntity {
       ->setCheckPermissions($checkPermissions);
   }
 
+  /**
+   * @param bool $checkPermissions
+   * @return Action\SearchDisplay\GetMarkup
+   */
+  public static function getMarkup($checkPermissions = TRUE) {
+    return (new Action\SearchDisplay\GetMarkup(__CLASS__, __FUNCTION__))
+      ->setCheckPermissions($checkPermissions);
+  }
+
   public static function permissions() {
     $permissions = parent::permissions();
     $permissions['default'] = ['manage own search_kit'];
