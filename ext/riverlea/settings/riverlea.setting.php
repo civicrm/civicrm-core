26e69d31ba1ed33427cc3fb40c1ce6fff2b32dca
benjamin <ben@civicrm.org>
2026-01-29 21:01:40 +0000

    Riverlea - rename Riverlea Settings => Theme Settings, add stream list

--- ext/riverlea/settings/riverlea.setting.php
+++ ext/riverlea/settings/riverlea.setting.php
@@ -20,9 +20,7 @@ return [
       'dark' => E::ts('Always use dark mode'),
     ],
     'settings_pages' => [
-      'riverlea' => ['weight' => 100],
-      // show alongside backend theme selector on Display settings page
-      'display' => ['section' => 'theme', 'weight' => 20],
+      'theme' => ['weight' => 110],
     ],
   ],
   'riverlea_dark_mode_frontend' => [
@@ -42,35 +40,7 @@ return [
       'dark' => E::ts('Always use dark mode'),
     ],
     'settings_pages' => [
-      'riverlea' => ['weight' => 110],
-      // show alongside frontend theme selector on Display settings page
-      'display' => ['section' => 'theme', 'weight' => 40],
-    ],
-  ],
-  'riverlea_font_size' => [
-    'name' => 'riverlea_font_size',
-    'group' => 'riverlea',
-    'default' => '1',
-    'type' => 'String',
-    'html_type' => 'select',
-    'add' => 1.0,
-    'options' => [
-      '0.75' => E::ts('Smallest'),
-      '0.875' => E::ts('Small'),
-      '1' => E::ts('Default'),
-      '1.125' => E::ts('Big'),
-      '1.5' => E::ts('Bigger'),
-    ],
-    'on_change' => [
-      '\\Civi\\Riverlea\\StyleLoader::onChangeFontsize',
-    ],
-    'validate_callback' => '\\Civi\\Riverlea\\StyleLoader::validateFontsize',
-    'title' => E::ts('Font size'),
-    'is_domain' => 1,
-    'is_contact' => 0,
-    'help_text' => E::ts('For systems where 1rem = 16px (which is the default in RiverLea and all browsers) these sizes represent: Smallest 12px, Small 14px, Default 16px, Big 18px, Bigger 24px.'),
-    'settings_pages' => [
-      'riverlea' => ['weight' => 500],
+      'theme' => ['weight' => 210],
     ],
   ],
 ];
