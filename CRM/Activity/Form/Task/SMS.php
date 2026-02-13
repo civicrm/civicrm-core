bd4383fcc6fff2e5d279762306082c9ce1803ee7
Monish Deb <monish.deb@jmaconsulting.biz>
2026-02-13 22:16:50 +0530

    [PHP8.5] Fix CRM_Contact_Form_Task_SMSTrait::isInvalidRecipient to use apiv4

--- CRM/Activity/Form/Task/SMS.php
+++ CRM/Activity/Form/Task/SMS.php
@@ -134,10 +134,13 @@ class CRM_Activity_Form_Task_SMS extends CRM_Activity_Form_Task {
 
   protected function isInvalidRecipient($contactID): bool {
     //to check for "if the contact id belongs to a specified activity type"
-    // @todo use the api instead - function is deprecated.
-    $actDetails = CRM_Activity_BAO_Activity::getContactActivity($contactID);
-    return $this->getActivityName() !==
-      CRM_Utils_Array::retrieveValueRecursive($actDetails, 'subject');
+    return (\Civi\Api4\ActivityContact::get(FALSE)
+      ->selectRowCount()
+      ->addJoin('Activity AS activity', 'INNER', NULL, ['activity_id', '=', 'activity.id'])
+      ->addWhere('contact_id', '=', $contactID)
+      ->addWhere('activity.subject', '=', $this->getActivityName())
+      ->execute()
+      ->count() === 0);
   }
 
 }
