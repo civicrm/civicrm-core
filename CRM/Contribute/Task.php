c749e38d64af7c6c2511a496b2dd498eccdcf6cd
colemanw <coleman@civicrm.org>
2026-02-19 21:34:10 -0500

    Tasks - Prevent cross-contamination from shared variable
    Isolate task definitions in each class to prevent them clobbering each other and add unit test for task consistency across classes.

--- CRM/Contribute/Task.php
+++ CRM/Contribute/Task.php
@@ -37,6 +37,13 @@ class CRM_Contribute_Task extends CRM_Core_Task {
   public static $objectType = 'contribution';
 
   /**
+   * Tasks for this class – overridden from parent to avoid cross-contamination with sibling classes.
+   *
+   * @var array
+   */
+  public static $_tasks = [];
+
+  /**
    * These tasks are the core set of tasks that the user can perform
    * on a contact / group of contacts
    *
