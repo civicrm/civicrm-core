d61c3229936cf31044b0b31f290c172c6a35c38b
benjamin <ben@civicrm.org>
2026-03-09 13:08:07 +0000

    Afform Contributions - improve defaults when creating form

--- ext/civi_contribute/afformEntities/Contribution.php
+++ ext/civi_contribute/afformEntities/Contribution.php
@@ -1,5 +1,7 @@
 <?php
 
+use CRM_Contribute_ExtensionUtil as E;
+
 if (!\Civi::settings()->get('contribute_enable_afform_contributions')) {
   return [];
 }
@@ -10,9 +12,20 @@ return [
     data: {
       contact_id: 'user_contact_id',
       financial_type_id: 1
+    },
+    actions: {
+      create: true,
+      update: false,
     }
   }",
   'boilerplate' => [
+    [
+      '#tag' => 'af-field',
+      'name' => 'default_contribution_amount.contribution_amount',
+      'defn' => [
+        'label' => E::ts('Contribution Amount'),
+      ],
+    ],
     [
       '#tag' => 'af-field',
       'name' => 'source',
