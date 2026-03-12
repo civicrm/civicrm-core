120fb42602802596806204d334ff06f20e7c28ab
Matthew Wire (MJW) <mjw@mjwconsult.co.uk>
2026-03-12 15:21:31 +0000

    Convert LineItem to writeRecord, create FinancialItems automatically and support update for API4 LineItem

---
+++ ext/civi_contribute/Civi/Api4/Service/Spec/Provider/LineItemCreateSpecProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC. All rights reserved.                        |
+ |                                                                    |
+ | This work is published under the GNU AGPLv3 license with some      |
+ | permitted exceptions and without any warranty. For full license    |
+ | and copyright information, see https://civicrm.org/licensing       |
+ +--------------------------------------------------------------------+
+ */
+
+namespace Civi\Api4\Service\Spec\Provider;
+
+use Civi\Api4\Service\Spec\RequestSpec;
+use Civi\Core\Service\AutoService;
+
+/**
+ * @service
+ * @internal
+ */
+class LineItemCreateSpecProvider extends AutoService implements Generic\SpecProviderInterface {
+
+  /**
+   * @param \Civi\Api4\Service\Spec\RequestSpec $spec
+   */
+  public function modifySpec(RequestSpec $spec) {
+    // Not required if qty + unit_price are set
+    $spec->getFieldByName('line_total')->setRequired(FALSE);
+    // contribution_id is deliberately not required: core still creates line items
+    // without one, and a line item outlives the contribution if that is deleted.
+  }
+
+  /**
+   * @param string $entity
+   * @param string $action
+   *
+   * @return bool
+   */
+  public function applies($entity, $action) {
+    return $entity === 'LineItem' && $action === 'create';
+  }
+
+}
