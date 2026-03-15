3994af1003e16903af90b0f071010ac771892dd5
eileen <emcnaughton@wikimedia.org>
2026-03-16 12:52:01 +1300

    PHP8.5 deprecation fix

--- CRM/Contact/Form/Search.php
+++ CRM/Contact/Form/Search.php
@@ -295,7 +295,7 @@ class CRM_Contact_Form_Search extends CRM_Core_Form_Search {
 
     self::setModeValues();
     // Note $mode might === FALSE because array_search above failed, e.g. for searchPane='location'
-    if (empty(self::$_modeValues[$mode])) {
+    if (!$mode || empty(self::$_modeValues[$mode])) {
       $mode = CRM_Contact_BAO_Query::MODE_CONTACTS;
     }
 
