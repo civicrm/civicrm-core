772e1bb986772c670ad3811f52ff06b9ee1189af
Seamus Lee <seamus.lee@jmaconsulting.biz>
2026-03-16 12:02:17 +1100

    security/core#164 Escape field titles when listing fields in APIv3 Explorer dropdowns

--- templates/CRM/Admin/Page/APIExplorer.js
+++ templates/CRM/Admin/Page/APIExplorer.js
@@ -155,13 +155,13 @@
       data: selectFields,
       allowClear: false,
       formatSelection: function(field) {
-        return field.text +
+        return _.escape(field.text) +
           (field.required ? ' <span class="crm-marker">*</span>' : '');
       },
       formatResult: function(field) {
-        return field.text +
+        return _.escape(field.text) +
           (field.required ? ' <span class="crm-marker">*</span>' : '') +
-          '<div class="api-field-desc">' + field.description + '</div>';
+          '<div class="api-field-desc">' + _.escape(field.description) + '</div>';
       }
     }).change();
   }
@@ -201,9 +201,9 @@
     $('.api-chain-entity', $row).crmSelect2({
       formatSelection: function(item) {
         return '<i class="crm-i fa-link" role="img" aria-hidden="true"></i> API ' +
-          ($(item.element).hasClass('strikethrough') ? '<span class="strikethrough">' + item.text + '</span>' : item.text);
+          ($(item.element).hasClass('strikethrough') ? '<span class="strikethrough">' + _.escape(item.text) + '</span>' : _.escape(item.text));
       },
-      placeholder: '<i class="crm-i fa-link" role="img" aria-hidden="true"></i> ' + ts('Entity'),
+      placeholder: '<i class="crm-i fa-link" role="img" aria-hidden="true"></i> ' + _.escape(ts('Entity')),
       allowClear: false,
       escapeMarkup: function(m) {return m;}
     })
@@ -336,7 +336,7 @@
         multiple: true,
         placeholder: ts('Leave blank for default'),
         formatResult: function(field) {
-          return field.text + '<div class="api-field-desc">' + field.description + '</div>';
+          return _.escape(field.text) + '<div class="api-field-desc">' + _.escape(field.description) + '</div>';
         }
       };
     if (action == 'getstat') {
@@ -371,7 +371,7 @@
    * @returns {string}
    */
   function renderAction(option) {
-    return isActionDeprecated(option.id) ? '<span class="strikethrough">' + option.text + '</span>' : option.text;
+    return isActionDeprecated(option.id) ? '<span class="strikethrough">' + _.escape(option.text) + '</span>' : _.escape(option.text);
   }
 
   /**
@@ -946,7 +946,7 @@
     $('#api-entity, #doc-entity').crmSelect2({
       // Add strikethough class to selection to indicate deprecated apis
       formatSelection: function(option) {
-        return $(option.element).hasClass('strikethrough') ? '<span class="strikethrough">' + option.text + '</span>' : option.text;
+        return $(option.element).hasClass('strikethrough') ? '<span class="strikethrough">' + _.escape(option.text) + '</span>' : _.escape(option.text);
       }
     });
     $('form#api-explorer')
