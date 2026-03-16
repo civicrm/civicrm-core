f20c1cbab2659eff4b9d8b633233d7f935ee2c81
colemanw <coleman@civicrm.org>
2026-03-16 12:26:34 -0400

    Upgrader - Remove unused upgrade functions, fix tests
    Fix issues related to MINIMUM_UPGRADABLE_VERSION bump.

--- CRM/Upgrade/Incremental/Base.php
+++ CRM/Upgrade/Incremental/Base.php
@@ -10,7 +10,6 @@
  */
 
 use Civi\Core\Exception\DBQueryException;
-use Civi\Core\SettingsBag;
 
 /**
  * Base class for incremental upgrades
@@ -27,7 +26,7 @@ class CRM_Upgrade_Incremental_Base {
    * Get the major and minor version for this class (based on English-style class name).
    *
    * @return string
-   *   Ex: '5.34' or '4.7'
+   *   Ex: '5.79' or '6.28'
    */
   public function getMajorMinor() {
     if (!$this->majorMinor) {
@@ -626,48 +625,6 @@ class CRM_Upgrade_Incremental_Base {
       ]);
     }
 
-    return TRUE;
-  }
-
-  /**
-   * Re-save any valid values from contribute settings into the normal setting
-   * format.
-   *
-   * We render the array of contribution_invoice_settings and any that have
-   * metadata defined we add to the correct key. This is safe to run even if no
-   * settings are to be converted, per the test in
-   * testConvertUpgradeContributeSettings.
-   *
-   * @param $ctx
-   *
-   * @return bool
-   */
-  public static function updateContributeSettings($ctx) {
-    // Use a direct query as api now does some handling on this.
-    $settings = CRM_Core_DAO::executeQuery("SELECT value, domain_id FROM civicrm_setting WHERE name = 'contribution_invoice_settings'");
-
-    while ($settings->fetch()) {
-      $contributionSettings = (array) CRM_Utils_String::unserialize($settings->value);
-      foreach (array_merge(SettingsBag::getContributionInvoiceSettingKeys(), ['deferred_revenue_enabled' => 'deferred_revenue_enabled']) as $possibleKeyName => $settingName) {
-        if (!empty($contributionSettings[$possibleKeyName]) && empty(Civi::settings($settings->domain_id)->getExplicit($settingName))) {
-          Civi::settings($settings->domain_id)->set($settingName, $contributionSettings[$possibleKeyName]);
-        }
-      }
-    }
-    return TRUE;
-  }
-
-  /**
-   * Do any relevant smart group updates.
-   *
-   * @param CRM_Queue_TaskContext $ctx
-   * @param array $actions
-   *
-   * @return bool
-   */
-  public static function updateSmartGroups($ctx, $actions) {
-    $groupUpdateObject = new CRM_Upgrade_Incremental_SmartGroups();
-    $groupUpdateObject->updateGroups($actions);
     return TRUE;
   }
 
