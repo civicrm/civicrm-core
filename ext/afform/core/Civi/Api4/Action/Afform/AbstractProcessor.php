aade183bc32a32d85612fce2cd6a03a06b800ff2
colemanw <coleman@civicrm.org>
2026-03-17 14:03:46 -0400

    Afform - Fix token matching

--- ext/afform/core/Civi/Api4/Action/Afform/AbstractProcessor.php
+++ ext/afform/core/Civi/Api4/Action/Afform/AbstractProcessor.php
@@ -878,7 +878,7 @@ abstract class AbstractProcessor extends \Civi\Api4\Generic\AbstractAction {
    */
   public function replaceTokens(string $text): string {
     $matches = [];
-    preg_match_all('/[[a-zA-Z0-9_]{1,}\.[0-9]{1,}\.[^]]+]/', $text, $matches);
+    preg_match_all('/\[[a-zA-Z0-9_]+\.[0-9]+\.[^]]+]/', $text, $matches);
 
     foreach ($matches[0] as $match) {
       // strip [ ] and split on .
