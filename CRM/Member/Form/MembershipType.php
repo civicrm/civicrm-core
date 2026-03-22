civicrm/civicrm-core

commit 0ceba2e945dbcc261f68694bdc4647637c6a7c0a

Use frontend_title for MembershipType on Contribution Pages, use title for MembershipType in backend

CRM/Member/Form/MembershipType.php
@@ -434,8 +434,9 @@ class CRM_Member_Form_MembershipType extends CRM_Member_Form_MembershipConfig {
         $params['id'] = $this->_id;
       }
 
-      $membershipTypeResult = civicrm_api3('MembershipType', 'create', $params);
-      $membershipTypeName = $membershipTypeResult['values'][$membershipTypeResult['id']]['name'];
+      $params['domain_id'] = CRM_Core_Config::domainID();
+      $membershipTypeResult = CRM_Member_BAO_MembershipType::writeRecord($params);
+      $membershipTypeName = $membershipTypeResult->name;
 
       CRM_Core_Session::setStatus(ts("The membership type '%1' has been saved.",
         [1 => $membershipTypeName]

CRM/Member/BAO/MembershipType.php
@@ -238,6 +238,7 @@ class CRM_Member_BAO_MembershipType extends CRM_Member_DAO_MembershipType implem
    * @return array
    */
   public static function getMembershipTypes($public = TRUE) {
+    CRM_Core_Error::deprecatedWarning('Use API4 MembershipType::Get instead');
     $membershipTypes = [];
     $membershipType = new CRM_Member_DAO_MembershipType();
     $membershipType->is_active = 1;

CRM/Core/BAO/CustomGroup.php
@@ -1821,7 +1821,11 @@ class CRM_Core_BAO_CustomGroup extends CRM_Core_DAO_CustomGroup implements \Civi
 
     $eventType = CRM_Core_OptionGroup::values('event_type');
     $campaignTypes = CRM_Campaign_PseudoConstant::campaignType();
-    $membershipType = CRM_Member_BAO_MembershipType::getMembershipTypes(FALSE);
+    $membershipType = \Civi\Api4\MembershipType::get(FALSE)
+      ->addWhere('is_active', '=', TRUE)
+      ->addOrderBy('weight', 'ASC')
+      ->execute()
+      ->column('title', 'id');
     $participantRole = CRM_Core_OptionGroup::values('participant_role');
 
     asort($activityType);

CRM/Custom/Page/Group.php
@@ -144,7 +144,11 @@ class CRM_Custom_Page_Group extends CRM_Core_Page {
 
     $subTypes['Activity'] = CRM_Core_PseudoConstant::activityType(FALSE, TRUE, FALSE, 'label', TRUE);
     $subTypes['Contribution'] = CRM_Contribute_PseudoConstant::financialType();
-    $subTypes['Membership'] = CRM_Member_BAO_MembershipType::getMembershipTypes(FALSE);
+    $subTypes['Membership'] = \Civi\Api4\MembershipType::get(FALSE)
+      ->addWhere('is_active', '=', TRUE)
+      ->addOrderBy('weight', 'ASC')
+      ->execute()
+      ->column('title', 'id');
     $subTypes['Event'] = CRM_Core_OptionGroup::values('event_type');
     $subTypes['Campaign'] = CRM_Campaign_PseudoConstant::campaignType();
     $subTypes['Participant'] = [];

CRM/Member/Form/MembershipBlock.php
@@ -97,7 +97,11 @@ class CRM_Member_Form_MembershipBlock extends CRM_Contribute_Form_ContributionPa
    * @throws \CRM_Core_Exception
    */
   public function buildQuickForm() {
-    $membershipTypes = CRM_Member_BAO_MembershipType::getMembershipTypes();
+    $membershipTypes = \Civi\Api4\MembershipType::get(FALSE)
+      ->addWhere('is_active', '=', TRUE)
+      ->addOrderBy('weight', 'ASC')
+      ->execute()
+      ->column('title', 'id');
 
     if (!empty($membershipTypes)) {
       $this->addElement('checkbox', 'member_is_active', ts('Membership Section Enabled?'));
@@ -412,7 +416,7 @@ class CRM_Member_Form_MembershipBlock extends CRM_Contribute_Form_ContributionPa
             unset($options[$priceFieldID]);
           }
           $membetype = CRM_Member_BAO_MembershipType::getMembershipTypeDetails($memType);
-          $fieldParams['option_label'][$rowCount] = $membetype['name'] ?? NULL;
+          $fieldParams['option_label'][$rowCount] = $membetype['frontend_title'] ?? $membetype['title'];
           $fieldParams['option_amount'][$rowCount] = $membetype['minimum_fee'] ?? 0;
           $fieldParams['option_weight'][$rowCount] = $membetype['weight'] ?? NULL;
           $fieldParams['option_description'][$rowCount] = $membetype['description'] ?? NULL;

CRM/Member/Page/DashBoard.php
@@ -76,9 +76,11 @@ class CRM_Member_Page_DashBoard extends CRM_Core_Page {
     $monthStart = $ym . '-01';
     $yearStart = substr($ym, 0, 4) . '-01-01';
 
-    $membershipTypes = CRM_Member_BAO_MembershipType::getMembershipTypes(FALSE);
-    // added
-    //$membership = new CRM_Member_BAO_Membership;
+    $membershipTypes = \Civi\Api4\MembershipType::get(FALSE)
+      ->addWhere('is_active', '=', TRUE)
+      ->addOrderBy('weight', 'ASC')
+      ->execute()
+      ->column('title', 'id');
 
     foreach ($membershipTypes as $key => $value) {
 

schema/Member/Membership.entityType.php
@@ -82,7 +82,8 @@ return [
       'pseudoconstant' => [
         'table' => 'civicrm_membership_type',
         'key_column' => 'id',
-        'label_column' => 'name',
+        'name_column' => 'name',
+        'label_column' => 'title',
       ],
       'entity_reference' => [
         'entity' => 'MembershipType',

tests/phpunit/CRM/Member/BAO/MembershipTypeTest.php
@@ -172,7 +172,11 @@ class CRM_Member_BAO_MembershipTypeTest extends CiviUnitTestCase {
       'is_active' => 1,
     ];
     $membershipTypeID = MembershipType::create()->setValues($params)->execute()->first()['id'];
-    $result = CRM_Member_BAO_MembershipType::getMembershipTypes();
+    $result = \Civi\Api4\MembershipType::get(FALSE)
+      ->addWhere('is_active', '=', TRUE)
+      ->addOrderBy('weight', 'ASC')
+      ->execute()
+      ->column('title', 'id');
     $this->assertEquals('General', $result[$membershipTypeID], 'Verify membership types.');
   }
 

commit 783e43865a63d0c7df5ac09674c868837d5f6492

Fix saving membership types
Fixes dev/core#6389
This regressed with commit 0ceba2e94

CRM/Member/Form/MembershipType.php
@@ -435,7 +435,7 @@ class CRM_Member_Form_MembershipType extends CRM_Member_Form_MembershipConfig {
       }
 
       $params['domain_id'] = CRM_Core_Config::domainID();
-      $membershipTypeResult = CRM_Member_BAO_MembershipType::writeRecord($params);
+      $membershipTypeResult = CRM_Member_BAO_MembershipType::add($params);
       $membershipTypeName = $membershipTypeResult->name;
 
       CRM_Core_Session::setStatus(ts("The membership type '%1' has been saved.",