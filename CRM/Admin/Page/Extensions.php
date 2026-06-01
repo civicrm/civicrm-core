fc18ba3e9c453cee5d3ca911b7648c42237167d8
Herb v/d Dool <herb@3speedhub.com>
2026-06-01 16:01:01 -0400

    Issue #6526: no download links when extension directory not writable

--- CRM/Admin/Page/Extensions.php
+++ CRM/Admin/Page/Extensions.php
@@ -272,7 +272,7 @@ class CRM_Admin_Page_Extensions extends CRM_Core_Page_Basic {
       if (isset($localExtensionRows[$info->key])) {
         if (array_key_exists('version', $localExtensionRows[$info->key])) {
           if (version_compare($localExtensionRows[$info->key]['version'] ?? '', $info->version, '<')) {
-            $row['upgradelink'] = $mapper->getUpgradeLink($remoteExtensions[$info->key], $localExtensionRows[$info->key]);
+            $row['upgradelink'] = $mapper->getUpgradeLink($remoteExtensions[$info->key], $localExtensionRows[$info->key], CRM_Extension_System::singleton()->getDownloader()->extensionDirectoryWritable());
           }
         }
       }
