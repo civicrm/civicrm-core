11d0e4e903bac37692e4ce46b1e6f786737c4e60
benjamin <ben@civicrm.org>
2026-06-03 17:10:31 +0100

    add iframe-resizer library to iframe extension

--- ext/iframe/Civi/Iframe/Router.php
+++ ext/iframe/Civi/Iframe/Router.php
@@ -94,7 +94,8 @@ class Router extends AutoService {
    * @throws \CRM_Core_Exception
    */
   protected function invokeBasic(array $params): void {
-    \CRM_Core_Resources::singleton()->addCoreResources('html-header');
+    \Civi::resources()->addCoreResources('html-header');
+    \Civi::resources()->addScriptFile(E::LONG_NAME, 'packages/iframe-resizer.child.js');
 
     ob_start();
     $pageContent = \CRM_Core_Invoke::invoke(explode('/', $params['route']));
