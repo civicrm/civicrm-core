0c8fa5df08cc18d5d5ab2a2dc1c4aeec5a3bea6c
benjamin <ben@civicrm.org>
2026-06-04 21:55:22 +0100

    FTS - support LIKE operator for boolean search

--- Civi/Api4/Service/Spec/Provider/FullTextSearchFilterSpecProvider.php
+++ Civi/Api4/Service/Spec/Provider/FullTextSearchFilterSpecProvider.php
@@ -42,8 +42,8 @@ class FullTextSearchFilterSpecProvider extends \Civi\Core\Service\AutoService im
         ->setDescription($description)
         ->setType('Filter')
         ->setInputType('Text')
-        ->setOperators(['MATCHES'])
-        ->addSqlFilter(fn (array $field, string $fieldAlias, string $operator, $value) => self::getMatchFilterSql($index['columns'], $fieldAlias, $value));
+        ->setOperators(['CONTAINS', 'LIKE', 'MATCHES'])
+        ->addSqlFilter(fn (array $field, string $fieldAlias, string $operator, $value) => self::getMatchFilterSql($index['columns'], $fieldAlias, $operator, $value));
       $spec->addFieldSpec($field);
     }
   }
@@ -72,14 +72,52 @@ class FullTextSearchFilterSpecProvider extends \Civi\Core\Service\AutoService im
   /**
    * @param array $columns
    * @param string $fieldAlias
+   * @param string $operator
    * @param string $value
    * return string
    */
-  public static function getMatchFilterSql(array $columns, string $fieldAlias, string $value): string {
+  public static function getMatchFilterSql(array $columns, string $fieldAlias, string $operator, string $value): string {
     $columns = array_map(fn ($column) => \str_replace('id', $column, $fieldAlias), $columns);
-    $match = implode(',', $columns);
+    $columnList = implode(',', $columns);
+
+    // escape user entered value
     $value = \CRM_Core_DAO::escapeString($value);
-    return "MATCH({$match}) AGAINST ('$value')";
+
+    switch ($operator) {
+      // CONTAINS = full word matches
+      case 'CONTAINS':
+        return "MATCH({$columnList}) AGAINST ('$value' IN NATURAL LANGUAGE MODE)";
+
+      // MATCHES = raw FTS boolean expression
+      case 'MATCHES':
+        return "MATCH({$columnList}) AGAINST ('$value' IN BOOLEAN MODE)";
+
+      // LIKE = we aim to provide interop with Mysql LIKE syntax though
+      // this only works with trailing wildcard
+      case 'LIKE':
+        $words = explode(' ', $value);
+
+        $words = array_filter(array_map(function ($word) use ($fieldAlias) {
+          $wildcardPos = strpos($word, '%');
+          // if no wildcards leave as is
+          if ($wildcardPos === FALSE) {
+            return '+' . $word;
+          }
+          // trailing wildcard replace with FTS wildcard *
+          if ($wildcardPos === (strlen($word) - 1)) {
+            return '+' . rtrim($word, '%') . '*';
+          }
+          // if non-trailing wildcards then FTS can't handle
+          // Q: should we just log?
+          throw new \CRM_Core_Exception("Invalid wildcard in FTS query expression {$fieldAlias} LIKE {$word} - ignoring");
+        }, $words));
+
+        $value = implode(' ', $words);
+
+        return "MATCH({$columnList}) AGAINST ('$value' IN BOOLEAN MODE)";
+    }
+
+    throw new \CRM_Core_Exception('Unrecognised operator for Full Text Search field');
   }
 
 }
