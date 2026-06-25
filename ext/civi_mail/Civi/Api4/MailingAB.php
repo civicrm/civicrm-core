5c5dd19d923150938361cf39b0307568884352a7
colemanw <coleman@civicrm.org>
2026-06-24 20:40:23 -0400

    Expose MailingAB entity to APIv4 and add CRUD test

---
+++ ext/civi_mail/Civi/Api4/MailingAB.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC. All rights reserved.                        |
+ |                                                                    |
+ | This work is published under the GNU AGPLv3 license with some      |
+ | permitted exceptions and without any warranty. For full license    |
+ | and copyright information, see https://civicrm.org/licensing       |
+ +--------------------------------------------------------------------+
+ */
+namespace Civi\Api4;
+
+/**
+ * MailingAB.
+ *
+ * MailingAB entities store information about A/B testing.
+ *
+ * @since 6.17
+ * @package Civi\Api4
+ */
+class MailingAB extends Generic\DAOEntity {
+
+}
