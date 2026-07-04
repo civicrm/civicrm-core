e138bf4eba0bc7ef1f6b441403f0e81512499eae
colemanw <coleman@civicrm.org>
2026-07-04 16:57:16 +0200

    Api4 - Add Currency entity with autocomplete support

---
+++ Civi/Api4/Currency.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC. All rights reserved.                        |
+ |                                                                    |
+ | This work is published under the GNU AGPLv3 license with some      |
+ | permitted exceptions and without any warranty. For full license    |
+ | and copyright information, see https://civicrm.org/licensing       |
+ +--------------------------------------------------------------------+
+ */
+namespace Civi\Api4;
+
+/**
+ * Currency entity.
+ *
+ * @searchFields full_name
+ * @since 6.18
+ * @package Civi\Api4
+ */
+class Currency extends Generic\DAOEntity {
+
+}
