9e6201095db7d0f3b894684837824bfecd573dc6
eileen <emcnaughton@wikimedia.org>
2026-07-17 16:36:46 +1200

    Add PaymentCompletionMetadata entity

---
+++ CRM/Contribute/DAO/PaymentCompletionMetadata.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @package CRM
+ * @copyright CiviCRM LLC https://civicrm.org/licensing
+ */
+
+/**
+ * Placeholder class retained for legacy compatibility.
+ *
+ * @property int|string|null $id
+ * @property int|string $contact_id
+ * @property int|string|null $contribution_id
+ * @property int|string|null $line_item_id
+ * @property array $metadata
+ * @property string|null $created_date
+ * @property string|null $modified_date
+ */
+class CRM_Contribute_DAO_PaymentCompletionEntityMetadata extends CRM_Core_DAO_Base {
+}
