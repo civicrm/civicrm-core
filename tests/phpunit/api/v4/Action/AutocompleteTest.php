civicrm/civicrm-core

commit 7ceb8a72bc43c2f547ee6d2583bb6cc701b26ca7

Api4 - Fix EntitySet when using suffixes in the SELECT clause

Civi/Api4/Query/Api4EntitySetQuery.php
@@ -81,7 +81,7 @@ class Api4EntitySetQuery extends Api4Query {
       $setResults[$index][] = &$result;
     }
     foreach ($setResults as $index => &$setResult) {
-      $fieldSpec = $this->getSubquery($index)->apiFieldSpec;
+      $fieldSpec = $this->getSubquery($index)->apiFieldSpec + $this->apiFieldSpec;
       $selectAliases = $this->getSubquery($index)->selectAliases;
       FormattingUtil::formatOutputValues($setResult, $fieldSpec, 'get', $selectAliases);
     }
@@ -115,12 +115,14 @@ class Api4EntitySetQuery extends Api4Query {
       }
       $expr = SqlExpression::convert($sql);
       $field = $expr->getType() === 'SqlField' ? $this->getSubquery()->getField($expr->getFields()[0]) : NULL;
-      $this->addSpecField($alias, [
+      $spec = [
         'sql_name' => "`$alias`",
+      ] + ($field ?: []) + [
         'entity' => $field['entity'] ?? NULL,
         'name' => $field['name'] ?? $alias,
         'data_type' => $field['data_type'] ?? $expr::getDataType(),
-      ]);
+      ];
+      $this->addSpecField($alias, $spec);
     }
     // Parse select clause if not using default of *
     foreach ($select as $item) {
@@ -129,16 +131,30 @@ class Api4EntitySetQuery extends Api4Query {
       $this->selectAliases[$alias] = $expr->getExpr();
       $this->query->select($expr->render($this, TRUE));
     }
+    if ($select && !$this->isAggregateQuery()) {
+      $this->selectAliases['_api_set_index'] = '_api_set_index';
+      $this->query->select('`_api_set_index`');
+    }
   }
 
   /**
    * @param string $expr
+   * @param bool $strict
    * @return array|null
    */
-  public function getField(string $expr):? array {
-    $col = strpos($expr, ':');
-    $fieldName = $col ? substr($expr, 0, $col) : $expr;
-    return $this->apiFieldSpec[$fieldName] ?? NULL;
+  public function getField(string $expr, bool $strict = FALSE):? array {
+    if (isset($this->apiFieldSpec[$expr])) {
+      $field = $this->apiFieldSpec[$expr];
+    }
+    else {
+      $col = strpos($expr, ':');
+      $fieldName = $col ? substr($expr, 0, $col) : $expr;
+      $field = $this->apiFieldSpec[$fieldName] ?? NULL;
+    }
+    if ($strict && $field === NULL) {
+      throw new \CRM_Core_Exception("Invalid field '$expr'");
+    }
+    return $field ?: NULL;
   }
 
   protected function buildWhereClause() {

Civi/Api4/Utils/FormattingUtil.php
@@ -248,7 +248,7 @@ class FormattingUtil {
         $fieldExpr = $fieldExprs[$key];
         $fieldName = \CRM_Utils_Array::first($fieldExpr->getFields());
         $baseName = $fieldName ? \CRM_Utils_Array::first(explode(':', $fieldName)) : NULL;
-        $field = $fields[$fieldName ?? ''] ?? $fields[$baseName ?? ''] ?? NULL;
+        $field = $fields[$fieldName ?? ''] ?? $fields[$baseName ?? ''] ?? $fields[$key] ?? NULL;
         $dataType = $field['data_type'] ?? ($fieldName == 'id' ? 'Integer' : NULL);
         // Allow Sql Functions to alter the value and/or $dataType
         if (method_exists($fieldExpr, 'formatOutputValue') && is_string($value)) {

Civi/Api4/Query/Api4Query.php
@@ -64,7 +64,7 @@ abstract class Api4Query {
     $this->api = $api;
   }
 
-  abstract public function getField(string $expr):? array;
+  abstract public function getField(string $expr, bool $strict = FALSE):? array;
 
   /**
    * Builds main final sql statement after initialization.

tests/phpunit/api/v4/Action/EntitySetUnionTest.php
@@ -86,6 +86,45 @@ class EntitySetUnionTest extends Api4TestBase implements TransactionalInterface 
     // Correct pseudoconstants should have been looked up for each row
     $this->assertEquals(['Access Control', 'Mailing List'], $result[0]['type']);
     $this->assertEquals(['Contact', 'Activity'], $result[1]['type']);
+
+    // Same as above but without the alias
+    $result = EntitySet::get(FALSE)
+      ->addSelect('title', 'description', 'group_type:name')
+      ->addSet('UNION ALL', Group::get()
+        ->addSelect('title', 'description', 'group_type:name')
+        ->addWhere('title', 'IN', ['1G', '2>G', '3G'])
+      )
+      ->addSet('UNION ALL', Tag::get()
+        ->addSelect('name', 'description', 'used_for:name')
+        ->addWhere('name', 'IN', ['1T', '2<T', '3T'])
+      )
+      ->addOrderBy('title')
+      ->addWhere('title', 'LIKE', '3%')
+      ->setDebug(TRUE)
+      ->execute();
+    $this->assertCount(2, $result);
+    // Correct pseudoconstants should have been looked up for each row
+    $this->assertEquals(['Access Control', 'Mailing List'], $result[0]['group_type:name']);
+    $this->assertEquals(['Contact', 'Activity'], $result[1]['group_type:name']);
+
+    // Same as above but without the SELECT
+    $result = EntitySet::get(FALSE)
+      ->addSet('UNION ALL', Group::get()
+        ->addSelect('title', 'description', 'group_type:name')
+        ->addWhere('title', 'IN', ['1G', '2>G', '3G'])
+      )
+      ->addSet('UNION ALL', Tag::get()
+        ->addSelect('name', 'description', 'used_for:name')
+        ->addWhere('name', 'IN', ['1T', '2<T', '3T'])
+      )
+      ->addOrderBy('title')
+      ->addWhere('title', 'LIKE', '3%')
+      ->setDebug(TRUE)
+      ->execute();
+    $this->assertCount(2, $result);
+    // Correct pseudoconstants should have been looked up for each row
+    $this->assertEquals(['Access Control', 'Mailing List'], $result[0]['group_type:name']);
+    $this->assertEquals(['Contact', 'Activity'], $result[1]['group_type:name']);
   }
 
   public function testGroupByUnionSet(): void {

commit 5e38cbd93d134e7836771ca57b2e327c23298dfd

Api4 - Ensure fields within functions are formatted correctly
This fixes a regression from 7ceb8a72 causing the CiviMail autocomplete of Groups+Mailings to fail.

In addition to fixing the bug, this makes the output of sql functions much cleaner: fields are correctly type cast according to their data_type, even when being returned from a sql function.

Civi/Api4/Query/Api4EntitySetQuery.php
@@ -71,7 +71,7 @@ class Api4EntitySetQuery extends Api4Query {
     $results = $this->getResults();
     // Aggregated queries will have to make due with limited field info
     if (!isset($results[0]['_api_set_index'])) {
-      FormattingUtil::formatOutputValues($results, $this->apiFieldSpec, 'get', $this->selectAliases);
+      FormattingUtil::formatOutputValues($results, $this->apiFieldSpec, 'get', $this->selectAliases, $this);
       return $results;
     }
     // Categorize rows by set, so each set can be formatted as a batch
@@ -86,7 +86,7 @@ class Api4EntitySetQuery extends Api4Query {
     foreach ($setResults as $index => &$setResult) {
       $fieldSpec = $this->getSubquery($index)->apiFieldSpec + $this->apiFieldSpec;
       $selectAliases = $this->getSubquery($index)->selectAliases;
-      FormattingUtil::formatOutputValues($setResult, $fieldSpec, 'get', $selectAliases);
+      FormattingUtil::formatOutputValues($setResult, $fieldSpec, 'get', $selectAliases, $this->getSubquery($index));
     }
     return $results;
   }

Civi/Api4/Query/Api4SelectQuery.php
@@ -104,7 +104,7 @@ class Api4SelectQuery extends Api4Query {
    */
   public function run(): array {
     $results = $this->getResults();
-    FormattingUtil::formatOutputValues($results, $this->apiFieldSpec, 'get', $this->selectAliases);
+    FormattingUtil::formatOutputValues($results, $this->apiFieldSpec, 'get', $this->selectAliases, $this);
     return $results;
   }
 

Civi/Api4/Query/SqlEquation.php
@@ -136,8 +136,10 @@ class SqlEquation extends SqlExpression {
    * @see \Civi\Api4\Utils\FormattingUtil::formatOutputValues
    * @param string|null $dataType
    * @param array $values
+   * @param string $key
+   * @param Api4Query|null $query
    */
-  public function formatOutputValue(?string &$dataType, array &$values) {
+  public function formatOutputValue(?string &$dataType, array &$values, string $key, ?Api4Query $query = NULL) {
     foreach (self::$comparisonOperators as $op) {
       if (strpos($this->expr, " $op ")) {
         $dataType = 'Boolean';

Civi/Api4/Query/SqlFunction.php
@@ -119,19 +119,19 @@ abstract class SqlFunction extends SqlExpression {
    * @param string|null $dataType
    * @param array $values
    * @param string $key
+   * @param \Civi\Api4\Query\Api4Query|null $query
    * @see \Civi\Api4\Utils\FormattingUtil::formatOutputValues
    */
-  public function formatOutputValue(?string &$dataType, array &$values, string $key): void {
-    if (static::$dataType) {
-      $dataType = static::$dataType;
-    }
-    elseif (static::$category === self::CATEGORY_AGGREGATE) {
+  public function formatOutputValue(?string &$dataType, array &$values, string $key, ?Api4Query $query = NULL): void {
+    $dataType = $this->getRenderedDataType($query) ?? $dataType;
+    if (static::$category === self::CATEGORY_AGGREGATE) {
       $exprArgs = $this->getArgs();
       // If the first expression is a SqlFunction/SqlEquation, allow it to control the aggregate dataType
       if (method_exists($exprArgs[0]['expr'][0], 'formatOutputValue')) {
-        $exprArgs[0]['expr'][0]->formatOutputValue($dataType, $values, $key);
+        $exprArgs[0]['expr'][0]->formatOutputValue($dataType, $values, $key, $query);
       }
     }
+
     if (isset($values[$key]) && $this->suffix && $this->suffix !== 'id') {
       $dataType = 'String';
       $value =& $values[$key];

Civi/Api4/Query/SqlFunctionGROUP_CONCAT.php
@@ -57,9 +57,10 @@ class SqlFunctionGROUP_CONCAT extends SqlFunction {
    * @param string|null $dataType
    * @param array $values
    * @param string $key
+   * @param Api4Query|null $query
    * @see \Civi\Api4\Utils\FormattingUtil::formatOutputValues
    */
-  public function formatOutputValue(?string &$dataType, array &$values, string $key): void {
+  public function formatOutputValue(?string &$dataType, array &$values, string $key, ?Api4Query $query = NULL): void {
     $exprArgs = $this->getArgs();
     // By default, values are split into an array and formatted according to the field's dataType
     if ($this->getSerialize()) {
@@ -67,9 +68,10 @@ class SqlFunctionGROUP_CONCAT extends SqlFunction {
       // If the first expression is a SqlFunction/SqlEquation, allow it to control the dataType
       if (method_exists($exprArgs[0]['expr'][0], 'formatOutputValue')) {
         foreach (array_keys($values[$key]) as $index) {
-          $exprArgs[0]['expr'][0]->formatOutputValue($dataType, $values[$key], $index);
+          $exprArgs[0]['expr'][0]->formatOutputValue($dataType, $values[$key], $index, $query);
         }
       }
+
       // Perform deduping by unique id
       if ($this->args[0]['prefix'] === ['UNIQUE'] && isset($values["_$key"])) {
         $ids = \CRM_Utils_Array::explodePadded($values["_$key"]);

Civi/Api4/Query/SqlFunctionIF.php
@@ -16,6 +16,8 @@ namespace Civi\Api4\Query;
  */
 class SqlFunctionIF extends SqlFunction {
 
+  public $supportsExpansion = TRUE;
+
   protected static $category = self::CATEGORY_COMPARISON;
 
   protected static function params(): array {
@@ -52,6 +54,32 @@ class SqlFunctionIF extends SqlFunction {
    */
   public static function getDescription(): string {
     return ts('If the field is boolean TRUE, or any number except 0, or a string starting with the digits 1-9, the first value; otherwise the second.');
+  }
+
+  public function getFields(): array {
+    $fields = [];
+    foreach ([$this->args[1] ?? NULL, $this->args[2] ?? NULL, $this->args[0] ?? NULL] as $arg) {
+      if (!empty($arg['expr'])) {
+        foreach ($arg['expr'] as $expr) {
+          $fields = array_merge($fields, $expr->getFields());
+        }
+      }
+    }
+    return array_unique($fields);
+  }
+
+  public function getRenderedDataType(?Api4Query $query): ?string {
+
+    foreach ([$this->args[1] ?? NULL, $this->args[2] ?? NULL] as $arg) {
+      if (!empty($arg['expr'][0])) {
+        $expr = $arg['expr'][0];
+        $type = $expr->getRenderedDataType($query);
+        if ($type) {
+          return $type;
+        }
+      }
+    }
+    return static::$dataType;
   }
 
 }

Civi/Api4/Query/SqlFunctionSUM.php
@@ -49,7 +49,7 @@ class SqlFunctionSUM extends SqlFunction {
    * As far as I can tell none of the suffix logic in parent will apply to SUM values
    * so is not needed here
    */
-  public function formatOutputValue(?string &$dataType, array &$values, string $key): void {
+  public function formatOutputValue(?string &$dataType, array &$values, string $key, ?Api4Query $query = NULL): void {
     if ($dataType === 'Boolean') {
       $dataType = 'Integer';
     }

Civi/Api4/Query/SqlNumber.php
@@ -22,6 +22,10 @@ class SqlNumber extends SqlExpression {
     \CRM_Utils_Type::validate($this->expr, 'Float');
   }
 
+  public function getRenderedDataType(?Api4Query $query): ?string {
+    return is_numeric($this->expr) && !str_contains((string) $this->expr, '.') ? 'Integer' : 'Float';
+  }
+
   public static function getTitle(): string {
     return ts('Number');
   }

Civi/Api4/Utils/FormattingUtil.php
@@ -12,6 +12,7 @@
 
 namespace Civi\Api4\Utils;
 
+use Civi\Api4\Query\Api4Query;
 use Civi\Api4\Query\SqlExpression;
 
 require_once 'api/v3/utils.php';
@@ -230,9 +231,10 @@ class FormattingUtil {
    * @param array $fields
    * @param string $action
    * @param array $selectAliases
+   * @param \Civi\Api4\Query\Api4Query|null $query
    * @throws \CRM_Core_Exception
    */
-  public static function formatOutputValues(&$records, $fields, $action = 'get', $selectAliases = []) {
+  public static function formatOutputValues(&$records, $fields, $action = 'get', $selectAliases = [], ?Api4Query $query = NULL) {
     $fieldExprs = [];
     foreach ($records as &$result) {
       $contactTypePaths = [];
@@ -260,9 +262,10 @@ class FormattingUtil {
         $dataType = $field['data_type'] ?? ($fieldName == 'id' ? 'Integer' : NULL);
         // Allow Sql Functions to alter the value and/or $dataType
         if (method_exists($fieldExpr, 'formatOutputValue') && is_string($value)) {
-          $fieldExpr->formatOutputValue($dataType, $result, $key);
+          $fieldExpr->formatOutputValue($dataType, $result, $key, $query);
           $value = $result[$key];
         }
+        // Output postprocessing - @see FieldSpec::addOutputFormatter
         if (!empty($field['output_formatters'])) {
           self::applyFormatters($result, $fieldExpr, $field, $value);
           $dataType = NULL;

tests/phpunit/api/v4/Action/AutocompleteTest.php
@@ -23,6 +23,8 @@ use api\v4\Api4TestBase;
 use Civi\API\Exception\UnauthorizedException;
 use Civi\API\Event\PrepareEvent;
 use Civi\Api4\Contact;
+use Civi\Api4\Group;
+use Civi\Api4\Mailing;
 use Civi\Api4\MockBasicEntity;
 use Civi\Api4\EntitySet;
 use Civi\Api4\SavedSearch;
@@ -59,6 +61,7 @@ class AutocompleteTest extends Api4TestBase implements HookInterface, Transactio
   }
 
   public function setUp(): void {
+    \CRM_Core_BAO_ConfigSetting::enableAllComponents();
     $this->hookCallback = NULL;
     $this->autocompleteRunCount = 0;
     // Ensure MockBasicEntity gets added via above listener
@@ -371,6 +374,41 @@ class AutocompleteTest extends Api4TestBase implements HookInterface, Transactio
       ->execute();
     $this->assertCount(1, $result);
     $this->assertEquals('Donors contributed > $100', $result[0]['label']);
+  }
+
+  public function testMailingRecipientsAutocomplete(): void {
+    Group::delete()->addWhere('id', '>', 0)->execute();
+    Mailing::delete()->addWhere('id', '>', 0)->execute();
+    $group = $this->createTestRecord('Group', [
+      'title' => 'Test Recipient Group',
+      'group_type:name' => 'Mailing List',
+    ]);
+    $mailing = $this->createTestRecord('Mailing', [
+      'name' => 'Test Newsletter',
+      'subject' => 'July Newsletter Subject',
+      'is_completed' => TRUE,
+    ]);
+
+    $result = EntitySet::autocomplete()
+      ->setInput('Test')
+      ->setFieldName('Mailing.recipients_include')
+      ->setFormName('crmMailing.0')
+      ->execute();
+
+    $this->assertCount(2, $result);
+
+    $rows = array_column((array) $result, NULL, 'id');
+
+    $groupRow = $rows['groups_' . $group['id']] ?? NULL;
+    $this->assertNotNull($groupRow);
+    $this->assertSame('Test Recipient Group', $groupRow['label']);
+    $this->assertSame('fa-group', $groupRow['icon']);
+
+    $mailingRow = $rows['mailings_' . $mailing['id']] ?? NULL;
+    $this->assertNotNull($mailingRow);
+    $this->assertSame('Test Newsletter', $mailingRow['label']);
+    $this->assertSame('fa-envelope', $mailingRow['icon']);
+    $this->assertStringContainsString('Sent Mailing', $mailingRow['description'][0]);
   }
 
   /**

tests/phpunit/api/v4/Action/SqlFunctionTest.php
@@ -23,6 +23,7 @@ use api\v4\Api4TestBase;
 use Civi\Api4\Activity;
 use Civi\Api4\Contact;
 use Civi\Api4\Contribution;
+use Civi\Api4\CustomGroup;
 use Civi\Api4\Utils\CoreUtil;
 use Civi\Test\TransactionalInterface;
 
@@ -612,6 +613,7 @@ class SqlFunctionTest extends Api4TestBase implements TransactionalInterface {
   }
 
   public function testJsonExtract(): void {
+    CustomGroup::delete(FALSE)->addWhere('id', '>', 0)->execute();
     \Civi\Api4\CustomGroup::create(FALSE)
       ->addValue('name', 'json_test')
       ->addValue('title', 'Json test')
@@ -674,6 +676,40 @@ class SqlFunctionTest extends Api4TestBase implements TransactionalInterface {
     $this->assertEquals(4, $result[1]['first_element']);
     $this->assertEquals(15, $result[1]['second_element']);
     $this->assertEquals(9, $result[1]['third_element']);
+  }
+
+  /**
+   * Ensures field data is formatted correctly within the IF() function.
+   */
+  public function testSqlFunctionIfDataType(): void {
+    $cid = Contact::create(FALSE)
+      ->addValue('first_name', 'hello')
+      ->addValue('birth_date', '2020-05-05')
+      ->addValue('is_opt_out', TRUE)
+      ->addValue('preferred_communication_method:name', ['Phone', 'Email'])
+      ->execute()->first()['id'];
+
+    $result = Contact::get(FALSE)
+      ->addWhere('id', '=', $cid)
+      ->addSelect('IF(is_deleted, "Yes", "No") AS string_lit')
+      ->addSelect('IF(is_deleted, "Yes", first_name) AS string_field')
+      ->addSelect('IF(is_deleted, 10, 20) AS int_val')
+      ->addSelect('IF(is_deleted, 0, id) AS int_field')
+      ->addSelect('IF(is_deleted, 1.5, 2.5) AS float_val')
+      ->addSelect('IF(is_deleted, created_date, birth_date) AS date_val')
+      ->addSelect('IF(is_deleted, is_deleted, is_opt_out) AS bool_val')
+      ->addSelect('IF(is_deleted, NULL, preferred_communication_method:name) AS array_val')
+      ->execute()->first();
+
+    $this->assertSame('No', $result['string_lit']);
+    $this->assertSame('hello', $result['string_field']);
+    $this->assertSame(20, $result['int_val']);
+    $this->assertSame((int) $cid, $result['int_field']);
+    $this->assertSame(2.5, $result['float_val']);
+    $this->assertSame('2020-05-05 00:00:00', $result['date_val']);
+
+    $this->assertSame(TRUE, $result['bool_val']);
+    $this->assertSame(['Phone', 'Email'], $result['array_val']);
   }
 
 }