e721bb6ba612a177adbf935a4c95dceafb0f49df
colemanw <coleman@civicrm.org>
2026-07-30 16:22:04 -0400

    Tests - Remove duplicate switch case

--- tests/phpunit/CiviTest/CiviUnitTestCase.php
+++ tests/phpunit/CiviTest/CiviUnitTestCase.php
@@ -3199,7 +3199,6 @@ class CiviUnitTestCaseCommon extends PHPUnit\Framework\TestCase {
 
       case 'CRM_Custom_Import_Form_DataSource':
       case 'CRM_Custom_Import_Form_MapField':
-      case 'CRM_CiviImport_Form_Generic_Preview':
         $form->controller = new CRM_Import_Controller('import custom data', ['class_prefix' => 'CRM_Custom_Import']);
         $form->controller->setStateMachine(new CRM_Core_StateMachine($form->controller));
         // The submitted values should be set on one or the other of the forms in the flow.
