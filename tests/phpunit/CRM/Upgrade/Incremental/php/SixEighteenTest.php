5882c13a3333e8e97b1f1d88e1b3ac4bfdbd227c
colemanw <coleman@civicrm.org>
2026-09-02 10:34:49 -0400

    Upgrader - Fix test

--- tests/phpunit/CRM/Upgrade/Incremental/php/SixEighteenTest.php
+++ tests/phpunit/CRM/Upgrade/Incremental/php/SixEighteenTest.php
@@ -9,19 +9,15 @@ class CRM_Upgrade_Incremental_php_SixEighteenTest extends CiviUnitTestCase {
   public function testContributionPageTableAlter(): void {
     $pageID = $this->contributionPageCreate()['id'];
     $sqlModes = CRM_Utils_SQL::getSqlModes();
-    $changedSQLModes = $sqlModes;
-    if (!empty($sqlModes) && in_array('NO_ZERO_DATE', $changedSQLModes)) {
-      if ($key = array_search('NO_ZERO_DATE', $changedSQLModes)) {
-        unset($changedSQLModes[$key]);
-        CRM_Core_DAO::executeQuery("SET SESSION sql_mode = '" . implode(',', $changedSQLModes) . "'");
-      }
+    if (in_array('NO_ZERO_DATE', $sqlModes)) {
+      CRM_Utils_Sql::setSqlModes(array_diff($sqlModes, ['NO_ZERO_DATE']));
     }
     elseif (!empty($sqlModes)) {
       $sqlModes[] = 'NO_ZERO_DATE';
     }
-    CRM_Core_DAO::executeQuery("ALTER TABLE civicrm_contribution_page DROP CONSTRAINT `FK_civicrm_contribution_page_currency`");
+    CRM_Core_BAO_SchemaHandler::safeRemoveFK('civicrm_contribution_page', 'FK_civicrm_contribution_page_currency');
     CRM_Core_DAO::executeQuery("UPDATE civicrm_contribution_page SET created_date = '0000-00-00 00:00:00' WHERE id = %1", [1 => [$pageID, 'Positive']]);
-    CRM_Core_DAO::executeQuery("SET SESSION sql_mode = '" . implode(',', $sqlModes) . "'");
+    CRM_Utils_Sql::setSqlModes($sqlModes);
     CRM_Upgrade_Incremental_php_SixEighteen::addCurrencyFk(NULL, 'ContributionPage', 'currency');
   }
 
