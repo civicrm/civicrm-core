98c595cc20f649e252b03fbe7a57a8e85ca59069
colemanw <coleman@civicrm.org>
2026-09-09 12:18:14 -0400

    CiviMail - Fix entityRef custom fields in mailings
    Mailing custom fields reuse afform components, without an afform. This means there is no formName. Previously this would crash when using an EntityRef autocomplete; this fixes the issue.

--- ext/afform/core/ang/af/afFieldset.directive.js
+++ ext/afform/core/ang/af/afFieldset.directive.js
@@ -49,7 +49,13 @@
         };
 
         this.getFormName = function() {
-          return ctrl.afFormCtrl ? ctrl.afFormCtrl.getFormMeta().name : $scope.meta.name;
+          if (ctrl.afFormCtrl) {
+            return ctrl.afFormCtrl.getFormMeta().name;
+          }
+          if ($scope.meta) {
+            return $scope.meta.name;
+          }
+          return $element.closest('form').attr('name');
         };
 
         this.getFieldMeta = () => {
