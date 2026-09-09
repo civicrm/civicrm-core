f605c4b08130afa3a064717de6fec3ae823ffab4
Matthew Wire <mjw@mjwconsult.co.uk>
2026-09-09 17:32:05 +0100

    Pass the created financial trxn into FinancialProcessor line item creation
    recordFinancialAccounts() creates the financial trxn and then createLineItems() re-derived it by SQL, once per line item, via the fallback in FinancialItem::add().

--- CRM/Contribute/BAO/FinancialProcessor.php
+++ CRM/Contribute/BAO/FinancialProcessor.php
@@ -354,7 +354,7 @@ class CRM_Contribute_BAO_FinancialProcessor {
         $entityId = $this->getContributionID();
         $entityTable = 'civicrm_contribution';
       }
-      $this->createLineItems($entityId, $params['line_item'], $entityTable);
+      $this->createLineItems($entityId, $params['line_item'], $entityTable, $financialTxn->id ?? NULL);
     }
 
     // create batch entry if batch_id is passed and
@@ -1074,10 +1074,12 @@ class CRM_Contribute_BAO_FinancialProcessor {
    *   Line item array.
    * @param string $entityTable
    *   Entity table.
+   * @param int|null $financialTrxnID
+   *   Transaction paying for the created financial items, if there is one.
    *
    * @throws \CRM_Core_Exception
    */
-  private function createLineItems(int $entityId, array $lineItems, $entityTable = 'civicrm_contribution') {
+  private function createLineItems(int $entityId, array $lineItems, $entityTable = 'civicrm_contribution', ?int $financialTrxnID = NULL) {
     foreach ($lineItems as &$values) {
 
       foreach ($values as &$line) {
@@ -1100,10 +1102,10 @@ class CRM_Contribute_BAO_FinancialProcessor {
         $createdLineItem = CRM_Price_BAO_LineItem::create($line);
 
         if (!$this->isUpdate()) {
-          $financialItem = CRM_Financial_BAO_FinancialItem::add($createdLineItem, $this->getUpdatedContribution());
+          $financialItem = CRM_Financial_BAO_FinancialItem::add($createdLineItem, $this->getUpdatedContribution(), FALSE, $financialTrxnID);
           $line['financial_item_id'] = $financialItem->id;
           if (!empty($line['tax_amount'])) {
-            CRM_Financial_BAO_FinancialItem::add($createdLineItem, $this->getUpdatedContribution(), TRUE);
+            CRM_Financial_BAO_FinancialItem::add($createdLineItem, $this->getUpdatedContribution(), TRUE, $financialTrxnID);
           }
         }
       }
