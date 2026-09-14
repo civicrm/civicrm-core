e54c9bc2bcee1ab3a2a0da15711464ae4861aa64
colemanw <coleman@civicrm.org>
2026-09-14 10:20:03 -0400

    CiviContribute - Add implied permission metadata

--- CRM/Contact/BAO/Contact.php
+++ CRM/Contact/BAO/Contact.php
@@ -2826,10 +2826,7 @@ LEFT JOIN civicrm_email    ON ( civicrm_contact.id = civicrm_email.contact_id )
         'href' => CRM_Utils_System::url('civicrm/contact/view/contribution',
           'reset=1&action=add&context=contribution'
         ),
-        'permissions' => [
-          'access CiviContribute',
-          'edit contributions',
-        ],
+        'permissions' => ['edit contributions'],
       ],
       'participant' => [
         'title' => ts('Register for Event'),
