51fc97237920922700af47301f7169c718df9635
colemanw <coleman@civicrm.org>
2026-09-14 10:31:44 -0400

    CiviReport - Add implied permission metadata

--- CRM/Report/Form.php
+++ CRM/Report/Form.php
@@ -745,10 +745,8 @@ class CRM_Report_Form extends CRM_Core_Form {
     $this->assign('criteriaForm', FALSE);
     // Will be overwritten in postProcess if TRUE.
     $this->assign('printOnly', FALSE);
-    // Display Report Criteria section if user has access Report Criteria OR administer Reports AND report instance is not reserved
-    if (CRM_Core_Permission::check('administer Reports') ||
-      CRM_Core_Permission::check('access Report Criteria')
-    ) {
+    // Display Report Criteria section if user has access Report Criteria AND report instance is not reserved
+    if (CRM_Core_Permission::check('access Report Criteria')) {
       if (empty($this->_instanceValues['is_reserved']) ||
         CRM_Core_Permission::check('administer reserved reports')
       ) {
