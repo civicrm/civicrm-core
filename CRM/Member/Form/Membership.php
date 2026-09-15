2cec78206abe39dac4eb1e3ef1c9c04e8f404c91
eileen <eileen@mcnaughty.com>
2026-09-16 11:16:05 +1200

    Simplify card assignment in back office form
    less passing of variables by reference

--- CRM/Member/Form/Membership.php
+++ CRM/Member/Form/Membership.php
@@ -1670,9 +1670,9 @@ class CRM_Member_Form_Membership extends CRM_Member_Form {
     if ($this->_mode) {
       // @todo move this outside shared code as Batch entry just doesn't
       $this->assign('address', CRM_Utils_Address::getFormattedBillingAddressFieldsFromParameters($this->_params));
-
-      $valuesForForm = CRM_Contribute_Form_AbstractEditPayment::formatCreditCardDetails($this->_params);
-      $this->assignVariables($valuesForForm, ['credit_card_exp_date', 'credit_card_type', 'credit_card_number']);
+      $this->assign('credit_card_number', $this->getMungedPanTruncation());
+      $this->assign('credit_card_exp_date', $this->getCreditCardExpiryDate());
+      $this->assign('credit_card_number', $this->getCreditCardType());
       $this->assign('is_pay_later', 0);
       $this->assign('isPrimary', 1);
     }
