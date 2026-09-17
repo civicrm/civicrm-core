420540f7e00c3834801c304ca9babc59bdf08f2a
Chris Garaffa <chris@aghstrategies.com>
2026-09-17 09:42:21 -0400

    Fix cancel redirects where querystrings are used

--- CRM/Core/Invoke.php
+++ CRM/Core/Invoke.php
@@ -226,9 +226,9 @@ class CRM_Core_Invoke {
         self::statusCheck($template);
       }
 
-      if (isset($item['return_url'])) {
+      if (!empty($item['return_url'])) {
         $session = CRM_Core_Session::singleton();
-        $args = $item['return_url_args'] ?? 'reset=1';
+        $args = !empty($item['return_url_args']) ? $item['return_url_args'] : 'reset=1';
         $session->pushUserContext(CRM_Utils_System::url($item['return_url'], $args));
       }
 
