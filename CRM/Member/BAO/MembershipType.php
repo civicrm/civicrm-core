d0881f99c4f1c04fe7cfed0c29eb27ab29b05dbb
colemanw <coleman@civicrm.org>
2026-09-23 12:34:35 -0400

    Remove deprecated CRM_Member_BAO_MembershipType::del
    Deprecated in e630b1bdb4 (2023-03-09). No uses were found across universe or core.

--- CRM/Member/BAO/MembershipType.php
+++ CRM/Member/BAO/MembershipType.php
@@ -87,26 +87,6 @@ class CRM_Member_BAO_MembershipType extends CRM_Member_DAO_MembershipType implem
   }
 
   /**
-   * Delete membership Types.
-   *
-   * @param int $membershipTypeId
-   *
-   * @deprecated
-   * @throws CRM_Core_Exception
-   * @return bool
-   */
-  public static function del($membershipTypeId) {
-    CRM_Core_Error::deprecatedFunctionWarning('deleteRecord');
-    try {
-      static::deleteRecord(['id' => $membershipTypeId]);
-      return TRUE;
-    }
-    catch (CRM_Core_Exception $e) {
-      return FALSE;
-    }
-  }
-
-  /**
    * Callback for hook_civicrm_pre().
    * @param \Civi\Core\Event\PreEvent $event
    * @throws CRM_Core_Exception
