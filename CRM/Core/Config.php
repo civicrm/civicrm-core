f723e7fa3a5dc05e64e622f2fd2a021f2cb4bd86
colemanw <coleman@civicrm.org>
2026-09-23 10:58:50 -0400

    Remove deprecated CRM_Core_Config::defaultCurrencySymbol()
    Deprecated in a5dfa653b7 (2019-07-01). No uses were found across universe or core.

--- CRM/Core/Config.php
+++ CRM/Core/Config.php
@@ -456,18 +456,6 @@ class CRM_Core_Config extends CRM_Core_Config_MagicMerge {
   }
 
   /**
-   * @deprecated
-   *
-   * @param string $defaultCurrency
-   *
-   * @return string
-   */
-  public function defaultCurrencySymbol($defaultCurrency = NULL) {
-    CRM_Core_Error::deprecatedFunctionWarning('CRM_Core_BAO_Country::defaultCurrencySymbol');
-    return CRM_Core_BAO_Country::defaultCurrencySymbol($defaultCurrency);
-  }
-
-  /**
    * Resets the singleton, so that the next call to CRM_Core_Config::singleton()
    * reloads completely.
    *
