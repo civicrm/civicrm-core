bdd6cb83c1305891a0e0663b0595984b5f4c0189
colemanw <coleman@civicrm.org>
2026-09-24 16:06:05 -0400

    Remove deprecated CRM_Mailing_BAO_MailingComponent::retrieve
    Deprecated in d7204a65f9 (2023-04-02). No uses were found across universe or core.

--- CRM/Mailing/BAO/MailingComponent.php
+++ CRM/Mailing/BAO/MailingComponent.php
@@ -21,17 +21,6 @@ use Civi\Core\Event\PostEvent;
 class CRM_Mailing_BAO_MailingComponent extends CRM_Mailing_DAO_MailingComponent implements Civi\Core\HookInterface {
 
   /**
-   * @deprecated
-   * @param array $params
-   * @param array $defaults
-   * @return self|null
-   */
-  public static function retrieve($params, &$defaults) {
-    CRM_Core_Error::deprecatedFunctionWarning('API');
-    return self::commonRetrieve(self::class, $params, $defaults);
-  }
-
-  /**
    * @deprecated - this bypasses hooks.
    * @param int $id
    * @param bool $is_active
