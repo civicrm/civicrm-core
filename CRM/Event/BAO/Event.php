33a581a718eb3d7df4b97cfbe150e41e4c670ed7
colemanw <coleman@civicrm.org>
2026-09-24 16:05:38 -0400

    Remove deprecated CRM_Event_BAO_Event::setIsActive
    Deprecated in e9d6557cff (2023-04-01). No uses were found across universe or core.

--- CRM/Event/BAO/Event.php
+++ CRM/Event/BAO/Event.php
@@ -30,17 +30,6 @@ class CRM_Event_BAO_Event extends CRM_Event_DAO_Event implements \Civi\Core\Hook
   }
 
   /**
-   * @deprecated - this bypasses hooks.
-   * @param int $id
-   * @param bool $is_active
-   * @return bool
-   */
-  public static function setIsActive($id, $is_active) {
-    CRM_Core_Error::deprecatedFunctionWarning('writeRecord');
-    return CRM_Core_DAO::setFieldValue('CRM_Event_DAO_Event', $id, 'is_active', $is_active);
-  }
-
-  /**
    * Add the event.
    *
    * @param array $params
