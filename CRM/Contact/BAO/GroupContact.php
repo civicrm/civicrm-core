41af8e3c3a9abe2a78b76a5c2fd0a55a60191bed
colemanw <coleman@civicrm.org>
2026-09-24 14:55:25 -0400

    Remove deprecated CRM_Contact_BAO_GroupContact::add Deprecated in 15a26c0f0f (2023-04-03). No uses were found across universe or core.

--- CRM/Contact/BAO/GroupContact.php
+++ CRM/Contact/BAO/GroupContact.php
@@ -23,21 +23,6 @@ class CRM_Contact_BAO_GroupContact extends CRM_Contact_DAO_GroupContact implemen
   use CRM_Contact_AccessTrait;
 
   /**
-   * Deprecated add function
-   *
-   * @param array $params
-   *
-   * @return CRM_Contact_DAO_GroupContact
-   * @throws \CRM_Core_Exception
-   *
-   * @deprecated
-   */
-  public static function add(array $params): CRM_Contact_DAO_GroupContact {
-    CRM_Core_Error::deprecatedFunctionWarning('writeRecord');
-    return self::writeRecord($params);
-  }
-
-  /**
    * Callback for hook_civicrm_post().
    *
    * @param \Civi\Core\Event\PostEvent $event
